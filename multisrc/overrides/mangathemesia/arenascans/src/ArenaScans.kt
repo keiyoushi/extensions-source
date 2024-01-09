c684c0692fd5ff1c7d31ac73136e9306c29eb2e2
FourTOne5 <107297513+FourTOne5@users.noreply.github.com>
2024-01-09 12:58:41 +0600

    Remove arena scans
    Closes  #6

--- multisrc/overrides/mangathemesia/arenascans/src/ArenaScans.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.arenascans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class ArenaScans : MangaThemesia("Arena Scans", "https://arenascans.net", "en") {
-    override val pageSelector = "div#readerarea img[data-src]"
-}
