a075482f83459a1e175372723fed82b545caa178
FourTOne5 <107297513+FourTOne5@users.noreply.github.com>
2024-01-09 13:15:51 +0600

    Remove Chibi Manga
    Closes #7

--- multisrc/overrides/madara/chibimanga/src/ChibiManga.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.chibimanga
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class ChibiManga : Madara("Chibi Manga", "https://www.cmreader.info", "en", dateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.US))
