3b7ce548b606f50be00aaa2be52f0869a8f89c24
CodeSpoof <42744253+CodeSpoof@users.noreply.github.com>
2024-01-10 12:19:40 +0100

    Delete src/ja/nicomanga/src/eu/kanade/tachiyomi/extension/ja/nicomanga/NMRegex.kt

--- src/ja/nicomanga/src/eu/kanade/tachiyomi/extension/ja/nicomanga/NMRegex.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.ja.nicomanga
-
-object NMRegex {
-    val thumbnailURLRegex: Regex = "background-image:[^;]url\\s*\\(\\s*'([^?']+)".toRegex()
-    val statusRegex: Regex = "(?<=-)[^.]+".toRegex()
-    val urlRegex: Regex = "(?<=manga-)[^/]+(?=\\.html\$)".toRegex()
-    val floatRegex: Regex = "\\d+(?:\\.\\d+)?".toRegex()
-    val chapterIdRegex: Regex = "(?<=imgsListchap\\()\\d+".toRegex()
-}
