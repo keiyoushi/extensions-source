f833ee93cdcb9a8cafceffe524d0594fc1fce679
beerpiss <lacvtg.a1.2023@gmail.com>
2024-01-13 02:05:27 +0700

    move-apks: Fix APK path

--- .github/scripts/move-apks.py
+++ .github/scripts/move-apks.py
@@ -10,7 +10,7 @@ except FileNotFoundError:
 
 REPO_APK_DIR.mkdir(parents=True, exist_ok=True)
 
-for apk in Path("src").glob("**/*.apk"):
+for apk in (Path.home() / "apk-artifacts").glob("**/*.apk"):
     apk_name = apk.name.replace("-release.apk", ".apk")
 
     shutil.move(apk, REPO_APK_DIR / apk_name)
