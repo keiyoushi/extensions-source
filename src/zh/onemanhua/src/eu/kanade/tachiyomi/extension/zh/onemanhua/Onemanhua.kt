342216581d672a6e20de97fb3774bc29ea80ec52
uy_sun <hmy0119@gmail.com>
2024-01-15 01:58:51 +0800

    Use UriPartFilter

--- src/zh/onemanhua/src/eu/kanade/tachiyomi/extension/zh/onemanhua/Onemanhua.kt
+++ src/zh/onemanhua/src/eu/kanade/tachiyomi/extension/zh/onemanhua/Onemanhua.kt
@@ -122,10 +122,87 @@ class Onemanhua : ConfigurableSource, ParsedHttpSource() {
     override fun latestUpdatesFromElement(element: Element) = commonMangaFromElement(element)
 
     // Filter
+    open class UriPartFilter(displayName: String, private val vals: Array<Pair<String, String>>) :
+        Filter.Select<String>(displayName, vals.map { it.first }.toTypedArray()) {
+        fun toUriPart() = vals[state].second
+    }
     private class StatusFilter : Filter.TriState("已完结")
-    private class SortFilter : Filter.Select<String>("排序", arrayOf("更新日", "收录日", "日点击", "月点击"), 2)
-    private class CategoryFilter : Filter.Select<String>("类型", arrayOf("全部", "热血", "玄幻", "恋爱", "冒险", "古风", "都市", "穿越", "奇幻", "其他", "少男", "搞笑", "战斗", "冒险热血", "重生", "爆笑", "逆袭", "后宫", "少年", "少女", "熱血", "系统", "动作", "校园", "冒險", "修真", "修仙", "剧情", "霸总", "大女主", "生活"), 0)
-    private class CharFilter : Filter.Select<String>("字母", arrayOf("全部", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"), 0)
+    private class SortFilter : UriPartFilter(
+        "排序",
+        arrayOf(
+            Pair("更新日", "update"),
+            Pair("日点击", "dailyCount"),
+            Pair("周点击", "weeklyCount"),
+            Pair("月点击", "monthlyCount"),
+        ),
+    )private class CategoryFilter : UriPartFilter(
+        "类型",
+        arrayOf(
+            Pair("全部", ""),
+            Pair("热血", "10023"),
+            Pair("玄幻", "10024"),
+            Pair("恋爱", "10126"),
+            Pair("冒险", "10210"),
+            Pair("古风", "10143"),
+            Pair("都市", "10124"),
+            Pair("穿越", "10129"),
+            Pair("奇幻", "10242"),
+            Pair("其他", "10560"),
+            Pair("少男", "10641"),
+            Pair("搞笑", "10122"),
+            Pair("战斗", "10309"),
+            Pair("冒险热血", "11224"),
+            Pair("重生", "10461"),
+            Pair("爆笑", "10201"),
+            Pair("逆袭", "10943"),
+            Pair("后宫", "10138"),
+            Pair("少年", "10321"),
+            Pair("少女", "10301"),
+            Pair("熱血", "12044"),
+            Pair("系统", "10722"),
+            Pair("动作", "10125"),
+            Pair("校园", "10131"),
+            Pair("冒險", "12123"),
+            Pair("修真", "10133"),
+            Pair("修仙", "10453"),
+            Pair("剧情", "10480"),
+            Pair("霸总", "10127"),
+            Pair("大女主", "10706"),
+            Pair("生活", "10142"),
+        ),
+    )
+    private class CharFilter : UriPartFilter(
+        "字母",
+        arrayOf(
+            Pair("全部", ""),
+            Pair("A", "10182"),
+            Pair("B", "10081"),
+            Pair("C", "10134"),
+            Pair("D", "10001"),
+            Pair("E", "10238"),
+            Pair("F", "10161"),
+            Pair("G", "10225"),
+            Pair("H", "10137"),
+            Pair("I", "10284"),
+            Pair("J", "10141"),
+            Pair("K", "10283"),
+            Pair("L", "10132"),
+            Pair("M", "10136"),
+            Pair("N", "10130"),
+            Pair("O", "10282"),
+            Pair("P", "10262"),
+            Pair("Q", "10164"),
+            Pair("R", "10240"),
+            Pair("S", "10121"),
+            Pair("T", "10123"),
+            Pair("U", "11184"),
+            Pair("V", "11483"),
+            Pair("W", "10135"),
+            Pair("X", "10061"),
+            Pair("Y", "10082"),
+            Pair("Z", "10128"),
+        ),
+    )
     override fun getFilterList() = FilterList(
         SortFilter(),
         CategoryFilter(),
@@ -149,16 +226,16 @@ class Onemanhua : ConfigurableSource, ParsedHttpSource() {
                         }
                     }
                     is SortFilter -> {
-                        url.addQueryParameter("orderBy", arrayOf("update", "create", "dailyCount", "weeklyCount", "monthlyCount")[filter.state])
+                        url.addQueryParameter("orderBy", filter.toUriPart())
                     }
                     is CategoryFilter -> {
                         if (filter.state != 0) {
-                            url.addQueryParameter("mainCategoryId", arrayOf("", "10023", "10024", "10126", "10210", "10143", "10124", "10129", "10242", "10560", "10641", "10122", "10309", "11224", "10461", "10201", "10943", "10138", "10321", "10301", "12044", "10722", "10125", "10131", "12123", "10133", "10453", "10480", "10127", "10706", "10142")[filter.state])
+                            url.addQueryParameter("mainCategoryId", filter.toUriPart())
                         }
                     }
                     is CharFilter -> {
                         if (filter.state != 0) {
-                            url.addQueryParameter("charCategoryId", arrayOf("", "10182", "10081", "10134", "10001", "10238", "10161", "10225", "10137", "10284", "10141", "10283", "10132", "10136", "10130", "10282", "10262", "10164", "10240", "10121", "10123", "11184", "11483", "10135", "10061", "10082", "10128")[filter.state])
+                            url.addQueryParameter("charCategoryId", filter.toUriPart())
                         }
                     }
                     else -> {}
