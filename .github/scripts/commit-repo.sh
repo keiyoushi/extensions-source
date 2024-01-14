a6599ba0d5fbf1584fdf0d8faa41ce6cae185be8
beerpiss <lacvtg.a1.2023@gmail.com>
2024-01-15 03:34:12 +0700

    eh

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 set -e
 
-MAIN_REVISION="$(git -C ../main rev-parse HEAD)"
-
 rsync -a --delete --exclude .git --exclude .gitignore --exclude README.md ../main/repo/ .
 git config --global user.email "156378334+keiyoushi-bot@users.noreply.github.com"
 git config --global user.name "keiyoushi-bot"
 git status
 if [ -n "$(git status --porcelain)" ]; then
-    cat > .git/COMMIT_EDITMSG << EOF
-Update extensions repo
-
-keiyoushi/extensions-source#$MAIN_REVISION
-EOF
-
     git add .
-    git commit
+    git commit -m "Update extensions repo"
     git push
 
     curl https://purge.jsdelivr.net/gh/keiyoushi/extensions@repo/index.min.json
