e3e89e0254559951fbbc8ea011558ca8e8d5b2a8
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-01-13 22:37:40 -0300

    Removing unnecessary override method

--- multisrc/overrides/madara/nexoscans/src/NexoScans.kt
+++ multisrc/overrides/madara/nexoscans/src/NexoScans.kt
@@ -14,7 +14,4 @@ class NexoScans : Madara(
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(1, 2, java.util.concurrent.TimeUnit.SECONDS)
         .build()
-
-    override val mangaDetailsSelectorDescription: String =
-        "div.description-summary div.summary__content h3 + p, div.description-summary div.summary__content:not(:has(h3)), div.summary_content div.post-content_item > h5 + div, div.summary_content div.manga-excerpt"
 }
