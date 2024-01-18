c9b56f4aa8cdd8f6192587eba1758ea122aeb94a
NotBlankyu <66680364+NotBlankyu@users.noreply.github.com>
2024-01-18 18:18:47 +0000

    Add hunters scans

---
+++ multisrc/overrides/madara/huntersscans/src/HuntersScans.kt
@@ -0,0 +1,59 @@
+package eu.kanade.tachiyomi.extension.pt.huntersscans
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.network.POST
+import okhttp3.OkHttpClient
+import okhttp3.FormBody
+import okhttp3.Request
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class HuntersScans : Madara(
+    "Hunters Scan",
+    "https://huntersscan.xyz/",
+    "pt-BR",
+    SimpleDateFormat("dd 'de' MMMMM 'de' yyyy", Locale("pt", "BR")),
+) {
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(1, 2, TimeUnit.SECONDS)
+        .build()
+
+    override val useNewChapterEndpoint = true
+
+    private fun loadMoreRequest(page: Int, metaKey: String): Request {
+        val formBody = FormBody.Builder().apply {
+            add("action", "madara_load_more")
+            add("page", page.toString())
+            add("template", "madara-core/content/content-archive")
+            add("vars[paged]", "1")
+            add("vars[orderby]", "meta_value_num")
+            add("vars[template]", "archive")
+            add("vars[sidebar]", "right")
+            add("vars[post_type]", "wp-manga")
+            add("vars[post_status]", "publish")
+            add("vars[meta_key]", metaKey)
+            add("vars[order]", "desc")
+            add("vars[meta_query][relation]", "AND")
+            add("vars[manga_archives_item_layout]", "default")
+        }.build()
+
+        val xhrHeaders = headersBuilder()
+            .add("Content-Length", formBody.contentLength().toString())
+            .add("Content-Type", formBody.contentType().toString())
+            .add("X-Requested-With", "XMLHttpRequest")
+            .build()
+
+        return POST("$baseUrl/wp-admin/admin-ajax.php", xhrHeaders, formBody)
+    }
+
+    override fun popularMangaRequest(page: Int): Request {
+        return loadMoreRequest(page - 1, "_wp_manga_views")
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request {
+        return loadMoreRequest(page - 1, "_latest_update")
+    }
+}
