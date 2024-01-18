e96882bdf34533fc1be8d4345994d3cc83a5d909
bapeey <90949336+bapeey@users.noreply.github.com>
2024-01-17 20:00:21 -0500

    Fix package name

--- multisrc/overrides/madara/tmomanga/src/TMOManga.kt
+++ multisrc/overrides/madara/tmomanga/src/TMOManga.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.es.atlantisscan
+package eu.kanade.tachiyomi.extension.es.tmomanga
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.POST
