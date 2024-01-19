7585d41e2a0496bec8b13a0533dab1f5b920d8c0
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-01-19 19:15:06 +0500

    Flix: update api (#384)
    * Flix multisrc: update api
    
    * fix search

--- multisrc/overrides/flixscans/flixscans/src/FlixScansNet.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.flixscans
-
-import eu.kanade.tachiyomi.multisrc.flixscans.FlixScans
-
-class FlixScansNet : FlixScans("Flix Scans", "https://flixscans.org", "en", cdnUrl = "https://media.flixscans.org/")
