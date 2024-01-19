ed87b35777aa75aded5dbb484471204be8e77a3c
KirinRaikage <manmitsu@hotmail.fr>
2024-01-19 17:31:43 +0100

    Manga-Scan: Fix manga name with useless words

--- multisrc/overrides/mmrcms/mangascan/src/MangaScan.kt
+++ multisrc/overrides/mmrcms/mangascan/src/MangaScan.kt
@@ -6,6 +6,13 @@ import eu.kanade.tachiyomi.source.model.Page
 import okhttp3.Request
 
 class MangaScan : MMRCMS("Manga-Scan", "https://mangascan-fr.com", "fr") {
+
+    override fun mangaDetailsParse(response: Response): SManga {
+        return super.mangaDetailsParse(response).apply {
+            title = title.substringBefore("Chapitres en ligne").substringAfter("Scan").trim()
+        }
+    }
+
     override fun imageRequest(page: Page): Request {
         val newHeaders = headersBuilder()
             .set("Referer", baseUrl)
