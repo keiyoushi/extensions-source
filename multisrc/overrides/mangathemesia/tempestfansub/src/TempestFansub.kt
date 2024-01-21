b11c4573aa832ea1878ebdb199eb2e59b646fdbc
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-01-21 12:04:46 -0300

    fix: Fix manga status

--- multisrc/overrides/mangathemesia/tempestfansub/src/TempestFansub.kt
+++ multisrc/overrides/mangathemesia/tempestfansub/src/TempestFansub.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.extension.tr.tempestfansub
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import eu.kanade.tachiyomi.source.model.SManga
 import okhttp3.HttpUrl.Companion.toHttpUrl
 
 class TempestFansub : MangaThemesia(
@@ -16,4 +17,11 @@ class TempestFansub : MangaThemesia(
     // =========================== Manga Details ============================
     override val seriesArtistSelector = ".tsinfo .imptdt:contains(İllüstratör) i"
     override val seriesAuthorSelector = ".tsinfo .imptdt:contains(Yazar) i"
+    override val seriesStatusSelector = ".tsinfo .imptdt:contains(Seri Durumu) i"
+
+    override fun String?.parseStatus(): Int = when (this?.trim()?.lowercase()) {
+        "devam ediyor" -> SManga.ONGOING
+        "bitti" -> SManga.COMPLETED
+        else -> SManga.UNKNOWN
+    }
 }
