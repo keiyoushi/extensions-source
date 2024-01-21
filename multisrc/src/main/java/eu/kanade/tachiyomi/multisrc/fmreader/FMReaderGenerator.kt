dc1c8cbec96b61cc095d38b7c072466d315eb62a
beerpsi <92439990+beerpiss@users.noreply.github.com>
2024-01-21 13:39:15 +0700

    ManhuaRock: FMReader to individual ext (#433)
    * ManhuaRock: FMReader to individual ext
    
    * Add trailing newline
    
    * fix: replace hardcoded base URL

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/fmreader/FMReaderGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/fmreader/FMReaderGenerator.kt
@@ -15,7 +15,6 @@ class FMReaderGenerator : ThemeSourceGenerator {
         SingleLang("Epik Manga", "https://www.epikmanga.com", "tr"),
         SingleLang("KissLove", "https://klz9.com", "ja", isNsfw = true, overrideVersionCode = 4),
         SingleLang("Manga-TR", "https://manga-tr.com", "tr", className = "MangaTR", overrideVersionCode = 3),
-        SingleLang("ManhuaRock", "https://manhuarock.net", "vi", overrideVersionCode = 1),
         SingleLang("Say Truyen", "https://saytruyenvip.com", "vi", overrideVersionCode = 3),
         SingleLang("WeLoveManga", "https://weloma.art", "ja", pkgName = "rawlh", isNsfw = true, overrideVersionCode = 5),
         SingleLang("Manga1000", "https://manga1000.top", "ja"),
