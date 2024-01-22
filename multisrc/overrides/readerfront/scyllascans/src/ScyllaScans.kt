b277e78cc3bc67b8490bc98b35a1075bd1df3f97
ObserverOfTime <chronobserver@disroot.org>
2024-01-22 11:01:17 +0200

    Remove some of my extensions

--- multisrc/overrides/readerfront/scyllascans/src/ScyllaScans.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.scyllascans
-
-import eu.kanade.tachiyomi.multisrc.readerfront.ReaderFront
-
-class ScyllaScans : ReaderFront("Scylla Scans", "https://scyllascans.org", "en") {
-    override fun getImageCDN(path: String, width: Int) =
-        "https://i${(0..2).random()}.wp.com/api.scyllascans.org" +
-            "$path?strip=all&quality=100&w=$width"
-}
