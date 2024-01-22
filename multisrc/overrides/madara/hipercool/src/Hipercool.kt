7740170b7a34312b23137df20541d41d5e93ff8a
Deivid Gabriel Pereira de Oliveira <75938421+deividgabrielpeira@users.noreply.github.com>
2024-01-22 01:02:17 -0300

    Daily Update Portugues (#447)
    * Hipercool change url
    
    * Fix Url HiperCool
    
    * Arkham Scan -> HuntersScan (Fusion)
    
    * CeriseScans Change Url
    
    * animaregia (Down)
    
    * SssScanlatorHentai (is closed)
    
    * Update multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
    
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>
    
    * Update multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
    
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>
    
    ---------
    
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>

--- multisrc/overrides/madara/hipercool/src/Hipercool.kt
+++ multisrc/overrides/madara/hipercool/src/Hipercool.kt
@@ -5,7 +5,7 @@ import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 
-class Hipercool : Madara("HipercooL", "https://hipercool.xyz", "pt-BR") {
+class Hipercool : Madara("HipercooL", "https://hiper.cool", "pt-BR") {
 
     // Migrated from a custom CMS to Madara.
     override val versionId = 2
