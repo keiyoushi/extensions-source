14b9d8b7fedcd38cd803e73c2232dcd1060cb50c
beerpsi <92439990+beerpiss@users.noreply.github.com>
2024-01-24 00:46:18 +0700

    Remove unused imports

--- multisrc/overrides/madara/adultwebtoon/src/AdultWebtoon.kt
+++ multisrc/overrides/madara/adultwebtoon/src/AdultWebtoon.kt
@@ -1,9 +1,6 @@
 package eu.kanade.tachiyomi.extension.en.adultwebtoon
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
-import okhttp3.CacheControl
-import okhttp3.Request
 
 class AdultWebtoon : Madara("Adult Webtoon", "https://adultwebtoon.com", "en") {
     override val mangaSubString = "adult-webtoon"
