6126224f185149e162d3e577f30de09fcf388d3c
Deivid Gabriel Pereira de Oliveira <75938421+deividgabrielpeira@users.noreply.github.com>
2024-01-23 14:12:38 -0300

    RandomScan to LuraToon (#524)
    * RandomScan to LuraToon
    
    * Fix icons
    
    * Update multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
    
    Co-authored-by: beerpsi <92439990+beerpiss@users.noreply.github.com>
    
    * fix ?
    
    * Update multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
    
    * Update multisrc/overrides/madara/randomscan/src/luratoon.kt
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
    
    * rename
    
    * class name
    
    * fix class name MadaraGenerator
    
    * change name, and class name
    
    * add override val id
    
    ---------
    
    Co-authored-by: beerpsi <92439990+beerpiss@users.noreply.github.com>
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- multisrc/overrides/madara/randomscan/src/LuraToon.kt
+++ multisrc/overrides/madara/randomscan/src/LuraToon.kt
@@ -7,12 +7,14 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
-class RandomScan : Madara(
-    "Random Scan",
-    "https://randomscanlators.net",
+class LuraToon : Madara(
+    "Lura Toon",
+    "https://luratoon.com",
     "pt-BR",
     SimpleDateFormat("MMMMM dd, yyyy", Locale("pt", "BR")),
 ) {
+    // Name changed from Random Scan to Lura Toon and Domain change
+    override val id: Long = 8295593204533043008
 
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(1, 2, TimeUnit.SECONDS)
