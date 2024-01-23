4a982ab40fd3d1ac420ca17218eed0ae4fcc98bf
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-01-23 11:39:06 -0300

    feat: Create HentaiCafe base

---
+++ src/all/hentaicafe/src/eu/kanade/tachiyomi/extension/all/hentaicafe/HentaiCafe.kt
@@ -0,0 +1,129 @@
+package eu.kanade.tachiyomi.extension.all.hentaicafe
+
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.asObservableSuccess
+import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import rx.Observable
+
+class HentaiCafe : ParsedHttpSource() {
+
+    override val name = "Hentai Cafe"
+
+    override val baseUrl = "https://hentaicafe.xxx"
+
+    override val lang = "all"
+
+    override val supportsLatest = false
+
+    override val client by lazy {
+        network.client.newBuilder()
+            .rateLimitHost(baseUrl.toHttpUrl(), 2)
+            .build()
+    }
+
+    // ============================== Popular ===============================
+    override fun popularMangaRequest(page: Int): Request {
+        throw UnsupportedOperationException()
+    }
+
+    override fun popularMangaSelector(): String {
+        throw UnsupportedOperationException()
+    }
+
+    override fun popularMangaFromElement(element: Element): SManga {
+        throw UnsupportedOperationException()
+    }
+
+    override fun popularMangaNextPageSelector(): String? {
+        throw UnsupportedOperationException()
+    }
+
+    // =============================== Latest ===============================
+    override fun latestUpdatesRequest(page: Int): Request {
+        throw UnsupportedOperationException()
+    }
+
+    override fun latestUpdatesSelector(): String {
+        throw UnsupportedOperationException()
+    }
+
+    override fun latestUpdatesFromElement(element: Element): SManga {
+        throw UnsupportedOperationException()
+    }
+
+    override fun latestUpdatesNextPageSelector(): String? {
+        throw UnsupportedOperationException()
+    }
+
+    // =============================== Search ===============================
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> {
+        return if (query.startsWith(PREFIX_SEARCH)) { // URL intent handler
+            val id = query.removePrefix(PREFIX_SEARCH)
+            client.newCall(GET("$baseUrl/g/$id"))
+                .asObservableSuccess()
+                .map(::searchMangaByIdParse)
+        } else {
+            super.fetchSearchManga(page, query, filters)
+        }
+    }
+
+    private fun searchMangaByIdParse(response: Response): MangasPage {
+        val details = mangaDetailsParse(response.use { it.asJsoup() })
+        return MangasPage(listOf(details), false)
+    }
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        throw UnsupportedOperationException()
+    }
+
+    override fun searchMangaSelector(): String {
+        throw UnsupportedOperationException()
+    }
+
+    override fun searchMangaFromElement(element: Element): SManga {
+        throw UnsupportedOperationException()
+    }
+
+    override fun searchMangaNextPageSelector(): String? {
+        throw UnsupportedOperationException()
+    }
+
+    // =========================== Manga Details ============================
+    override fun mangaDetailsParse(document: Document): SManga {
+        throw UnsupportedOperationException()
+    }
+
+    // ============================== Chapters ==============================
+    override fun chapterListSelector(): String {
+        throw UnsupportedOperationException()
+    }
+
+    override fun chapterFromElement(element: Element): SChapter {
+        throw UnsupportedOperationException()
+    }
+
+    // =============================== Pages ================================
+    override fun pageListParse(document: Document): List<Page> {
+        throw UnsupportedOperationException()
+    }
+
+    override fun imageUrlParse(document: Document): String {
+        throw UnsupportedOperationException()
+    }
+
+    companion object {
+        const val PREFIX_SEARCH = "id:"
+    }
+}
