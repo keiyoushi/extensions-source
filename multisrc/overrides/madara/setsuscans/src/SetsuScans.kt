9cb36e3c280fb46beda1135cbf7038c5bc999cd4
Johannes Jöns <34899572+jopejoe1@users.noreply.github.com>
2024-01-24 19:04:13 +0000

    Setsu Scans: Fix some more pages not loading (#590)

--- multisrc/overrides/madara/setsuscans/src/SetsuScans.kt
+++ multisrc/overrides/madara/setsuscans/src/SetsuScans.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.extension.en.setsuscans
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.MangasPage
+import okhttp3.Interceptor
 import okhttp3.Response
 
 class SetsuScans : Madara(
@@ -17,7 +18,6 @@ class SetsuScans : Madara(
             if (url.host == "i0.wp.com") {
                 val newUrl = url.newBuilder()
                     .removeAllQueryParameters("fit")
-                    .removeAllQueryParameters("ssl")
                     .build()
 
                 return@addNetworkInterceptor chain.proceed(
@@ -29,8 +29,35 @@ class SetsuScans : Madara(
 
             return@addNetworkInterceptor chain.proceed(request)
         }
+        .addInterceptor(::handleFailedImages)
         .rateLimit(2)
         .build()
+
+    private fun handleFailedImages(chain: Interceptor.Chain): Response {
+        val response: Response = chain.proceed(chain.request())
+        val url = response.request.url
+        if (url.host == "i0.wp.com" && response.code == 404) {
+            val ssl = response.request.url.queryParameter("ssl")
+            var newUrl = url.newBuilder()
+                .removeAllQueryParameters("ssl")
+
+            if (ssl.isNullOrBlank()) {
+                newUrl = newUrl.addQueryParameter("ssl", "0")
+            } else if (ssl.toInt() >= 5) {
+                return response
+            } else if (ssl.toInt() == 0) {
+                newUrl = newUrl.addQueryParameter("ssl", "2")
+            } else if (ssl.toInt() >= 2) {
+                newUrl = newUrl.addQueryParameter("ssl", (ssl.toInt() + 1).toString())
+            }
+
+            val newRequest = chain.request().newBuilder()
+                .url(newUrl.build())
+                .build()
+            return client.newCall(newRequest).execute()
+        }
+        return response
+    }
 
     override val useNewChapterEndpoint = true
 
