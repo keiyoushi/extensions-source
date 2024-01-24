6bcb2b3d8209530d9369fa8ba76380f33c646abd
jopejoe1 <jopejoe1.de@gmail.com>
2024-01-24 14:06:46 +0100

    Setsu Scans: fix pages loading

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -435,7 +435,7 @@ class MadaraGenerator : ThemeSourceGenerator {
         SingleLang("Scan Hentai Menu", "https://scan.hentai.menu", "fr", isNsfw = true, overrideVersionCode = 1),
         SingleLang("Scantrad-VF", "https://scantrad-vf.co", "fr", className = "ScantradVF"),
         SingleLang("Sdl scans", "https://sdlscans.com", "es", className = "SdlScans"),
-        SingleLang("Setsu Scans", "https://setsuscans.com", "en"),
+        SingleLang("Setsu Scans", "https://setsuscans.com", "en", overrideVersionCode = 1),
         SingleLang("Shadowtrad", "https://shadowtrad.net", "fr"),
         SingleLang("ShavelProiection", "https://www.shavelproiection.com", "it", true),
         SingleLang("Shayami", "https://shayami.com", "es"),
