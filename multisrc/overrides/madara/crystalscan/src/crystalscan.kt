fd8206528d34b61e59b0a883289d61f0c268d9de
Deivid Gabriel Pereira de Oliveira <75938421+deividgabrielpeira@users.noreply.github.com>
2024-01-24 18:26:20 +0000

    Add CrystalScan to madara

---
+++ multisrc/overrides/madara/crystalscan/src/crystalscan.kt
@@ -0,0 +1,23 @@
+package eu.kanade.tachiyomi.extension.pt.crystalscan
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class CrystalScan : Madara(
+    "Crystal Scan",
+    "https://crystalscan.net",
+    "pt-BR",
+    SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")),
+) {
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(2, 1, TimeUnit.SECONDS) // Limite de 2 solicitações por segundo
+        .build()
+
+    override val useNewChapterEndpoint = true
+
+    override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
+}
