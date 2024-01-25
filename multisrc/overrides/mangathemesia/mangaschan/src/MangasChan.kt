19e9c7a16ea7f99cb62d722e927de924ca07e4ef
beerpsi <92439990+beerpiss@users.noreply.github.com>
2024-01-26 01:39:09 +0700

    Mangas Chan: Fix Cloudflare block (#637)
    * Mangas Chan: Fix Cloudflare block
    
    * Add an interceptor to remove X-Requested-With
    
    * use CharArray instead of List
    
    Co-authored-by: stevenyomi <95685115+stevenyomi@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: stevenyomi <95685115+stevenyomi@users.noreply.github.com>

--- multisrc/overrides/mangathemesia/mangaschan/src/MangasChan.kt
+++ multisrc/overrides/mangathemesia/mangaschan/src/MangasChan.kt
@@ -15,12 +15,28 @@ class MangasChan : MangaThemesia(
 ) {
 
     override val client: OkHttpClient = super.client.newBuilder()
+        .addInterceptor { chain ->
+            val request = chain.request()
+            val headers = request.headers.newBuilder()
+                .removeAll("X-Requested-With")
+                .build()
+
+            chain.proceed(request.newBuilder().headers(headers).build())
+        }
         .rateLimit(1, 2, TimeUnit.SECONDS)
         .build()
+
+    override fun headersBuilder() = super.headersBuilder()
+        .set("X-Requested-With", randomString((10..20).random()))
 
     override val altNamePrefix = "Nomes alternativos: "
 
     override val seriesArtistSelector = ".tsinfo .imptdt:contains(Artista) > i"
     override val seriesAuthorSelector = ".tsinfo .imptdt:contains(Autor) > i"
     override val seriesTypeSelector = ".tsinfo .imptdt:contains(Tipo) > a"
+
+    private fun randomString(length: Int): String {
+        val charPool = ('a'..'z') + ('A'..'Z')
+        return CharArray(length) { charPool.random() }.concatToString()
+    }
 }
