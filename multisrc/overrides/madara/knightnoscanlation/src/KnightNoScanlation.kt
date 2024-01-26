88b60005f3557e04fd507275a37559271689ab5f
bapeey <90949336+bapeey@users.noreply.github.com>
2024-01-25 23:02:08 -0500

    Add Knight No Scanlation to Madara (#645)
    * Add KnightNoScanlation
    
    * Add icon
    
    * Missing import
    
    * Fix status

---
+++ multisrc/overrides/madara/knightnoscanlation/src/KnightNoScanlation.kt
@@ -0,0 +1,24 @@
+package eu.kanade.tachiyomi.extension.es.knightnoscanlation
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class KnightNoScanlation : Madara(
+    "Knight No Scanlation",
+    "https://knightnoscanlation.com",
+    "es",
+    SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
+) {
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimitHost(baseUrl.toHttpUrl(), 2, 1, TimeUnit.SECONDS)
+        .build()
+
+    override val mangaSubString = "sr"
+
+    override val mangaDetailsSelectorStatus = "div.post-content_item:contains(Status) div.summary-content"
+}
