914146883e6dc1acf3284f60b4edc5dd681a0ffe
SnakeDoc83 <51273546+SnakeDoc83@users.noreply.github.com>
2024-01-27 01:40:57 -0500

    comma

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/hentaihand/HentaiHandGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/hentaihand/HentaiHandGenerator.kt
@@ -15,7 +15,7 @@ class HentaiHandGenerator : ThemeSourceGenerator {
     override val sources = listOf(
         MultiLang("HentaiHand", "https://hentaihand.com", listOf("all", "ja", "en", "zh", "bg", "ceb", "other", "tl", "ar", "el", "sr", "jv", "uk", "tr", "fi", "la", "mn", "eo", "sk", "cs", "ko", "ru", "it", "es", "pt-BR", "th", "fr", "id", "vi", "de", "pl", "hu", "nl", "hi"), isNsfw = true, overrideVersionCode = 5),
         MultiLang("nHentai.com (unoriginal)", "https://nhentai.com", listOf("all", "ja", "en", "zh", "bg", "ceb", "other", "tl", "ar", "el", "sr", "jv", "uk", "tr", "fi", "la", "mn", "eo", "sk", "cs", "ko", "ru", "it", "es", "pt-BR", "th", "fr", "id", "vi", "de", "pl", "hu", "nl", "hi"), isNsfw = true, className = "NHentaiComFactory", overrideVersionCode = 4),
-        SingleLang("ManhwaClub", "https://manhwa.club", "en", isNsfw = true, overrideVersionCode = 3)
+        SingleLang("ManhwaClub", "https://manhwa.club", "en", isNsfw = true, overrideVersionCode = 3),
     )
 
     companion object {
