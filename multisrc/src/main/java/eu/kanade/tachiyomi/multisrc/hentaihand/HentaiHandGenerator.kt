796f6811349b17825b02ec17a1ee2753d9de79d7
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-01-27 01:48:59 -0500

    Prune HentaiHand (#687)
    * Prune HentaiHand
    
    * comma

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/hentaihand/HentaiHandGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/hentaihand/HentaiHandGenerator.kt
@@ -15,9 +15,7 @@ class HentaiHandGenerator : ThemeSourceGenerator {
     override val sources = listOf(
         MultiLang("HentaiHand", "https://hentaihand.com", listOf("all", "ja", "en", "zh", "bg", "ceb", "other", "tl", "ar", "el", "sr", "jv", "uk", "tr", "fi", "la", "mn", "eo", "sk", "cs", "ko", "ru", "it", "es", "pt-BR", "th", "fr", "id", "vi", "de", "pl", "hu", "nl", "hi"), isNsfw = true, overrideVersionCode = 5),
         MultiLang("nHentai.com (unoriginal)", "https://nhentai.com", listOf("all", "ja", "en", "zh", "bg", "ceb", "other", "tl", "ar", "el", "sr", "jv", "uk", "tr", "fi", "la", "mn", "eo", "sk", "cs", "ko", "ru", "it", "es", "pt-BR", "th", "fr", "id", "vi", "de", "pl", "hu", "nl", "hi"), isNsfw = true, className = "NHentaiComFactory", overrideVersionCode = 4),
-        SingleLang("HentaiSphere", "https://hentaisphere.com", "en", isNsfw = true),
         SingleLang("ManhwaClub", "https://manhwa.club", "en", isNsfw = true, overrideVersionCode = 3),
-        SingleLang("ReadManhwa", "https://readmanhwa.com", "en", isNsfw = true, overrideVersionCode = 10),
     )
 
     companion object {
