d8f12b61f5d6d854778ce6d535328fd2bc6aa288
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-01-28 21:43:25 -0500

    Prune MangaThemesia (#767)

--- multisrc/overrides/mangathemesia/mangaindome/src/MangaIndoMe.kt
+++
@@ -1,8 +0,0 @@
-package eu.kanade.tachiyomi.extension.id.mangaindome
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class MangaIndoMe : MangaThemesia("Manga Indo.me", "https://mangaindo.me", "id") {
-
-    override val hasProjectPage = true
-}
--- multisrc/overrides/mangathemesia/piscans/src/PiScans.kt
+++
@@ -1,8 +0,0 @@
-package eu.kanade.tachiyomi.extension.id.piscans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class PiScans : MangaThemesia("Pi Scans", "https://piscans.in", "id") {
-
-    override val hasProjectPage = true
-}
