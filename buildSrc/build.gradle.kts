6f3861f102a050796cf95960f2c31ef10e9e6c18
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2024-01-30 07:22:53 +0800

    adjustments

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,14 @@ plugins {
 }
 
 repositories {
+    gradlePluginPortal()
     mavenCentral()
+    google()
+}
+
+dependencies {
+    implementation(libs.gradle.agp)
+    implementation(libs.gradle.kotlin)
+    implementation(libs.gradle.serialization)
+    implementation(libs.gradle.kotlinter)
 }
--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/webtoons/WebtoonsGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/webtoons/WebtoonsGenerator.kt
@@ -15,6 +15,7 @@ class WebtoonsGenerator : ThemeSourceGenerator {
     override val sources = listOf(
         MultiLang("Webtoons.com", "https://www.webtoons.com", listOf("en", "fr", "es", "id", "th", "zh-Hant", "de"), className = "WebtoonsFactory", pkgName = "webtoons", overrideVersionCode = 39),
         SingleLang("Dongman Manhua", "https://www.dongmanmanhua.cn", "zh"),
+        MultiLang("Webtoons.com Translations", "https://translate.webtoons.com", listOf("en", "zh-Hans", "zh-Hant", "th", "id", "fr", "vi", "ru", "ar", "fil", "de", "hi", "it", "ja", "pt-BR", "tr", "ms", "pl", "pt", "bg", "da", "nl", "ro", "mn", "el", "lt", "cs", "sv", "bn", "fa", "uk", "es"), className = "WebtoonsTranslateFactory", pkgName = "webtoonstranslate", overrideVersionCode = 4),
     )
 
     companion object {
