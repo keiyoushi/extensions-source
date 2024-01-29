f5c462456a0437ec8785da175e413bd652e67d1e
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-01-28 21:43:25 -0500

    Prune MangaThemesia (#767)

--- multisrc/overrides/mangathemesia/nocturnalscans/src/NocturnalScans.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.nocturnalscans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class NocturnalScans : MangaThemesia("Nocturnal Scans", "https://nocturnalscans.com", "en", "/MANGA") {
-    override val hasProjectPage = true
-}
