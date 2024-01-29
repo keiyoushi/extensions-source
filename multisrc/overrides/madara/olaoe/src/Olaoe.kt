1333225cfa649833326c85dc692003fb1946d3c5
mohamedotaku <mohamed.otaku.2016@gmail.com>
2024-01-29 18:59:49 +0100

    Add source ar.Olaoe

---
+++ multisrc/overrides/madara/olaoe/src/Olaoe.kt
@@ -0,0 +1,22 @@
+package eu.kanade.tachiyomi.extension.ar.olaoe
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.SChapter
+import okhttp3.Request
+import org.jsoup.nodes.Element
+
+class Olaoe : Madara("Olaoe", "https://olaoe.cyou", "ar") {
+    override val mangaSubString = "works"
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/page/$page/?m_orderby=views", headers)
+    override fun chapterListSelector() = "li.wp-manga-chapter:not(.premium-block)" // Filter fake chapters
+    override fun chapterFromElement(element: Element): SChapter {
+        val chapter = SChapter.create()
+
+        element.select("a").let {
+            chapter.url = it.attr("href").substringAfter(baseUrl)
+            chapter.name = it.text()
+        }
+        return chapter
+    }
+}
