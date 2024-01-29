587b571422c41d183a60f0b300b4588e3a49f677
beerpsi <92439990+beerpiss@users.noreply.github.com>
2024-01-29 19:35:02 +0700

    Add PeachScan (#759)
    * Add PeachScan
    
    * I love linting
    
    * Also remove old WickedWitchScan
    
    * It's not novo anymore
    
    * Remove unneeded className declaration
    
    * Filter light novel entries from latest view (because we can here)
    
    * ACTUALLY filter light novel entries from latest view (because we can here)
    
    * Fix icon path + add Dango Scan
    
    * Move image-decoder dependency to multisrc project
    
    * shut up android studio
    
    * fix dep specifier in additional.gradle
    
    * Update multisrc/overrides/peachscan/default/additional.gradle
    
    Co-authored-by: stevenyomi <95685115+stevenyomi@users.noreply.github.com>
    
    * Revert "Update multisrc/overrides/peachscan/default/additional.gradle"
    
    This reverts commit 6b8722193a2181a8e9d8f6b65fb24afd30e5a548.
    The comment serves as an explanation for why the dependency shouldn't
    be in the Gradle version catalog; because no one else cares about this
    dependency.
    
    * Optimize icons
    
    ---------
    
    Co-authored-by: stevenyomi <95685115+stevenyomi@users.noreply.github.com>

---
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/peachscan/PeachScanGenerator.kt
@@ -0,0 +1,28 @@
+package eu.kanade.tachiyomi.multisrc.peachscan
+
+import generator.ThemeSourceData.SingleLang
+import generator.ThemeSourceGenerator
+
+class PeachScanGenerator : ThemeSourceGenerator {
+
+    override val themePkg = "peachscan"
+
+    override val themeClass = "PeachScan"
+
+    override val baseVersionCode = 1
+
+    override val sources = listOf(
+        SingleLang("Dango Scan", "https://dangoscan.com.br", "pt-BR"),
+        SingleLang("Mode Scanlator", "https://modescanlator.com", "pt-BR"),
+        SingleLang("Nazarick Scan", "https://nazarickscan.com.br", "pt-BR"),
+        SingleLang("RF Dragon Scan", "https://rfdragonscan.com", "pt-BR"),
+        SingleLang("Wicked Witch Scan", "https://wicked-witch-scan.com", "pt-BR", pkgName = "wickedwitchscannovo", overrideVersionCode = 1),
+    )
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            PeachScanGenerator().createAll()
+        }
+    }
+}
