89a06bc33c31446f5ff06a8bae94765b254918c3
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-01-30 07:04:08 -0300

    fix: Fix chapter dates

--- multisrc/overrides/mangathemesia/mangakings/src/MangaKings.kt
+++ multisrc/overrides/mangathemesia/mangakings/src/MangaKings.kt
@@ -3,8 +3,16 @@ package eu.kanade.tachiyomi.extension.tr.mangakings
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class MangaKings : MangaThemesia("Manga Kings", "https://mangakings.com.tr", "tr") {
+class MangaKings : MangaThemesia(
+    "Manga Kings",
+    "https://mangakings.com.tr",
+    "tr",
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("tr")),
+
+) {
     override val client = network.client.newBuilder()
         .rateLimitHost(baseUrl.toHttpUrl(), 2)
         .build()
