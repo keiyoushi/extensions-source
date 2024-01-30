bc7815ba655135b2aee2e82307af6dc56504ea00
mohamedotaku <mohamed.otaku.2016@gmail.com>
2024-01-30 14:13:55 +0100

    Update Olaoe.kt

--- multisrc/overrides/madara/olaoe/src/Olaoe.kt
+++ multisrc/overrides/madara/olaoe/src/Olaoe.kt
@@ -7,15 +7,7 @@ import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.extension.BuildConfig
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.network.POST
-import eu.kanade.tachiyomi.source.model.FilterList
-import eu.kanade.tachiyomi.source.model.MangasPage
-import eu.kanade.tachiyomi.source.model.SManga
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import okhttp3.FormBody
 import okhttp3.Request
-import okhttp3.Response
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 import java.text.SimpleDateFormat
@@ -36,45 +28,6 @@ class Olaoe : Madara(
     private val preferences: SharedPreferences by lazy {
         Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
     }
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request =
-        POST(
-            "$baseUrl/wp-admin/admin-ajax.php",
-            headers,
-            FormBody.Builder()
-                .add("action", "wp-manga-search-manga")
-                .add("title", query)
-                .build(),
-        )
-    private inline fun <reified T> Response.parseAs(): T {
-        return json.decodeFromString(body.string())
-    }
-    override fun searchMangaParse(response: Response): MangasPage {
-        val dto = response.parseAs<SearchResponseDto>()
-
-        if (!dto.success) {
-            return MangasPage(emptyList(), false)
-        }
-
-        val manga = dto.data.map {
-            SManga.create().apply {
-                setUrlWithoutDomain(it.url)
-                title = it.title
-            }
-        }
-        return MangasPage(manga, false)
-    }
-
-    @Serializable
-    data class SearchResponseDto(
-        val data: List<SearchEntryDto>,
-        val success: Boolean,
-    )
-
-    @Serializable
-    data class SearchEntryDto(
-        val url: String = "",
-        val title: String = "",
-    )
 
     companion object {
         private const val RESTART_TACHIYOMI = ".لتطبيق الإعدادات الجديدة Tachiyomi أعد تشغيل"
