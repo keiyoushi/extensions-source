52da5f004ed966b99a7ed17918c9d5e348480fcd
ghost <ghost@gmail.com>
2024-01-31 04:46:26 +0700

    Mangaforfreecom: trust all certificate

---
+++ multisrc/overrides/madara/mangaforfreecom/src/Mangaforfreecom.kt
@@ -0,0 +1,40 @@
+package eu.kanade.tachiyomi.extension.all.mangaforfreecom
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.Source
+import eu.kanade.tachiyomi.source.SourceFactory
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import android.annotation.SuppressLint
+
+class Mangaforfreecom : Madara("Mangaforfreecom", "https://mangaforfree.com", "en") {
+    override val client = getUnsafeOkHttpClient()
+
+    private fun getUnsafeOkHttpClient(): OkHttpClient {
+        val trustAllCerts = arrayOf<TrustManager>(
+            @SuppressLint("CustomX509TrustManager")
+            object : X509TrustManager {
+                @SuppressLint("TrustAllX509TrustManager")
+                override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+                }
+                @SuppressLint("TrustAllX509TrustManager")
+                override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+                }
+
+                override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
+            },
+        )
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+        val sslSocketFactory = sslContext.socketFactory
+        return OkHttpClient.Builder()
+            .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }.build()
+    }
+}
