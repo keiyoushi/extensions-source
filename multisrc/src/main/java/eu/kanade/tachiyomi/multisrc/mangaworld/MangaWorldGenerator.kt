e802aa481cab489baf494f697e97dc11b8a2f9c5
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-01-30 04:21:13 -0500

    Mangaworld - update domain (#801)

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangaworld/MangaWorldGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangaworld/MangaWorldGenerator.kt
@@ -10,7 +10,7 @@ class MangaWorldGenerator : ThemeSourceGenerator {
     override val baseVersionCode: Int = 2
 
     override val sources = listOf(
-        SingleLang("Mangaworld", "https://www.mangaworld.so", "it", overrideVersionCode = 5),
+        SingleLang("Mangaworld", "https://www.mangaworld.ac", "it", overrideVersionCode = 6),
         SingleLang("MangaworldAdult", "https://www.mangaworldadult.com", "it", isNsfw = true),
     )
 
