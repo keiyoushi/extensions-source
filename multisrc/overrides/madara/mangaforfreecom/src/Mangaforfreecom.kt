bb1b5546472e051e02d43fef631690a6aee82757
Sofie <117321707+Sofie99@users.noreply.github.com>
2024-01-31 05:50:30 +0700

    comply

--- multisrc/overrides/madara/mangaforfreecom/src/Mangaforfreecom.kt
+++ multisrc/overrides/madara/mangaforfreecom/src/Mangaforfreecom.kt
@@ -1,40 +1,44 @@
 package eu.kanade.tachiyomi.extension.all.mangaforfreecom
 
+import android.annotation.SuppressLint
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.source.Source
-import eu.kanade.tachiyomi.source.SourceFactory
 import okhttp3.OkHttpClient
-import java.util.concurrent.TimeUnit
 import java.security.cert.X509Certificate
 import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManager
 import javax.net.ssl.X509TrustManager
-import android.annotation.SuppressLint
 
 class Mangaforfreecom : Madara("Mangaforfree.com", "https://mangaforfree.com", "en") {
     override val client = getUnsafeOkHttpClient()
 
     private fun getUnsafeOkHttpClient(): OkHttpClient {
-        val trustAllCerts = arrayOf<TrustManager>(
-            @SuppressLint("CustomX509TrustManager")
-            object : X509TrustManager {
-                @SuppressLint("TrustAllX509TrustManager")
-                override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
-                }
-                @SuppressLint("TrustAllX509TrustManager")
-                override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
-                }
+        val trustAllCerts =
+            arrayOf<TrustManager>(
+                @SuppressLint("CustomX509TrustManager")
+                object : X509TrustManager {
+                    @SuppressLint("TrustAllX509TrustManager")
+                    override fun checkClientTrusted(
+                        chain: Array<out X509Certificate>?,
+                        authType: String?,
+                    ) {
+                    }
 
-                override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
-            },
-        )
+                    @SuppressLint("TrustAllX509TrustManager")
+                    override fun checkServerTrusted(
+                        chain: Array<out X509Certificate>?,
+                        authType: String?,
+                    ) {
+                    }
+
+                    override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
+                },
+            )
 
         val sslContext = SSLContext.getInstance("SSL")
         sslContext.init(null, trustAllCerts, java.security.SecureRandom())
         val sslSocketFactory = sslContext.socketFactory
-        return OkHttpClient.Builder()
+        return super.client.newBuilder()
             .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
             .hostnameVerifier { _, _ -> true }.build()
     }
-
 }
