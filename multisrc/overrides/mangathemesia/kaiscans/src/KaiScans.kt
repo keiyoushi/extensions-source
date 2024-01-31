f65bf6f8412868edd5940fdb1a04da50f7b11292
Karanam Sandeep <sandeepjak2007@gmail.com>
2024-01-31 22:22:59 +0530

    Issue #855 Kai Scans domain change
    Kai Scans domain changed

--- multisrc/overrides/mangathemesia/kaiscans/src/KaiScans.kt
+++ multisrc/overrides/mangathemesia/kaiscans/src/KaiScans.kt
@@ -2,4 +2,4 @@ package eu.kanade.tachiyomi.extension.en.kaiscans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 
-class KaiScans : MangaThemesia("Kai Scans", "https://www.kaiscans.com", "en", mangaUrlDirectory = "/series")
+class KaiScans : MangaThemesia("Kai Scans", "https://kaiscans.org", "en", mangaUrlDirectory = "/series")
