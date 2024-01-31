b6119630fb9e67e3d7d3e263c1df961483b41048
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-01-30 22:54:23 -0500

    Madara - update 3 domains (#833)

--- multisrc/overrides/madara/zinchanmanga/src/ZinChanManga.kt
+++ multisrc/overrides/madara/zinchanmanga/src/ZinChanManga.kt
@@ -2,6 +2,6 @@ package eu.kanade.tachiyomi.extension.en.zinchanmanga
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class ZinChanManga : Madara("ZinChanManga", "https://zinchanmanga.com", "en") {
+class ZinChanManga : Madara("ZinChanManga", "https://zinchanmanga.net", "en") {
     override val versionId = 2
 }
