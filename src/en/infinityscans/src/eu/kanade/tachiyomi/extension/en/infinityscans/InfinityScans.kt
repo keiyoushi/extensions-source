b0212d5d9fe360ff9e52e78dfc4e140d4d96b2f6
Secozzi <49240133+Secozzi@users.noreply.github.com>
2024-01-31 01:22:32 +0000

    Infinityscans, try to solve cloudflare turnstile in webview (#821)
    solve cloudflare turnstile in webview

--- src/en/infinityscans/src/eu/kanade/tachiyomi/extension/en/infinityscans/InfinityScans.kt
+++ src/en/infinityscans/src/eu/kanade/tachiyomi/extension/en/infinityscans/InfinityScans.kt
@@ -23,7 +23,6 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import org.jsoup.nodes.Element
 import uy.kohesive.injekt.injectLazy
-import java.io.IOException
 
 class InfinityScans : HttpSource() {
 
@@ -37,14 +36,7 @@ class InfinityScans : HttpSource() {
     override val supportsLatest = true
 
     override val client: OkHttpClient = network.cloudflareClient.newBuilder()
-        .addInterceptor { chain ->
-            val response = chain.proceed(chain.request())
-            if (response.code == 401) {
-                response.close()
-                throw IOException("Solve Captcha in WebView")
-            }
-            response
-        }
+        .addInterceptor(WebviewInterceptor(baseUrl))
         .rateLimit(1)
         .build()
 
