3028754de42a010f8fdaaa968a742cac8ecfd05d
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-01-30 21:59:52 -0500

    Prune Madara part 1 (#817)

--- multisrc/overrides/madara/bluesolo/src/BlueSolo.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.bluesolo
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class BlueSolo : Madara("Blue Solo", "https://www1.bluesolo.org", "fr", dateFormat = SimpleDateFormat("d MMMM yyyy", Locale.FRENCH)) {
-    override val mangaDetailsSelectorStatus = "div.summary-heading:contains(Statut) + .summary-content"
-}
