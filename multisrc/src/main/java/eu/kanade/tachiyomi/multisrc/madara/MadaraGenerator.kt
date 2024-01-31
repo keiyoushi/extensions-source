e6f340371123a8306f1d09c14976d1b30e61841e
SnakeDoc83 <51273546+SnakeDoc83@users.noreply.github.com>
2024-01-31 10:18:56 -0500

    remove version bump

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -136,7 +136,7 @@ class MadaraGenerator : ThemeSourceGenerator {
         SingleLang("HM2D", "https://mangadistrict.com/hdoujin", "en", isNsfw = true, overrideVersionCode = 1),
         SingleLang("HouseMangas", "https://housemangas.com", "es"),
         SingleLang("Hreads", "https://hreads.net", "en", isNsfw = true, overrideVersionCode = 1),
-        SingleLang("Hunters Scans", "https://huntersscan.xyz", "pt-BR", isNsfw = true, overrideVersionCode = 1),
+        SingleLang("Hunters Scans", "https://huntersscan.xyz", "pt-BR", isNsfw = true),
         SingleLang("I Love Manhwa", "https://ilovemanhwa.com", "en", isNsfw = true),
         SingleLang("Illusion Scan", "https://illusionscan.com", "pt-BR", isNsfw = true),
         SingleLang("Immortal Updates", "https://immortalupdates.com", "en", overrideVersionCode = 6),
