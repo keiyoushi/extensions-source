2a3a1282ebd007a9917128f9e6fe3993638a9bec
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-01-30 21:59:52 -0500

    Prune Madara part 1 (#817)

--- multisrc/overrides/madara/firstkissmangablog/src/FirstKissMangaBlog.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.firstkissmangablog
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class FirstKissMangaBlog : Madara("1stKissManga.blog", "https://1stkissmanga.blog", "en") {
-    override val useNewChapterEndpoint = false
-
-    override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
-}
