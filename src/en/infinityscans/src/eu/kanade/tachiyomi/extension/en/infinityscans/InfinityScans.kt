fc121230510c896b0cf6b44c52d6ef22e762ffd6
Secozzi <49240133+Secozzi@users.noreply.github.com>
2024-02-02 22:10:56 +0000

    update infinityscans url (#940)

--- src/en/infinityscans/src/eu/kanade/tachiyomi/extension/en/infinityscans/InfinityScans.kt
+++ src/en/infinityscans/src/eu/kanade/tachiyomi/extension/en/infinityscans/InfinityScans.kt
@@ -28,8 +28,8 @@ class InfinityScans : HttpSource() {
 
     override val name = "InfinityScans"
 
-    override val baseUrl = "https://infinityscans.xyz"
-    private val cdnHost = "cdn.${baseUrl.toHttpUrl().host}"
+    override val baseUrl = "https://infinityscans.net"
+    private val cdnHost = "cdn.infinityscans.xyz"
 
     override val lang = "en"
 
