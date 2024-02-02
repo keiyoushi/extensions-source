7131b59831e4c69cec4c3b58526ba56dd58d0bad
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-02-02 05:07:49 -0500

    Prune Madara part 3 (#916)

--- multisrc/overrides/madara/mangakakalotone/src/MangakakalotOne.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangakakalotone
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class MangakakalotOne : Madara("Mangakakalot.one (unoriginal)", "https://mangakakalot.one", "en") {
-    override val useNewChapterEndpoint = true
-
-    override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
-}
