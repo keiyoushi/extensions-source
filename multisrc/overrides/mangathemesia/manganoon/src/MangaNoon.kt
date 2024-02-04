388ab9fe6612c6f90a4c58d5017d4828210523f7
98Hani <158824396+98Hani@users.noreply.github.com>
2024-02-04 18:02:54 +0300

    Update MangaNoon.kt

--- multisrc/overrides/mangathemesia/manganoon/src/MangaNoon.kt
+++ multisrc/overrides/mangathemesia/manganoon/src/MangaNoon.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 
 class MangaNoon : MangaThemesia(
     "مانجا نون",
-    "https://manjanoon.com",
+    "https://manjanoon.net",
     "ar",
     dateFormat = SimpleDateFormat("MMM d, yyy", Locale("ar")),
 )
