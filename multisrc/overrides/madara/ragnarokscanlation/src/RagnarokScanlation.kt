39e9b61793604ddaa816f94c1a81508b337184ef
bapeey <90949336+bapeey@users.noreply.github.com>
2024-02-04 08:17:31 -0500

    Update domain

--- multisrc/overrides/madara/ragnarokscanlation/src/RagnarokScanlation.kt
+++ multisrc/overrides/madara/ragnarokscanlation/src/RagnarokScanlation.kt
@@ -4,4 +4,4 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class RagnarokScanlation : Madara("Ragnarok Scanlation", "https://ragnarokscanlation.com", "es", SimpleDateFormat("MMMM d, yyyy", Locale("es")))
+class RagnarokScanlation : Madara("Ragnarok Scanlation", "https://ragnarokscanlation.net", "es", SimpleDateFormat("MMMM d, yyyy", Locale("es")))
