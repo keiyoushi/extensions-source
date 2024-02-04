edcb6490d454fa971250f325f429ef7b1abc78f4
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-02-03 22:12:06 -0500

    Mangahub update (#966)
    * Mangahub update
    
    * lint
    
    * Constructor

---
+++ multisrc/overrides/mangahub/mangapandaonl/src/MangaPandaOnl.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.mangapandaonl
+
+import eu.kanade.tachiyomi.multisrc.mangahub.MangaHub
+
+class MangaPandaOnl : MangaHub("MangaPanda.onl", "https://mangapanda.onl", "en", "mr02")
---
+++ multisrc/overrides/mangahub/mangareadersite/src/MangaReaderSite.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.mangareadersite
+
+import eu.kanade.tachiyomi.multisrc.mangahub.MangaHub
+
+class MangaReaderSite : MangaHub("MangaReader.site", "https://mangareader.site", "en", "mr01")
