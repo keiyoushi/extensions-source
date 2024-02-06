28ba710e0d4ab14fa526454e1c9919281b85afdb
devil6venom <92388530+devil6venom@users.noreply.github.com>
2024-02-06 08:21:44 +0545

    Update hentai20.kt
    Fix Hentai20

--- multisrc/overrides/mangathemesia/hentai20/src/hentai20.kt
+++ multisrc/overrides/mangathemesia/hentai20/src/hentai20.kt
@@ -1,1 +1,6 @@
 
+package eu.kanade.tachiyomi.extension.en.hentai20
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class Hentai20 : MangaThemesia("Hentai20", "https://hentai20.io", "en", mangaUrlDirectory = "/manga")
