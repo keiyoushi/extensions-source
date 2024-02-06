fef9bdac3b4369bb485fe708ce9395d272a3f276
mohamedotaku <mohamed.otaku.2016@gmail.com>
2024-02-06 20:18:44 +0100

    fix "impossible de controuner Cloudflare" MangaLionz

--- multisrc/overrides/madara/mangalionz/src/MangaLionz.kt
+++ multisrc/overrides/madara/mangalionz/src/MangaLionz.kt
@@ -1,26 +1,79 @@
 package eu.kanade.tachiyomi.extension.ar.mangalionz
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.SManga
-import org.jsoup.nodes.Element
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import okhttp3.CacheControl
+import okhttp3.FormBody
+import okhttp3.Request
+import okhttp3.Response
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class MangaLionz : Madara("MangaLionz", "https://mangalionz.org", "ar") {
-    override fun popularMangaFromElement(element: Element): SManga {
-        val manga = SManga.create()
+class MangaLionz : Madara(
+    "MangaLionz",
+    "https://mangalionz.org",
+    "ar",
+    dateFormat = SimpleDateFormat("d MMMM، yyyy", Locale("ar")),
+) {
+    override val chapterUrlSuffix = ""
 
-        with(element) {
-            select(popularMangaUrlSelector).first()?.let {
-                manga.setUrlWithoutDomain(it.attr("abs:href"))
-                manga.title = it.ownText()
-            }
+    override val useNewChapterEndpoint = false
 
-            select("img").first()?.let {
-                manga.thumbnail_url = imageFromElement(it)?.replace("mangalionz", "mangalek")
+    override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
+
+    override fun popularMangaRequest(page: Int): Request {
+        return GET(
+            url = "$baseUrl/$mangaSubString/${searchPage(page)}",
+            headers = headers,
+            cache = CacheControl.FORCE_NETWORK,
+        )
+    }
+    override fun latestUpdatesRequest(page: Int): Request = popularMangaRequest(page)
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request =
+        POST(
+            "$baseUrl/wp-admin/admin-ajax.php",
+            headers,
+            FormBody.Builder()
+                .add("action", "wp-manga-search-manga")
+                .add("title", query)
+                .build(),
+        )
+
+    private inline fun <reified T> Response.parseAs(): T {
+        return json.decodeFromString(body.string())
+    }
+
+    @Serializable
+    data class SearchResponseDto(
+        val data: List<SearchEntryDto>,
+        val success: Boolean,
+    )
+
+    @Serializable
+    data class SearchEntryDto(
+        val url: String = "",
+        val title: String = "",
+    )
+    override fun searchMangaParse(response: Response): MangasPage {
+        val dto = response.parseAs<SearchResponseDto>()
+
+        if (!dto.success) {
+            return MangasPage(emptyList(), false)
+        }
+
+        val manga = dto.data.map {
+            SManga.create().apply {
+                setUrlWithoutDomain(it.url)
+                title = it.title
             }
         }
 
-        return manga
+        return MangasPage(manga, false)
     }
-
-    override val chapterUrlSuffix = ""
 }
