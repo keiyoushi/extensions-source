b965535311f3bfdc64776ad26cbde0173ef8b1af
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-02-06 10:02:57 +0700

    Remove dead sources, update Ổ Cú Mèo URL (#1021)
    * Remove MangaGo Yaoi, it's redirecting to another source already available
    
    * Remove dead source 1stKissManga.tv
    
    * Remove dead source: ZinManga.top
    
    * Remove dead source: TruyenChon
    
    * Remove dead source: ReadAdult
    
    * Remove dead source: O Cu Meo
    
    it’s redirecting to another existed source
    
    * Revert "Remove dead source: O Cu Meo"
    
    This reverts commit bab4873e36690c90fe00b68a690d51c244ddca32.
    
    * Update domain for "O Cu Meo"

--- multisrc/overrides/madara/firstkissmangatv/src/FirstKissMangaTv.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.firstkissmangatv
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class FirstKissMangaTv : Madara("1stKissManga.tv", "https://1stkissmanga.tv", "en") {
-    override val useNewChapterEndpoint = false
-
-    override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
-}
