1f29083e5c0439ce3adaf8f675d610a7d4bb3834
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-02-09 01:35:48 +0700

    NetTruyen/NhatTruyen: add filter, fix searching, update genres
    * Add Filter to Nhattruyen by moving FilterList to WPComics
    * Fix searching Nettruyen/Nhattruyen with filter Genre on
    * Fix searching Nhattruyen when filter status=Completed
    * Update Nettruyen/Nhattruyen genres list

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpcomics/WPComics.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpcomics/WPComics.kt
@@ -73,6 +73,9 @@ abstract class WPComics(
 
     protected open val searchPath = "tim-truyen"
 
+    protected open val replaceSearchPathOld = ""
+    protected open val replaceSearchPathNew = ""
+
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         val filterList = filters.let { if (it.isEmpty()) getFilterList() else it }
         return if (filterList.isEmpty()) {
@@ -94,7 +97,7 @@ abstract class WPComics(
                 addQueryParameter("sort", "0")
             }
 
-            GET(url.toString().replace("/tim-truyen?status=2&", "/truyen-full?"), headers)
+            GET(url.toString().replace(replaceSearchPathOld, replaceSearchPathNew), headers)
         }
     }
 
@@ -287,6 +290,13 @@ abstract class WPComics(
         "webtoon" to "Webtoon",
         "xuyen-khong" to "Xuyên Không",
     )
+
+    override fun getFilterList(): FilterList {
+        return FilterList(
+            StatusFilter(getStatusList()),
+            GenreFilter(getGenreList()),
+        )
+    }
 
     protected open class UriPartFilter(displayName: String, val vals: Array<Pair<String?, String>>) :
         Filter.Select<String>(displayName, vals.map { it.second }.toTypedArray()) {
