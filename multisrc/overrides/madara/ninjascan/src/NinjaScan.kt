fa3f14836fc2cea3f88762fb7193b16aabaf5ce6
Secozzi <49240133+Secozzi@users.noreply.github.com>
2024-02-08 11:35:54 +0000

    add ninjascan (#1132)

---
+++ multisrc/overrides/madara/ninjascan/src/NinjaScan.kt
@@ -0,0 +1,21 @@
+package eu.kanade.tachiyomi.extension.pt.ninjascan
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class NinjaScan : Madara(
+    "Ninja Scan",
+    "https://ninjascan.site",
+    "pt-BR",
+    SimpleDateFormat("dd 'de' MMMMM 'de' yyyy", Locale("pt", "BR")),
+) {
+    override val client = super.client.newBuilder()
+        .rateLimit(2)
+        .build()
+
+    override val useNewChapterEndpoint = true
+
+    override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
+}
