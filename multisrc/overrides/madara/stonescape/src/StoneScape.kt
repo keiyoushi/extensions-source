76cb13bc41def1e35596ebcd36280703e1e72017
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-02-09 03:02:26 -0500

    StoneScape cleanup (#1156)

--- multisrc/overrides/madara/stonescape/src/StoneScape.kt
+++ multisrc/overrides/madara/stonescape/src/StoneScape.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.ar.stonescape
+package eu.kanade.tachiyomi.extension.en.stonescape
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
@@ -11,4 +11,5 @@ class StoneScape : Madara(
     SimpleDateFormat("MMMM dd, yyyy", Locale("en")),
 ) {
     override val mangaSubString = "series"
+    override val chapterUrlSelector = "div + a"
 }
