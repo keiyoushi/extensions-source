b6478bee4891260a5498bfbceceae9fe90c37340
KirinRaikage <manmitsu@hotmail.fr>
2024-02-13 19:44:41 +0100

    Add Scan Hentai FR to Madara (#1232)

--- multisrc/overrides/madara/karatcamscans/src/KaratcamScans.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.karatcamscans
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class KaratcamScans : Madara("Karatcam Scans", "https://karatcam-scans.fr", "fr", dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)) {
-    override val mangaSubString = "projets"
-}
--- multisrc/overrides/madara/kataitake/src/Kataitake.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.kataitake
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class Kataitake : Madara("Kataitake", "https://www.kataitake.fr", "fr", dateFormat = SimpleDateFormat("dd/mm/yyyy", Locale.FRANCE)) {
-    override val altName: String = "Noms alternatifs :"
-}
