10b9c8b5bf816d12aa03137d111506eb9565a2bb
Secozzi <folke.steen85@gmail.com>
2024-02-14 18:34:13 +0100

    fix name + remove rate limit

--- src/en/rudrascans/src/eu/kanade/tachiyomi/extension/en/rudrascans/RudraScans.kt
+++ src/en/rudrascans/src/eu/kanade/tachiyomi/extension/en/rudrascans/RudraScans.kt
@@ -1,10 +1,5 @@
 package eu.kanade.tachiyomi.extension.en.rudrascans
 
 import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
-import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
-class RudraScans : Keyoapp("RudraScans", "https://rudrascans.com", "en") {
-    override val client = super.client.newBuilder()
-        .rateLimit(2)
-        .build()
-}
+class RudraScans : Keyoapp("Rudra Scans", "https://rudrascans.com", "en")
