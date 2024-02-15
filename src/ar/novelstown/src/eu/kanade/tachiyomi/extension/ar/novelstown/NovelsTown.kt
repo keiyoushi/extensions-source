6f737984962cea33d750007be6e986c2a12bdc70
mohamedotaku <mohamed.otaku.2016@gmail.com>
2024-02-15 21:36:48 +0100

    change URL and override mangaSubString (#1281)

--- src/ar/novelstown/src/eu/kanade/tachiyomi/extension/ar/novelstown/NovelsTown.kt
+++ src/ar/novelstown/src/eu/kanade/tachiyomi/extension/ar/novelstown/NovelsTown.kt
@@ -2,4 +2,6 @@ package eu.kanade.tachiyomi.extension.ar.novelstown
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class NovelsTown : Madara("Novels Town", "https://novelstown.cyou", "ar")
+class NovelsTown : Madara("Novels Town", "https://novelstown.com", "ar") {
+    override val mangaSubString = "الاعمال"
+}
