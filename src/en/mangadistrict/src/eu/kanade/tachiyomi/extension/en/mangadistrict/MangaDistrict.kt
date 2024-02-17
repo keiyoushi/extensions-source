711e44a8f267cc6526f92597a7ae8f22c411af87
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-02-18 01:50:59 +0700

    MangaDistrict: settings to parse only chapters with full/high quality
    # Conflicts:
    #	src/en/mangadistrict/src/eu/kanade/tachiyomi/extension/en/mangadistrict/MangaDistrict.kt

--- src/en/mangadistrict/src/eu/kanade/tachiyomi/extension/en/mangadistrict/MangaDistrict.kt
+++ src/en/mangadistrict/src/eu/kanade/tachiyomi/extension/en/mangadistrict/MangaDistrict.kt
@@ -2,10 +2,17 @@ package eu.kanade.tachiyomi.extension.en.mangadistrict
 
 import android.app.Application
 import android.content.SharedPreferences
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
 import androidx.preference.SwitchPreferenceCompat
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.MediaType
+import okhttp3.Response
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import uy.kohesive.injekt.Injekt
@@ -51,6 +58,29 @@ class MangaDistrict : Madara(
         }
     }
 
+    private fun chaptersStripSelector(): String {
+        return when (getImgResPref()) {
+            "Full quality" -> "a:contains(High Quality) ~ ul.list-chap"
+            "High quality" -> "a:contains(Full Quality) ~ ul.list-chap"
+            else -> "dummy"
+        }
+    }
+
+    private fun Response.stripElements(selector: String): Response {
+        val contentType: MediaType? = body.contentType()
+        val document = asJsoup()
+        document.select(selector).remove()
+
+        val body: ResponseBody = document.body().toString().toResponseBody(contentType)
+        return newBuilder().body(body).build()
+    }
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        return super.chapterListParse(response.stripElements(chaptersStripSelector()))
+    }
+
+    private fun getImgResPref(): String? = preferences.getString(IMG_RES_PREF_KEY, IMG_RES_PREF_DEFAULT_VALUE)
+
     override fun setupPreferenceScreen(screen: PreferenceScreen) {
         SwitchPreferenceCompat(screen.context).apply {
             key = REMOVE_TITLE_COMMENT_PREF
@@ -59,10 +89,36 @@ class MangaDistrict : Madara(
             setDefaultValue(true)
         }.let(screen::addPreference)
 
+        ListPreference(screen.context).apply {
+            key = IMG_RES_PREF_KEY
+            title = IMG_RES_PREF_TITLE
+            entries = IMG_RES_PREF_ENTRIES
+            entryValues = IMG_RES_PREF_ENTRY_VALUES
+            setDefaultValue(IMG_RES_PREF_DEFAULT_VALUE)
+            summary = "%s"
+
+            setOnPreferenceChangeListener { _, newValue ->
+                val selected = newValue as String
+                val index = findIndexOfValue(selected)
+                val entry = entryValues[index] as String
+                preferences.edit().putString(IMG_RES_PREF_KEY, entry).commit()
+            }
+        }.let(screen::addPreference)
+
         super.setupPreferenceScreen(screen)
     }
 
     companion object {
         private const val REMOVE_TITLE_COMMENT_PREF = "REMOVE_TITLE_COMMENT"
+
+        private const val IMG_RES_PREF_KEY = "IMG_RES"
+        private const val IMG_RES_PREF_TITLE = "Image resolution"
+        private val IMG_RES_PREF_ENTRIES = arrayOf(
+            "Full quality",
+            "High quality",
+            "Both",
+        )
+        private val IMG_RES_PREF_ENTRY_VALUES = IMG_RES_PREF_ENTRIES
+        private val IMG_RES_PREF_DEFAULT_VALUE = IMG_RES_PREF_ENTRY_VALUES[0]
     }
 }
