7377ddb8247217dc7094ecf29ee7f4f27781e722
Secozzi <folke.steen85@gmail.com>
2024-02-18 16:58:05 +0100

    add referer

--- src/ko/newtoki/src/eu/kanade/tachiyomi/extension/ko/newtoki/ManaToki.kt
+++ src/ko/newtoki/src/eu/kanade/tachiyomi/extension/ko/newtoki/ManaToki.kt
@@ -18,6 +18,9 @@ object ManaToki : NewToki("ManaToki", "comic", manaTokiPreferences) {
 
     override val baseUrl get() = "https://$MANATOKI_PREFIX$domainNumber.net"
 
+    override fun headersBuilder() = super.headersBuilder()
+        .add("Referer", "$baseUrl/")
+
     private val chapterRegex by lazy { Regex(""" [ \d,~.-]+화$""") }
 
     fun latestUpdatesElementParse(element: Element): SManga {
