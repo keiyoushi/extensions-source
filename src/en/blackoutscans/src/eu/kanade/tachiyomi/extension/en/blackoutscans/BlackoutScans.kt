5e98219178d3fed5b73505a3028e03399f531263
Secozzi <folke.steen85@gmail.com>
2024-02-18 16:09:27 +0100

    add blackoutscans

---
+++ src/en/blackoutscans/src/eu/kanade/tachiyomi/extension/en/blackoutscans/BlackoutScans.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.blackoutscans
+
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+
+class BlackoutScans : Keyoapp("Blackout Scans", "https://blackoutscans.com", "en")
