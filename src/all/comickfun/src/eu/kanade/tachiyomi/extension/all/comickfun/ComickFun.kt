23b916428256012dfcd344932529edc56bb80668
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-02-18 11:57:32 +0500

    update baseUrl

--- src/all/comickfun/src/eu/kanade/tachiyomi/extension/all/comickfun/ComickFun.kt
+++ src/all/comickfun/src/eu/kanade/tachiyomi/extension/all/comickfun/ComickFun.kt
@@ -38,7 +38,7 @@ abstract class ComickFun(
 
     override val name = "Comick"
 
-    override val baseUrl = "https://comick.cc"
+    override val baseUrl = "https://comick.io"
 
     private val apiUrl = "https://api.comick.fun"
 
