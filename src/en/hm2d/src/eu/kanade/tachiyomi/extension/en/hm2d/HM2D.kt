6869674087a55bdeeaa7ab8bd517ea993c4e9df9
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-02-19 10:25:50 +0700

    update HM2D URL (doujindistrict.com) (#1375)
    * new source: DoujinDistrict (theme: madara)
    
    * Revert "new source: DoujinDistrict (theme: madara)"
    
    This reverts commit 0b12e6be1425192e28166243dbd4fc6e2375ad12.
    
    * update HM2D URL (doujindistrict.com)
    
    * remove unused imports

--- src/en/hm2d/src/eu/kanade/tachiyomi/extension/en/hm2d/HM2D.kt
+++ src/en/hm2d/src/eu/kanade/tachiyomi/extension/en/hm2d/HM2D.kt
@@ -1,24 +1,11 @@
 package eu.kanade.tachiyomi.extension.en.hm2d
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.SManga
-import okhttp3.Request
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class HM2D : Madara(
     "HM2D",
-    "https://mangadistrict.com/hdoujin/",
+    "https://doujindistrict.com",
     "en",
-    dateFormat = SimpleDateFormat("MMM d, yyyy", Locale.US),
 ) {
-
-    override fun mangaDetailsRequest(manga: SManga): Request {
-        return GET(baseUrl + manga.url.removePrefix("/hdoujin/"), headers)
-    }
-
-    override fun chapterListRequest(manga: SManga): Request {
-        return GET(baseUrl + manga.url.removePrefix("/hdoujin/"), headers)
-    }
+    override fun searchMangaNextPageSelector() = "div[role=navigation] span.current + a.page"
 }
