fc4c973a8c64657bef80477c03c076f1cdf3785c
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2024-02-19 14:00:22 +0000

    Deduplicate lib build scripts (#1392)

--- lib/synchrony/build.gradle.kts
+++ lib/synchrony/build.gradle.kts
@@ -1,22 +1,3 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
-}
-
-android {
-    compileSdk = AndroidConfig.compileSdk
-
-    defaultConfig {
-        minSdk = AndroidConfig.minSdk
-    }
-
-    namespace = "eu.kanade.tachiyomi.lib.synchrony"
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    compileOnly(libs.bundles.common)
+    id("lib-android")
 }
