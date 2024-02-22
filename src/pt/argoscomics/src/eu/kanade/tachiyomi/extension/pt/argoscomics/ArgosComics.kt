89043b7ba69b1d8ec9a9b99fba194a5c024eb6ab
ttony2chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-02-21 22:50:43 -0300

    Add Argos Comics

---
+++ src/pt/argoscomics/src/eu/kanade/tachiyomi/extension/pt/argoscomics/ArgosComics.kt
@@ -0,0 +1,12 @@
+package eu.kanade.tachiyomi.extension.pt.argoscomics
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class ArgosComics : Madara(
+    "Argos Comics",
+    "https://argoscomics.com/",
+    "pt-BR",
+    SimpleDateFormat("MMMMM dd, yyyy", Locale("pt", "BR")),
+)
