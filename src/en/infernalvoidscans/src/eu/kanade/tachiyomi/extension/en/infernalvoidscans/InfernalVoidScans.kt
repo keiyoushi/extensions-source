44f4608d355cfc08efe0c13da98f7216162e9d43
Henry <60916302+hyknn@users.noreply.github.com>
2024-02-29 19:24:53 +0700

    Void Scans: Update Domain (#1605)
    Void Scans: Update domain

--- src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
+++ src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
@@ -2,6 +2,6 @@ package eu.kanade.tachiyomi.extension.en.infernalvoidscans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 
-class InfernalVoidScans : MangaThemesia("Infernal Void Scans", "https://void-scans.com", "en") {
+class InfernalVoidScans : MangaThemesia("Infernal Void Scans", "https://hivescans.com", "en") {
     override val pageSelector = "div#readerarea > p > img"
 }
