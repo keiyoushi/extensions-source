720b1d4271b67b93cd1ddf2b418b1a96d516444c
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-03-01 22:21:46 +0500

    lint

--- lib-multisrc/mangathemesia/src/eu/kanade/tachiyomi/multisrc/mangathemesia/MangaThemesia.kt
+++ lib-multisrc/mangathemesia/src/eu/kanade/tachiyomi/multisrc/mangathemesia/MangaThemesia.kt
@@ -536,11 +536,11 @@ abstract class MangaThemesia(
                 listOf(
                     Filter.Header(intl["genre_exclusion_warning"]),
                     GenreListFilter(intl["genre_filter_title"], getGenreList()),
-                )
+                ),
             )
         } else {
             filters.add(
-                Filter.Header(intl["genre_missing_warning"])
+                Filter.Header(intl["genre_missing_warning"]),
             )
         }
         if (hasProjectPage) {
