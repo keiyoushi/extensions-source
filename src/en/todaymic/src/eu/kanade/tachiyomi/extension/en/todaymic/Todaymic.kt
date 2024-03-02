b8bd54b818c2258e90ab2b3a2a642701e35ddcb1
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-03-02 16:14:15 +0500

    add description selector

--- src/en/todaymic/src/eu/kanade/tachiyomi/extension/en/todaymic/Todaymic.kt
+++ src/en/todaymic/src/eu/kanade/tachiyomi/extension/en/todaymic/Todaymic.kt
@@ -6,4 +6,5 @@ class Todaymic : Madara("Todaymic", "https://todaymic.com", "en") {
     override val filterNonMangaItems = false
     override val useNewChapterEndpoint = true
     override val useLoadMoreRequest = LoadMoreStrategy.Always
+    override val mangaDetailsSelectorDescription = ".manga-about > p:nth-child(2)"
 }
