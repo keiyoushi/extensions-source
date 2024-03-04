ce162cdf4e60cc7685980bd504c08f9a21cbd03c
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-03-03 22:45:25 -0300

    feat: Create RawINU base

---
+++ src/ja/rawinu/src/eu/kanade/tachiyomi/extension/ja/rawinu/RawINU.kt
@@ -0,0 +1,15 @@
+package eu.kanade.tachiyomi.extension.ja.rawinu
+
+import eu.kanade.tachiyomi.multisrc.fmreader.FMReader
+import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import okhttp3.HttpUrl.Companion.toHttpUrl
+
+class RawINU : FMReader(
+    "RawINU",
+    "https://rawinu.com",
+    "ja",
+) {
+    override val client = network.client.newBuilder()
+        .rateLimitHost(baseUrl.toHttpUrl(), 2)
+        .build()
+}
