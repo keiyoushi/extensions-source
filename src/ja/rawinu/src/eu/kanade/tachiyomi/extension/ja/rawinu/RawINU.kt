76ebe0728b7842c20f90bed5034db3abd8fa05c2
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-03-03 23:43:54 -0300

    fix: Fix page list

--- src/ja/rawinu/src/eu/kanade/tachiyomi/extension/ja/rawinu/RawINU.kt
+++ src/ja/rawinu/src/eu/kanade/tachiyomi/extension/ja/rawinu/RawINU.kt
@@ -3,12 +3,14 @@ package eu.kanade.tachiyomi.extension.ja.rawinu
 import eu.kanade.tachiyomi.multisrc.fmreader.FMReader
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
+import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
 class RawINU : FMReader(
@@ -20,13 +22,15 @@ class RawINU : FMReader(
         .rateLimitHost(baseUrl.toHttpUrl(), 2)
         .build()
 
+    private val apiEndpoint = "$baseUrl/app/manga/controllers"
+
     // =========================== Manga Details ============================
     override val infoElementSelector = "div.card-body div.row"
 
     // ============================== Chapters ==============================
     override fun chapterListRequest(manga: SManga): Request {
         val slug = manga.url.substringAfter("/manga-").substringBefore(".html")
-        return GET("$baseUrl/app/manga/controllers/cont.Listchapter.php?slug=$slug", headers)
+        return GET("$apiEndpoint/cont.Listchapter.php?slug=$slug", headers)
     }
 
     override fun chapterListParse(response: Response): List<SChapter> {
@@ -39,5 +43,15 @@ class RawINU : FMReader(
         setUrlWithoutDomain(element.absUrl("href"))
         name = element.attr(chapterNameAttrSelector).trim()
         date_upload = element.select(chapterTimeSelector).run { if (hasText()) parseRelativeDate(text()) else 0 }
+    }
+
+    // =============================== Pages ================================
+    override fun pageListParse(document: Document): List<Page> {
+        val id = document.selectFirst("input[name=chapter]#chapter")!!.attr("value")
+        val req = client.newCall(GET("$apiEndpoint/cont.imagesChap.php?cid=$id", headers)).execute()
+
+        return req.asJsoup().select(pageListImageSelector).mapIndexed { i, img ->
+            Page(i, document.location(), getImgAttr(img))
+        }
     }
 }
