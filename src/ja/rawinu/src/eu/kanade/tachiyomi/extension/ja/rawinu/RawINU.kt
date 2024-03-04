e8b9820c049ac6aa1a06711a77858feda087c9a9
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-03-03 23:19:34 -0300

    fix: Fix chapter list

--- src/ja/rawinu/src/eu/kanade/tachiyomi/extension/ja/rawinu/RawINU.kt
+++ src/ja/rawinu/src/eu/kanade/tachiyomi/extension/ja/rawinu/RawINU.kt
@@ -1,8 +1,15 @@
 package eu.kanade.tachiyomi.extension.ja.rawinu
 
 import eu.kanade.tachiyomi.multisrc.fmreader.FMReader
+import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Element
 
 class RawINU : FMReader(
     "RawINU",
@@ -15,4 +22,22 @@ class RawINU : FMReader(
 
     // =========================== Manga Details ============================
     override val infoElementSelector = "div.card-body div.row"
+
+    // ============================== Chapters ==============================
+    override fun chapterListRequest(manga: SManga): Request {
+        val slug = manga.url.substringAfter("/manga-").substringBefore(".html")
+        return GET("$baseUrl/app/manga/controllers/cont.Listchapter.php?slug=$slug", headers)
+    }
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val doc = response.asJsoup()
+        doc.setBaseUri(baseUrl) // Fixes chapter URLs
+        return doc.select(chapterListSelector()).map(::chapterFromElement)
+    }
+
+    override fun chapterFromElement(element: Element) = SChapter.create().apply {
+        setUrlWithoutDomain(element.absUrl("href"))
+        name = element.attr(chapterNameAttrSelector).trim()
+        date_upload = element.select(chapterTimeSelector).run { if (hasText()) parseRelativeDate(text()) else 0 }
+    }
 }
