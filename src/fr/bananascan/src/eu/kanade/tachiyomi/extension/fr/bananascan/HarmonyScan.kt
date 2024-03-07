c1f04dcef0c380a81dc5bb0f7f23c971909fae34
Faïd Abdourahim <manmitsu@hotmail.fr>
2024-03-07 20:45:12 +0100

    Add Banana Scan ID

--- src/fr/bananascan/src/eu/kanade/tachiyomi/extension/fr/bananascan/HarmonyScan.kt
+++ src/fr/bananascan/src/eu/kanade/tachiyomi/extension/fr/bananascan/HarmonyScan.kt
@@ -2,4 +2,7 @@ package eu.kanade.tachiyomi.extension.fr.bananascan
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class HarmonyScan : Madara("Harmony-Scan", "https://harmony-scan.fr", "fr")
+class HarmonyScan : Madara("Harmony-Scan", "https://harmony-scan.fr", "fr") {
+    // formally Banana-Scan
+    override val id = 3121632933690925888
+}
