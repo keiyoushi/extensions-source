224b7cdb71fee703d3eeb432cda46de0d19062fe
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-03-08 00:47:44 -0300

    Add ToshiWaYume

---
+++ src/pt/toshiwayume/src/eu/kanade/tachiyomi/extension/pt/toshiwayume/ToshiWaYume.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.pt.toshiwayume
+
+import eu.kanade.tachiyomi.multisrc.foolslide.FoolSlide
+
+class ToshiWaYume : FoolSlide(
+    "Toshi wa Yume",
+    "https://leitoronline.toshiwayume.com.br",
+    "pt-BR",
+)
