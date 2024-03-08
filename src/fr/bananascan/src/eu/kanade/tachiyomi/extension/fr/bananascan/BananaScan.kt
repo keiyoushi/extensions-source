f0036beaf19db9e2da8847e1f15852eca9fd585d
KirinRaikage <manmitsu@hotmail.fr>
2024-03-08 11:00:49 +0100

    Banana-Scan: Rename to Harmony-Scan and migrate to Madara (#1754)
    * Banana-Scan: Rename to Harmony-Scan and migrate to Madara
    
    * Add Banana Scan ID

--- src/fr/bananascan/src/eu/kanade/tachiyomi/extension/fr/bananascan/BananaScan.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.bananascan
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class BananaScan : MangaThemesia("Banana-Scan", "https://banana-scan.com", "fr", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH))
