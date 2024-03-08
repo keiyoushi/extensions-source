5faf53d49c062303d384dd540c03548521f02562
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-03-08 14:58:19 +0500

    DiamondFansub: fix selectors and directory (#1732)

--- src/tr/diamondfansub/src/eu/kanade/tachiyomi/extension/tr/diamondfansub/DiamondFansub.kt
+++ src/tr/diamondfansub/src/eu/kanade/tachiyomi/extension/tr/diamondfansub/DiamondFansub.kt
@@ -4,4 +4,14 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class DiamondFansub : Madara("DiamondFansub", "https://diamondfansub.com", "tr", SimpleDateFormat("MMMM dd, yyyy", Locale.forLanguageTag("tr")))
+class DiamondFansub : Madara(
+    "DiamondFansub",
+    "https://diamondfansub.com",
+    "tr",
+    SimpleDateFormat("d MMMM", Locale("tr", "TR")),
+) {
+    override val mangaSubString = "seri"
+    override val useNewChapterEndpoint = true
+    override val mangaDetailsSelectorAuthor = ".manga-authors"
+    override val mangaDetailsSelectorDescription = ".manga-info"
+}
