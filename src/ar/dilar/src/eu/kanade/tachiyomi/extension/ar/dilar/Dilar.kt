a5834615d6aefba8be22d364206afcb272288675
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-03-09 08:30:11 +0500

    Dilar: filter paid chapters

--- src/ar/dilar/src/eu/kanade/tachiyomi/extension/ar/dilar/Dilar.kt
+++ src/ar/dilar/src/eu/kanade/tachiyomi/extension/ar/dilar/Dilar.kt
@@ -1,9 +1,25 @@
 package eu.kanade.tachiyomi.extension.ar.dilar
 
 import eu.kanade.tachiyomi.multisrc.gmanga.Gmanga
+import eu.kanade.tachiyomi.source.model.SChapter
+import okhttp3.Response
 
 class Dilar : Gmanga(
     "Dilar",
     "https://dilar.tube",
     "ar",
-)
+) {
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val releases = response.parseAs<ChapterListDto>().releases
+            .filterNot { it.has_rev_link && it.support_link.isNotEmpty() }
+
+        return releases.map {
+            it.toSChapter(dateFormat)
+        }.sortedWith(
+            compareBy(
+                { -it.chapter_number },
+                { -it.date_upload },
+            ),
+        )
+    }
+}
---
+++ src/ar/dilar/src/eu/kanade/tachiyomi/extension/ar/dilar/Dto.kt
@@ -0,0 +1,39 @@
+package eu.kanade.tachiyomi.extension.ar.dilar
+
+import eu.kanade.tachiyomi.source.model.SChapter
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.float
+import java.text.SimpleDateFormat
+
+@Serializable
+class ChapterListDto(
+    val releases: List<ChapterRelease>,
+)
+
+@Serializable
+class ChapterRelease(
+    private val id: Int,
+    private val chapter: JsonPrimitive,
+    private val title: String,
+    @SerialName("team_name") private val teamName: String,
+    @SerialName("created_at") private val createdAt: String,
+
+    val has_rev_link: Boolean,
+    val support_link: String,
+) {
+    fun toSChapter(dateFormat: SimpleDateFormat) = SChapter.create().apply {
+        url = "/r/$id"
+        chapter_number = chapter.float
+        date_upload = try {
+            dateFormat.parse(createdAt)!!.time
+        } catch (_: Exception) {
+            0L
+        }
+        scanlator = teamName
+
+        val chapterName = title.let { if (it.trim() != "") " - $it" else "" }
+        name = "${chapter_number.let { if (it % 1 > 0) it else it.toInt() }}$chapterName"
+    }
+}
