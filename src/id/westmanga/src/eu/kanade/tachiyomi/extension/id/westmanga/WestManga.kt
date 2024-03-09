6bb48e6fbf2c8ffdcb6719a2ca46c3024e72ca21
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-03-09 16:07:26 +0500

    Fix NPEs in some sources (#1773)
    * Siren Komik: fix NPE
    
    * West Manga: fix NPE

--- src/id/westmanga/src/eu/kanade/tachiyomi/extension/id/westmanga/WestManga.kt
+++ src/id/westmanga/src/eu/kanade/tachiyomi/extension/id/westmanga/WestManga.kt
@@ -2,7 +2,10 @@ package eu.kanade.tachiyomi.extension.id.westmanga
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.SManga
 import okhttp3.OkHttpClient
+import org.jsoup.nodes.Document
+import java.util.Locale
 
 class WestManga : MangaThemesia("West Manga", "https://westmanga.fun", "id") {
     // Formerly "West Manga (WP Manga Stream)"
@@ -12,8 +15,39 @@ class WestManga : MangaThemesia("West Manga", "https://westmanga.fun", "id") {
         .rateLimit(4)
         .build()
 
+    override val seriesTitleSelector = "h1"
     override val seriesDetailsSelector = ".seriestucontent"
     override val seriesTypeSelector = ".infotable tr:contains(Type) td:last-child"
+
+    override fun mangaDetailsParse(document: Document) = SManga.create().apply {
+        document.selectFirst(seriesDetailsSelector)!!.let { seriesDetails ->
+            title = document.selectFirst("div.postbody h1")!!.text()
+            artist = seriesDetails.selectFirst(seriesArtistSelector)?.ownText().removeEmptyPlaceholder()
+            author = seriesDetails.selectFirst(seriesAuthorSelector)?.ownText().removeEmptyPlaceholder()
+            description = seriesDetails.select(seriesDescriptionSelector).joinToString("\n") { it.text() }.trim()
+            // Add alternative name to manga description
+            val altName = document.selectFirst(".seriestualt")?.ownText().takeIf { it.isNullOrBlank().not() }
+            altName?.let {
+                description = "$description\n\n$altNamePrefix$altName".trim()
+            }
+            val genres = seriesDetails.select(seriesGenreSelector).map { it.text() }.toMutableList()
+            // Add series type (manga/manhwa/manhua/other) to genre
+            seriesDetails.selectFirst(seriesTypeSelector)?.ownText().takeIf { it.isNullOrBlank().not() }?.let { genres.add(it) }
+            genre = genres.map { genre ->
+                genre.lowercase(Locale.forLanguageTag(lang)).replaceFirstChar { char ->
+                    if (char.isLowerCase()) {
+                        char.titlecase(Locale.forLanguageTag(lang))
+                    } else {
+                        char.toString()
+                    }
+                }
+            }
+                .joinToString { it.trim() }
+
+            status = seriesDetails.selectFirst(seriesStatusSelector)?.text().parseStatus()
+            thumbnail_url = seriesDetails.select(seriesThumbnailSelector).imgAttr()
+        }
+    }
 
     override val hasProjectPage = true
 }
