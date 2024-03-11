0e8598269e6f90e6fb43031c86363f522efc120f
altaccosc <97206249+altaccosc@users.noreply.github.com>
2024-03-11 03:40:06 +0100

    Add ClownCorps comic source

---
+++ src/en/clowncorps/src/eu/kanade/tachiyomi/extension/en/clowncorps/ClownCorps.kt
@@ -0,0 +1,158 @@
+package eu.kanade.tachiyomi.extension.en.clowncorps
+
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.preference.PreferenceScreen
+import androidx.preference.SwitchPreferenceCompat
+import eu.kanade.tachiyomi.lib.textinterceptor.TextInterceptor
+import eu.kanade.tachiyomi.lib.textinterceptor.TextInterceptorHelper
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.ConfigurableSource
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.HttpSource
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import rx.Observable
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+
+class ClownCorps : ConfigurableSource, HttpSource() {
+    override val baseUrl = "https://clowncorps.net"
+    override val lang = "en"
+    override val name = "Clown Corps"
+    override val supportsLatest = false
+
+    private val creator = "Joe Chouinard"
+
+    //Text from: https://clowncorps.net/about/
+    private val synopsis = "Clown Corps is a comic about crime-fighting clowns. " +
+        "It's pronounced \"core.\" Like marine corps."
+
+    override val client: OkHttpClient
+        get() = super.client.newBuilder()
+            .addInterceptor(TextInterceptor())
+            .build()
+
+    override fun fetchPopularManga(page: Int): Observable<MangasPage> =
+        SManga.create().apply {
+            title = name
+            artist = creator
+            author = creator
+            description = synopsis
+            status = SManga.ONGOING
+            //Image from: https://clowncorps.net/about/
+            thumbnail_url = "https://clowncorps.net/wp-content/uploads/2022/11/clowns41.jpg"
+            setUrlWithoutDomain("/comic")
+        }.let { Observable.just(MangasPage(listOf(it), false))!! }
+
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList) =
+        Observable.just(MangasPage(emptyList(), false))!!
+
+    override fun fetchMangaDetails(manga: SManga) =
+        Observable.just(manga.apply { initialized = true })!!
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val chapters = mutableListOf<SChapter>()
+
+        for (page in 1..Int.MAX_VALUE) {
+            val url = "https://clowncorps.net/comic/page/$page/"
+            val resp = client.newCall(GET(url, headers)).execute()
+            if (resp.code == 404) break
+            val comics = resp.asJsoup().select(".comic")
+            for (comic in comics) {
+                val link = comic.selectFirst(".post-title a")?.attr("href")
+                val title = comic.selectFirst(".post-title a")?.text()
+                if (link == null || title == null) continue
+                val chapter = SChapter.create().apply {
+                    setUrlWithoutDomain(link)
+                    name = title
+                }
+                chapters.add(chapter)
+            }
+        }
+
+        chapters.forEachIndexed { index, sChapter ->
+            sChapter.chapter_number = chapters.size - index.toFloat()
+        }
+
+        return chapters
+    }
+
+    override fun pageListParse(response: Response): List<Page> {
+        val doc = response.asJsoup()
+        val pages = mutableListOf<Page>()
+
+        val image = doc.selectFirst("#comic img") ?: return pages
+
+        val url = image.attr("src")
+        pages.add(Page(0, "", url))
+
+        if (showAuthorsNotesPref()) {
+            val title = image.attr("title")
+
+            // Ignore chapters that don't really have author's notes
+            val ignoreRegex = Regex("""^chapter \d+ page \d+$""", RegexOption.IGNORE_CASE)
+            if (ignoreRegex.matches(title)) return pages
+
+            val localURL = TextInterceptorHelper.createUrl("Author's Notes from $creator", title)
+            val textPage = Page(pages.size, "", localURL)
+            pages.add(textPage)
+        }
+
+        return pages
+    }
+
+    override fun imageUrlParse(response: Response) =
+        throw UnsupportedOperationException()
+
+    override fun latestUpdatesParse(response: Response) =
+        throw UnsupportedOperationException()
+
+    override fun latestUpdatesRequest(page: Int) =
+        throw UnsupportedOperationException()
+
+    override fun mangaDetailsParse(response: Response) =
+        throw UnsupportedOperationException()
+
+    override fun popularMangaParse(response: Response) =
+        throw UnsupportedOperationException()
+
+    override fun popularMangaRequest(page: Int) =
+        throw UnsupportedOperationException()
+
+    override fun searchMangaParse(response: Response) =
+        throw UnsupportedOperationException()
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList) =
+        throw UnsupportedOperationException()
+
+    private val preferences: SharedPreferences by lazy {
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+    }
+
+    private fun showAuthorsNotesPref() = preferences.getBoolean(SHOW_AUTHORS_NOTES_KEY, false)
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        val authorsNotesPref = SwitchPreferenceCompat(screen.context).apply {
+            key = SHOW_AUTHORS_NOTES_KEY
+            title = "Show author's notes"
+            summary = "Enable to see the author's notes at the end of chapters (if they're there)."
+            setDefaultValue(false)
+
+            setOnPreferenceChangeListener { _, newValue ->
+                val checkValue = newValue as Boolean
+                preferences.edit().putBoolean(SHOW_AUTHORS_NOTES_KEY, checkValue).commit()
+            }
+        }
+        screen.addPreference(authorsNotesPref)
+    }
+
+    companion object {
+        private const val SHOW_AUTHORS_NOTES_KEY = "showAuthorsNotes"
+    }
+}
