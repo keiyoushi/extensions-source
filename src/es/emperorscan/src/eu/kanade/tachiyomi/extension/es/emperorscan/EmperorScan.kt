074d2f3f6207d653979543b713a6e44e8e8a5770
bapeey <90949336+bapeey@users.noreply.github.com>
2024-03-11 05:57:30 -0500

    Emperor Scan: Add randomUA (#1803)
    * Add randomUa and fix description
    
    * newline
    
    ---------
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/es/emperorscan/src/eu/kanade/tachiyomi/extension/es/emperorscan/EmperorScan.kt
+++ src/es/emperorscan/src/eu/kanade/tachiyomi/extension/es/emperorscan/EmperorScan.kt
@@ -1,14 +1,47 @@
 package eu.kanade.tachiyomi.extension.es.emperorscan
 
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.lib.randomua.addRandomUAPreferenceToScreen
+import eu.kanade.tachiyomi.lib.randomua.getPrefCustomUA
+import eu.kanade.tachiyomi.lib.randomua.getPrefUAType
+import eu.kanade.tachiyomi.lib.randomua.setRandomUserAgent
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import eu.kanade.tachiyomi.source.ConfigurableSource
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class EmperorScan : Madara(
-    "Emperor Scan",
-    "https://emperorscan.com",
-    "es",
-    SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
-) {
-    override val mangaDetailsSelectorDescription = "div.sinopsis div.contenedor"
+class EmperorScan :
+    Madara(
+        "Emperor Scan",
+        "https://emperorscan.com",
+        "es",
+        SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
+    ),
+    ConfigurableSource {
+
+    private val preferences: SharedPreferences =
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = true
+
+    override val client = super.client.newBuilder()
+        .setRandomUserAgent(
+            preferences.getPrefUAType(),
+            preferences.getPrefCustomUA(),
+        )
+        .rateLimitHost(baseUrl.toHttpUrl(), 2)
+        .build()
+
+    override val mangaDetailsSelectorDescription = "div.tab-summary div.sinopsis p"
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        addRandomUAPreferenceToScreen(screen)
+    }
 }
