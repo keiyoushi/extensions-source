af631bf975b0b07c81acfd89b58c76ad0f430043
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-03-16 10:12:14 +0500

    Ozul Scans to King of Manga
    also enable dynamic urls

---
+++ src/ar/ozulscans/src/eu/kanade/tachiyomi/extension/ar/ozulscans/KingOfManga.kt
@@ -0,0 +1,15 @@
+package eu.kanade.tachiyomi.extension.ar.ozulscans
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaAlt
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class KingOfManga : MangaThemesiaAlt(
+    "King Of Manga",
+    "https://kingofmanga.com",
+    "ar",
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("ar")),
+) {
+    // Ozul Scans -> King of Manga
+    override val id = 3453769904666687440
+}
