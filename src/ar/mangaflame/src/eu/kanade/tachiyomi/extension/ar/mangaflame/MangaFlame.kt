fca1e42e53b8001057fe42fdeb3aae580700dcf8
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-03-16 21:09:32 +0100

    raise read timeout

--- src/ar/mangaflame/src/eu/kanade/tachiyomi/extension/ar/mangaflame/MangaFlame.kt
+++ src/ar/mangaflame/src/eu/kanade/tachiyomi/extension/ar/mangaflame/MangaFlame.kt
@@ -1,8 +1,10 @@
 package eu.kanade.tachiyomi.extension.ar.mangaflame
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import okhttp3.OkHttpClient
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 class MangaFlame : MangaThemesia(
     "Manga Flame",
@@ -11,4 +13,8 @@ class MangaFlame : MangaThemesia(
     dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("ar")),
 ) {
     override val id = 1501237443119573205
+
+    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
+        .readTimeout(3, TimeUnit.MINUTES)
+        .build()
 }
