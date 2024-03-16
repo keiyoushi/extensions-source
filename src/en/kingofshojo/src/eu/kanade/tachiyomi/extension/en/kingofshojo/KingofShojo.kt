7687c72d995bac2d4ef964a9956f3cbed1458dca
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-03-16 10:27:09 +0500

    move King of Shojo to en

---
+++ src/en/kingofshojo/src/eu/kanade/tachiyomi/extension/en/kingofshojo/KingofShojo.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.en.kingofshojo
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class KingofShojo : MangaThemesia("King of Shojo", "https://kingofshojo.com", "en") {
+
+    // ar -> en
+    override val id = 5071402517183328880
+}
