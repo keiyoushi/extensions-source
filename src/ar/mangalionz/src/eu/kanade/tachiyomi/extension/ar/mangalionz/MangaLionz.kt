5584e4a0565e4fd7ec30c5ae08f98fff887e7377
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-03-22 15:59:31 +0000

    MangaLionz: update domain, fix date format (#2026)

--- src/ar/mangalionz/src/eu/kanade/tachiyomi/extension/ar/mangalionz/MangaLionz.kt
+++ src/ar/mangalionz/src/eu/kanade/tachiyomi/extension/ar/mangalionz/MangaLionz.kt
@@ -3,8 +3,15 @@ package eu.kanade.tachiyomi.extension.ar.mangalionz
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.source.model.SManga
 import org.jsoup.nodes.Element
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class MangaLionz : Madara("MangaLionz", "https://mangalionz.org", "ar") {
+class MangaLionz : Madara(
+    "MangaLionz",
+    "https://manga-lionz.com",
+    "ar",
+    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("ar")),
+) {
     override val useLoadMoreRequest = LoadMoreStrategy.Always
     override fun popularMangaFromElement(element: Element): SManga {
         val manga = SManga.create()
