a43fe8066737c35edf19f0a2a78658a724688fb2
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-03-25 19:59:13 +0100

    Remove Manganelo related sites

--- src/en/manganelowebsite/src/eu/kanade/tachiyomi/extension/en/manganelowebsite/ManganeloWebsite.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.manganelowebsite
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class ManganeloWebsite : Madara("Manganelo.website (unoriginal)", "https://manganelo.website", "en") {
-    override val useNewChapterEndpoint = false
-}
