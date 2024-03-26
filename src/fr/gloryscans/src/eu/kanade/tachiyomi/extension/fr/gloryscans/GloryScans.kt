c462d1563dfd7c1defce4376659e2661144bb630
KirinRaikage <manmitsu@hotmail.fr>
2024-03-26 13:46:02 +0100

    Cleanup dead sources (#2121)

--- src/fr/gloryscans/src/eu/kanade/tachiyomi/extension/fr/gloryscans/GloryScans.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.gloryscans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class GloryScans : MangaThemesia("Glory Scans", "https://gloryscans.fr", "fr", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH))
