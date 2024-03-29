196f79b9a37833aea6e3d6b14e2387d345c64c48
Secozzi <folke.steen85@gmail.com>
2024-03-29 19:57:41 +0100

    Move ezmanga to keyoapp theme

--- src/en/ezmanga/src/eu/kanade/tachiyomi/extension/en/ezmanga/EZmanga.kt
+++ src/en/ezmanga/src/eu/kanade/tachiyomi/extension/en/ezmanga/EZmanga.kt
@@ -1,19 +1,12 @@
 package eu.kanade.tachiyomi.extension.en.ezmanga
 
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import java.text.SimpleDateFormat
-import java.util.Locale
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
 
-class EZmanga : Madara(
+class EZmanga : Keyoapp(
     "EZmanga",
     "https://ezmanga.net",
     "en",
-    dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH),
 ) {
-    override val useNewChapterEndpoint = true
-
-    override val client = super.client.newBuilder()
-        .rateLimit(1)
-        .build()
+    // Migrated from Madara to Keyoapp
+    override val versionId = 2
 }
