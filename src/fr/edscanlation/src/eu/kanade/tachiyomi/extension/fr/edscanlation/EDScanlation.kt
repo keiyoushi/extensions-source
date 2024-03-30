208954a51b8c957a81b0da4c4049b7db578bb5bf
Secozzi <49240133+Secozzi@users.noreply.github.com>
2024-03-30 11:56:43 +0000

    Add edscanlation (#2162)

---
+++ src/fr/edscanlation/src/eu/kanade/tachiyomi/extension/fr/edscanlation/EDScanlation.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.fr.edscanlation
+
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+
+class EDScanlation : Keyoapp("ED Scanlation", "https://edscanlation.fr", "fr")
