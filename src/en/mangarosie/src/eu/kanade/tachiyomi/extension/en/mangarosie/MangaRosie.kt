8260f4d42c3b8e013473356024dd3c3ceb60a6dc
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-03-30 13:21:37 +0100

    Remove MangaRosie

--- src/en/mangarosie/src/eu/kanade/tachiyomi/extension/en/mangarosie/MangaRosie.kt
+++
@@ -1,8 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangarosie
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class MangaRosie : Madara("MangaRosie", "https://mangarosie.in", "en") {
-    override val useNewChapterEndpoint = false
-    override val mangaDetailsSelectorStatus = "div.summary-heading:contains(Status) + div.summary-content"
-}
