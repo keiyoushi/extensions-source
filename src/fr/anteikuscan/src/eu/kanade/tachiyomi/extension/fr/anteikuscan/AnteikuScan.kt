69b4457576a18035327d3133f6865f5246163ce2
Secozzi <49240133+Secozzi@users.noreply.github.com>
2024-03-30 11:56:20 +0000

    Add anteikuscan (#2161)

---
+++ src/fr/anteikuscan/src/eu/kanade/tachiyomi/extension/fr/anteikuscan/AnteikuScan.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.fr.anteikuscan
+
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+
+class AnteikuScan : Keyoapp("Anteiku Scan", "https://anteikuscan.fr", "fr")
