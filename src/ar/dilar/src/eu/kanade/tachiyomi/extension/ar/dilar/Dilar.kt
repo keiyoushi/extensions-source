8d3cb755740989d7af139076bfe3a67d33425b28
mohamedotaku <mohamed.otaku.2016@gmail.com>
2024-03-31 06:56:57 +0000

    override cdnUrl

--- src/ar/dilar/src/eu/kanade/tachiyomi/extension/ar/dilar/Dilar.kt
+++ src/ar/dilar/src/eu/kanade/tachiyomi/extension/ar/dilar/Dilar.kt
@@ -66,4 +66,6 @@ class Dilar :
     private val preferences: SharedPreferences by lazy {
         Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
     }
+
+    override val cdnUrl = baseUrl
 }
