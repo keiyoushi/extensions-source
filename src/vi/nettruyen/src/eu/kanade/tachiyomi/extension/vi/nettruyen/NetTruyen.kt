4bc58d52734bc39dbc66b7327059d0cab5dfb84d
haruki-takeshi <nguyenson.bkacad@gmail.com>
2024-03-31 13:28:02 +0700

    Update NetTruyen Domain (#2184)
    * Update NetTruyen.kt
    
    * Update build.gradle

--- src/vi/nettruyen/src/eu/kanade/tachiyomi/extension/vi/nettruyen/NetTruyen.kt
+++ src/vi/nettruyen/src/eu/kanade/tachiyomi/extension/vi/nettruyen/NetTruyen.kt
@@ -15,7 +15,7 @@ import java.util.Locale
 
 class NetTruyen : WPComics(
     "NetTruyen",
-    "https://www.nettruyenff.com",
+    "https://www.nettruyenhh.com",
     "vi",
     dateFormat = SimpleDateFormat("dd/MM/yy", Locale.getDefault()),
     gmtOffset = null,
