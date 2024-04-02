3454e649f39c7b446af9c3845be37ba8385aa3e9
WarmSeeker6 <130319694+WarmSeeker6@users.noreply.github.com>
2024-04-02 23:13:02 +0700

    HentaiVN URL change (#2225)
    * URL update for LXhentai
    
    * hentaivn change url

--- src/vi/hentaivn/src/eu/kanade/tachiyomi/extension/vi/hentaivn/HentaiVN.kt
+++ src/vi/hentaivn/src/eu/kanade/tachiyomi/extension/vi/hentaivn/HentaiVN.kt
@@ -43,7 +43,7 @@ class HentaiVN : ParsedHttpSource(), ConfigurableSource {
         Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
     }
 
-    private val defaultBaseUrl = "https://hentaivn.tv"
+    private val defaultBaseUrl = "https://henvn.tv"
     override val baseUrl by lazy { preferences.getString(PREF_KEY_BASE_URL, defaultBaseUrl)!! }
 
     override val name = "HentaiVN"
@@ -421,7 +421,7 @@ class HentaiVN : ParsedHttpSource(), ConfigurableSource {
     )
 
     // console.log(jQuery.makeArray($('ul.ul-search > li').map((i, e) => `Genre("${e.textContent}", "${e.children[0].value}")`)).join(',\n'))
-    // https://hentaivn.autos/forum/search-plus.php
+    // https://henvn.tv/forum/search-plus.php
     private fun getGenreList() = listOf(
         Genre("3D Hentai", "3"),
         Genre("Action", "5"),
