10e1d81fe0741773af16239aae02877cdb4cdeb4
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-05 17:20:33 -0300

    Add LeitorDeManga

---
+++ src/pt/leitordemanga/src/eu/kanade/tachiyomi/extension/pt/leitordemanga/LeitorDeManga.kt
@@ -0,0 +1,19 @@
+package eu.kanade.tachiyomi.extension.pt.leitordemanga
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class LeitorDeManga : Madara(
+    "Leitor de mangá",
+    "https://leitordemanga.com",
+    "pt-BR",
+    SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")),
+) {
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(1, 2, TimeUnit.SECONDS)
+        .build()
+}
