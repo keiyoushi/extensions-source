a7c0fc8d866d0f1b5ee6545963168287bc9198ea
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-04-05 11:32:26 +0000

    YuraManga: move to Madara (#2257)

--- src/id/yuramanga/src/eu/kanade/tachiyomi/extension/id/yuramanga/YuraManga.kt
+++ src/id/yuramanga/src/eu/kanade/tachiyomi/extension/id/yuramanga/YuraManga.kt
@@ -1,5 +1,11 @@
 package eu.kanade.tachiyomi.extension.id.yuramanga
 
-import eu.kanade.tachiyomi.multisrc.makaru.Makaru
+import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class YuraManga : Makaru("YuraManga", "https://www.yuramanga.my.id", "id")
+class YuraManga : Madara("YuraManga", "https://yuramanga.my.id", "id") {
+    // Moved from Makaru to Madara
+    override val versionId = 2
+
+    // If .list-chapter is empty the link is 404
+    override fun popularMangaSelector() = "div.page-item-detail:not(:has(a[href*='bilibilicomics.com']))$mangaEntrySelector:has(.chapter-item)"
+}
