f78210a7e5b8aec4819a9c390c14c41748f99b8c
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-06 11:17:30 -0300

    Fix no unused imports

--- src/en/spmanhwa/src/eu/kanade/tachiyomi/extension/en/spmanhwa/Spmanhwa.kt
+++ src/en/spmanhwa/src/eu/kanade/tachiyomi/extension/en/spmanhwa/Spmanhwa.kt
@@ -1,7 +1,5 @@
 package eu.kanade.tachiyomi.extension.en.spmanhwa
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class Spmanhwa : Madara("Spmanhwa", "https://spmanhwa.online", "en")
