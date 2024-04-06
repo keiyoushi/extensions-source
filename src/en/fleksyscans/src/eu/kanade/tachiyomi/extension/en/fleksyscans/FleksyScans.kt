f9798f6ad4d147fce7bf5a63fa4e9e3c99cbac46
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-04-07 00:14:10 +0500

    FleksyScans: new source

---
+++ src/en/fleksyscans/src/eu/kanade/tachiyomi/extension/en/fleksyscans/FleksyScans.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.fleksyscans
+
+import eu.kanade.tachiyomi.multisrc.cloudrecess.CloudRecess
+
+class FleksyScans : CloudRecess("FleksyScans", "https://flexscans.com", "en")
