b35b2cb0fbf2ec29c7b567a87fb57ec523d56942
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-04-06 11:44:13 +0500

    LuraToon: move to PeachScan (#2268)

--- src/pt/randomscan/src/eu/kanade/tachiyomi/extension/pt/randomscan/LuraToon.kt
+++ src/pt/randomscan/src/eu/kanade/tachiyomi/extension/pt/randomscan/LuraToon.kt
@@ -1,24 +1,10 @@
 package eu.kanade.tachiyomi.extension.pt.randomscan
 
-import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.multisrc.peachscan.PeachScan
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import okhttp3.OkHttpClient
-import java.text.SimpleDateFormat
-import java.util.Locale
-import java.util.concurrent.TimeUnit
 
-class LuraToon : Madara(
-    "Lura Toon",
-    "https://luratoon.com",
-    "pt-BR",
-    SimpleDateFormat("MMMMM dd, yyyy", Locale("pt", "BR")),
-) {
-    // Name changed from Random Scan to Lura Toon and Domain change
-    override val id: Long = 8295593204533043008
-
-    override val client: OkHttpClient = super.client.newBuilder()
-        .rateLimit(1, 2, TimeUnit.SECONDS)
+class LuraToon : PeachScan("Lura Toon", "https://luratoon.com", "pt-BR") {
+    override val client = super.client.newBuilder()
+        .rateLimit(1, 2)
         .build()
-
-    override val useNewChapterEndpoint = true
 }
