142b88cd2966754dd48d6fb34ed47a39b92088f3
Secozzi <49240133+Secozzi@users.noreply.github.com>
2024-04-06 06:46:09 +0000

    add laid back scans (#2277)

---
+++ src/en/laidbackscans/src/eu/kanade/tachiyomi/extension/en/laidbackscans/LaidBackScans.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.laidbackscans
+
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+
+class LaidBackScans : Keyoapp("Laid Back Scans", "https://laidbackscans.org", "en")
