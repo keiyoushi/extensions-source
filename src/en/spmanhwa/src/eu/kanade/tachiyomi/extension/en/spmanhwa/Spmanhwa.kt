160cbfbb98908bf54b65f1a2af3d4cb20a695915
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-06 00:38:56 -0300

    Add Spmanhwa

---
+++ src/en/spmanhwa/src/eu/kanade/tachiyomi/extension/en/spmanhwa/Spmanhwa.kt
@@ -0,0 +1,12 @@
+package eu.kanade.tachiyomi.extension.en.spmanhwa
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Spmanhwa : Madara(
+    "Spmanhwa",
+    "https://spmanhwa.online",
+    "en",
+    SimpleDateFormat("MMMMM dd, yyyy", Locale("en")),
+)
