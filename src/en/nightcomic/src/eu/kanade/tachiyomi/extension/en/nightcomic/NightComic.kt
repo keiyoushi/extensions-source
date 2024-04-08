b7f34ec66f8b5708b2d95cf19e48e72fc2c8ec94
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-04-08 01:59:40 -0700

    Added override for author detail

--- src/en/nightcomic/src/eu/kanade/tachiyomi/extension/en/nightcomic/NightComic.kt
+++ src/en/nightcomic/src/eu/kanade/tachiyomi/extension/en/nightcomic/NightComic.kt
@@ -4,4 +4,5 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 
 class NightComic : Madara("Night Comic", "https://nightcomic.com", "en") {
     override val useNewChapterEndpoint = true
+    override val mangaDetailsSelectorAuthor = "div.manga-authors > a"
 }
