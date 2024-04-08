05953421fc4a4d54a609ca303a88d6430501a51a
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-07 21:26:23 -0300

    Add MangaTerra

---
+++ src/pt/mangaterra/src/eu/kanade.tachiyomi.extension.pt.mangaterra/MangaTerra.kt
@@ -0,0 +1,193 @@
+package eu.kanade.tachiyomi.extension.pt.mangaterra
+
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.util.asJsoup
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import rx.Observable
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class MangaTerra : ParsedHttpSource() {
+    override val lang: String = "pt-BR"
+    override val supportsLatest: Boolean = true
+    override val name: String = "Manga Terra"
+    override val baseUrl: String = "https://manga-terra.com"
+
+    override val client = network.client.newBuilder()
+        .rateLimit(1, 2, TimeUnit.SECONDS)
+        .build()
+
+    override fun chapterFromElement(element: Element) = SChapter.create().apply {
+        name = element.selectFirst("h5")!!.ownText()
+        date_upload = element.selectFirst("h5 > div")!!.ownText().toDate()
+        setUrlWithoutDomain(element.absUrl("href"))
+    }
+
+    override fun chapterListSelector() = ".card-list-chapter a"
+
+    override fun imageUrlParse(document: Document) = ""
+
+    override fun latestUpdatesFromElement(element: Element) = popularMangaFromElement(element)
+
+    override fun latestUpdatesNextPageSelector() = popularMangaNextPageSelector()
+
+    override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/manga?q=u&page=$page", headers)
+
+    override fun latestUpdatesSelector() = popularMangaSelector()
+
+    override fun mangaDetailsParse(document: Document) = SManga.create().apply {
+        title = document.selectFirst(".card-body h1")!!.ownText()
+        description = document.selectFirst(".card-body p")?.ownText()
+        thumbnail_url = document.selectFirst(".card-body img")?.absUrl("src")
+        genre = document.select(".card-series-about a").joinToString { it.ownText() }
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        val pages = mutableListOf<Page>()
+        var page = 1
+        do {
+            val dom = client.newCall(GET("${document.location()}/${page++}", headers)).execute().asJsoup()
+            pages += Page(index = page, imageUrl = dom.selectFirst("img")?.absUrl("src"))
+        } while (dom.selectFirst(".btn-next") != null)
+
+        return pages
+    }
+
+    override fun popularMangaFromElement(element: Element) = SManga.create().apply {
+        title = element.selectFirst("p")!!.ownText()
+        thumbnail_url = element.selectFirst("img")?.srcAttr()
+        setUrlWithoutDomain(element.selectFirst("a")!!.absUrl("href"))
+    }
+
+    override fun popularMangaNextPageSelector() = ".pagination > .page-item:not(.disabled):last-child"
+
+    override fun popularMangaRequest(page: Int) = GET("$baseUrl/manga?q=p&page=$page", headers)
+
+    override fun popularMangaSelector(): String = ".card-body .row > div"
+
+    override fun searchMangaFromElement(element: Element) = popularMangaFromElement(element)
+
+    override fun searchMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select(searchMangaSelector()).map { element ->
+            searchMangaFromElement(element)
+        }
+        return MangasPage(
+            mangas = mangas,
+            hasNextPage = document.selectFirst(searchMangaNextPageSelector()) != null,
+        )
+    }
+
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> {
+        val request = searchMangaRequest(page, query, filters)
+        val response = client.newCall(request).execute()
+        val pathSegments = response.request.url.pathSegments
+        if (pathSegments.contains("search")) {
+            Observable.just(MangasPage(mangas = emptyList(), false))
+        }
+        return Observable.just(searchMangaParse(response))
+    }
+
+    override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val url = "$baseUrl".toHttpUrl().newBuilder()
+
+        if (query.isNotBlank()) {
+            url.addPathSegment("search")
+            url.addQueryParameter("q", query)
+            return GET(url.build())
+        }
+
+        url.addPathSegment("manga")
+
+        filters.forEach { filter ->
+            when (filter) {
+                is GenreFilter -> {
+                    filter.state.forEach {
+                        if (it.state) {
+                            url.addQueryParameter(it.query, it.value)
+                        }
+                    }
+                }
+                else -> {}
+            }
+        }
+
+        url.addQueryParameter("page", "$page")
+
+        return GET(url.build(), headers)
+    }
+
+    override fun searchMangaSelector() = popularMangaSelector()
+
+    override fun getFilterList(): FilterList {
+        CoroutineScope(Dispatchers.IO).launch { fetchGenres() }
+        val filters = mutableListOf(
+            GenreFilter(
+                title = "Categorias",
+                genres = genresList,
+            ),
+        )
+        return FilterList(filters)
+    }
+
+    private fun Element.srcAttr(): String = when {
+        hasAttr("data-src") -> absUrl("data-src")
+        else -> absUrl("src")
+    }
+
+    private fun String.toDate() = try { dateFormat.parse(trim())!!.time } catch (_: Exception) { 0L }
+
+    var fetchGenresAttempts: Int = 0
+
+    fun fetchGenres() {
+        if (fetchGenresAttempts < 3 && genresList.isEmpty()) {
+            try {
+                genresList = client.newCall(GET("$baseUrl/manga")).execute()
+                    .use { parseGenres(it.asJsoup()) }
+            } catch (_: Exception) {
+            } finally {
+                fetchGenresAttempts++
+            }
+        }
+    }
+
+    fun parseGenres(document: Document): List<Genre> {
+        return document.select(".form-filters .custom-checkbox")
+            .map { element ->
+                val input = element.selectFirst("input")!!
+                Genre(
+                    name = element.selectFirst("label")!!.ownText(),
+                    query = input.attr("name"),
+                    value = input.attr("value"),
+                )
+            }
+    }
+
+    companion object {
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale("pt", "BR"))
+    }
+}
+
+class GenreFilter(title: String, genres: List<Genre>) :
+    Filter.Group<Genre>(title, genres)
+
+class Genre(name: String, val query: String, val value: String) : Filter.CheckBox(name)
+var genresList: List<Genre> = emptyList()
---
+++ src/pt/mangaterra/src/eu/kanade.tachiyomi.extension.pt.mangaterra/MangaTerraUrlActivity.kt
@@ -0,0 +1,39 @@
+package eu.kanade.tachiyomi.extension.pt.mangaterra
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import kotlin.system.exitProcess
+
+/**
+ * Springboard that accepts https://blackoutcomics.com/comics/<item> intents
+ * and redirects them to the main Tachiyomi process.
+ */
+class MangaTerraUrlActivity : Activity() {
+
+    private val tag = javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val pathSegments = intent?.data?.pathSegments
+        if (pathSegments != null && pathSegments.size > 1) {
+            val mainIntent = Intent().apply {
+                action = "eu.kanade.tachiyomi.SEARCH"
+                putExtra("filter", packageName)
+            }
+
+            try {
+                startActivity(mainIntent)
+            } catch (e: ActivityNotFoundException) {
+                Log.e(tag, e.toString())
+            }
+        } else {
+            Log.e(tag, "could not parse uri from intent $intent")
+        }
+
+        finish()
+        exitProcess(0)
+    }
+}
