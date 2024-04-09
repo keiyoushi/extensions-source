48577024a6fa0b58aea54bd71c4450d086dd5ebd
Norsze <66540317+Norsze@users.noreply.github.com>
2024-04-09 13:26:32 +0200

    Add Serein Scan

---
+++ src/tr/sereinscan/src/eu/kanade/tachiyomi/extension/tr/sereinscan/SereinScan.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.tr.sereinscan
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class SereinScan : MangaThemesia(
+    "Serein Scan",
+    "https://sereinscan.com/",
+    "tr",
+)
