429bca859ee0338aaa67ae4fa693ae3a983c8b38
Karuto <nguyenthaison609@outlook.com>
2024-04-11 22:45:30 +0700

    Broken commit

---
+++ src/vi/tusachxinhxinh/src/eu/kanade/tachiyomi/extension/vi/tusachxinhxinh/tusachxinhxinh.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.vi.tusachxinhxinh
+
+import eu.kanade.tachiyomi.multisrc.a3manga
+
+class A3Manga : A3Manga("Tủ sách xinh xinh", "https://tusachxinh.online", "vi")
