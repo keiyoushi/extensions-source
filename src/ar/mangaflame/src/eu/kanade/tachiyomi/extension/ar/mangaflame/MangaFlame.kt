e47ba7c7d474268e57ebafc9e769d709e116c35d
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-04-11 06:04:14 +0000

    Manga Flame: update domain (reverts #1931) (#2335)
    * Manga Flame: update domain (reverts #1931)
    
    * remove unused import

--- src/ar/mangaflame/src/eu/kanade/tachiyomi/extension/ar/mangaflame/MangaFlame.kt
+++ src/ar/mangaflame/src/eu/kanade/tachiyomi/extension/ar/mangaflame/MangaFlame.kt
@@ -1,20 +1,14 @@
 package eu.kanade.tachiyomi.extension.ar.mangaflame
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import okhttp3.OkHttpClient
 import java.text.SimpleDateFormat
 import java.util.Locale
-import java.util.concurrent.TimeUnit
 
 class MangaFlame : MangaThemesia(
     "Manga Flame",
-    "https://arisescans.com",
+    "https://mangaflame.org",
     "ar",
     dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("ar")),
 ) {
     override val id = 1501237443119573205
-
-    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
-        .readTimeout(3, TimeUnit.MINUTES)
-        .build()
 }
