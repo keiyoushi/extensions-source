37d4b89cd83ce8b828b0b7ca536e296d178ab3b7
bapeey <90949336+bapeey@users.noreply.github.com>
2024-04-11 23:59:03 -0500

    Bump versionId

--- src/es/templescanesp/src/eu/kanade/tachiyomi/extension/es/templescanesp/TempleScanEsp.kt
+++ src/es/templescanesp/src/eu/kanade/tachiyomi/extension/es/templescanesp/TempleScanEsp.kt
@@ -2,4 +2,8 @@ package eu.kanade.tachiyomi.extension.es.templescanesp
 
 import eu.kanade.tachiyomi.multisrc.mangaesp.MangaEsp
 
-class TempleScanEsp : MangaEsp("Temple Scan", "https://templescanesp.net", "es")
+class TempleScanEsp : MangaEsp("Temple Scan", "https://templescanesp.net", "es") {
+
+    // Site moved from custom theme to MangaEsp
+    override val versionId = 3
+}
