6fa5021d887d8b9b3a30efb6da5a84393eb05c25
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-04-13 08:19:54 +0200

    Add ZinChanManga.com

---
+++ src/en/zinchanmangacom/src/eu/kanade/tachiyomi/extension/en/zinchanmangacom/ZinChanMangaCom.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.zinchanmangacom
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class ZinChanMangaCom : Madara("ZinChanManga.com", "https://zinchanmanga.com", "en")
