48706291387e2856bae249461d894cc683a723d7
bapeey <90949336+bapeey@users.noreply.github.com>
2024-04-12 21:26:47 -0500

    intellij > android studio

--- src/es/templescanesp/src/eu/kanade/tachiyomi/extension/es/templescanesp/TempleScanEsp.kt
+++ src/es/templescanesp/src/eu/kanade/tachiyomi/extension/es/templescanesp/TempleScanEsp.kt
@@ -1,9 +1,15 @@
 package eu.kanade.tachiyomi.extension.es.templescanesp
 
 import eu.kanade.tachiyomi.multisrc.mangaesp.MangaEsp
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.SChapter
 
 class TempleScanEsp : MangaEsp("Temple Scan", "https://templescanesp.net", "es") {
 
     // Site moved from custom theme to MangaEsp
     override val versionId = 3
+
+    private val readerDomain = "https://templescanesp.xyz"
+
+    override fun pageListRequest(chapter: SChapter) = GET(readerDomain + chapter.url, headers)
 }
