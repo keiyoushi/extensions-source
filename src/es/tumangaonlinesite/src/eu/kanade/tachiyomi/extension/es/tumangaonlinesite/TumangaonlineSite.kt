e10c07924e79958ef6803a026680e3bab4fbe96a
bapeey <90949336+bapeey@users.noreply.github.com>
2024-04-15 05:48:15 -0500

    Remove dead sources (#2434)
    * Remove Mangadoor
    
    Id: 5598298311107274167
    
    * Remove Last Knight Translation
    
    Id: 4608749082635217570
    
    * Remove TuMangaOnline.site
    
    Id: 8850093899291265785
    
    * Remove Unitoon Oficial
    
    Id: 2422714322713320966
    
    * Remove Shayami
    
    Id: 3984924321110067740
    
    * Remove Eromiau
    
    Id: 7532305797477636173
    
    * Remove Cocorip
    
    Id: 2811607756759454878
    
    * Revert "Remove Cocorip"
    
    This reverts commit 8c1e5588cab9e3f3ddd7b855420307f365f568d9.
    
    * Reapply "Remove Cocorip"
    
    smh i forgot why i remove it
    
    * Revert "Reapply "Remove Cocorip"" XD
    
    This reverts commit f5240304d876433c25e6a976a2ce04aa0fc405d2.
    
    * Remove MangaPT
    
    ID: 4501201462814418979

--- src/es/tumangaonlinesite/src/eu/kanade/tachiyomi/extension/es/tumangaonlinesite/TumangaonlineSite.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.es.tumangaonlinesite
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class TumangaonlineSite : Madara("Tumangaonline.site", "https://tumangaonline.site", "es", SimpleDateFormat("dd MMMMM, yyyy", Locale("es")))
