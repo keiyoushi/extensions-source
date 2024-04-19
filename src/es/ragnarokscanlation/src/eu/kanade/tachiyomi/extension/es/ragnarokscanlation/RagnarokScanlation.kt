7993fce6c9c8e5090a65cbb8af0b9413f20157d4
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-19 20:12:09 -0300

    Remove unused import

--- src/es/ragnarokscanlation/src/eu/kanade/tachiyomi/extension/es/ragnarokscanlation/RagnarokScanlation.kt
+++ src/es/ragnarokscanlation/src/eu/kanade/tachiyomi/extension/es/ragnarokscanlation/RagnarokScanlation.kt
@@ -1,7 +1,5 @@
 package eu.kanade.tachiyomi.extension.es.ragnarokscanlation
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class RagnarokScanlation : Madara("Ragnarok Scanlation", "https://ragnarokscanlation.org", "es")
