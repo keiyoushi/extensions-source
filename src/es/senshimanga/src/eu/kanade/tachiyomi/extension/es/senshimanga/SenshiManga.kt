968dd20d8c98ad4f9e20ac56e4ca43f61279971f
bapeey <90949336+bapeey@users.noreply.github.com>
2024-04-18 22:24:52 -0500

    Add SenshiManga

---
+++ src/es/senshimanga/src/eu/kanade/tachiyomi/extension/es/senshimanga/SenshiManga.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.es.senshimanga
+
+import eu.kanade.tachiyomi.multisrc.latmanga.LatManga
+
+class SenshiManga : LatManga("Senshi Manga", "https://senshimanga.com", "es")
