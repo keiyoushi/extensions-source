87b34c616648898a5518749c07d775c810e9a24a
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-20 18:50:58 -0300

    Add GalaxScanlator

---
+++ src/pt/galaxscanlator/src/eu/kanade/tachiyomi/extension/pt/galaxscanlator/GalaxScanlator.kt
@@ -0,0 +1,30 @@
+package eu.kanade.tachiyomi.extension.pt.galaxscanlator
+
+import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.Request
+import java.util.concurrent.TimeUnit
+
+class GalaxScanlator : ZeistManga(
+    "GALAX Scans",
+    "https://galaxscanlator.blogspot.com",
+    "pt-BR",
+) {
+    override val client = super.client.newBuilder()
+        .rateLimit(5, 2, TimeUnit.SECONDS)
+        .build()
+
+    override fun popularMangaRequest(page: Int): Request =
+        GET("$baseUrl/search/label/Mangá?max-results=150")
+
+    override val popularMangaSelector = ".blog-posts > section"
+    override val popularMangaSelectorTitle: String = "h2"
+    override val popularMangaSelectorUrl = "div > a"
+
+    override val mangaDetailsSelector = ".grid.gta-series"
+    override val mangaDetailsSelectorGenres = "aside > div > dl dt:contains(Genre) + dd > a[rel=tag]"
+
+    override val chapterCategory = "Capítulo"
+    override val pageListSelector = "#reader"
+}
