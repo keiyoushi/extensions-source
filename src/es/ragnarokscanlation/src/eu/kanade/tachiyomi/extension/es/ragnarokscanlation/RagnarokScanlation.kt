110fb214a8ac53058dc5891d924e81f0955d3921
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-20 16:00:34 -0300

    Update versionId

--- src/es/ragnarokscanlation/src/eu/kanade/tachiyomi/extension/es/ragnarokscanlation/RagnarokScanlation.kt
+++ src/es/ragnarokscanlation/src/eu/kanade/tachiyomi/extension/es/ragnarokscanlation/RagnarokScanlation.kt
@@ -2,4 +2,10 @@ package eu.kanade.tachiyomi.extension.es.ragnarokscanlation
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class RagnarokScanlation : Madara("Ragnarok Scanlation", "https://ragnarokscanlation.org", "es")
+class RagnarokScanlation : Madara(
+    "Ragnarok Scanlation",
+    "https://ragnarokscanlation.org",
+    "es"
+) {
+    override val versionId: Int = 2
+}
