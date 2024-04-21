fcace1f235d20e47e7f2982cca531ac2f20543c4
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-21 04:10:24 -0300

    MangaShiina:  Update baseUrl and rebrand (#2495)
    * Update baseUrl and rebrand
    
    * Add isNsfw
    
    * Fix source name
    
    * Keeps package name

--- src/es/mangashiina/src/eu/kanade/tachiyomi/extension/es/mangashiina/MangaMukai.kt
+++ src/es/mangashiina/src/eu/kanade/tachiyomi/extension/es/mangashiina/MangaMukai.kt
@@ -4,9 +4,11 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class MangaShiina : MangaThemesia(
-    "MangaShiina",
-    "https://mangashiina.com",
+class MangaMukai : MangaThemesia(
+    "Manga Mukai",
+    "https://mangamukai.com",
     "es",
     dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
-)
+) {
+    override val id: Long = 711368877221654433
+}
