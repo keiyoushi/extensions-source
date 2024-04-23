bd1678f40a10236656231f77c396c0bb378ef7c2
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-04-23 08:01:12 +0000

    Remove Phantom Scans (#2525)

--- src/en/phantomscans/src/eu/kanade/tachiyomi/extension/en/phantomscans/PhantomScans.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.phantomscans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class PhantomScans : MangaThemesia("Phantom Scans", "https://phantomscans.com", "en")
