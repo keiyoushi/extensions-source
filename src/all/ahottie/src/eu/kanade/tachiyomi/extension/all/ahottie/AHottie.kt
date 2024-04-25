1e0a260c66ed280a5b6d0b6c0fc2b465bcb3635b
Fadl Olawumi <chaos@efqr.dev>
2024-04-25 20:05:45 +0100

    resolve suggestions

--- src/all/ahottie/src/eu/kanade/tachiyomi/extension/all/ahottie/AHottie.kt
+++ src/all/ahottie/src/eu/kanade/tachiyomi/extension/all/ahottie/AHottie.kt
@@ -1,16 +1,17 @@
 package eu.kanade.tachiyomi.extension.all.ahottie
 
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -28,6 +29,7 @@ class AHottie() : ParsedHttpSource() {
         }
         title = element.select("h2").text()
         setUrlWithoutDomain(element.select("a").attr("href"))
+        initialized = true
     }
 
     override fun popularMangaNextPageSelector() = "a[rel=next]"
@@ -43,7 +45,13 @@ class AHottie() : ParsedHttpSource() {
     override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         return when {
-            query.isNotEmpty() -> GET("$baseUrl/search?kw=$query&page=$page")
+            query.isNotEmpty() -> GET(
+                baseUrl.toHttpUrl().newBuilder().apply {
+                    addPathSegment("search")
+                    addQueryParameter("kw", query)
+                    addQueryParameter("page", page.toString())
+                }.build(),
+            )
             else -> popularMangaRequest(page)
         }
     }
@@ -73,13 +81,10 @@ class AHottie() : ParsedHttpSource() {
     }
 
     override fun chapterFromElement(element: Element) = SChapter.create().apply {
-        val pages = element.select("[role=navigation] :nth-last-child(2)")?.text() ?: "1"
-        setUrlWithoutDomain(element.select("link[rel=canonical]").attr("abs:href") + "#pages=$pages")
+        setUrlWithoutDomain(element.select("link[rel=canonical]").attr("abs:href"))
         chapter_number = 0F
         name = "GALLERY"
-        date_upload =
-            SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(element.select("time").text())?.time
-                ?: 0L
+        date_upload = getDate(element.select("time").text())
     }
 
     override fun chapterListSelector() = "html"
@@ -103,8 +108,17 @@ class AHottie() : ParsedHttpSource() {
         throw UnsupportedOperationException()
     }
 
-    // Filters
-    override fun getFilterList(): FilterList = FilterList(
-        Filter.Header("Filters not implemented, use search"),
-    )
+    private fun getDate(str: String): Long {
+        return try {
+            DATE_FORMAT.parse(str)?.time ?: 0L
+        } catch (e: ParseException) {
+            0L
+        }
+    }
+
+    companion object {
+        private val DATE_FORMAT by lazy {
+            SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
+        }
+    }
 }
