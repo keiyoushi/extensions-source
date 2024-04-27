7e743131056e8b4e5ed733e12034103397c942e0
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-27 17:08:44 -0300

    Remove id

--- src/pt/cerisescans/src/eu/kanade/tachiyomi/extension/pt/cerisescans/CeriseScan.kt
+++ src/pt/cerisescans/src/eu/kanade/tachiyomi/extension/pt/cerisescans/CeriseScan.kt
@@ -4,10 +4,7 @@ import eu.kanade.tachiyomi.multisrc.peachscan.PeachScan
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
 class CeriseScan : PeachScan("Cerise Scan", "https://cerisetoon.com", "pt-BR") {
-    override val id: Long = 8629915907358523454
-
     override val versionId: Int = 2
-
     override val client = super.client.newBuilder()
         .rateLimit(1, 2)
         .build()
