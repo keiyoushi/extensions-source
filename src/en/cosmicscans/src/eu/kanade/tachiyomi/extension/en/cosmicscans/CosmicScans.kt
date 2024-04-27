c26d6c543f73dabe880f77698ca0acb903c6d2d3
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-04-27 08:51:16 +0000

    Remove Cosmic Scans (#2603)

--- src/en/cosmicscans/src/eu/kanade/tachiyomi/extension/en/cosmicscans/CosmicScans.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.cosmicscans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class CosmicScans : MangaThemesia("Cosmic Scans", "https://cosmic-scans.com", "en") {
-    override val pageSelector = "div#readerarea img[data-src]"
-}
