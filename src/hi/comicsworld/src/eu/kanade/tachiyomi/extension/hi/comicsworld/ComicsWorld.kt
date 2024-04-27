62bda40d21a9684f23cf413b2a005260fb7379f6
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-27 05:45:50 -0300

    Remove ComicsWorld (#2597)

--- src/hi/comicsworld/src/eu/kanade/tachiyomi/extension/hi/comicsworld/ComicsWorld.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.hi.comicsworld
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class ComicsWorld : Madara("ComicsWorld", "https://comicsworld.in", "hi")
