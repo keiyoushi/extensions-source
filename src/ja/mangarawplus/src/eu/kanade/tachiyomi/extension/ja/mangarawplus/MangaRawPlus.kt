d51bb49c4cb3b10b8de602d659ceb2820d29df7d
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-04-27 09:58:26 +0200

    MANGARAW+: update domain

--- src/ja/mangarawplus/src/eu/kanade/tachiyomi/extension/ja/mangarawplus/MangaRawPlus.kt
+++ src/ja/mangarawplus/src/eu/kanade/tachiyomi/extension/ja/mangarawplus/MangaRawPlus.kt
@@ -2,7 +2,8 @@ package eu.kanade.tachiyomi.extension.ja.mangarawplus
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class MangaRawPlus : Madara("MANGARAW+", "https://mangarawplus.org", "ja") {
+class MangaRawPlus : Madara("MANGARAW+", "https://newmangaraw.com", "ja") {
     override val mangaSubString = "sp"
-    override val useLoadMoreRequest = LoadMoreStrategy.Always
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = false
 }
