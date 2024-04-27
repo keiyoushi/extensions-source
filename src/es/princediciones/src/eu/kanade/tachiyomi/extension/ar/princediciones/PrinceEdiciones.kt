ecf70a82d9eede02000e61c8b6b18b84dea4526e
mohamedotaku <mohamed.otaku.2016@gmail.com>
2024-04-27 20:51:59 +0100

    Add source Prince Ediciones "es"

---
+++ src/es/princediciones/src/eu/kanade/tachiyomi/extension/ar/princediciones/PrinceEdiciones.kt
@@ -0,0 +1,14 @@
+package eu.kanade.tachiyomi.extension.es.princediciones
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class PrinceEdiciones : Madara(
+    "Prince Ediciones",
+    "https://princediciones.com",
+    "es",
+    SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
+) {
+    override val mangaSubString = "media"
+}
