17eef9e7c8eb34beb2bad2fb923a7628cac65343
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-04-27 15:51:23 +0200

    Remove unnecessary overrides, sort chapters

--- src/tr/raindropfansub/src/eu/kanade/tachiyomi/extension/tr/raindropfansub/RaindropFansub.kt
+++ src/tr/raindropfansub/src/eu/kanade/tachiyomi/extension/tr/raindropfansub/RaindropFansub.kt
@@ -1,6 +1,9 @@
 package eu.kanade.tachiyomi.extension.tr.raindropfansub
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.SChapter
+import okhttp3.Response
+import org.jsoup.Jsoup
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -8,18 +11,33 @@ class RaindropFansub : MangaThemesia(
     "Raindrop Fansub",
     "https://www.raindropteamfan.com",
     "tr",
-    dateFormat = SimpleDateFormat("MMM d, yyy", Locale("tr")),
+    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("tr")),
 ) {
-    override val seriesAuthorSelector = ".fmed b:contains(Yazar) + span"
-    override val seriesArtistSelector = ".fmed b:contains(Çizer) + span"
     override val seriesTypeSelector = ".tsinfo .imptdt:contains(Tür) a"
-    override val seriesStatusSelector = ".tsinfo .imptdt:contains(Durum) i"
 
-    override fun String?.parseStatus(): Int = when {
-        this == null -> SManga.UNKNOWN
-        listOf("güncel", "devam ediyor").any { this.contains(it, ignoreCase = true) } -> SManga.ONGOING
-        this.contains("tamamlandı", ignoreCase = true) -> SManga.COMPLETED
-        this.contains("bırakıldı", ignoreCase = true) -> SManga.CANCELLED
-        else -> SManga.UNKNOWN
+    override fun chapterListParse(response: Response): List<SChapter> {
+        // "İlk Bölüm" points to the first chapter, but is often wrong on the site
+        // We look at "Son Bölüm" to find the last chapter and sort accordingly
+        val document = Jsoup.parse(response.peekBody(Long.MAX_VALUE).string())
+        val chapters = super.chapterListParse(response)
+
+        return document.selectFirst("a:has(.epcurlast)")
+            ?.attr("href")
+            ?.let {
+                val dummyChapter = SChapter.create()
+                dummyChapter.setUrlWithoutDomain(it)
+                val url = dummyChapter.url
+
+                if (chapters.first().url == url) {
+                    // Last chapter was in the first index, descending as expected by Tachiyomi API
+                    chapters
+                } else if (chapters.last().url == url) {
+                    // Last chapter was in the last index, ascending, we reverse it
+                    chapters.reversed()
+                } else {
+                    // Last chapter wasn't found. Site sort is ascending, we reverse it
+                    chapters.reversed()
+                }
+            } ?: chapters.reversed() // Missing button. Site sort is ascending, we reverse it
     }
 }
