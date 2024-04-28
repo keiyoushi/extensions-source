1b93df87e057a8a69ddb75f3afcf445e915aca7a
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-04-28 13:51:04 +0700

    add multi-src: GalleryAdults (#2553)
    * Convert AsmHentai into multisrc GalleryAdults
    
    Also convert some selector into function
    
    * Move HentaiFox to theme GalleryAdults
    
    * GalleryAdults: Fix search
    
    * MangaFox: fix TagFilter
    
    * fast page load & preference for slowly parsing image’s URL
    
    * AsmHentai: change shortTitle reference from list to swith
    
    * HentaiFox: add Korean
    
    * move HentaiFox from en to all
    
    * fix build
    
    * fix search: convert space to +
    
    * Request for tags list from site
    
    * Fix request for user’s favorites
    
    * - Optimize popular/latest request
    
    - Improve ‘page’ param
    - AsmHentai: support Latest/Popular
    
    * add SortFilter
    
    * Support multiple tags filter
    
    * Support exact match query
    
    * getTime
    
    * Fix Lang when searching
    
    * fix searchById
    
    * add language companion
    
    * Fix URL action
    
    * renovate
    
    * Support parsing json for page list
    
    Fix generating page if less than 10 pages
    HentaiFox: Random server selection
    
    * Migrate IMHentai to GalleryAdults
    
    * Preferences to support all methods for page querying
    
    * IMHentai: tagList
    
    * Expose some filters to child class, add more space to description
    
    * Fix Factory lang
    
    * Support browsing tags, speechless & favorite
    
    * IMHentai:
    
    - support favorite browsing (require login)
    - tag filter with queried popular tags
    - advanced search for artist, group, character, parody, tag (include/exclude)
    - remove language filters
    - Fix language search
    
    * Move advance search to multi-src
    
    * Fix: hide speechless when not supported
    
    * add Hint to use comma
    
    * split code to Filters & Utils
    
    * bump version all 3 extensions
    
    * fix getTime
    
    * fix lint
    
    * Fix alternative name
    
    * improve cleanTag
    
    * move out of Object
    
    * move Regex out
    
    * remove RandomUA
    
    * fix build
    
    * remove images parsing setting, pick a default one
    
    * fix build
    
    * Move shortTitle to base clash
    
    * HentaiFox: add language keyword to search query
    
    * if all mangas in current searching page is of other language then include at least 1 entry so it can request for next page
    
    * Alternative methods for images parsing
    Revert "remove images parsing setting, pick a default one"
    
    This reverts commit e49e3aaeb74e3643abc2e303924da18a52491793.
    
    # Conflicts:
    #	lib-multisrc/galleryadults/src/eu/kanade/tachiyomi/multisrc/galleryadults/GalleryAdults.kt
    #	src/all/asmhentai/src/eu/kanade/tachiyomi/extension/all/asmhentai/AsmHentai.kt
    #	src/all/imhentai/src/eu/kanade/tachiyomi/extension/all/imhentai/IMHentai.kt
    
    * Fall back if failed to decode JSON
    
    * remove supportLatest from base class
    
    * Remove preference for parsing page by page, switch to override val instead.
    
    * Split searchRequest into parts
    
    * Don't using generic Filter.Text to avoid other kind of text field which extensions might have

---
+++ src/all/hentaifox/src/eu/kanade/tachiyomi/extension/all/hentaifox/HentaiFox.kt
@@ -0,0 +1,97 @@
+package eu.kanade.tachiyomi.extension.all.hentaifox
+
+import eu.kanade.tachiyomi.multisrc.galleryadults.GalleryAdults
+import eu.kanade.tachiyomi.multisrc.galleryadults.toDate
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import kotlin.random.Random
+
+class HentaiFox(
+    lang: String = "all",
+    override val mangaLang: String = LANGUAGE_MULTI,
+) : GalleryAdults(
+    "HentaiFox",
+    "https://hentaifox.com",
+    lang = lang,
+    mangaLang = mangaLang,
+    simpleDateFormat = null,
+) {
+    override val supportsLatest = mangaLang.isNotBlank()
+
+    private val languages: List<Pair<String, String>> = listOf(
+        Pair(LANGUAGE_ENGLISH, "1"),
+        Pair(LANGUAGE_TRANSLATED, "2"),
+        Pair(LANGUAGE_JAPANESE, "5"),
+        Pair(LANGUAGE_CHINESE, "6"),
+        Pair(LANGUAGE_KOREAN, "11"),
+    )
+    private val langCode = languages.firstOrNull { lang -> lang.first == mangaLang }?.second
+
+    override fun Element.mangaLang() = attr("data-languages")
+        .split(' ').let {
+            when {
+                it.contains(langCode) -> mangaLang
+                // search result doesn't have "data-languages" which will return a list with 1 blank element
+                it.size > 1 || (it.size == 1 && it.first().isNotBlank()) -> "other"
+                // if we don't know which language to filter then set to mangaLang to not filter at all
+                else -> mangaLang
+            }
+        }
+
+    override fun Element.mangaTitle(selector: String): String? = mangaFullTitle(selector)
+
+    override fun Element.getTime(): Long {
+        return selectFirst(".pages:contains(Posted:)")?.ownText()
+            ?.removePrefix("Posted: ")
+            .toDate(simpleDateFormat)
+    }
+
+    override fun HttpUrl.Builder.addPageUri(page: Int): HttpUrl.Builder {
+        val url = toString()
+        when {
+            url == "$baseUrl/" && page == 2 ->
+                addPathSegments("page/$page")
+            url.contains('?') ->
+                addQueryParameter("page", page.toString())
+            page > 1 ->
+                addPathSegments("pag/$page")
+        }
+        addPathSegment("") // trailing slash (/)
+        return this
+    }
+
+    /* Pages */
+    override val pagesRequest = "includes/thumbs_loader.php"
+
+    override fun getServer(document: Document, galleryId: String): String {
+        val domain = baseUrl.toHttpUrl().host
+        // Randomly choose between servers
+        return if (Random.nextBoolean()) "i2.$domain" else "i.$domain"
+    }
+
+    /**
+     * Convert space( ) typed in search-box into plus(+) in URL. Then:
+     * - ignore the word preceding by a special character (e.g. 'school-girl' will ignore 'girl')
+     *    => replace to plus(+),
+     * - use plus(+) for separate terms, as AND condition.
+     * - use double quote(") to search for exact match.
+     */
+    override fun buildQueryString(tags: List<String>, query: String): String {
+        val regexSpecialCharacters = Regex("""[^a-zA-Z0-9"]+(?=[a-zA-Z0-9"])""")
+        return (tags + query + mangaLang).filterNot { it.isBlank() }.joinToString("+") {
+            it.trim().replace(regexSpecialCharacters, "+")
+        }
+    }
+
+    override fun getFilterList() = FilterList(
+        listOf(
+            Filter.Header("HINT: Use double quote (\") for exact match"),
+        ) + super.getFilterList().list,
+    )
+
+    override val idPrefixUri = "gallery"
+}
