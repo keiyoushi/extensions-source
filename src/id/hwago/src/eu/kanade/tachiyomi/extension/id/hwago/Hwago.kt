c9e3662f761c25be94f2922f1c0969289dfaa4af
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-04-28 03:12:01 +0000

    Add Hwago (#2618)

---
+++ src/id/hwago/src/eu/kanade/tachiyomi/extension/id/hwago/Hwago.kt
@@ -0,0 +1,18 @@
+package eu.kanade.tachiyomi.extension.id.hwago
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Hwago : Madara(
+    "Hwago",
+    "https://hwago.org",
+    "id",
+    dateFormat = SimpleDateFormat("d MMMM yyyy", Locale("en")),
+) {
+    override val useNewChapterEndpoint = true
+
+    override val mangaDetailsSelectorStatus = "div.summary-heading:contains(Status) + div.summary-content"
+
+    override val mangaSubString = "komik"
+}
