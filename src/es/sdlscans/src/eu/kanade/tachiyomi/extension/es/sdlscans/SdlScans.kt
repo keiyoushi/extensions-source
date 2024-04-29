3ecf3f181ec98ce94126a2cc3b35f6189a1cb333
bapeey <90949336+bapeey@users.noreply.github.com>
2024-04-29 05:37:08 -0500

    Remove SDLScans (#2650)

--- src/es/sdlscans/src/eu/kanade/tachiyomi/extension/es/sdlscans/SdlScans.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.es.sdlscans
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class SdlScans : Madara("Sdl scans", "https://sdlscans.com", "es", SimpleDateFormat("MMMMM dd, yyyy", Locale("es")))
