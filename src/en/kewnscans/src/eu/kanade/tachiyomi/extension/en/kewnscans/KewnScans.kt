e004d75848868adfe5feb357e381146181be036a
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-04-30 10:38:07 +0500

    KewnScans: remove hidden images (#2665)
    PeepoJoker

--- src/en/kewnscans/src/eu/kanade/tachiyomi/extension/en/kewnscans/KewnScans.kt
+++ src/en/kewnscans/src/eu/kanade/tachiyomi/extension/en/kewnscans/KewnScans.kt
@@ -1,5 +1,14 @@
 package eu.kanade.tachiyomi.extension.en.kewnscans
 
 import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+import eu.kanade.tachiyomi.source.model.Page
+import org.jsoup.nodes.Document
 
-class KewnScans : Keyoapp("Kewn Scans", "https://kewnscans.org", "en")
+class KewnScans : Keyoapp("Kewn Scans", "https://kewnscans.org", "en") {
+    override fun pageListParse(document: Document): List<Page> {
+        return document.select("#pages > img:not(.hidden)").map {
+            val index = it.attr("count").toInt()
+            Page(index, document.location(), it.imgAttr("150"))
+        }
+    }
+}
