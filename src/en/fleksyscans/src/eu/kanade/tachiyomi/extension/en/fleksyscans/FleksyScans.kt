32e464330def19b9f25521accd926f853477a0d3
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-04-30 10:38:26 +0500

    FleksyScans: filter out vip chapters (#2666)

--- src/en/fleksyscans/src/eu/kanade/tachiyomi/extension/en/fleksyscans/FleksyScans.kt
+++ src/en/fleksyscans/src/eu/kanade/tachiyomi/extension/en/fleksyscans/FleksyScans.kt
@@ -2,4 +2,6 @@ package eu.kanade.tachiyomi.extension.en.fleksyscans
 
 import eu.kanade.tachiyomi.multisrc.fuzzydoodle.FuzzyDoodle
 
-class FleksyScans : FuzzyDoodle("FleksyScans", "https://flexscans.com", "en")
+class FleksyScans : FuzzyDoodle("FleksyScans", "https://flexscans.com", "en") {
+    override fun chapterListSelector() = "div#chapters-list > a[href]:not(:has(span:contains(vip)))"
+}
