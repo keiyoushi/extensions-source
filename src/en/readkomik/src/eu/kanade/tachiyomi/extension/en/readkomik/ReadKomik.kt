b0062345e77f28119c8f724c5d05da6a369471d7
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-04-29 23:53:00 -0300

    Update domain

--- src/en/readkomik/src/eu/kanade/tachiyomi/extension/en/readkomik/ReadKomik.kt
+++ src/en/readkomik/src/eu/kanade/tachiyomi/extension/en/readkomik/ReadKomik.kt
@@ -4,11 +4,13 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 
-class ReadKomik : MangaThemesia("Readkomik", "https://readkomik.com", "en") {
-
+class ReadKomik : MangaThemesia(
+    "Readkomik",
+    "https://rkreader.org",
+    "en",
+    "/archives/manga",
+) {
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(4)
         .build()
-
-    override val hasProjectPage = true
 }
