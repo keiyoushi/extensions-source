477768f51310bc5639e04b7348fdd8f80d75b796
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-01 22:07:11 -0300

    Add MonzeeKomik (#2736)

---
+++ src/id/monzeekomik/src/eu/kanade/tachiyomi/extension/id/monzeekomik/MonzeeKomik.kt
@@ -0,0 +1,11 @@
+package eu.kanade.tachiyomi.extension.id.monzeekomik
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class MonzeeKomik : MangaThemesia(
+    "Monzee Komik",
+    "https://monzeekomik.my.id",
+    "id",
+) {
+    override val hasProjectPage = true
+}
