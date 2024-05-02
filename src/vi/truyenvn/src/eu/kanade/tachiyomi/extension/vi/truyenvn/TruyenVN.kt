5741d199fa4e2d32a957393a75549773e40e8c5c
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-02 00:46:39 +0000

    Add TruyenVN (#2692)

---
+++ src/vi/truyenvn/src/eu/kanade/tachiyomi/extension/vi/truyenvn/TruyenVN.kt
@@ -0,0 +1,17 @@
+package eu.kanade.tachiyomi.extension.vi.truyenvn
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class TruyenVN : Madara(
+    "TruyenVN",
+    "https://truyenvn.lol",
+    "vi",
+    dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ROOT),
+) {
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = true
+
+    override val mangaSubString = "truyen-tranh"
+}
