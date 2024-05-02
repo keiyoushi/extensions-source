9f48602aa09dfc449df6267926e4436d95dc9ad3
mohamedotaku <mohamed.otaku.2016@gmail.com>
2024-05-02 01:51:08 +0100

    MangaTak "ar" use random Url (#2703)
    * Update build.gradle
    
    * Update MangaTak.kt

--- src/ar/mangatak/src/eu/kanade/tachiyomi/extension/ar/mangatak/MangaTak.kt
+++ src/ar/mangatak/src/eu/kanade/tachiyomi/extension/ar/mangatak/MangaTak.kt
@@ -1,10 +1,10 @@
 package eu.kanade.tachiyomi.extension.ar.mangatak
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaAlt
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class MangaTak : MangaThemesia(
+class MangaTak : MangaThemesiaAlt(
     "MangaTak",
     "https://mangatak.com",
     "ar",
