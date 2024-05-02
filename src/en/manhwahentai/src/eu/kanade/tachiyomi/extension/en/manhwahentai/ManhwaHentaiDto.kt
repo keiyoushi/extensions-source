0107008a20945cb6968948f2634d90b335fc4fc3
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-02 03:15:36 +0200

    data class -> class

--- src/en/manhwahentai/src/eu/kanade/tachiyomi/extension/en/manhwahentai/ManhwaHentaiDto.kt
+++ src/en/manhwahentai/src/eu/kanade/tachiyomi/extension/en/manhwahentai/ManhwaHentaiDto.kt
@@ -3,12 +3,12 @@ package eu.kanade.tachiyomi.extension.en.manhwahentai
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class XhrResponseDto(
+class XhrResponseDto(
     val success: Boolean,
     val data: String,
 )
 
 @Serializable
-data class PageDto(
+class PageDto(
     val src: String,
 )
