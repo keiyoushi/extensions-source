2307fa2415125d22acc8ec715ce59236f7a47c25
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-03 10:42:19 +0000

    MangaReader.cc: Switch from http to https (#2794)
    Scheme for thumbnail might change, but that is to be expected.

--- src/en/mangareadercc/src/eu/kanade/tachiyomi/extension/en/mangareadercc/MangaReaderCC.kt
+++ src/en/mangareadercc/src/eu/kanade/tachiyomi/extension/en/mangareadercc/MangaReaderCC.kt
@@ -2,4 +2,4 @@ package eu.kanade.tachiyomi.extension.en.mangareadercc
 
 import eu.kanade.tachiyomi.multisrc.paprika.PaprikaAlt
 
-class MangaReaderCC : PaprikaAlt("MangaReader.cc", "http://mangareader.cc", "en")
+class MangaReaderCC : PaprikaAlt("MangaReader.cc", "https://www.mangareader.cc", "en")
