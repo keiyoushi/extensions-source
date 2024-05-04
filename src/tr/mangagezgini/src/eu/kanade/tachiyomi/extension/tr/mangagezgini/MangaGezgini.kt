c6a85ae648ba2a37bda8d393b0391db0f597a42e
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-04 14:31:02 -0300

    MangaGezgini: Fix chapter name and date format (#2811)
    * Fix chapter name
    
    * Fix date format
    
    * Update icons
    
    * Add useLoadMoreRequest and useNewChapterEndpoint settings

--- src/tr/mangagezgini/src/eu/kanade/tachiyomi/extension/tr/mangagezgini/MangaGezgini.kt
+++ src/tr/mangagezgini/src/eu/kanade/tachiyomi/extension/tr/mangagezgini/MangaGezgini.kt
@@ -1,8 +1,6 @@
 package eu.kanade.tachiyomi.extension.tr.mangagezgini
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.source.model.SChapter
-import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -10,21 +8,8 @@ class MangaGezgini : Madara(
     "MangaGezgini",
     "https://mangagezgini.com",
     "tr",
-    SimpleDateFormat("dd/MM/yyy", Locale("tr")),
+    SimpleDateFormat("MMMM dd, yyyy", Locale("tr")),
 ) {
-    override fun chapterFromElement(element: Element): SChapter {
-        val chapter = SChapter.create()
-        with(element) {
-            selectFirst(chapterUrlSelector)!!.let { urlElement ->
-                chapter.url = urlElement.attr("abs:href").let {
-                    it.substringBefore("?style=paged") + if (!it.endsWith(chapterUrlSuffix)) chapterUrlSuffix else ""
-                }
-                chapter.name = element.select("li.wp-manga-chapter.has-thumb a").text()
-            }
-            chapter.date_upload = selectFirst("img:not(.thumb)")?.attr("alt")?.let { parseRelativeDate(it) }
-                ?: selectFirst("span a")?.attr("title")?.let { parseRelativeDate(it) }
-                ?: parseChapterDate(selectFirst(chapterDateSelector())?.text())
-        }
-        return chapter
-    }
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = true
 }
