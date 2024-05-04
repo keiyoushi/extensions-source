8463e123f2f7092f5148c9f1f444e125dd111bd4
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-04 17:45:38 +0000

    Add HentaiVN.plus (#2832)

---
+++ src/vi/hentaivnplus/src/eu/kanade/tachiyomi/extension/vi/hentaivnplus/HentaiVNPlus.kt
@@ -0,0 +1,17 @@
+package eu.kanade.tachiyomi.extension.vi.hentaivnplus
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class HentaiVNPlus : Madara(
+    "HentaiVN.plus",
+    "https://hentaivn.plus",
+    "vi",
+    dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.ROOT),
+) {
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = false
+
+    override val mangaSubString = "truyen-hentai"
+}
