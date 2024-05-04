2431e041613a13db073cd850635c6c9d6429c5a3
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-04 17:29:11 +0000

    Remove dead sources, domain for sale (#2807)

--- src/en/mangaclashtv/src/eu/kanade/tachiyomi/extension/en/mangaclashtv/MangaClashTv.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangaclashtv
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class MangaClashTv : Madara("MangaClash.tv (unoriginal)", "https://mangaclash.tv", "en") {
-    override val useNewChapterEndpoint = false
-}
--- src/en/mangaupdatestop/src/eu/kanade/tachiyomi/extension/en/mangaupdatestop/MangaUpdatesTop.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangaupdatestop
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class MangaUpdatesTop : Madara("MangaUpdates.top (unoriginal)", "https://mangaupdates.top", "en") {
-    override val useNewChapterEndpoint = false
-}
