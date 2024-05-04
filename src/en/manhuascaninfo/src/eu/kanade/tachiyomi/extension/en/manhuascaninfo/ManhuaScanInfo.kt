9be7cef3e57b4013057de15e9a78c8f78cb1b047
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-04 17:29:11 +0000

    Remove dead sources, domain for sale (#2807)

--- src/en/manhuascaninfo/src/eu/kanade/tachiyomi/extension/en/manhuascaninfo/ManhuaScanInfo.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.manhuascaninfo
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class ManhuaScanInfo : Madara("ManhuaScan.info (unoriginal)", "https://manhuascan.info", "en") {
-    override val useNewChapterEndpoint = false
-}
