62b766946d3a8f0bb9e8198ce0ef2176e4e7dbd6
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-04 17:38:55 +0000

    Add ManhwaHub (#2819)

---
+++ src/en/manhwahub/src/eu/kanade/tachiyomi/extension/en/manhwahub/ManhwaHub.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.en.manhwahub
+
+import eu.kanade.tachiyomi.multisrc.manhwaz.ManhwaZ
+
+class ManhwaHub : ManhwaZ(
+    "ManhwaHub",
+    "https://manhwahub.net",
+    "en",
+)
