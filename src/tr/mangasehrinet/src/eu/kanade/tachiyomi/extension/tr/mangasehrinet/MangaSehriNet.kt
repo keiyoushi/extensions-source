ac49b73d4516362120669cd8766b3f6e2015b6ed
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-05 15:09:22 +0200

    Add Manga Şehri.net

---
+++ src/tr/mangasehrinet/src/eu/kanade/tachiyomi/extension/tr/mangasehrinet/MangaSehriNet.kt
@@ -0,0 +1,15 @@
+package eu.kanade.tachiyomi.extension.tr.mangasehrinet
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class MangaSehriNet : Madara(
+    "Manga Şehri.net",
+    "https://mangasehri.net",
+    "tr",
+    dateFormat = SimpleDateFormat("d MMMM yyyy", Locale("tr")),
+) {
+    override val useLoadMoreRequest = LoadMoreStrategy.AutoDetect
+    override val useNewChapterEndpoint = false
+}
