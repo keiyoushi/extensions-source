168db2af8c89a2c330f4294167389b58eb0bacbf
bapeey <90949336+bapeey@users.noreply.github.com>
2024-05-06 15:37:38 -0500

    Update domain

--- src/es/ragnarokscanlation/src/eu/kanade/tachiyomi/extension/es/ragnarokscanlation/RagnarokScanlation.kt
+++ src/es/ragnarokscanlation/src/eu/kanade/tachiyomi/extension/es/ragnarokscanlation/RagnarokScanlation.kt
@@ -4,4 +4,16 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class RagnarokScanlation : Madara("Ragnarok Scanlation", "https://ragnarokscanlation.net", "es", SimpleDateFormat("MMMM d, yyyy", Locale("es")))
+class RagnarokScanlation : Madara(
+    "Ragnarok Scanlation",
+    "https://ragnarokscanlation.org",
+    "es",
+    SimpleDateFormat("MMMM d, yyyy", Locale("en")),
+) {
+    override val versionId = 2
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+    override val useNewChapterEndpoint = true
+
+    override val mangaSubString = "series"
+}
