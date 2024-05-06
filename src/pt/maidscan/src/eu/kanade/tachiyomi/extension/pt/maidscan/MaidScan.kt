67e57a9817ce66dc9a678e7a8a81976e3e61a922
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-06 12:33:57 -0300

    Maidscan: update domain and bug fix (#2866)
    * Update domain
    
    * Update rateLimit
    
    * Update icons
    
    * Fix date format
    
    * Add LoadMoreStrategy.Never
    
    * Cleanup

--- src/pt/maidscan/src/eu/kanade/tachiyomi/extension/pt/maidscan/MaidScan.kt
+++ src/pt/maidscan/src/eu/kanade/tachiyomi/extension/pt/maidscan/MaidScan.kt
@@ -5,18 +5,17 @@ import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 import java.text.SimpleDateFormat
 import java.util.Locale
-import java.util.concurrent.TimeUnit
 
 class MaidScan : Madara(
     "Maid Scan",
-    "https://maidscan.com.br",
+    "https://maidscans.com",
     "pt-BR",
-    SimpleDateFormat("dd 'de' MMMMM 'de' yyyy", Locale("pt", "BR")),
+    SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")),
 ) {
-
     override val client: OkHttpClient = super.client.newBuilder()
-        .rateLimit(1, 2, TimeUnit.SECONDS)
+        .rateLimit(2)
         .build()
 
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
     override val useNewChapterEndpoint = true
 }
