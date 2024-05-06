9025b888f065c7c84ead5d4873ec4d1399d89275
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-06 13:01:57 -0300

    Add date format

--- src/pt/sussyscan/src/eu/kanade/tachiyomi/extension/pt/sussyscan/SussyScan.kt
+++ src/pt/sussyscan/src/eu/kanade/tachiyomi/extension/pt/sussyscan/SussyScan.kt
@@ -2,11 +2,14 @@ package eu.kanade.tachiyomi.extension.pt.sussyscan
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class SussyScan : Madara(
     "Sussy Scan",
     "https://sussyscan.com",
     "pt-BR",
+    SimpleDateFormat("MMMM dd, yyyy", Locale("pt", "BR"))
 ) {
     override val client = super.client.newBuilder()
         .rateLimit(2)
