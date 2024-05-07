6ecb2a6f87f9df21232a85b71b9a83b6a8fecaf4
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-07 08:08:26 -0300

    SussyScan: Fix load mangas (#2870)
    * Remove mangaSubString
    
    * Keeps thumbnail from popular, latest and search
    
    * Remove date format
    
    * Remove unused import
    
    * Add date format
    
    * Fix lint
    
    * Add thumbnail from manga datails

--- src/pt/sussyscan/src/eu/kanade/tachiyomi/extension/pt/sussyscan/SussyScan.kt
+++ src/pt/sussyscan/src/eu/kanade/tachiyomi/extension/pt/sussyscan/SussyScan.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.extension.pt.sussyscan
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -9,7 +10,7 @@ class SussyScan : Madara(
     "Sussy Scan",
     "https://sussyscan.com",
     "pt-BR",
-    SimpleDateFormat("MMMMM dd, yyyy", Locale("pt", "BR")),
+    SimpleDateFormat("MMMM dd, yyyy", Locale("pt", "BR")),
 ) {
     override val client = super.client.newBuilder()
         .rateLimit(2)
@@ -17,5 +18,10 @@ class SussyScan : Madara(
 
     override val useNewChapterEndpoint = true
 
-    override val mangaSubString = "sus"
+    override val mangaDetailsSelectorThumbnail = "head meta[property='og:image']"
+
+    override fun imageFromElement(element: Element): String? {
+        return super.imageFromElement(element)?.takeIf { it.isNotEmpty() }
+            ?: element.attr("content") // Thumbnail from <head>
+    }
 }
