a8b0e9d7a0de67a00ca72c5dd95859bcad11beb9
bapeey <90949336+bapeey@users.noreply.github.com>
2024-05-08 14:52:22 -0500

    Remove EN Id: 7467592837760360366

--- src/en/darkscan/src/eu/kanade/tachiyomi/extension/en/darkscan/DarkScan.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.darkscan
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class DarkScan : Madara("Dark-scan", "https://dark-scan.com", "en") {
-    override val useNewChapterEndpoint = true
-}
