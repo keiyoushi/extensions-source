ae805abd67edf20a7a3af47a4e514df97d7c9bf4
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-08 20:58:06 +0200

    Add Türkçe Manga Oku

---
+++ src/tr/turkcemangaoku/src/eu/kanade/tachiyomi/extension/tr/turkcemangaoku/TurkceMangaOku.kt
@@ -0,0 +1,17 @@
+package eu.kanade.tachiyomi.extension.tr.turkcemangaoku
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class TurkceMangaOku : Madara(
+    "Türkçe Manga Oku",
+    "https://turkcemangaoku.com",
+    "tr",
+    dateFormat = SimpleDateFormat("d MMMM yyyy", Locale("tr")),
+) {
+    override val mangaDetailsSelectorStatus = "div.summary-heading:contains(Durumu) + div.summary-content"
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = true
+}
