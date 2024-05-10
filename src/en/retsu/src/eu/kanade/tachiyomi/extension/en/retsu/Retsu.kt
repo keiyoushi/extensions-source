154c4e9df004bf3ea846299252acf166d83fbec9
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-10 15:12:38 +0000

    Add Retsu (#2941)

---
+++ src/en/retsu/src/eu/kanade/tachiyomi/extension/en/retsu/Retsu.kt
@@ -0,0 +1,20 @@
+package eu.kanade.tachiyomi.extension.en.retsu
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class Retsu : Madara(
+    "Retsu",
+    "https://retsu.org",
+    "en",
+) {
+    override fun popularMangaSelector() = "div.manga__item"
+    override val popularMangaUrlSelector = "h4 a"
+
+    override val mangaDetailsSelectorTitle = "h1.post-title"
+    override val mangaDetailsSelectorGenre = "div.manga-genres a"
+    override val seriesTypeSelector = ".manga-type .summary-content"
+    override val altNameSelector = ".manga-alternative"
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+    override val useNewChapterEndpoint = false
+}
