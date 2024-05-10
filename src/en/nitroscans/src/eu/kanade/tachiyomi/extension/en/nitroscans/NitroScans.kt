048a08e8b7341809c856fb468e824f78cc64150e
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-10 14:59:09 +0000

    Nitro Manga -> Nitro Scans: update domain (#2909)

--- src/en/nitroscans/src/eu/kanade/tachiyomi/extension/en/nitroscans/NitroScans.kt
+++ src/en/nitroscans/src/eu/kanade/tachiyomi/extension/en/nitroscans/NitroScans.kt
@@ -2,12 +2,13 @@ package eu.kanade.tachiyomi.extension.en.nitroscans
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class NitroScans : Madara("Nitro Manga", "https://nitromanga.com", "en") {
+class NitroScans : Madara("Nitro Scans", "https://nitroscans.net", "en") {
     override val id = 1310352166897986481
 
     override val mangaSubString = "mangas"
 
     override val filterNonMangaItems = false
 
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
     override val useNewChapterEndpoint = true
 }
