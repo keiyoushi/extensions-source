262624ba74ca749697c168221d46692defbf8911
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-10 18:22:07 +0200

    Remove dead sources (at least 1wk)

--- src/en/toonmany/src/eu/kanade/tachiyomi/extension/en/toonmany/ToonMany.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.toonmany
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class ToonMany : Madara("ToonMany", "https://toonmany.com", "en", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.US))
