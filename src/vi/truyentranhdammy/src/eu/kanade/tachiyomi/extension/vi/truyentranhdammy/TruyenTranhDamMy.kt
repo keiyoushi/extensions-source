a604e677d0e61b797dfa27b4f4183a9009811e99
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-10 15:15:21 +0000

    Add Truyen tranh dam my (#2946)

---
+++ src/vi/truyentranhdammy/src/eu/kanade/tachiyomi/extension/vi/truyentranhdammy/TruyenTranhDamMy.kt
@@ -0,0 +1,37 @@
+package eu.kanade.tachiyomi.extension.vi.truyentranhdammy
+
+import android.annotation.SuppressLint
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.DateFormatSymbols
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+@SuppressLint("SimpleDateFormat")
+class TruyenTranhDamMy : Madara(
+    "Truyện tranh đam mỹ",
+    "https://truyentranhdammyy.top",
+    "vi",
+    dateFormat = SimpleDateFormat(
+        "MMMM d, yyyy",
+        DateFormatSymbols(Locale("vi")).apply {
+            // Month One, Month Two, ...
+            months = arrayOf(
+                "Tháng Một",
+                "Tháng Hai",
+                "Tháng Ba",
+                "Tháng Tư",
+                "Tháng Năm",
+                "Tháng Sáu",
+                "Tháng Bảy",
+                "Tháng Tám",
+                "Tháng Chín",
+                "Tháng Mười",
+                "Tháng Mười Một",
+                "Tháng Mười Hai",
+            )
+        },
+    ),
+) {
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = false
+}
