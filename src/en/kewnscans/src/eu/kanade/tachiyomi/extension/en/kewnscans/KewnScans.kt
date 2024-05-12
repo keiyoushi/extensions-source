1abdc18bf2120634d8c229caeb9b32e92428ceb3
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-05-12 10:53:30 +0500

    unused imports

--- src/en/kewnscans/src/eu/kanade/tachiyomi/extension/en/kewnscans/KewnScans.kt
+++ src/en/kewnscans/src/eu/kanade/tachiyomi/extension/en/kewnscans/KewnScans.kt
@@ -1,7 +1,5 @@
 package eu.kanade.tachiyomi.extension.en.kewnscans
 
 import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
-import eu.kanade.tachiyomi.source.model.Page
-import org.jsoup.nodes.Document
 
 class KewnScans : Keyoapp("Kewn Scans", "https://kewnscans.org", "en")
