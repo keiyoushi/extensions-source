2802adff5111c049f5fd10ca333de61e0922c321
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-12 04:21:56 +0000

    Remove dead sources (at least 1wk) (#2953)

--- src/en/luffymanga/src/eu/kanade/tachiyomi/extension/en/luffymanga/LuffyManga.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.luffymanga
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class LuffyManga : Madara("Luffy Manga", "https://luffymanga.com", "en") {
-    override val useNewChapterEndpoint = false
-}
--- src/en/mangatoo/src/eu/kanade/tachiyomi/extension/en/mangatoo/MangaToo.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangatoo
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class MangaToo : Madara("Manga Too", "https://mangatoo.com", "en")
