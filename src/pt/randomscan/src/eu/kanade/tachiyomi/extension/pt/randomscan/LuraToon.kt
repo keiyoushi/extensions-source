1f03ebc0b09bb96bdf904a9d263e96793ca74502
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-12 01:26:59 -0300

    LuraToon: Add random user agent (#2967)
    Add random user agent

--- src/pt/randomscan/src/eu/kanade/tachiyomi/extension/pt/randomscan/LuraToon.kt
+++ src/pt/randomscan/src/eu/kanade/tachiyomi/extension/pt/randomscan/LuraToon.kt
@@ -1,10 +1,39 @@
 package eu.kanade.tachiyomi.extension.pt.randomscan
 
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.lib.randomua.addRandomUAPreferenceToScreen
+import eu.kanade.tachiyomi.lib.randomua.getPrefCustomUA
+import eu.kanade.tachiyomi.lib.randomua.getPrefUAType
+import eu.kanade.tachiyomi.lib.randomua.setRandomUserAgent
 import eu.kanade.tachiyomi.multisrc.peachscan.PeachScan
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.ConfigurableSource
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
-class LuraToon : PeachScan("Lura Toon", "https://luratoon.com", "pt-BR") {
+class LuraToon :
+    PeachScan(
+        "Lura Toon",
+        "https://luratoon.com",
+        "pt-BR",
+    ),
+    ConfigurableSource {
+
+    private val preferences: SharedPreferences by lazy {
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+    }
+
     override val client = super.client.newBuilder()
         .rateLimit(1, 2)
+        .setRandomUserAgent(
+            preferences.getPrefUAType(),
+            preferences.getPrefCustomUA(),
+        )
         .build()
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        addRandomUAPreferenceToScreen(screen)
+    }
 }
