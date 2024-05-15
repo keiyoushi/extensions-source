c529b88e5ca5228bb21a9a2c4f08599f75bcb07a
bapeey <90949336+bapeey@users.noreply.github.com>
2024-05-15 10:25:36 -0500

    Mantraz Scan: Update domain (#3029)
    update domain

--- src/es/mantrazscan/src/eu/kanade/tachiyomi/extension/es/mantrazscan/MantrazScan.kt
+++ src/es/mantrazscan/src/eu/kanade/tachiyomi/extension/es/mantrazscan/MantrazScan.kt
@@ -1,14 +1,21 @@
 package eu.kanade.tachiyomi.extension.es.mantrazscan
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 class MantrazScan : Madara(
     "Mantraz Scan",
-    "https://mantrazscan.com",
+    "https://mantrazscans.net",
     "es",
     SimpleDateFormat("dd/MM/yyyy", Locale("es")),
 ) {
+    override val client = super.client.newBuilder()
+        .rateLimitHost(baseUrl.toHttpUrl(), 3, 1, TimeUnit.SECONDS)
+        .build()
+
     override val useNewChapterEndpoint = true
 }
