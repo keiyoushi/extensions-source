ef3dac049281b9738f3fa978393eb00e77bde02e
bapeey <90949336+bapeey@users.noreply.github.com>
2024-05-14 21:25:38 -0500

    mv to madara

--- src/es/tresdaosscan/src/eu/kanade/tachiyomi/extension/es/tresdaosscan/TresDaosScan.kt
+++ src/es/tresdaosscan/src/eu/kanade/tachiyomi/extension/es/tresdaosscan/TresDaosScan.kt
@@ -1,20 +1,23 @@
 package eu.kanade.tachiyomi.extension.es.tresdaosscan
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class TresDaosScan : MangaThemesia(
+class TresDaosScan : Madara(
     "Tres Daos Scan",
     "https://tresdaos.com",
     "es",
-    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
+    SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
 ) {
-    // Site move from Madara to MangaThemesia
-    override val versionId = 3
+    // Site move from MangaThemesia to Madara
+    override val versionId = 4
 
     override val client = super.client.newBuilder()
         .rateLimit(2)
         .build()
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+    override val useNewChapterEndpoint = true
 }
