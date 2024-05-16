e7bab995eb9d5b18ea626416d6235020f304163b
bapeey <90949336+bapeey@users.noreply.github.com>
2024-05-16 16:19:42 -0500

    update domain

--- src/es/olympusscanlation/src/eu/kanade/tachiyomi/extension/es/olympusscanlation/OlympusScanlation.kt
+++ src/es/olympusscanlation/src/eu/kanade/tachiyomi/extension/es/olympusscanlation/OlympusScanlation.kt
@@ -24,8 +24,8 @@ class OlympusScanlation : HttpSource() {
 
     override val versionId = 2
 
-    override val baseUrl: String = "https://leelolympus.com"
-    private val apiBaseUrl: String = "https://dashboard.leelolympus.com"
+    override val baseUrl: String = "https://leerolymp.com"
+    private val apiBaseUrl: String = "https://dashboard.leerolymp.com"
 
     override val lang: String = "es"
     override val name: String = "Olympus Scanlation"
