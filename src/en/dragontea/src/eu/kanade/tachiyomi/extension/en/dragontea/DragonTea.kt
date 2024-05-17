0069ab0473346d7a0c2a2d4580483a7015bb3885
Secozzi <folke.steen85@gmail.com>
2024-05-17 08:42:08 +0200

    didn't even need to override part 2

--- src/en/dragontea/src/eu/kanade/tachiyomi/extension/en/dragontea/DragonTea.kt
+++ src/en/dragontea/src/eu/kanade/tachiyomi/extension/en/dragontea/DragonTea.kt
@@ -1,11 +1,8 @@
 package eu.kanade.tachiyomi.extension.en.dragontea
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import eu.kanade.tachiyomi.source.model.Page
 import okhttp3.OkHttpClient
-import okhttp3.Request
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -22,12 +19,4 @@ class DragonTea : Madara(
     override val mangaSubString = "novel"
 
     override val useNewChapterEndpoint = true
-
-    override fun imageRequest(page: Page): Request {
-        val imgHeaders = headersBuilder().apply {
-            add("Accept", "image/avif,image/webp,*/*")
-            set("Referer", page.url)
-        }.build()
-        return GET(page.imageUrl!!, imgHeaders)
-    }
 }
