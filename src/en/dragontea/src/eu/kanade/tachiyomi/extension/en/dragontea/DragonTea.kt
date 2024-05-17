71f41aafabd0fae912464b7d91029b136cff81d6
Secozzi <folke.steen85@gmail.com>
2024-05-17 08:31:36 +0200

    update pageListParse and imageRequest

--- src/en/dragontea/src/eu/kanade/tachiyomi/extension/en/dragontea/DragonTea.kt
+++ src/en/dragontea/src/eu/kanade/tachiyomi/extension/en/dragontea/DragonTea.kt
@@ -1,14 +1,11 @@
 package eu.kanade.tachiyomi.extension.en.dragontea
 
-import android.util.Base64
-import eu.kanade.tachiyomi.lib.cryptoaes.CryptoAES
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.Page
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -27,42 +24,17 @@ class DragonTea : Madara(
 
     override val useNewChapterEndpoint = true
 
-    private val pageIndexRegex = Regex("""image-(\d+)[a-z]+""", RegexOption.IGNORE_CASE)
-
     override fun pageListParse(document: Document): List<Page> {
-        val dataId = document.selectFirst(".entry-header.header")?.attr("data-id")?.toInt()
-            ?: return super.pageListParse(document)
-        val elements = document.select(".reading-content .page-break img")
-        val pageCount = elements.size
-
-        val idKey = "11" + ((dataId + 1307) * 3 - pageCount).toString()
-        elements.forEach {
-            val decryptedId = decryptAesJson(it.attr("id"), idKey).jsonPrimitive.content
-            it.attr("id", decryptedId)
-        }
-
-        val orderedElements = elements.sortedBy {
-            pageIndexRegex.find(it.attr("id"))?.groupValues?.get(1)?.toInt() ?: 0
-        }
-
-        val dtaKey = "15" + orderedElements.joinToString("") { it.attr("id").takeLast(1) } + (((dataId + 88) * 2) - pageCount - 5).toString()
-        val srcKey = (dataId + 20).toString() + orderedElements.joinToString("") {
-            decryptAesJson(it.attr("dta"), dtaKey).jsonPrimitive.content.takeLast(2)
-        } + (pageCount * 4).toString()
-
-        return orderedElements.mapIndexed { i, element ->
-            val src = decryptAesJson(element.attr("data-src"), srcKey).jsonPrimitive.content
-            Page(i, document.location(), src)
+        return document.select(".reading-content .page-break img").mapIndexed { index, element ->
+            Page(index, document.location(), element.attr("abs:data-src"))
         }
     }
 
-    private fun decryptAesJson(ciphertext: String, key: String): JsonElement {
-        val cipherData = json.parseToJsonElement(ciphertext).jsonObject
-
-        val unsaltedCiphertext = Base64.decode(cipherData["ct"]!!.jsonPrimitive.content, Base64.DEFAULT)
-        val salt = cipherData["s"]!!.jsonPrimitive.content.decodeHex()
-        val saltedCiphertext = salted + salt + unsaltedCiphertext
-
-        return json.parseToJsonElement(CryptoAES.decrypt(Base64.encodeToString(saltedCiphertext, Base64.DEFAULT), key))
+    override fun imageRequest(page: Page): Request {
+        val imgHeaders = headersBuilder().apply {
+            add("Accept", "image/avif,image/webp,*/*")
+            set("Referer", page.url)
+        }.build()
+        return GET(page.imageUrl!!, imgHeaders)
     }
 }
