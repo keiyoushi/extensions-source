0b1c07614a5ed11e26313cf5e323e42773cb78f5
DumbCodeGenerator <krogon9@gmail.com>
2024-05-17 04:35:21 +0300

    Hentai20: Switch back to Madara

--- src/en/hentai20/src/eu/kanade/tachiyomi/extension/en/hentai20/Hentai20.kt
+++ src/en/hentai20/src/eu/kanade/tachiyomi/extension/en/hentai20/Hentai20.kt
@@ -1,10 +1,10 @@
 package eu.kanade.tachiyomi.extension.en.hentai20
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 
-class Hentai20 : MangaThemesia("Hentai20", "https://hentai20.io", "en") {
+class Hentai20 : Madara("Hentai20", "https://hentai20.io", "en") {
 
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(1)
