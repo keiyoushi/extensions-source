25fcf85aca7ae4c5bb5d53a9fa49b51177d87185
bapeey <90949336+bapeey@users.noreply.github.com>
2024-05-18 09:35:22 -0500

    Traducciones Moonlight: Change theme to MangaEsp (#3073)
    * move to mangaesp
    
    * bruh

--- src/es/traduccionesmoonlight/src/eu/kanade/tachiyomi/extension/es/traduccionesmoonlight/TraduccionesMoonlight.kt
+++ src/es/traduccionesmoonlight/src/eu/kanade/tachiyomi/extension/es/traduccionesmoonlight/TraduccionesMoonlight.kt
@@ -1,27 +1,12 @@
 package eu.kanade.tachiyomi.extension.es.traduccionesmoonlight
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import java.text.SimpleDateFormat
-import java.util.Locale
+import eu.kanade.tachiyomi.multisrc.mangaesp.MangaEsp
 
-class TraduccionesMoonlight : MangaThemesia(
+class TraduccionesMoonlight : MangaEsp(
     "Traducciones Moonlight",
     "https://traduccionesmoonlight.com",
     "es",
-    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("es")),
 ) {
-    // Site moved from Madara to MangaThemesia
-    override val versionId = 2
-
-    override val client = super.client.newBuilder()
-        .rateLimitHost(baseUrl.toHttpUrl(), 2, 1)
-        .build()
-
-    override val seriesAuthorSelector = ".tsinfo .imptdt:contains(autor) i"
-    override val seriesStatusSelector = ".tsinfo .imptdt:contains(estado) i"
-
-    // Filter out novels
-    override fun searchMangaSelector() = ".utao .uta .imgu:not(:has(.novelabel)), .listupd .bs .bsx:not(:has(.novelabel)), .listo .bs .bsx:not(:has(.novelabel))"
+    // Mangathemesia -> MangaEsp
+    override val versionId = 3
 }
