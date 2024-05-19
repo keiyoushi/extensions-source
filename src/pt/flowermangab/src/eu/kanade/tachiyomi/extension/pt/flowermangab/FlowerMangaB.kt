86ee44b47986fcb7f338ddd21a08a5170b8acca4
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-19 11:26:24 -0300

    Add FlowerMangaB

---
+++ src/pt/flowermangab/src/eu/kanade/tachiyomi/extension/pt/flowermangab/FlowerMangaB.kt
@@ -0,0 +1,22 @@
+package eu.kanade.tachiyomi.extension.pt.flowermangab
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class FlowerMangaB : Madara(
+    "Flower Manga B",
+    "https://flowermanga.com",
+    "pt-BR",
+    SimpleDateFormat("d 'de' MMMMM 'de' yyyy", Locale("pt", "BR")),
+) {
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(1, 2, TimeUnit.SECONDS)
+        .build()
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = false
+}
