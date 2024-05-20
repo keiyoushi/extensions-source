36cb56d93d1bfeccfc754da2f68635560f6fded4
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-20 10:31:22 -0300

    Raname source

--- src/pt/flowermanga/src/eu/kanade/tachiyomi/extension/pt/flowermanga/FlowerMangaDotNet.kt
+++ src/pt/flowermanga/src/eu/kanade/tachiyomi/extension/pt/flowermanga/FlowerMangaDotNet.kt
@@ -7,12 +7,15 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
-class FlowerManga : Madara(
-    "Flower Manga",
+class FlowerMangaDotNet : Madara(
+    "Flower Manga (.net)",
     "https://flowermanga.net",
     "pt-BR",
     SimpleDateFormat("d 'de' MMMMM 'de' yyyy", Locale("pt", "BR")),
 ) {
+
+    override val id = 2421010180391442293
+
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(1, 2, TimeUnit.SECONDS)
         .build()
