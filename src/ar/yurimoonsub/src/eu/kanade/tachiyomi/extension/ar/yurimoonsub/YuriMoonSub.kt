f37aaed62d8080d120288fcce3a96b319fe94061
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-21 17:54:32 -0300

    Change rateLimit

--- src/ar/yurimoonsub/src/eu/kanade/tachiyomi/extension/ar/yurimoonsub/YuriMoonSub.kt
+++ src/ar/yurimoonsub/src/eu/kanade/tachiyomi/extension/ar/yurimoonsub/YuriMoonSub.kt
@@ -5,6 +5,6 @@ import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
 class YuriMoonSub : ZeistManga("Yuri Moon Sub", "https://yurimoonsub.blogspot.com", "ar") {
     override val client = super.client.newBuilder()
-        .rateLimit(3)
+        .rateLimit(2)
         .build()
 }
