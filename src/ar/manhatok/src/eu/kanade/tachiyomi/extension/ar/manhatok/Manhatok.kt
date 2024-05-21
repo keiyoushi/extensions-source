7d6d986aed74de485f1d4dccb642b398d35b4c5d
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-21 18:05:07 -0300

    Remove unused import

--- src/ar/manhatok/src/eu/kanade/tachiyomi/extension/ar/manhatok/Manhatok.kt
+++ src/ar/manhatok/src/eu/kanade/tachiyomi/extension/ar/manhatok/Manhatok.kt
@@ -2,7 +2,6 @@ package eu.kanade.tachiyomi.extension.ar.manhatok
 
 import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import java.util.concurrent.TimeUnit
 
 class Manhatok : ZeistManga("Manhatok", "https://manhatok.blogspot.com", "ar") {
     override val client = super.client.newBuilder()
