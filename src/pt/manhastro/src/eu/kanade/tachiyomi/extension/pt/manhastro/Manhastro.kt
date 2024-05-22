41645cd5fabfa81253cf7faa7a8f0dd381c8d9f2
bapeey <90949336+bapeey@users.noreply.github.com>
2024-05-22 00:41:15 -0500

    Manhastro: fix downloads (#3171)
    fix

--- src/pt/manhastro/src/eu/kanade/tachiyomi/extension/pt/manhastro/Manhastro.kt
+++ src/pt/manhastro/src/eu/kanade/tachiyomi/extension/pt/manhastro/Manhastro.kt
@@ -5,7 +5,9 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.Page
 import kotlinx.serialization.decodeFromString
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -20,6 +22,21 @@ class Manhastro : Madara(
 
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(1, 2, TimeUnit.SECONDS)
+        .addInterceptor { chain ->
+            val response = chain.proceed(chain.request())
+            val mime = response.headers["Content-Type"]
+            if (response.isSuccessful) {
+                if (mime != "application/octet-stream") {
+                    return@addInterceptor response
+                }
+                // Fix image content type
+                val type = IMG_CONTENT_TYPE.toMediaType()
+                val body = response.body.bytes().toResponseBody(type)
+                return@addInterceptor response.newBuilder().body(body)
+                    .header("Content-Type", IMG_CONTENT_TYPE).build()
+            }
+            response
+        }
         .build()
 
     override val useNewChapterEndpoint = true
@@ -39,4 +56,8 @@ class Manhastro : Madara(
     }
 
     private val imageLinksPattern = """var\s+?imageLinks\s*?=\s*?(\[.*]);""".toRegex()
+
+    companion object {
+        private const val IMG_CONTENT_TYPE = "image/jpeg"
+    }
 }
