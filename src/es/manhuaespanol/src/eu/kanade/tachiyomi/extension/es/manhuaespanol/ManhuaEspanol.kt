b4553474fd72172d622ba337e5c5007d5c6aaa23
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-05-21 18:15:47 -0700

    Add Manhua Español

---
+++ src/es/manhuaespanol/src/eu/kanade/tachiyomi/extension/es/manhuaespanol/ManhuaEspanol.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.es.manhuaespanol
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class ManhuaEspanol : Madara("Manhua Español", "https://manhuaespanol.com", "es")
