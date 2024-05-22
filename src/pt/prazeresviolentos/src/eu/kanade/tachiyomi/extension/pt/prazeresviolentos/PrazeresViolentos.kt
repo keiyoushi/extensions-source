e4b6f73f65baf6948f2219adcbdfa9d757f59a51
kana-shii <79055104+kana-shii@users.noreply.github.com>
2024-05-22 12:20:48 -0300

    exception message 2

--- src/pt/prazeresviolentos/src/eu/kanade/tachiyomi/extension/pt/prazeresviolentos/PrazeresViolentos.kt
+++ src/pt/prazeresviolentos/src/eu/kanade/tachiyomi/extension/pt/prazeresviolentos/PrazeresViolentos.kt
@@ -2,13 +2,13 @@ package eu.kanade.tachiyomi.extension.pt.prazeresviolentos
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
-import okhttp3.Interceptor
-import okhttp3.Response
-import java.io.IOException
 
 class PrazeresViolentos : Madara(
     "Prazeres Violentos",
@@ -18,25 +18,20 @@ class PrazeresViolentos : Madara(
 ) {
 
     override val client: OkHttpClient = super.client.newBuilder()
-        .addInterceptor(::loginCheckIntercept)
         .rateLimit(1, 2, TimeUnit.SECONDS)
+        .addInterceptor(::checkPasswordProtectedIntercept)
         .build()
 
-    private fun loginCheckIntercept(chain: Interceptor.Chain): Response {
+    private fun checkPasswordProtectedIntercept(chain: Interceptor.Chain): Response {
         val response = chain.proceed(chain.request())
 
-        if (response.request.url.queryParameter("password-protected").isNullOrEmpty()) {
-            return response
+        if (response.request.url.queryParameter("password-protected") != null) {
+            response.close()
+            throw IOException("Autentique-se através da WebView e tente novamente.")
         }
 
-        response.close()
-        throw IOException(LOGIN_THROUGH_WEBVIEW_ERROR)
-    }
-
-    companion object {
-        private const val LOGIN_THROUGH_WEBVIEW_ERROR = "Autentique-se pela WebView para usar a extensão."
+        return response
     }
 
     override val useNewChapterEndpoint = true
-
 }
