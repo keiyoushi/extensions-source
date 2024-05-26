7049b75c7ae2514f5d4a5d2099497ea490f48f12
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-26 00:50:02 -0300

    Fix domain

--- src/vi/lxhentai/src/eu/kanade/tachiyomi/extension/vi/lxhentai/LxHentai.kt
+++ src/vi/lxhentai/src/eu/kanade/tachiyomi/extension/vi/lxhentai/LxHentai.kt
@@ -23,7 +23,7 @@ class LxHentai : ParsedHttpSource() {
 
     override val name = "LXHentai"
 
-    override val baseUrl = "https://lxmanga.club"
+    override val baseUrl = "https://lxmanga.icu"
 
     override val lang = "vi"
 
