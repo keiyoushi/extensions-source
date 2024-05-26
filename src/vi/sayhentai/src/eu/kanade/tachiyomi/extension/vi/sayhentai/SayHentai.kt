ba1826860dd11524385caa7aaf449ce3ac9f79cd
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-05-26 01:38:48 -0300

    Update domain

--- src/vi/sayhentai/src/eu/kanade/tachiyomi/extension/vi/sayhentai/SayHentai.kt
+++ src/vi/sayhentai/src/eu/kanade/tachiyomi/extension/vi/sayhentai/SayHentai.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.manhwaz.ManhwaZ
 
 class SayHentai : ManhwaZ(
     "SayHentai",
-    "https://sayhentai.pro",
+    "https://sayhentai.co",
     "vi",
     mangaDetailsAuthorHeading = "Tác giả",
     mangaDetailsStatusHeading = "Trạng thái",
