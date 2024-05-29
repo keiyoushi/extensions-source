dd00ec4067a8656446099270cda038d5257588ee
bapeey <90949336+bapeey@users.noreply.github.com>
2024-05-28 23:54:13 -0500

    EternalMangas: Move to Multi and fix pages not found (#3258)
    * Move to Multi and fix pages
    
    * skill issue
    
    * mmmm
    
    * remode id

--- src/es/eternalmangas/src/eu/kanade/tachiyomi/extension/es/eternalmangas/EternalMangas.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.es.eternalmangas
-
-import eu.kanade.tachiyomi.multisrc.mangaesp.MangaEsp
-
-class EternalMangas : MangaEsp("EternalMangas", "https://eternalmangas.com", "es")
