c3bd3c310221ea1c4326ac9d786e0bf52ee5a40d
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-29 19:53:16 +0200

    Kai Scans: update domain

--- src/en/kaiscans/src/eu/kanade/tachiyomi/extension/en/kaiscans/KaiScans.kt
+++ src/en/kaiscans/src/eu/kanade/tachiyomi/extension/en/kaiscans/KaiScans.kt
@@ -3,7 +3,7 @@ package eu.kanade.tachiyomi.extension.en.kaiscans
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
-class KaiScans : MangaThemesia("Kai Scans", "https://kaiscans.org", "en") {
+class KaiScans : MangaThemesia("Kai Scans", "https://kaiscans.com", "en") {
     override val client = super.client.newBuilder()
         .rateLimit(2)
         .build()
