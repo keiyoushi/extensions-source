c57640d4759441d6b4be44edff7ce0162808a69f
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-05-31 10:59:19 +0000

    Kai Scans: update domain (#3294)
    * Kai Scans: update domain
    
    * switch to MangaThemesiaAlt

--- src/en/kaiscans/src/eu/kanade/tachiyomi/extension/en/kaiscans/KaiScans.kt
+++ src/en/kaiscans/src/eu/kanade/tachiyomi/extension/en/kaiscans/KaiScans.kt
@@ -1,9 +1,9 @@
 package eu.kanade.tachiyomi.extension.en.kaiscans
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaAlt
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
-class KaiScans : MangaThemesia("Kai Scans", "https://kaiscans.org", "en") {
+class KaiScans : MangaThemesiaAlt("Kai Scans", "https://kaiscans.com", "en") {
     override val client = super.client.newBuilder()
         .rateLimit(2)
         .build()
