e97e93f4ca30dac0f02d28824a979397ecde7fd5
bapeey <90949336+bapeey@users.noreply.github.com>
2024-06-02 01:54:14 -0500

    Comick: Fix dateFormat (#3342)
    fix dateFormat

--- src/all/comickfun/src/eu/kanade/tachiyomi/extension/all/comickfun/Helpers.kt
+++ src/all/comickfun/src/eu/kanade/tachiyomi/extension/all/comickfun/Helpers.kt
@@ -7,7 +7,7 @@ import java.util.Locale
 import java.util.TimeZone
 
 private val dateFormat by lazy {
-    SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH).apply {
+    SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.ENGLISH).apply {
         timeZone = TimeZone.getTimeZone("UTC")
     }
 }
