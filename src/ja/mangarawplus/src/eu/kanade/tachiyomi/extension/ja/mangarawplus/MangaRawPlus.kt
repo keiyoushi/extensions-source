d38a18905f0271533b4ba8c3ec0f7bcea173fa76
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-06-02 16:50:56 -0700

    Updated domain URL, substring, and added override for thumbnails

--- src/ja/mangarawplus/src/eu/kanade/tachiyomi/extension/ja/mangarawplus/MangaRawPlus.kt
+++ src/ja/mangarawplus/src/eu/kanade/tachiyomi/extension/ja/mangarawplus/MangaRawPlus.kt
@@ -1,8 +1,15 @@
 package eu.kanade.tachiyomi.extension.ja.mangarawplus
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import org.jsoup.nodes.Element
 
-class MangaRawPlus : Madara("MANGARAW+", "https://newmangaraw.com", "ja") {
-    override val mangaSubString = "sp"
+class MangaRawPlus : Madara("MANGARAW+", "https://newmangaraw.net", "ja") {
+    override val mangaSubString = "ts"
+    override fun imageFromElement(element: Element): String? {
+        return when {
+            element.hasAttr("data-src-img") -> element.attr("abs:data-src-img")
+            else -> element.attr("abs:src")
+        }
+    }
     override val useNewChapterEndpoint = false
 }
