b3409268f01d53f45eb8a3a0c934626542fc154b
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-06-03 15:50:27 +0500

    LuaScans: set js cookie (#3361)
    * LuaScans: set js cookie
    
    * bump

--- src/en/luascans/src/eu/kanade/tachiyomi/extension/en/luascans/LuaScans.kt
+++ src/en/luascans/src/eu/kanade/tachiyomi/extension/en/luascans/LuaScans.kt
@@ -2,7 +2,11 @@ package eu.kanade.tachiyomi.extension.en.luascans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.Cookie
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import org.jsoup.Jsoup
 
 class LuaScans : MangaThemesia(
     "Lua Scans",
@@ -10,6 +14,35 @@ class LuaScans : MangaThemesia(
     "en",
 ) {
     override val client: OkHttpClient = super.client.newBuilder()
-        .rateLimit(4)
+        .addInterceptor(::wafffCookieInterceptor)
+        .rateLimit(2)
         .build()
+
+    private fun wafffCookieInterceptor(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val response = chain.proceed(request)
+
+        val document = Jsoup.parse(
+            response.peekBody(Long.MAX_VALUE).string(),
+            response.request.url.toString(),
+        )
+
+        return if (document.selectFirst("script:containsData(wafff)") != null) {
+            val script = document.selectFirst("script:containsData(wafff)")!!.data()
+
+            val cookie = script.substringAfter("document.cookie=\"")
+                .substringBefore("\"")
+
+            client.cookieJar.saveFromResponse(
+                request.url,
+                listOfNotNull(Cookie.parse(request.url, cookie)),
+            )
+
+            response.close()
+
+            chain.proceed(request)
+        } else {
+            response
+        }
+    }
 }
