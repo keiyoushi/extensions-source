02a90eb1978743205d3da60e7f82180d4acfea82
Chopper <156493704+ttony2chopper@users.noreply.github.com>
2024-06-03 23:05:56 -0300

    Add HaremDeKira

---
+++ src/es/haremdekira/src/eu/kanade/tachiyomi/extension/es/haremscann/HaremDeKira.kt
@@ -0,0 +1,20 @@
+package eu.kanade.tachiyomi.extension.es.haremdekira
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class HaremDeKira : Madara(
+    "Harem de Kira",
+    "https://haremscann.es",
+    "es",
+    SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
+) {
+    override val client = super.client.newBuilder()
+        .rateLimit(3)
+        .build()
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+}
