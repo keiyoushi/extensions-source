364c21e076f75e8066596e1855ecf8308b8ad308
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-06-06 14:04:02 +0500

    LuaScan: use regex for cookie extraction

--- src/en/luascans/src/eu/kanade/tachiyomi/extension/en/luascans/LuaScans.kt
+++ src/en/luascans/src/eu/kanade/tachiyomi/extension/en/luascans/LuaScans.kt
@@ -30,12 +30,13 @@ class LuaScans : MangaThemesia(
         return if (document.selectFirst("script:containsData(wafff)") != null) {
             val script = document.selectFirst("script:containsData(wafff)")!!.data()
 
-            val cookie = script.substringAfter("document.cookie=\"")
-                .substringBefore("\"")
+            val cookie = waffRegex.find(script)?.groups?.get("waff")?.value
+                ?.let { Cookie.parse(request.url, it) }
+                ?: return response
 
             client.cookieJar.saveFromResponse(
                 request.url,
-                listOfNotNull(Cookie.parse(request.url, cookie)),
+                listOf(cookie),
             )
 
             response.close()
@@ -45,4 +46,6 @@ class LuaScans : MangaThemesia(
             response
         }
     }
+
+    private val waffRegex = Regex("""document\.cookie\s*=\s*['"](?<waff>.*)['"]""")
 }
