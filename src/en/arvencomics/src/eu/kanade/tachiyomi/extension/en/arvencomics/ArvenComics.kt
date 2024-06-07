0cc8833e893b31d7f7e0db84b4c6af29a12fb02c
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-06-07 01:25:51 -0700

    Add Arven Scans (#3454)
    * Add Arven Scans
    
    * Swapped version code back to 0
    
    * Swapped icons for non-outlined version

---
+++ src/en/arvencomics/src/eu/kanade/tachiyomi/extension/en/arvencomics/ArvenComics.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.en.arvencomics
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class ArvenComics : MangaThemesia(
+    "Arven Scans",
+    "https://arvencomics.com",
+    "en",
+    mangaUrlDirectory = "/series",
+)
