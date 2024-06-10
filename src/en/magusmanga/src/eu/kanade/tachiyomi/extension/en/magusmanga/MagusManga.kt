4b083ef1005b8170d166c1a815f8bb42652a99af
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-06-10 05:54:24 -0700

    Magus Manga: Update domain & icons (#3491)
    * MagusManga: Update domain
    
    * MagusManga: Update icons
    
    * MagusManga: Swap to `MangaThemesiaALt`
    
    * MagusManga: Use LS `wafffCookieInterceptor` code for chapter page loading issue

--- src/en/magusmanga/src/eu/kanade/tachiyomi/extension/en/magusmanga/MagusManga.kt
+++ src/en/magusmanga/src/eu/kanade/tachiyomi/extension/en/magusmanga/MagusManga.kt
@@ -1,21 +1,57 @@
 package eu.kanade.tachiyomi.extension.en.magusmanga
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaAlt
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.Cookie
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import org.jsoup.Jsoup
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
-class MagusManga : MangaThemesia(
+class MagusManga : MangaThemesiaAlt(
     "Magus Manga",
-    "https://neroscans.com",
+    "https://dmvdepot.com",
     "en",
     mangaUrlDirectory = "/series",
     dateFormat = SimpleDateFormat("MMMMM dd, yyyy", Locale("en")),
 ) {
     override val id = 7792477462646075400
 
-    override val client = super.client.newBuilder()
+    override val client: OkHttpClient = super.client.newBuilder()
+        .addInterceptor(::wafffCookieInterceptor)
         .rateLimit(1, 1, TimeUnit.SECONDS)
         .build()
+
+    private fun wafffCookieInterceptor(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val response = chain.proceed(request)
+
+        val document = Jsoup.parse(
+            response.peekBody(Long.MAX_VALUE).string(),
+            response.request.url.toString(),
+        )
+
+        return if (document.selectFirst("script:containsData(wafff)") != null) {
+            val script = document.selectFirst("script:containsData(wafff)")!!.data()
+
+            val cookie = waffRegex.find(script)?.groups?.get("waff")?.value
+                ?.let { Cookie.parse(request.url, it) }
+
+            client.cookieJar.saveFromResponse(
+                request.url,
+                listOfNotNull(cookie),
+            )
+
+            response.close()
+
+            chain.proceed(request)
+        } else {
+            response
+        }
+    }
+
+    private val waffRegex = Regex("""document\.cookie\s*=\s*['"](?<waff>.*)['"]""")
 }
