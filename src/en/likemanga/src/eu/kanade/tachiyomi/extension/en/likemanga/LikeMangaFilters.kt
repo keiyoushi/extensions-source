04be842da74b4debc586e8c5c08ab102ce907b4b
Chopper <156493704+choppeh@users.noreply.github.com>
2024-06-14 04:25:19 -0300

    Remove ZinMangaIO and Move theme to Individual Extension (#3520)
    * Remove ZinMangaIO
    
    * Move likemanga to individual extension
    
    * Cleanup
    
    * Remove breakline
    
    * Add named parameter
    
    * Undo removing headersBuilder

--- src/en/likemanga/src/eu/kanade/tachiyomi/extension/en/likemanga/LikeMangaFilters.kt
+++ src/en/likemanga/src/eu/kanade/tachiyomi/extension/en/likemanga/LikeMangaFilters.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.multisrc.likemanga
+package eu.kanade.tachiyomi.extension.en.likemanga
 
 import eu.kanade.tachiyomi.source.model.Filter
 
--- src/en/zinmanhwa/src/eu/kanade/tachiyomi/extension/en/zinmanhwa/ZinMangaIO.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.zinmanhwa
-
-import eu.kanade.tachiyomi.multisrc.likemanga.LikeManga
-
-class ZinMangaIO : LikeManga("ZinManga.io", "https://zinmanga.io", "en") {
-    override val versionId = 2
-}
