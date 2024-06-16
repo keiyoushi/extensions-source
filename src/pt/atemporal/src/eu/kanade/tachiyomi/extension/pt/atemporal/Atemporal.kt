76d5e87b434f1d15ad1034b477d76cc7f718681d
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-06-16 10:33:34 +0200

    use cookieinterceptor

--- src/pt/atemporal/src/eu/kanade/tachiyomi/extension/pt/atemporal/Atemporal.kt
+++ src/pt/atemporal/src/eu/kanade/tachiyomi/extension/pt/atemporal/Atemporal.kt
@@ -1,10 +1,7 @@
 package eu.kanade.tachiyomi.extension.pt.atemporal
 
-import android.webkit.CookieManager
+import eu.kanade.tachiyomi.lib.cookieinterceptor.CookieInterceptor
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import okhttp3.Cookie
-import okhttp3.CookieJar
-import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -15,35 +12,11 @@ class Atemporal : Madara(
     "pt-BR",
     dateFormat = SimpleDateFormat("d 'de' MMMM 'de' yyyy", Locale("pt", "BR")),
 ) {
+    // Cookie required for search
+    private val cookieInterceptor = CookieInterceptor(baseUrl.toHttpUrl().host, "visited" to "true")
+
     override val client = network.cloudflareClient.newBuilder()
-        // Force visited=true cookie so we can use search
-        // Implementation adapted from MangaFox
-        .cookieJar(
-            object : CookieJar {
-                private val cookieManager by lazy { CookieManager.getInstance() }
-
-                init {
-                    cookieManager.setCookie(baseUrl.toHttpUrl().host, "visited=true")
-                }
-
-                override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
-                    val urlString = url.toString()
-                    cookies.forEach { cookieManager.setCookie(urlString, it.toString()) }
-                }
-
-                override fun loadForRequest(url: HttpUrl): List<Cookie> {
-                    val cookies = cookieManager.getCookie(url.toString())
-
-                    return if (cookies != null && cookies.isNotEmpty()) {
-                        cookies.split(";").mapNotNull {
-                            Cookie.parse(url, it)
-                        }
-                    } else {
-                        emptyList()
-                    }
-                }
-            },
-        )
+        .addNetworkInterceptor(cookieInterceptor)
         .build()
 
     override val useLoadMoreRequest = LoadMoreStrategy.Never
