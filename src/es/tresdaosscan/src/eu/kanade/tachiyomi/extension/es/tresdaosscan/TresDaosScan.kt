d3b0835ba0b4a6e7d763bfe2a6da91d1baadfd80
bapeey <90949336+bapeey@users.noreply.github.com>
2024-06-17 07:36:20 -0500

    TresDaosScan: Change theme (#3611)
    Update domain

--- src/es/tresdaosscan/src/eu/kanade/tachiyomi/extension/es/tresdaosscan/TresDaosScan.kt
+++ src/es/tresdaosscan/src/eu/kanade/tachiyomi/extension/es/tresdaosscan/TresDaosScan.kt
@@ -1,23 +1,19 @@
 package eu.kanade.tachiyomi.extension.es.tresdaosscan
 
-import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class TresDaosScan : Madara(
+class TresDaosScan : MangaThemesia(
     "Tres Daos Scan",
     "https://tresdaos.com",
     "es",
-    SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
 ) {
-    // Site move from MangaThemesia to Madara
     override val versionId = 4
 
     override val client = super.client.newBuilder()
         .rateLimit(2)
         .build()
-
-    override val useLoadMoreRequest = LoadMoreStrategy.Always
-    override val useNewChapterEndpoint = true
 }
