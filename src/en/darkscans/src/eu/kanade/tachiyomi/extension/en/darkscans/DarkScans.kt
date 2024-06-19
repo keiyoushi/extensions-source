4c313444fb763ce9470377c7371043ba346ff293
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-06-19 18:12:25 +0200

    Dark Scans: update domain

--- src/en/darkscans/src/eu/kanade/tachiyomi/extension/en/darkscans/DarkScans.kt
+++ src/en/darkscans/src/eu/kanade/tachiyomi/extension/en/darkscans/DarkScans.kt
@@ -4,13 +4,13 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 
-class DarkScans : Madara("Dark Scans", "https://darkscans.com", "en") {
-
+class DarkScans : Madara("Dark Scans", "https://rightdark-scan.com", "en") {
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(20, 4)
         .build()
 
-    override val mangaSubString = "all-series"
+    override val mangaDetailsSelectorStatus = "div.summary-heading:contains(Status) + div.summary-content"
 
-    override val filterNonMangaItems = false
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = true
 }
