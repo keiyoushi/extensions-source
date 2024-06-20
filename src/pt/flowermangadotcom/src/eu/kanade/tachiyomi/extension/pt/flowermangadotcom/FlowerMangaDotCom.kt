d1174639720d0058c184f6ea2bea997c90bf1bec
Chopper <156493704+choppeh@users.noreply.github.com>
2024-06-20 06:07:43 -0300

    FlowerMangaDotCom: Update domain (#3628)
    * Update domain
    
    * Remove unused import

--- src/pt/flowermangadotcom/src/eu/kanade/tachiyomi/extension/pt/flowermangadotcom/FlowerMangaDotCom.kt
+++ src/pt/flowermangadotcom/src/eu/kanade/tachiyomi/extension/pt/flowermangadotcom/FlowerMangaDotCom.kt
@@ -5,16 +5,15 @@ import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 import java.text.SimpleDateFormat
 import java.util.Locale
-import java.util.concurrent.TimeUnit
 
 class FlowerMangaDotCom : Madara(
     "FlowerManga.com",
-    "https://flowermanga.com",
+    "https://flowermangas.com",
     "pt-BR",
-    SimpleDateFormat("dd MMMMM yyyy", Locale("pt", "BR")),
+    SimpleDateFormat("dd MMM yyyy", Locale("pt", "BR")),
 ) {
     override val client: OkHttpClient = super.client.newBuilder()
-        .rateLimit(1, 2, TimeUnit.SECONDS)
+        .rateLimit(2)
         .build()
 
     override val useLoadMoreRequest = LoadMoreStrategy.Never
