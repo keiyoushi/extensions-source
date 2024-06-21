196a45041def54c96ec547f7684778c8c403a208
bapeey <90949336+bapeey@users.noreply.github.com>
2024-06-21 03:42:43 -0500

    DarkScans: Update domain (#3662)
    * Update domain
    
    * why AS breaks my adb

--- src/en/darkscans/src/eu/kanade/tachiyomi/extension/en/darkscans/DarkScans.kt
+++ src/en/darkscans/src/eu/kanade/tachiyomi/extension/en/darkscans/DarkScans.kt
@@ -4,13 +4,16 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 
-class DarkScans : Madara("Dark Scans", "https://darkscans.com", "en") {
+class DarkScans : Madara("Dark Scans", "https://darkscans.net", "en") {
 
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(20, 4)
         .build()
 
-    override val mangaSubString = "all-series"
+    override val mangaSubString = "mangas"
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+    override val useNewChapterEndpoint = true
 
     override val filterNonMangaItems = false
 }
