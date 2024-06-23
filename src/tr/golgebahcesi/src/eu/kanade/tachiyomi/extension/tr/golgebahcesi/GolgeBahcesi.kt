4962aeea4fefc6d48a70e6d26dad088256d97762
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-06-23 20:15:23 +0200

    Add Gölge Bahçesi

---
+++ src/tr/golgebahcesi/src/eu/kanade/tachiyomi/extension/tr/golgebahcesi/GolgeBahcesi.kt
@@ -0,0 +1,33 @@
+package eu.kanade.tachiyomi.extension.tr.golgebahcesi
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.Request
+import org.jsoup.nodes.Document
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class GolgeBahcesi : MangaThemesia(
+    "Gölge Bahçesi",
+    "https://golgebahcesi.com",
+    "tr",
+    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("tr")),
+) {
+    private var captchaUrl: String? = null
+
+    override fun mangaDetailsRequest(manga: SManga): Request =
+        captchaUrl?.let { GET(it, headers) }.also { captchaUrl = null }
+            ?: super.mangaDetailsRequest(manga)
+
+    override fun pageListParse(document: Document): List<Page> {
+        if (document.selectFirst("#readerarea form, #readerarea input[value=Doğrula]") != null) {
+            captchaUrl = document.selectFirst("#readerarea form")
+                ?.attr("abs:action")
+                ?: "$baseUrl/kontrol/"
+            throw Exception("WebView'da captcha çözün.")
+        }
+        return super.pageListParse(document)
+    }
+}
