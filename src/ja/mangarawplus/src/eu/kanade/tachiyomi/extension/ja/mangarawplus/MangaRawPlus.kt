1e0373838374f80804315a42f7dbf0cbb8c2e093
Yush0DAN <157328957+Yush0DAN@users.noreply.github.com>
2024-06-24 09:14:10 -0600

    MANGARAW+: update domain  (#3727)
    update domain

--- src/ja/mangarawplus/src/eu/kanade/tachiyomi/extension/ja/mangarawplus/MangaRawPlus.kt
+++ src/ja/mangarawplus/src/eu/kanade/tachiyomi/extension/ja/mangarawplus/MangaRawPlus.kt
@@ -3,8 +3,7 @@ package eu.kanade.tachiyomi.extension.ja.mangarawplus
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import org.jsoup.nodes.Element
 
-class MangaRawPlus : Madara("MANGARAW+", "https://newmangaraw.net", "ja") {
-    override val mangaSubString = "ts"
+class MangaRawPlus : Madara("MANGARAW+", "https://mangarawpedia.com", "ja") {
     override fun imageFromElement(element: Element): String? {
         return when {
             element.hasAttr("data-src-img") -> element.absUrl("data-src-img")
