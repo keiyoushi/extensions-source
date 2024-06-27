30da6534beed029a63cbb097e0a4ddb210149455
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-06-27 02:16:07 -0700

    Removed unnecessary `dateFormat` override

--- src/tr/adumanga/src/eu/kanade/tachiyomi/extension/tr/adumanga/AduManga.kt
+++ src/tr/adumanga/src/eu/kanade/tachiyomi/extension/tr/adumanga/AduManga.kt
@@ -1,12 +1,9 @@
 package eu.kanade.tachiyomi.extension.tr.adumanga
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class AduManga : MangaThemesia(
     "Adu Manga",
     "https://adumanga.com",
     "tr",
-    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("en")),
 )
