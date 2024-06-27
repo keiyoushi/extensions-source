36757ed67a38ce8889322be4d181a3dddf036692
bapeey <90949336+bapeey@users.noreply.github.com>
2024-06-27 03:23:52 -0500

    Traducciones Moonlight: Fix pages not found (#3766)
    another one

--- src/es/traduccionesmoonlight/src/eu/kanade/tachiyomi/extension/es/traduccionesmoonlight/TraduccionesMoonlight.kt
+++ src/es/traduccionesmoonlight/src/eu/kanade/tachiyomi/extension/es/traduccionesmoonlight/TraduccionesMoonlight.kt
@@ -1,6 +1,11 @@
 package eu.kanade.tachiyomi.extension.es.traduccionesmoonlight
 
 import eu.kanade.tachiyomi.multisrc.mangaesp.MangaEsp
+import eu.kanade.tachiyomi.network.POST
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.FormBody
+import okhttp3.Response
 
 class TraduccionesMoonlight : MangaEsp(
     "Traducciones Moonlight",
@@ -9,4 +14,21 @@ class TraduccionesMoonlight : MangaEsp(
 ) {
     // Mangathemesia -> MangaEsp
     override val versionId = 3
+
+    override fun pageListParse(response: Response): List<Page> {
+        var doc = response.asJsoup()
+        val form = doc.selectFirst("body > form[method=post]")
+        if (form != null) {
+            val url = form.attr("action")
+            val headers = headersBuilder().set("Referer", doc.location()).build()
+            val body = FormBody.Builder()
+            form.select("input").forEach {
+                body.add(it.attr("name"), it.attr("value"))
+            }
+            doc = client.newCall(POST(url, headers, body.build())).execute().asJsoup()
+        }
+        return doc.select("main.contenedor.read img, main > img[src]").mapIndexed { i, element ->
+            Page(i, imageUrl = element.attr("abs:src"))
+        }
+    }
 }
