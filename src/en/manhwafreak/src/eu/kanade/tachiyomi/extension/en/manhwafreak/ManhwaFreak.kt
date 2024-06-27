5db7fd26b7e6d33207e27e333476ee404c94cbd6
Yush0DAN <157328957+Yush0DAN@users.noreply.github.com>
2024-06-26 21:37:27 -0600

    minor change

--- src/en/manhwafreak/src/eu/kanade/tachiyomi/extension/en/manhwafreak/ManhwaFreak.kt
+++ src/en/manhwafreak/src/eu/kanade/tachiyomi/extension/en/manhwafreak/ManhwaFreak.kt
@@ -6,5 +6,8 @@ import okhttp3.HttpUrl.Companion.toHttpUrl
 
 class ManhwaFreak : MangaThemesia("Manhwa Freak", "https://manhwafreak.site", "en") {
 
-    override val client = super.client.newBuilder().rateLimitHost(baseUrl.toHttpUrl(), 3, 1).build()
+    override val client = super.client.newBuilder()
+        .rateLimitHost(baseUrl
+        .toHttpUrl(), 3, 1)
+        .build()
 }
