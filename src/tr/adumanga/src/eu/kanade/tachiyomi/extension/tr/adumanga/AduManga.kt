0431eddddc68d6a0e816b6681c3bfaefd83ae22e
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-06-28 03:47:45 -0700

    Fix Adu Manga URL & date format (#3763)
    * Fixed baseURL mismatch
    
    * Replaced `Locale` in `dateFormat` to `en`
    
    * Removed unnecessary `dateFormat` override

--- src/tr/adumanga/src/eu/kanade/tachiyomi/extension/tr/adumanga/AduManga.kt
+++ src/tr/adumanga/src/eu/kanade/tachiyomi/extension/tr/adumanga/AduManga.kt
@@ -1,12 +1,9 @@
 package eu.kanade.tachiyomi.extension.tr.adumanga
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class AduManga : MangaThemesia(
     "Adu Manga",
-    "https://www.mangacim.com",
+    "https://adumanga.com",
     "tr",
-    dateFormat = SimpleDateFormat("MMMM d, yyy", Locale("tr")),
 )
