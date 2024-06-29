9fdb6f0c9b4313d2f2c39da7a6ab01b852917d50
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-06-29 19:03:03 +0500

    dates

--- lib-multisrc/flixscans/src/eu/kanade/tachiyomi/multisrc/flixscans/FlixScans.kt
+++ lib-multisrc/flixscans/src/eu/kanade/tachiyomi/multisrc/flixscans/FlixScans.kt
@@ -18,6 +18,7 @@ import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
+import java.util.Calendar
 
 abstract class FlixScans(
     override val name: String,
@@ -239,8 +240,78 @@ abstract class FlixScans(
             SChapter.create().apply {
                 setUrlWithoutDomain(element.absUrl("href"))
                 name = element.select("span.font-normal").text()
-                // TODO relative date
+                date_upload = element.selectFirst("div:not(:has(> svg)) > span.text-xs")
+                    ?.text().parseRelativeDate()
             }
+        }
+    }
+
+    protected open fun String?.parseRelativeDate(): Long {
+        this ?: return 0L
+
+        val number = Regex("""(\d+)""").find(this)?.value?.toIntOrNull() ?: 0
+        val cal = Calendar.getInstance()
+
+        return when {
+            listOf("second", "ثانية").any { contains(it, true) } -> {
+                cal.apply { add(Calendar.SECOND, -number) }.timeInMillis
+            }
+
+            contains("دقيقتين", true) -> {
+                cal.apply { add(Calendar.MINUTE, -2) }.timeInMillis
+            }
+            listOf("minute", "دقائق").any { contains(it, true) } -> {
+                cal.apply { add(Calendar.MINUTE, -number) }.timeInMillis
+            }
+
+            contains("ساعتان", true) -> {
+                cal.apply { add(Calendar.HOUR, -2) }.timeInMillis
+            }
+            listOf("hour", "ساعات").any { contains(it, true) } -> {
+                cal.apply { add(Calendar.HOUR, -number) }.timeInMillis
+            }
+
+            contains("يوم", true) -> {
+                cal.apply { add(Calendar.DAY_OF_YEAR, -1) }.timeInMillis
+            }
+            contains("يومين", true) -> {
+                cal.apply { add(Calendar.DAY_OF_YEAR, -2) }.timeInMillis
+            }
+            listOf("day", "أيام").any { contains(it, true) } -> {
+                cal.apply { add(Calendar.DAY_OF_YEAR, -number) }.timeInMillis
+            }
+
+            contains("أسبوع", true) -> {
+                cal.apply { add(Calendar.WEEK_OF_YEAR, -1) }.timeInMillis
+            }
+            contains("أسبوعين", true) -> {
+                cal.apply { add(Calendar.WEEK_OF_YEAR, -2) }.timeInMillis
+            }
+            listOf("week", "أسابيع").any { contains(it, true) } -> {
+                cal.apply { add(Calendar.WEEK_OF_YEAR, -number) }.timeInMillis
+            }
+
+            contains("شهر", true) -> {
+                cal.apply { add(Calendar.MONTH, -1) }.timeInMillis
+            }
+            contains("شهرين", true) -> {
+                cal.apply { add(Calendar.MONTH, -2) }.timeInMillis
+            }
+            listOf("month", "أشهر").any { contains(it, true) } -> {
+                cal.apply { add(Calendar.MONTH, -number) }.timeInMillis
+            }
+
+            contains("سنة", true) -> {
+                cal.apply { add(Calendar.YEAR, -1) }.timeInMillis
+            }
+            contains("سنتان", true) -> {
+                cal.apply { add(Calendar.YEAR, -2) }.timeInMillis
+            }
+            listOf("year", "سنوات").any { contains(it, true) } -> {
+                cal.apply { add(Calendar.YEAR, -number) }.timeInMillis
+            }
+
+            else -> 0L
         }
     }
 
