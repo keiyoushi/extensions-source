ae29d16c2582c04f8b64986717a363cf3bc0b6bf
Chopper <156493704+choppeh@users.noreply.github.com>
2024-06-29 04:11:20 -0300

    Remove FleksyScans (#3802)

--- src/en/fleksyscans/src/eu/kanade/tachiyomi/extension/en/fleksyscans/FleksyScans.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.fleksyscans
-
-import eu.kanade.tachiyomi.multisrc.fuzzydoodle.FuzzyDoodle
-
-class FleksyScans : FuzzyDoodle("FleksyScans", "https://flexscans.com", "en") {
-    override fun chapterListSelector() = "div#chapters-list > a[href]:not(:has(span:contains(vip)))"
-}
