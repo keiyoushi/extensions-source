22736ebe0534cdf2736f8d7cf4599a116cbd06af
Chopper <156493704+choppeh@users.noreply.github.com>
2024-06-29 18:40:40 -0300

    Add random UA

--- src/pt/hentaiteca/src/eu/kanade/tachiyomi/extension/pt/hentaiteca/HentaiTeca.kt
+++ src/pt/hentaiteca/src/eu/kanade/tachiyomi/extension/pt/hentaiteca/HentaiTeca.kt
@@ -1,20 +1,41 @@
 package eu.kanade.tachiyomi.extension.pt.hentaiteca
 
+import android.app.Application
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.lib.randomua.addRandomUAPreferenceToScreen
+import eu.kanade.tachiyomi.lib.randomua.getPrefCustomUA
+import eu.kanade.tachiyomi.lib.randomua.getPrefUAType
+import eu.kanade.tachiyomi.lib.randomua.setRandomUserAgent
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.ConfigurableSource
 import okhttp3.OkHttpClient
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
-class HentaiTeca : Madara(
-    "Hentai Teca",
-    "https://hentaiteca.net",
-    "pt-BR",
-    SimpleDateFormat("MMMMM dd, yyyy", Locale("pt", "BR")),
-) {
+class HentaiTeca :
+    Madara(
+        "Hentai Teca",
+        "https://hentaiteca.net",
+        "pt-BR",
+        SimpleDateFormat("MMMMM dd, yyyy", Locale("pt", "BR")),
+    ),
+    ConfigurableSource {
+
+    private val preferences = Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
 
     override val client: OkHttpClient = super.client.newBuilder()
+        .setRandomUserAgent(
+            preferences.getPrefUAType(),
+            preferences.getPrefCustomUA(),
+        )
         .rateLimit(1, 2, TimeUnit.SECONDS)
         .build()
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        addRandomUAPreferenceToScreen(screen)
+    }
 }
