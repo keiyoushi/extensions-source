737fd150b6b79b70e3ed3c13c15fdbe531fa2d9b
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-06-30 15:44:10 +0500

    rebrand, remove multisrc

--- src/en/flixscans/src/eu/kanade/tachiyomi/extension/en/flixscans/FlixScans.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.flixscans
-
-import eu.kanade.tachiyomi.multisrc.flixscans.FlixScans
-
-class FlixScans : FlixScans(
-    "Flix Scans",
-    "https://flixscans.net",
-    "en",
-)
