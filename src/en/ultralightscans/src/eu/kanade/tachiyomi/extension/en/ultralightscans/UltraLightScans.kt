5686dec392a2c99bf4cc4b2c1717162fa2281185
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-05 05:56:03 -0300

    Remove UltraLightScans (#3863)

--- src/en/ultralightscans/src/eu/kanade/tachiyomi/extension/en/ultralightscans/UltraLightScans.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.ultralightscans
-
-import eu.kanade.tachiyomi.multisrc.bakkin.BakkinReaderX
-
-class UltraLightScans : BakkinReaderX("UltraLight Scans", "https://readlight.org/", "en")
