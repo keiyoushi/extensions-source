ccf31779fdaf436680cab6866db07ac2f8b13e61
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-05 05:53:28 -0300

    Siimanga: Migrate theme (#3857)
    * Migrate theme
    
    * Update icon
    
    * Bump version
    
    * Enable project filter
    
    * Remove id

--- src/id/siimanga/src/eu/kanade/tachiyomi/extension/id/siimanga/Siikomik.kt
+++ src/id/siimanga/src/eu/kanade/tachiyomi/extension/id/siimanga/Siikomik.kt
@@ -1,25 +1,18 @@
 package eu.kanade.tachiyomi.extension.id.siimanga
 
-import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import java.text.SimpleDateFormat
-import java.util.Locale
 
-class Siikomik : Madara(
+class Siikomik : MangaThemesia(
     "Siikomik",
     "https://siikomik.com",
     "id",
-    dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.US),
 ) {
-    override val id = 5693774260946188681
-
-    override val mangaSubString = "komik"
+    override val versionId = 2
 
     override val client = super.client.newBuilder()
         .rateLimit(3)
         .build()
 
-    override val useLoadMoreRequest = LoadMoreStrategy.Always
-
-    override val useNewChapterEndpoint = false
+    override val hasProjectPage = true
 }
