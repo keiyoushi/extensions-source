0f6ad1354e2b94c6fa5f691bd955fabcba1bfb2a
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-07-06 20:04:13 +0200

    SummerToon: update domain

--- src/tr/summertoon/src/eu/kanade/tachiyomi/extension/tr/summertoon/SummerToon.kt
+++ src/tr/summertoon/src/eu/kanade/tachiyomi/extension/tr/summertoon/SummerToon.kt
@@ -7,7 +7,7 @@ import java.util.Locale
 
 class SummerToon : MangaThemesia(
     "SummerToon",
-    "https://summertoon.me",
+    "https://summertoon.biz",
     "tr",
     dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("tr")),
 ) {
