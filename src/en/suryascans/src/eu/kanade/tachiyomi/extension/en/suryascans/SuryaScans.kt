b9e5df5c5310c96bccf8104427a4c8e7b76278d0
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-06 08:12:44 -0300

    GenzToons(Surya Scans): Update domain, icons and extension name (#3883)
    * Update domain, icons and extension name
    
    * Cleanup

--- src/en/suryascans/src/eu/kanade/tachiyomi/extension/en/suryascans/SuryaScans.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.suryascans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class SuryaScans : MangaThemesia("Surya Scans", "https://suryatoon.com", "en")
