b35a4834578c91d55c95fc1e6ac578c324ee0af7
Yush0DAN <157328957+Yush0DAN@users.noreply.github.com>
2024-07-07 06:17:06 -0600

    Blazescans: Update domain (#3897)
    * Update domain
    
    * remove character (")

--- src/en/blazescans/src/eu/kanade/tachiyomi/extension/en/blazescans/Blazescans.kt
+++ src/en/blazescans/src/eu/kanade/tachiyomi/extension/en/blazescans/Blazescans.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import java.util.concurrent.TimeUnit
 
-class Blazescans : MangaThemesia("Blazescans", "https://blazescans.com", "en") {
+class Blazescans : MangaThemesia("Blazescans", "https://blazetoon.com", "en") {
     override val client = super.client.newBuilder()
         .rateLimit(1, 2, TimeUnit.SECONDS)
         .build()
