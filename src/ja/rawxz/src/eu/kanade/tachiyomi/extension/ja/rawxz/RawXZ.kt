66d6e7adf9b508fd435859aaf0291cc1c7cb13da
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-07-07 14:20:02 +0200

    RawXZ: update domain (#3901)

--- src/ja/rawxz/src/eu/kanade/tachiyomi/extension/ja/rawxz/RawXZ.kt
+++ src/ja/rawxz/src/eu/kanade/tachiyomi/extension/ja/rawxz/RawXZ.kt
@@ -1,14 +1,17 @@
 package eu.kanade.tachiyomi.extension.ja.rawxz
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class RawXZ : Madara(
     "RawXZ",
-    "https://rawxz.net",
+    "https://rawxz.si",
     "ja",
+    dateFormat = SimpleDateFormat("M月 d, yyyy", Locale.ROOT),
 ) {
     override val useLoadMoreRequest = LoadMoreStrategy.Never
     override val useNewChapterEndpoint = false
 
-    override val mangaSubString = "comic"
+    override val mangaSubString = "jp-manga"
 }
