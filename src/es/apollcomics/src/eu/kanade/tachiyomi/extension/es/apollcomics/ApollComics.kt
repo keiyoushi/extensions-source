f3e5cc030a070e47804e15f8a3bd4b02a4c481bd
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-07-07 14:17:45 +0200

    ApollComics: update domain (#3899)

--- src/es/apollcomics/src/eu/kanade/tachiyomi/extension/es/apollcomics/ApollComics.kt
+++ src/es/apollcomics/src/eu/kanade/tachiyomi/extension/es/apollcomics/ApollComics.kt
@@ -4,4 +4,12 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class ApollComics : Madara("ApollComics", "https://apollcomics.xyz", "es", SimpleDateFormat("dd MMMM, yyyy", Locale("es")))
+class ApollComics : Madara(
+    "ApollComics",
+    "https://apollcomics.es",
+    "es",
+    SimpleDateFormat("MMMM d, yyyy", Locale("es")),
+) {
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = false
+}
