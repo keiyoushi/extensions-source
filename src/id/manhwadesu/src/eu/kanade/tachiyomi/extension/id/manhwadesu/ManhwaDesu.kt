f9fcd6d660e56fdad8bb1bf298f2f818aa01cafd
Yush0DAN <157328957+Yush0DAN@users.noreply.github.com>
2024-07-12 22:33:13 -0600

    Fix - Update domain, add ratelimit

--- src/id/manhwadesu/src/eu/kanade/tachiyomi/extension/id/manhwadesu/ManhwaDesu.kt
+++ src/id/manhwadesu/src/eu/kanade/tachiyomi/extension/id/manhwadesu/ManhwaDesu.kt
@@ -1,11 +1,14 @@
 package eu.kanade.tachiyomi.extension.id.manhwadesu
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import org.jsoup.nodes.Element
 
 class ManhwaDesu : MangaThemesia("ManhwaDesu", "https://manhwadesu.cc", "id", "/komik") {
 
-    override val client = network.cloudflareClient
+    override val client = network.cloudflareClient.newBuilder()
+        .rateLimit(4,2)
+        .build()
 
     override fun headersBuilder() = super.headersBuilder()
         .set("referer", "$baseUrl/")
