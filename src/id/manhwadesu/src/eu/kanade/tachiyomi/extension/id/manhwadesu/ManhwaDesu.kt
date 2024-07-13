8c16b550911221b8c5d3f19d43d1b8d48c7329ff
Yush0DAN <157328957+Yush0DAN@users.noreply.github.com>
2024-07-12 22:25:25 -0600

    Fix - Update domain - error CF

--- src/id/manhwadesu/src/eu/kanade/tachiyomi/extension/id/manhwadesu/ManhwaDesu.kt
+++ src/id/manhwadesu/src/eu/kanade/tachiyomi/extension/id/manhwadesu/ManhwaDesu.kt
@@ -1,15 +1,14 @@
 package eu.kanade.tachiyomi.extension.id.manhwadesu
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import okhttp3.OkHttpClient
 import org.jsoup.nodes.Element
 
-class ManhwaDesu : MangaThemesia("ManhwaDesu", "https://manhwadesu.guru", "id", "/komik") {
+class ManhwaDesu : MangaThemesia("ManhwaDesu", "https://manhwadesu.cc", "id", "/komik") {
 
-    override val client: OkHttpClient = super.client.newBuilder()
-        .rateLimit(4)
-        .build()
+    override val client = network.cloudflareClient
+
+    override fun headersBuilder() = super.headersBuilder()
+        .set("referer", "$baseUrl/")
 
     override fun Element.imgAttr(): String {
         attributes()
