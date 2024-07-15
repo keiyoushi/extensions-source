2315ea8004cea7b3723da1ab27dbf5faf1ddb952
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-15 02:21:38 -0300

    Remove ToshiWaYume (#4013)

--- src/pt/toshiwayume/src/eu/kanade/tachiyomi/extension/pt/toshiwayume/ToshiWaYume.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.pt.toshiwayume
-
-import eu.kanade.tachiyomi.multisrc.foolslide.FoolSlide
-
-class ToshiWaYume : FoolSlide(
-    "Toshi wa Yume",
-    "https://leitoronline.toshiwayume.com.br",
-    "pt-BR",
-)
