3fe1d40e3981d00e0248155a821f892501b547d8
bapeey <90949336+bapeey@users.noreply.github.com>
2024-07-15 00:15:48 -0500

    Add AnonimusTLS (#3998)

---
+++ src/es/anonimustls/src/eu/kanade/tachiyomi/extension/es/anonimustls/AnonimusTls.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.es.anonimustls
+
+import eu.kanade.tachiyomi.multisrc.mangaesp.MangaEsp
+
+class AnonimusTls : MangaEsp("AnonimusTLS", "https://anonimustls.com", "es", "https://api.anonimustls.com")
