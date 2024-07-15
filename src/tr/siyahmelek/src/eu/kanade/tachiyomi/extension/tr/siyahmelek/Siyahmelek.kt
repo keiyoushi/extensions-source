cf553fcb109a09acb301df8ea5b880197605286c
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-15 02:24:42 -0300

    Siyahmelek: Update domain (#4016)
    Update domain

--- src/tr/siyahmelek/src/eu/kanade/tachiyomi/extension/tr/siyahmelek/Siyahmelek.kt
+++ src/tr/siyahmelek/src/eu/kanade/tachiyomi/extension/tr/siyahmelek/Siyahmelek.kt
@@ -7,13 +7,20 @@ import okhttp3.Response
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class Siyahmelek : Madara("Gri Melek", "https://grimelek.co", "tr", SimpleDateFormat("dd MMM yyyy", Locale("tr"))) {
+class Siyahmelek : Madara(
+    "Gri Melek",
+    "https://grimelek.org",
+    "tr",
+    SimpleDateFormat("dd MMM yyyy", Locale("tr")),
+) {
     // Siyahmelek (tr) -> Gri Melek (tr)
     override val id = 6419959498055001014
 
     override val mangaSubString = "seri"
 
     override val useNewChapterEndpoint = true
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
 
     // Skip fake image
     // OK: <div class="page-break no-gaps">
