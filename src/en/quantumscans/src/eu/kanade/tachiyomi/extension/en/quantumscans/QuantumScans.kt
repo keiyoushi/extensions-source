017645c7497350807d03bbb19a4194d69aa33787
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-07-16 10:52:38 +0500

    remove spac

--- src/en/quantumscans/src/eu/kanade/tachiyomi/extension/en/quantumscans/QuantumScans.kt
+++ src/en/quantumscans/src/eu/kanade/tachiyomi/extension/en/quantumscans/QuantumScans.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import java.util.concurrent.TimeUnit
 
-class QuantumScans : MangaThemesia("Quantum Scans", " https://qscomics.org", "en", "/series") {
+class QuantumScans : MangaThemesia("Quantum Scans", "https://qscomics.org", "en", "/series") {
 
     override val client = super.client.newBuilder()
         .rateLimit(3, 1, TimeUnit.SECONDS)
