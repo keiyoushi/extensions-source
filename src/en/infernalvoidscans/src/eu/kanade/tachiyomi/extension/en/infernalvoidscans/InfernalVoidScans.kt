19783c2e830f2608547aae89d445750bdc2e7181
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-17 09:37:55 -0300

    Remove duplicate code

--- src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
+++ src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
@@ -2,9 +2,7 @@ package eu.kanade.tachiyomi.extension.en.infernalvoidscans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
-import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 
 class InfernalVoidScans : MangaThemesia(
@@ -17,50 +15,13 @@ class InfernalVoidScans : MangaThemesia(
     override val hasProjectPage = true
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val url = baseUrl.toHttpUrl().newBuilder()
-            .addPathSegment(mangaUrlDirectory.substring(1))
-            .addQueryParameter("page", page.toString())
+        val url = super.searchMangaRequest(page, query, filters).url.newBuilder()
+            .removeAllQueryParameters("title")
 
         // Filters are not loaded with the ‘s’ parameter. Fix genres filter
         if (query.isNotBlank()) {
             url.addQueryParameter("s", query)
         }
-
-        filters.forEach { filter ->
-            when (filter) {
-                is AuthorFilter -> {
-                    url.addQueryParameter("author", filter.state)
-                }
-                is YearFilter -> {
-                    url.addQueryParameter("yearx", filter.state)
-                }
-                is StatusFilter -> {
-                    url.addQueryParameter("status", filter.selectedValue())
-                }
-                is TypeFilter -> {
-                    url.addQueryParameter("type", filter.selectedValue())
-                }
-                is OrderByFilter -> {
-                    url.addQueryParameter("order", filter.selectedValue())
-                }
-                is GenreListFilter -> {
-                    filter.state
-                        .filter { it.state != Filter.TriState.STATE_IGNORE }
-                        .forEach {
-                            val value = if (it.state == Filter.TriState.STATE_EXCLUDE) "-${it.value}" else it.value
-                            url.addQueryParameter("genre[]", value)
-                        }
-                }
-                // if site has project page, default value "hasProjectPage" = false
-                is ProjectFilter -> {
-                    if (filter.selectedValue() == "project-filter-on") {
-                        url.setPathSegment(0, projectPageString.substring(1))
-                    }
-                }
-                else -> { /* Do Nothing */ }
-            }
-        }
-        url.addPathSegment("")
         return GET(url.build(), headers)
     }
 }
