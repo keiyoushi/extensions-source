ae66e8c9b60fddfb7af458f93f069f9fc42de17e
KenjieDec <65448230+KenjieDec@users.noreply.github.com>
2024-07-17 17:17:10 +0700

    Koharu: Add More Filters, fix popular (#4044)
    * Add More Filters, fix popular
    
    - Website added more filters
    - Apply feature suggestions? -> Title Preferences
    - Little change to description
    
    * Apply Suggestions
    
    - Apply vetleledaal's suggestions
    
    * Apply Suggestion
    
    * kB
    
    * Apply suggestion

--- src/en/koharu/src/eu/kanade/tachiyomi/extension/en/koharu/KoharuDto.kt
+++ src/en/koharu/src/eu/kanade/tachiyomi/extension/en/koharu/KoharuDto.kt
@@ -60,8 +60,16 @@ class Data(
 @Serializable
 class DataKey(
     val id: Int,
+    val size: Double = 0.0,
     val public_key: String,
-)
+) {
+    fun readableSize() = when {
+        size >= 300 * 1000 * 1000 -> "${"%.2f".format(size / (1000.0 * 1000.0 * 1000.0))} GB"
+        size >= 100 * 1000 -> "${"%.2f".format(size / (1000.0 * 1000.0))} MB"
+        size >= 1000 -> "${"%.2f".format(size / (1000.0))} kB"
+        else -> "$size B"
+    }
+}
 
 @Serializable
 class ImagesInfo(
