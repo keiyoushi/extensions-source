38584cd75fd0f18b14f7e6a669461041a374bf82
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-17 07:10:34 -0300

    LXHentai: Update domain (#4065)
    Update domain

--- src/vi/lxhentai/src/eu/kanade/tachiyomi/extension/vi/lxhentai/LxHentai.kt
+++ src/vi/lxhentai/src/eu/kanade/tachiyomi/extension/vi/lxhentai/LxHentai.kt
@@ -23,7 +23,7 @@ class LxHentai : ParsedHttpSource() {
 
     override val name = "LXHentai"
 
-    override val baseUrl = "https://lxmanga.cfd"
+    override val baseUrl = "https://lxmanga.life"
 
     override val lang = "vi"
 
