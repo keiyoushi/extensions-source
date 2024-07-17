9b5dc2abee8b71b3fb40b894faf73fddda804d6e
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-17 13:02:17 -0300

    Rebrand

--- src/en/blazescans/src/eu/kanade/tachiyomi/extension/en/blazescans/FuryManga.kt
+++ src/en/blazescans/src/eu/kanade/tachiyomi/extension/en/blazescans/FuryManga.kt
@@ -4,7 +4,14 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import java.util.concurrent.TimeUnit
 
-class Blazescans : MangaThemesia("Blazescans", "https://blazetoon.com", "en") {
+class FuryManga : MangaThemesia(
+    "Fury Manga",
+    "https://furymanga.com",
+    "en",
+    "/comics",
+) {
+    override val id = 3912200442923601567
+
     override val client = super.client.newBuilder()
         .rateLimit(1, 2, TimeUnit.SECONDS)
         .build()
