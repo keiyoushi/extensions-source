571c21d92ef11a456014ac66040664347c172d44
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-18 07:51:33 -0300

    Add Utoon (#4086)

---
+++ src/en/utoon/src/eu/kanade/tachiyomi/extension/en/utoon/Utoon.kt
@@ -0,0 +1,29 @@
+package eu.kanade.tachiyomi.extension.en.utoon
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.model.SChapter
+import org.jsoup.nodes.Element
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class Utoon : Madara(
+    "Utoon",
+    "https://utoon.net",
+    "en",
+    SimpleDateFormat("dd MMM yyyy", Locale.US),
+) {
+    override val useNewChapterEndpoint = true
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+
+    override val chapterUrlSelector = "div > a"
+
+    override fun chapterFromElement(element: Element): SChapter {
+        return super.chapterFromElement(element).apply {
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+            date_upload = element.selectFirst("span a")?.attr("title")?.let { parseRelativeDate(it) }
+                ?: parseChapterDate("${element.selectFirst(chapterDateSelector())?.text()} $currentYear")
+        }
+    }
+}
