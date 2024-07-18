bca6f71eb90d14aaef978bbef5215be45389d000
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-18 07:43:31 -0300

    InfernalVoidScans: Update domain and fix loading mangas (#4061)
    * Update domain and fix loading mangas
    
    * Fix genre filter
    
    * Remove duplicate code

--- src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
+++ src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
@@ -1,7 +1,27 @@
 package eu.kanade.tachiyomi.extension.en.infernalvoidscans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import okhttp3.Request
 
-class InfernalVoidScans : MangaThemesia("Infernal Void Scans", "https://hivescans.com", "en") {
+class InfernalVoidScans : MangaThemesia(
+    "Infernal Void Scans",
+    "https://hivetoon.com",
+    "en",
+) {
     override val pageSelector = "div#readerarea > p > img"
+
+    override val hasProjectPage = true
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val url = super.searchMangaRequest(page, query, filters).url.newBuilder()
+            .removeAllQueryParameters("title")
+
+        // Filters are not loaded with the ‘s’ parameter. Fix genres filter
+        if (query.isNotBlank()) {
+            url.addQueryParameter("s", query)
+        }
+        return GET(url.build(), headers)
+    }
 }
