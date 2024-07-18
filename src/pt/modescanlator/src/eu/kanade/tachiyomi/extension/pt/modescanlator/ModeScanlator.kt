3eaa92cb2840461f621a73a85f72d7101452acc8
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-17 22:37:17 -0300

    Update domain

--- src/pt/modescanlator/src/eu/kanade/tachiyomi/extension/pt/modescanlator/ModeScanlator.kt
+++ src/pt/modescanlator/src/eu/kanade/tachiyomi/extension/pt/modescanlator/ModeScanlator.kt
@@ -2,7 +2,12 @@ package eu.kanade.tachiyomi.extension.pt.modescanlator
 
 import eu.kanade.tachiyomi.multisrc.heancms.HeanCms
 
-class ModeScanlator : HeanCms("Mode Scanlator", "https://modescanlator.com", "pt-BR") {
+class ModeScanlator : HeanCms(
+    "Mode Scanlator",
+    "https://site.modescanlator.net",
+    "pt-BR",
+    "https://api.modescanlator.net",
+) {
 
     // PeachScan -> HeanCms
     override val versionId = 2
