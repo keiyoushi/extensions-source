eef654f85e4612c3ce1de6ecd46cad9df07627ee
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-07-18 15:53:20 +0500

    Remove King of Manga
    redirects to furymanga

--- src/en/kingofmanga/src/eu/kanade/tachiyomi/extension/en/kingofmanga/KingOfManga.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.kingofmanga
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class KingOfManga : MangaThemesia("King Of Manga", "https://king-ofmanga.com", "en")
