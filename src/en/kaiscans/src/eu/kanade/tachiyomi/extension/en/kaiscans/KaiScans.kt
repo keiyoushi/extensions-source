52831dcfbcd2aa9a327382de0b15af63ba929cb9
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-07-21 09:37:40 +0200

    Umi Scans -> Kai Scans: update domain (#4132)
    * Umi Scans -> Kai Scans: update domain
    
    * use git for history, not comments

--- src/en/kaiscans/src/eu/kanade/tachiyomi/extension/en/kaiscans/KaiScans.kt
+++ src/en/kaiscans/src/eu/kanade/tachiyomi/extension/en/kaiscans/KaiScans.kt
@@ -3,11 +3,8 @@ package eu.kanade.tachiyomi.extension.en.kaiscans
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
-class UmiScans : MangaThemesia("Umi Scans", "https://umiscans.org", "en") {
+class KaiScans : MangaThemesia("Kai Scans", "https://kaiscans.org", "en") {
     override val client = super.client.newBuilder()
         .rateLimit(2)
         .build()
-
-    // Kai Scans -> Umi Scans
-    override val id: Long = 4825368993215448425
 }
