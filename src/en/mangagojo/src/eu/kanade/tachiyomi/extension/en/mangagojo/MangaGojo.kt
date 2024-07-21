c1cd2fc8f5adbf2669f3827f7a6a3d9f273cc1ae
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-07-21 09:17:26 +0200

    Add MangaGojo (#4128)

---
+++ src/en/mangagojo/src/eu/kanade/tachiyomi/extension/en/mangagojo/MangaGojo.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.en.mangagojo
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class MangaGojo : MangaThemesia(
+    "MangaGojo",
+    "https://mangagojo.com",
+    "en",
+)
