42dbbdbebe3ceec12689edadfb99923b94106336
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-07-21 12:15:31 +0500

    remove dead sources (#4126)
    * remove manhwa freak
    
    * remove mangamonks & readm
    
    * remove suger babies
    
    * remove freakscans

--- src/en/freakscans/src/eu/kanade/tachiyomi/extension/en/freakscans/FreakScans.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.freakscans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class FreakScans : MangaThemesia("Freak Scans", "https://freakscans.com", "en")
--- src/en/sugarbabies/src/eu/kanade/tachiyomi/extension/en/sugarbabies/SugarBabies.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.sugarbabies
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class SugarBabies : Madara("Sugar Babies", "https://sugarbbscan.com", "en")
