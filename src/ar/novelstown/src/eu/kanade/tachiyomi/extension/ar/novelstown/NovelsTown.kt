3010063c8edf8a4bc46f61a1cb338e0d33b76f4e
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-24 18:00:35 -0300

    Remove NovelsTown

--- src/ar/novelstown/src/eu/kanade/tachiyomi/extension/ar/novelstown/NovelsTown.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.ar.novelstown
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class NovelsTown : Madara("Novels Town", "https://novelstown.com", "ar") {
-    override val mangaSubString = "الاعمال"
-}
