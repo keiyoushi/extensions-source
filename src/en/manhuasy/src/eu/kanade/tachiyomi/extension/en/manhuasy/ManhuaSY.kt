351dacdef8318f55420fc63057cdc31fa530e308
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-23 22:55:49 -0300

    Remove ManhuaSY

--- src/en/manhuasy/src/eu/kanade/tachiyomi/extension/en/manhuasy/ManhuaSY.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.manhuasy
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class ManhuaSY : Madara("Manhua SY", "https://www.manhuasy.com", "en") {
-    override val mangaSubString = "manhua"
-}
