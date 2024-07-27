9f0f77f00e40da416555026aea55d1e52e5169c9
KenjieDec <65448230+KenjieDec@users.noreply.github.com>
2024-07-27 14:16:58 +0700

    Lint Fix

--- src/en/kami18/src/eu/kanade/tachiyomi/extension/en/kami18/Kami18.kt
+++ src/en/kami18/src/eu/kanade/tachiyomi/extension/en/kami18/Kami18.kt
@@ -8,7 +8,6 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
