e46cfb9493c3a2a399c6ccaf11701330a422a119
bapeey <90949336+bapeey@users.noreply.github.com>
2024-07-27 00:31:33 -0500

    InfernalVoidScans: Fix cloudflare (#4273)
    fix cloudflare

--- src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
+++ src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
@@ -10,6 +10,19 @@ class InfernalVoidScans : MangaThemesia(
     "https://hivetoon.com",
     "en",
 ) {
+    override val client = super.client.newBuilder()
+        .addInterceptor { chain ->
+            val request = chain.request()
+            val headers = request.headers.newBuilder()
+                .set("Cache-Control", "max-age=0")
+                .build()
+            chain.proceed(request.newBuilder().headers(headers).build())
+        }
+        .build()
+
+    override fun headersBuilder() = super.headersBuilder()
+        .set("Cache-Control", "max-age=0")
+
     override val pageSelector = "div#readerarea > p > img"
 
     override val hasProjectPage = true
