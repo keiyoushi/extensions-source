280cb6f819e71c53be9908db633f53675b470d39
Chopper <156493704+choppeh@users.noreply.github.com>
2024-07-28 02:52:20 -0300

    Remove RawMangas (#4303)

--- src/ja/rawmangas/src/eu/kanade/tachiyomi/extension/ja/rawmangas/RawMangas.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.ja.rawmangas
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class RawMangas : Madara("RawMangas", "https://raw-mangas.com", "ja") {
-    override val mangaSubString = "comic"
-}
