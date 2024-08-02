2ce2a33fb1cb03e0f3a9720b371e1aef20664499
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-02 17:17:45 +0500

    Reaper Scans: fix covers and date (#4382)

--- src/en/reaperscans/src/eu/kanade/tachiyomi/extension/en/reaperscans/ReaperScans.kt
+++ src/en/reaperscans/src/eu/kanade/tachiyomi/extension/en/reaperscans/ReaperScans.kt
@@ -1,14 +1,21 @@
 package eu.kanade.tachiyomi.extension.en.reaperscans
 
 import eu.kanade.tachiyomi.multisrc.heancms.HeanCms
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class ReaperScans : HeanCms("Reaper Scans", "https://reaperscans.com", "en") {
 
     override val versionId = 3
 
+    override val client = super.client.newBuilder()
+        .rateLimit(1)
+        .build()
+
     override val useNewChapterEndpoint = true
     override val useNewQueryEndpoint = true
     override val enableLogin = true
-
+    override val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
     override val cdnUrl = "https://media.reaperscans.com/file/4SRBHm"
 }
