e87c1a6b765242b27f1d0933efa9f06da78eb206
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-08-03 05:23:05 +0200

    Remove Manga-Raw.info (unoriginal) (#4387)

--- src/en/mangarawinfo/src/eu/kanade/tachiyomi/extension/en/mangarawinfo/MangaRawInfo.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangarawinfo
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class MangaRawInfo : Madara("Manga-Raw.info (unoriginal)", "https://manga-raw.info", "en") {
-    override val useNewChapterEndpoint = false
-}
