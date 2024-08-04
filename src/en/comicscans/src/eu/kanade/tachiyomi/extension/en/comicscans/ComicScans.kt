27a48480d4ae254518070630aa381f62c1f7a633
lamaxama <163385206+lamaxama@users.noreply.github.com>
2024-08-04 18:56:35 +0800

    Remove ComicScans and AnshScans (#4410)

--- src/en/comicscans/src/eu/kanade/tachiyomi/extension/en/comicscans/ComicScans.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.comicscans
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class ComicScans : Madara("Comic Scans", "https://www.comicscans.org", "en") {
-    override val useNewChapterEndpoint = true
-}
