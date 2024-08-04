d54ed361e789956555a5aad4bfe22515a6680c86
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-08-04 12:47:42 +0200

    Add PussyToons (#4394)

---
+++ src/pt/pussytoons/src/eu/kanade/tachiyomi/extension/pt/pussytoons/PussyToons.kt
@@ -0,0 +1,15 @@
+package eu.kanade.tachiyomi.extension.pt.pussytoons
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class PussyToons : Madara(
+    "PussyToons",
+    "https://pussy.sussytoons.com",
+    "pt-BR",
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("pt", "BR")),
+) {
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = true
+}
