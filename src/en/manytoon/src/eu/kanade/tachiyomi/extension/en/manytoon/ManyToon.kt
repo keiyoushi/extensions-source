3789823bb07a941674ee5803323ce010c5bf7425
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-07 08:44:16 +0500

    ManyToons: fix chapter list & search (#4459)

--- src/en/manytoon/src/eu/kanade/tachiyomi/extension/en/manytoon/ManyToon.kt
+++ src/en/manytoon/src/eu/kanade/tachiyomi/extension/en/manytoon/ManyToon.kt
@@ -1,10 +1,35 @@
 package eu.kanade.tachiyomi.extension.en.manytoon
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.POST
+import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.FormBody
+import okhttp3.Request
+import org.jsoup.nodes.Element
 
 class ManyToon : Madara("ManyToon", "https://manytoon.com", "en") {
 
     override val mangaSubString = "comic"
 
-    override val useNewChapterEndpoint: Boolean = true
+    override val useNewChapterEndpoint = false
+    override val sendViewCount = false
+    override val fetchGenres = false
+
+    override fun searchMangaSelector() = "li.movie-item > a"
+
+    override fun searchMangaFromElement(element: Element): SManga {
+        return SManga.create().apply {
+            setUrlWithoutDomain(element.absUrl("href"))
+            title = element.attr("title")
+        }
+    }
+
+    override fun oldXhrChaptersRequest(mangaId: String): Request {
+        val form = FormBody.Builder()
+            .add("action", "ajax_chap")
+            .add("post_id", mangaId)
+            .build()
+
+        return POST("$baseUrl/wp-admin/admin-ajax.php", xhrHeaders, form)
+    }
 }
