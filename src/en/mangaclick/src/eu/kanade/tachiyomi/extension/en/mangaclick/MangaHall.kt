684bf6517fac4e379eacca2007d31dc821fc3cbf
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-07 10:16:06 +0500

    MangaClick -> MangaHill: update domain

--- src/en/mangaclick/src/eu/kanade/tachiyomi/extension/en/mangaclick/MangaHall.kt
+++ src/en/mangaclick/src/eu/kanade/tachiyomi/extension/en/mangaclick/MangaHall.kt
@@ -2,11 +2,12 @@ package eu.kanade.tachiyomi.extension.en.mangaclick
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class MangaClick : Madara(
-    "MangaClick",
-    "https://mangaclick.org",
+class MangaHall : Madara(
+    "MangaHall",
+    "https://mangahall.org",
     "en",
 ) {
+    override val id = 1234573178818746503
     override val useLoadMoreRequest = LoadMoreStrategy.Always
     override val useNewChapterEndpoint = false
 }
