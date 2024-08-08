28a70388a5b05e6af7d1422d3dcc330f05e14431
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-08 07:24:48 +0500

    Comicaso: change domain and fix page list (#4473)

--- src/id/comicaso/src/eu/kanade/tachiyomi/extension/id/comicaso/Comicaso.kt
+++ src/id/comicaso/src/eu/kanade/tachiyomi/extension/id/comicaso/Comicaso.kt
@@ -1,12 +1,41 @@
 package eu.kanade.tachiyomi.extension.id.comicaso
 
+import android.util.Base64
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.Page
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class Comicaso : MangaThemesia(
     "Comicaso",
-    "https://comicaso.com",
+    "https://comicaso.org",
     "id",
     dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("id")),
-)
+) {
+    override fun pageListParse(document: Document): List<Page> {
+        val script = document.select("script[src^=data:text/javascript;base64,]").map {
+            Base64.decode(
+                it.attr("src").substringAfter("base64,"),
+                Base64.DEFAULT,
+            ).toString(Charsets.UTF_8)
+        }.firstOrNull { it.startsWith("ts_reader.run") }
+            ?: throw Exception("Couldn't find page script")
+
+        countViews(document)
+
+        val imageListJson = JSON_IMAGE_LIST_REGEX.find(script)?.destructured?.toList()?.get(0).orEmpty()
+        val imageList = try {
+            json.parseToJsonElement(imageListJson).jsonArray
+        } catch (_: IllegalArgumentException) {
+            emptyList()
+        }
+        val scriptPages = imageList.mapIndexed { i, jsonEl ->
+            Page(i, document.location(), jsonEl.jsonPrimitive.content)
+        }
+
+        return scriptPages
+    }
+}
