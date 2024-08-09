9217564b385646aab7fa6e62d1bc09d25eeba75e
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-09 10:30:46 +0500

    ngkomik: update domain (#4508)

--- src/id/ngomik/src/eu/kanade/tachiyomi/extension/id/ngomik/Ngomik.kt
+++ src/id/ngomik/src/eu/kanade/tachiyomi/extension/id/ngomik/Ngomik.kt
@@ -8,7 +8,7 @@ import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 
-class Ngomik : MangaThemesia("Ngomik", "https://ngomik.net", "id", "/manga") {
+class Ngomik : MangaThemesia("Ngomik", "https://ngomik.mom", "id", "/manga") {
 
     private val userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
 
