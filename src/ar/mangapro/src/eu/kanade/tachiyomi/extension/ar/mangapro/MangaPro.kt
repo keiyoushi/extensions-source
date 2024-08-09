6d5b8d95a78927f6d8f60c05002ab0672ce68da8
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-09 17:29:53 +0500

    MangaPro: deproxify images (#4533)
    * MangaPro: deproxify images
    
    * remove log

--- src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/MangaPro.kt
+++ src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/MangaPro.kt
@@ -1,6 +1,9 @@
 package eu.kanade.tachiyomi.extension.ar.mangapro
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.Page
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -11,4 +14,30 @@ class MangaPro : MangaThemesia(
     dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("ar")),
 ) {
     override val versionId = 3
+
+    override fun pageListParse(document: Document): List<Page> {
+        return super.pageListParse(document).onEach {
+            val httpUrl = it.imageUrl!!.toHttpUrl()
+
+            if (wpImgRegex.containsMatchIn(httpUrl.host)) {
+                it.imageUrl = StringBuilder().apply {
+                    val ssl = httpUrl.queryParameter("ssl")
+                    when (ssl) {
+                        null -> append(httpUrl.scheme)
+                        "0" -> append("http")
+                        else -> append("https")
+                    }
+                    append("://")
+                    append(httpUrl.pathSegments.joinToString("/"))
+                    val search = httpUrl.queryParameter("q")
+                    if (search != null) {
+                        append("?q=")
+                        append(search)
+                    }
+                }.toString()
+            }
+        }
+    }
 }
+
+private val wpImgRegex = Regex("""i\d+\.wp\.com""")
