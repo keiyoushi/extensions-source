e1d173d204d912b770339314eb7eca3015cdca5e
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-09 10:31:05 +0500

    EnryuManga: update domain (#4510)

--- src/en/enryumanga/src/eu/kanade/tachiyomi/extension/en/enryumanga/EnryuManga.kt
+++ src/en/enryumanga/src/eu/kanade/tachiyomi/extension/en/enryumanga/EnryuManga.kt
@@ -2,4 +2,4 @@ package eu.kanade.tachiyomi.extension.en.enryumanga
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 
-class EnryuManga : MangaThemesia("EnryuManga", "https://enryumanga.com", "en")
+class EnryuManga : MangaThemesia("EnryuManga", "https://enryumanga.net", "en")
