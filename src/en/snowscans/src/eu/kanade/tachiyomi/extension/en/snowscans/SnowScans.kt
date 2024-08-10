ed98791b314dbc3f3c79000094ebda081909e199
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-08-10 14:54:24 +0200

    Add Snow Scans (#4548)

---
+++ src/en/snowscans/src/eu/kanade/tachiyomi/extension/en/snowscans/SnowScans.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.en.snowscans
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class SnowScans : MangaThemesia(
+    "Snow Scans",
+    "https://snowscans.com",
+    "en",
+    mangaUrlDirectory = "/series",
+)
