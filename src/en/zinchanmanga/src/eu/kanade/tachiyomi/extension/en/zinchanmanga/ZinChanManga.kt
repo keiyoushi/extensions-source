57eaf75ca87431e1915e368b88a2f6c05cc8c25e
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-10 17:39:59 +0500

    ZinChanManga: cleaner titles

--- src/en/zinchanmanga/src/eu/kanade/tachiyomi/extension/en/zinchanmanga/ZinChanManga.kt
+++ src/en/zinchanmanga/src/eu/kanade/tachiyomi/extension/en/zinchanmanga/ZinChanManga.kt
@@ -1,7 +1,52 @@
 package eu.kanade.tachiyomi.extension.en.zinchanmanga
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.model.SManga
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 
 class ZinChanManga : Madara("ZinChanManga", "https://zinchanmanga.net", "en") {
     override val versionId = 2
+    override val useNewChapterEndpoint = true
+
+    override fun popularMangaFromElement(element: Element): SManga {
+        return super.popularMangaFromElement(element).apply {
+            title = url.urlToTitle() ?: title
+        }
+    }
+
+    override fun searchMangaFromElement(element: Element): SManga {
+        return super.searchMangaFromElement(element).apply {
+            title = url.urlToTitle() ?: title
+        }
+    }
+
+    override fun mangaDetailsParse(document: Document): SManga {
+        return super.mangaDetailsParse(document).apply {
+            title = document.location().urlToTitle() ?: title
+        }
+    }
+
+    private fun String.urlToTitle(): String? {
+        val slug = substringAfter("/manga/", "")
+            .substringBefore("/")
+            .takeUnless { it.isBlank() }
+            ?: return null
+
+        val result = StringBuilder(slug.length)
+        var capitalize = true
+        for (char in slug) {
+            result.append(
+                if (char == '-') {
+                    ' '
+                } else if (capitalize) {
+                    char.uppercase()
+                } else {
+                    char.lowercase()
+                },
+            )
+            capitalize = char == '-'
+        }
+        return result.toString()
+    }
 }
