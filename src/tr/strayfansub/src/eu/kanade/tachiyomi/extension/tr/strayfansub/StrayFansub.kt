29fa19ef0d757bc7367c6f5e01c0c495f366bd9d
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-10 17:52:57 +0500

    StrayFansub: update theme (#4545)

--- src/tr/strayfansub/src/eu/kanade/tachiyomi/extension/tr/strayfansub/StrayFansub.kt
+++ src/tr/strayfansub/src/eu/kanade/tachiyomi/extension/tr/strayfansub/StrayFansub.kt
@@ -1,17 +1,34 @@
 package eu.kanade.tachiyomi.extension.tr.strayfansub
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import eu.kanade.tachiyomi.source.model.Page
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class StrayFansub : MangaThemesia(
+class StrayFansub : Madara(
     "Stray Fansub",
     "https://strayfansub.com",
     "tr",
     dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("tr")),
 ) {
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = true
+
+    override fun pageListParse(document: Document): List<Page> {
+        val pageList = super.pageListParse(document)
+
+        if (
+            pageList.isEmpty() &&
+            document.select(".content-blocked, .login-required").isNotEmpty()
+        ) {
+            throw Exception("Okumak için WebView üzerinden giriş yapın")
+        }
+        return pageList
+    }
+
     override val client = super.client.newBuilder()
         .rateLimitHost(baseUrl.toHttpUrl(), 3)
         .build()
