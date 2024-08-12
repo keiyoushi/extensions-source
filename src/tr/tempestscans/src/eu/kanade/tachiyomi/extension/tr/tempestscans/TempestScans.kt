75f36c88a2b4d4d17995755ed1875b103193aa5a
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-08-12 18:21:58 +0200

    Add Tempest Scans

---
+++ src/tr/tempestscans/src/eu/kanade/tachiyomi/extension/tr/tempestscans/TempestScans.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.tr.tempestscans
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class TempestScans : MangaThemesia(
+    "Tempest Scans",
+    "https://tempestscans.net",
+    "tr",
+)
