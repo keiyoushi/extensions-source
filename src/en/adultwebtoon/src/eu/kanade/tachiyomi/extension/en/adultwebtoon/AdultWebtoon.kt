1b3350c9e25fc3c956a1c68fde9f8711853aa4c1
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-13 06:13:01 +0500

    AdultWebtoons: fix chapter list & search (#4580)

--- src/en/adultwebtoon/src/eu/kanade/tachiyomi/extension/en/adultwebtoon/AdultWebtoon.kt
+++ src/en/adultwebtoon/src/eu/kanade/tachiyomi/extension/en/adultwebtoon/AdultWebtoon.kt
@@ -1,7 +1,36 @@
 package eu.kanade.tachiyomi.extension.en.adultwebtoon
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.POST
+import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.FormBody
+import okhttp3.Request
+import org.jsoup.nodes.Element
 
 class AdultWebtoon : Madara("Adult Webtoon", "https://adultwebtoon.com", "en") {
     override val mangaSubString = "adult-webtoon"
+    override val useNewChapterEndpoint = false
+    override val sendViewCount = false
+    override val fetchGenres = false
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+
+    override fun popularMangaNextPageSelector() = "a.next"
+    override fun searchMangaSelector() = "li.movie-item > a"
+    override fun searchMangaNextPageSelector() = "a.next"
+
+    override fun searchMangaFromElement(element: Element): SManga {
+        return SManga.create().apply {
+            setUrlWithoutDomain(element.absUrl("href"))
+            title = element.attr("title")
+        }
+    }
+
+    override fun oldXhrChaptersRequest(mangaId: String): Request {
+        val form = FormBody.Builder()
+            .add("action", "ajax_chap")
+            .add("post_id", mangaId)
+            .build()
+
+        return POST("$baseUrl/wp-admin/admin-ajax.php", xhrHeaders, form)
+    }
 }
