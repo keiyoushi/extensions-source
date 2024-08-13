9fd9dda55a20b02caff472159093aa84a6857e86
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-08-13 03:14:27 +0200

    Add Koreli Scans (#4583)

---
+++ src/tr/koreliscans/src/eu/kanade/tachiyomi/extension/tr/koreliscans/KoreliScans.kt
@@ -0,0 +1,25 @@
+package eu.kanade.tachiyomi.extension.tr.koreliscans
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.model.MangasPage
+import okhttp3.Response
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class KoreliScans : Madara(
+    "Koreli Scans",
+    "https://koreliscans.com",
+    "tr",
+    dateFormat = SimpleDateFormat("d MMMM yyyy", Locale("tr")),
+) {
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = true
+
+    override val popularMangaUrlSelector = "div.chap-title a"
+
+    override fun searchMangaParse(response: Response): MangasPage {
+        val mangasPage = super.searchMangaParse(response)
+        val filteredMangas = mangasPage.mangas.filterNot { it.title.endsWith(" Novel") }
+        return MangasPage(filteredMangas, mangasPage.hasNextPage)
+    }
+}
