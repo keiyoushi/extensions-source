615401cc5fd3da2eb27aee5581489856cb18a08a
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-14 18:25:15 +0500

    dualeotryuen: domain change

--- src/vi/dualeotruyen/src/eu/kanade/tachiyomi/extension/vi/dualeotruyen/DuaLeoTruyen.kt
+++ src/vi/dualeotruyen/src/eu/kanade/tachiyomi/extension/vi/dualeotruyen/DuaLeoTruyen.kt
@@ -22,7 +22,7 @@ class DuaLeoTruyen : ParsedHttpSource() {
 
     override val name = "Dưa Leo Truyện"
 
-    override val baseUrl = "https://dualeotruyenkk.com"
+    override val baseUrl = "dualeotruyenbi.com"
 
     override val lang = "vi"
 
