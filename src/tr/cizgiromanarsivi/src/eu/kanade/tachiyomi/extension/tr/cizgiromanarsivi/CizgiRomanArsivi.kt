fe4ecfc2244b763c76f66b106fd0a190c37ede0e
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-14 16:16:16 +0500

    cizgiromanarsivi: remove dead source (#4612)

--- src/tr/cizgiromanarsivi/src/eu/kanade/tachiyomi/extension/tr/cizgiromanarsivi/CizgiRomanArsivi.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.tr.cizgiromanarsivi
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class CizgiRomanArsivi : Madara("Çizgi Roman Arşivi", "https://cizgiromanarsivi.com", "tr")
