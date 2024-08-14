93c8e7e4c308e671f5870ae43ae9aa0824ea19a5
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-14 18:36:31 +0500

    porncomix: domain change

--- src/en/porncomix/src/eu/kanade/tachiyomi/extension/en/porncomix/PornComix.kt
+++ src/en/porncomix/src/eu/kanade/tachiyomi/extension/en/porncomix/PornComix.kt
@@ -2,4 +2,4 @@ package eu.kanade.tachiyomi.extension.en.porncomix
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class PornComix : Madara("PornComix", "https://www.porncomixonline.net", "en")
+class PornComix : Madara("PornComix", " https://porncomix.online", "en")
