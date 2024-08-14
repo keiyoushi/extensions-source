74eac55207e03a87fb3fbd579da0e14201fccdf4
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-14 18:34:30 +0500

    manhwaland: domain change

--- src/id/manhwalandmom/src/eu/kanade/tachiyomi/extension/id/manhwalandmom/ManhwaLandMom.kt
+++ src/id/manhwalandmom/src/eu/kanade/tachiyomi/extension/id/manhwalandmom/ManhwaLandMom.kt
@@ -8,7 +8,7 @@ import java.util.Locale
 
 class ManhwaLandMom : MangaThemesia(
     "ManhwaLand.mom",
-    "https://www.manhwaland.ink",
+    "https://manhwaland.ink",
     "id",
     dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("id")),
 ) {
