a1d41c1daaa3107074841f238ba98bf0bb856702
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-15 09:16:38 +0500

    remove some websites (#4622)
    * remove Hentai3z
    
    redirects to hentai20
    
    * remove ReadMangaToday
    
    * remove Readmanhua
    
    * remove mangakik
    
    * remove mangapure
    
    * remove mangashiro
    
    * remove moonwithcinlove
    
    * remove readkomik
    
    * remove rio2manga

--- src/en/hentai3z/src/eu/kanade/tachiyomi/extension/en/hentai3z/Hentai3z.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.hentai3z
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class Hentai3z : Madara("Hentai3z", "https://hentai3z.xyz", "en") {
-    override val useNewChapterEndpoint = false
-}
--- src/en/mangakik/src/eu/kanade/tachiyomi/extension/en/mangakik/Mangakik.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangakik
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class Mangakik : Madara("Mangakik", "https://mangakik.org", "en")
