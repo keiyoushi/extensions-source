861d05549249a0a4f1d27fb4eda3ad8b504452df
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-08-15 09:16:38 +0500

    remove some websites (#4622)
    * remove Hentai3z
    
    redirects to hentai20
    
    * remove ReadMangaToday
    
    * remove Readmanhua
    
    * remove mangakik
    
    * remove mangapure
    
    * remove mangashiro
    
    * remove moonwithcinlove
    
    * remove readkomik
    
    * remove rio2manga

--- src/en/rio2manga/src/eu/kanade/tachiyomi/extension/en/rio2manga/Rio2Manga.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.rio2manga
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class Rio2Manga : Madara("Rio2 Manga", "https://rio2manga.net", "en") {
-    override val useNewChapterEndpoint = true
-}
