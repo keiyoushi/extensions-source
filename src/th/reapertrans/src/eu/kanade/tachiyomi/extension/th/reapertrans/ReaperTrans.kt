fa7a6366193576168cbee242437ee0c1501589d4
iloverabbit <immortaliodine@gmail.com>
2024-08-17 21:53:22 +0700

    Add ReaperTrans (#4661)
    * Add ReaperTrans
    
    * Use naked domain instead of www prefix
    
    * Fix wrong date format from Thai to English
    
    * Remove redundant default argument from constructor

---
+++ src/th/reapertrans/src/eu/kanade/tachiyomi/extension/th/reapertrans/ReaperTrans.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.th.reapertrans
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class ReaperTrans : MangaThemesia(
+    "ReaperTrans",
+    "https://reapertrans.com",
+    "th",
+)
