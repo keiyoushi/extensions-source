457c7ee20bf62cca4f9ecf1a10e28b0f9bd16719
bapeey <90949336+bapeey@users.noreply.github.com>
2024-08-17 00:50:14 -0500

    Re-add InariPikav (#4656)
    Readd

---
+++ src/es/inarimanga/src/eu/kanade/tachiyomi/extension/es/inarimanga/InariPikav.kt
@@ -0,0 +1,20 @@
+package eu.kanade.tachiyomi.extension.es.inarimanga
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class InariPikav : MangaThemesia(
+    "Inari Pikav",
+    "https://inaripikav.org",
+    "es",
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
+) {
+    override val id = 7949577653918285764
+
+    override val client = super.client.newBuilder()
+        .rateLimitHost(baseUrl.toHttpUrl(), 3, 1)
+        .build()
+}
