c25498ec72a5d4202155995fe8ed2236eecae769
Chopper <156493704+choppeh@users.noreply.github.com>
2024-08-17 16:43:34 -0300

    Fix url chapter

--- src/ar/yurimoonsub/src/eu/kanade/tachiyomi/extension/ar/yurimoonsub/YuriMoonSub.kt
+++ src/ar/yurimoonsub/src/eu/kanade/tachiyomi/extension/ar/yurimoonsub/YuriMoonSub.kt
@@ -2,9 +2,30 @@ package eu.kanade.tachiyomi.extension.ar.yurimoonsub
 
 import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import org.jsoup.nodes.Document
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
-class YuriMoonSub : ZeistManga("Yuri Moon Sub", "https://yurimoonsub.blogspot.com", "ar") {
+class YuriMoonSub : ZeistManga(
+    "Yuri Moon Sub",
+    "https://yurimoonsub.blogspot.com",
+    "ar",
+) {
     override val client = super.client.newBuilder()
         .rateLimit(2)
         .build()
+
+    override fun getChapterFeedUrl(doc: Document): String {
+        return URLDecoder.decode(super.getChapterFeedUrl(doc), StandardCharsets.UTF_8.toString())
+            .removeArabicChars()
+    }
+
+    private fun String.removeArabicChars() =
+        this.replace(ARABIC_CHARS_REGEX, "")
+            .replace(EXTRA_SPACES_REGEX, "")
+
+    companion object {
+        val ARABIC_CHARS_REGEX = "[\\u0600-\\u06FF]".toRegex()
+        val EXTRA_SPACES_REGEX = "\\s{2,}".toRegex()
+    }
 }
