fd94b00e4c66cc15427f39817849386efa5fa3f7
iloverabbit <immortaliodine@gmail.com>
2024-08-17 19:39:54 +0700

    Remove redundant default argument from constructor

--- src/th/reapertrans/src/eu/kanade/tachiyomi/extension/th/reapertrans/ReaperTrans.kt
+++ src/th/reapertrans/src/eu/kanade/tachiyomi/extension/th/reapertrans/ReaperTrans.kt
@@ -1,12 +1,9 @@
 package eu.kanade.tachiyomi.extension.th.reapertrans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class ReaperTrans : MangaThemesia(
     "ReaperTrans",
     "https://reapertrans.com",
     "th",
-    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("en")),
 )
