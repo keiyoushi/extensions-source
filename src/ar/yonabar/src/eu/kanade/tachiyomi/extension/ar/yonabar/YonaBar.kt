edecd0317c5c0e625dd84922b8c3c9d48ae82bdd
Chopper <156493704+choppeh@users.noreply.github.com>
2024-08-18 06:26:07 -0300

    YonaBar: Update domain (#4668)
    Update domain

--- src/ar/yonabar/src/eu/kanade/tachiyomi/extension/ar/yonabar/YonaBar.kt
+++ src/ar/yonabar/src/eu/kanade/tachiyomi/extension/ar/yonabar/YonaBar.kt
@@ -1,7 +1,19 @@
 package eu.kanade.tachiyomi.extension.ar.yonabar
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class YonaBar : Madara("YonaBar", "https://yonabar.com", "ar", SimpleDateFormat("yyyy-MM-dd", Locale("ar")))
+class YonaBar : Madara(
+    "YonaBar",
+    "https://yonabar.xyz",
+    "ar",
+    SimpleDateFormat("MMM dd, yyyy", Locale("ar")),
+) {
+    override val client = super.client.newBuilder()
+        .rateLimit(3)
+        .build()
+
+    override val mangaSubString = "yaoi"
+}
