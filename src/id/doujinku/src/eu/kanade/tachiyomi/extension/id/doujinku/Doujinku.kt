c63bb966e7e5742675693d378caf77f339be5fb6
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-08-18 18:05:37 +0200

    Doujinku: update domain, add override URL setting

--- src/id/doujinku/src/eu/kanade/tachiyomi/extension/id/doujinku/Doujinku.kt
+++ src/id/doujinku/src/eu/kanade/tachiyomi/extension/id/doujinku/Doujinku.kt
@@ -1,12 +1,65 @@
 package eu.kanade.tachiyomi.extension.id.doujinku
 
+import android.app.Application
+import android.content.SharedPreferences
+import android.widget.Toast
+import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.ConfigurableSource
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class Doujinku : MangaThemesia(
-    "Doujinku",
-    "https://doujinku.org",
-    "id",
-    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("id")),
-)
+class Doujinku :
+    MangaThemesia(
+        "Doujinku",
+        "https://doujinku.xyz",
+        "id",
+        dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("id")),
+    ),
+    ConfigurableSource {
+    private val preferences: SharedPreferences =
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+
+    init {
+        preferences.getString(DEFAULT_BASE_URL_PREF, null).let { prefDefaultBaseUrl ->
+            if (prefDefaultBaseUrl != super.baseUrl) {
+                preferences.edit()
+                    .putString(BASE_URL_PREF, super.baseUrl)
+                    .putString(DEFAULT_BASE_URL_PREF, super.baseUrl)
+                    .apply()
+            }
+        }
+    }
+
+    override val baseUrl by lazy { getPrefBaseUrl() }
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        val baseUrlPref = androidx.preference.EditTextPreference(screen.context).apply {
+            key = BASE_URL_PREF
+            title = BASE_URL_PREF_TITLE
+            summary = BASE_URL_PREF_SUMMARY
+            setDefaultValue(super.baseUrl)
+            dialogTitle = BASE_URL_PREF_TITLE
+            dialogMessage = "Default: ${super.baseUrl}"
+
+            setOnPreferenceChangeListener { _, _ ->
+                Toast.makeText(screen.context, RESTART_APP, Toast.LENGTH_LONG).show()
+                true
+            }
+        }
+        screen.addPreference(baseUrlPref)
+    }
+
+    private fun getPrefBaseUrl(): String = preferences.getString(BASE_URL_PREF, super.baseUrl)!!
+
+    companion object {
+        private const val DEFAULT_BASE_URL_PREF = "defaultBaseUrl"
+        private const val RESTART_APP = "Restart app to apply new setting."
+        private const val BASE_URL_PREF_TITLE = "Override BaseUrl"
+        private const val BASE_URL_PREF = "overrideBaseUrl"
+        private const val BASE_URL_PREF_SUMMARY =
+            "For temporary uses. Updating the extension will erase this setting."
+    }
+}
