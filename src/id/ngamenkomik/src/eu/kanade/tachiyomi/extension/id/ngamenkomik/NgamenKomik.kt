660de770cd291d962d2e4e61584bceef631afb98
TheKingTermux <50316075+TheKingTermux@users.noreply.github.com>
2024-08-20 13:51:45 +0700

    Add some filter

--- src/id/ngamenkomik/src/eu/kanade/tachiyomi/extension/id/ngamenkomik/NgamenKomik.kt
+++ src/id/ngamenkomik/src/eu/kanade/tachiyomi/extension/id/ngamenkomik/NgamenKomik.kt
@@ -1,5 +1,8 @@
 package eu.kanade.tachiyomi.extension.id.ngamenkomik
 
+import eu.kanade.tachiyomi.multisrc.zeistmanga.Genre
+import eu.kanade.tachiyomi.multisrc.zeistmanga.Status
+import eu.kanade.tachiyomi.multisrc.zeistmanga.Type
 import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
@@ -7,4 +10,40 @@ class NgamenKomik : ZeistManga("NgamenKomik", "https://ngamenkomik05.blogspot.co
     override val client = super.client.newBuilder()
         .rateLimit(3)
         .build()
+
+    override val hasFilters = true
+
+    override val hasLanguageFilter = false
+
+    override fun getTypeList() = listOf(
+        Type("Semua", ""),
+        Type("Manhua", "Manhua"),
+        Type("Manhwa", "Manhwa"),
+    )
+
+    override fun getStatusList() = listOf(
+        Status("Semua", ""),
+        Status("Ongoing", "Ongoing"),
+        Status("Completed", "Completed"),
+    )
+
+    override fun getGenreList() = listOf(
+        Genre("Action", "Action"),
+        Genre("Adventure", "Adventure"),
+        Genre("Comedy", "Comedy"),
+        Genre("Drama", "Drama"),
+        Genre("Ecchi", "Ecchi"),
+        Genre("Fantasy", "Fantasy"),
+        Genre("Harem", "Harem"),
+        Genre("Horror", "Horror"),
+        Genre("Isekai", "Isekai"),
+        Genre("Magic", "Magic"),
+        Genre("Martial Arts", "Martial Arts"),
+        Genre("Mystery", "Mystery"),
+        Genre("Romance", "Romance"),
+        Genre("Shounen", "Shounen"),
+        Genre("Slice of Life", "Slice of Life"),
+        Genre("Supernatural", "Supernatural"),
+        Genre("Thriller", "Thriller"),
+    )
 }
