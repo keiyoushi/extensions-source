49a7ce9e7d32d8a7ab2ed3ff77aba54f622fc369
KirinRaikage <manmitsu@hotmail.fr>
2024-08-25 04:17:00 +0200

    Sushiscan.fr: Fix search (#4740)

--- src/fr/sushiscanfr/src/eu/kanade/tachiyomi/extension/fr/sushiscanfr/SushiScanFR.kt
+++ src/fr/sushiscanfr/src/eu/kanade/tachiyomi/extension/fr/sushiscanfr/SushiScanFR.kt
@@ -6,7 +6,7 @@ import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class SushiScanFR : MangaThemesia("Sushiscan.fr", "https://sushiscan.fr", "fr", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH)) {
+class SushiScanFR : MangaThemesia("Sushiscan.fr", "https://sushiscan.fr", "fr", mangaUrlDirectory = "/catalogue", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH)) {
     override val altNamePrefix = "Nom alternatif : "
     override val seriesAuthorSelector = ".imptdt:contains(Auteur) i, .fmed b:contains(Auteur)+span"
     override val seriesStatusSelector = ".imptdt:contains(Statut) i"
