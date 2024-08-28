9a7e771aac17c677b4484a28781ffdfd2d154585
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-08-28 23:00:18 +0200

    NIFTeam: upgrade to https

--- src/it/nifteam/src/eu/kanade/tachiyomi/extension/it/nifteam/NIFTeam.kt
+++ src/it/nifteam/src/eu/kanade/tachiyomi/extension/it/nifteam/NIFTeam.kt
@@ -2,4 +2,4 @@ package eu.kanade.tachiyomi.extension.it.nifteam
 
 import eu.kanade.tachiyomi.multisrc.foolslide.FoolSlide
 
-class NIFTeam : FoolSlide("NIFTeam", "http://read-nifteam.info", "it", "/slide")
+class NIFTeam : FoolSlide("NIFTeam", "https://read-nifteam.info", "it", "/slide")
