16557f66c07d755bf8eed94321c3a13824ba2334
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-08-28 22:47:38 +0200

    KomikIndo.info: upgrade to https

--- src/id/komikindoinfo/src/eu/kanade/tachiyomi/extension/id/komikindoinfo/KomikIndoInfo.kt
+++ src/id/komikindoinfo/src/eu/kanade/tachiyomi/extension/id/komikindoinfo/KomikIndoInfo.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.zmanga.ZManga
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class KomikIndoInfo : ZManga("KomikIndo.info", "http://komikindo.info", "id", dateFormat = SimpleDateFormat("MMM d, yyyy", Locale("id"))) {
+class KomikIndoInfo : ZManga("KomikIndo.info", "https://komikindo.info", "id", dateFormat = SimpleDateFormat("MMM d, yyyy", Locale("id"))) {
 
     override val hasProjectPage = true
 }
