59cd659154eb09f260be3cab18ed2b8b478ac196
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-08-29 10:17:44 +0200

    Upgrade some extensions from HTTP to HTTPS (#4816)
    * Café com Yaoi: upgrade to https
    
    * KomikIndo.info: upgrade to https
    
    * LemonFont: upgrade to https
    
    * NIFTeam: upgrade to https
    
    * TuttoAnimeManga: upgrade to https
    
    * Sodsaime: upgrade to https

--- src/it/tuttoanimemanga/src/eu/kanade/tachiyomi/extension/it/tuttoanimemanga/TuttoAnimeManga.kt
+++ src/it/tuttoanimemanga/src/eu/kanade/tachiyomi/extension/it/tuttoanimemanga/TuttoAnimeManga.kt
@@ -3,7 +3,7 @@ package eu.kanade.tachiyomi.extension.it.tuttoanimemanga
 import eu.kanade.tachiyomi.multisrc.pizzareader.PizzaReader
 import kotlinx.serialization.json.Json
 
-class TuttoAnimeManga : PizzaReader("TuttoAnimeManga", "http://tuttoanimemanga.net", "it") {
+class TuttoAnimeManga : PizzaReader("TuttoAnimeManga", "https://tuttoanimemanga.net", "it") {
     override val json = Json {
         ignoreUnknownKeys = true
         coerceInputValues = true
--- src/th/sodsaime/src/eu/kanade/tachiyomi/extension/th/sodsaime/Sodsaime.kt
+++ src/th/sodsaime/src/eu/kanade/tachiyomi/extension/th/sodsaime/Sodsaime.kt
@@ -4,4 +4,4 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class Sodsaime : MangaThemesia("สดใสเมะ", "http://www.xn--l3c0azab5a2gta.com", "th", dateFormat = SimpleDateFormat("MMMMM dd, yyyy", Locale("th")))
+class Sodsaime : MangaThemesia("สดใสเมะ", "https://www.xn--l3c0azab5a2gta.com", "th", dateFormat = SimpleDateFormat("MMMMM dd, yyyy", Locale("th")))
