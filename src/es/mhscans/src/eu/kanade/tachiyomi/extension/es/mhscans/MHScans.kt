a87a905d3559df24154ef47800934a2f6f4c54fb
bapeey <90949336+bapeey@users.noreply.github.com>
2024-08-30 06:13:49 -0500

    MHScans: Update domain (#4832)
    update domain

--- src/es/mhscans/src/eu/kanade/tachiyomi/extension/es/mhscans/MHScans.kt
+++ src/es/mhscans/src/eu/kanade/tachiyomi/extension/es/mhscans/MHScans.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 class MHScans : Madara(
     "MHScans",
-    "https://mh.twobluescans.com",
+    "https://mhscans.manhuaonline.org",
     "es",
     dateFormat = SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("es")),
 ) {
