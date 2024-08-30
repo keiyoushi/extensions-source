23ddeb3e95c7816e8d10cb17f1dc0f9d7091f4a5
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-08-30 04:19:00 -0700

    Lua Scans (Unoriginal): Update domain (#4848)
    * Bump versionCode
    
    * Update baseURL

--- src/en/kaiscans/src/eu/kanade/tachiyomi/extension/en/kaiscans/LuaScans.kt
+++ src/en/kaiscans/src/eu/kanade/tachiyomi/extension/en/kaiscans/LuaScans.kt
@@ -3,7 +3,7 @@ package eu.kanade.tachiyomi.extension.en.kaiscans
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
-class LuaScans : MangaThemesia("Lua Scans (unoriginal)", "https://luacomic.com", "en") {
+class LuaScans : MangaThemesia("Lua Scans (unoriginal)", "https://ponvi.online", "en") {
     override val client = super.client.newBuilder()
         .rateLimit(2)
         .build()
