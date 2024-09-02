f7bbb0b92d659ea18ea220ab64e4095963af821e
dngonz <dapalacios95@gmail.com>
2024-09-02 14:09:28 +0200

    Comics Valley: fix latest and popular (#4863)
    * Comics Valley: change mangaSubString
    
    * Comics Valley: bump
    
    * ComicsValley: improve request performance

--- src/hi/comicsvalley/src/eu/kanade/tachiyomi/extension/hi/comicsvalley/ComicsValley.kt
+++ src/hi/comicsvalley/src/eu/kanade/tachiyomi/extension/hi/comicsvalley/ComicsValley.kt
@@ -2,4 +2,12 @@ package eu.kanade.tachiyomi.extension.hi.comicsvalley
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class ComicsValley : Madara("Comics Valley", "https://comicsvalley.com", "hi")
+class ComicsValley : Madara(
+    "Comics Valley",
+    "https://comicsvalley.com",
+    "hi",
+) {
+    override val mangaSubString = "comics-new"
+    override val useNewChapterEndpoint = true
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+}
