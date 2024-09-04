e2dba705b1f440f1dafdff11c95d521e3a7dd44e
dngonz <dapalacios95@gmail.com>
2024-09-04 22:23:10 +0200

    MangaEffect: remove source

--- src/en/mangaeffect/src/eu/kanade/tachiyomi/extension/en/mangaeffect/MangaEffect.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangaeffect
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class MangaEffect : Madara("MangaEffect", "https://mangaeffect.com", "en", SimpleDateFormat("dd.MM.yyyy", Locale.US))
