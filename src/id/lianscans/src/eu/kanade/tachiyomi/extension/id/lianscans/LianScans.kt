8c0d99aba08fa9832c8053e619167f5b4a988551
TheKingTermux <50316075+TheKingTermux@users.noreply.github.com>
2024-09-04 21:21:01 +0700

    LianScans : Update BaseURL (#4914)
    * LianScans : Update BaseURL
    
    * Increase VersionCode

--- src/id/lianscans/src/eu/kanade/tachiyomi/extension/id/lianscans/LianScans.kt
+++ src/id/lianscans/src/eu/kanade/tachiyomi/extension/id/lianscans/LianScans.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 
-class LianScans : MangaThemesia("LianScans", "https://www.lianscans.my.id", "id") {
+class LianScans : MangaThemesia("LianScans", "https://www.lianscans.com", "id") {
 
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(4)
