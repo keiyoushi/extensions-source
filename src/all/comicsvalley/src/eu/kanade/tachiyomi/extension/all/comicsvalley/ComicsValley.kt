fa258a905b871dbcea2e487f31b78d8718b53456
Cuong-Tran <cuongtran.tm@gmail.com>
2024-09-04 21:17:37 +0700

    Move ComicsValley to multi-language (#4904)

--- src/all/comicsvalley/src/eu/kanade/tachiyomi/extension/all/comicsvalley/ComicsValley.kt
+++ src/all/comicsvalley/src/eu/kanade/tachiyomi/extension/all/comicsvalley/ComicsValley.kt
@@ -5,9 +5,10 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 class ComicsValley : Madara(
     "Comics Valley",
     "https://comicsvalley.com",
-    "hi",
+    "all",
 ) {
     override val mangaSubString = "comics-new"
     override val useNewChapterEndpoint = true
     override val useLoadMoreRequest = LoadMoreStrategy.Always
+    override val id = 1103204227230640533
 }
