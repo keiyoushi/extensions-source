98e179daf89feba3b72fe2668606cf2bb105b45b
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-09-07 01:26:37 -0700

    Cat300: Update baseUrl

--- src/th/cat300/src/eu/kanade/tachiyomi/extension/th/cat300/Cat300.kt
+++ src/th/cat300/src/eu/kanade/tachiyomi/extension/th/cat300/Cat300.kt
@@ -2,4 +2,4 @@ package eu.kanade.tachiyomi.extension.th.cat300
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class Cat300 : Madara("Cat300", "https://cat300.com", "th")
+class Cat300 : Madara("Cat300", "https://cat300.net", "th")
