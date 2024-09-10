8cde1890548e92de7042aaf5b89df870d7d9b3d1
unknown <marcibr20@gmail.com>
2024-09-10 09:53:08 +0200

    mitaku 1.0

---
+++ src/all/mitaku/src/eu/kanade/tachiyomi/extension/all/mitaku/Mitaku.kt
@@ -0,0 +1,156 @@
+package eu.kanade.tachiyomi.extension.it.mitaku
+
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.model.UpdateStrategy
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import okhttp3.Request
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import rx.Observable
+
+class Mitaku : ParsedHttpSource() {
+    override val name = "Mitaku"
+
+    override val baseUrl = "https://mitaku.net"
+
+    override val lang = "all"
+
+    override val supportsLatest = false
+
+    // ============================== Popular ===============================
+    override fun popularMangaRequest(page: Int) = GET("$baseUrl/category/ero-cosplay/page/$page", headers)
+
+    override fun popularMangaSelector() = "div.article-container article"
+
+    override fun popularMangaFromElement(element: Element) = SManga.create().apply {
+        // Extract and set the URL without domain
+
+        setUrlWithoutDomain(element.selectFirst("a")!!.absUrl("href"))
+
+        // Extract and set the title
+        title = element.selectFirst("a")!!.attr("title")
+
+        // Extract and set the thumbnail URL
+        thumbnail_url = element.selectFirst("img")?.absUrl("src")
+    }
+
+    override fun popularMangaNextPageSelector() = "div.wp-pagenavi a.page.larger"
+
+    // =============================== Latest ===============================
+    override fun latestUpdatesRequest(page: Int): Request {
+        throw UnsupportedOperationException()
+    }
+
+    override fun latestUpdatesSelector(): String {
+        throw UnsupportedOperationException()
+    }
+
+    override fun latestUpdatesFromElement(element: Element): SManga {
+        throw UnsupportedOperationException()
+    }
+
+    override fun latestUpdatesNextPageSelector(): String? {
+        throw UnsupportedOperationException()
+    }
+
+    // =============================== Search ===============================
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val filterList = if (filters.isEmpty()) getFilterList() else filters
+        val tagFilter = filterList.findInstance<TagFilter>()!!
+        val tagValue = tagFilter.state.replace(" ", "-")
+        val categoryFilter = filterList.findInstance<CategoryFilter>()!!
+        return when {
+            query.isEmpty() && categoryFilter.state != 0 -> GET("$baseUrl/category/${categoryFilter.toUriPart()}/page/$page/")
+            query.isEmpty() && tagFilter.state.isNotEmpty() -> GET("$baseUrl/tag/${tagFilter.state}/page/$page/")
+            query.isNotEmpty() -> GET("$baseUrl/page/$page/?s=$query")
+            else -> latestUpdatesRequest(page)
+        }
+    }
+
+    override fun searchMangaSelector() = popularMangaSelector()
+
+    override fun searchMangaFromElement(element: Element) = popularMangaFromElement(element)
+
+    override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
+
+    override fun mangaDetailsParse(document: Document) = SManga.create().apply {
+        status = SManga.COMPLETED
+        update_strategy = UpdateStrategy.ONLY_FETCH_ONCE
+        with(document.selectFirst("article")!!) {
+            title = selectFirst("h1")!!.text()
+            val catGenres = select("span.cat-links a").joinToString { it.text() }
+            val tagGenres = select("span.tag-links a").joinToString { it.text() }
+            genre = listOf(catGenres, tagGenres).filter { it.isNotEmpty() }.joinToString(", ")
+        }
+    }
+
+    // ============================== Chapters ==============================
+    override fun fetchChapterList(manga: SManga): Observable<List<SChapter>> {
+        val chapter = SChapter.create().apply {
+            url = manga.url
+            chapter_number = 1F
+            name = "Chapter"
+        }
+
+        return Observable.just(listOf(chapter))
+    }
+
+    override fun chapterListSelector(): String {
+        throw UnsupportedOperationException()
+    }
+
+    override fun chapterFromElement(element: Element): SChapter {
+        throw UnsupportedOperationException()
+    }
+
+    // =============================== Pages ================================
+
+    override fun pageListParse(document: Document): List<Page> {
+        // Select all a elements with the class msacwl-img-link
+        val imageElements = document.select("a.msacwl-img-link")
+
+        // Extract the data-mfp-src attribute from each element and create a list of Page objects
+        return imageElements.mapIndexed { index, element ->
+            val imageUrl = element.attr("data-mfp-src")
+            Page(index, imageUrl = imageUrl)
+        }
+    }
+
+    override fun imageUrlParse(document: Document): String {
+        throw UnsupportedOperationException()
+    }
+
+    open class UriPartFilter(
+        displayName: String,
+        private val valuePair: Array<Pair<String, String>>,
+    ) : Filter.Select<String>(displayName, valuePair.map { it.first }.toTypedArray()) {
+        fun toUriPart() = valuePair[state].second
+    }
+
+    class CategoryFilter : UriPartFilter(
+        "Category",
+        arrayOf(
+            Pair("Any", ""),
+            Pair("Ero Cosplay", "/ero-cosplay"),
+            Pair("Nude", "/nude"),
+            Pair("Sexy Set", "/sexy-set"),
+            Pair("Online Video", "/online-video"),
+        ),
+    )
+    override fun getFilterList(): FilterList = FilterList(
+        Filter.Header("NOTE: Only one filter will be applied! Only one tag search"),
+        Filter.Separator(),
+        CategoryFilter(),
+        TagFilter(),
+    )
+
+    class TagFilter : Filter.Text("Tag")
+
+    private inline fun <reified T> Iterable<*>.findInstance() = find { it is T } as? T
+}
