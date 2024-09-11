1c955db2bf8a11111998486a0287e6e04a2374e4
bapeey <90949336+bapeey@users.noreply.github.com>
2024-09-11 00:20:37 -0500

    MHScans: Update domain (#5003)
    dd

--- src/es/mhscans/src/eu/kanade/tachiyomi/extension/es/mhscans/MHScans.kt
+++ src/es/mhscans/src/eu/kanade/tachiyomi/extension/es/mhscans/MHScans.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 class MHScans : Madara(
     "MHScans",
-    "https://mhscans.manhuaonline.org",
+    "https://mh.cookni.net",
     "es",
     dateFormat = SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("es")),
 ) {
