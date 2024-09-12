6e79ac8a9abbbff223b82112365f9d65b32fe5cb
Hasan <hturkyilmaz19@posta.pau.edu.tr>
2024-09-12 14:24:55 +0300

    Fix for chapter dates on source Hayalistic (#4890)
    * Add source: Nox Scans
    
    * Add source: Manga Tilkisi
    
    * Update src/tr/noxscans/build.gradle
    
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>
    
    * Update src/tr/mangatilkisi/build.gradle
    
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>
    
    * Update MangaTilkisi.kt
    
    * Remove MajorScans
    
    * Remove MajorScans
    
    * Fix for chapter dates on source Hayalistic
    
    * Update src/tr/hayalistic/src/eu/kanade/tachiyomi/extension/tr/hayalistic/Hayalistic.kt
    
    ---------
    
    Co-authored-by: hasanturkylmz <hasanturkylmz@outlook.com>
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/tr/hayalistic/src/eu/kanade/tachiyomi/extension/tr/hayalistic/Hayalistic.kt
+++ src/tr/hayalistic/src/eu/kanade/tachiyomi/extension/tr/hayalistic/Hayalistic.kt
@@ -1,5 +1,12 @@
 package eu.kanade.tachiyomi.extension.tr.hayalistic
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class Hayalistic : Madara("Hayalistic", "https://hayalistic.com.tr", "tr")
+class Hayalistic : Madara(
+    "Hayalistic",
+    "https://hayalistic.com.tr",
+    "tr",
+    dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH),
+)
