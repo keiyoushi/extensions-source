0686e089fa21b88da96de3770227a51c4527ba6f
KirinRaikage <manmitsu@hotmail.fr>
2024-09-18 15:08:34 +0200

    Cleanup dead sources (#5094)
    * Cleanup dead sources
    
    * Add Myreadingmanga FR and Myrockmanga FR lang
    
    * Bump myreadingmanga version

--- src/fr/thebeginningaftertheend/src/eu/kanade/tachiyomi/extension/fr/thebeginningaftertheend/TheBeginningAfterTheEnd.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.thebeginningaftertheend
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class TheBeginningAfterTheEnd : Madara("The Beginning After The End", "https://www.thebeginningaftertheend.fr", "fr", SimpleDateFormat("MMMMM dd, yyyy", Locale("fr")))
