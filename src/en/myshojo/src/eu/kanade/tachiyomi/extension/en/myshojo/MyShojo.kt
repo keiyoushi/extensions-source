cae2ee050626eb56918c66c71b8074011d1ee93e
Dani <17619547+shabnix@users.noreply.github.com>
2024-09-18 15:10:01 +0200

    Add MyShojo (#5100)

---
+++ src/en/myshojo/src/eu/kanade/tachiyomi/extension/en/myshojo/MyShojo.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.myshojo
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class MyShojo : MangaThemesia("MyShojo", "https://myshojo.com", "en")
