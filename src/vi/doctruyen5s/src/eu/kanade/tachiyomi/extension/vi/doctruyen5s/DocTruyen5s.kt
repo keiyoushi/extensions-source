fa6719001d678ea56836d0fb4cfeecd4b130c1c1
Chopper <156493704+choppeh@users.noreply.github.com>
2024-09-18 22:44:10 -0300

    Update domain

--- src/vi/doctruyen5s/src/eu/kanade/tachiyomi/extension/vi/doctruyen5s/DocTruyen5s.kt
+++ src/vi/doctruyen5s/src/eu/kanade/tachiyomi/extension/vi/doctruyen5s/DocTruyen5s.kt
@@ -2,4 +2,4 @@ package eu.kanade.tachiyomi.extension.vi.doctruyen5s
 
 import eu.kanade.tachiyomi.multisrc.liliana.Liliana
 
-class DocTruyen5s : Liliana("DocTruyen5s", "https://manga.io.vn", "vi")
+class DocTruyen5s : Liliana("DocTruyen5s", "https://dongmoe.com", "vi")
