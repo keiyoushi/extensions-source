cbd4e915ba7b73c724a8ddb033aa71fe3107466d
Chopper <156493704+choppeh@users.noreply.github.com>
2024-09-19 22:02:36 -0300

    Update domain

--- src/pt/sinensis/src/eu/kanade/tachiyomi/extension/pt/sinensis/SinensisScan.kt
+++ src/pt/sinensis/src/eu/kanade/tachiyomi/extension/pt/sinensis/SinensisScan.kt
@@ -1,12 +1,17 @@
 package eu.kanade.tachiyomi.extension.pt.sinensis
 
 import eu.kanade.tachiyomi.multisrc.peachscan.PeachScan
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class SinensisScan : PeachScan(
     "Sinensis Scan",
-    "https://sinensistoon.com",
+    "https://sinensis.leitorweb.com",
     "pt-BR",
     SimpleDateFormat("dd 'de' MMMM 'de' yyyy 'às' HH:mm", Locale("pt", "BR")),
-)
+) {
+    override val client = super.client.newBuilder()
+        .rateLimit(3)
+        .build()
+}
