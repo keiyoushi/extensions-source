2f462ad37b8c44817df3d74f7f6d16ab5f42b96f
Chopper <156493704+choppeh@users.noreply.github.com>
2024-09-20 04:28:09 -0300

    SinensisScan: Update domain (#5131)
    * Update domain
    
    * Remove date format

--- src/pt/sinensis/src/eu/kanade/tachiyomi/extension/pt/sinensis/SinensisScan.kt
+++ src/pt/sinensis/src/eu/kanade/tachiyomi/extension/pt/sinensis/SinensisScan.kt
@@ -1,12 +1,14 @@
 package eu.kanade.tachiyomi.extension.pt.sinensis
 
 import eu.kanade.tachiyomi.multisrc.peachscan.PeachScan
-import java.text.SimpleDateFormat
-import java.util.Locale
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
 class SinensisScan : PeachScan(
     "Sinensis Scan",
-    "https://sinensistoon.com",
+    "https://sinensis.leitorweb.com",
     "pt-BR",
-    SimpleDateFormat("dd 'de' MMMM 'de' yyyy 'às' HH:mm", Locale("pt", "BR")),
-)
+) {
+    override val client = super.client.newBuilder()
+        .rateLimit(3)
+        .build()
+}
