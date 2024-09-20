2595ffe3daa28ec7a14ec534e1f9ff81b0c1d053
Chopper <156493704+choppeh@users.noreply.github.com>
2024-09-20 04:22:26 -0300

    Remove MortalsGroove (#5122)

--- src/en/mortalsgroove/src/eu/kanade/tachiyomi/extension/en/mortalsgroove/MortalsGroove.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mortalsgroove
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class MortalsGroove : Madara("Mortals Groove", "https://mortalsgroove.com", "en")
