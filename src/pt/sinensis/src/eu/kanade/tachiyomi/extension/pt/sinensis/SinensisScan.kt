6325a881a649379c54e1ce09de0ef9a9d52d44e4
Chopper <156493704+choppeh@users.noreply.github.com>
2024-09-19 22:10:19 -0300

    Remove date format

--- src/pt/sinensis/src/eu/kanade/tachiyomi/extension/pt/sinensis/SinensisScan.kt
+++ src/pt/sinensis/src/eu/kanade/tachiyomi/extension/pt/sinensis/SinensisScan.kt
@@ -2,14 +2,11 @@ package eu.kanade.tachiyomi.extension.pt.sinensis
 
 import eu.kanade.tachiyomi.multisrc.peachscan.PeachScan
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class SinensisScan : PeachScan(
     "Sinensis Scan",
     "https://sinensis.leitorweb.com",
     "pt-BR",
-    SimpleDateFormat("dd 'de' MMMM 'de' yyyy 'às' HH:mm", Locale("pt", "BR")),
 ) {
     override val client = super.client.newBuilder()
         .rateLimit(3)
