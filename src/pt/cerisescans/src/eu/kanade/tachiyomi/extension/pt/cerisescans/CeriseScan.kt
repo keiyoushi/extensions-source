af6932bb0ac79f426cca40947283224ed0ed4db2
Chopper <156493704+choppeh@users.noreply.github.com>
2024-09-20 04:28:37 -0300

    CeriseScan: Update domain (#5132)
    Update domain

--- src/pt/cerisescans/src/eu/kanade/tachiyomi/extension/pt/cerisescans/CeriseScan.kt
+++ src/pt/cerisescans/src/eu/kanade/tachiyomi/extension/pt/cerisescans/CeriseScan.kt
@@ -3,8 +3,13 @@ package eu.kanade.tachiyomi.extension.pt.cerisescans
 import eu.kanade.tachiyomi.multisrc.peachscan.PeachScan
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
-class CeriseScan : PeachScan("Cerise Scan", "https://cerisetoon.com", "pt-BR") {
+class CeriseScan : PeachScan(
+    "Cerise Scan",
+    "https://cerise.leitorweb.com",
+    "pt-BR",
+) {
     override val versionId: Int = 2
+
     override val client = super.client.newBuilder()
         .rateLimit(1, 2)
         .build()
