7276932b7e1a1e260a3fde4c3f2f4080e18d3f76
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-09-25 15:53:49 +0200

    HentaiVN.plus: update domain (#5204)
    Skips some redirect.

--- src/vi/hentaivnplus/src/eu/kanade/tachiyomi/extension/vi/hentaivnplus/HentaiVNPlus.kt
+++ src/vi/hentaivnplus/src/eu/kanade/tachiyomi/extension/vi/hentaivnplus/HentaiVNPlus.kt
@@ -14,7 +14,7 @@ import java.util.Locale
 class HentaiVNPlus :
     Madara(
         "HentaiVN.plus",
-        "https://hentaivn.fit",
+        "https://hentaivn.vin",
         "vi",
         dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.ROOT),
     ),
