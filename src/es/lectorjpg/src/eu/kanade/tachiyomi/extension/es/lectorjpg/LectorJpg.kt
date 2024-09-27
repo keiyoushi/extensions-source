8fad0ad116fdf3cb2863ac87ab142778c7709740
bapeey <90949336+bapeey@users.noreply.github.com>
2024-09-27 00:09:49 -0500

    lectorjpg

---
+++ src/es/lectorjpg/src/eu/kanade/tachiyomi/extension/es/lectorjpg/LectorJpg.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.es.lectorjpg
+
+import eu.kanade.tachiyomi.multisrc.mangaesp.MangaEsp
+
+class LectorJpg : MangaEsp(
+    "LectorJPG",
+    "https://lectorjpg.com",
+    "es",
+    "https://apis.pichulasjpg.xyz",
+)
