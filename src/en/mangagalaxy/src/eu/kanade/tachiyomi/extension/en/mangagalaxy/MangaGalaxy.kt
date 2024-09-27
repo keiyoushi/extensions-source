3a1a5b0472d83197dd2bab63617a9a6015a41365
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-09-27 15:21:43 +0200

    Update domain for 3 extensions (#5245)
    * Manga168: update domain
    
    * MangaDenizi: update domain
    
    * Manga Galaxy: update domain

--- src/en/mangagalaxy/src/eu/kanade/tachiyomi/extension/en/mangagalaxy/MangaGalaxy.kt
+++ src/en/mangagalaxy/src/eu/kanade/tachiyomi/extension/en/mangagalaxy/MangaGalaxy.kt
@@ -5,7 +5,7 @@ import eu.kanade.tachiyomi.multisrc.iken.Iken
 class MangaGalaxy : Iken(
     "Manga Galaxy",
     "en",
-    "https://mangagalaxy.org",
+    "https://mangagalaxy.net",
 ) {
     // moved from Madara to MangaThemesia to Iken
     override val versionId = 3
