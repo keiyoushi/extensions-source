d88977e09cade44bfb89a48fa5e899aed9f32a66
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-09-27 20:48:11 +0200

    Jimanga -> S2Manga.io: update domain

--- src/en/jimanga/src/eu/kanade/tachiyomi/extension/en/jimanga/Jimanga.kt
+++
@@ -1,8 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.jimanga
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class Jimanga : Madara("Jimanga", "https://jimanga.com", "en") {
-    override val useNewChapterEndpoint = false
-    override val filterNonMangaItems = false
-}
