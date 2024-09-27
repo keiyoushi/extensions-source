293868fa842c4d39fb03d6a550e20c12243617d7
Chopper <156493704+choppeh@users.noreply.github.com>
2024-09-27 11:40:22 -0300

    Remove ScanHentaiFR

--- src/fr/scanhentaifr/src/eu/kanade/tachiyomi/extension/fr/scanhentaifr/ScanHentaiFR.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.scanhentaifr
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class ScanHentaiFR : Madara("Scan Hentai FR", "https://scan-hentai.fr", "fr", dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.FRENCH))
