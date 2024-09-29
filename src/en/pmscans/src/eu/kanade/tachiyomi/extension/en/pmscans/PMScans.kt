d42182f0dca1b08c973fbc4968463695fe68f393
Chopper <156493704+choppeh@users.noreply.github.com>
2024-09-29 20:24:38 -0300

    Migrate theme

--- src/en/pmscans/src/eu/kanade/tachiyomi/extension/en/pmscans/PMScans.kt
+++ src/en/pmscans/src/eu/kanade/tachiyomi/extension/en/pmscans/PMScans.kt
@@ -1,15 +1,11 @@
 package eu.kanade.tachiyomi.extension.en.pmscans
 
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 
-class PMScans : Madara(
+class PMScans : MangaThemesia(
     "PMScans",
     "https://rackusreads.com",
     "en",
-    dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ROOT),
 ) {
-    override val versionId = 2
-    override val useNewChapterEndpoint = true
+    override val versionId = 3
 }
