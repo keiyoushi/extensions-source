ceed113eb9117c6047ecb3eda9a7a8b07089de99
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-10-04 09:52:02 +0200

    Remove Manga Queen.com (#5320)

--- src/en/mangaqueencom/src/eu/kanade/tachiyomi/extension/en/mangaqueencom/MangaQueenCom.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangaqueencom
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class MangaQueenCom : Madara("Manga Queen.com", "https://mangaqueen.com", "en") {
-    override val useNewChapterEndpoint = false
-}
