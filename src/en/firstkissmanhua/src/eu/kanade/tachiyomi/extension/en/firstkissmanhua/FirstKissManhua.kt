7710b28080a47b934bea10ea8257b2aaa56a2d26
Chopper <156493704+choppeh@users.noreply.github.com>
2024-10-06 15:10:34 -0300

    Add FirstKissManhua

---
+++ src/en/firstkissmanhua/src/eu/kanade/tachiyomi/extension/en/firstkissmanhua/FirstKissManhua.kt
@@ -0,0 +1,17 @@
+package eu.kanade.tachiyomi.extension.en.firstkissmanhua
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.OkHttpClient
+
+class FirstKissManhua : Madara(
+    "First Kiss Manhua",
+    "https://1stkissmanhua.net",
+    "en",
+) {
+    override val client = super.client.newBuilder()
+        .rateLimit(3)
+        .build()
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+}
