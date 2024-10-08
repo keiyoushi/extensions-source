2013e98947fc78e2425e5948c11afdb5e2ae7e09
Chopper <156493704+choppeh@users.noreply.github.com>
2024-10-08 11:54:17 -0300

    InfernalVoidScans: Update domain (#5397)
    * Update domain
    
    * Remove 'isNsfw'

--- src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
+++ src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
@@ -7,7 +7,7 @@ import okhttp3.Request
 
 class InfernalVoidScans : MangaThemesia(
     "Infernal Void Scans",
-    "https://hivetoon.net",
+    "https://hivetoon.com",
     "en",
 ) {
     override val client = super.client.newBuilder()
