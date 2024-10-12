30d0fd2dec280ee92bf0c934c01e6f9eed7a4999
Chopper <156493704+choppeh@users.noreply.github.com>
2024-10-12 14:59:38 -0300

    Update domain

--- src/pt/huntersscans/src/eu/kanade/tachiyomi/extension/pt/huntersscans/HuntersScans.kt
+++ src/pt/huntersscans/src/eu/kanade/tachiyomi/extension/pt/huntersscans/HuntersScans.kt
@@ -4,20 +4,22 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 class HuntersScans : Madara(
     "Hunters Scan",
-    "https://hunterscomics.com",
+    "https://htoons.online",
     "pt-BR",
     SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")),
 ) {
     override val client = super.client.newBuilder()
         .rateLimit(1, 2)
+        .readTimeout(3, TimeUnit.MINUTES)
         .build()
 
     override val mangaSubString = "series"
 
-    override val useLoadMoreRequest = LoadMoreStrategy.Always
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
 
     override val useNewChapterEndpoint = true
 }
