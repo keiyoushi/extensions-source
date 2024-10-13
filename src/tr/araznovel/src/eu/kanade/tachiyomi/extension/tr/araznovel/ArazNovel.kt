cd86f10ef5b4a6bfba67868d1a0d225dab2b7a11
lefan hu <lefanhu1@gmail.com>
2024-10-12 22:36:52 -0400

    default madara works

--- src/tr/araznovel/src/eu/kanade/tachiyomi/extension/tr/araznovel/ArazNovel.kt
+++ src/tr/araznovel/src/eu/kanade/tachiyomi/extension/tr/araznovel/ArazNovel.kt
@@ -1,22 +1,12 @@
 package eu.kanade.tachiyomi.extension.tr.araznovel
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.source.model.SChapter
-import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Response
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class ArazNovel : Madara("ArazNovel", "https://www.araznovel.com", "tr", SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())) {
-
-    override fun chapterListParse(response: Response): List<SChapter> {
-        val document = response.asJsoup()
-        val mangaUrl = document.location().removeSuffix("/")
-
-        val xhrRequest = xhrChaptersRequest(mangaUrl)
-        val xhrResponse = client.newCall(xhrRequest).execute()
-
-        val chapterElements = xhrResponse.asJsoup().select("ul.version-chap li.wp-manga-chapter")
-        return chapterElements.map(::chapterFromElement)
-    }
-}
+class ArazNovel : Madara(
+    "ArazNovel",
+    "https://www.araznovel.com",
+    "tr",
+    SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()),
+)
