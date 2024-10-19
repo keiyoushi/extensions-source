3cca8496a0b7ea873e65c83e7e8b17f096ee4108
bapeey <90949336+bapeey@users.noreply.github.com>
2024-10-18 22:35:10 -0500

    update theme and domain

--- src/en/suryascans/src/eu/kanade/tachiyomi/extension/en/suryascans/GenzToons.kt
+++ src/en/suryascans/src/eu/kanade/tachiyomi/extension/en/suryascans/GenzToons.kt
@@ -1,16 +1,24 @@
 package eu.kanade.tachiyomi.extension.en.suryascans
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.SChapter
+import org.jsoup.nodes.Element
 
-class GenzToons : MangaThemesia(
+class GenzToons : Keyoapp(
     "Genz Toons",
     "https://genztoons.com",
     "en",
 ) {
-    override val id = 2909429739457928148
-
     override val client = super.client.newBuilder()
         .rateLimit(3)
         .build()
+
+    override fun chapterFromElement(element: Element): SChapter {
+        return super.chapterFromElement(element).apply {
+            if (element.select("img[src*=Coin.svg]").isNotEmpty()) {
+                name = "🔒 $name"
+            }
+        }
+    }
 }
