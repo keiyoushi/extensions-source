a8fa1a4e933a086c975079ee6edb6eedc9a690c6
kana-shii <79055104+kana-shii@users.noreply.github.com>
2024-10-22 23:11:17 -0300

    Clean ZinChanManga.com titles (#5646)

--- src/en/zinchanmangacom/src/eu/kanade/tachiyomi/extension/en/zinchanmangacom/ZinChanMangaCom.kt
+++ src/en/zinchanmangacom/src/eu/kanade/tachiyomi/extension/en/zinchanmangacom/ZinChanMangaCom.kt
@@ -1,5 +1,51 @@
 package eu.kanade.tachiyomi.extension.en.zinchanmangacom
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.model.SManga
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 
-class ZinChanMangaCom : Madara("ZinChanManga.com", "https://zinchanmanga.com", "en")
+class ZinChanMangaCom : Madara("ZinChanManga.com", "https://zinchanmanga.com", "en") {
+    override val useNewChapterEndpoint = true
+
+    override fun popularMangaFromElement(element: Element): SManga {
+        return super.popularMangaFromElement(element).apply {
+            title = url.urlToTitle() ?: title
+        }
+    }
+
+    override fun searchMangaFromElement(element: Element): SManga {
+        return super.searchMangaFromElement(element).apply {
+            title = url.urlToTitle() ?: title
+        }
+    }
+
+    override fun mangaDetailsParse(document: Document): SManga {
+        return super.mangaDetailsParse(document).apply {
+            title = document.location().urlToTitle() ?: title
+        }
+    }
+
+    private fun String.urlToTitle(): String? {
+        val slug = substringAfter("/manga/", "")
+            .substringBefore("/")
+            .takeUnless { it.isBlank() }
+            ?: return null
+
+        val result = StringBuilder(slug.length)
+        var capitalize = true
+        for (char in slug) {
+            result.append(
+                if (char == '-') {
+                    ' '
+                } else if (capitalize) {
+                    char.uppercase()
+                } else {
+                    char.lowercase()
+                },
+            )
+            capitalize = char == '-'
+        }
+        return result.toString()
+    }
+}
