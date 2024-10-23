d021c4fb90d39b1972f9d7355c7b1814ef942e24
bapeey <90949336+bapeey@users.noreply.github.com>
2024-10-23 16:29:22 -0500

    update cdn and move preference to theme

--- src/ar/scans4u/src/eu/kanade/tachiyomi/extension/ar/scans4u/Scans4u.kt
+++ src/ar/scans4u/src/eu/kanade/tachiyomi/extension/ar/scans4u/Scans4u.kt
@@ -2,4 +2,12 @@ package eu.kanade.tachiyomi.extension.ar.scans4u
 
 import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
 
-class Scans4u : Keyoapp("Scans 4u", "https://4uscans.com", "ar")
+class Scans4u : Keyoapp("Scans 4u", "https://4uscans.com", "ar") {
+
+    override fun chapterListSelector(): String {
+        if (!preferences.showPaidChapters) {
+            return "#chapters > a:not(:has(.text-sm span:matches(قادم))):not(:has(img[src*=Coin.svg]))"
+        }
+        return "#chapters > a:not(:has(.text-sm span:matches(قادم)))"
+    }
+}
