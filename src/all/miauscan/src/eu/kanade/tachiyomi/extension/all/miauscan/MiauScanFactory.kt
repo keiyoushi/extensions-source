92fad8297f56316e0ef2e4f3558c4e769f480dba
bapeey <90949336+bapeey@users.noreply.github.com>
2024-10-24 06:44:11 -0500

    MiauScan: Update domain  (#5669)
    * update domain
    
    * bump

--- src/all/miauscan/src/eu/kanade/tachiyomi/extension/all/miauscan/MiauScanFactory.kt
+++ src/all/miauscan/src/eu/kanade/tachiyomi/extension/all/miauscan/MiauScanFactory.kt
@@ -20,7 +20,7 @@ class MiauScanFactory : SourceFactory {
 
 open class MiauScan(lang: String) : MangaThemesia(
     name = "Miau Scan",
-    baseUrl = "https://lectormiau.com",
+    baseUrl = "https://zonamiau.com",
     lang = lang,
     dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
 ) {
