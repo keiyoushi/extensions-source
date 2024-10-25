abe3db791abe343231ec2f1d73892b4bc2d086c6
KirinRaikage <manmitsu@hotmail.fr>
2024-10-25 08:00:41 +0200

    Reaper Scans (FR): Migrate theme (#5678)

--- src/fr/reaperscans/src/eu/kanade/tachiyomi/extension/fr/reaperscans/ReaperScans.kt
+++ src/fr/reaperscans/src/eu/kanade/tachiyomi/extension/fr/reaperscans/ReaperScans.kt
@@ -1,34 +1,15 @@
 package eu.kanade.tachiyomi.extension.fr.reaperscans
 
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.source.model.SChapter
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import org.jsoup.nodes.Element
-import java.text.SimpleDateFormat
-import java.util.Locale
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
 
-class ReaperScans : Madara(
+class ReaperScans : Keyoapp(
     "Reaper Scans",
-    "https://reaperscans.fr",
+    "https://reaper-scans.fr",
     "fr",
-    SimpleDateFormat("dd/MM/yyyy", Locale.US),
 ) {
-    // Migrated from WpMangaReader to Madara.
-    override val versionId = 2
 
-    override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
-        val urlElement = element.selectFirst(chapterUrlSelector)!!
+    // Migrated from Madara to Keyoapp.
+    override val versionId = 4
 
-        name = urlElement.selectFirst("p.chapter-manhwa-title")?.text()
-            ?: urlElement.ownText()
-        date_upload = urlElement.selectFirst("span.chapter-release-date > i")?.text()
-            .let { parseChapterDate(it) }
-
-        val fixedUrl = urlElement.attr("abs:href").toHttpUrl().newBuilder()
-            .removeAllQueryParameters("style")
-            .addQueryParameter("style", "list")
-            .toString()
-
-        setUrlWithoutDomain(fixedUrl)
-    }
+    override val cdnUrl = "https://3xfsjdlc.is1.buzz/uploads"
 }
