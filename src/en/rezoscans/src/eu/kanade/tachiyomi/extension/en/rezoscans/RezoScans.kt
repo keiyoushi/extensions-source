8ca82e23f865fc16c1c3f628a93a091f33478f65
Chopper <156493704+choppeh@users.noreply.github.com>
2024-10-26 03:25:04 -0300

    Fix CDN URL

--- src/en/rezoscans/src/eu/kanade/tachiyomi/extension/en/rezoscans/RezoScans.kt
+++ src/en/rezoscans/src/eu/kanade/tachiyomi/extension/en/rezoscans/RezoScans.kt
@@ -1,11 +1,34 @@
 package eu.kanade.tachiyomi.extension.en.rezoscans
 
 import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+import eu.kanade.tachiyomi.source.model.Page
+import org.jsoup.nodes.Document
 
 class RezoScans : Keyoapp(
     "Rezo Scans",
     "https://rezoscans.com",
     "en",
 ) {
-    override val cdnUrl = "https://3xfsjdlc.is1.buzz/uploads"
+    override fun pageListParse(document: Document): List<Page> {
+        val cdnUrl = getCdnUrl(document)
+        return document.select("#pages > img")
+            .map { it.attr("uid") }
+            .filter(String::isNotEmpty)
+            .mapIndexed { index, img ->
+                Page(index, document.location(), "$cdnUrl/$img")
+            }
+    }
+
+    private fun getCdnUrl(document: Document): String {
+        val cdnHost = document.select("script")
+            .firstOrNull { CDN_HOST.containsMatchIn(it.html()) }
+            ?.let { CDN_HOST.find(it.html())?.groups?.get("host")?.value }
+            ?: throw Exception("CDN host not found")
+
+        return "https://$cdnHost/uploads"
+    }
+
+    companion object {
+        val CDN_HOST = """realUrl\s+?=\s+?`[^}]+.(?<host>[^/]+)""".toRegex()
+    }
 }
