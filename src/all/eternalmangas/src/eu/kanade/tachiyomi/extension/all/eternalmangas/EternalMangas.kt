33bf418ec2c63d3cc8a77bfcd064b68ac4779fed
bapeey <90949336+bapeey@users.noreply.github.com>
2024-10-27 12:58:27 -0500

    fix

--- src/all/eternalmangas/src/eu/kanade/tachiyomi/extension/all/eternalmangas/EternalMangas.kt
+++ src/all/eternalmangas/src/eu/kanade/tachiyomi/extension/all/eternalmangas/EternalMangas.kt
@@ -7,11 +7,17 @@ import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.util.asJsoup
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import okhttp3.FormBody
 import okhttp3.Response
+import org.jsoup.nodes.Document
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 open class EternalMangas(
     lang: String,
@@ -55,9 +61,46 @@ open class EternalMangas(
         return parseComicsList(page, query, filters)
     }
 
-    override fun pageListParse(response: Response): List<Page> {
-        var document = response.asJsoup()
+    override fun mangaDetailsParse(response: Response) = SManga.create().apply {
+        val document = jsRedirect(response)
+        with(document.selectFirst("div#info")!!) {
+            title = select("div:has(p.font-bold:contains(Títuto)) > p.text-sm").text()
+            author = select("div:has(p.font-bold:contains(Autor)) > p.text-sm").text()
+            artist = select("div:has(p.font-bold:contains(Artista)) > p.text-sm").text()
+            genre = select("div:has(p.font-bold:contains(Género)) > p.text-sm > span").joinToString { it.ownText() }
+        }
+        description = document.select("div#sinopsis p").text()
+        thumbnail_url = document.selectFirst("div.contenedor img.object-cover")?.imgAttr()
+    }
 
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val document = jsRedirect(response)
+        return document.select("div.contenedor > div.grid > div > a").map {
+            SChapter.create().apply {
+                name = it.selectFirst("span.text-sm")!!.text()
+                date_upload = try {
+                    it.selectFirst("span.chapter-date")?.attr("data-date")?.let { date ->
+                        dateFormat.parse(date)?.time
+                    } ?: 0
+                } catch (e: ParseException) {
+                    0
+                }
+                setUrlWithoutDomain(it.selectFirst("a")!!.attr("href"))
+            }
+        }
+    }
+
+    override fun pageListParse(response: Response): List<Page> {
+        val doc = jsRedirect(response)
+        return doc.select("main > img").mapIndexed { i, img ->
+            Page(i, imageUrl = img.imgAttr())
+        }
+    }
+
+    private fun jsRedirect(response: Response): Document {
+        var document = response.asJsoup()
         document.selectFirst("body > form[method=post]")?.let {
             val action = it.attr("action")
             val inputs = it.select("input")
@@ -69,10 +112,7 @@ open class EternalMangas(
 
             document = client.newCall(POST(action, headers, form.build())).execute().asJsoup()
         }
-
-        return document.select("main > img").mapIndexed { i, img ->
-            Page(i, imageUrl = img.imgAttr())
-        }
+        return document
     }
 
     @Serializable
