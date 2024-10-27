29b524503a25aa19d84bf1aa32aed187f1d776a8
Chopper <156493704+choppeh@users.noreply.github.com>
2024-10-27 04:14:20 -0300

    Remove LaidBackScans and Astrames (#5715)

--- src/en/laidbackscans/src/eu/kanade/tachiyomi/extension/en/laidbackscans/LaidBackScans.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.laidbackscans
-
-import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
-
-class LaidBackScans : Keyoapp("Laid Back Scans", "https://laidbackscans.org", "en")
