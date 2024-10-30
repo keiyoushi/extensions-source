c5b83586bea247d9054cf9a31eada9771cb4123b
are-are-are <62763969+dejavui@users.noreply.github.com>
2024-10-30 22:02:47 +0700

    Otaku Sanctuary: Update domain (#5764)
    update domain otakusan

--- src/all/otakusanctuary/src/eu/kanade/tachiyomi/extension/all/otakusanctuary/OtakuSanctuaryFactory.kt
+++ src/all/otakusanctuary/src/eu/kanade/tachiyomi/extension/all/otakusanctuary/OtakuSanctuaryFactory.kt
@@ -5,11 +5,11 @@ import eu.kanade.tachiyomi.source.SourceFactory
 
 class OtakuSanctuaryFactory : SourceFactory {
     override fun createSources() = listOf(
-        OtakuSanctuary("Otaku Sanctuary", "https://otakusan.net", "all"),
-        OtakuSanctuary("Otaku Sanctuary", "https://otakusan.net", "vi"),
-        OtakuSanctuary("Otaku Sanctuary", "https://otakusan.net", "en"),
-        OtakuSanctuary("Otaku Sanctuary", "https://otakusan.net", "it"),
-        OtakuSanctuary("Otaku Sanctuary", "https://otakusan.net", "fr"),
-        OtakuSanctuary("Otaku Sanctuary", "https://otakusan.net", "es"),
+        OtakuSanctuary("Otaku Sanctuary", "https://otakusan1.net", "all"),
+        OtakuSanctuary("Otaku Sanctuary", "https://otakusan1.net", "vi"),
+        OtakuSanctuary("Otaku Sanctuary", "https://otakusan1.net", "en"),
+        OtakuSanctuary("Otaku Sanctuary", "https://otakusan1.net", "it"),
+        OtakuSanctuary("Otaku Sanctuary", "https://otakusan1.net", "fr"),
+        OtakuSanctuary("Otaku Sanctuary", "https://otakusan1.net", "es"),
     )
 }
