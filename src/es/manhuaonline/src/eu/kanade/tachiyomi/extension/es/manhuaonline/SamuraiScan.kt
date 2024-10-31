08b5441dab1a3214ce4beaef307b3167726ba7e6
bapeey <90949336+bapeey@users.noreply.github.com>
2024-10-31 02:30:17 -0500

    SamuraiScan: Fix http 404 (#5802)
    change mangaSubstring

--- src/es/manhuaonline/src/eu/kanade/tachiyomi/extension/es/manhuaonline/SamuraiScan.kt
+++ src/es/manhuaonline/src/eu/kanade/tachiyomi/extension/es/manhuaonline/SamuraiScan.kt
@@ -17,7 +17,7 @@ class SamuraiScan : Madara(
     override val useLoadMoreRequest = LoadMoreStrategy.Never
     override val useNewChapterEndpoint = true
 
-    override val mangaSubString = "dl"
+    override val mangaSubString = "read"
 
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(3)
