96c0af15390b01f502b54dc5c8f53306aae3018f
bapeey <90949336+bapeey@users.noreply.github.com>
2024-11-03 06:41:37 -0500

    Manhwa-Latino: Fix missing chapters (#5823)
    paginated chapter list

--- src/es/manhwalatino/src/eu/kanade/tachiyomi/extension/es/manhwalatino/ManhwaLatino.kt
+++ src/es/manhwalatino/src/eu/kanade/tachiyomi/extension/es/manhwalatino/ManhwaLatino.kt
@@ -1,9 +1,14 @@
 package eu.kanade.tachiyomi.extension.es.manhwalatino
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
 import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.ResponseBody.Companion.toResponseBody
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
@@ -17,6 +22,7 @@ class ManhwaLatino : Madara(
 ) {
 
     override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimitHost(baseUrl.toHttpUrl(), 1, 1)
         .addInterceptor { chain ->
             val request = chain.request()
             val headers = request.headers.newBuilder()
@@ -42,6 +48,30 @@ class ManhwaLatino : Madara(
     override val mangaDetailsSelectorStatus = "div.post-content_item:contains(Estado del comic) > div.summary-content"
     override val mangaDetailsSelectorDescription = "div.post-content_item:contains(Resumen) div.summary-container"
     override val pageListParseSelector = "div.page-break img.wp-manga-chapter-img"
+
+    private val chapterListNextPageSelector = "div.pagination > span.current + span"
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val mangaUrl = response.request.url
+        var document = response.asJsoup()
+        launchIO { countViews(document) }
+
+        val chapterList = mutableListOf<SChapter>()
+        var page = 1
+
+        do {
+            val chapterElements = document.select(chapterListSelector())
+            if (chapterElements.isEmpty()) break
+            chapterList.addAll(chapterElements.map { chapterFromElement(it) })
+            val hasNextPage = document.select(chapterListNextPageSelector).isNotEmpty()
+            if (hasNextPage) {
+                page++
+                val nextPageUrl = mangaUrl.newBuilder().setQueryParameter("t", page.toString()).build()
+                document = client.newCall(GET(nextPageUrl, headers)).execute().asJsoup()
+            } else { break }
+        } while (true)
+
+        return chapterList
+    }
 
     override fun chapterFromElement(element: Element): SChapter {
         val chapter = SChapter.create()
