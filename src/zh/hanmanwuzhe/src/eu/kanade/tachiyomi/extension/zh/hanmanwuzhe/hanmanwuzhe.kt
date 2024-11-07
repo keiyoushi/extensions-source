684e78fcc21db6572d8edd6053a0c1b8d4b43f44
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-11-07 16:08:02 +0500

    Apply suggestions from code review

--- src/zh/hanmanwuzhe/src/eu/kanade/tachiyomi/extension/zh/hanmanwuzhe/hanmanwuzhe.kt
+++ src/zh/hanmanwuzhe/src/eu/kanade/tachiyomi/extension/zh/hanmanwuzhe/hanmanwuzhe.kt
@@ -3,8 +3,6 @@ package eu.kanade.tachiyomi.extension.zh.hanmanwuzhe
 import eu.kanade.tachiyomi.multisrc.mccms.MCCMS
 import eu.kanade.tachiyomi.multisrc.mccms.MCCMSConfig
 import eu.kanade.tachiyomi.source.model.Page
-import eu.kanade.tachiyomi.source.model.SChapter
-import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Response
 
@@ -14,9 +12,6 @@ class hanmanwuzhe : MCCMS(
     "zh",
     MCCMSConfig(useMobilePageList = true),
 ) {
-    // Details and chapter pages are broken
-    override fun getMangaUrl(manga: SManga) = baseUrl
-    override fun getChapterUrl(chapter: SChapter) = baseUrl
 
     override fun pageListParse(response: Response): List<Page> {
         val document = response.asJsoup()
