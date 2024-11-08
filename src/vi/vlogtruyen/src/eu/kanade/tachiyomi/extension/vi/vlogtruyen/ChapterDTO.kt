cae9941276a532569b98ddad1ead5fdfe2f5c890
are-are-are <62763969+dejavui@users.noreply.github.com>
2024-11-08 09:29:30 +0700

    Delete SerialName & variables not use

--- src/vi/vlogtruyen/src/eu/kanade/tachiyomi/extension/vi/vlogtruyen/ChapterDTO.kt
+++ src/vi/vlogtruyen/src/eu/kanade/tachiyomi/extension/vi/vlogtruyen/ChapterDTO.kt
@@ -1,6 +1,5 @@
 package eu.kanade.tachiyomi.extension.vi.vlogtruyen
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -10,23 +9,6 @@ data class ChapterDTO(
 )
 
 @Serializable
-data class Data(
-    @SerialName("historyHtml") val historyHtml: String,
-    @SerialName("user") val user: User? = null,
-    @SerialName("userHtml") val userHtml: String,
-    @SerialName("bookmark_manga") val bookmarkManga: Boolean,
-    @SerialName("alertWarningHtml") val alertWarningHtml: String,
-    @SerialName("chaptersHtml") val chaptersHtml: String,
-    @SerialName("comments") val comments: String,
-)
-
-@Serializable
-data class User(
-    @SerialName("username") val username: String,
-    @SerialName("name") val name: String,
-    @SerialName("email") val email: String,
-    @SerialName("status") val status: Boolean,
-    @SerialName("type") val type: Int,
-    @SerialName("image") val image: String,
-    @SerialName("utm_source") val utmSource: String,
+class Data(
+    val chaptersHtml: String,
 )
