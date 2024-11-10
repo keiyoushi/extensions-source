90dfdcffe38b689d33e57757cd71a7f5949c56d3
Chopper <156493704+choppeh@users.noreply.github.com>
2024-11-09 21:06:17 -0300

    Update domain

--- src/id/yuramanga/src/eu/kanade/tachiyomi/extension/id/yuramanga/YuraManga.kt
+++ src/id/yuramanga/src/eu/kanade/tachiyomi/extension/id/yuramanga/YuraManga.kt
@@ -1,9 +1,25 @@
 package eu.kanade.tachiyomi.extension.id.yuramanga
 
 import eu.kanade.tachiyomi.multisrc.zmanga.ZManga
+import java.io.IOException
 import java.text.SimpleDateFormat
 
-class YuraManga : ZManga("YuraManga", "https://yuramanga.my.id", "id", SimpleDateFormat("dd/MM/yyyy")) {
+class YuraManga : ZManga(
+    "YuraManga",
+    "https://www.yuramanga.my.id",
+    "id",
+    SimpleDateFormat("dd/MM/yyyy"),
+) {
     // Moved from Madara to ZManga
     override val versionId = 3
+
+    override val client = super.client.newBuilder()
+        .addInterceptor { chain ->
+            val response = chain.proceed(chain.request())
+            if (response.request.url.pathSegments.contains("login")) {
+                throw IOException("Please log in to the WebView to continue")
+            }
+            response
+        }
+        .build()
 }
