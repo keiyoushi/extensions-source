885ed2796de8fd0be48edc67106f76ce1b22d919
Chopper <156493704+choppeh@users.noreply.github.com>
2024-11-11 05:38:22 -0300

    LScans: Theme changed (#5977)
    Theme changed

--- src/pt/lscans/src/eu/kanade/tachiyomi/extension/pt/lscans/LScans.kt
+++ src/pt/lscans/src/eu/kanade/tachiyomi/extension/pt/lscans/LScans.kt
@@ -1,15 +1,15 @@
 package eu.kanade.tachiyomi.extension.pt.lscans
 
-import eu.kanade.tachiyomi.multisrc.peachscan.PeachScan
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 
-class LScans : PeachScan(
+class LScans : MangaThemesia(
     "L Scans",
     "https://lscans.com",
     "pt-BR",
 ) {
-    // Moved from Madara to PeachScan
-    override val versionId = 2
+    // Moved from PeachScan to Mangathemsia
+    override val versionId = 3
 
     override val client = super.client.newBuilder()
         .rateLimit(3)
