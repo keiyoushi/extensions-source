79ca6667f00b5901ec7cd74f587df92270302fef
Cuong-Tran <cuongtran.tm@gmail.com>
2024-11-14 18:58:56 +0700

    working browsing/latest/reading

--- src/en/manhwa18/src/eu/kanade/tachiyomi/extension/en/manhwa18/Manhwa18.kt
+++ src/en/manhwa18/src/eu/kanade/tachiyomi/extension/en/manhwa18/Manhwa18.kt
@@ -1,62 +1,181 @@
 package eu.kanade.tachiyomi.extension.en.manhwa18
 
-import eu.kanade.tachiyomi.multisrc.mymangacms.MyMangaCMS
+import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.HttpSource
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import okhttp3.Request
+import okhttp3.Response
+import uy.kohesive.injekt.injectLazy
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class Manhwa18 : MyMangaCMS("Manhwa18", "https://manhwa18.com", "en") {
+class Manhwa18 : HttpSource() {
 
-    // Migrated from FMReader to MyMangaCMS.
-    override val versionId = 2
+    override val baseUrl = "https://manhwa18.com"
+    private val apiUrl = "https://cdn3.manhwa18.com/api/v1"
+    override val lang = "en"
+    override val name = "Manhwa18"
+    override val supportsLatest = true
 
-    override val parseAuthorString = "Author"
-    override val parseAlternativeNameString = "Other name"
-    override val parseAlternative2ndNameString = "Doujinshi"
-    override val parseStatusString = "Status"
-    override val parseStatusOngoingStringLowerCase = "on going"
-    override val parseStatusOnHoldStringLowerCase = "on hold"
-    override val parseStatusCompletedStringLowerCase = "completed"
+    override val versionId = 3
 
-    override fun getFilterList(): FilterList = FilterList(
-        Author("Author"),
-        Status(
-            "Status",
-            "All",
-            "Ongoing",
-            "On hold",
-            "Completed",
-        ),
-        Sort(
-            "Order",
-            "A-Z",
-            "Z-A",
-            "Latest update",
-            "New manhwa",
-            "Most view",
-            "Most like",
-        ),
-        GenreList(getGenreList(), "Genre"),
-    )
+    private val json: Json by injectLazy()
 
-    // To populate this list:
-    // console.log([...document.querySelectorAll("div.search-gerne_item")].map(elem => `Genre("${elem.textContent.trim()}", ${elem.querySelector("label").getAttribute("data-genre-id")}),`).join("\n"))
-    override fun getGenreList() = listOf(
-        Genre("Adult", 4),
-        Genre("Doujinshi", 9),
-        Genre("Harem", 17),
-        Genre("Manga", 24),
-        Genre("Manhwa", 26),
-        Genre("Mature", 28),
-        Genre("NTR", 33),
-        Genre("Romance", 36),
-        Genre("Webtoon", 57),
-        Genre("Action", 59),
-        Genre("Comedy", 60),
-        Genre("BL", 61),
-        Genre("Horror", 62),
-        Genre("Raw", 63),
-        Genre("Uncensore", 64),
-    )
+    // popular
+    override fun popularMangaRequest(page: Int): Request {
+        return GET("$apiUrl/get-data-products?page=$page", headers)
+    }
 
-    override fun dateUpdatedParser(date: String): Long =
-        runCatching { dateFormatter.parse(date.substringAfter(" - "))?.time }.getOrNull() ?: 0L
+    override fun popularMangaParse(response: Response): MangasPage {
+        val result = json.decodeFromString<MangaListBrowse>(response.body.string()).browseList
+        return MangasPage(
+            result.mangaList.map { manga ->
+                manga.toSManga()
+            },
+            hasNextPage = result.current_page < result.last_page,
+        )
+    }
+
+    // latest
+    override fun latestUpdatesRequest(page: Int): Request {
+        return GET("$apiUrl/get-data-products-in-filter?arange=new-updated?page=$page", headers)
+    }
+
+    override fun latestUpdatesParse(response: Response) = popularMangaParse(response)
+
+    // search
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        return GET("$apiUrl/get-search-suggest/$query", headers)
+    }
+
+    override fun searchMangaParse(response: Response): MangasPage {
+        val result = json.decodeFromString<List<Manga>>(response.body.string())
+        return MangasPage(
+            result.map { manga ->
+                manga.toSManga()
+            },
+            hasNextPage = false,
+        )
+    }
+
+    // manga details
+    override fun mangaDetailsRequest(manga: SManga): Request {
+        val slug = manga.url.substringAfterLast('/')
+        return GET("$apiUrl/get-detail-product/$slug", headers)
+    }
+
+    override fun mangaDetailsParse(response: Response): SManga {
+        val mangaDetail = json.decodeFromString<MangaDetail>(response.body.string())
+        return mangaDetail.manga.toSManga()
+    }
+
+    override fun getMangaUrl(manga: SManga): String {
+        val slug = manga.url.substringAfterLast('/')
+        return "$baseUrl/manga/$slug"
+    }
+
+    // chapter list
+    override fun chapterListRequest(manga: SManga): Request {
+        return mangaDetailsRequest(manga)
+    }
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val mangaDetail = json.decodeFromString<MangaDetail>(response.body.string())
+        val mangaSlug = mangaDetail.manga.slug
+
+        return mangaDetail.manga.episodes?.map { chapter ->
+            SChapter.create().apply {
+                url = "$mangaSlug/${chapter.slug}"
+                name = chapter.name
+                date_upload = chapter.created_at?.parseDate() ?: 0L
+                chapter_number = chapter.name.toFloat()
+            }
+        } ?: emptyList()
+    }
+
+    private fun String.toFloat(): Float {
+        val cleanedString = replace(Regex("[^0-9.]"), "")
+        return cleanedString.toFloatOrNull() ?: 0f
+    }
+
+    override fun getChapterUrl(chapter: SChapter): String {
+        return "$baseUrl/manga/${chapter.url}"
+    }
+
+    // page list
+    override fun pageListRequest(chapter: SChapter): Request {
+        return GET("$apiUrl/get-episode/${chapter.url}", headers)
+    }
+
+    override fun pageListParse(response: Response): List<Page> {
+        val result = json.decodeFromString<ChapterDetail>(response.body.string())
+        return result.episode.servers?.first()?.images?.mapIndexed { index, image ->
+            Page(index = index, imageUrl = image)
+        } ?: emptyList()
+    }
+
+    // unused
+    override fun imageUrlParse(response: Response): String {
+        throw UnsupportedOperationException()
+    }
+
+    private fun String.parseDate(): Long {
+        return runCatching { DATE_FORMATTER.parse(this)?.time }
+            .getOrNull() ?: 0L
+    }
+
+    companion object {
+        private val DATE_FORMATTER by lazy {
+            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH)
+        }
+    }
+//
+//    override fun getFilterList(): FilterList = FilterList(
+//        Status(
+//            "Status",
+//            "All",
+//            "Ongoing",
+//            "On hold",
+//            "Completed",
+//        ),
+//        Sort(
+//            "Order",
+//            "A-Z",
+//            "Z-A",
+//            "Latest update",
+//            "New manhwa",
+//            "Most view",
+//            "Most like",
+//        ),
+//        GenreList(getGenreList(), "Genre"),
+//    )
+//
+//    // To populate this list:
+//    // console.log([...document.querySelectorAll("div.search-gerne_item")].map(elem => `Genre("${elem.textContent.trim()}", ${elem.querySelector("label").getAttribute("data-genre-id")}),`).join("\n"))
+//    override fun getGenreList() = listOf(
+//        Genre("Adult", 4),
+//        Genre("Doujinshi", 9),
+//        Genre("Harem", 17),
+//        Genre("Manga", 24),
+//        Genre("Manhwa", 26),
+//        Genre("Mature", 28),
+//        Genre("NTR", 33),
+//        Genre("Romance", 36),
+//        Genre("Webtoon", 57),
+//        Genre("Action", 59),
+//        Genre("Comedy", 60),
+//        Genre("BL", 61),
+//        Genre("Horror", 62),
+//        Genre("Raw", 63),
+//        Genre("Uncensore", 64),
+//    )
+//
+//    override fun dateUpdatedParser(date: String): Long =
+//        runCatching { dateFormatter.parse(date.substringAfter(" - "))?.time }.getOrNull() ?: 0L
 }
---
+++ src/en/manhwa18/src/eu/kanade/tachiyomi/extension/en/manhwa18/Manhwa18Dto.kt
@@ -0,0 +1,105 @@
+package eu.kanade.tachiyomi.extension.en.manhwa18
+
+import eu.kanade.tachiyomi.source.model.SManga
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MangaListBrowse(
+    @SerialName("products") val browseList: MangaList,
+)
+
+@Serializable
+data class MangaList(
+    val current_page: Int,
+    val last_page: Int,
+    @SerialName("data") val mangaList: List<Manga>,
+)
+
+@Serializable
+data class MangaDetail(
+    @SerialName("product") val manga: Manga,
+)
+
+@Serializable
+data class Manga(
+    val name: String,
+    val full_name: String?,
+    val url_avatar: String,
+    val slug: String,
+    // raw / sub
+    val category_id: Int?,
+    val nation_id: Int?,
+    val is_end: Int?,
+    val rating_qnt: Int?,
+    val status: Int?,
+    val desc: String?,
+    val views: Int?,
+    val created_at: String?,
+    val updated_at: String?,
+    val episodes: List<Episode>?,
+    // genre
+    val types: List<Type>?,
+    // korea / japan
+    val nation: Nation?,
+) {
+    fun toSManga(): SManga {
+        return SManga.create().apply {
+            url = slug
+            title = name
+            description = desc
+            genre = listOf(
+                types?.joinToString { it.name },
+                nation?.name,
+                category_id?.let { Categories[it] },
+            ).filterNotNull()
+                .joinToString()
+
+            status = when (is_end) {
+                1 -> SManga.COMPLETED
+                0 -> SManga.ONGOING
+                else -> SManga.UNKNOWN
+            }
+            thumbnail_url = url_avatar
+        }
+    }
+}
+
+val Categories = mapOf(
+    1 to "Raw",
+    2 to "Sub",
+)
+
+@Serializable
+data class ChapterDetail(
+    val episode: Episode,
+)
+
+@Serializable
+data class Episode(
+    val name: String,
+    val slug: String,
+    val created_at: String?,
+    val updated_at: String?,
+    val update_time: String?,
+    @SerialName("servers") val servers: List<Images>?,
+)
+
+@Serializable
+data class Images(
+    val images: List<String>,
+)
+
+@Serializable
+data class Nation(
+    val status: Int,
+    val name: String,
+    val slug: String,
+)
+
+@Serializable
+data class Type(
+    val status: Int,
+    val name: String,
+    val slug: String,
+)
