a91f8b8b9da4bc5f8007975e5246b200db333ae5
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-11-18 17:48:45 +0100

    Weeb Central: fix broken chapter link

--- src/en/weebcentral/src/eu/kanade/tachiyomi/extension/en/weebcentral/WeebCentral.kt
+++ src/en/weebcentral/src/eu/kanade/tachiyomi/extension/en/weebcentral/WeebCentral.kt
@@ -8,6 +8,7 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.Request
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
@@ -149,6 +150,21 @@ class WeebCentral : ParsedHttpSource() {
         }
     }
     // =============================== Pages ================================
+
+    override fun pageListRequest(chapter: SChapter): Request {
+        val newUrl = (baseUrl + chapter.url)
+            .toHttpUrlOrNull()
+            ?.newBuilder()
+            ?.addPathSegment("images")
+            ?.addQueryParameter("is_prev", "False")
+            ?.addQueryParameter("reading_style", "long_strip")
+            ?.build()
+            ?.toString()
+            ?: chapter.url
+
+        chapter.setUrlWithoutDomain(newUrl)
+        return super.pageListRequest(chapter)
+    }
 
     override fun pageListParse(document: Document): List<Page> {
         return document.select("section[x-data~=scroll] > img").mapIndexed { index, element ->
