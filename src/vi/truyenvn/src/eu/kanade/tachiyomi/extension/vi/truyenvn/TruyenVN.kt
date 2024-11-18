9eca1d4125e38f0b1847ef44566f62ff7dcb6eb3
Bui Dai <140616004+buidai123@users.noreply.github.com>
2024-11-18 20:47:16 +0700

    update some domains (#6080)
    * update some domains
    
    * remove trailing slashes
    
    * revert domains
    
    * revert domains
    
    * update xxmanhwa and truyenvn domains
    
    * revert inarimanga domain
    
    it worked yesterday but now this new domain is not working anymore

--- src/vi/truyenvn/src/eu/kanade/tachiyomi/extension/vi/truyenvn/TruyenVN.kt
+++ src/vi/truyenvn/src/eu/kanade/tachiyomi/extension/vi/truyenvn/TruyenVN.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 
 class TruyenVN : Madara(
     "TruyenVN",
-    "https://truyenvn.wiki",
+    "https://truyenvn.vin",
     "vi",
     dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ROOT),
 ) {
