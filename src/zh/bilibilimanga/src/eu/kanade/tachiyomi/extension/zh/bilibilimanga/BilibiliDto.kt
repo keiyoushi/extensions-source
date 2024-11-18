0a6a5da88abcef1e43dbc7a568a0272995dbe505
AlphaBoom <30779939+AlphaBoom@users.noreply.github.com>
2024-11-18 21:46:26 +0800

    BiliBiliManga(zh-hans): Fix image load. (#6117)
    * BiliBiliManga(zh-hans): Fix image load.
    
    * Remove mistype unused field
    
    * Remove extra data copy

--- src/zh/bilibilimanga/src/eu/kanade/tachiyomi/extension/zh/bilibilimanga/BilibiliDto.kt
+++ src/zh/bilibilimanga/src/eu/kanade/tachiyomi/extension/zh/bilibilimanga/BilibiliDto.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.multisrc.bilibili
+package eu.kanade.tachiyomi.extension.zh.bilibilimanga
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -74,7 +74,12 @@ data class BilibiliImageDto(
 data class BilibiliPageDto(
     val token: String,
     val url: String,
-)
+    @SerialName("complete_url")
+    val completeUrl: String,
+) {
+    val imageUrl: String
+        get() = completeUrl.ifEmpty { "$url?token=$token" }
+}
 
 @Serializable
 data class BilibiliAccessTokenCookie(
