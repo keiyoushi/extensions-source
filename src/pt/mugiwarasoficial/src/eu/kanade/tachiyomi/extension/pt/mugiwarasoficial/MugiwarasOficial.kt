da3ad2e4f1a3b87367092e815bac294637b3f5fb
Chopper <156493704+choppeh@users.noreply.github.com>
2024-11-20 01:39:06 -0300

    Add ignore ssl errors

--- src/pt/mugiwarasoficial/src/eu/kanade/tachiyomi/extension/pt/mugiwarasoficial/MugiwarasOficial.kt
+++ src/pt/mugiwarasoficial/src/eu/kanade/tachiyomi/extension/pt/mugiwarasoficial/MugiwarasOficial.kt
@@ -1,8 +1,16 @@
 package eu.kanade.tachiyomi.extension.pt.mugiwarasoficial
 
+import android.annotation.SuppressLint
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.OkHttpClient
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.text.SimpleDateFormat
 import java.util.Locale
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class MugiwarasOficial : Madara(
     "Mugiwaras Oficial",
@@ -10,7 +18,30 @@ class MugiwarasOficial : Madara(
     "pt-BR",
     SimpleDateFormat("d 'de' MMM 'de' yyyy", Locale("pt", "BR")),
 ) {
+    override val client = super.client.newBuilder()
+        .rateLimit(3)
+        .ignoreAllSSLErrors()
+        .build()
+
     override val useNewChapterEndpoint = true
     override val useLoadMoreRequest = LoadMoreStrategy.Always
     override val mangaDetailsSelectorStatus = "div.summary-heading:contains(Estado) + .summary-content"
+
+    private fun OkHttpClient.Builder.ignoreAllSSLErrors(): OkHttpClient.Builder {
+        val naiveTrustManager = @SuppressLint("CustomX509TrustManager")
+        object : X509TrustManager {
+            override fun getAcceptedIssuers(): Array<X509Certificate> = emptyArray()
+            override fun checkClientTrusted(certs: Array<X509Certificate>, authType: String) = Unit
+            override fun checkServerTrusted(certs: Array<X509Certificate>, authType: String) = Unit
+        }
+
+        val insecureSocketFactory = SSLContext.getInstance("TLSv1.2").apply {
+            val trustAllCerts = arrayOf<TrustManager>(naiveTrustManager)
+            init(null, trustAllCerts, SecureRandom())
+        }.socketFactory
+
+        sslSocketFactory(insecureSocketFactory, naiveTrustManager)
+        hostnameVerifier { _, _ -> true }
+        return this
+    }
 }
