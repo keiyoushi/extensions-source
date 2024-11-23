09aecd63d1528555562475b5e384fdfcb972e7e8
bapeey <90949336+bapeey@users.noreply.github.com>
2024-11-23 05:09:15 -0500

    LMTO Online: Rebrand to Jobsibe (#6226)
    * rebrand
    
    * remove jobsibe

--- src/es/lmtoonline/src/eu/kanade/tachiyomi/extension/es/lmtoonline/Jobsibe.kt
+++ src/es/lmtoonline/src/eu/kanade/tachiyomi/extension/es/lmtoonline/Jobsibe.kt
@@ -6,12 +6,15 @@ import okhttp3.HttpUrl.Companion.toHttpUrl
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class LMTOOnline : Madara(
-    "LMTO Online",
+class Jobsibe : Madara(
+    "Jobsibe",
     "https://lmtos.com",
     "es",
     dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("es")),
 ) {
+
+    override val id = 3752522006902890093
+
     override val client = super.client.newBuilder()
         .rateLimitHost(baseUrl.toHttpUrl(), 3, 1)
         .build()
