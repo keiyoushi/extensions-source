19cc3d506ae80b4610c7c2b7d6311d0a158c7cea
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-11-23 10:50:36 +0100

    YuraManga: fix page selector

--- src/id/yuramanga/src/eu/kanade/tachiyomi/extension/id/yuramanga/YuraManga.kt
+++ src/id/yuramanga/src/eu/kanade/tachiyomi/extension/id/yuramanga/YuraManga.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.extension.id.yuramanga
 
 import eu.kanade.tachiyomi.multisrc.zmanga.ZManga
+import eu.kanade.tachiyomi.source.model.Page
+import org.jsoup.nodes.Document
 import java.io.IOException
 import java.text.SimpleDateFormat
 
@@ -22,4 +24,10 @@ class YuraManga : ZManga(
             response
         }
         .build()
+
+    override fun pageListParse(document: Document): List<Page> {
+        return document.select("div.reader-area img.lazyload").mapIndexed { i, img ->
+            Page(i, "", img.attr("abs:data-src"))
+        }
+    }
 }
