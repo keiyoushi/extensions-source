ffe076cd317202050ad92cdf3c3ab3a30c71007f
dngonz <dapalacios95@gmail.com>
2024-11-25 20:21:39 +0100

    remove slash
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>

--- src/tr/mangabari/src/eu/kanade/tachiyomi/extension/tr/mangabari/MangaBari.kt
+++ src/tr/mangabari/src/eu/kanade/tachiyomi/extension/tr/mangabari/MangaBari.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 
 class MangaBari : Madara(
     "Manga Bari",
-    "https://mangabari.me/",
+    "https://mangabari.me",
     "tr",
     dateFormat = java.text.SimpleDateFormat("MMMM dd, yyyy", Locale("tr")),
 ) {
