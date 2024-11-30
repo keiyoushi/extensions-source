e46b4ddbf6e893de7db9588e357b472aca0617e2
Creepler13 <denis.schlichting03@gmail.com>
2024-12-01 00:47:53 +0100

    Add Yaoibar

---
+++ src/tr/yaoibar/src/eu/kanade/tachiyomi/extension/tr/yaoibar/Yaoibar.kt
@@ -0,0 +1,12 @@
+package eu.kanade.tachiyomi.extension.tr.yaoibar
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Yaoibar : Madara(
+    "Yaoibar",
+    "https://yaoibar.gay/",
+    "tr",
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("tr")),
+)
