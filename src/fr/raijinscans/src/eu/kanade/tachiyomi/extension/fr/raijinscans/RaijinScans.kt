dddd42915972799ea9b73c9af4bc3062d6f17710
KirinRaikage <manmitsu@hotmail.fr>
2024-12-03 14:26:20 +0100

    Raijin Scans: Update domain (#6438)

--- src/fr/raijinscans/src/eu/kanade/tachiyomi/extension/fr/raijinscans/RaijinScans.kt
+++ src/fr/raijinscans/src/eu/kanade/tachiyomi/extension/fr/raijinscans/RaijinScans.kt
@@ -4,6 +4,7 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class RaijinScans : Madara("Raijin Scans", "https://raijinscans.fr", "fr", dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.FRENCH)) {
+class RaijinScans : Madara("Raijin Scans", "https://raijinscans.net", "fr", dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.FRENCH)) {
     override val useNewChapterEndpoint = true
+    override val mangaDetailsSelectorStatus = "div.summary-heading:contains(Statut) + div.summary-content"
 }
