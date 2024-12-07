b777a88db100806f272a524b0357da2b81c3896a
are-are-are <62763969+dejavui@users.noreply.github.com>
2024-12-07 13:45:39 +0700

    Remove mangasubtring and delete popularMangaRequest & latestUpdatesRequest

--- src/vi/hentaicube/src/eu/kanade/tachiyomi/extension/vi/hentaicube/HentaiCB.kt
+++ src/vi/hentaicube/src/eu/kanade/tachiyomi/extension/vi/hentaicube/HentaiCB.kt
@@ -1,9 +1,7 @@
 package eu.kanade.tachiyomi.extension.vi.hentaicube
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Page
-import okhttp3.Request
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -12,23 +10,7 @@ class HentaiCB : Madara("CBHentai", "https://hentaicube.xyz", "vi", SimpleDateFo
 
     override val id: Long = 823638192569572166
 
-    override val mangaSubString = "read"
-
     override val filterNonMangaItems = false
-
-    override fun popularMangaRequest(page: Int): Request =
-        if (useLoadMoreRequest()) {
-            loadMoreRequest(page, popular = true)
-        } else {
-            GET("$baseUrl/$mangaSubString/${searchPage(page)}?s&post_type=wp-manga&m_orderby=views", headers)
-        }
-
-    override fun latestUpdatesRequest(page: Int): Request =
-        if (useLoadMoreRequest()) {
-            loadMoreRequest(page, popular = false)
-        } else {
-            GET("$baseUrl/$mangaSubString/${searchPage(page)}?s&post_type=wp-manga&m_orderby=latest", headers)
-        }
 
     override fun pageListParse(document: Document): List<Page> {
         return super.pageListParse(document).distinctBy { it.imageUrl }
