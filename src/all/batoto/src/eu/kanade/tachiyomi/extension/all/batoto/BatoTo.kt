03a11d44bfa74cfeb12f08d10bbc2988538ab43b
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-12-12 12:28:23 +0500

    lint

--- src/all/batoto/src/eu/kanade/tachiyomi/extension/all/batoto/BatoTo.kt
+++ src/all/batoto/src/eu/kanade/tachiyomi/extension/all/batoto/BatoTo.kt
@@ -33,7 +33,6 @@ import okhttp3.Response
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import org.jsoup.parser.ParseError
 import org.jsoup.parser.Parser
 import rx.Observable
 import uy.kohesive.injekt.Injekt
