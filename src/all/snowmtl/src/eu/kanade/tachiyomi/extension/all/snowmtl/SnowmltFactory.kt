89913007c700c0758228a94deb015e29a2d5a5e5
Chopper <156493704+choppeh@users.noreply.github.com>
2024-12-17 02:42:06 -0300

    Snowmtl: Adds support for multiple languages (#6563)
    * Move to src/all
    
    * Optimize translation
    
    * Fix image loading timeout and expired translator token
    
    * Fix extension initialization
    
    * Fix translator response

---
+++ src/all/snowmtl/src/eu/kanade/tachiyomi/extension/all/snowmtl/SnowmltFactory.kt
@@ -0,0 +1,21 @@
+package eu.kanade.tachiyomi.extension.all.snowmtl
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import eu.kanade.tachiyomi.source.Source
+import eu.kanade.tachiyomi.source.SourceFactory
+
+@RequiresApi(Build.VERSION_CODES.O)
+class SnowmltFactory : SourceFactory {
+    override fun createSources(): List<Source> = languageList.map(::Snowmtl)
+}
+
+data class Source(val lang: String, val target: String = lang, val origin: String = "en")
+
+private val languageList = listOf(
+    Source("en"),
+    Source("es"),
+    Source("id"),
+    Source("it"),
+    Source("pt-BR", "pt"),
+)
