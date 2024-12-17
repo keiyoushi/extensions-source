e9fe75f6954d94d459845f64ba49621ab647adc7
Creepler13 <denis.schlichting03@gmail.com>
2024-12-17 18:01:28 +0100

    public titleCache

--- src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
+++ src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/InfernalVoidScans.kt
@@ -1,11 +1,8 @@
 package eu.kanade.tachiyomi.extension.en.infernalvoidscans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
-import eu.kanade.tachiyomi.multisrc.iken.SearchResponse
-import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.util.asJsoup
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import okhttp3.Response
 import uy.kohesive.injekt.injectLazy
@@ -29,14 +26,6 @@ class InfernalVoidScans : Iken(
 
     override fun headersBuilder() = super.headersBuilder()
         .set("Cache-Control", "max-age=0")
-
-    private val titleCache by lazy {
-        val response = client.newCall(GET("$baseUrl/api/query?perPage=9999", headers)).execute()
-        val data = json.decodeFromString<SearchResponse>(response.body.string())
-        data.posts
-            .filterNot { it.isNovel }
-            .associateBy { it.slug }
-    }
 
     override fun popularMangaParse(response: Response): MangasPage {
         val document = response.asJsoup()
