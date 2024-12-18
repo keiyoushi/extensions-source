1b7c7c90dcaf898134ee862ac3f7496790a0dc04
Lev Vinokurov <blendamedkiba94@gmail.com>
2024-12-18 06:18:02 +0300

    Update ReadChainsawManMangaOnline source

--- src/en/readchainsawmanmangaonline/src/eu/kanade/tachiyomi/extension/en/readchainsawmanmangaonline/ReadChainsawManMangaOnline.kt
+++ src/en/readchainsawmanmangaonline/src/eu/kanade/tachiyomi/extension/en/readchainsawmanmangaonline/ReadChainsawManMangaOnline.kt
@@ -4,9 +4,20 @@ import eu.kanade.tachiyomi.multisrc.mangacatalog.MangaCatalog
 
 class ReadChainsawManMangaOnline : MangaCatalog("Read Chainsaw Man Manga Online", "https://ww1.readchainsawman.com", "en") {
     override val sourceList = listOf(
+        // Chainsaw Man
         Pair("Chainsaw Man", "$baseUrl/manga/chainsaw-man/"),
+        Pair("Chainsaw Man: Colored", "$baseUrl/manga/chainsaw-man-colored/"),
+        Pair("Chainsaw Man: Buddy Stories light novel", "$baseUrl/manga/chainsaw-man-buddy-stories/"),
+
+        // Other titles
         Pair("Fire Punch", "$baseUrl/manga/fire-punch/"),
         Pair("Nayuta", "$baseUrl/manga/yogen-no-nayuta/"),
-        Pair("Colored", "$baseUrl/manga/chainsaw-man-colored/"),
+        Pair("Look Back", "$baseUrl/manga/look-back/"),
+        Pair("Listen to Song", "$baseUrl/manga/futsuu-ni-kiite-kure/"),
+        Pair("Goodbye, Eri", "$baseUrl/manga/sayonara-eri-goodbye-eri/"),
+
+        // Storybooks
+        Pair("17-21", "$baseUrl/manga/17-21-fujimoto-tatsuki-tanpenshuu/"),
+        Pair("22-26", "$baseUrl/manga/22-26-fujimoto-tatsuki-tanpenshuu/"),
     ).sortedBy { it.first }.distinctBy { it.second }
 }
