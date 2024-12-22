e2d2df630803dde29b44e51365ac74c6c09467a8
Shahzaib <shahzaib1234@outlook.com>
2024-12-22 10:10:24 +0500

    Add Asura Scans Free (#6706)

---
+++ src/en/asurascansfree/src/eu/kanade/tachiyomi/extension/en/asurascansfree/AsuraScansFree.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.en.asurascansfree
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class AsuraScansFree : MangaThemesia(
+    "Asura Scans Free (unoriginal)",
+    "https://asurascansfree.com",
+    "en",
+    "/serie",
+)
