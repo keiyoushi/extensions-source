7db63a259f2c79cf0a83517388e6a8fd825cdf77
Chopper <156493704+choppeh@users.noreply.github.com>
2024-12-23 16:40:24 -0300

    Fix Siikomik response code 500

--- src/id/siimanga/src/eu/kanade/tachiyomi/extension/id/siimanga/Siikomik.kt
+++ src/id/siimanga/src/eu/kanade/tachiyomi/extension/id/siimanga/Siikomik.kt
@@ -2,16 +2,27 @@ package eu.kanade.tachiyomi.extension.id.siimanga
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.internal.http.HTTP_INTERNAL_SERVER_ERROR
+import okhttp3.internal.http.HTTP_OK
 
 class Siikomik : MangaThemesia(
     "Siikomik",
-    "https://siikomik.cc",
+    "https://siikomik.lat",
     "id",
 ) {
     override val versionId = 2
 
     override val client = super.client.newBuilder()
         .rateLimit(3)
+        .addInterceptor { chain ->
+            val response = chain.proceed(chain.request())
+            if (response.code == HTTP_INTERNAL_SERVER_ERROR) {
+                return@addInterceptor response.newBuilder()
+                    .code(HTTP_OK)
+                    .build()
+            }
+            response
+        }
         .build()
 
     override val hasProjectPage = true
