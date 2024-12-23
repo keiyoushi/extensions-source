873d2def881befec1c9bca3b7c1001ded89a1a78
Chopper <156493704+choppeh@users.noreply.github.com>
2024-12-23 17:09:24 -0300

    Add MangaMania

---
+++ src/pt/mangamania/src/eu/kanade/tachiyomi/extension/pt/mangamania/MangaMania.kt
@@ -0,0 +1,17 @@
+package eu.kanade.tachiyomi.extension.pt.mangamania
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class MangaMania : Madara(
+    "Mangá Mania",
+    "https://mangamania.com.br",
+    "pt-BR",
+    SimpleDateFormat("dd 'de' MMMMM 'de' yyyy", Locale("pt", "BR")),
+) {
+    override val useNewChapterEndpoint = true
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+}
