80cb2603bc3a87bfdfc211525053b9488cd73d08
Chopper <156493704+choppeh@users.noreply.github.com>
2024-12-23 16:51:02 -0300

    Holiday: Fix popularManga path segment

--- src/en/holymanga/src/eu/kanade/tachiyomi/extension/en/holymanga/HolyManga.kt
+++ src/en/holymanga/src/eu/kanade/tachiyomi/extension/en/holymanga/HolyManga.kt
@@ -1,11 +1,15 @@
 package eu.kanade.tachiyomi.extension.en.holymanga
 
 import eu.kanade.tachiyomi.multisrc.zbulu.Zbulu
+import eu.kanade.tachiyomi.network.GET
+import okhttp3.Request
 
 class HolyManga : Zbulu(
     "HolyManga",
     "https://w34.holymanga.net",
     "en",
 ) {
-    override val supportsLatest = false
+    override fun popularMangaRequest(page: Int): Request {
+        return GET("$baseUrl/popular-manga/page-$page/", headers)
+    }
 }
