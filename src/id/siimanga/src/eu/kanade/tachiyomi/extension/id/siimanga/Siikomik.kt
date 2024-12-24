d6cb70043deb66003e4c2644c2ee8bb77772dddd
Chopper <156493704+choppeh@users.noreply.github.com>
2024-12-24 20:53:47 -0300

    Update domains (#6776)
    * Update domains
    
    * Migrate theme
    
    * Fix Siikomik response code 500
    
    * Holiday: Fix popularManga path segment

--- src/id/siimanga/src/eu/kanade/tachiyomi/extension/id/siimanga/Siikomik.kt
+++ src/id/siimanga/src/eu/kanade/tachiyomi/extension/id/siimanga/Siikomik.kt
@@ -2,6 +2,8 @@ package eu.kanade.tachiyomi.extension.id.siimanga
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.internal.http.HTTP_INTERNAL_SERVER_ERROR
+import okhttp3.internal.http.HTTP_OK
 
 class Siikomik : MangaThemesia(
     "Siikomik",
@@ -12,6 +14,15 @@ class Siikomik : MangaThemesia(
 
     override val client = super.client.newBuilder()
         .rateLimit(3)
+        .addInterceptor { chain ->
+            val response = chain.proceed(chain.request())
+            if (response.code == HTTP_INTERNAL_SERVER_ERROR) {
+                return@addInterceptor response.newBuilder()
+                    .code(HTTP_OK)
+                    .build()
+            }
+            response
+        }
         .build()
 
     override val hasProjectPage = true
