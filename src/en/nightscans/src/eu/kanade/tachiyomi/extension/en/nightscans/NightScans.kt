b389786e49e6ffaab758eafb0068a4b5ef31b8b3
lord-ne <34069872+lord-ne@users.noreply.github.com>
2024-12-24 06:49:18 -0500

    [MangaThemesia] Option to hide paid chapters (#6598)
    * Initial implementation
    
    * Move implementation to helper
    
    * Update gradle files
    
    * Formatting
    
    * Use base class

--- src/en/nightscans/src/eu/kanade/tachiyomi/extension/en/nightscans/NightScans.kt
+++ src/en/nightscans/src/eu/kanade/tachiyomi/extension/en/nightscans/NightScans.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.extension.en.nightscans
 
+import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaAlt
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaPaidChapterHelper
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
@@ -13,4 +15,18 @@ class NightScans : MangaThemesiaAlt("NIGHT SCANS", "https://nightsup.net", "en",
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(20, 4, TimeUnit.SECONDS)
         .build()
+
+    private val paidChapterHelper = MangaThemesiaPaidChapterHelper()
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        super.setupPreferenceScreen(screen)
+        paidChapterHelper.addHidePaidChaptersPreferenceToScreen(screen, intl)
+    }
+
+    override fun chapterListSelector(): String {
+        return paidChapterHelper.getChapterListSelectorBasedOnHidePaidChaptersPref(
+            super.chapterListSelector(),
+            preferences,
+        )
+    }
 }
