aeb07f2ec6bc264df4825329ac199238c9f27601
Chopper <156493704+choppeh@users.noreply.github.com>
2024-12-24 20:54:13 -0300

    QuantumScans: Theme and domain changed (#6800)
    Theme changed

--- src/en/quantumscans/src/eu/kanade/tachiyomi/extension/en/quantumscans/QuantumScans.kt
+++ src/en/quantumscans/src/eu/kanade/tachiyomi/extension/en/quantumscans/QuantumScans.kt
@@ -1,10 +1,16 @@
 package eu.kanade.tachiyomi.extension.en.quantumscans
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import java.util.concurrent.TimeUnit
 
-class QuantumScans : MangaThemesia("Quantum Scans", "https://qscomics.org", "en", "/series") {
+class QuantumScans : Keyoapp(
+    "Quantum Scans",
+    "https://quantumscans.org",
+    "en",
+) {
+    // Moved from Mangathemsia to Keyoapp
+    override val versionId = 2
 
     override val client = super.client.newBuilder()
         .rateLimit(3, 1, TimeUnit.SECONDS)
