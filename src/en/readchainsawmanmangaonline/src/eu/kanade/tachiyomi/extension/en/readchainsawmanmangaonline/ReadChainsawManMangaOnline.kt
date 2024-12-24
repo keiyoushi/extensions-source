ca056ab1964fd845ad18090f35161d54043deb31
Lev <blendamedkiba94@gmail.com>
2024-12-24 14:49:53 +0300

    Update ReadChainsawManMangaOnline extension (#6665)
    * Update ReadChainsawManMangaOnline source
    
    * Updated domain
    
    * Update domain name in the class constructor and changed titles to match source naming
    
    * Update ReadChainsawManMangaOnline nsfw flag
    
    * Update src/en/readchainsawmanmangaonline/build.gradle
    
    ---------
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/en/readchainsawmanmangaonline/src/eu/kanade/tachiyomi/extension/en/readchainsawmanmangaonline/ReadChainsawManMangaOnline.kt
+++ src/en/readchainsawmanmangaonline/src/eu/kanade/tachiyomi/extension/en/readchainsawmanmangaonline/ReadChainsawManMangaOnline.kt
@@ -2,11 +2,22 @@ package eu.kanade.tachiyomi.extension.en.readchainsawmanmangaonline
 
 import eu.kanade.tachiyomi.multisrc.mangacatalog.MangaCatalog
 
-class ReadChainsawManMangaOnline : MangaCatalog("Read Chainsaw Man Manga Online", "https://ww1.readchainsawman.com", "en") {
+class ReadChainsawManMangaOnline : MangaCatalog("Read Chainsaw Man Manga Online", "https://ww4.readchainsawman.com", "en") {
     override val sourceList = listOf(
+        // Chainsaw Man
         Pair("Chainsaw Man", "$baseUrl/manga/chainsaw-man/"),
+        Pair("Chainsaw Man Colored", "$baseUrl/manga/chainsaw-man-colored/"),
+        Pair("Chainsaw Man: Buddy Stories", "$baseUrl/manga/chainsaw-man-buddy-stories/"),
+
+        // Other titles
         Pair("Fire Punch", "$baseUrl/manga/fire-punch/"),
-        Pair("Nayuta", "$baseUrl/manga/yogen-no-nayuta/"),
-        Pair("Colored", "$baseUrl/manga/chainsaw-man-colored/"),
+        Pair("Yogen no Nayuta", "$baseUrl/manga/yogen-no-nayuta/"),
+        Pair("Look Back", "$baseUrl/manga/look-back/"),
+        Pair("Just Listen to the Song", "$baseUrl/manga/futsuu-ni-kiite-kure/"),
+        Pair("Sayonara Eri (Goodbye, Eri)", "$baseUrl/manga/sayonara-eri-goodbye-eri/"),
+
+        // Storybooks
+        Pair("Tatsuki Fujimoto Before Chainsaw Man: 17-21", "$baseUrl/manga/17-21-fujimoto-tatsuki-tanpenshuu/"),
+        Pair("Tatsuki Fujimoto Before Chainsaw Man: 22-26", "$baseUrl/manga/22-26-fujimoto-tatsuki-tanpenshuu/"),
     ).sortedBy { it.first }.distinctBy { it.second }
 }
