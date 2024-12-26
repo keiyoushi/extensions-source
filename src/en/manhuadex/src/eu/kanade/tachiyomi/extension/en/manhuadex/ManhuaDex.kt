f0c93d43593c222173e4a75f714e2f4dbf871610
Vetle Ledaal <vetle.ledaal@gmail.com>
2024-12-26 12:53:11 +0100

    Remove parked domains
    Remove Atlantis Scan
    Remove Boosei
    Remove CreepyScans
    Remove EarlyManga
    Remove Glory Manga
    Remove Heroxia
    Remove Lucky Manga
    Remove MangaDeemak
    Remove MangaKitsune
    Remove Manga Mitsu
    Remove Mangá Nanquim
    Remove ManhuaDex
    Remove Mirai Scans
    Remove Pony Manga
    Remove RawZ
    Remove TuManhwas

--- src/en/manhuadex/src/eu/kanade/tachiyomi/extension/en/manhuadex/ManhuaDex.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.manhuadex
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class ManhuaDex : Madara("ManhuaDex", "https://manhuadex.com", "en") {
-    override val useNewChapterEndpoint = true
-}
