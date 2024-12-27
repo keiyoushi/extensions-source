1196260a446a14beffef86af8a899d0fade2c67e
Creepler13 <denis.schlichting03@gmail.com>
2024-12-27 07:10:52 +0100

    mangapro to Iken multisrc (#6826)
    mangapro to Iken

--- src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/MangaPro.kt
+++ src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/MangaPro.kt
@@ -1,43 +1,11 @@
 package eu.kanade.tachiyomi.extension.ar.mangapro
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import eu.kanade.tachiyomi.source.model.Page
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import org.jsoup.nodes.Document
-import java.text.SimpleDateFormat
-import java.util.Locale
+import eu.kanade.tachiyomi.multisrc.iken.Iken
 
-class MangaPro : MangaThemesia(
+class MangaPro : Iken(
     "Manga Pro",
-    "https://promanga.pro",
     "ar",
-    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("ar")),
+    "https://promanga.pro",
 ) {
-    override val versionId = 3
-
-    override fun pageListParse(document: Document): List<Page> {
-        return super.pageListParse(document).onEach {
-            val httpUrl = it.imageUrl!!.toHttpUrl()
-
-            if (wpImgRegex.containsMatchIn(httpUrl.host)) {
-                it.imageUrl = StringBuilder().apply {
-                    val ssl = httpUrl.queryParameter("ssl")
-                    when (ssl) {
-                        null -> append(httpUrl.scheme)
-                        "0" -> append("http")
-                        else -> append("https")
-                    }
-                    append("://")
-                    append(httpUrl.pathSegments.joinToString("/"))
-                    val search = httpUrl.queryParameter("q")
-                    if (search != null) {
-                        append("?q=")
-                        append(search)
-                    }
-                }.toString()
-            }
-        }
-    }
+    override val versionId = 4
 }
-
-private val wpImgRegex = Regex("""i\d+\.wp\.com""")
