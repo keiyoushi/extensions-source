a6943faeb6f17111dc5bcbee92f00e9724f39d40
FunnyTiming <192810776+FunnyTiming@users.noreply.github.com>
2024-12-28 21:11:57 +0100

    More advencements

--- src/en/mangafre/src/eu/kanade/tachiyomi/extension/en/mangafre/MangafreGlobal.kt
+++ src/en/mangafre/src/eu/kanade/tachiyomi/extension/en/mangafre/MangafreGlobal.kt
@@ -1,25 +1,24 @@
 package eu.kanade.tachiyomi.extension.en.mangafre
 
+import android.util.Log
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.asObservableSuccess
+import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Request
+import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import java.text.SimpleDateFormat
-import java.util.Locale
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.network.asObservableSuccess
-import eu.kanade.tachiyomi.source.model.Filter
-import eu.kanade.tachiyomi.source.model.MangasPage
-import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import okhttp3.Response
-import org.jsoup.Jsoup
 import org.jsoup.select.Evaluator
 import rx.Observable
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 abstract class MangafreGlobal(
     override val lang: String,
@@ -48,7 +47,7 @@ abstract class MangafreGlobal(
     }
 
     override fun popularMangaSelector(): String {
-        return ".list .row"
+        return "#list-page > .col-truyen-main .row"
     }
 
     override fun popularMangaFromElement(element: Element): SManga {
@@ -98,7 +97,91 @@ abstract class MangafreGlobal(
             }
     }
 
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        if (query.isNotEmpty()) {
+            return GET("$baseUrl/comic/search.html?keyword=$query&page_num=$page")
+        }
+        filters.forEach { filter ->
+            when (filter) {
+                is GenreFilter -> {
+                    val filterId = filter.selected
+                    return GET("$baseUrl/comic/bookclass.html?type=category_novel&id=$filterId&page_num=$page&language=$lang")
+                }
+                else -> { }
+            }
+        }
+        TODO("Need to implement empty search somewhere")
+    }
 
+    override fun searchMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select(searchMangaSelector()).map { searchMangaFromElement(it) }
+        return MangasPage(mangas, false)
+    }
+    override fun searchMangaSelector(): String = popularMangaSelector()
+
+    override fun searchMangaFromElement(element: Element): SManga = popularMangaFromElement(element)
+
+    override fun searchMangaNextPageSelector(): String? = popularMangaNextPageSelector()
+
+    override fun fetchSearchManga(
+        page: Int,
+        query: String,
+        filters: FilterList,
+    ): Observable<MangasPage> {
+        if (!supportsSearch) {
+            return Observable.just(MangasPage(emptyList(), false))
+        }
+        return client.newCall(searchMangaRequest(page, query, filters))
+            .asObservableSuccess()
+            .map { response ->
+                searchMangaParse(response)
+            }
+    }
+
+    // =============================== Filters ==============================
+
+    // Can accept only one Genre at the time
+    override fun getFilterList() = FilterList(
+        Note,
+        Filter.Separator(),
+        GenreFilter(getGenreFilter(lang), 0),
+    )
+
+    // =========================== Manga Details ============================
+
+    override fun mangaDetailsParse(document: Document): SManga {
+        TODO("Not yet implemented")
+    }
+
+    private fun Element.parseAuthorsTo(manga: SManga) {
+        TODO("Not yet implemented")
+    }
+
+    // =============================== Chapters ==============================
+
+    override fun chapterFromElement(element: Element): SChapter {
+        TODO("Not yet implemented")
+    }
+
+
+    // =============================== Pages ================================
+
+    override fun fetchPageList(chapter: SChapter): Observable<List<Page>> {
+        return client.newCall(GET(baseUrl + chapter.url))
+            .asObservableSuccess()
+            .map { response ->
+                pageListParse(response.asJsoup())
+            }
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        TODO("Not yet implemented")
+    }
+
+    override fun imageUrlParse(document: Document): String {
+        TODO("Not yet implemented")
+    }
 }
 
 // ============================= Utilities ==============================
