22e423d191f9791341b6eaf4dd9fdb8fd4934534
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-12-29 19:38:50 +0500

    workflow

--- .github/scripts/modules-to-build.py
+++ .github/scripts/modules-to-build.py
@@ -2,59 +2,72 @@ import os
 import subprocess as sp
 import re
 import json
-import git
+import sys
+from pathlib import Path
+from typing import List, Tuple, Dict, Any
 
-extRegex = re.compile(r"(^src/\w+/\w+)")
-multisrcRegex = re.compile(r"^lib-multisrc/(\w+)")
-libsRegex = re.compile(r"^lib/(\w+)")
-
-def getModuleList(commitHash):
-    repo = git.Repo(search_parent_directories=True)
-    fileList = repo.git.diff('--name-only', commitHash)
+def get_module_list(commitHash: str) -> Tuple[List[str], List[str]]:
+    file_list = sp.run(
+        ["git", "diff", "--name-only", commitHash],
+        check=True,
+        capture_output=True,
+        text=True
+    ).stdout
             
     modules = set()
-    deletedModules = set()
+    libs = set()
+    deleted = set()
+    
+    ext_regex = re.compile(r"^src/(?P<lang>\w+)/(?P<ext_name>\w+)")
+    multisrc_regex = re.compile(r"^lib-multisrc/(?P<multisrc>\w+)")
+    libs_regex = re.compile(r"^lib/(?P<lib>\w+)")
 
-    for file in fileList.splitlines():
-        extMatch = extRegex.search(file)
-        multisrcMatch = multisrcRegex.search(file)
-        libMatch = libsRegex.search(file)
-        if extMatch:
-            directory = extMatch.group(1)
-            if os.path.isdir(directory):
-                modules.add(f':{directory.replace("/", ":")}:assembleRelease')
-            else:
-                name = directory.split("src/", 1)[1].replace("/", ".")
-                deletedModules.add(name)
-        elif multisrcMatch:
-            multisrc = multisrcMatch.group(1)
-            if os.path.isdir(f"lib-multisrc/{multisrc}"):
-                modules.add(f":lib-multisrc:{multisrc}:assembleReleaseAll")
-        elif libMatch:
-            lib = libMatch.group(1)
-            if os.path.isdir(f"lib/{lib}"):
-                modules.add(f":lib:{lib}:assembleRelease")
+    for _file in file_list.splitlines():
+        file = Path(_file).as_posix()
                 
-    return list(modules), list(deletedModules)
+        ext_match = ext_regex.search(file)
+        multisrc_match = multisrc_regex.search(file)
+        lib_match = libs_regex.search(file)
+        
+        if ext_match:
+            lang = ext_match.group("lang")
+            ext_name = ext_match.group("ext_name")
+            if Path("src", lang, ext_name).is_dir():
+                modules.add(f':src:{lang}:{ext_name}:assembleRelease')
+            else:
+                deleted.add(f"{lang}.{ext_name}") # TODO: adjust as needed
+        elif multisrc_match:
+            multisrc = multisrc_match.group("multisrc")
+            if Path("lib-multisrc", multisrc).is_dir():
+                libs.add(f":lib-multisrc:{multisrc}:getDependents")
+        elif lib_match:
+            lib = lib_match.group("lib")
+            if Path("lib", lib).is_dir():
+                libs.add(f":lib:{lib}:getDependents")
+        
+    result = sp.run(
+        ["./gradlew", "-q"] + list(libs),
+        check=True,
+        capture_output=True,
+        text=True
+    ).stdout
+    
+    modules.update([f"{i}:assembleRelease" for i in result.splitlines()])
+                
+    return list(modules), list(deleted)
 
-def chunker(iter, size):
-    chunks = []
+def chunker(iterable: List[str], size: int) -> Dict[str, Any]:
     if size < 1:
         raise ValueError('Chunk size must be greater than 0.')
-    num=0
-    for i in range(0, len(iter), size):
-        chunks.append({"num":num, "modules":iter[i:(i+size)]})
-        num+=1
-    return {"chunk":chunks}
+    return {"chunk": [{"num": i, "modules": iterable[i:i + size]} for i in range(0, len(iterable), size)]}
 
-#commit = getLastSuccessfulCommitHash()
-commit = "c9dd5e2a5ca4bb8731fa3afe34862d91772e016a"
-modules, deleted = getModuleList(commit)
+commit = sys.argv[1]
+modules, deleted = get_module_list(commit)
 chunked = chunker(modules, int(os.getenv("CI_CHUNK_SIZE", 65)))
 
 print(f"Module chunks to build:\n{json.dumps(chunked, indent=2)}\n\nModule to delete:\n{json.dumps(deleted, indent=2)}")
 
-if (os.getenv("CI") == "true"):
-    with open(os.getenv("GITHUB_OUTPUT"), 'a') as outFile:
-        outFile.write(f"individualMatrix={json.dumps(chunked)}\n")
-        outFile.write(f"deletedModules={json.dumps(deleted)}")
+if os.getenv("CI") == "true":
+    with open(os.getenv("GITHUB_OUTPUT"), 'a') as out_file:
+        out_file.write(f"individualMatrix={json.dumps(chunked)}\n")
+        out_file.write(f"deletedModules={json.dumps(deleted)}\n")
