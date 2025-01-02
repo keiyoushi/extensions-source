057213309ee1224bfea3f38f01ef9c02b3224c39
dngonz <dapalacios95@gmail.com>
2025-01-02 08:33:12 +0100

    fix images

--- src/en/arvenscans/src/eu/kanade/tachiyomi/extension/en/arvenscans/VortexScans.kt
+++ src/en/arvenscans/src/eu/kanade/tachiyomi/extension/en/arvenscans/VortexScans.kt
@@ -1,9 +1,36 @@
 package eu.kanade.tachiyomi.extension.en.arvenscans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.util.asJsoup
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.Response
+import uy.kohesive.injekt.injectLazy
 
 class VortexScans : Iken(
     "Vortex Scans",
     "en",
     "https://vortexscans.org",
-)
+) {
+
+    private val json by injectLazy<Json>()
+
+    override fun pageListParse(response: Response): List<Page> {
+        val document = response.asJsoup()
+
+        val images = document.selectFirst("script:containsData(images)")
+            ?.data()
+            ?.substringAfter("\\\"images\\\":")
+            ?.substringBefore(",\\\"next")
+            ?.replace("""\"""", "\"")
+            ?.let { json.parseToJsonElement(it).jsonArray }
+            ?: throw Exception("Unable to find images")
+
+        return images.mapIndexed { idx, img ->
+            Page(idx, imageUrl = img.jsonObject["url"]!!.jsonPrimitive.content)
+        }
+    }
+}
