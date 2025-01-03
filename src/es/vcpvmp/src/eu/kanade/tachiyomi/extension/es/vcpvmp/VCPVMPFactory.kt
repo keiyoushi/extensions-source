1a3c61d0739f0917fbf57a236ffcfe6e0ffc7256
dngonz <dapalacios95@gmail.com>
2025-01-03 22:02:32 +0100

    fix pages not all loading

--- src/es/vcpvmp/src/eu/kanade/tachiyomi/extension/es/vcpvmp/VCPVMPFactory.kt
+++ src/es/vcpvmp/src/eu/kanade/tachiyomi/extension/es/vcpvmp/VCPVMPFactory.kt
@@ -39,6 +39,7 @@ class VCP : VerComics("VCP", "https://vercomicsporno.com", "es") {
             Pair("Sole Male", "sole-male"),
             Pair("Tetonas", "tetonas"),
         )
+    override val pageListSelector = "div.wp-content > img, div.wp-content > p img"
 }
 
 class VMP : VerComics("VMP", "https://vermangasporno.com", "es") {
