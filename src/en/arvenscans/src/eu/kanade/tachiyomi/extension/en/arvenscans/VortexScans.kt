5c30f47e13f534df9755703e6c89c1047c3a7d78
dngonz <dapalacios95@gmail.com>
2025-01-04 00:53:45 +0100

    Vortex Scans: Fix no pages found (#6948)
    * fix images
    
    * replace substring with regexp
    
    * change approach
    
    * use kotlinx.seriaze intead of replace for json
    
    * update comment

--- src/en/arvenscans/src/eu/kanade/tachiyomi/extension/en/arvenscans/VortexScans.kt
+++ src/en/arvenscans/src/eu/kanade/tachiyomi/extension/en/arvenscans/VortexScans.kt
@@ -1,9 +1,38 @@
 package eu.kanade.tachiyomi.extension.en.arvenscans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.util.asJsoup
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.Response
+import uy.kohesive.injekt.injectLazy
 
 class VortexScans : Iken(
     "Vortex Scans",
     "en",
     "https://vortexscans.org",
-)
+) {
+
+    private val json by injectLazy<Json>()
+
+    private val regexImages = """\\"images\\":(.*?)\\"next""".toRegex()
+
+    override fun pageListParse(response: Response): List<Page> {
+        val document = response.asJsoup()
+
+        val images = document.selectFirst("script:containsData(images)")
+            ?.data()
+            ?.let { regexImages.find(it)!!.groupValues[1].trim(',') }
+            ?.let { json.decodeFromString<String>("\"$it\"") }
+            ?.let { json.parseToJsonElement(it).jsonArray }
+            ?: throw Exception("Unable to parse images")
+
+        return images.mapIndexed { idx, img ->
+            Page(idx, imageUrl = img.jsonObject["url"]!!.jsonPrimitive.content)
+        }
+    }
+}
