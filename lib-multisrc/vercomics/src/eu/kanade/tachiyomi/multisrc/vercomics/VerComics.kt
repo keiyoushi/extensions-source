8928b060c3eca76e92dfdc0e3ddddc1e332e1dcb
dngonz <dapalacios95@gmail.com>
2025-01-05 01:36:42 +0100

    VCPVMP: Fix pages don't load (#6966)
    * fix pages not all loading
    
    * enhance pageListSelector VerComics

--- lib-multisrc/vercomics/src/eu/kanade/tachiyomi/multisrc/vercomics/VerComics.kt
+++ lib-multisrc/vercomics/src/eu/kanade/tachiyomi/multisrc/vercomics/VerComics.kt
@@ -118,7 +118,8 @@ abstract class VerComics(
     protected open val pageListSelector =
         "div.wp-content p > img:not(noscript img), " +
             "div.wp-content div#lector > img:not(noscript img), " +
-            "div.wp-content > figure img:not(noscript img)"
+            "div.wp-content > figure img:not(noscript img)," +
+            "div.wp-content > img, div.wp-content > p img"
 
     override fun pageListParse(document: Document): List<Page> = document.select(pageListSelector)
         .mapIndexed { i, img -> Page(i, imageUrl = img.imgAttr()) }
