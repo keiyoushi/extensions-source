8886bc8e2f899957b34797adc8081a3436b9c677
Chopper <156493704+choppeh@users.noreply.github.com>
2025-01-05 13:10:28 -0300

    Add IgnisComic

---
+++ src/en/igniscomic/src/eu/kanade/tachiyomi/extension/en/igniscomic/IgnisComic.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.en.igniscomic
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaAlt
+
+class IgnisComic : MangaThemesiaAlt(
+    "Ignis Comic",
+    "https://igniscomic.com",
+    "en",
+)
