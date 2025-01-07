ae3025e9d2622bf657d579089d58f121dabf4402
Dr1ks <dmytro@kushchenko.net>
2025-01-07 10:36:04 -0800

    Update MangaGezgini & Meitu domains (#7028)

--- src/tr/mangagezgini/src/eu/kanade/tachiyomi/extension/tr/mangagezgini/MangaGezgini.kt
+++ src/tr/mangagezgini/src/eu/kanade/tachiyomi/extension/tr/mangagezgini/MangaGezgini.kt
@@ -11,7 +11,7 @@ import java.util.Locale
 
 class MangaGezgini : Madara(
     "MangaGezgini",
-    "https://mangagezgini.me",
+    "https://mangagezgini.vip",
     "tr",
     SimpleDateFormat("dd/MM/yyyy", Locale.ROOT),
 ) {
