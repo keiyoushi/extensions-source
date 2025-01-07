efd0244f6be5333e91ee6d6386871f3f65fea6f5
Dmytro <dmytro@kushchenko.net>
2025-01-06 19:46:45 -0800

    Mintmanga: Fix image loading

--- src/ru/mintmanga/src/eu/kanade/tachiyomi/extension/ru/mintmanga/MintManga.kt
+++ src/ru/mintmanga/src/eu/kanade/tachiyomi/extension/ru/mintmanga/MintManga.kt
@@ -8,10 +8,11 @@ import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import okhttp3.Request
+import org.jsoup.nodes.Document
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
-class MintManga : GroupLe("MintManga", "https://1.mintmanga.com", "ru") {
+class MintManga : GroupLe("MintManga", "https://2.mintmanga.one", "ru") {
 
     override val id: Long = 6
 
@@ -19,6 +20,16 @@ class MintManga : GroupLe("MintManga", "https://1.mintmanga.com", "ru") {
         Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
 
     override val baseUrl by lazy { getPrefBaseUrl() }
+
+    override fun getChapterSearchParams(document: Document): String {
+        val html = document.html()
+
+        val userHashRegex = "user_hash.+'(.+)'".toRegex()
+
+        val userHash = userHashRegex.find(html)?.groupValues?.get(1)
+
+        return userHash?.let { "?d=$it" } ?: ""
+    }
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         val url = super.searchMangaRequest(page, query, filters).url.newBuilder()
