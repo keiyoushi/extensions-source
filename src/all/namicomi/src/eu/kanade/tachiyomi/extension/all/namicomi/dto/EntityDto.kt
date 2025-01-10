dbc588d02d84b0c806a3fb5ca4f2e4f26789b551
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-01-10 00:52:23 +0000

    Change abstract to sealed in EntityDto
    Co-authored-by: FourTOne5 <107297513+FourTOne5@users.noreply.github.com>

--- src/all/namicomi/src/eu/kanade/tachiyomi/extension/all/namicomi/dto/EntityDto.kt
+++ src/all/namicomi/src/eu/kanade/tachiyomi/extension/all/namicomi/dto/EntityDto.kt
@@ -3,14 +3,14 @@ package eu.kanade.tachiyomi.extension.all.namicomi.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-abstract class EntityDto {
+sealed class EntityDto {
     val id: String = ""
     val relationships: List<EntityDto> = emptyList()
     abstract val attributes: AttributesDto?
 }
 
 @Serializable
-abstract class AttributesDto
+sealed interface AttributesDto
 
 @Serializable
 data class UnknownEntity(override val attributes: AttributesDto? = null) : EntityDto()
