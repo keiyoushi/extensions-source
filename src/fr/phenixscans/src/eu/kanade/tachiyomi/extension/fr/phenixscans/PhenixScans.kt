eee4674e93199b32f0dd2a4ae92d2072bcababff
asuc <sucaurel@gmail.com>
2025-01-11 20:46:06 +0100

    Fix PhenixScans: Remove unused chapter parsing logic

--- src/fr/phenixscans/src/eu/kanade/tachiyomi/extension/fr/phenixscans/PhenixScans.kt
+++ src/fr/phenixscans/src/eu/kanade/tachiyomi/extension/fr/phenixscans/PhenixScans.kt
@@ -24,40 +24,4 @@ class PhenixScans : MangaThemesia("PhenixScans", "https://phenixscans.fr", "fr",
         super.mangaDetailsParse(document).apply {
             status = document.select(seriesStatusSelector).text().parseStatus()
         }
-
-    private val chapterUrlSelector = "a"
-    private val chapterNameSelector = ".chapternum"
-    private val chapterDateSelector = ".chapterdate"
-    private val chapterDateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.FRENCH)
-
-    override fun chapterFromElement(element: Element): SChapter {
-        val chapter = SChapter.create()
-
-        with(element) {
-            selectFirst(chapterUrlSelector)?.let { urlElement: Element ->
-                chapter.url = urlElement.attr("abs:href")
-                chapter.name = urlElement.selectFirst(chapterNameSelector)?.text() ?: ""
-            }
-
-            val dateString = selectFirst(chapterDateSelector)?.text()
-            if (!dateString.isNullOrEmpty()) {
-                try {
-                    val parsedDate = chapterDateFormat.parse(dateString)?.time ?: 0
-                    val currentDate = System.currentTimeMillis()
-
-                    if (parsedDate > currentDate) {
-                        chapter.date_upload = 0L
-                    } else {
-                        chapter.date_upload = parsedDate
-                    }
-                } catch (e: ParseException) {
-                    chapter.date_upload = 0L
-                }
-            } else {
-                chapter.date_upload = 0L
-            }
-        }
-
-        return chapter
-    }
 }
