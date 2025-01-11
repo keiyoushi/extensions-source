06970a42100e1a80b44e1883b40ef74d1e93d5b2
asuc <sucaurel@gmail.com>
2025-01-11 11:15:15 +0100

    Fix: PhenixScans: Handle incorrect chapter dates and future dates

--- src/fr/phenixscans/src/eu/kanade/tachiyomi/extension/fr/phenixscans/PhenixScans.kt
+++ src/fr/phenixscans/src/eu/kanade/tachiyomi/extension/fr/phenixscans/PhenixScans.kt
@@ -1,12 +1,15 @@
 package eu.kanade.tachiyomi.extension.fr.phenixscans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class PhenixScans : MangaThemesia("PhenixScans", "https://phenixscans.fr", "fr", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.FRENCH)) {
+class PhenixScans : MangaThemesia("PhenixScans", "https://phenixscans.fr", "fr", dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.FRENCH)) {
     override val seriesAuthorSelector = ".imptdt:contains(Auteur) i, .fmed b:contains(Auteur)+span"
     override val seriesStatusSelector = ".imptdt:contains(Statut) i"
 
@@ -21,4 +24,40 @@ class PhenixScans : MangaThemesia("PhenixScans", "https://phenixscans.fr", "fr",
         super.mangaDetailsParse(document).apply {
             status = document.select(seriesStatusSelector).text().parseStatus()
         }
+
+    private val chapterUrlSelector = "a"
+    private val chapterNameSelector = ".chapternum"
+    private val chapterDateSelector = ".chapterdate"
+    private val chapterDateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.FRENCH)
+
+    override fun chapterFromElement(element: Element): SChapter {
+        val chapter = SChapter.create()
+
+        with(element) {
+            selectFirst(chapterUrlSelector)?.let { urlElement: Element ->
+                chapter.url = urlElement.attr("abs:href")
+                chapter.name = urlElement.selectFirst(chapterNameSelector)?.text() ?: ""
+            }
+
+            val dateString = selectFirst(chapterDateSelector)?.text()
+            if (!dateString.isNullOrEmpty()) {
+                try {
+                    val parsedDate = chapterDateFormat.parse(dateString)?.time ?: 0
+                    val currentDate = System.currentTimeMillis()
+
+                    if (parsedDate > currentDate) {
+                        chapter.date_upload = 0L
+                    } else {
+                        chapter.date_upload = parsedDate
+                    }
+                } catch (e: ParseException) {
+                    chapter.date_upload = 0L
+                }
+            } else {
+                chapter.date_upload = 0L
+            }
+        }
+
+        return chapter
+    }
 }
