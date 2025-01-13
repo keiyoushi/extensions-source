1c16a7bad80aee671d289421ae310aba7e4a8802
dngonz <dapalacios95@gmail.com>
2025-01-13 14:02:27 +0100

    Jiangzaitoon: update domain (#7155)
    update domain

--- src/tr/jiangzaitoon/src/eu/kanade/tachiyomi/extension/tr/jiangzaitoon/Jiangzaitoon.kt
+++ src/tr/jiangzaitoon/src/eu/kanade/tachiyomi/extension/tr/jiangzaitoon/Jiangzaitoon.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit
 
 class Jiangzaitoon : Madara(
     "Jiangzaitoon",
-    "https://jiangzaitoon.gay",
+    "https://jiangzaitoon.one",
     "tr",
     SimpleDateFormat("d MMM yyy", Locale("tr")),
 ) {
