8014da7ff19d0312114981d7cf0941d63bae09bc
dngonz <dapalacios95@gmail.com>
2025-01-13 08:29:54 +0100

    fix chapters name

--- src/tr/kuroimanga/src/eu/kanade/tachiyomi/extension/tr/kuroimanga/KuroiManga.kt
+++ src/tr/kuroimanga/src/eu/kanade/tachiyomi/extension/tr/kuroimanga/KuroiManga.kt
@@ -2,7 +2,9 @@ package eu.kanade.tachiyomi.extension.tr.kuroimanga
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -25,5 +27,25 @@ class KuroiManga : Madara(
             throw Exception("Bu seriyi okumak için abone olmalısınız")
         }
         return pageList
+    }
+
+    override fun chapterFromElement(element: Element): SChapter {
+        val chapter = SChapter.create()
+
+        with(element) {
+            selectFirst(chapterUrlSelector)!!.let { urlElement ->
+                chapter.url = urlElement.attr("abs:href").let {
+                    it.substringBefore("?style=paged") + if (!it.endsWith(chapterUrlSuffix)) chapterUrlSuffix else ""
+                }
+            }
+            chapter.name = selectFirst("li > a")!!.text()
+            // Dates can be part of a "new" graphic or plain text
+            // Added "title" alternative
+            chapter.date_upload = selectFirst("img:not(.thumb)")?.attr("alt")?.let { parseRelativeDate(it) }
+                ?: selectFirst("span a")?.attr("title")?.let { parseRelativeDate(it) }
+                ?: parseChapterDate(selectFirst(chapterDateSelector())?.text())
+        }
+
+        return chapter
     }
 }
