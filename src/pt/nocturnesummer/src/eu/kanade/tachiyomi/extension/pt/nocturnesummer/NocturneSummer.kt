f5fce3d08e206f9596b1f54bb1b1f512f8fe7bdc
dngonz <dapalacios95@gmail.com>
2025-01-19 00:04:41 +0100

    fix chapter order for nocturnesummer

--- src/pt/nocturnesummer/src/eu/kanade/tachiyomi/extension/pt/nocturnesummer/NocturneSummer.kt
+++ src/pt/nocturnesummer/src/eu/kanade/tachiyomi/extension/pt/nocturnesummer/NocturneSummer.kt
@@ -2,7 +2,9 @@ package eu.kanade.tachiyomi.extension.pt.nocturnesummer
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.SChapter
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
@@ -21,4 +23,10 @@ class NocturneSummer : Madara(
     override val useNewChapterEndpoint = true
 
     override val mangaDetailsSelectorStatus = "div.post-content_item:contains(Estado) > div.summary-content"
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        return super.chapterListParse(response)
+            .sortedBy(SChapter::name)
+            .reversed()
+    }
 }
