b1d7226d18d1b81ad3f418e00d51f45b27219589
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-01-19 13:40:47 +0500

    move some source files to standard location (#7247)
    * move files to standard locations
    
    * format
    
    * package
    
    * lint

--- src/fr/rimuscans/src/eu/kanade/tachiyomi/extension/fr/rimuscans/RimuScans.kt
+++ src/fr/rimuscans/src/eu/kanade/tachiyomi/extension/fr/rimuscans/RimuScans.kt
@@ -4,4 +4,9 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class RimuScans : MangaThemesia("Rimu Scans", "https://rimuscans.fr", "fr", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH))
+class RimuScans : MangaThemesia(
+    "Rimu Scans",
+    "https://rimuscans.fr",
+    "fr",
+    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH),
+)
