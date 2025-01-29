184ad22c73290a747094bbbea69091e192d5abf7
MrRobotCoder <alberlandoherculano@gmail.com>
2025-01-29 17:34:24 -0300

    domain update

--- src/es/mangaesp/src/eu/kanade/tachiyomi/extension/es/mangaesp/MangaEsp.kt
+++ src/es/mangaesp/src/eu/kanade/tachiyomi/extension/es/mangaesp/MangaEsp.kt
@@ -2,4 +2,4 @@ package eu.kanade.tachiyomi.extension.es.mangaesp
 
 import eu.kanade.tachiyomi.multisrc.mangaesp.MangaEsp
 
-class MangaEsp : MangaEsp("MangaEsp", "https://mangaesp.net", "es")
+class MangaEsp : MangaEsp("MangaEsp", "https://mangaesp.topmanhuas.org", "es")
