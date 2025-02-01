e1df9c718c34ad3b7a9e19da6e238bb714ccdb5b
Hellkaros <61388561+Hellkaros@users.noreply.github.com>
2025-02-01 09:36:30 -0300

    Domain update (#7370)
    * Domain update
    
    * fix addheader

--- src/en/manhuaga/src/eu/kanade/tachiyomi/extension/en/manhuaga/Manhuaga.kt
+++ src/en/manhuaga/src/eu/kanade/tachiyomi/extension/en/manhuaga/Manhuaga.kt
@@ -3,14 +3,14 @@ package eu.kanade.tachiyomi.extension.en.manhuaga
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import okhttp3.OkHttpClient
 
-class Manhuaga : Madara("Manhuaga", "https://manhuaga.com", "en") {
+class Manhuaga : Madara("Manhuaga", "https://manhua-ga.org", "en") {
     override val client: OkHttpClient = super.client.newBuilder()
         .addInterceptor { chain ->
             val originalRequest = chain.request()
             chain.proceed(originalRequest).let { response ->
                 if (response.code == 403) {
                     response.close()
-                    chain.proceed(originalRequest.newBuilder().removeHeader("Referer").addHeader("Referer", "https://manhuaga.com").build())
+                    chain.proceed(originalRequest.newBuilder().removeHeader("Referer").addHeader("Referer", baseUrl).build())
                 } else {
                     response
                 }
