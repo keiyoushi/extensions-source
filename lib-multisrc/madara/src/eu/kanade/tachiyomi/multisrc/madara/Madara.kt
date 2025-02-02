85bdfb7b324d425cd683767cc4b5f3bc2e4dad9d
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-02-02 13:35:52 +0500

    fix lint

--- lib-multisrc/madara/src/eu/kanade/tachiyomi/multisrc/madara/Madara.kt
+++ lib-multisrc/madara/src/eu/kanade/tachiyomi/multisrc/madara/Madara.kt
@@ -109,7 +109,9 @@ abstract class Madara(
     protected open val useLoadMoreRequest = LoadMoreStrategy.AutoDetect
 
     enum class LoadMoreStrategy {
-        AutoDetect, Always, Never
+        AutoDetect,
+        Always,
+        Never,
     }
 
     /**
@@ -118,7 +120,9 @@ abstract class Madara(
     private var loadMoreRequestDetected = LoadMoreDetection.Pending
 
     private enum class LoadMoreDetection {
-        Pending, True, False
+        Pending,
+        True,
+        False,
     }
 
     protected fun detectLoadMore(document: Document) {
@@ -132,12 +136,10 @@ abstract class Madara(
         }
     }
 
-    protected fun useLoadMoreRequest(): Boolean {
-        return when (useLoadMoreRequest) {
-            LoadMoreStrategy.Always -> true
-            LoadMoreStrategy.Never -> false
-            else -> loadMoreRequestDetected == LoadMoreDetection.True
-        }
+    protected fun useLoadMoreRequest(): Boolean = when (useLoadMoreRequest) {
+        LoadMoreStrategy.Always -> true
+        LoadMoreStrategy.Never -> false
+        else -> loadMoreRequestDetected == LoadMoreDetection.True
     }
 
     // Popular Manga
@@ -176,19 +178,17 @@ abstract class Madara(
         return manga
     }
 
-    override fun popularMangaRequest(page: Int): Request =
-        if (useLoadMoreRequest()) {
-            loadMoreRequest(page, popular = true)
-        } else {
-            GET("$baseUrl/$mangaSubString/${searchPage(page)}?m_orderby=views", headers)
-        }
+    override fun popularMangaRequest(page: Int): Request = if (useLoadMoreRequest()) {
+        loadMoreRequest(page, popular = true)
+    } else {
+        GET("$baseUrl/$mangaSubString/${searchPage(page)}?m_orderby=views", headers)
+    }
 
-    override fun popularMangaNextPageSelector(): String? =
-        if (useLoadMoreRequest()) {
-            "body:not(:has(.no-posts))"
-        } else {
-            "div.nav-previous, nav.navigation-ajax, a.nextpostslink"
-        }
+    override fun popularMangaNextPageSelector(): String? = if (useLoadMoreRequest()) {
+        "body:not(:has(.no-posts))"
+    } else {
+        "div.nav-previous, nav.navigation-ajax, a.nextpostslink"
+    }
 
     // Latest Updates
 
@@ -199,12 +199,11 @@ abstract class Madara(
         return popularMangaFromElement(element)
     }
 
-    override fun latestUpdatesRequest(page: Int): Request =
-        if (useLoadMoreRequest()) {
-            loadMoreRequest(page, popular = false)
-        } else {
-            GET("$baseUrl/$mangaSubString/${searchPage(page)}?m_orderby=latest", headers)
-        }
+    override fun latestUpdatesRequest(page: Int): Request = if (useLoadMoreRequest()) {
+        loadMoreRequest(page, popular = false)
+    } else {
+        GET("$baseUrl/$mangaSubString/${searchPage(page)}?m_orderby=latest", headers)
+    }
 
     override fun latestUpdatesNextPageSelector(): String? = popularMangaNextPageSelector()
 
@@ -251,20 +250,16 @@ abstract class Madara(
         return super.fetchSearchManga(page, query, filters)
     }
 
-    protected open fun searchPage(page: Int): String {
-        return if (page == 1) {
-            ""
-        } else {
-            "page/$page/"
-        }
+    protected open fun searchPage(page: Int): String = if (page == 1) {
+        ""
+    } else {
+        "page/$page/"
     }
 
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        return if (useLoadMoreRequest()) {
-            searchLoadMoreRequest(page, query, filters)
-        } else {
-            searchRequest(page, query, filters)
-        }
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request = if (useLoadMoreRequest()) {
+        searchLoadMoreRequest(page, query, filters)
+    } else {
+        searchRequest(page, query, filters)
     }
 
     protected open fun searchRequest(page: Int, query: String, filters: FilterList): Request {
@@ -310,7 +305,9 @@ abstract class Madara(
                     filter.state
                         .filter { it.state }
                         .let { list ->
-                            if (list.isNotEmpty()) { list.forEach { genre -> url.addQueryParameter("genre[]", genre.id) } }
+                            if (list.isNotEmpty()) {
+                                list.forEach { genre -> url.addQueryParameter("genre[]", genre.id) }
+                            }
                         }
                 }
                 else -> {}
@@ -489,8 +486,7 @@ abstract class Madara(
             intl["adult_content_filter_only"] to "1",
         )
 
-    open class UriPartFilter(displayName: String, private val vals: Array<Pair<String, String>>, state: Int = 0) :
-        Filter.Select<String>(displayName, vals.map { it.first }.toTypedArray(), state) {
+    open class UriPartFilter(displayName: String, private val vals: Array<Pair<String, String>>, state: Int = 0) : Filter.Select<String>(displayName, vals.map { it.first }.toTypedArray(), state) {
         fun toUriPart() = vals[state].second
     }
 
@@ -499,21 +495,21 @@ abstract class Madara(
     protected class AuthorFilter(title: String) : Filter.Text(title)
     protected class ArtistFilter(title: String) : Filter.Text(title)
     protected class YearFilter(title: String) : Filter.Text(title)
-    protected class StatusFilter(title: String, status: List<Tag>) :
-        Filter.Group<Tag>(title, status)
+    protected class StatusFilter(title: String, status: List<Tag>) : Filter.Group<Tag>(title, status)
 
-    protected class OrderByFilter(title: String, options: List<Pair<String, String>>, state: Int = 0) :
-        UriPartFilter(title, options.toTypedArray(), state)
+    protected class OrderByFilter(title: String, options: List<Pair<String, String>>, state: Int = 0) : UriPartFilter(title, options.toTypedArray(), state)
 
-    protected class GenreConditionFilter(title: String, options: List<Pair<String, String>>) : UriPartFilter(
-        title,
-        options.toTypedArray(),
-    )
+    protected class GenreConditionFilter(title: String, options: List<Pair<String, String>>) :
+        UriPartFilter(
+            title,
+            options.toTypedArray(),
+        )
 
-    protected class AdultContentFilter(title: String, options: List<Pair<String, String>>) : UriPartFilter(
-        title,
-        options.toTypedArray(),
-    )
+    protected class AdultContentFilter(title: String, options: List<Pair<String, String>>) :
+        UriPartFilter(
+            title,
+            options.toTypedArray(),
+        )
 
     protected class GenreList(title: String, genres: List<Genre>) : Filter.Group<GenreCheckBox>(title, genres.map { GenreCheckBox(it.name, it.id) })
     class GenreCheckBox(name: String, val id: String = name) : Filter.CheckBox(name)
@@ -757,32 +753,24 @@ abstract class Madara(
     open val altName = intl["alt_names_heading"]
     open val updatingRegex = "Updating|Atualizando".toRegex(RegexOption.IGNORE_CASE)
 
-    fun String.notUpdating(): Boolean {
-        return this.contains(updatingRegex).not()
-    }
+    fun String.notUpdating(): Boolean = this.contains(updatingRegex).not()
 
-    private fun String.containsIn(array: Array<String>): Boolean {
-        return this.lowercase() in array.map { it.lowercase() }
-    }
+    private fun String.containsIn(array: Array<String>): Boolean = this.lowercase() in array.map { it.lowercase() }
 
-    protected open fun imageFromElement(element: Element): String? {
-        return when {
-            element.hasAttr("data-src") -> element.attr("abs:data-src")
-            element.hasAttr("data-lazy-src") -> element.attr("abs:data-lazy-src")
-            element.hasAttr("srcset") -> element.attr("abs:srcset").getSrcSetImage()
-            element.hasAttr("data-cfsrc") -> element.attr("abs:data-cfsrc")
-            else -> element.attr("abs:src")
-        }
+    protected open fun imageFromElement(element: Element): String? = when {
+        element.hasAttr("data-src") -> element.attr("abs:data-src")
+        element.hasAttr("data-lazy-src") -> element.attr("abs:data-lazy-src")
+        element.hasAttr("srcset") -> element.attr("abs:srcset").getSrcSetImage()
+        element.hasAttr("data-cfsrc") -> element.attr("abs:data-cfsrc")
+        else -> element.attr("abs:src")
     }
 
     /**
      *  Get the best image quality available from srcset
      */
-    protected fun String.getSrcSetImage(): String? {
-        return this.split(" ")
-            .filter(URL_REGEX::matches)
-            .maxOfOrNull(String::toString)
-    }
+    protected fun String.getSrcSetImage(): String? = this.split(" ")
+        .filter(URL_REGEX::matches)
+        .maxOfOrNull(String::toString)
 
     /**
      * Set it to true if the source uses the new AJAX endpoint to
@@ -807,9 +795,7 @@ abstract class Madara(
         return POST("$baseUrl/wp-admin/admin-ajax.php", xhrHeaders, form)
     }
 
-    protected open fun xhrChaptersRequest(mangaUrl: String): Request {
-        return POST("$mangaUrl/ajax/chapters", xhrHeaders)
-    }
+    protected open fun xhrChaptersRequest(mangaUrl: String): Request = POST("$mangaUrl/ajax/chapters", xhrHeaders)
 
     override fun chapterListParse(response: Response): List<SChapter> {
         val document = response.asJsoup()
@@ -880,12 +866,10 @@ abstract class Madara(
     open fun parseChapterDate(date: String?): Long {
         date ?: return 0
 
-        fun SimpleDateFormat.tryParse(string: String): Long {
-            return try {
-                parse(string)?.time ?: 0
-            } catch (_: ParseException) {
-                0
-            }
+        fun SimpleDateFormat.tryParse(string: String): Long = try {
+            parse(string)?.time ?: 0
+        } catch (_: ParseException) {
+            0
         }
 
         return when {
@@ -1006,9 +990,7 @@ abstract class Madara(
         }
     }
 
-    override fun imageRequest(page: Page): Request {
-        return GET(page.imageUrl!!, headers.newBuilder().set("Referer", page.url).build())
-    }
+    override fun imageRequest(page: Page): Request = GET(page.imageUrl!!, headers.newBuilder().set("Referer", page.url).build())
 
     override fun imageUrlParse(document: Document) = ""
 
@@ -1083,26 +1065,22 @@ abstract class Madara(
     /**
      * The request to the search page (or another one) that have the genres list.
      */
-    protected open fun genresRequest(): Request {
-        return GET("$baseUrl/?s=genre&post_type=wp-manga", headers)
-    }
+    protected open fun genresRequest(): Request = GET("$baseUrl/?s=genre&post_type=wp-manga", headers)
 
     /**
      * Get the genres from the search page document.
      *
      * @param document The search page document
      */
-    protected open fun parseGenres(document: Document): List<Genre> {
-        return document.selectFirst("div.checkbox-group")
-            ?.select("div.checkbox")
-            .orEmpty()
-            .map { li ->
-                Genre(
-                    li.selectFirst("label")!!.text(),
-                    li.selectFirst("input[type=checkbox]")!!.`val`(),
-                )
-            }
-    }
+    protected open fun parseGenres(document: Document): List<Genre> = document.selectFirst("div.checkbox-group")
+        ?.select("div.checkbox")
+        .orEmpty()
+        .map { li ->
+            Genre(
+                li.selectFirst("label")!!.text(),
+                li.selectFirst("input[type=checkbox]")!!.`val`(),
+            )
+        }
 
     // https://stackoverflow.com/a/66614516
     protected fun String.decodeHex(): ByteArray {
