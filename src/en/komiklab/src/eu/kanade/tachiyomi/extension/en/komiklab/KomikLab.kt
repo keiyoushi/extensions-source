c7b49f3cb0d3c171e799b95cace07dbfc5b17510
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-02-02 09:28:18 +0100

    Remove some parked and available domains (#7450)
    Remove parked and available domains
    
    BestManhua
    Girls Love Manga!
    Komik Lab
    Lala Manga
    Manga Weebs
    VinManga
    AiYuManhua
    Manga Latino
    FR-Scan
    Indo18h
    KomikGan
    Komik Say
    WorldManhwas

--- src/en/komiklab/src/eu/kanade/tachiyomi/extension/en/komiklab/KomikLab.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.komiklab
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class KomikLab : MangaThemesia("Komik Lab", "https://komiklab.com", "en")
--- src/id/worldmanhwas/src/eu/kanade/tachiyomi/extension/id/worldmanhwas/WorldManhwas.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.id.worldmanhwas
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class WorldManhwas : Madara("WorldManhwas", "https://worldmanhwas.zone", "id", SimpleDateFormat("MMMM dd, yyyy", Locale("id"))) {
-    override val useNewChapterEndpoint = false
-}
