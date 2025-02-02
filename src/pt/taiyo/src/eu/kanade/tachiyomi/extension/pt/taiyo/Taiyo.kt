da3b307bcc71625ba59199b7b33366350378eb33
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-02-02 07:31:15 +0500

    Revert "lint..."
    This reverts commit cc2f99fb218726d90045c5104ab9592a179cb6b6.

--- src/pt/taiyo/src/eu/kanade/tachiyomi/extension/pt/taiyo/Taiyo.kt
+++ src/pt/taiyo/src/eu/kanade/tachiyomi/extension/pt/taiyo/Taiyo.kt
@@ -85,13 +85,15 @@ class Taiyo : ParsedHttpSource() {
 
     // =============================== Search ===============================
 
-    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> = if (query.startsWith(PREFIX_SEARCH)) { // URL intent handler
-        val id = query.removePrefix(PREFIX_SEARCH)
-        client.newCall(GET("$baseUrl/media/$id"))
-            .asObservableSuccess()
-            .map(::searchMangaByIdParse)
-    } else {
-        super.fetchSearchManga(page, query, filters)
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> {
+        return if (query.startsWith(PREFIX_SEARCH)) { // URL intent handler
+            val id = query.removePrefix(PREFIX_SEARCH)
+            client.newCall(GET("$baseUrl/media/$id"))
+                .asObservableSuccess()
+                .map(::searchMangaByIdParse)
+        } else {
+            super.fetchSearchManga(page, query, filters)
+        }
     }
 
     private fun searchMangaByIdParse(response: Response): MangasPage {
@@ -144,11 +146,17 @@ class Taiyo : ParsedHttpSource() {
         return MangasPage(mangas, mangas.isNotEmpty())
     }
 
-    override fun searchMangaSelector(): String = throw UnsupportedOperationException()
+    override fun searchMangaSelector(): String {
+        throw UnsupportedOperationException()
+    }
 
-    override fun searchMangaFromElement(element: Element): SManga = throw UnsupportedOperationException()
+    override fun searchMangaFromElement(element: Element): SManga {
+        throw UnsupportedOperationException()
+    }
 
-    override fun searchMangaNextPageSelector(): String? = throw UnsupportedOperationException()
+    override fun searchMangaNextPageSelector(): String? {
+        throw UnsupportedOperationException()
+    }
 
     // =========================== Manga Details ============================
     override fun mangaDetailsParse(document: Document) = SManga.create().apply {
@@ -227,9 +235,13 @@ class Taiyo : ParsedHttpSource() {
         return Observable.just(chapters.sortedByDescending { it.chapter_number })
     }
 
-    override fun chapterListSelector(): String = throw UnsupportedOperationException()
+    override fun chapterListSelector(): String {
+        throw UnsupportedOperationException()
+    }
 
-    override fun chapterFromElement(element: Element): SChapter = throw UnsupportedOperationException()
+    override fun chapterFromElement(element: Element): SChapter {
+        throw UnsupportedOperationException()
+    }
 
     // =============================== Pages ================================
     override fun pageListParse(document: Document): List<Page> {
@@ -244,7 +256,9 @@ class Taiyo : ParsedHttpSource() {
         }
     }
 
-    override fun imageUrlParse(document: Document): String = throw UnsupportedOperationException()
+    override fun imageUrlParse(document: Document): String {
+        throw UnsupportedOperationException()
+    }
 
     // ============================= Utilities ==============================
     private fun Element.getImageUrl() = absUrl("srcset").substringBefore(" ")
@@ -253,19 +267,23 @@ class Taiyo : ParsedHttpSource() {
         json.decodeFromStream(it.body.byteStream())
     }
 
-    private fun String.toDate(): Long = runCatching { DATE_FORMATTER.parse(this)?.time }
-        .getOrNull() ?: 0L
+    private fun String.toDate(): Long {
+        return runCatching { DATE_FORMATTER.parse(this)?.time }
+            .getOrNull() ?: 0L
+    }
 
     private inline fun <reified T> Document.parseJsonFromDocument(
         itemName: String = "media",
         crossinline transformer: String.() -> String,
-    ): T? = runCatching {
-        val script = selectFirst("script:containsData($itemName\\\\\":):containsData(\\\"6:\\[)")!!.data()
-        val obj = script.substringAfter(",{\\\"$itemName\\\":")
-            .run(transformer)
-            .replace("\\", "")
-        json.decodeFromString<T>(obj)
-    }.onFailure { it.printStackTrace() }.getOrNull()
+    ): T? {
+        return runCatching {
+            val script = selectFirst("script:containsData($itemName\\\\\":):containsData(\\\"6:\\[)")!!.data()
+            val obj = script.substringAfter(",{\\\"$itemName\\\":")
+                .run(transformer)
+                .replace("\\", "")
+            json.decodeFromString<T>(obj)
+        }.onFailure { it.printStackTrace() }.getOrNull()
+    }
 
     // ============================= Authorization ========================
 
@@ -286,10 +304,12 @@ class Taiyo : ParsedHttpSource() {
         return chain.proceed(req)
     }
 
-    private fun getToken(): String = fetchBearerToken().also {
-        preferences.edit()
-            .putString(BEARER_TOKEN_PREF, it)
-            .apply()
+    private fun getToken(): String {
+        return fetchBearerToken().also {
+            preferences.edit()
+                .putString(BEARER_TOKEN_PREF, it)
+                .apply()
+        }
     }
 
     private fun fetchBearerToken(): String {
