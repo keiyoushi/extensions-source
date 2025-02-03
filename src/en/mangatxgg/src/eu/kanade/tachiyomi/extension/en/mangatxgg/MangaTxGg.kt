333b98724c7c8ed642e7773dca7395b97434194b
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-02-03 15:13:25 +0100

    Remove domains with no A record (#7481)

--- src/en/mangatxgg/src/eu/kanade/tachiyomi/extension/en/mangatxgg/MangaTxGg.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangatxgg
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class MangaTxGg : Madara("Manga Tx.gg (unoriginal)", "https://mangatx.gg", "en") {
-    override val useNewChapterEndpoint = false
-}
--- src/en/silentsky/src/eu/kanade/tachiyomi/extension/en/silentsky/SilentSky.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.silentsky
-
-import eu.kanade.tachiyomi.multisrc.foolslide.FoolSlide
-
-class SilentSky : FoolSlide("Silent Sky", "https://reader.silentsky-scans.net", "en")
