55184c08dd2d8dd3a7ba4230b64c91017bdb389c
Wind He <g311571057@gmail.com>
2025-02-03 14:46:37 +0800

    comicgrowl: add new source

---
+++ src/all/comicgrowl/src/eu/kanade/tachiyomi/extension/all/comicgrowl/ComicGrowl.kt
@@ -0,0 +1,45 @@
+package eu.kanade.tachiyomi.extension.all.comicgrowl
+
+import eu.kanade.tachiyomi.multisrc.gigaviewer.GigaViewer
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.jsoup.nodes.Element
+
+// TODO: get manga status
+// TODO: filter by status
+class ComicGrowl : GigaViewer(
+    "コミックグロウル",
+    "https://comic-growl.com",
+    "all",
+    "https://cdn-img.comic-growl.com/public/page",
+) {
+
+    override val publisher = "BUSHIROAD WORKS"
+
+    override val chapterListMode = 2 // No paid chapters
+
+    override val supportsLatest: Boolean = false
+
+    override val client: OkHttpClient =
+        super.client.newBuilder().addInterceptor(::imageIntercept).build()
+
+    override fun popularMangaRequest(page: Int): Request = GET(baseUrl, headers)
+
+    // Show only ongoing works
+    override fun popularMangaSelector(): String = "ul[class=\"lineup-list ongoing\"] > li > div > a"
+
+    override fun popularMangaFromElement(element: Element): SManga {
+        return SManga.create().apply {
+            title = element.select("h5").text()
+            thumbnail_url = element.select("div > img").attr("data-src")
+            setUrlWithoutDomain(element.attr("href"))
+        }
+    }
+
+    override fun getCollections(): List<Collection> = listOf(
+        Collection("連載作品", ""),
+        Collection("完結作品", ""),
+    )
+}
