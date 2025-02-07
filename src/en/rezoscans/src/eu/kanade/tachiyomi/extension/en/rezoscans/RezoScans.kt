cc02bb69eb198ec7df244f8481be9ed0c62647a1
mrtear <91820779+mrtear@users.noreply.github.com>
2025-02-07 21:02:35 +0500

    RezoScans: Fix Null Tag (#7549)
    RezoScan: Fix Null Tag

--- src/en/rezoscans/src/eu/kanade/tachiyomi/extension/en/rezoscans/RezoScans.kt
+++ src/en/rezoscans/src/eu/kanade/tachiyomi/extension/en/rezoscans/RezoScans.kt
@@ -6,10 +6,4 @@ class RezoScans : Keyoapp(
     "Rezo Scans",
     "https://rezoscans.com",
     "en",
-) {
-    override val descriptionSelector: String = "div.grid > div.overflow-hidden > p"
-    override val statusSelector: String = "div[alt=Status]"
-    override val authorSelector: String = "div[alt=Author]"
-    override val artistSelector: String = "div[alt=Artist]"
-    override val genreSelector: String = "div[alt='Series Type']"
-}
+)
