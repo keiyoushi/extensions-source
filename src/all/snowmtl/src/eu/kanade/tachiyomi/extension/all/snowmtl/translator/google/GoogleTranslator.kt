062f5b5ce5f097290363852381c05b206bc23a31
Chopper <156493704+choppeh@users.noreply.github.com>
2025-02-08 16:20:26 -0300

    Remove unused code

--- src/all/snowmtl/src/eu/kanade/tachiyomi/extension/all/snowmtl/translator/google/GoogleTranslator.kt
+++ src/all/snowmtl/src/eu/kanade/tachiyomi/extension/all/snowmtl/translator/google/GoogleTranslator.kt
@@ -3,9 +3,7 @@ package eu.kanade.tachiyomi.extension.all.snowmtl.translator.google
 import eu.kanade.tachiyomi.extension.all.snowmtl.translator.TranslatorEngine
 import eu.kanade.tachiyomi.network.GET
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.Headers
@@ -13,6 +11,8 @@ import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
+import uy.kohesive.injekt.injectLazy
 import java.io.IOException
 
 /**
@@ -28,9 +28,11 @@ class GoogleTranslator(private val client: OkHttpClient, private val headers: He
 
     override val capacity: Int = 5000
 
+    private val json: Json by injectLazy()
+
     override fun translate(from: String, to: String, text: String): String {
         val request = translateRequest(text, from, to)
-        return try { executeRequest(request, text, from, to).text } catch (_: Exception) { text }
+        return try { executeRequest(request) } catch (_: Exception) { text }
     }
 
     private fun translateRequest(text: String, from: String, to: String): Request {
@@ -62,71 +64,21 @@ class GoogleTranslator(private val client: OkHttpClient, private val headers: He
             }
     }
 
-    private fun executeRequest(request: Request, origin: String, from: String, to: String): Translated {
+    private fun executeRequest(request: Request): String {
         val response = client.newCall(request).execute()
 
         if (response.isSuccessful.not()) {
             throw IOException("Request failed: ${response.code}")
         }
 
-        val json = parseJson(response.body.string())
-
-        val translatedText = extractTranslatedText(json)
-        val pronunciation = extractPronunciation(json, translatedText)
-        val srcLang = json.jsonArray.getOrNull(2)?.jsonPrimitive?.content ?: from
-
-        return Translated(
-            from = srcLang,
-            to = to,
-            origin = origin,
-            text = translatedText,
-            pronunciation = pronunciation,
-            extraData = parseExtraData(json),
-            response = response,
-        )
+        return response.parseJson().let(::extractTranslatedText)
     }
 
-    private fun parseJson(jsonString: String): JsonElement {
-        return Json.parseToJsonElement(jsonString)
-    }
+    private fun Response.parseJson(): JsonElement = json.parseToJsonElement(this.body.string())
 
     private fun extractTranslatedText(data: JsonElement): String {
         return data.jsonArray[0].jsonArray.joinToString("") {
             it.jsonArray[0].jsonPrimitive.content
-        }
-    }
-
-    private fun extractPronunciation(data: JsonElement, translated: String): String {
-        return try {
-            data.jsonArray[0].jsonArray[0].jsonArray[1].jsonPrimitive.content
-        } catch (e: Exception) {
-            translated
-        }
-    }
-
-    private fun parseExtraData(data: JsonElement): Map<String, Any?> {
-        val parts = mapOf(
-            "0" to "translation",
-            "1" to "all-translations",
-            "2" to "original-language",
-            "5" to "possible-translations",
-            "6" to "confidence",
-            "7" to "possible-mistakes",
-            "8" to "language",
-            "11" to "synonyms",
-            "12" to "definitions",
-            "13" to "examples",
-            "14" to "see-also",
-        )
-
-        return parts.mapValues { (index, _) ->
-            data.jsonArray.getOrNull(index.toInt())?.let { element ->
-                when (element) {
-                    is JsonArray -> element.toList()
-                    is JsonPrimitive -> element.content
-                    else -> null
-                }
-            }
         }
     }
 }
