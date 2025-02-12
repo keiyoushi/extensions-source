a13f7fffe4d0aa027dfa72cdf48748f0b71a7409
Prem Kumar <60751338+prem-k-r@users.noreply.github.com>
2025-02-12 21:47:08 +0530

    Manga Gezgini(tr): Update Domain, and DoujinDesu (id): Fix typo in Genre (#7619)
    * update domain MangaGezgini
    
    * fix typo

--- src/tr/mangagezgini/src/eu/kanade/tachiyomi/extension/tr/mangagezgini/MangaGezgini.kt
+++ src/tr/mangagezgini/src/eu/kanade/tachiyomi/extension/tr/mangagezgini/MangaGezgini.kt
@@ -11,7 +11,7 @@ import java.util.Locale
 
 class MangaGezgini : Madara(
     "MangaGezgini",
-    "https://mangagezgini.vip",
+    "https://mangagezgini.site",
     "tr",
     SimpleDateFormat("dd/MM/yyyy", Locale.ROOT),
 ) {
