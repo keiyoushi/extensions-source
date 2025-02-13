7fc9eb9a0d7545b43be585e0e0f97e55f1a4c5c2
Creepler13 <denis.schlichting03@gmail.com>
2025-02-13 23:20:39 +0100

    Fix PageListParse

--- src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/HiveScans.kt
+++ src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/HiveScans.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.extension.en.infernalvoidscans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
+import eu.kanade.tachiyomi.source.model.Page
+import okhttp3.Response
 
 class HiveScans : Iken(
     "Hive Scans",
@@ -19,6 +21,14 @@ class HiveScans : Iken(
             chain.proceed(request.newBuilder().headers(headers).build())
         }
         .build()
+
+    val PageRegex = Regex("""https://storage.\w+.\w+/public//upload/series/(\w+)/(\w+)/(\d+).webp""")
+
+    override fun pageListParse(response: Response): List<Page> {
+        return PageRegex.findAll(response.body.string()).mapIndexed { idx, match ->
+            Page(idx, imageUrl = match.value)
+        }.toList()
+    }
 
     override fun headersBuilder() = super.headersBuilder()
         .set("Cache-Control", "max-age=0")
