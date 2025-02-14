bb785815a706bb11037bf55b0423ff7e193201fc
Creepler13 <denis.schlichting03@gmail.com>
2025-02-14 09:02:24 +0100

    change to json

--- src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/HiveScans.kt
+++ src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/HiveScans.kt
@@ -2,13 +2,19 @@ package eu.kanade.tachiyomi.extension.en.infernalvoidscans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
 import eu.kanade.tachiyomi.source.model.Page
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import okhttp3.Response
+import uy.kohesive.injekt.injectLazy
 
 class HiveScans : Iken(
     "Hive Scans",
     "en",
     "https://hivetoon.com",
 ) {
+
+    private val json by injectLazy<Json>()
 
     override val versionId = 2
 
@@ -22,12 +28,18 @@ class HiveScans : Iken(
         }
         .build()
 
-    val PageRegex = Regex("""https://storage.\w+.\w+/public//upload/series/(\w+)/(\w+)/(\d+).webp""")
+    private val pageRegex = Regex("""\\"images\\":(\[.*?]).*?nextChapter""")
+
+    @Serializable
+    class PageDTO(
+        val url: String,
+    )
 
     override fun pageListParse(response: Response): List<Page> {
-        return PageRegex.findAll(response.body.string()).mapIndexed { idx, match ->
-            Page(idx, imageUrl = match.value)
-        }.toList()
+        val pageDataArray = pageRegex.find(response.body.string())?.destructured?.component1()?.replace("\\", "") ?: return listOf()
+        return json.decodeFromString<List<PageDTO>>(pageDataArray).mapIndexed { idx, page ->
+            Page(idx, imageUrl = page.url)
+        }
     }
 
     override fun headersBuilder() = super.headersBuilder()
