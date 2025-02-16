5efe88196077ddc00d12b8ac258e6707de270f12
Creepler13 <denis.schlichting03@gmail.com>
2025-02-16 10:31:28 +0100

    Philliascans change to madara (#7648)
    * Philliascans change to madara
    
    * Bump versionID
    
    * lint
    
    * Update src/en/philiascans/src/eu/kanade/tachiyomi/extension/en/philiascans/PhiliaScans.kt
    
    ---------
    
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>

--- src/en/philiascans/src/eu/kanade/tachiyomi/extension/en/philiascans/PhiliaScans.kt
+++ src/en/philiascans/src/eu/kanade/tachiyomi/extension/en/philiascans/PhiliaScans.kt
@@ -1,9 +1,37 @@
 package eu.kanade.tachiyomi.extension.en.philiascans
 
-import eu.kanade.tachiyomi.multisrc.iken.Iken
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class PhiliaScans : Iken(
+class PhiliaScans : Madara(
     "Philia Scans",
-    "en",
     "https://philiascans.org",
-)
+    "en",
+    SimpleDateFormat("dd/MMM", Locale.US),
+) {
+    override val versionId: Int = 2
+    override val useNewChapterEndpoint = true
+
+    override fun popularMangaRequest(page: Int): Request {
+        val url = baseUrl.toHttpUrl().newBuilder().apply {
+            addQueryParameter("post_type", "wp-manga")
+            addQueryParameter("m_orderby", "views")
+        }.build()
+        return GET(url, headers)
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request {
+        val url = baseUrl.toHttpUrl().newBuilder().apply {
+            addQueryParameter("post_type", "wp-manga")
+            addQueryParameter("m_orderby", "latest")
+        }.build()
+        return GET(url, headers)
+    }
+
+    // needed to exclude paid chapters
+    override fun chapterListSelector(): String = """li.wp-manga-chapter:not(:has(a[href="#"]))"""
+}
