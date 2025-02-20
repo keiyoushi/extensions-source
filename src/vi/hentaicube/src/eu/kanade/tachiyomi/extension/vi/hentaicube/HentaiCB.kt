8a6ba3a98fae45d8de40ed0fbbe79ebad7a37086
Cuong-Tran <cuongtran.tm@gmail.com>
2025-02-20 21:45:13 +0700

    CBHentai: fix uri path changed & search failed with special characters (#7671)
    * CBHentai: fix uri path changed
    
    also fix altNameSelector
    
    * extract regex
    
    * move comment
    
    * fix search
    
    * HttpUrlBuilder

--- src/vi/hentaicube/src/eu/kanade/tachiyomi/extension/vi/hentaicube/HentaiCB.kt
+++ src/vi/hentaicube/src/eu/kanade/tachiyomi/extension/vi/hentaicube/HentaiCB.kt
@@ -1,8 +1,15 @@
 package eu.kanade.tachiyomi.extension.vi.hentaicube
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.asObservableSuccess
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.jsoup.nodes.Document
+import rx.Observable
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -11,6 +18,46 @@ class HentaiCB : Madara("CBHentai", "https://hentaicube.xyz", "vi", SimpleDateFo
     override val id: Long = 823638192569572166
 
     override val filterNonMangaItems = false
+
+    // Changed from 'manga' to 'read'
+    override val mangaSubString = "read"
+
+    override val altNameSelector = ".post-content_item:contains(Tên khác) .summary-content"
+
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> {
+        if (query.startsWith(URL_SEARCH_PREFIX)) {
+            val mangaUrl = baseUrl.toHttpUrl().newBuilder().apply {
+                addPathSegment("manga")
+                addPathSegment(query.substringAfter(URL_SEARCH_PREFIX))
+            }.build()
+            return client.newCall(GET(mangaUrl, headers))
+                .asObservableSuccess().map { response ->
+                    val manga = mangaDetailsParse(response).apply {
+                        setUrlWithoutDomain(mangaUrl.toString())
+                    }
+
+                    MangasPage(listOf(manga), false)
+                }
+        }
+
+        // Special characters causing search to fail
+        val queryFixed = query
+            .replace("–", "-")
+            .replace("’", "'")
+            .replace("“", "\"")
+            .replace("”", "\"")
+            .replace("…", "...")
+
+        return super.fetchSearchManga(page, queryFixed, filters)
+    }
+
+    private val oldMangaUrlRegex = Regex("""^$baseUrl/read/""")
+
+    // Change old entries from 'read' to 'manga'
+    override fun getMangaUrl(manga: SManga): String {
+        return super.getMangaUrl(manga)
+            .replace(oldMangaUrlRegex, "$baseUrl/manga/")
+    }
 
     override fun pageListParse(document: Document): List<Page> {
         return super.pageListParse(document).distinctBy { it.imageUrl }
