4374c9dfced508e08f292ba9c9a920b7fe9e07a6
JakeeLuwi <jeloubirad@gmail.com>
2025-02-24 01:27:55 +0800

    Manganato - Updated domain, selector, and filters
    Manganato essentially became an exact copy mangakakalot so all changes (except for URLs) are the same with mangakakalot

--- src/en/manganelo/src/eu/kanade/tachiyomi/extension/en/manganelo/Manganato.kt
+++ src/en/manganelo/src/eu/kanade/tachiyomi/extension/en/manganelo/Manganato.kt
@@ -2,20 +2,92 @@ package eu.kanade.tachiyomi.extension.en.manganelo
 
 import eu.kanade.tachiyomi.multisrc.mangabox.MangaBox
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.Page
+import okhttp3.Headers
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
-import java.text.SimpleDateFormat
-import java.util.Locale
 
-class Manganato : MangaBox("Manganato", "https://manganato.com", "en", SimpleDateFormat("MMM dd,yy", Locale.ENGLISH)) {
+class Manganato : MangaBox("Manganato", "https://www.natomanga.com", "en") {
     override val id: Long = 1024627298672457456
 
-    // Nelo's date format is part of the base class
-    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/genre-all/$page?type=topview", headers)
-    override fun popularMangaSelector() = "div.content-genres-item"
-    override val latestUrlPath = "genre-all/"
+    override fun headersBuilder(): Headers.Builder = super.headersBuilder().set("Referer", "https://www.natomanga.com/") // for covers
+    override val popularUrlPath = "manga-list/hot-manga?page="
+    override val latestUrlPath = "manga-list/latest-manga?page="
     override val simpleQueryPath = "search/story/"
-    override fun searchMangaSelector() = "div.search-story-item, div.content-genres-item"
-    override fun getAdvancedGenreFilters(): List<AdvGenre> = getGenreFilters()
-        .drop(1)
-        .map { AdvGenre(it.first, it.second) }
+    override fun searchMangaSelector() = "${super.searchMangaSelector()}, div.list-truyen-item-wrap"
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        return if (query.isNotBlank() && getAdvancedGenreFilters().isEmpty()) {
+            GET("$baseUrl/$simpleQueryPath${normalizeSearchQuery(query)}?page=$page", headers)
+        } else {
+            val url = baseUrl.toHttpUrl().newBuilder()
+            url.addPathSegment("genre")
+            url.addQueryParameter("page", page.toString())
+            filters.forEach { filter ->
+                when (filter) {
+                    is SortFilter -> url.addQueryParameter("type", filter.toUriPart())
+                    is StatusFilter -> url.addQueryParameter("state", filter.toUriPart())
+                    is GenreFilter -> url.addPathSegment(filter.toUriPart()!!)
+                    else -> {}
+                }
+            }
+
+            GET(url.build(), headers)
+        }
+    }
+
+    override val descriptionSelector = "div#contentBox"
+
+    override fun imageRequest(page: Page): Request {
+        return if (page.url.contains(baseUrl)) {
+            GET(page.imageUrl!!, headersBuilder().build())
+        } else { // Avoid 403 errors on non-migrated mangas
+            GET(page.imageUrl!!, headersBuilder().set("Referer", baseUrl).build())
+        }
+    }
+
+    override fun getGenreFilters(): Array<Pair<String?, String>> = arrayOf(
+        Pair("all", "ALL"),
+        Pair("action", "Action"),
+        Pair("adult", "Adult"),
+        Pair("adventure", "Adventure"),
+        Pair("comedy", "Comedy"),
+        Pair("cooking", "Cooking"),
+        Pair("doujinshi", "Doujinshi"),
+        Pair("drama", "Drama"),
+        Pair("ecchi", "Ecchi"),
+        Pair("fantasy", "Fantasy"),
+        Pair("gender-bender", "Gender bender"),
+        Pair("harem", "Harem"),
+        Pair("historical", "Historical"),
+        Pair("horror", "Horror"),
+        Pair("isekai", "Isekai"),
+        Pair("josei", "Josei"),
+        Pair("manhua", "Manhua"),
+        Pair("manhwa", "Manhwa"),
+        Pair("martial-arts", "Martial arts"),
+        Pair("mature", "Mature"),
+        Pair("mecha", "Mecha"),
+        Pair("medical", "Medical"),
+        Pair("mystery", "Mystery"),
+        Pair("one-shot", "One shot"),
+        Pair("psychological", "Psychological"),
+        Pair("romance", "Romance"),
+        Pair("school-life", "School life"),
+        Pair("sci-fi", "Sci fi"),
+        Pair("seinen", "Seinen"),
+        Pair("shoujo", "Shoujo"),
+        Pair("shoujo-ai", "Shoujo ai"),
+        Pair("shounen", "Shounen"),
+        Pair("shounen-ai", "Shounen ai"),
+        Pair("slice-of-life", "Slice of life"),
+        Pair("smut", "Smut"),
+        Pair("sports", "Sports"),
+        Pair("supernatural", "Supernatural"),
+        Pair("tragedy", "Tragedy"),
+        Pair("webtoons", "Webtoons"),
+        Pair("yaoi", "Yaoi"),
+        Pair("yuri", "Yuri"),
+    )
 }
