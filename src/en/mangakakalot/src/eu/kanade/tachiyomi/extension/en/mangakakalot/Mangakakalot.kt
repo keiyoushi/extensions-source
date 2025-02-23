c06941f4e8bb8dba49c5f83d9dba58c333418819
JakeeLuwi <jeloubirad@gmail.com>
2025-02-23 22:42:32 +0800

    Mangakakalot - updated domain paths

--- src/en/mangakakalot/src/eu/kanade/tachiyomi/extension/en/mangakakalot/Mangakakalot.kt
+++ src/en/mangakakalot/src/eu/kanade/tachiyomi/extension/en/mangakakalot/Mangakakalot.kt
@@ -1,10 +1,19 @@
 package eu.kanade.tachiyomi.extension.en.mangakakalot
 
 import eu.kanade.tachiyomi.multisrc.mangabox.MangaBox
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.Page
 import okhttp3.Headers
+import okhttp3.Request
 
-class Mangakakalot : MangaBox("Mangakakalot", "https://mangakakalot.com", "en") {
-    override fun headersBuilder(): Headers.Builder = super.headersBuilder().set("Referer", "https://manganato.com") // for covers
+class Mangakakalot : MangaBox("Mangakakalot", "https://www.mangakakalot.gg", "en") {
+    override fun headersBuilder(): Headers.Builder = super.headersBuilder().set("Referer", "https://www.mangakakalot.gg/") // for covers
+    override val popularUrlPath = "manga-list/hot-manga?page="
+    override val latestUrlPath = "manga-list/latest-manga?page="
     override val simpleQueryPath = "search/story/"
     override fun searchMangaSelector() = "${super.searchMangaSelector()}, div.list-truyen-item-wrap"
+
+    override fun imageRequest(page: Page): Request {
+        return GET(page.imageUrl!!, headersBuilder().build())
+    }
 }
