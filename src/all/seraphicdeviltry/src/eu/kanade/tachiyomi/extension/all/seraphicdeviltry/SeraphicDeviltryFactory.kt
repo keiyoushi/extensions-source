a8308e4f948e15897ec533295c2c8a3dfa0aefd5
Corvidae <39727721+C0rvidae@users.noreply.github.com>
2025-02-24 15:20:51 +0100

    Update src/all/seraphicdeviltry/src/eu/kanade/tachiyomi/extension/all/seraphicdeviltry/SeraphicDeviltryFactory.kt
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/all/seraphicdeviltry/src/eu/kanade/tachiyomi/extension/all/seraphicdeviltry/SeraphicDeviltryFactory.kt
+++ src/all/seraphicdeviltry/src/eu/kanade/tachiyomi/extension/all/seraphicdeviltry/SeraphicDeviltryFactory.kt
@@ -4,12 +4,8 @@ import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.SourceFactory
 
 class SeraphicDeviltryFactory : SourceFactory {
-
     override fun createSources(): List<Source> = listOf(
-        SeraphicDeviltryEn(),
-        SeraphicDeviltryEs(),
+        SeraphicDeviltry("en", "https://seraphic-deviltry.com"),
+        SeraphicDeviltry("es", "https://spanish.seraphic-deviltry.com"),
     )
 }
-
-class SeraphicDeviltryEn : SeraphicDeviltry("en", "https://seraphic-deviltry.com")
-class SeraphicDeviltryEs : SeraphicDeviltry("es", "https://spanish.seraphic-deviltry.com")
