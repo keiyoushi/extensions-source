1481dab5b00c095667a8141cf93df7e242b11c11
Corvidae <39727721+C0rvidae@users.noreply.github.com>
2025-02-24 15:07:58 +0100

    fix(Siyahmelek): Domain Change (#7777)
    Co-authored-by: Kon <k@a.a>

--- src/tr/siyahmelek/src/eu/kanade/tachiyomi/extension/tr/siyahmelek/Siyahmelek.kt
+++ src/tr/siyahmelek/src/eu/kanade/tachiyomi/extension/tr/siyahmelek/Siyahmelek.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 
 class Siyahmelek : Madara(
     "Gri Melek",
-    "https://grimelek.love",
+    "https://grimelek.online",
     "tr",
     SimpleDateFormat("dd MMM yyyy", Locale("tr")),
 ) {
