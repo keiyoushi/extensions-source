635a907294f516673874b011f519bf85e936020a
Angel <122130728+taos15@users.noreply.github.com>
2025-02-25 22:04:13 -0500

    Update Mangakakalot.kt
    Update base URL since mangakakalot has changed their website on their official Facebook, and discord to `www.mangakakalot.gg`

--- src/en/mangakakalot/src/eu/kanade/tachiyomi/extension/en/mangakakalot/Mangakakalot.kt
+++ src/en/mangakakalot/src/eu/kanade/tachiyomi/extension/en/mangakakalot/Mangakakalot.kt
@@ -3,8 +3,8 @@ package eu.kanade.tachiyomi.extension.en.mangakakalot
 import eu.kanade.tachiyomi.multisrc.mangabox.MangaBox
 import okhttp3.Headers
 
-class Mangakakalot : MangaBox("Mangakakalot", "https://mangakakalot.com", "en") {
-    override fun headersBuilder(): Headers.Builder = super.headersBuilder().set("Referer", "https://manganato.com") // for covers
+class Mangakakalot : MangaBox("Mangakakalot", "https://www.mangakakalot.gg", "en") {
+    override fun headersBuilder(): Headers.Builder = super.headersBuilder().set("Referer", "https://www.mangakakalot.gg") // for covers
     override val simpleQueryPath = "search/story/"
     override fun searchMangaSelector() = "${super.searchMangaSelector()}, div.list-truyen-item-wrap"
 }
