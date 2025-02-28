36bc0cb560fc693e2b6f26b1bbb4153f56acae91
bapeey <90949336+bapeey@users.noreply.github.com>
2025-02-28 13:13:47 -0500

    filter out chapters and update urls

--- lib-multisrc/lectormoe/src/eu/kanade/tachiyomi/multisrc/lectormoe/LectorMoe.kt
+++ lib-multisrc/lectormoe/src/eu/kanade/tachiyomi/multisrc/lectormoe/LectorMoe.kt
@@ -111,7 +111,10 @@ abstract class LectorMoe(
     override fun chapterListParse(response: Response): List<SChapter> {
         val result = json.decodeFromString<Data<SeriesDto>>(response.body.string())
         val seriesSlug = result.data.slug
-        return result.data.chapters?.map { it.toSChapter(seriesSlug) } ?: emptyList()
+        return result.data.chapters
+            ?.map { it.toSChapter(seriesSlug) }
+            ?.filter { it.date_upload < System.currentTimeMillis() }
+            ?: emptyList()
     }
 
     override fun pageListRequest(chapter: SChapter): Request {
