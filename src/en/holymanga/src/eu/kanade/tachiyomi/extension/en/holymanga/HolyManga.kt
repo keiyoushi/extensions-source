588fe07c9efde0809ed3b00c353037f3c9a74f50
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-03-07 17:33:56 +0100

    fix date parsing

--- src/en/holymanga/src/eu/kanade/tachiyomi/extension/en/holymanga/HolyManga.kt
+++ src/en/holymanga/src/eu/kanade/tachiyomi/extension/en/holymanga/HolyManga.kt
@@ -3,11 +3,14 @@ package eu.kanade.tachiyomi.extension.en.holymanga
 import eu.kanade.tachiyomi.multisrc.fmreader.FMReader
 import eu.kanade.tachiyomi.source.model.SChapter
 import org.jsoup.nodes.Element
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class HolyManga : FMReader(
     "HolyManga",
     "https://w34.holymanga.net",
     "en",
+    SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH),
 ) {
     override val versionId = 2
 
