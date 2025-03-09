e99b680c010820d7a3c04fe0f147a57d280b87a0
mrtear <91820779+mrtear@users.noreply.github.com>
2025-03-09 05:34:43 +0500

    Update Domains (#7970)
    * MilaSub
    
    * TraduccionesAmistosas
    
    * BlackoutComics
    
    * TaurusFansub
    
    * LxH: name + domain
    
    * milasub f
    
    * Taurus f
    
    * again
    
    * sorry guys
    
    * LxH: id

--- src/tr/milasub/src/eu/kanade/tachiyomi/extension/tr/milasub/MilaSub.kt
+++ src/tr/milasub/src/eu/kanade/tachiyomi/extension/tr/milasub/MilaSub.kt
@@ -8,7 +8,7 @@ import java.util.Locale
 
 class MilaSub : Madara(
     "MilaSub",
-    "https://www.milasub.co",
+    "https://www.milascans.com",
     "tr",
     dateFormat = SimpleDateFormat("d MMMM yyyy", Locale("tr")),
 ) {
