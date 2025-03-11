babf08ab888e926fce163451424ad5b11adcca8c
JakeeLuwi <jeloubirad@gmail.com>
2025-03-11 15:28:10 +0800

    lint

--- src/en/mangabat/src/eu/kanade/tachiyomi/extension/en/mangabat/Mangabat.kt
+++ src/en/mangabat/src/eu/kanade/tachiyomi/extension/en/mangabat/Mangabat.kt
@@ -2,6 +2,9 @@ package eu.kanade.tachiyomi.extension.en.mangabat
 
 import eu.kanade.tachiyomi.multisrc.mangabox.MangaBox
 
-class Mangabat : MangaBox("Mangabat", arrayOf(
-    "mangabats.com"
-), "en")
+class Mangabat : MangaBox(
+    "Mangabat",
+    arrayOf(
+        "mangabats.com"
+    ),
+    "en")
--- src/en/manganelo/src/eu/kanade/tachiyomi/extension/en/manganelo/Manganato.kt
+++ src/en/manganelo/src/eu/kanade/tachiyomi/extension/en/manganelo/Manganato.kt
@@ -2,10 +2,14 @@ package eu.kanade.tachiyomi.extension.en.manganelo
 
 import eu.kanade.tachiyomi.multisrc.mangabox.MangaBox
 
-class Manganato : MangaBox("Manganato", arrayOf(
-    "natomanga.com",
-    "nelomanga.com",
-    "manganato.gg"
-), "en") {
+class Manganato : MangaBox(
+    "Manganato",
+    arrayOf(
+        "natomanga.com",
+        "nelomanga.com",
+        "manganato.gg",
+    ),
+    "en") {
+
     override val id: Long = 1024627298672457456
 }
