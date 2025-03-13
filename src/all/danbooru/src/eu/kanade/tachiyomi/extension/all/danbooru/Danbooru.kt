585d47e4559c32369c6434643052886acf91cb66
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-03-13 18:09:32 +0500

    refctor

--- src/all/danbooru/src/eu/kanade/tachiyomi/extension/all/danbooru/Danbooru.kt
+++ src/all/danbooru/src/eu/kanade/tachiyomi/extension/all/danbooru/Danbooru.kt
@@ -7,8 +7,8 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.model.UpdateStrategy
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
+import keiyoushi.utils.parseAs
+import keiyoushi.utils.tryParse
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonPrimitive
@@ -18,7 +18,6 @@ import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import uy.kohesive.injekt.injectLazy
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -29,11 +28,9 @@ class Danbooru : ParsedHttpSource() {
     override val supportsLatest: Boolean = true
 
     override val client: OkHttpClient = network.cloudflareClient
-    private val json: Json by injectLazy()
 
-    private val dateFormat: SimpleDateFormat by lazy {
+    private val dateFormat =
         SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH)
-    }
 
     override fun popularMangaRequest(page: Int): Request =
         searchMangaRequest(page, "", FilterList())
@@ -127,14 +124,14 @@ class Danbooru : ParsedHttpSource() {
 
     override fun chapterListParse(response: Response): List<SChapter> = listOf(
         SChapter.create().apply {
-            val data = json.decodeFromString<JsonObject>(response.body.string())
+            val data = response.parseAs<JsonObject>()
 
             val id = data["id"]!!.jsonPrimitive.content
             val createdAt = data["created_at"]?.jsonPrimitive?.content
 
             url = "/pools/$id"
             name = "Oneshot"
-            date_upload = createdAt?.let(::parseTimestamp) ?: 0
+            date_upload = dateFormat.tryParse(createdAt)
             chapter_number = 0F
         },
     )
@@ -149,8 +146,7 @@ class Danbooru : ParsedHttpSource() {
         GET("$baseUrl${chapter.url}.json?only=post_ids", headers)
 
     override fun pageListParse(response: Response): List<Page> =
-        json.decodeFromString<JsonObject>(response.body.string())
-            .get("post_ids")?.jsonArray
+        response.parseAs<JsonObject>()["post_ids"]?.jsonArray
             ?.map { it.jsonPrimitive.content }
             ?.mapIndexed { i, id -> Page(index = i, url = "/posts/$id") }
             ?: emptyList()
@@ -162,8 +158,7 @@ class Danbooru : ParsedHttpSource() {
         GET("$baseUrl${page.url}.json?only=file_url", headers)
 
     override fun imageUrlParse(response: Response): String =
-        json.decodeFromString<JsonObject>(response.body.string())
-            .get("file_url")!!.jsonPrimitive.content
+        response.parseAs<JsonObject>()["file_url"]!!.jsonPrimitive.content
 
     override fun imageUrlParse(document: Document): String =
         throw IllegalStateException("Not used")
@@ -180,7 +175,4 @@ class Danbooru : ParsedHttpSource() {
             FilterOrder(),
         ),
     )
-
-    private fun parseTimestamp(string: String): Long? =
-        runCatching { dateFormat.parse(string)?.time!! }.getOrNull()
 }
