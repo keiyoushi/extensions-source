2e1c464eb27f789e9d8e6cd9bff0b80623728554
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-03-14 19:53:36 +0500

    fix

--- src/ja/mangamura/src/eu/kanade/tachiyomi/extension/ja/mangamura/MangaMura.kt
+++ src/ja/mangamura/src/eu/kanade/tachiyomi/extension/ja/mangamura/MangaMura.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.extension.ja.mangamura
 
 import eu.kanade.tachiyomi.multisrc.mangareader.MangaReader
+import eu.kanade.tachiyomi.source.model.FilterList
+import okhttp3.Request
 
 class MangaMura : MangaReader(
     "Manga Mura",
@@ -11,5 +13,31 @@ class MangaMura : MangaReader(
 
     override fun getAjaxUrl(id: String): String {
         return "$baseUrl/json/chapter?mode=vertical&id=$id"
+    }
+
+    override val searchPathSegment = ""
+    override val searchKeyword = "q"
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val request = super.searchMangaRequest(page, query, filters)
+
+        // avoid 302
+        val newUrl = request.url.newBuilder()
+            .addPathSegment("")
+            .build()
+
+        return request.newBuilder()
+            .url(newUrl)
+            .build()
+    }
+
+    override fun getFilterList(): FilterList {
+        return FilterList(
+            Note,
+            TypeFilter(),
+            StatusFilter(),
+            LanguageFilter(),
+            SortFilter(),
+        )
     }
 }
--- src/ja/mangamura/src/eu/kanade/tachiyomi/extension/ja/mangamura/MangaMuraFilters.kt
+++ src/ja/mangamura/src/eu/kanade/tachiyomi/extension/ja/mangamura/MangaMuraFilters.kt
@@ -1,110 +1,73 @@
 package eu.kanade.tachiyomi.extension.ja.mangamura
 
-import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.multisrc.mangareader.MangaReader.UriMultiSelectFilter
+import eu.kanade.tachiyomi.multisrc.mangareader.MangaReader.UriPartFilter
 
-object Note : Filter.Header("NOTE: Ignored if using text search!")
+class TypeFilter : UriPartFilter(
+    "タイプ",
+    "type",
+    arrayOf(
+        "全て" to "all",
+        "Raw Manga" to "Raw Manga",
+        "BLコミック" to "BLコミック",
+        "TLコミック" to "TLコミック",
+        "オトナコミック" to "オトナコミック",
+        "女性マンガ" to "女性マンガ",
+        "少女マンガ" to "少女マンガ",
+        "少年マンガ" to "少年マンガ",
+        "青年マンガ" to "青年マンガ",
+    ),
+)
 
-sealed class Select(
-    name: String,
-    val param: String,
-    values: Array<String>,
-) : Filter.Select<String>(name, values) {
-    open val selection: String
-        get() = if (state == 0) "" else state.toString()
-}
+class StatusFilter : UriPartFilter(
+    "地位",
+    "status",
+    arrayOf(
+        "全て" to "all",
+        "Publishing" to "Publishing",
+        "Finished" to "Finished",
+    ),
+)
 
-class TypeFilter(
-    values: Array<String> = types.keys.toTypedArray(),
-) : Select("タイプ", "type", values) {
-    override val selection: String
-        get() = types[values[state]]!!
+class LanguageFilter : UriPartFilter(
+    "言語",
+    "language",
+    arrayOf(
+        "全て" to "all",
+        "Japanese" to "ja",
+        "English" to "en",
+    ),
+)
 
-    companion object {
-        private val types = mapOf(
-            "全て" to "all",
-            "Raw Manga" to "Raw Manga",
-            "BLコミック" to "BLコミック",
-            "TLコミック" to "TLコミック",
-            "オトナコミック" to "オトナコミック",
-            "女性マンガ" to "女性マンガ",
-            "少女マンガ" to "少女マンガ",
-            "少年マンガ" to "少年マンガ",
-            "青年マンガ" to "青年マンガ",
-        )
-    }
-}
+class SortFilter : UriPartFilter(
+    "選別",
+    "sort",
+    arrayOf(
+        "デフォルト" to "default",
+        "最新の更新" to "latest-updated",
+        "最も見られました" to "most-viewed",
+        "Title [A-Z]" to "title-az",
+        "Title [Z-A]" to "title-za",
+    ),
+)
 
-class StatusFilter(
-    values: Array<String> = statuses.keys.toTypedArray(),
-) : Select("地位", "status", values) {
-    override val selection: String
-        get() = statuses[values[state]]!!
-
-    companion object {
-        private val statuses = mapOf(
-            "全て" to "all",
-            "Publishing" to "Publishing",
-            "Finished" to "Finished",
-        )
-    }
-}
-
-class LanguageFilter(
-    values: Array<String> = languages.keys.toTypedArray(),
-) : Select("言語", "language", values) {
-    override val selection: String
-        get() = languages[values[state]]!!
-
-    companion object {
-        private val languages = mapOf(
-            "全て" to "all",
-            "Japanese" to "ja",
-            "English" to "en",
-        )
-    }
-}
-
-class SortFilter(
-    values: Array<String> = sort.keys.toTypedArray(),
-) : Select("選別", "sort", values) {
-    override val selection: String
-        get() = sort[values[state]]!!
-
-    companion object {
-        private val sort = mapOf(
-            "デフォルト" to "default",
-            "最新の更新" to "latest-updated",
-            "最も見られました" to "most-viewed",
-            "Title [A-Z]" to "title-az",
-            "Title [Z-A]" to "title-za",
-        )
-    }
-}
-
-class Genre(name: String, val id: String) : Filter.CheckBox(name)
-
-class GenresFilter(
-    values: List<Genre> = genres,
-) : Filter.Group<Genre>("ジャンル", values) {
-    val param = "genre[]"
-
-    companion object {
-        private val genres: List<Genre>
-            get() = listOf(
-                Genre("アクション", "55"),
-                Genre("エッチ", "15706"),
-                Genre("コメディ", "91"),
-                Genre("ドラマ", "56"),
-                Genre("ハーレム", "20"),
-                Genre("ファンタジー", "1"),
-                Genre("冒険", "54"),
-                Genre("悪魔", "6820"),
-                Genre("武道", "1064"),
-                Genre("歴史的", "9600"),
-                Genre("警察・特殊部隊", "6089"),
-                Genre("車･バイク", "4329"),
-                Genre("音楽", "473"),
-                Genre("魔法", "1416"),
-            )
-    }
-}
+class GenreFilter : UriMultiSelectFilter(
+    "ジャンル",
+    "genre[]",
+    arrayOf(
+        "アクション" to "55",
+        "エッチ" to "15706",
+        "コメディ" to "91",
+        "ドラマ" to "56",
+        "ハーレム" to "20",
+        "ファンタジー" to "1",
+        "冒険" to "54",
+        "悪魔" to "6820",
+        "武道" to "1064",
+        "歴史的" to "9600",
+        "警察・特殊部隊" to "6089",
+        "車･バイク" to "4329",
+        "音楽" to "473",
+        "魔法" to "1416",
+    ),
+)
