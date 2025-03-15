4c8c4e511d1860530dd323fe11c91f8c3198e3ed
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-03-15 06:56:12 +0100

    Remove extensions redirecting to other extensions (#8076)
    * Remove Apolltoons (replaced by Mundo Manhwa)
    
    * Remove Arctic Scan (replaced by Yushuke Mangas)
    
    * chore: update comment bacakomik.co -> bacakomik.one
    
    * Remove Black Scans (replaced by Yugen Mangás)
    
    * Remove KomikIndo.info (replaced by Mangasusu)
    
    * Remove Snow Scans (replaced by Galaxy)
    
    * Remove Vex Manga (replaced by Vortex Scans)
    
    * Remove MangaSaki (replaced by Mangasail)
    
    * Remove Xmanhwa (replaced by ManhwaDen)

--- src/es/apolltoons/src/eu/kanade/tachiyomi/extension/es/apolltoons/Apolltoons.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.es.apolltoons
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class Apolltoons : Madara("Apolltoons", "https://apolltoons.xyz", "es", SimpleDateFormat("dd MMMMM, yyyy", Locale("es")))
