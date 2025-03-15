c433d5815362b93b59a7eadac1991fa365eecf47
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-03-15 06:55:56 +0100

    Remove extensions without A record (#8075)
    Hreads
    Manga 18h
    Mangá Ninja
    Mangaxico
    Mangayaro
    Otsugami
    ReadComic.Top
    Tyrant Scans
    Wonderland Scan

--- src/en/manga18h/src/eu/kanade/tachiyomi/extension/en/manga18h/Manga18h.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.manga18h
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class Manga18h : Madara("Manga 18h", "https://manga18h.com", "en") {
-    override val useNewChapterEndpoint = false
-}
