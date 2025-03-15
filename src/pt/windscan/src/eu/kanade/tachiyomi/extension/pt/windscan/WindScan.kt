5ec8988362c1c76212ff254f8e2ecf6444335923
Chopper <156493704+choppeh@users.noreply.github.com>
2025-03-15 20:12:41 -0300

    Add WindScan

---
+++ src/pt/windscan/src/eu/kanade/tachiyomi/extension/pt/windscan/WindScan.kt
@@ -0,0 +1,17 @@
+package eu.kanade.tachiyomi.extension.pt.windscan
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class WindScan : Madara(
+    "Wind Scan",
+    "https://windscan.xyz",
+    "pt-BR",
+    dateFormat = SimpleDateFormat("MMMMM dd, yyyy", Locale("pt", "BR")),
+) {
+    override val client = super.client.newBuilder()
+        .rateLimit(2)
+        .build()
+}
