5254f93a0394203e7d50aac84d140e09a261548c
Chopper <156493704+choppeh@users.noreply.github.com>
2025-03-16 16:32:56 -0300

    Update SamuraiScan domain

--- src/es/manhuaonline/src/eu/kanade/tachiyomi/extension/es/manhuaonline/SamuraiScan.kt
+++ src/es/manhuaonline/src/eu/kanade/tachiyomi/extension/es/manhuaonline/SamuraiScan.kt
@@ -3,12 +3,13 @@ package eu.kanade.tachiyomi.extension.es.manhuaonline
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class SamuraiScan : Madara(
     "SamuraiScan",
-    "https://samuraiscan.cookni.net",
+    "https://samurai.wordoco.com",
     "es",
     SimpleDateFormat("dd MMMM, yyyy", Locale("es")),
 ) {
@@ -17,11 +18,24 @@ class SamuraiScan : Madara(
     override val useLoadMoreRequest = LoadMoreStrategy.Never
     override val useNewChapterEndpoint = true
 
-    override val mangaSubString = "rd"
+    override val mangaSubString = "leer"
 
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(3)
         .build()
 
     override val mangaDetailsSelectorDescription = "div.summary_content div.manga-summary"
+
+    override fun pageListParse(document: Document) = super.pageListParse(document).map {
+        it.apply {
+            imageUrl = imageUrl
+                ?.replace(SSL_REGEX, "https")
+                ?.replace(WWW_REGEX, "")
+        }
+    }
+
+    companion object {
+        val SSL_REGEX = """https?""".toRegex()
+        val WWW_REGEX = """[wW]{3}\.""".toRegex()
+    }
 }
