15e48c2d52ac7f4d3073e7551a672467396c0b5d
Creepler13 <denis.schlichting03@gmail.com>
2025-03-17 19:20:43 +0100

    remove dto

--- lib-multisrc/iken/src/eu/kanade/tachiyomi/multisrc/iken/Dto.kt
+++ lib-multisrc/iken/src/eu/kanade/tachiyomi/multisrc/iken/Dto.kt
@@ -93,7 +93,6 @@ class Chapter(
     private val id: Int,
     private val slug: String,
     private val number: JsonPrimitive,
-    private val createdBy: Name,
     private val createdAt: String,
     private val chapterStatus: String,
     private val isAccessible: Boolean,
