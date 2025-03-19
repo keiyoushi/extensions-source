1d16eae9a362a9bc24e7d7d01e87e74b868606b9
Haxel Caribou <haxelcaribou@proton.me>
2025-03-18 22:04:13 -0700

    Implement suggested maintainer edits
    Update src/en/egscomics/src/eu/kanade/tachiyomi/extension/en/egscomics/ElGoonishShive.kt
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/en/egscomics/src/eu/kanade/tachiyomi/extension/en/egscomics/ElGoonishShive.kt
+++ src/en/egscomics/src/eu/kanade/tachiyomi/extension/en/egscomics/ElGoonishShive.kt
@@ -6,6 +6,7 @@ import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import keiyoushi.utils.tryParse
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
@@ -35,8 +36,9 @@ class ElGoonishShive : ParsedHttpSource() {
                     "both real life and bizarre, supernatural situations. \n\n" +
                     "It is a comedy mixed with drama and is recommended for audiences thirteen " +
                     "and older."
-
-                thumbnail_url = "https://static.tumblr.com/8cee5e83d26a8a96ad5e51b67f2e340e/j8ipbno/fXFoj0zh9/tumblr_static_1f2fhwjyya74gsgs888g8k880.png"
+                thumbnail_url =
+                    "https://static.tumblr.com/8cee5e83d26a8a96ad5e51b67f2e340e/j8ipbno/fXFoj0zh9/tumblr_static_1f2fhwjyya74gsgs888g8k880.png"
+                initialized = true
             },
         )
 
@@ -51,10 +53,11 @@ class ElGoonishShive : ParsedHttpSource() {
                     "both real life and bizarre, supernatural situations. \n\n" +
                     "It is a comedy mixed with drama and is recommended for audiences thirteen " +
                     "and older. \n\n" +
-                    "EGS:NP is a subsection with short stories that generally aren't canon unless" +
-                    "stated"
-
-                thumbnail_url = "https://static.tumblr.com/8cee5e83d26a8a96ad5e51b67f2e340e/j8ipbno/fXFoj0zh9/tumblr_static_1f2fhwjyya74gsgs888g8k880.png"
+                    "EGS:NP is a subsection with short stories that generally aren't canon " +
+                    "unless stated"
+                thumbnail_url =
+                    "https://static.tumblr.com/8cee5e83d26a8a96ad5e51b67f2e340e/j8ipbno/fXFoj0zh9/tumblr_static_1f2fhwjyya74gsgs888g8k880.png"
+                initialized = true
             },
         )
 
@@ -71,15 +74,20 @@ class ElGoonishShive : ParsedHttpSource() {
                     "and older. \n\n" + "" +
                     "The Sketchbook section is full of one-shot gags, sketches, comics that " +
                     "don't fit elsewhere."
-
-                thumbnail_url = "https://static.tumblr.com/8cee5e83d26a8a96ad5e51b67f2e340e/j8ipbno/fXFoj0zh9/tumblr_static_1f2fhwjyya74gsgs888g8k880.png"
+                thumbnail_url =
+                    "https://static.tumblr.com/8cee5e83d26a8a96ad5e51b67f2e340e/j8ipbno/fXFoj0zh9/tumblr_static_1f2fhwjyya74gsgs888g8k880.png"
+                initialized = true
             },
         )
 
         return Observable.just(MangasPage(manga, false))
     }
 
-    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> = Observable.just(MangasPage(emptyList(), false))
+    override fun fetchSearchManga(
+        page: Int,
+        query: String,
+        filters: FilterList,
+    ): Observable<MangasPage> = Observable.just(MangasPage(emptyList(), false))
 
     override fun fetchMangaDetails(manga: SManga): Observable<SManga> {
         return Observable.just(manga)
@@ -97,40 +105,29 @@ class ElGoonishShive : ParsedHttpSource() {
             chapter_number = element.previousElementSiblings().size.toFloat()
             setUrlWithoutDomain("/" + element.attr("value"))
             name = element.text().split(" - ", limit = 2).last()
-            date_upload = dateFormat.parse(element.text().split(" - ", limit = 2).first())?.time ?: 0L
+            date_upload = dateFormat.tryParse(element.text().split(" - ", limit = 2).first())
         }
     }
 
     override fun pageListParse(document: Document): List<Page> =
-        document.select("#cc-comic").mapIndexed { i, element -> Page(i, "", baseUrl + element.attr("src").substring(25)) }
+        document.select("#cc-comic").mapIndexed { i, element ->
+            Page(i, imageUrl = element.absUrl("src"))
+        }
 
     // <editor-fold desc="Not Used">
     override fun imageUrlParse(document: Document): String = throw UnsupportedOperationException()
-
     override fun popularMangaSelector(): String = throw UnsupportedOperationException()
-
     override fun searchMangaFromElement(element: Element): SManga = throw UnsupportedOperationException()
-
     override fun searchMangaNextPageSelector(): String = throw UnsupportedOperationException()
-
     override fun searchMangaSelector(): String = throw UnsupportedOperationException()
-
     override fun popularMangaRequest(page: Int): Request = throw UnsupportedOperationException()
-
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request = throw UnsupportedOperationException()
-
     override fun popularMangaNextPageSelector(): String = throw UnsupportedOperationException()
-
     override fun popularMangaFromElement(element: Element): SManga = throw UnsupportedOperationException()
-
     override fun mangaDetailsParse(document: Document): SManga = throw UnsupportedOperationException()
-
     override fun latestUpdatesNextPageSelector(): String = throw UnsupportedOperationException()
-
     override fun latestUpdatesFromElement(element: Element): SManga = throw UnsupportedOperationException()
-
     override fun latestUpdatesRequest(page: Int): Request = throw UnsupportedOperationException()
-
     override fun latestUpdatesSelector(): String = throw UnsupportedOperationException()
     // </editor-fold>
 }
