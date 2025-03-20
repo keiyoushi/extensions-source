42040bba0b9cc9be24667684bba19261f05250c3
Chopper <156493704+choppeh@users.noreply.github.com>
2025-03-20 03:36:36 -0300

    VortexScans: Fix popularManga url (#8136)
    Fix popularManga url

--- src/en/arvenscans/src/eu/kanade/tachiyomi/extension/en/arvenscans/VortexScans.kt
+++ src/en/arvenscans/src/eu/kanade/tachiyomi/extension/en/arvenscans/VortexScans.kt
@@ -1,9 +1,12 @@
 package eu.kanade.tachiyomi.extension.en.arvenscans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
+import eu.kanade.tachiyomi.network.GET
 
 class VortexScans : Iken(
     "Vortex Scans",
     "en",
     "https://vortexscans.org",
-)
+) {
+    override fun popularMangaRequest(page: Int) = GET(baseUrl, headers)
+}
