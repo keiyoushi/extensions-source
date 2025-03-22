3d6898d14c084683cb214f53b092b666d35b92ff
Pandu <144518692+arasea2@users.noreply.github.com>
2025-03-22 15:58:36 +0700

    Noromax: Update Domain

--- src/id/noromax/src/eu/kanade/tachiyomi/extension/id/noromax/Noromax.kt
+++ src/id/noromax/src/eu/kanade/tachiyomi/extension/id/noromax/Noromax.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.extension.id.noromax
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 
-class Noromax : MangaThemesia("Noromax", "https://noromax.my.id", "id", "/Komik") {
+class Noromax : MangaThemesia("Noromax", "https://noromax01.my.id", "id", "/Komik") {
 
     // Site changed from ZeistManga to MangaThemesia
     override val versionId = 2
