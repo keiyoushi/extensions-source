81d1b997386b515063dc16044234c2d19d703153
Chopper <156493704+choppeh@users.noreply.github.com>
2025-03-23 12:04:28 -0300

    SussyToons: Fix source (#8155)
    * Fix popular manga and latest manga
    
    * Fix details and chapter
    
    * Fix search
    
    * Bump version

--- src/pt/sussyscan/src/eu/kanade/tachiyomi/extension/pt/sussyscan/SussyToonsDto.kt
+++ src/pt/sussyscan/src/eu/kanade/tachiyomi/extension/pt/sussyscan/SussyToonsDto.kt
@@ -1,12 +1,14 @@
 package eu.kanade.tachiyomi.extension.pt.sussyscan
 
+import eu.kanade.tachiyomi.extension.pt.sussyscan.SussyToons.Companion.CDN_URL
 import eu.kanade.tachiyomi.source.model.SManga
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonNames
+import org.jsoup.Jsoup
 
 @Serializable
-data class WrapperDto<T>(
+class ResultDto<T>(
     @SerialName("pagina")
     val currentPage: Int = 0,
     @SerialName("totalPaginas")
@@ -17,6 +19,20 @@ data class WrapperDto<T>(
     val results: T get() = resultados
 
     fun hasNextPage() = currentPage < lastPage
+
+    fun toSMangaList() = (results as List<MangaDto>)
+        .filterNot { it.slug.isNullOrBlank() }.map { it.toSManga() }
+}
+
+@Serializable
+class WrapperDto(
+    @SerialName("dataTop")
+    val popular: ResultDto<List<MangaDto>>?,
+    @JsonNames("atualizacoesInicial")
+    private val dataLatest: ResultDto<List<MangaDto>>?,
+
+) {
+    val latest: ResultDto<List<MangaDto>> get() = dataLatest!!
 }
 
 @Serializable
@@ -35,7 +51,38 @@ class MangaDto(
     val status: MangaStatus,
     @SerialName("scan_id")
     val scanId: Int,
+    @SerialName("tags")
+    val genres: List<Genre>,
 ) {
+
+    fun toSManga(): SManga {
+        val sManga = SManga.create().apply {
+            title = name
+            thumbnail_url = thumbnail?.let {
+                when {
+                    it.startsWith("http") -> thumbnail
+                    else -> "$CDN_URL/scans/$scanId/obras/${this@MangaDto.id}/$thumbnail"
+                }
+            }
+            initialized = true
+            url = "/obra/${this@MangaDto.id}/${this@MangaDto.slug}"
+            genre = genres.joinToString()
+        }
+
+        description?.let { Jsoup.parseBodyFragment(it).let { sManga.description = it.text() } }
+        sManga.status = status.toStatus()
+
+        return sManga
+    }
+
+    @Serializable
+    class Genre(
+        @SerialName("tag_nome")
+        val value: String,
+    ) {
+        override fun toString(): String = value
+    }
+
     @Serializable
     class MangaStatus(
         @SerialName("stt_nome")
