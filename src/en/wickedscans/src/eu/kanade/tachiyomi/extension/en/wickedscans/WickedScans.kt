2efbddfae82b0f1121c0ee342fff6be6e8bcc0cc
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-03-23 16:05:03 +0100

    Remove broken extensions/sites (#8167)
    * Remove HerenScan (403)
    
    * Remove Its Your Right Manhua (403)
    
    * Remove PrinceEdiciones (403)
    
    * Remove Manhwas.es (403)
    
    * Remove Yaoi Manga (403)
    
    * Remove Kings Of Darkness (404)
    
    * Remove ManhwaFreak.xyz (404)
    
    * Remove HenChan (404)
    
    * Remove Yaoi Flix (404)
    
    * Remove Asura Scans.us (unoriginal) (disabled)
    
    * Remove Bento Manga (404)
    
    * Remove MR Yaoi Fansub (expired)
    
    * Remove Aurora Scan (empty)
    
    * Remove Varna Scan (unrelated)
    
    * Remove Guilda Tier Draw (disabled / invite only)
    
    * Remove Norte Rose Scan (under maintenance)
    
    * Remove Nox Scans (disabled)
    
    * Remove Novelle Leggere (unrelated)
    
    * Remove IchiroManga (disabled)
    
    * Remove LeerMangasXYZ (unrelated)
    
    * Remove Wicked Scans (526, invalid SSL certificate)

--- src/en/wickedscans/src/eu/kanade/tachiyomi/extension/en/wickedscans/WickedScans.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.wickedscans
-
-import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
-
-class WickedScans : Keyoapp("Wicked Scans", "https://wickedscans.org", "en")
