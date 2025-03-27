9b1a4ef8821127122462424af28df11a9b77f7af
mrtear <91820779+mrtear@users.noreply.github.com>
2025-03-28 01:07:18 +0500

    Arvenscans & Aeinscans: Update URL & Fix Details (#8216)
    * Arven: Domain Update
    
    * Aein: Fix null tag & details

--- src/en/arvencomics/src/eu/kanade/tachiyomi/extension/en/arvencomics/ArvenComics.kt
+++ src/en/arvencomics/src/eu/kanade/tachiyomi/extension/en/arvencomics/ArvenComics.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
 
 class ArvenComics : Keyoapp(
     "Arven Scans",
-    "https://arvencomics.com",
+    "https://arvencomic.net",
     "en",
 ) {
     // migrated from Mangathemesia to Keyoapp
