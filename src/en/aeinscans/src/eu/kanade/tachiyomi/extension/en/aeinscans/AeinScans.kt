374f21ccdb757afbce1f4ae4a46cab911d5744c4
mrtear <91820779+mrtear@users.noreply.github.com>
2025-03-28 01:07:18 +0500

    Arvenscans & Aeinscans: Update URL & Fix Details (#8216)
    * Arven: Domain Update
    
    * Aein: Fix null tag & details

--- src/en/aeinscans/src/eu/kanade/tachiyomi/extension/en/aeinscans/AeinScans.kt
+++ src/en/aeinscans/src/eu/kanade/tachiyomi/extension/en/aeinscans/AeinScans.kt
@@ -6,4 +6,10 @@ class AeinScans : Keyoapp(
     "Aein Scans",
     "https://aeinscans.com",
     "en",
-)
+) {
+    override val descriptionSelector: String = "div.grid > div.overflow-hidden > p"
+    override val statusSelector: String = "div[alt=Status]"
+    override val authorSelector: String = "div[alt=Author]"
+    override val artistSelector: String = "div[alt=Artist]"
+    override val genreSelector: String = "div[alt='Series Type']"
+}
