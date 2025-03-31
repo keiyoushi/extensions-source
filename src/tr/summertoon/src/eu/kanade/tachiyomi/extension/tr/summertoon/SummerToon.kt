9f6fd0205992fc236f45390e041a83470128878a
Chopper <156493704+choppeh@users.noreply.github.com>
2025-03-31 11:41:25 -0300

    Update domains (#8297)
    Update domain

--- src/tr/summertoon/src/eu/kanade/tachiyomi/extension/tr/summertoon/SummerToon.kt
+++ src/tr/summertoon/src/eu/kanade/tachiyomi/extension/tr/summertoon/SummerToon.kt
@@ -1,13 +1,13 @@
 package eu.kanade.tachiyomi.extension.tr.summertoon
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class SummerToon : MangaThemesia(
+class SummerToon : Madara(
     "SummerToon",
-    "https://summertoon.co",
+    "https://summertoons.com",
     "tr",
     dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("tr")),
 ) {
@@ -15,6 +15,7 @@ class SummerToon : MangaThemesia(
         .rateLimit(1, 1)
         .build()
 
-    override val seriesStatusSelector = ".tsinfo .imptdt:contains(Durum) i"
-    override val seriesAuthorSelector = ".fmed b:contains(Yazar)+span"
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+
+    override val chapterUrlSelector = "div + a"
 }
