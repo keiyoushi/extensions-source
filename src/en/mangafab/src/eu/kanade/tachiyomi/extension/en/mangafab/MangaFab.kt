3697a83a2a153f2d08c7cdda1fa96c9a5a8065e0
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-03-31 16:40:54 +0200

    Revert removal of some MadTheme sites (#8290)
    * Revert "MadTheme: Fix MangaForest, remove redirect domains (#14661)"
    
    This reverts commit 42d9c0b1184ea23679bb7471696c3d7b1bebd36e.
    
    * Switch to new multisrc structure

---
+++ src/en/mangafab/src/eu/kanade/tachiyomi/extension/en/mangafab/MangaFab.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.mangafab
+
+import eu.kanade.tachiyomi.multisrc.madtheme.MadTheme
+
+class MangaFab : MadTheme("MangaFab", "https://mangafab.com", "en")
---
+++ src/en/mangasaga/src/eu/kanade/tachiyomi/extension/en/mangasaga/MangaSaga.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.mangasaga
+
+import eu.kanade.tachiyomi.multisrc.madtheme.MadTheme
+
+class MangaSaga : MadTheme("MangaSaga", "https://mangasaga.com", "en")
