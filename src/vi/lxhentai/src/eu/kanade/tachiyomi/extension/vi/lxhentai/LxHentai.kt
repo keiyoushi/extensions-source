fed916f54ef360bcea41a168e166e4aa1981c045
are-are-are <62763969+dejavui@users.noreply.github.com>
2025-04-06 20:10:12 +0700

    LXManga update domain

--- src/vi/lxhentai/src/eu/kanade/tachiyomi/extension/vi/lxhentai/LxHentai.kt
+++ src/vi/lxhentai/src/eu/kanade/tachiyomi/extension/vi/lxhentai/LxHentai.kt
@@ -29,7 +29,7 @@ class LxHentai : ParsedHttpSource(), ConfigurableSource {
 
     override val id = 6495630445796108150
 
-    private val defaultBaseUrl = "https://lxmanga.art"
+    private val defaultBaseUrl = "https://lxmanga.sbs"
 
     override val baseUrl by lazy { getPrefBaseUrl() }
 
