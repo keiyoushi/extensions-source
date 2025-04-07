195c19b708c3c24d04ab31c06aaf97a6ba27f98c
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-04-07 17:41:58 +0500

    Rework the Dynasty Source (#8326)
    * exact match
    
    * single search
    
    * single search fixed
    
    * rm
    
    * thumbnail fast
    
    * details
    
    * chapter details in browse
    
    * chapter details in browse
    
    * man
    
    * chapters and page
    
    * chapters and page
    
    * cleanup
    
    * small cleanup
    
    * enforce type filter manually
    
    * enforce type filter manually
    
    * reversed list or not
    
    * newlines in description adjustment
    
    * pairing filter
    
    * remove single fetching logic
    
    not worth it
    
    * add other dynasty factories for legacy compatibility
    
    * cleanup
    
    * fix status and header being null in some cases
    
    * update covers
    
    * status
    
    * unused
    
    * inline function
    
    * selector, reorder etc
    
    * lint
    
    * no empty types
    
    * author in chapter name
    
    * add InputStream parseAs utils function
    
    * Review Changes
    
    * dont include all authors
    
    prevent https://imgur.com/dJ9LI4z
    
    * Revert "add InputStream parseAs utils function"
    
    This reverts commit 911b2bcd217044821c4d667bb323559d806073d5.
    
    * revert
    
    * use encodedPath for covers
    
    * more constants
    
    * update covers

--- src/en/dynasty/src/eu/kanade/tachiyomi/extension/en/dynasty/DynastyFactory.kt
+++ src/en/dynasty/src/eu/kanade/tachiyomi/extension/en/dynasty/DynastyFactory.kt
@@ -1,18 +1,13 @@
 package eu.kanade.tachiyomi.extension.en.dynasty
 
-import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.SourceFactory
 
 class DynastyFactory : SourceFactory {
-    override fun createSources(): List<Source> = getAllDynasty()
-}
-
-fun getAllDynasty() =
-    listOf(
-        DynastyAnthologies(),
-        DynastyChapters(),
-        DynastyDoujins(),
-        DynastyIssues(),
-        DynastySeries(),
-        DynastyScanlator(),
+    override fun createSources() = listOf(
+        Dynasty(),
+        DynastyLegacy("Dynasty-Anthologies (Deprecated)", 738706855355689486),
+        DynastyLegacy("Dynasty-Chapters (Deprecated)", 4399127807078496448),
+        DynastyLegacy("Dynasty-Doujins (Deprecated)", 6243685045159195166),
+        DynastyLegacy("Dynasty-Issues (Deprecated)", 2548005429321146934),
     )
+}
