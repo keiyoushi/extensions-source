cc004a8e443bd214ba05fd1b213213c726c1f407
bapeey <90949336+bapeey@users.noreply.github.com>
2025-04-11 11:38:37 -0500

    utils: add `toJsonString` function (#8439)
    add util

--- utils/src/keiyoushi/utils/Json.kt
+++ utils/src/keiyoushi/utils/Json.kt
@@ -1,6 +1,7 @@
 package keiyoushi.utils
 
 import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
 import okhttp3.Response
@@ -19,3 +20,9 @@ inline fun <reified T> String.parseAs(json: Json = jsonInstance): T =
  */
 inline fun <reified T> Response.parseAs(json: Json = jsonInstance): T =
     json.decodeFromStream(body.byteStream())
+
+/**
+ * Serializes the object to a JSON String.
+ */
+inline fun <reified T> T.toJsonString(json: Json = jsonInstance): String =
+    json.encodeToString(this)
