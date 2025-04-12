b7209bab7d684e0e006e5ac684fabc98f52dcd0b
riveX7 <miguelpintoribeiro07@gmail.com>
2025-04-12 13:27:31 +0100

    Adds The Beginning After The End Manga

---
+++ src/en/readthebeginningaftertheendmanga/src/eu/kanade/tachiyomi/extension/en/readthebeginningaftertheendmanga/TbateManga.kt
@@ -0,0 +1,12 @@
+
+package eu.kanade.tachiyomi.extension.en.tbatemanga
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class TbateManga : Madara(
+    "The Beginning After the End Manga",
+    "https://thebeginningaftertheendmanga.com",
+    "en"
+) {
+    override val mangaSubString = "manga"
+}
