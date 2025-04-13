5fc107abe28b35ca7fc11474bf0bd25537871488
Dmytro <dmytro@kushchenko.net>
2025-04-13 20:14:00 +0700

    Usagi: update filters list

--- src/ru/usagi/src/eu/kanade/tachiyomi/extension/ru/usagi/Usagi.kt
+++ src/ru/usagi/src/eu/kanade/tachiyomi/extension/ru/usagi/Usagi.kt
@@ -45,12 +45,10 @@ class Usagi : GroupLe("Usagi", "https://web.usagi.one/", "ru") {
                     }
                 }
                 is OrderBy -> {
-                    if (url.toString().contains("&") && filter.state < 6) {
-                        url.addQueryParameter("sortType", arrayOf("RATING", "POPULARITY", "YEAR", "NAME", "DATE_CREATE", "DATE_UPDATE, USER_RATING")[filter.state])
-                    } else {
-                        val ord = arrayOf("rate", "popularity", "year", "name", "created", "updated", "votes")[filter.state]
-                        return GET("$baseUrl/list?sortType=$ord&offset=${50 * (page - 1)}", headers)
-                    }
+                    url.addQueryParameter(
+                        "sortType",
+                        arrayOf("RATING", "POPULARITY", "YEAR", "NAME", "DATE_CREATE", "DATE_UPDATE", "USER_RATING")[filter.state],
+                    )
                 }
                 else -> {}
             }
@@ -64,7 +62,7 @@ class Usagi : GroupLe("Usagi", "https://web.usagi.one/", "ru") {
 
     private class OrderBy : Filter.Select<String>(
         "Сортировка",
-        arrayOf("По популярности", "Популярно сейчас", "По году", "По имени", "Новинки", "По дате обновления", "По рейтингу"),
+        arrayOf("По популярности", "Популярно сейчас", "По году", "По алфавиту", "Новинки", "По дате обновления", "По рейтингу"),
     )
 
     private class Genre(name: String, val id: String) : Filter.TriState(name)
@@ -92,8 +90,8 @@ class Usagi : GroupLe("Usagi", "https://web.usagi.one/", "ru") {
         Genre("Заброшен перевод", "s_abandoned_popular"),
         Genre("Длинная", "s_many_chapters"),
         Genre("Ожидает загрузки", "s_wait_upload"),
-        Genre("Лицензия", "s_sale"),
         Genre("Белые жанры", "s_not_pessimized"),
+        Genre("Онгоинг", "s_ongoing"),
     )
     private fun getMore() = listOf(
         Genre("В цвете", "el_7290"),
@@ -126,31 +124,27 @@ class Usagi : GroupLe("Usagi", "https://web.usagi.one/", "ru") {
         Genre("боевые искусства", "el_2143"),
         Genre("гарем", "el_2142"),
         Genre("гендерная интрига", "el_2156"),
-        Genre("героическое фэнтези", "el_2146"),
         Genre("детектив", "el_2152"),
         Genre("дзёсэй", "el_2158"),
         Genre("драма", "el_2118"),
-        Genre("игра", "el_2154"),
         Genre("история", "el_2119"),
         Genre("исэкай", "el_9450"),
         Genre("киберпанк", "el_8032"),
         Genre("кодомо", "el_2137"),
         Genre("комедия", "el_2136"),
-        Genre("махо-сёдзё", "el_2147"),
-        Genre("меха", "el_2126"),
+        Genre("музыка", "el_9514"),
         Genre("научная фантастика", "el_2133"),
+        Genre("пародия", "el_9524"),
         Genre("повседневность", "el_2135"),
         Genre("постапокалиптика", "el_2151"),
         Genre("приключения", "el_2130"),
         Genre("психология", "el_2144"),
         Genre("романтика", "el_2121"),
-        Genre("самурайский боевик", "el_2124"),
         Genre("сверхъестественное", "el_2159"),
         Genre("сёдзё", "el_2122"),
         Genre("сёнэн", "el_2134"),
         Genre("спорт", "el_2129"),
         Genre("сэйнэн", "el_2138"),
-        Genre("сянься", "el_9561"),
         Genre("трагедия", "el_2153"),
         Genre("триллер", "el_2150"),
         Genre("ужасы", "el_2125"),
