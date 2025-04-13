1a39594395a4de033421370d665a91b09d7c1dd6
Dmytro <dmytro@kushchenko.net>
2025-04-13 20:00:18 +0700

    Seimanga: update filters list

--- src/ru/seimanga/src/eu/kanade/tachiyomi/extension/ru/seimanga/SeiManga.kt
+++ src/ru/seimanga/src/eu/kanade/tachiyomi/extension/ru/seimanga/SeiManga.kt
@@ -45,12 +45,10 @@ class SeiManga : GroupLe("SeiManga", "https://1.seimanga.me", "ru") {
                     }
                 }
                 is OrderBy -> {
-                    if (url.toString().contains("&") && filter.state < 6) {
-                        url.addQueryParameter("sortType", arrayOf("RATING", "POPULARITY", "YEAR", "NAME", "DATE_CREATE", "DATE_UPDATE")[filter.state])
-                    } else {
-                        val ord = arrayOf("rate", "popularity", "year", "name", "created", "updated", "votes")[filter.state]
-                        return GET("$baseUrl/list?sortType=$ord&offset=${50 * (page - 1)}", headers)
-                    }
+                    url.addQueryParameter(
+                        "sortType",
+                        arrayOf("RATING", "POPULARITY", "YEAR", "NAME", "DATE_CREATE", "DATE_UPDATE", "USER_RATING")[filter.state],
+                    )
                 }
                 else -> {}
             }
@@ -64,7 +62,7 @@ class SeiManga : GroupLe("SeiManga", "https://1.seimanga.me", "ru") {
 
     private class OrderBy : Filter.Select<String>(
         "Сортировка",
-        arrayOf("По популярности", "Популярно сейчас", "По году", "По имени", "Новинки", "По дате обновления", "По рейтингу"),
+        arrayOf("По популярности", "Популярно сейчас", "По году", "По алфавиту", "Новинки", "По дате обновления", "По рейтингу"),
     )
 
     private class Genre(name: String, val id: String) : Filter.TriState(name)
@@ -92,33 +90,29 @@ class SeiManga : GroupLe("SeiManga", "https://1.seimanga.me", "ru") {
         Genre("Заброшен перевод", "s_abandoned_popular"),
         Genre("Длинная", "s_many_chapters"),
         Genre("Ожидает загрузки", "s_wait_upload"),
-        Genre("Белые жанры", "s_not_pessimized"),
+        Genre("Онгоинг", "s_ongoing"),
     )
     private fun getMore() = listOf(
-        Genre("Анонс", "el_6641"),
         Genre("В цвете", "el_4614"),
         Genre("Веб", "el_1355"),
-        Genre("Выпуск приостановлен", "el_5232"),
-        Genre("Не Яой", "el_1874"),
+        Genre("На экранах", "el_6827"),
         Genre("Сборник", "el_1348"),
     )
 
     private fun getAgeList() = listOf(
-        Genre("R(16+)", "el_3968"),
-        Genre("NC-17(18+)", "el_3969"),
-        Genre("R18+(18+)", "el_3990"),
+        Genre("NC-17 (17+)", "el_3969"),
+        Genre("R (17+)", "el_3968"),
     )
 
     private fun getCategoryList() = listOf(
         Genre("OEL-манга", "el_6637"),
-        Genre("Додзинси", "el_1332"),
         Genre("Арт", "el_2220"),
+        Genre("Додзинси", "el_1332"),
         Genre("Ёнкома", "el_2741"),
         Genre("Комикс", "el_1903"),
         Genre("Манга", "el_6421"),
         Genre("Манхва", "el_1873"),
         Genre("Маньхуа", "el_1875"),
-        Genre("Ранобэ", "el_5688"),
     )
 
     private fun getGenreList() = listOf(
@@ -126,42 +120,33 @@ class SeiManga : GroupLe("SeiManga", "https://1.seimanga.me", "ru") {
         Genre("боевые искусства", "el_1334"),
         Genre("гарем", "el_1333"),
         Genre("гендерная интрига", "el_1347"),
-        Genre("героическое фэнтези", "el_1337"),
         Genre("детектив", "el_1343"),
         Genre("дзёсэй", "el_1349"),
         Genre("драма", "el_1310"),
-        Genre("игра", "el_5229"),
-        Genre("исэкай", "el_6420"),
         Genre("история", "el_1311"),
+        Genre("исэкай", "el_6420"),
         Genre("киберпанк", "el_1351"),
         Genre("комедия", "el_1328"),
-        Genre("меха", "el_1318"),
+        Genre("музыка", "el_6585"),
         Genre("научная фантастика", "el_1325"),
-        Genre("омегаверс", "el_5676"),
+        Genre("пародия", "el_6595"),
         Genre("повседневность", "el_1327"),
         Genre("постапокалиптика", "el_1342"),
         Genre("приключения", "el_1322"),
         Genre("психология", "el_1335"),
         Genre("романтика", "el_1313"),
-        Genre("самурайский боевик", "el_1316"),
         Genre("сверхъестественное", "el_1350"),
         Genre("сёдзё", "el_1314"),
-        Genre("сёдзё-ай", "el_1320"),
         Genre("сёнэн", "el_1326"),
-        Genre("сёнэн-ай", "el_1330"),
         Genre("спорт", "el_1321"),
         Genre("сэйнэн", "el_1329"),
-        Genre("сянься", "el_6631"),
         Genre("трагедия", "el_1344"),
         Genre("триллер", "el_1341"),
         Genre("ужасы", "el_1317"),
-        Genre("уся", "el_6632"),
         Genre("фэнтези", "el_1323"),
         Genre("школа", "el_1319"),
         Genre("эротика", "el_1340"),
         Genre("этти", "el_1354"),
-        Genre("юри", "el_1315"),
-        Genre("яой", "el_1336"),
     )
 
     override fun setupPreferenceScreen(screen: androidx.preference.PreferenceScreen) {
