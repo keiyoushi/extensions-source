be25ba5f14b042233e192a0a8f5faccba6b56280
ping2000 <tom.darocha@free.fr>
2025-04-14 19:29:10 +0000

    fix(pornhwa): domain changed

--- src/fr/lunarscanshentai/src/eu/kanade/tachiyomi/extension/fr/lunarscanshentai/OrtegaScans.kt
+++ src/fr/lunarscanshentai/src/eu/kanade/tachiyomi/extension/fr/lunarscanshentai/OrtegaScans.kt
@@ -4,9 +4,9 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class PornhwaScans : MangaThemesia(
-    "Pornhwa Scans",
-    "https://pornhwascans.fr",
+class OrtegaScans : MangaThemesia(
+    "Ortega Scans",
+    "https://ortegascans.fr",
     "fr",
     dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH),
 ) {
