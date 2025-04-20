ddacd7b61a24c278e752e2a5218ac169842b054a
Senxian Zhuo <9044823+flashsphere@users.noreply.github.com>
2025-04-20 18:46:07 +0800

    Code review changes

--- src/zh/sixmh/src/eu/kanade/tachiyomi/extension/zh/sixmh/SixMH.kt
+++ src/zh/sixmh/src/eu/kanade/tachiyomi/extension/zh/sixmh/SixMH.kt
@@ -1,6 +1,5 @@
 package eu.kanade.tachiyomi.extension.zh.sixmh
 
-import android.net.Uri
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
@@ -8,6 +7,7 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
@@ -26,11 +26,10 @@ class SixMH : SimpleParsedHttpSource() {
     override fun popularMangaRequest(page: Int) = GET("$baseUrl/category/order/hits/page/$page", headers)
     override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/category/order/addtime/page/$page", headers)
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val uri = Uri.parse(baseUrl).buildUpon()
-            .appendPath("index.php")
-            .appendPath("search")
-            .appendQueryParameter("key", query).toString()
-        return GET(uri, headers)
+        val url = "$baseUrl/index.php/search".toHttpUrl().newBuilder()
+            .addQueryParameter("key", query)
+            .build()
+        return GET(url, headers)
     }
 
     override fun simpleNextPageSelector(): String? = null
@@ -43,7 +42,7 @@ class SixMH : SimpleParsedHttpSource() {
 
     // Details
     override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
-        val element = document.select("div.cy_info").first()!!
+        val element = document.selectFirst("div.cy_info")!!
         title = element.selectFirst("div.cy_title")!!.text()
         thumbnail_url = element.selectFirst("div.cy_info_cover > a > img.pic")?.absUrl("src")
         description = element.selectFirst("div.cy_desc #comic-description")?.text()
