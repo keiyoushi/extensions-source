a3c60e09de9e03a14da0f5b748a4d87e5c37d97d
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-04-28 15:17:35 +0200

    Doujins: add headers, encode search (#8660)

--- src/en/doujins/src/eu/kanade/tachiyomi/extension/en/doujins/Doujins.kt
+++ src/en/doujins/src/eu/kanade/tachiyomi/extension/en/doujins/Doujins.kt
@@ -15,6 +15,7 @@ import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
@@ -96,7 +97,7 @@ class Doujins : HttpSource() {
         return "$baseUrl/folders?start=$startDateSec&end=$endDateSec"
     }
 
-    override fun latestUpdatesRequest(page: Int) = GET(getLatestPageUrl(page))
+    override fun latestUpdatesRequest(page: Int) = GET(getLatestPageUrl(page), headers)
 
     override fun mangaDetailsParse(response: Response): SManga {
         val document = response.asJsoup()
@@ -118,7 +119,7 @@ class Doujins : HttpSource() {
 
     override fun popularMangaParse(response: Response) = parseGalleryPage(response.asJsoup())
 
-    override fun popularMangaRequest(page: Int) = GET("$baseUrl/top/month")
+    override fun popularMangaRequest(page: Int) = GET("$baseUrl/top/month", headers)
 
     override fun searchMangaParse(response: Response) = parseGalleryPage(response.asJsoup())
 
@@ -130,13 +131,21 @@ class Doujins : HttpSource() {
 
         return when {
             query != "" -> {
-                GET("$baseUrl/searches?words=$query&page=$page&sort=${sortFilter.toUriPart()}")
+                val url = "$baseUrl/searches".toHttpUrl().newBuilder()
+                    .addQueryParameter("words", query)
+                    .addQueryParameter("page", page.toString())
+                    .addQueryParameter("sort", sortFilter.toUriPart())
+                    .build()
+                GET(url, headers)
             }
             seriesFilter.toUriPart() != "" -> {
-                GET("$baseUrl${seriesFilter.toUriPart()}?sort=${sortFilter.toUriPart()}")
+                val url = "$baseUrl${seriesFilter.toUriPart()}".toHttpUrl().newBuilder()
+                    .addQueryParameter("sort", sortFilter.toUriPart())
+                    .build()
+                GET(url, headers)
             }
             else -> {
-                GET("$baseUrl${popularityPeriodFilter.toUriPart()}")
+                GET("$baseUrl${popularityPeriodFilter.toUriPart()}", headers)
             }
         }
     }
