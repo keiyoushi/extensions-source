d2e7120f1d323f5bb279c8df52798cf35f6d2c79
bapeey <90949336+bapeey@users.noreply.github.com>
2025-05-04 20:39:14 -0500

    fix decrypt

--- src/es/catharsisworld/src/eu/kanade/tachiyomi/extension/es/catharsisworld/CatharsisWorld.kt
+++ src/es/catharsisworld/src/eu/kanade/tachiyomi/extension/es/catharsisworld/CatharsisWorld.kt
@@ -1,10 +1,17 @@
 package eu.kanade.tachiyomi.extension.es.catharsisworld
 
+import android.util.Base64
+import eu.kanade.tachiyomi.lib.cryptoaes.CryptoAES
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
 class CatharsisWorld : Madara(
@@ -49,6 +56,42 @@ class CatharsisWorld : Madara(
         name = element.selectFirst("div.grid > span")!!.text()
         date_upload = element.selectFirst("div.grid > div")?.text()?.let { parseChapterDate(it) } ?: 0
         setUrlWithoutDomain(element.selectFirst("a")!!.attr("href"))
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        launchIO { countViews(document) }
+
+        val chapterProtector = document.selectFirst(chapterProtectorSelector)
+            ?: return document.select(pageListParseSelector).mapIndexed { index, element ->
+                val imageUrl = element.selectFirst("img")?.let { imageFromElement(it) }
+                Page(index, document.location(), imageUrl)
+            }
+        val chapterProtectorHtml = chapterProtector.attr("src")
+            .takeIf { it.startsWith("data:text/javascript;base64,") }
+            ?.substringAfter("data:text/javascript;base64,")
+            ?.let { Base64.decode(it, Base64.DEFAULT).toString(Charsets.UTF_8) }
+            ?: chapterProtector.html()
+        val password = chapterProtectorHtml
+            .substringAfter("protectornonce='")
+            .substringBefore("';")
+        val chapterData = json.parseToJsonElement(
+            chapterProtectorHtml
+                .substringAfter("_data='")
+                .substringBefore("';")
+                .replace("\\/", "/"),
+        ).jsonObject
+
+        val unsaltedCiphertext = Base64.decode(chapterData["ct"]!!.jsonPrimitive.content, Base64.DEFAULT)
+        val salt = chapterData["s"]!!.jsonPrimitive.content.decodeHex()
+        val ciphertext = salted + salt + unsaltedCiphertext
+
+        val rawImgArray = CryptoAES.decrypt(Base64.encodeToString(ciphertext, Base64.DEFAULT), password)
+        val imgArrayString = json.parseToJsonElement(rawImgArray).jsonPrimitive.content
+        val imgArray = json.parseToJsonElement(imgArrayString).jsonArray
+
+        return imgArray.mapIndexed { idx, it ->
+            Page(idx, document.location(), it.jsonPrimitive.content)
+        }
     }
 
     private fun Element.imageFromStyle(): String {
