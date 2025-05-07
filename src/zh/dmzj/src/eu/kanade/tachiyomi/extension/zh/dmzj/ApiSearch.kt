8c78c5e58666aaf40cfdd5602238e0d5159e4673
zhongfly <11155705+zhongfly@users.noreply.github.com>
2025-05-07 23:21:50 +0800

    dmzj: remove old text search

--- src/zh/dmzj/src/eu/kanade/tachiyomi/extension/zh/dmzj/ApiSearch.kt
+++ src/zh/dmzj/src/eu/kanade/tachiyomi/extension/zh/dmzj/ApiSearch.kt
@@ -3,27 +3,10 @@ package eu.kanade.tachiyomi.extension.zh.dmzj
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.SManga
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Response
 
 object ApiSearch {
-
-    fun textSearchUrl(query: String) =
-        "http://sacg.idmzj.com/comicsum/search.php".toHttpUrl().newBuilder()
-            .addQueryParameter("s", query)
-            .toString()
-
-    fun parsePage(response: Response): MangasPage {
-        if (!response.isSuccessful) {
-            response.close()
-            return MangasPage(emptyList(), false)
-        }
-        // "var g_search_data = [...];"
-        val js = response.body.string().run { substring(20, length - 1) }
-        val data: List<MangaDto> = json.decodeFromString(js)
-        return MangasPage(data.map { it.toSManga() }, false)
-    }
 
     fun searchUrlV1(page: Int, query: String) =
         "https://manhua.idmzj.com/api/v1/comic2/search".toHttpUrl().newBuilder()
@@ -49,21 +32,6 @@ object ApiSearch {
             } else {
                 MangasPage(emptyList(), false)
             }
-        }
-    }
-
-    @Serializable
-    class MangaDto(
-        private val id: Int,
-        private val comic_name: String,
-        private val comic_author: String,
-        private val comic_cover: String,
-    ) {
-        fun toSManga() = SManga.create().apply {
-            url = getMangaUrl(id.toString())
-            title = comic_name
-            author = comic_author.formatList()
-            thumbnail_url = comic_cover
         }
     }
 
