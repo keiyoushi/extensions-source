75251eca57020157faa16185520865fde6b77e10
Chopper <156493704+choppeh@users.noreply.github.com>
2025-05-08 10:11:41 -0300

    Fix loading content

--- src/pt/pizzariascan/src/eu/kanade/tachiyomi/extension/pt/pizzariascan/Pizzariascan.kt
+++ src/pt/pizzariascan/src/eu/kanade/tachiyomi/extension/pt/pizzariascan/Pizzariascan.kt
@@ -1,6 +1,9 @@
 package eu.kanade.tachiyomi.extension.pt.pizzariascan
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import okhttp3.Request
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -10,5 +13,15 @@ class Pizzariascan : Madara(
     "pt-BR",
     SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("pt")),
 ) {
+    override val supportsLatest = false
+
+    // It's used in search request
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+
     override val useNewChapterEndpoint: Boolean = true
+
+    override fun popularMangaRequest(page: Int): Request =
+        GET("$baseUrl/$mangaSubString/${searchPage(page)}", headers)
+
+    override fun getFilterList() = FilterList()
 }
