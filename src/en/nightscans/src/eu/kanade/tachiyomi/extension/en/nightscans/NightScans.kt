4c3d5057b1c0f4dd7738911b45557002115a6fbb
dimiarbre <dimitri.lereverend@gmail.com>
2025-05-13 21:20:07 +0200

    Remove duplicate page

--- src/en/nightscans/src/eu/kanade/tachiyomi/extension/en/nightscans/NightScans.kt
+++ src/en/nightscans/src/eu/kanade/tachiyomi/extension/en/nightscans/NightScans.kt
@@ -4,7 +4,11 @@ import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaAlt
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaPaidChapterHelper
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.Page
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.OkHttpClient
+import org.jsoup.nodes.Document
 import java.util.concurrent.TimeUnit
 
 class NightScans : MangaThemesiaAlt("NIGHT SCANS", "https://nightsup.net", "en", "/series") {
@@ -28,5 +32,31 @@ class NightScans : MangaThemesiaAlt("NIGHT SCANS", "https://nightsup.net", "en",
             super.chapterListSelector(),
             preferences,
         )
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        countViews(document)
+
+        val chapterUrl = document.location()
+        val htmlPages = document.select(pageSelector)
+            .filterNot { it.imgAttr().isEmpty() }
+            .distinctBy { img -> img.imgAttr() }
+            .mapIndexed { i, img -> Page(i, chapterUrl, img.imgAttr()) }
+
+        // Some sites also loads pages via javascript
+        if (htmlPages.isNotEmpty()) { return htmlPages }
+
+        val docString = document.toString()
+        val imageListJson = JSON_IMAGE_LIST_REGEX.find(docString)?.destructured?.toList()?.get(0).orEmpty()
+        val imageList = try {
+            json.parseToJsonElement(imageListJson).jsonArray
+        } catch (_: IllegalArgumentException) {
+            emptyList()
+        }
+        val scriptPages = imageList.mapIndexed { i, jsonEl ->
+            Page(i, chapterUrl, jsonEl.jsonPrimitive.content)
+        }
+
+        return scriptPages
     }
 }
