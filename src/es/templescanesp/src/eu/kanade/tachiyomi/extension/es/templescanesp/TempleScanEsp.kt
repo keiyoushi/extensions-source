271d598ccddef8501a944e008d5dbdbfa96fa98c
bapeey <90949336+bapeey@users.noreply.github.com>
2025-05-15 10:24:57 -0500

    TempleScan(es): Add preference for domain change (#8828)
    * update domain pref
    
    * update domain

--- src/es/templescanesp/src/eu/kanade/tachiyomi/extension/es/templescanesp/TempleScanEsp.kt
+++ src/es/templescanesp/src/eu/kanade/tachiyomi/extension/es/templescanesp/TempleScanEsp.kt
@@ -1,29 +1,58 @@
 package eu.kanade.tachiyomi.extension.es.templescanesp
 
+import android.content.SharedPreferences
+import android.widget.Toast
+import androidx.preference.EditTextPreference
+import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import keiyoushi.utils.getPreferences
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class TempleScanEsp : Madara(
-    "Temple Scan",
-    "https://templescanesp.kawi.lat",
-    "es",
-    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
-) {
+class TempleScanEsp :
+    Madara(
+        "Temple Scan",
+        "https://templescanesp.vxviral.xyz",
+        "es",
+        dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
+    ),
+    ConfigurableSource {
+
+    private val isCi = System.getenv("CI") == "true"
+
+    override val baseUrl get() = when {
+        isCi -> super.baseUrl
+        else -> preferences.prefBaseUrl
+    }
+
     override val versionId = 4
 
     override val mangaSubString = "serie"
 
     override val useLoadMoreRequest = LoadMoreStrategy.Always
 
-    override val client = super.client.newBuilder()
-        .rateLimitHost(baseUrl.toHttpUrl(), 3, 1)
-        .build()
+    override val client by lazy {
+        super.client.newBuilder()
+            .rateLimitHost(baseUrl.toHttpUrl(), 3, 1)
+            .build()
+    }
+
+    private val preferences = getPreferences {
+        this.getString(DEFAULT_BASE_URL_PREF, null).let { domain ->
+            if (domain != super.baseUrl) {
+                this.edit()
+                    .putString(BASE_URL_PREF, super.baseUrl)
+                    .putString(DEFAULT_BASE_URL_PREF, super.baseUrl)
+                    .apply()
+            }
+        }
+    }
 
     override fun popularMangaSelector() = "div.latest-poster"
 
@@ -56,5 +85,38 @@ class TempleScanEsp : Madara(
 
     private fun Element.imageFromStyle(): String {
         return this.attr("style").substringAfter("url(").substringBefore(")")
+    }
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        EditTextPreference(screen.context).apply {
+            key = BASE_URL_PREF
+            title = "Editar URL de la fuente"
+            summary = "Para uso temporal, si la extensión se actualiza se perderá el cambio."
+            dialogTitle = "Editar URL de la fuente"
+            dialogMessage = "URL por defecto:\n${super.baseUrl}"
+            setDefaultValue(super.baseUrl)
+            setOnPreferenceChangeListener { _, _ ->
+                Toast.makeText(screen.context, "Reinicie la aplicación para aplicar los cambios", Toast.LENGTH_LONG).show()
+                true
+            }
+        }.also { screen.addPreference(it) }
+    }
+
+    private var _cachedBaseUrl: String? = null
+    private var SharedPreferences.prefBaseUrl: String
+        get() {
+            if (_cachedBaseUrl == null) {
+                _cachedBaseUrl = getString(BASE_URL_PREF, super.baseUrl)!!
+            }
+            return _cachedBaseUrl!!
+        }
+        set(value) {
+            _cachedBaseUrl = value
+            edit().putString(BASE_URL_PREF, value).apply()
+        }
+
+    companion object {
+        private const val BASE_URL_PREF = "overrideBaseUrl"
+        private const val DEFAULT_BASE_URL_PREF = "defaultBaseUrl"
     }
 }
