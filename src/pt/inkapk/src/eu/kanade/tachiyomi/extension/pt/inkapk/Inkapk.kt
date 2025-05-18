0b9da27e40c46d6f8a745071cb1211b8de67e6f9
Chopper <156493704+choppeh@users.noreply.github.com>
2025-05-18 02:34:18 -0300

    Add Inkapk (#8855)

---
+++ src/pt/inkapk/src/eu/kanade/tachiyomi/extension/pt/inkapk/Inkapk.kt
@@ -0,0 +1,21 @@
+package eu.kanade.tachiyomi.extension.pt.inkapk
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Inkapk : Madara(
+    "Inkapk",
+    "https://inkapk.net",
+    "pt-BR",
+    SimpleDateFormat("MM dd, yyyy", Locale("pt", "BR")),
+) {
+    override val client = super.client.newBuilder()
+        .rateLimit(2)
+        .build()
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+
+    override val useNewChapterEndpoint = true
+}
