d0e3857ac5d91b46918d421874de087941fb1548
Chopper <156493704+choppeh@users.noreply.github.com>
2025-05-18 02:36:39 -0300

    KomikStation: Fix manga details (#8892)
    Fix manga details

--- src/id/komikstation/src/eu/kanade/tachiyomi/extension/id/komikstation/KomikStation.kt
+++ src/id/komikstation/src/eu/kanade/tachiyomi/extension/id/komikstation/KomikStation.kt
@@ -2,7 +2,6 @@ package eu.kanade.tachiyomi.extension.id.komikstation
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import eu.kanade.tachiyomi.source.model.SManga
 import okhttp3.OkHttpClient
 
 class KomikStation : MangaThemesia("Komik Station", "https://komikstation.co", "id") {
@@ -12,20 +11,6 @@ class KomikStation : MangaThemesia("Komik Station", "https://komikstation.co", "
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(4)
         .build()
-
-    override val seriesTitleSelector = ".ts-breadcrumb li[itemprop]:last-child span"
-    override val seriesAuthorSelector = ".infox .fmed:has(.fa-pen-fancy) span"
-    override val seriesArtistSelector = ".infox .fmed:has(.fa-paintbrush) span"
-    override val seriesTypeSelector = ".tsinfo .imptdt:has(a[href*=\"type\"]) a"
-    override val seriesStatusSelector = ".tsinfo .imptdt:first-child i"
-
-    override fun String?.parseStatus(): Int = when {
-        this == null -> SManga.UNKNOWN
-        listOf("ongoing", "berjalan").any { this.contains(it, ignoreCase = true) } -> SManga.ONGOING
-        this.contains("hiatus", ignoreCase = true) -> SManga.ON_HIATUS
-        this.contains("tamat", ignoreCase = true) -> SManga.COMPLETED
-        else -> SManga.UNKNOWN
-    }
 
     override val projectPageString = "/project-list"
 
