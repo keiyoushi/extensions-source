2e21c1e95b5c863979446097c59df8c0cde9f167
Chopper <156493704+choppeh@users.noreply.github.com>
2025-05-17 23:00:35 -0300

    Update mangaUrlDirectory

--- src/id/noromax/src/eu/kanade/tachiyomi/extension/id/noromax/Noromax.kt
+++ src/id/noromax/src/eu/kanade/tachiyomi/extension/id/noromax/Noromax.kt
@@ -2,7 +2,11 @@ package eu.kanade.tachiyomi.extension.id.noromax
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 
-class Noromax : MangaThemesia("Noromax", "https://noromax01.my.id", "id", "/Komik") {
+class Noromax : MangaThemesia(
+    "Noromax",
+    "https://noromax01.my.id",
+    "id",
+) {
 
     // Site changed from ZeistManga to MangaThemesia
     override val versionId = 2
