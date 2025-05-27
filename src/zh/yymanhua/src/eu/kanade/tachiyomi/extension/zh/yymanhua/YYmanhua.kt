603a75c3e0bc64174982840e14835430d0c70954
周华梁 <642615676@qq.com>
2025-05-28 02:58:20 +0800

    set user-agent and update version code

--- src/zh/yymanhua/src/eu/kanade/tachiyomi/extension/zh/yymanhua/YYmanhua.kt
+++ src/zh/yymanhua/src/eu/kanade/tachiyomi/extension/zh/yymanhua/YYmanhua.kt
@@ -30,6 +30,7 @@ class YYmanhua : ParsedHttpSource() {
     }
 
     override fun headersBuilder() = super.headersBuilder()
+        .set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0")
         .add("Referer", baseUrl)
         .add("Cookie", "yymanhua_lang=2")
 
