f1dfa2f49ac9ea2b652def93072a4b313bf219ab
Liam Rooney <liam@roon.dev>
2025-05-28 14:06:47 -0700

    VortexScans: actually get latest chapters

--- src/en/arvenscans/src/eu/kanade/tachiyomi/extension/en/arvenscans/VortexScans.kt
+++ src/en/arvenscans/src/eu/kanade/tachiyomi/extension/en/arvenscans/VortexScans.kt
@@ -2,6 +2,8 @@ package eu.kanade.tachiyomi.extension.en.arvenscans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
 import eu.kanade.tachiyomi.network.GET
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
 
 class VortexScans : Iken(
     "Vortex Scans",
@@ -10,4 +12,18 @@ class VortexScans : Iken(
     "https://api.vortexscans.org",
 ) {
     override fun popularMangaRequest(page: Int) = GET(baseUrl, headers)
+
+    override fun latestUpdatesRequest(page: Int) = latestMangaRequest(page)
+    private fun latestMangaRequest(page: Int): Request {
+        val url = "$apiUrl/api/posts".toHttpUrl().newBuilder().apply {
+            addQueryParameter("page", page.toString())
+            addQueryParameter("perPage", latestPerPage.toString())
+            addQueryParameter("tag", "latestUpdate")
+            addQueryParameter("isNovel", "false")
+        }.build()
+
+        return GET(url, headers)
+    }
 }
+
+private const val latestPerPage = 18
