7ce36c14d1cb8815e9c166cdb27a58d8f9eb60ba
Hualiang <78242797+hualiong@users.noreply.github.com>
2025-05-29 23:43:11 +0800

    Update src/zh/yymanhua/src/eu/kanade/tachiyomi/extension/zh/yymanhua/YYmanhua.kt
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>

--- src/zh/yymanhua/src/eu/kanade/tachiyomi/extension/zh/yymanhua/YYmanhua.kt
+++ src/zh/yymanhua/src/eu/kanade/tachiyomi/extension/zh/yymanhua/YYmanhua.kt
@@ -31,7 +31,7 @@ class YYmanhua : ParsedHttpSource() {
 
     override fun headersBuilder() = super.headersBuilder()
         .set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0")
-        .add("Referer", baseUrl)
+        .add("Referer", "$baseUrl/")
         .add("Cookie", "yymanhua_lang=2")
 
     override val client = network.cloudflareClient.newBuilder()
