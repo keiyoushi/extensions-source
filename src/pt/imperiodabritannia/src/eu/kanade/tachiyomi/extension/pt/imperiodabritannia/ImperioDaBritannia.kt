121de15938c8d5c516138502a6b3c74c10e2e440
Chopper <156493704+choppeh@users.noreply.github.com>
2025-05-29 22:42:10 -0300

    ImperioDaBritannia: Fix popular and latest (#9015)
    Fix popular and latest

--- src/pt/imperiodabritannia/src/eu/kanade/tachiyomi/extension/pt/imperiodabritannia/ImperioDaBritannia.kt
+++ src/pt/imperiodabritannia/src/eu/kanade/tachiyomi/extension/pt/imperiodabritannia/ImperioDaBritannia.kt
@@ -3,6 +3,8 @@ package eu.kanade.tachiyomi.extension.pt.imperiodabritannia
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
+import okhttp3.internal.http.HTTP_FORBIDDEN
+import okhttp3.internal.http.HTTP_OK
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
@@ -16,6 +18,16 @@ class ImperioDaBritannia : Madara(
 
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(1, 2, TimeUnit.SECONDS)
+        .readTimeout(1, TimeUnit.MINUTES)
+        .addInterceptor { chain ->
+            val response = chain.proceed(chain.request())
+            if (response.code == HTTP_FORBIDDEN) {
+                return@addInterceptor response.newBuilder()
+                    .code(HTTP_OK)
+                    .build()
+            }
+            response
+        }
         .build()
 
     override val useNewChapterEndpoint = true
