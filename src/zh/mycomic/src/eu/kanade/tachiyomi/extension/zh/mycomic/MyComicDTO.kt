311dfe4d58d519d067f38d59dbc05e326fa324ee
AlphaBoom <30779939+AlphaBoom@users.noreply.github.com>
2025-05-31 08:18:39 +0800

    Add MyComic (#8986)
    * Add MyComic
    
    * MyComic: Replace Object filter to class filter
    
    * Apply suggestions from code review
    
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>
    
    * Apply review suggestions.
    
    * Try to use selectFirst as much as possible.
    
    * Apply review suggestions
    
    ---------
    
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>

---
+++ src/zh/mycomic/src/eu/kanade/tachiyomi/extension/zh/mycomic/MyComicDTO.kt
@@ -0,0 +1,8 @@
+// ktlint-disable filename
+
+package eu.kanade.tachiyomi.extension.zh.mycomic
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Chapter(val id: Long, val title: String)
