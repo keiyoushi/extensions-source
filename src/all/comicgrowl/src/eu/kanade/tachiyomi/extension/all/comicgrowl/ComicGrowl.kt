35cfb179159d19b8b51408c6a1468320a0997396
Wind He <g311571057@gmail.com>
2025-05-31 13:06:49 +0800

    clean code

--- src/all/comicgrowl/src/eu/kanade/tachiyomi/extension/all/comicgrowl/ComicGrowl.kt
+++ src/all/comicgrowl/src/eu/kanade/tachiyomi/extension/all/comicgrowl/ComicGrowl.kt
@@ -17,6 +17,7 @@ import kotlinx.serialization.json.int
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
@@ -37,37 +38,25 @@ class ComicGrowl(
     override val supportsLatest: Boolean = false,
 ) : ParsedHttpSource() {
 
-    private val json: Json by injectLazy()
-
     companion object {
         private const val PUBLISHER = "BUSHIROAD WORKS"
 
         private val imageUrlRegex by lazy { Regex("^.*?webp") }
 
-        /**
-         * Get cover image url from [element]
-         */
-        private fun getImageFromElement(element: Element): String {
-            val match = imageUrlRegex.find(element.selectFirst("source")!!.attr("data-srcset"))
-                ?: throw Exception("Cover image url not found")
-            return "https:" + match.value
-        }
+        private val DATE_PARSER by lazy { SimpleDateFormat("yyyy-MM-dd", Locale.ROOT) }
 
-        private val DATE_PARSER by lazy { SimpleDateFormat("yyyy-MM-dd", Locale.JAPAN) }
-
-        /**
-         * Parse date from string like "3月31日" to UNIX Epoch time.
-         */
-        private fun String.toDate(): Long {
-            return DATE_PARSER.tryParse(this)
-        }
+        private val json: Json by injectLazy()
     }
 
     override val client = super.client.newBuilder()
         .addNetworkInterceptor(::imageDescrambler)
         .build()
 
-    override fun popularMangaRequest(page: Int) = GET("$baseUrl/ranking/manga")
+    override fun headersBuilder(): Headers.Builder {
+        return super.headersBuilder().set("Referer", "$baseUrl/")
+    }
+
+    override fun popularMangaRequest(page: Int) = GET("$baseUrl/ranking/manga", headers)
 
     override fun popularMangaNextPageSelector() = null
 
@@ -75,53 +64,46 @@ class ComicGrowl(
 
     override fun popularMangaFromElement(element: Element): SManga {
         return SManga.create().apply {
-            url = element.selectFirst("a")!!.attr("href")
+            setUrlWithoutDomain(element.selectFirst("a")!!.absUrl("href"))
             title = element.selectFirst(".title-text")!!.text()
-            author = element.selectFirst(".author-link")!!.text()
-            thumbnail_url = getImageFromElement(element)
+            author = element.selectFirst(".author-link")?.text()
+            setImageUrlFromElement(element)
         }
     }
-
-    override fun mangaDetailsRequest(manga: SManga) = GET(manga.url)
 
     override fun mangaDetailsParse(document: Document): SManga {
         val infoElement = document.selectFirst(".series-h-info")!!
-        val updateDateElement = infoElement.selectFirst("series-h-tag-label")
+        val updateDateElement = infoElement.selectFirst(".series-h-tag-label")
         return SManga.create().apply {
             title = infoElement.select("h1 > span")[1]!!.text()
-            author = infoElement.selectFirst(".series-h-credit-user-item > .article-text")!!.text() // TODO: get 脚本/漫画
-            description = infoElement.selectFirst(".series-h-credit-info-text-text > div > p > span > span")!!.text()
-            thumbnail_url = getImageFromElement(document.getElementsByClass("series-h-img").first()!!)
-            status = if (updateDateElement != null) SManga.ONGOING else SManga.COMPLETED
+            author = infoElement.selectFirst(".series-h-credit-user-item > .article-text")?.text()
+            description = infoElement.selectFirst(".series-h-credit-info-text-text p")?.wholeText()?.trim()
+            setImageUrlFromElement(document.getElementsByClass("series-h-img").first())
+            status = if (updateDateElement != null) SManga.ONGOING else SManga.COMPLETED // TODO: need validate
         }
     }
 
-    override fun chapterListRequest(manga: SManga) = GET(manga.url) // TODO: get chapters from `/list`
+//    override fun chapterListRequest(manga: SManga) = GET("$", headers) // TODO: get chapters from `/list`
 
     override fun chapterListParse(response: Response): List<SChapter> {
         val document = response.asJsoup()
         return document.select(chapterListSelector()).mapIndexed { index, element ->
             chapterFromElement(element).apply {
                 chapter_number = index.toFloat()
-                if (url.isEmpty()) { // Login required
-                    throw Exception("Some chapters don't have url, login and refresh again")
-                }
             }
-        }
+        }.filter { it.url.isNotEmpty() }
     }
 
     override fun chapterListSelector() = ".article-ep-list-item-img-link"
 
     override fun chapterFromElement(element: Element): SChapter {
         return SChapter.create().apply {
-            url = element.attr("data-href")
+            setUrlWithoutDomain(element.absUrl("data-href"))
             name = element.selectFirst(".series-ep-list-item-h-text")!!.text()
-            date_upload = element.selectFirst(".series-ep-list-date-time")!!.attr("datetime").toDate()
+            setUploadDate(element.selectFirst(".series-ep-list-date-time"))
             scanlator = PUBLISHER
         }
     }
-
-    override fun pageListRequest(chapter: SChapter) = GET(chapter.url)
 
     override fun pageListParse(document: Document): List<Page> {
         val pageList = mutableListOf<Page>()
@@ -129,26 +111,24 @@ class ComicGrowl(
         val viewer = document.selectFirst("#comici-viewer")!!
         val comiciViewerId = viewer.attr("comici-viewer-id")
         val memberJwt = viewer.attr("data-member-jwt")
-        val requestUrl = baseUrl.toHttpUrl().newBuilder()
-            .addPathSegment("book")
-            .addPathSegments("contentsInfo")
+        val requestUrl = "$baseUrl/book/contentsInfo".toHttpUrl().newBuilder()
             .addQueryParameter("comici-viewer-id", comiciViewerId)
             .addQueryParameter("user-id", memberJwt)
             .addQueryParameter("page-from", "0")
 
         // Initial request to get total pages
-        val initialRequest = GET(requestUrl.addQueryParameter("page-to", "1").build())
+        val initialRequest = GET(requestUrl.addQueryParameter("page-to", "1").build(), headers)
         client.newCall(initialRequest).execute().use { initialResponse ->
-            if (initialResponse.code != 200) {
+            if (!initialResponse.isSuccessful) {
                 throw Exception("Failed to get page list")
             }
             // FIXME: use util in core
             val totalPages =
                 json.parseToJsonElement(initialResponse.body.string()).jsonObject["totalPages"]!!.jsonPrimitive.content
             // Get all pages
-            val getAllPagesRequest = GET(requestUrl.setQueryParameter("page-to", totalPages).build())
+            val getAllPagesRequest = GET(requestUrl.setQueryParameter("page-to", totalPages).build(), headers)
             client.newCall(getAllPagesRequest).execute().use {
-                if (it.code != 200) {
+                if (!it.isSuccessful) {
                     throw Exception("Failed to get page list")
                 }
                 val result = json.parseToJsonElement(it.body.string())
@@ -172,23 +152,76 @@ class ComicGrowl(
         return pageList
     }
 
-    override fun imageRequest(page: Page): Request {
-        val newHeaders = headersBuilder()
-            .set("Referer", baseUrl)
-            .build()
-        return GET(page.imageUrl!!, newHeaders)
+    override fun imageUrlParse(document: Document): String {
+        throw UnsupportedOperationException()
     }
 
+    override fun searchMangaFromElement(element: Element): SManga {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesFromElement(element: Element): SManga {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesNextPageSelector(): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesSelector(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchMangaNextPageSelector(): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchMangaSelector(): String {
+        TODO("Not yet implemented")
+    }
+
+    // ========================================= Helper Functions =====================================
+
+    /**
+     * Set cover image url from [element] for [SManga]
+     */
+    private fun SManga.setImageUrlFromElement(element: Element?) {
+        if (element == null) {
+            return
+        }
+        val match = imageUrlRegex.find(element.selectFirst("source")!!.attr("data-srcset"))
+        // Add missing protocol
+        if (match != null) {
+            this.thumbnail_url = "https:${match.value}"
+        }
+    }
+
+    /**
+     * Set date_upload to [SChapter], parsing from string like "3月31日" to UNIX Epoch time.
+     */
+    private fun SChapter.setUploadDate(element: Element?) {
+        if (element == null) {
+            return
+        }
+        this.date_upload = DATE_PARSER.tryParse(element.attr("datetime"))
+    }
+
+    /**
+     * Interceptor to descramble the image.
+     */
     private fun imageDescrambler(chain: Interceptor.Chain): Response {
         val request = chain.request()
-        val rawRequest = request.newBuilder().url(request.url.newBuilder().fragment(null).build()).build()
-        val response = chain.proceed(rawRequest)
+        val response = chain.proceed(request)
 
-        if (request.url.fragment.isNullOrEmpty()) {
-            return response
-        }
-
-        val scramble = request.url.fragment!!
+        val scramble = request.url.fragment ?: return response // return if no scramble fragment
         val tiles = buildList {
             scramble.split("-").forEachIndexed { index, s ->
                 val scrambleInt = s.toInt()
@@ -237,94 +270,6 @@ class ComicGrowl(
         return descrambledImg
     }
 
-    private data class TilePos(val x: Int, val y: Int) // Left-top corner position
-
-    override fun imageUrlParse(document: Document): String {
-        throw UnsupportedOperationException()
-    }
-
-    override fun searchMangaFromElement(element: Element): SManga {
-        TODO("Not yet implemented")
-    }
-
-    override fun latestUpdatesFromElement(element: Element): SManga {
-        TODO("Not yet implemented")
-    }
-
-    override fun latestUpdatesNextPageSelector(): String? {
-        TODO("Not yet implemented")
-    }
-
-    override fun latestUpdatesRequest(page: Int): Request {
-        TODO("Not yet implemented")
-    }
-
-    override fun latestUpdatesSelector(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun searchMangaNextPageSelector(): String? {
-        TODO("Not yet implemented")
-    }
-
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        TODO("Not yet implemented")
-    }
-
-    override fun searchMangaSelector(): String {
-        TODO("Not yet implemented")
-    }
+    // Left-top corner position
+    private class TilePos(val x: Int, val y: Int)
 }
-
-// // TODO: get manga status
-// // TODO: filter by status
-// // TODO: change cdnUrl as a array(upstream)
-// class ComicGrowl : GigaViewer(
-//    "コミックグロウル",
-//    "https://comic-growl.com",
-//    "all",
-//    "https://cdn-img.comic-growl.com/public/page",
-// ) {
-//
-//    override val publisher = "BUSHIROAD WORKS"
-//
-//    override val chapterListMode = CHAPTER_LIST_LOCKED
-//
-//    override val supportsLatest: Boolean = true
-//
-//    override val client: OkHttpClient =
-//        super.client.newBuilder().addInterceptor(::imageIntercept).build()
-//
-//    override fun popularMangaRequest(page: Int): Request = GET(baseUrl, headers)
-//
-//    // Show only ongoing works
-//    override fun popularMangaSelector(): String = "ul[class=\"lineup-list ongoing\"] > li > div > a"
-//
-//    override fun popularMangaFromElement(element: Element) = SManga.create().apply {
-//        title = element.select("h5").text()
-//        thumbnail_url = element.select("div > img").attr("data-src")
-//        setUrlWithoutDomain(element.attr("href"))
-//    }
-//
-//    override fun latestUpdatesSelector() =
-//        "div[class=\"update latest\"] > div.card-board > " + "div[class~=card]:not([class~=ad]) > div > a"
-//
-//    override fun latestUpdatesFromElement(element: Element) = SManga.create().apply {
-//        title = element.select("div.data h3").text()
-//        thumbnail_url = element.select("div.thumb-container img").attr("data-src")
-//        setUrlWithoutDomain(element.attr("href"))
-//    }
-//
-//    override fun getCollections(): List<Collection> = listOf(
-//        Collection("連載作品", ""),
-//    )
-//
-//    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-//        if (query.isNotEmpty()) {
-//            val url = "$baseUrl/search".toHttpUrl().newBuilder().addQueryParameter("q", query)
-//
-//            return GET(url.build(), headers)
-//        }
-//        return GET(baseUrl, headers) // Currently just get all ongoing works
-//    }
-// }
