46b2fb66a605188aadd6038e686fbba3aaf5a225
are-are-are <62763969+dejavui@users.noreply.github.com>
2025-06-01 09:29:33 +0700

    TruyenVN update domain

--- src/vi/truyenvn/src/eu/kanade/tachiyomi/extension/vi/truyenvn/TruyenVN.kt
+++ src/vi/truyenvn/src/eu/kanade/tachiyomi/extension/vi/truyenvn/TruyenVN.kt
@@ -12,7 +12,7 @@ import java.util.Locale
 class TruyenVN :
     Madara(
         "TruyenVN",
-        "https://truyenvn.ad",
+        "https://truyenvn.shop",
         "vi",
         dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ROOT),
     ),
