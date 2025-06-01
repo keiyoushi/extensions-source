7b757ce2be3ce495025c93573bef3c1d9beebb1b
are-are-are <62763969+dejavui@users.noreply.github.com>
2025-06-01 23:30:57 +0700

    Update some domain  (#9050)
    * HentaiVNPlus update domain
    
    * TruyenVN update domain
    
    * Vlogtruyen update domain
    
    * HentaiCB update domain
    
    * Sayhentai update domain
    
    * DocTruyen3Q update domain
    
    * TopTruyen update domain
    
    * Teamlanhlung update domain

--- src/vi/toptruyen/src/eu/kanade/tachiyomi/extension/vi/toptruyen/TopTruyen.kt
+++ src/vi/toptruyen/src/eu/kanade/tachiyomi/extension/vi/toptruyen/TopTruyen.kt
@@ -23,7 +23,7 @@ import java.util.TimeZone
 class TopTruyen :
     WPComics(
         "Top Truyen",
-        "https://www.toptruyentv6.pro",
+        "https://www.toptruyentv7.pro",
         "vi",
         dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.ROOT).apply {
             timeZone = TimeZone.getTimeZone("Asia/Ho_Chi_Minh")
