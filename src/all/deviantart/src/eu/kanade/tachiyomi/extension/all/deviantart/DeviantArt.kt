b0bd1547fda5543df9f36c39ae3ad457a5b304d9
DokterKaj <54882101+DokterKaj@users.noreply.github.com>
2025-06-09 22:30:56 +0800

    DeviantArt: No need for two variables

--- src/all/deviantart/src/eu/kanade/tachiyomi/extension/all/deviantart/DeviantArt.kt
+++ src/all/deviantart/src/eu/kanade/tachiyomi/extension/all/deviantart/DeviantArt.kt
@@ -160,8 +160,8 @@ class DeviantArt : HttpSource(), ConfigurableSource {
             else -> buttons.mapIndexed { i, button ->
                 // Remove everything past "/v1/" to get original instead of thumbnail
                 // But need to preserve the query parameter where the token is
-                val thumbnailUrl = button.selectFirst("img")?.absUrl("src")
-                val imageUrl = thumbnailUrl?.replaceFirst(Regex("""/v1(/.*)?(?=\?)"""), "")
+                val imageUrl = button.selectFirst("img")?.absUrl("src")
+                    ?.replaceFirst(Regex("""/v1(/.*)?(?=\?)"""), "")
                 Page(i, imageUrl = imageUrl)
             }
         }
