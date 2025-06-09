dd9f87c6fedf5cde7cbd0acc341aed4bd50cec23
Chopper <156493704+choppeh@users.noreply.github.com>
2025-06-08 23:12:42 -0300

    ArgosScan: Fixes (#9135)
    * Fix
    
    * Use relative manga url

--- src/pt/argosscan/src/eu/kanade/tachiyomi/extension/pt/argosscan/ArgosScanDto.kt
+++ src/pt/argosscan/src/eu/kanade/tachiyomi/extension/pt/argosscan/ArgosScanDto.kt
@@ -1,47 +1,42 @@
 package eu.kanade.tachiyomi.extension.pt.argosscan
 
+import eu.kanade.tachiyomi.source.model.SManga
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ArgosResponseDto<T>(
-    val data: Map<String, T>? = null,
-)
+class MangaDto(
+    @SerialName("attributes")
+    val details: DetailsDto,
+    @SerialName("cover_image_url")
+    val thumbnailUrl: String,
+    val id: String,
+    val type: String,
+) {
+    fun toSManga(baseUrl: String) = SManga.create().apply {
+        title = details.title.values.first()
+        description = details.description.values.first()
+        genre = details.genres.joinToString()
+        thumbnail_url = "$baseUrl/$thumbnailUrl"
+        status = details.status.toStatus()
+        url = "/projetos/${this@MangaDto.id}"
+        initialized = true
+    }
+}
+
+fun String.toStatus(): Int =
+    when (lowercase()) {
+        "em-lancamento" -> SManga.ONGOING
+        "completa" -> SManga.COMPLETED
+        "em-pausa" -> SManga.ON_HIATUS
+        else -> SManga.UNKNOWN
+    }
 
 @Serializable
-data class ArgosProjectListDto(
-    val count: Int = 0,
-    val currentPage: Int = 0,
-    val limit: Int = 0,
-    val projects: List<ArgosProjectDto> = emptyList(),
-    val totalPages: Int = 0,
-)
-
-@Serializable
-data class ArgosProjectDto(
-    val adult: Boolean? = false,
-    val alternative: List<String>? = emptyList(),
-    val authors: List<String>? = emptyList(),
-    val cover: String? = "",
-    @SerialName("getChapters") val chapters: List<ArgosChapterDto> = emptyList(),
-    val description: String? = "",
-    val id: Int = 0,
-    val name: String? = "",
-    @SerialName("getTags") val tags: List<ArgosTagDto>? = emptyList(),
-    val type: String? = "",
-)
-
-@Serializable
-data class ArgosChapterDto(
-    val createAt: String? = "",
-    val id: String = "",
-    val images: List<String>? = emptyList(),
-    val number: Int? = 0,
-    val project: ArgosProjectDto? = null,
-    val title: String? = "",
-)
-
-@Serializable
-data class ArgosTagDto(
-    val name: String,
+class DetailsDto(
+    val title: Map<String, String>,
+    val description: Map<String, String> = emptyMap(),
+    val status: String,
+    @SerialName("tags")
+    val genres: List<String> = emptyList(),
 )
