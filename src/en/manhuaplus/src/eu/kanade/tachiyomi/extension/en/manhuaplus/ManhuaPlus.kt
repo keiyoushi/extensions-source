bffaff8a99622d9e2ae6e2e9b73bf0b60824c2f7
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2025-06-14 00:51:03 -0800

    Manhua Plus: Fix `No Results Found` & search error (#9248)
    * Manhua Plus: Bump versionCode
    
    * Manhua Plus: Set `LoadMoreStrategy` to Never

--- src/en/manhuaplus/src/eu/kanade/tachiyomi/extension/en/manhuaplus/ManhuaPlus.kt
+++ src/en/manhuaplus/src/eu/kanade/tachiyomi/extension/en/manhuaplus/ManhuaPlus.kt
@@ -6,6 +6,6 @@ class ManhuaPlus : Madara("Manhua Plus", "https://manhuaplus.com", "en") {
 
     // The website does not flag the content.
     override val filterNonMangaItems = false
-
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
     override val pageListParseSelector = ".read-container img"
 }
