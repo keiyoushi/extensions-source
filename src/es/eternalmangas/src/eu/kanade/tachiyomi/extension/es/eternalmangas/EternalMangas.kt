b05cf57c8419a8538b453dd9db4e317f423e53f1
kanoou <90949336+kanoou@users.noreply.github.com>
2025-06-13 21:46:21 -0500

    move to ES

---
+++ src/es/eternalmangas/src/eu/kanade/tachiyomi/extension/es/eternalmangas/EternalMangas.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.es.eternalmangas
+
+import eu.kanade.tachiyomi.multisrc.iken.Iken
+
+class EternalMangas : Iken(
+    "EternalMangas",
+    "es",
+    "https://eternalmangas.com",
+    "https://api.eternalmangas.com",
+)
