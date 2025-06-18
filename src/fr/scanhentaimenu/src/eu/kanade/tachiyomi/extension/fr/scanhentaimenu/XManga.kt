c5d9fda997b657528360f1dd60d0a87b84a8d00a
Aurel <68382673+Nyantad@users.noreply.github.com>
2025-06-18 10:00:10 -0400

    Fix (X-manga) : Update X-manga URL (#9283)
    Update Url

--- src/fr/scanhentaimenu/src/eu/kanade/tachiyomi/extension/fr/scanhentaimenu/XManga.kt
+++ src/fr/scanhentaimenu/src/eu/kanade/tachiyomi/extension/fr/scanhentaimenu/XManga.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class XManga : Madara("X-Manga", "https://x-manga.net", "fr", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH)) {
+class XManga : Madara("X-Manga", "https://x-manga.org", "fr", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH)) {
     // Scan Hentai Menu -> X-Manga
     override val id = 4153742697148883998
 
