8df9d2a15f36c5197cf2c3c7bfcf6ae9f1d7d01a
FlaminSarge <FlaminSarge@users.noreply.github.com>
2025-06-19 01:47:03 -0700

    Switch from nullable bool to bool for isUnavailable, with default false
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/all/mangadex/src/eu/kanade/tachiyomi/extension/all/mangadex/dto/ChapterDto.kt
+++ src/all/mangadex/src/eu/kanade/tachiyomi/extension/all/mangadex/dto/ChapterDto.kt
@@ -20,7 +20,7 @@ data class ChapterAttributesDto(
     val pages: Int,
     val publishAt: String,
     val externalUrl: String?,
-    val isUnavailable: Boolean? = null,
+    val isUnavailable: Boolean = false,
 ) : AttributesDto() {
 
     /**
