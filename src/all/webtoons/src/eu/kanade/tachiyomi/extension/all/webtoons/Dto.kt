a3624b4c2f289dccfa882e60be1e44ce7c1682a3
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-06-20 07:26:12 +0500

    Webtoons.com: use api to get chapters (#9332)
    * Webtoons.com: use api to get chapters
    
    * completed status
    
    * rename

--- src/all/webtoons/src/eu/kanade/tachiyomi/extension/all/webtoons/Dto.kt
+++ src/all/webtoons/src/eu/kanade/tachiyomi/extension/all/webtoons/Dto.kt
@@ -2,6 +2,27 @@ package eu.kanade.tachiyomi.extension.all.webtoons
 
 import kotlinx.serialization.Serializable
 
+typealias EpisodeListResponse = ResultDto<EpisodeList>
+
+@Serializable
+class ResultDto<T>(
+    val result: T,
+)
+
+@Serializable
+class EpisodeList(
+    val episodeList: List<Episode>,
+)
+
+@Serializable
+class Episode(
+    val episodeNo: Float,
+    val episodeTitle: String,
+    val viewerLink: String,
+    val exposureDateMillis: Long,
+    val hasBgm: Boolean = false,
+)
+
 @Serializable
 class MotionToonResponse(
     val assets: MotionToonAssets,
