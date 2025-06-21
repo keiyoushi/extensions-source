d4d9ddc118055e406e9bab4a11b4440637cd6feb
Chopper <156493704+choppeh@users.noreply.github.com>
2025-06-21 20:32:10 -0300

    TiaManhwa: Update mangaSubString (#9354)
    * Update mangaSubString
    
    * Update mangaSubString

--- src/pt/tiamanhwa/src/eu/kanade/tachiyomi/extension/pt/tiamanhwa/TiaManhwa.kt
+++ src/pt/tiamanhwa/src/eu/kanade/tachiyomi/extension/pt/tiamanhwa/TiaManhwa.kt
@@ -17,7 +17,7 @@ class TiaManhwa : Madara(
         .rateLimit(2)
         .build()
 
-    override val mangaSubString = "manhwa-em-portugues"
+    override val mangaSubString = "manhwa"
 
     override val useLoadMoreRequest = LoadMoreStrategy.Never
 
