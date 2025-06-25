c467c644ea3f96940a6f12569b86aad4e19426d1
AlphaBoom <rsshinide38@gmail.com>
2025-06-25 19:27:30 +0800

    Bakamh: Add missing http header

--- src/zh/bakamh/src/eu/kanade/tachiyomi/extension/zh/bakamh/Bakamh.kt
+++ src/zh/bakamh/src/eu/kanade/tachiyomi/extension/zh/bakamh/Bakamh.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.extension.zh.bakamh
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import okhttp3.Headers
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -11,4 +12,9 @@ class Bakamh : Madara(
     SimpleDateFormat("yyyy 年 M 月 d 日", Locale.CHINESE),
 ) {
     override val mangaDetailsSelectorStatus = ".post-content_item:contains(状态) .summary-content"
+
+    override fun headersBuilder(): Headers.Builder {
+        return super.headersBuilder()
+            .add("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
+    }
 }
