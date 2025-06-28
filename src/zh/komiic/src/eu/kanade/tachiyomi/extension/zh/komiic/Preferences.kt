6ecd72192b07360064c7bea3502951669587961a
周华梁 <642615676@qq.com>
2025-06-28 22:40:44 +0800

    modify config option summary

--- src/zh/komiic/src/eu/kanade/tachiyomi/extension/zh/komiic/Preferences.kt
+++ src/zh/komiic/src/eu/kanade/tachiyomi/extension/zh/komiic/Preferences.kt
@@ -10,7 +10,7 @@ fun preferencesInternal(context: Context) = arrayOf(
     ListPreference(context).apply {
         key = CHAPTER_FILTER_PREF
         title = "章节列表显示"
-        summary = "%s"
+        summary = "注意：部分漫画小概率会有章节缺失，仅显示章节就不能通过卷的内容来补充了。建议不要仅显示卷，会导致无法获取及时章节更新"
         entries = arrayOf("同时显示卷和章节", "仅显示章节", "仅显示卷")
         entryValues = arrayOf("all", "chapter", "book")
         setDefaultValue("all")
