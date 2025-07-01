9b07d3a9fa03f5223474686e3fca62ba7a299963
周华梁 <642615676@qq.com>
2025-07-01 18:10:08 +0800

    modify config summary

--- src/zh/komiic/src/eu/kanade/tachiyomi/extension/zh/komiic/Preferences.kt
+++ src/zh/komiic/src/eu/kanade/tachiyomi/extension/zh/komiic/Preferences.kt
@@ -10,16 +10,16 @@ const val CHECK_API_LIMIT_PREF = "CHECK_API_LIMIT"
 fun preferencesInternal(context: Context) = arrayOf(
     ListPreference(context).apply {
         key = CHAPTER_FILTER_PREF
-        title = "章节列表显示"
-        summary = "注：部分漫画小概率会有章节缺失，仅显示章节就没法通过看卷的内容来补充了。建议不要仅显示卷，会导致无法获取及时章节更新"
-        entries = arrayOf("同时显示卷和章节", "仅显示章节", "仅显示卷")
+        title = "章節列表顯示"
+        summary = "注：部分漫畫小概率會有章節缺失，僅顯示章節就沒法通過看卷的內容來補充了。建議不要僅顯示卷，會導致無法獲取及時章節更新"
+        entries = arrayOf("同時顯示卷和章節", "僅顯示章節", "僅顯示卷")
         entryValues = arrayOf("all", "chapter", "book")
         setDefaultValue("all")
     },
     SwitchPreferenceCompat(context).apply {
         key = CHECK_API_LIMIT_PREF
-        title = "自动检查API是否受限"
-        summary = "每次点击单个章节请求漫画图片时，会自动检查一次图片API是否达到今日请求上限。若已达上限，则终止后续操作"
+        title = "自動檢查API受限"
+        summary = "點擊單個章節請求漫畫圖片時，自動檢查一次圖片API是否達到今日請求上限。若已達上限，則終止後續操作"
         setDefaultValue(true)
     },
 )
