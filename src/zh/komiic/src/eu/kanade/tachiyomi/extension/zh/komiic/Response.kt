616c0d775204844ed206928e41865cb5c99415d5
Hualiang <78242797+hualiong@users.noreply.github.com>
2025-07-02 16:09:36 +0800

    Komiic: Add manga description and refactor some code (#9445)
    * add comic description
    
    * fix manga search results missing descriptions
    
    * clean unused variables
    
    * clean unused class
    
    * Add some config options and refactor some code
    
    * refactor some code
    
    * modify config option summary
    
    * apply comments
    
    * modify Queries.kt
    
    * small modification
    
    * Format code
    
    * replace parse method
    
    * optimize check API limit
    
    * modify config summary
    
    * add getChapterUrl()

--- src/zh/komiic/src/eu/kanade/tachiyomi/extension/zh/komiic/Response.kt
+++ src/zh/komiic/src/eu/kanade/tachiyomi/extension/zh/komiic/Response.kt
@@ -1,141 +1,70 @@
 package eu.kanade.tachiyomi.extension.zh.komiic
 
+import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Data<T>(val data: T)
-
-interface ComicListResult {
-    val comics: List<Comic>
-}
+class Data<T>(val data: Result<T>)
 
 @Serializable
-data class HotComicsResponse(
-    @SerialName("hotComics") override val comics: List<Comic>,
-) : ComicListResult
+class MultiData<T, V>(val data: MultiResult<T, V>)
 
 @Serializable
-data class RecentUpdateResponse(
-    @SerialName("recentUpdate") override val comics: List<Comic>,
-) : ComicListResult
-
-interface SearchResult {
-    val action: ComicsAndAuthors
-}
+class Result<T>(val result: T)
 
 @Serializable
-data class SearchResponse(
-    @SerialName("searchComicsAndAuthors") override val action: ComicsAndAuthors,
-) : SearchResult
+class MultiResult<T, V>(val result1: T, val result2: V)
 
 @Serializable
-data class ComicsAndAuthors(
-    val comics: List<Comic>,
-    val authors: List<Author>,
-    @SerialName("__typename") val typeName: String,
-)
-
-interface ComicResult {
-    val comic: Comic
-}
-
-@Serializable
-data class ComicByIDResponse(
-    @SerialName("comicById") override val comic: Comic,
-) : ComicResult
+data class ComicItem(val id: String, val name: String)
 
 @Serializable
 data class Comic(
     val id: String,
     val title: String,
+    val description: String,
     val status: String,
-    val year: Int,
     val imageUrl: String,
-    var authors: List<ComicAuthor>,
-    val categories: List<ComicCategory>,
-    val dateCreated: String = "",
-    val dateUpdated: String,
-    val monthViews: Int = 0,
-    val views: Int,
-    val favoriteCount: Int,
-    val lastBookUpdate: String,
-    val lastChapterUpdate: String,
-    @SerialName("__typename") val typeName: String,
+    var authors: List<ComicItem>,
+    val categories: List<ComicItem>,
 ) {
-    private val parseStatus = when (status) {
-        "ONGOING" -> SManga.ONGOING
-        "END" -> SManga.COMPLETED
-        else -> SManga.UNKNOWN
-    }
-
     fun toSManga() = SManga.create().apply {
         url = "/comic/$id"
         title = this@Comic.title
         thumbnail_url = this@Comic.imageUrl
         author = this@Comic.authors.joinToString { it.name }
         genre = this@Comic.categories.joinToString { it.name }
-        description = buildString {
-            append("年份: $year | ")
-            append("點閱: ${simplifyNumber(views)} | ")
-            append("喜愛: ${simplifyNumber(favoriteCount)}\n")
+        description = this@Comic.description
+        status = when (this@Comic.status) {
+            "ONGOING" -> SManga.ONGOING
+            "END" -> SManga.COMPLETED
+            else -> SManga.UNKNOWN
         }
-        status = parseStatus
-        initialized = true
+        initialized = this@Comic.description.isNotEmpty()
     }
 }
-
-@Serializable
-data class ComicCategory(
-    val id: String,
-    val name: String,
-    @SerialName("__typename") val typeName: String,
-)
-
-@Serializable
-data class ComicAuthor(
-    val id: String,
-    val name: String,
-    @SerialName("__typename") val typeName: String,
-)
-
-@Serializable
-data class Author(
-    val id: String,
-    val name: String,
-    val chName: String,
-    val enName: String,
-    val wikiLink: String,
-    val comicCount: Int,
-    val views: Int,
-    @SerialName("__typename") val typeName: String,
-)
-
-interface ChaptersResult {
-    val chapters: List<Chapter>
-}
-
-@Serializable
-data class ChaptersResponse(
-    @SerialName("chaptersByComicId") override val chapters: List<Chapter>,
-) : ChaptersResult
 
 @Serializable
 data class Chapter(
     val id: String,
     val serial: String,
     val type: String,
-    val dateCreated: String,
-    val dateUpdated: String,
     val size: Int,
-    @SerialName("__typename") val typeName: String,
-)
-
-@Serializable
-data class ImagesResponse(
-    @SerialName("imagesByChapterId") val images: List<Image>,
-)
+    val dateCreated: String,
+) {
+    fun toSChapter(comicUrl: String, parseDate: (String) -> Long) = SChapter.create().apply {
+        url = "$comicUrl/chapter/${this@Chapter.id}"
+        name = when (this@Chapter.type) {
+            "chapter" -> "第 ${this@Chapter.serial} 話"
+            "book" -> "第 ${this@Chapter.serial} 卷"
+            else -> this@Chapter.serial
+        }
+        scanlator = "${this@Chapter.size}P"
+        date_upload = parseDate(this@Chapter.dateCreated)
+        chapter_number = if (this@Chapter.type == "book") 0F else this@Chapter.serial.toFloatOrNull() ?: -1f
+    }
+}
 
 @Serializable
 data class Image(
@@ -143,18 +72,4 @@ data class Image(
     val kid: String,
     val height: Int,
     val width: Int,
-    @SerialName("__typename") val typeName: String,
-)
-
-@Serializable
-data class APILimitData(
-    @SerialName("getImageLimit") val getImageLimit: APILimit,
-)
-
-@Serializable
-data class APILimit(
-    val limit: Int,
-    val usage: Int,
-    val resetInSeconds: String,
-    @SerialName("__typename") val typeName: String,
 )
