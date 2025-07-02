b55c008759ca6a85372447f311cce0a35abe2a6e
Watcher B <00watcherb@gmail.com>
2025-07-02 20:40:53 +0700

    Update Ikiru import details

--- src/id/mangatale/src/eu/kanade/tachiyomi/extension/id/mangatale/Ikiru.kt
+++ src/id/mangatale/src/eu/kanade/tachiyomi/extension/id/mangatale/Ikiru.kt
@@ -3,17 +3,11 @@ package eu.kanade.tachiyomi.extension.id.mangatale
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.GET
 
-import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.ResponseBody.Companion.toResponseBody
-import org.jsoup.nodes.Document
-
-class Ikiru : MangaThemesia (
+class Ikiru : MangaThemesia(
     "Ikiru",
     "id",
-    "https://id.ikiru.wtf/",
-    "https://api.id.ikiru.wtf/",
+    "https://id.ikiru.wtf",
+    "https://api.id.ikiru.wtf",
 ) {
     override fun popularMangaRequest(page: Int) = GET(baseUrl, headers)
 }
