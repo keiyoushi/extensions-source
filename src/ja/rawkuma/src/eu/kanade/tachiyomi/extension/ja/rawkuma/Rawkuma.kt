23846352e134c141fecca234115717fe656bf945
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2025-07-03 00:34:14 -0800

    Rawkuma: Update domain (#9528)
    * Rawkuma: Bump versionCode
    
    * Rawkuma: Update baseUrl

--- src/ja/rawkuma/src/eu/kanade/tachiyomi/extension/ja/rawkuma/Rawkuma.kt
+++ src/ja/rawkuma/src/eu/kanade/tachiyomi/extension/ja/rawkuma/Rawkuma.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 
-class Rawkuma : MangaThemesia("Rawkuma", "https://rawkuma.com", "ja") {
+class Rawkuma : MangaThemesia("Rawkuma", "https://rawkuma.net", "ja") {
 
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(4)
