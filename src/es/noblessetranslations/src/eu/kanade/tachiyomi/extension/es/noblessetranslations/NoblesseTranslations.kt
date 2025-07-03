e8bcfc008582de43637f8d6bf89fe1cccec552a3
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2025-07-03 00:34:14 -0800

    Rawkuma: Update domain (#9528)
    * Rawkuma: Bump versionCode
    
    * Rawkuma: Update baseUrl

--- src/es/noblessetranslations/src/eu/kanade/tachiyomi/extension/es/noblessetranslations/NoblesseTranslations.kt
+++ src/es/noblessetranslations/src/eu/kanade/tachiyomi/extension/es/noblessetranslations/NoblesseTranslations.kt
@@ -13,7 +13,7 @@ import java.util.Locale
 class NoblesseTranslations :
     Madara(
         "Noblesse Translations",
-        "https://www.swordalada.org",
+        "https://nobledicion.yoveo.xyz",
         "es",
         dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("es")),
     ),
@@ -24,11 +24,9 @@ class NoblesseTranslations :
     override val baseUrl by lazy { getPrefBaseUrl() }
 
     override val useNewChapterEndpoint = true
-    override val useLoadMoreRequest = LoadMoreStrategy.Always
-    override val mangaSubString = "manga"
 
-    override val mangaDetailsSelectorDescription = "div.summary_content > div.post-content div.manga-summary"
-    override val mangaDetailsSelectorStatus = "div.summary_content > div.post-content div.post-content_item:has(div.summary-heading:contains(Status)) div.summary-content"
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+
     override val mangaDetailsSelectorTag = "div.tags-content a.notUsed" // Site uses this for the scanlator
 
     override fun setupPreferenceScreen(screen: PreferenceScreen) {
