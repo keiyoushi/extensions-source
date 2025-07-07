77ad98e380f173ba7b918c70fccd808396ce2863
Hualiang <78242797+hualiong@users.noreply.github.com>
2025-07-08 00:27:34 +0800

    Komiic: Add search filter (#9531)
    * add comic description
    
    * fix manga search results missing descriptions
    
    * clean unused variables
    
    * clean unused class
    
    * Add some config options and refactor some code
    
    * refactor some code
    
    * modify config option summary
    
    * apply comments
    
    * modify Queries.kt
    
    * small modification
    
    * Format code
    
    * Format code
    
    * replace parse method
    
    * optimize check API limit
    
    * modify config summary
    
    * add search filter
    
    * add getChapterUrl()
    
    * refactor Query.kt
    
    * use filters.firstInstance()
    
    * nothing
    
    * Replace require() with check()

--- src/zh/komiic/src/eu/kanade/tachiyomi/extension/zh/komiic/Payload.kt
+++ src/zh/komiic/src/eu/kanade/tachiyomi/extension/zh/komiic/Payload.kt
@@ -12,24 +12,26 @@ data class Payload<T>(
     val operationName: String,
     val variables: T,
     val query: String,
-)
+) {
+    constructor(query: Query, variables: T) : this(query.operation, variables, query.body)
+}
 
 @Serializable
 data class Pagination(
     val offset: Int,
     val orderBy: String,
     @EncodeDefault
-    val limit: Int = Komiic.PAGE_SIZE,
-    @EncodeDefault
     val status: String = "",
     @EncodeDefault
     val asc: Boolean = true,
+    @EncodeDefault
+    val limit: Int = Komiic.PAGE_SIZE,
 )
 
 class Variables {
     val variableMap = mutableMapOf<String, JsonElement>()
 
-    inline fun <reified T> set(key: String, value: T): Variables {
+    inline fun <reified T> field(key: String, value: T): Variables {
         variableMap[key] = Json.encodeToJsonElement(value)
         return this
     }
