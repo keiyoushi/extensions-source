c2a31ff88c8060f9cd26ccd7ebf80179a5cb7129
Chopper <156493704+choppeh@users.noreply.github.com>
2025-07-07 22:43:37 -0300

    Update domains

--- src/es/lectormangalat/src/eu/kanade/tachiyomi/extension/es/lectormangalat/LectorMangaLat.kt
+++ src/es/lectormangalat/src/eu/kanade/tachiyomi/extension/es/lectormangalat/LectorMangaLat.kt
@@ -1,19 +1,35 @@
 package eu.kanade.tachiyomi.extension.es.lectormangalat
 
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.lib.randomua.addRandomUAPreferenceToScreen
+import eu.kanade.tachiyomi.lib.randomua.getPrefCustomUA
+import eu.kanade.tachiyomi.lib.randomua.getPrefUAType
+import eu.kanade.tachiyomi.lib.randomua.setRandomUserAgent
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.ConfigurableSource
+import keiyoushi.utils.getPreferences
 import okhttp3.OkHttpClient
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
-class LectorMangaLat : Madara(
-    "LectorManga.lat",
-    "https://www.lectormanga.lat",
-    "es",
-    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
-) {
+class LectorMangaLat :
+    Madara(
+        "LectorManga.lat",
+        "https://lectormangaa.com",
+        "es",
+        dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
+    ),
+    ConfigurableSource {
+
+    private val preferences = getPreferences()
+
     override val client: OkHttpClient = super.client.newBuilder()
+        .setRandomUserAgent(
+            preferences.getPrefUAType(),
+            preferences.getPrefCustomUA(),
+        )
         .rateLimit(2, 1, TimeUnit.SECONDS)
         .build()
 
@@ -22,4 +38,8 @@ class LectorMangaLat : Madara(
     override val useNewChapterEndpoint = true
 
     override val pageListParseSelector = "div.reading-content div.page-break > img"
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        addRandomUAPreferenceToScreen(screen)
+    }
 }
