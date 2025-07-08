a02bee484939103708b0dd21a13a1ad122b5a9fd
Chopper <156493704+choppeh@users.noreply.github.com>
2025-07-08 14:28:27 -0300

    Add new multi-src: zerotheme (#9544)

---
+++ src/pt/lertoons/src/eu/kanade/tachiyomi/extension/pt/lertoons/LerToons.kt
@@ -0,0 +1,16 @@
+package eu.kanade.tachiyomi.extension.pt.lertoons
+
+import eu.kanade.tachiyomi.multisrc.zerotheme.ZeroTheme
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+
+class LerToons : ZeroTheme(
+    "Ler Toons",
+    "https://lertoons.com",
+    "pt-BR",
+) {
+    override val versionId = 3
+
+    override val client = network.cloudflareClient.newBuilder()
+        .rateLimit(2)
+        .build()
+}
