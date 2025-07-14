f0442741456f22fba6318bef4efc427915dde2c4
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2025-07-14 08:47:55 +0000

    Update MCCMS sources (#9631)
    * Add back Manhuawu, closes #1567
    
    * Clean up 6Manhua
    
    * Add Miaoqu Manhua, closes #4482
    
    * Add 2 French sources

--- src/zh/sixmh/src/eu/kanade/tachiyomi/extension/zh/sixmh/SixMH.kt
+++ src/zh/sixmh/src/eu/kanade/tachiyomi/extension/zh/sixmh/SixMH.kt
@@ -1,35 +1,19 @@
 package eu.kanade.tachiyomi.extension.zh.sixmh
 
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.multisrc.mccms.MCCMSWeb
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.utils.parseAs
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import okhttp3.Request
+import kotlinx.serialization.Serializable
 import okhttp3.Response
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
-class SixMH : SimpleParsedHttpSource() {
+class SixMH : MCCMSWeb("六漫画", "https://www.liumanhua.com") {
     private val paramsRegex = Regex("params = '([A-Za-z0-9+/=]+)'")
     override val versionId get() = 3
-    override val name: String = "六漫画"
-    override val lang: String = "zh"
-    override val supportsLatest: Boolean = true
-    override val baseUrl: String = "https://www.liumanhua.com"
 
-    override fun popularMangaRequest(page: Int) = GET("$baseUrl/category/order/hits/page/$page", headers)
-    override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/category/order/addtime/page/$page", headers)
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val url = "$baseUrl/index.php/search".toHttpUrl().newBuilder()
-            .addQueryParameter("key", query)
-            .build()
-        return GET(url, headers)
-    }
-
-    override fun simpleNextPageSelector(): String? = null
     override fun simpleMangaSelector(): String = "div.cy_list_mh ul"
     override fun simpleMangaFromElement(element: Element): SManga = SManga.create().apply {
         title = element.selectFirst("li.title > a")!!.text()
@@ -38,7 +22,9 @@ class SixMH : SimpleParsedHttpSource() {
     }
 
     // Details
-    override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
+    override fun mangaDetailsParse(response: Response): SManga = SManga.create().apply {
+        val document = response.asJsoup()
+
         val element = document.selectFirst("div.cy_info")!!
         title = element.selectFirst("div.cy_title")!!.text()
         thumbnail_url = element.selectFirst("div.cy_info_cover > a > img.pic")?.absUrl("src")
@@ -47,15 +33,12 @@ class SixMH : SimpleParsedHttpSource() {
         val infoElements = element.select("div.cy_xinxi")
         author = infoElements[0].selectFirst("span:first-child > a")?.text()
         status = parseStatus(infoElements[0].selectFirst("span:nth-child(2)")?.text())
-        genre = infoElements[1].selectFirst("span:first-child > a")?.text()
+        genre = infoElements[1].select("span:first-child > a").joinToString { it.text() }
     }
 
     // Chapters
     override fun chapterListSelector(): String = "ul#mh-chapter-list-ol-0 li.chapter__item"
-    override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
-        setUrlWithoutDomain(element.selectFirst("a")!!.absUrl("href"))
-        name = element.selectFirst("a > p")!!.text()
-    }
+    override fun getDescendingChapters(chapters: List<SChapter>) = chapters
 
     // Pages
     override fun pageListParse(response: Response): List<Page> {
@@ -67,7 +50,8 @@ class SixMH : SimpleParsedHttpSource() {
         return images.mapIndexed { index, url -> Page(index, imageUrl = url) }
     }
 
-    override fun pageListParse(document: Document): List<Page> = throw UnsupportedOperationException()
+    @Serializable
+    private class Data(val images: List<String>)
 
     private fun parseStatus(status: String?): Int {
         return when {
