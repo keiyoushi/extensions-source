11ea8c32e398c5ea5db8acdb98d126df373059de
kanoou <90949336+kanoou@users.noreply.github.com>
2025-07-20 13:04:24 -0500

    Cartel de Manhwas: Update domain (#9733)
    update domain

--- src/es/carteldemanhwas/src/eu/kanade/tachiyomi/extension/es/carteldemanhwas/CarteldeManhwas.kt
+++ src/es/carteldemanhwas/src/eu/kanade/tachiyomi/extension/es/carteldemanhwas/CarteldeManhwas.kt
@@ -1,20 +1,24 @@
 package eu.kanade.tachiyomi.extension.es.carteldemanhwas
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.SChapter
+import okhttp3.Response
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class CarteldeManhwas : MangaThemesia(
     "Cartel de Manhwas",
-    "https://carteldemanhwas.com",
+    "https://carteldemanhwas.net",
     "es",
-    mangaUrlDirectory = "/series",
+    mangaUrlDirectory = "/proyectos",
     dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
 ) {
-    override val hasProjectPage = true
-    override val projectPageString = "/proyectos"
 
     override fun searchMangaSelector() = ".utao .uta .imgu:not(:has(span.novelabel)), " +
         ".listupd .bs .bsx:not(:has(span.novelabel)), " +
         ".listo .bs .bsx:not(:has(span.novelabel))"
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        return super.chapterListParse(response).reversed()
+    }
 }
