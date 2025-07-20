8fbfde969721658cecf276ef2642c451679ca45a
are-are-are <62763969+dejavui@users.noreply.github.com>
2025-07-20 09:59:40 +0700

    Delete PageDTO.kt

--- src/vi/mimihentai/src/eu/kanade/tachiyomi/extension/vi/mimihentai/PageDTO.kt
+++
@@ -1,8 +0,0 @@
-package eu.kanade.tachiyomi.extension.vi.mimihentai
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class PageDTO(
-    val pages: ArrayList<String> = arrayListOf(),
-)
