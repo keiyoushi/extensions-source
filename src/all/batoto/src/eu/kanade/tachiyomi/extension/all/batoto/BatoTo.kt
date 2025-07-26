3da230170366f1c29ae898522f5df1a6b9dd4b10
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-07-26 20:39:04 +0200

    remove proxy variable `mirror`
    Co-authored-by: stevenyomi <95685115+stevenyomi@users.noreply.github.com>

--- src/all/batoto/src/eu/kanade/tachiyomi/extension/all/batoto/BatoTo.kt
+++ src/all/batoto/src/eu/kanade/tachiyomi/extension/all/batoto/BatoTo.kt
@@ -53,7 +53,17 @@ open class BatoTo(
     private val preferences by getPreferencesLazy { migrateMirrorPref() }
 
     override val name: String = "Bato.to"
-    override val baseUrl: String get() = mirror
+
+    override var baseUrl: String = ""
+        get() {
+            val current = field
+            if (current.isNotEmpty()) {
+                return current
+            }
+            field = getMirrorPref()
+            return field
+        }
+
     override val id: Long = when (lang) {
         "zh-Hans" -> 2818874445640189582
         "zh-Hant" -> 38886079663327225
@@ -70,7 +80,7 @@ open class BatoTo(
             setDefaultValue(MIRROR_PREF_DEFAULT_VALUE)
             summary = "%s"
             setOnPreferenceChangeListener { _, newValue ->
-                mirror = newValue as String
+                baseUrl = newValue as String
                 true
             }
         }
@@ -93,16 +103,6 @@ open class BatoTo(
         screen.addPreference(altChapterListPref)
         screen.addPreference(removeOfficialPref)
     }
-
-    private var mirror = ""
-        get() {
-            val current = field
-            if (current.isNotEmpty()) {
-                return current
-            }
-            field = getMirrorPref()
-            return field
-        }
 
     private fun getMirrorPref(): String {
         if (System.getenv("CI") == "true") {
