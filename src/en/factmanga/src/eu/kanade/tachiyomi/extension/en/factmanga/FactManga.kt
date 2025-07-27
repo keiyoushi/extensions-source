d8703410ca4a7506cd90b550e78795f6556f536d
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-07-27 08:36:50 +0200

    Remove dead sources (#9811)
    * Remove CAT-translator
    
    * Remove Jellyring
    
    * Remove ManWe
    
    * Remove Yetiskin Ruya Manga
    
    * Remove Mugi Manga
    
    * Remove Deccal Scans
    
    * Remove FactManga
    
    * Remove Komikuzan
    
    * Remove MangaKazani
    
    * Remove Manga One Love
    
    * Remove Free Manhwa
    
    * Remove Minda Fansub
    
    * Remove SeriManga
    
    * Remove ScamberTraslator
    
    * Remove Arabs Doujin
    
    * Remove DragonManga
    
    * Remove KomikRealm
    
    * Remove Magerin
    
    * Remove YonaBar
    
    * Remove Manga Raw.org
    
    * Remove MangaKomi
    
    * Remove DMC Scans
    
    * Remove Disaster Scans

--- src/en/factmanga/src/eu/kanade/tachiyomi/extension/en/factmanga/FactManga.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.factmanga
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class FactManga : Madara("FactManga", "https://factmanga.com", "en") {
-    override val useNewChapterEndpoint = true
-}
