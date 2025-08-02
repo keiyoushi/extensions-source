caa1c4feb708034f509bbf898910fd38e2349331
Creepler13 <denis.schlichting03@gmail.com>
2025-08-02 22:08:07 +0200

    Kemono: update Url (#9937)

--- src/all/kemono/src/eu/kanade/tachiyomi/extension/all/kemono/Kemono.kt
+++ src/all/kemono/src/eu/kanade/tachiyomi/extension/all/kemono/Kemono.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.extension.all.kemono
 
 import eu.kanade.tachiyomi.multisrc.kemono.Kemono
 
-class Kemono : Kemono("Kemono", "https://kemono.su", "all") {
+class Kemono : Kemono("Kemono", "https://kemono.cr", "all") {
     override val getTypes = listOf(
         "Patreon",
         "Pixiv Fanbox",
