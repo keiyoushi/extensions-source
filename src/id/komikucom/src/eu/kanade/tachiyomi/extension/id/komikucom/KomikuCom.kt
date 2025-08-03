36027a3ef4a4e99a4d915e5d615484f74c3eff99
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-08-03 10:25:26 +0200

    Komiku.com: update domain (#9926)

--- src/id/komikucom/src/eu/kanade/tachiyomi/extension/id/komikucom/KomikuCom.kt
+++ src/id/komikucom/src/eu/kanade/tachiyomi/extension/id/komikucom/KomikuCom.kt
@@ -4,4 +4,4 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class KomikuCom : MangaThemesia("Komiku.com", "https://komiku.one", "id", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id")))
+class KomikuCom : MangaThemesia("Komiku.com", "https://01.komiku.asia", "id", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id")))
