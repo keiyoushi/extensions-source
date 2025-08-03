d42db5482077516af2e32950e01e762cd1bb6afc
osamu00 <osamu.kozu@gmail.com>
2025-08-03 17:06:35 +0200

    Update src/fr/scanmanga/src/eu/kanade/tachiyomi/extension/fr/scanmanga/ScanManga.kt
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>

--- src/fr/scanmanga/src/eu/kanade/tachiyomi/extension/fr/scanmanga/ScanManga.kt
+++ src/fr/scanmanga/src/eu/kanade/tachiyomi/extension/fr/scanmanga/ScanManga.kt
@@ -104,7 +104,7 @@ class ScanManga : ParsedHttpSource() {
             headersBuilder()
                 .add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36")
                 .add("Accept-Language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7")
-                .add("Referer", desktopBaseUrl)
+                .add("Referer", "$desktopBaseUrl/")
                 .build(),
         )
     }
