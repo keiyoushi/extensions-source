273b1f25169cd818f8196c86d3fd98471b36e583
D-Brox <davidfbg00@gmail.com>
2025-08-04 09:34:53 -0300

    ref: suggested changes

--- src/en/rokaricomics/src/eu/kanade/tachiyomi/extension/en/rokaricomics/RokariComics.kt
+++ src/en/rokaricomics/src/eu/kanade/tachiyomi/extension/en/rokaricomics/RokariComics.kt
@@ -10,13 +10,23 @@ class RokariComics : MangaThemesia(
     "en",
 ) {
     override fun getFilterList(): FilterList {
-        return FilterList(
-            Filter.Separator(),
+        val filters = mutableListOf<Filter<*>>(
             StatusFilter(intl["status_filter_title"], statusOptions),
             TypeFilter(intl["type_filter_title"], typeFilterOptions),
             OrderByFilter(intl["order_by_filter_title"], orderByFilterOptions),
-            Filter.Header(intl["genre_exclusion_warning"]),
-            GenreListFilter(intl["genre_filter_title"], getGenreList()),
         )
+        if (!genrelist.isNullOrEmpty()) {
+            filters.addAll(
+                listOf(
+                    Filter.Header(intl["genre_exclusion_warning"]),
+                    GenreListFilter(intl["genre_filter_title"], getGenreList()),
+                ),
+            )
+        } else {
+            filters.add(
+                Filter.Header(intl["genre_missing_warning"]),
+            )
+        }
+        return FilterList(filters)
     }
 }
