a5e9fa7aadbf10a421fafb153d8b57ed5cfc92c7
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2025-08-07 16:07:48 +0000

    Just use `parse(String, ParsePosition)`

--- core/src/main/kotlin/keiyoushi/utils/Date.kt
+++ core/src/main/kotlin/keiyoushi/utils/Date.kt
@@ -1,14 +1,10 @@
 package keiyoushi.utils
 
-import java.text.ParseException
+import java.text.ParsePosition
 import java.text.SimpleDateFormat
 
 fun SimpleDateFormat.tryParse(date: String?): Long {
     date ?: return 0L
 
-    return try {
-        parse(date)!!.time
-    } catch (_: ParseException) {
-        0L
-    }
+    return parse(date, ParsePosition(0))?.time ?: 0L
 }
