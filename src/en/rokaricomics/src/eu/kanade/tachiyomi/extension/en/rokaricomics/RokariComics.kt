d2ed2e07d372a20405f6646c0a09b04a5d3ab2f8
D-Brox <davidfbg00@gmail.com>
2025-08-07 09:01:14 -0300

    simplify filter list

--- src/en/rokaricomics/src/eu/kanade/tachiyomi/extension/en/rokaricomics/RokariComics.kt
+++ src/en/rokaricomics/src/eu/kanade/tachiyomi/extension/en/rokaricomics/RokariComics.kt
@@ -1,7 +1,6 @@
 package eu.kanade.tachiyomi.extension.en.rokaricomics
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 
 class RokariComics : MangaThemesia(
@@ -12,23 +11,7 @@ class RokariComics : MangaThemesia(
     override fun chapterListSelector() = "#chapterlist li:has(div.chbox):has(div.eph-num):has(a[href])"
 
     override fun getFilterList(): FilterList {
-        val filters = mutableListOf<Filter<*>>(
-            StatusFilter(intl["status_filter_title"], statusOptions),
-            TypeFilter(intl["type_filter_title"], typeFilterOptions),
-            OrderByFilter(intl["order_by_filter_title"], orderByFilterOptions),
-        )
-        if (!genrelist.isNullOrEmpty()) {
-            filters.addAll(
-                listOf(
-                    Filter.Header(intl["genre_exclusion_warning"]),
-                    GenreListFilter(intl["genre_filter_title"], getGenreList()),
-                ),
-            )
-        } else {
-            filters.add(
-                Filter.Header(intl["genre_missing_warning"]),
-            )
-        }
+        val filters = super.getFilterList().filterNot { it is AuthorFilter || it is YearFilter }
         return FilterList(filters)
     }
 }
