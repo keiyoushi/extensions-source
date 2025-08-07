0c65ee97077db1525a5b27d4826322aec20ff170
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2025-08-07 15:33:37 +0000

    Update Date.kt

--- core/src/main/kotlin/keiyoushi/utils/Date.kt
+++ core/src/main/kotlin/keiyoushi/utils/Date.kt
@@ -3,12 +3,11 @@ package keiyoushi.utils
 import java.text.ParseException
 import java.text.SimpleDateFormat
 
-@Suppress("NOTHING_TO_INLINE")
-inline fun SimpleDateFormat.tryParse(date: String?): Long {
+fun SimpleDateFormat.tryParse(date: String?): Long {
     date ?: return 0L
 
     return try {
-        parse(date)?.time ?: 0L
+        parse(date)!!.time
     } catch (_: ParseException) {
         0L
     }
