5a24c588fe98b63a0412c25cea5492440d927fc3
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-08-11 04:50:50 +0200

    Update ww# subdomain, sync titles (#10089)

--- src/en/readhaikyuumangaonline/src/eu/kanade/tachiyomi/extension/en/readhaikyuumangaonline/ReadHaikyuuMangaOnline.kt
+++ src/en/readhaikyuumangaonline/src/eu/kanade/tachiyomi/extension/en/readhaikyuumangaonline/ReadHaikyuuMangaOnline.kt
@@ -2,11 +2,12 @@ package eu.kanade.tachiyomi.extension.en.readhaikyuumangaonline
 
 import eu.kanade.tachiyomi.multisrc.mangacatalog.MangaCatalog
 
-class ReadHaikyuuMangaOnline : MangaCatalog("Read Haikyuu!! Manga Online", "https://ww6.readhaikyuu.com", "en") {
+class ReadHaikyuuMangaOnline : MangaCatalog("Read Haikyuu!! Manga Online", "https://ww9.readhaikyuu.com", "en") {
     override val sourceList = listOf(
         Pair("Haikyuu", "$baseUrl/manga/haikyuu/"),
         Pair("Haikyuu-bu!!", "$baseUrl/manga/haikyuu-bu/"),
         Pair("Haikyuu! x Nisekoi", "$baseUrl/manga/haikyuu-x-nisekoi/"),
         Pair("Let’s! Haikyu!?", "$baseUrl/manga/lets-haikyu/"),
-    ).sortedBy { it.first }.distinctBy { it.second }
+        Pair("Colored", "$baseUrl/manga/haikyu-digital-colored-comics/"),
+    )
 }
