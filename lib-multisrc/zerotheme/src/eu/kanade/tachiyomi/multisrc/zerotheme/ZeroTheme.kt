bd6c6113eae0660180aa42454d3088f9b89b095f
Chopper <156493704+choppeh@users.noreply.github.com>
2025-08-13 12:51:16 -0300

    Throws an exception when old entries are being processed

--- lib-multisrc/zerotheme/src/eu/kanade/tachiyomi/multisrc/zerotheme/ZeroTheme.kt
+++ lib-multisrc/zerotheme/src/eu/kanade/tachiyomi/multisrc/zerotheme/ZeroTheme.kt
@@ -1,7 +1,6 @@
 package eu.kanade.tachiyomi.multisrc.zerotheme
 
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
@@ -13,6 +12,8 @@ import keiyoushi.utils.parseAs
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
+import org.jsoup.nodes.Element
+import java.io.IOException
 
 abstract class ZeroTheme(
     override val name: String,
@@ -22,15 +23,22 @@ abstract class ZeroTheme(
 
     override val supportsLatest: Boolean = true
 
-    override val client = network.cloudflareClient.newBuilder()
-        .rateLimit(2)
-        .build()
+    override val client = network.cloudflareClient
 
     open val cdnUrl: String = "https://cdn.${baseUrl.substringAfterLast("/")}"
 
     open val imageLocation: String = "images"
 
-    open val mangaSubString: String = "comic"
+    open val mangaSubString: String by lazy {
+        val response = client.newCall(GET(baseUrl, headers)).execute()
+        val script = response.asJsoup().select("script")
+            .map(Element::data)
+            .firstOrNull(MANGA_SUBSTRING_REGEX::containsMatchIn)
+            ?: throw IOException("manga substring não foi localizado")
+
+        MANGA_SUBSTRING_REGEX.find(script)?.groups?.get(1)?.value
+            ?: throw IOException("Não foi extrair a substring do manga")
+    }
 
     open val chapterSubString: String = "chapter"
 
@@ -69,7 +77,10 @@ abstract class ZeroTheme(
 
     override fun getMangaUrl(manga: SManga) = "$baseUrl/$mangaSubString/${manga.url.substringAfterLast("/")}"
 
-    override fun mangaDetailsRequest(manga: SManga) = GET(getMangaUrl(manga), headers)
+    override fun mangaDetailsRequest(manga: SManga): Request {
+        checkEntry(manga.url)
+        return GET(getMangaUrl(manga), headers)
+    }
 
     override fun mangaDetailsParse(response: Response) = response.toDto<MangaDetailsDto>().toSManga(sourceLocation)
 
@@ -83,7 +94,10 @@ abstract class ZeroTheme(
 
     // =========================== Pages ===================================
 
-    override fun pageListRequest(chapter: SChapter) = GET(getChapterUrl(chapter), headers)
+    override fun pageListRequest(chapter: SChapter): Request {
+        checkEntry(chapter.url)
+        return GET(getChapterUrl(chapter), headers)
+    }
 
     override fun pageListParse(response: Response): List<Page> =
         response.toDto<PageDto>().toPageList(sourceLocation)
@@ -92,8 +106,18 @@ abstract class ZeroTheme(
 
     // =========================== Utilities ===============================
 
+    private fun checkEntry(url: String) {
+        if (listOf(mangaSubString, chapterSubString).any(url::contains)) {
+            throw IOException("Migre a obra para extensão $name")
+        }
+    }
+
     inline fun <reified T> Response.toDto(): T {
         val jsonString = asJsoup().selectFirst("[data-page]")!!.attr("data-page")
         return jsonString.parseAs<T>()
+    }
+
+    companion object {
+        val MANGA_SUBSTRING_REGEX = """"(\w+)\\/\{slug\}""".toRegex()
     }
 }
