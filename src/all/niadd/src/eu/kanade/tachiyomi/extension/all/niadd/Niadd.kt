529015357a92976ed8a42095cd6c2dfc8733d842
Zev Lonewolf <gleidsongonzagasilva@gmail.com>
2025-08-15 16:28:41 -0400

    Organização de pastas

--- src/all/niadd/src/eu/kanade/tachiyomi/extension/all/niadd/Niadd.kt
+++ src/all/niadd/src/eu/kanade/tachiyomi/extension/all/niadd/Niadd.kt
@@ -1,13 +1,6 @@
 package eu.kanade.tachiyomi.extension.all.niadd
 
 import eu.kanade.tachiyomi.source.SourceFactory
-import eu.kanade.tachiyomi.extension.de.niadd.NiaddDe
-import eu.kanade.tachiyomi.extension.en.niadd.NiaddEn
-import eu.kanade.tachiyomi.extension.fr.niadd.NiaddFr
-import eu.kanade.tachiyomi.extension.it.niadd.NiaddIt
-import eu.kanade.tachiyomi.extension.pt.niadd.NiaddPtBr
-import eu.kanade.tachiyomi.extension.es.niadd.NiaddEs
-import eu.kanade.tachiyomi.extension.ru.niadd.NiaddRu
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
