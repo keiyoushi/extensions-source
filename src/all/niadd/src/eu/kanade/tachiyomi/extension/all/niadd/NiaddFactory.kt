ebd8b64e7880e55a2d415fdb53907b24688b0168
Zev Lonewolf <gleidsongonzagasilva@gmail.com>
2025-08-15 12:02:10 -0400

    Correção da Atualização Final

--- src/all/niadd/src/eu/kanade/tachiyomi/extension/all/niadd/NiaddFactory.kt
+++ src/all/niadd/src/eu/kanade/tachiyomi/extension/all/niadd/NiaddFactory.kt
@@ -1,9 +1,22 @@
 package eu.kanade.tachiyomi.extension.en.niadd
 
 import eu.kanade.tachiyomi.source.SourceFactory
+import eu.kanade.tachiyomi.extension.de.niadd.NiaddDe
+import eu.kanade.tachiyomi.extension.en.niadd.NiaddEn
+import eu.kanade.tachiyomi.extension.fr.niadd.NiaddFr
+import eu.kanade.tachiyomi.extension.it.niadd.NiaddIt
+import eu.kanade.tachiyomi.extension.pt.niadd.NiaddPTbr
+import eu.kanade.tachiyomi.extension.es.niadd.NiaddEs
+import eu.kanade.tachiyomi.extension.ru.niadd.NiaddRu
 
 class NiaddFactory : SourceFactory {
     override fun createSources() = listOf(
+        NiaddDe(),
         NiaddEn(),
+        NiaddEs(),
+        NiaddFr(),
+        NiaddIt(),
+        NiaddPTbr(),
+        NiaddRu()
     )
 }
