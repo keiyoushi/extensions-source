9e7237356f7843e0837afc39f894bbd3a526a1f6
Zev Lonewolf <gleidsongonzagasilva@gmail.com>
2025-08-16 17:32:41 -0400

    Ghost error 3

--- src/pt/niadd/src/eu/kanade/tachiyomi/extension/pt/niadd/NiaddPtBr.kt
+++ src/pt/niadd/src/eu/kanade/tachiyomi/extension/pt/niadd/NiaddPtBr.kt
@@ -4,10 +4,12 @@ import eu.kanade.tachiyomi.source.SourceFactory
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.network.GET
-import okhttp3.Response
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.FilterList
+import rx.Observable
 import okhttp3.Request
-import org.jsoup.nodes.Document
+import okhttp3.Response
 
 class NiaddPtBr : HttpSource() {
 
@@ -16,18 +18,30 @@ class NiaddPtBr : HttpSource() {
     override val lang = "pt-BR"
     override val supportsLatest = false
 
-    // Lista de mangás populares - por enquanto retorna vazio
-    override fun fetchPopularManga(page: Int) = MangasPage(emptyList(), false)
+    // Popular mangás - retorna vazio
+    override fun fetchPopularManga(page: Int): Observable<MangasPage> =
+        Observable.just(MangasPage(emptyList(), false))
 
-    // Pesquisa de mangás - por enquanto retorna vazio
-    override fun fetchSearchManga(page: Int, query: String, filters: List<Filter>) =
-        MangasPage(emptyList(), false)
+    // Busca de mangás - retorna vazio
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> =
+        Observable.just(MangasPage(emptyList(), false))
 
-    // Mangás mais recentes - não suportado
-    override fun fetchLatestUpdates(page: Int) = throw UnsupportedOperationException("Not supported")
+    // Detalhes do mangá - apenas retorna o SManga original
+    override fun fetchMangaDetails(manga: SManga): Observable<SManga> =
+        Observable.just(manga)
 
-    // Requerido pelo HttpSource mas ainda vazio
-    override fun fetchMangaDetails(manga: SManga) = manga
-    override fun fetchChapterList(manga: SManga) = emptyList<Chapter>()
-    override fun fetchPageList(chapter: Chapter) = emptyList<Page>()
+    // Lista de capítulos - retorna vazia
+    override fun fetchChapterList(manga: SManga): Observable<List<SChapter>> =
+        Observable.just(emptyList())
+
+    // Lista de páginas - retorna vazia
+    override fun fetchPageList(chapter: SChapter): Observable<List<Page>> =
+        Observable.just(emptyList())
+
+    // Métodos obrigatórios de parse (não usados ainda)
+    override fun popularMangaRequest(page: Int): Request = throw NotImplementedError()
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request = throw NotImplementedError()
+    override fun mangaDetailsParse(response: Response): SManga = throw NotImplementedError()
+    override fun chapterListParse(response: Response): List<SChapter> = throw NotImplementedError()
+    override fun pageListParse(response: Response): List<Page> = throw NotImplementedError()
 }
