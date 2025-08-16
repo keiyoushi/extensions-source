b3e5a4a354a666b8bf253b1a6cb91c4cebc01d3c
Chopper <156493704+choppeh@users.noreply.github.com>
2025-08-16 03:52:29 -0300

    Fix chapter and pages

--- src/pt/manhastro/src/eu/kanade/tachiyomi/extension/pt/manhastro/Manhastro.kt
+++ src/pt/manhastro/src/eu/kanade/tachiyomi/extension/pt/manhastro/Manhastro.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.extension.pt.manhastro
 
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
@@ -8,15 +9,10 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
 import keiyoushi.utils.parseAs
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.Request
 import okhttp3.Response
 import rx.Observable
+import java.io.IOException
 import java.text.Normalizer
 
 class Manhastro : HttpSource() {
@@ -25,43 +21,34 @@ class Manhastro : HttpSource() {
 
     override val baseUrl: String = "https://manhastro.net"
 
-    override val client = network.cloudflareClient
+    override val lang: String = "pt-BR"
+
+    override val client = network.cloudflareClient.newBuilder()
+        .rateLimit(2)
+        .build()
 
     private val apiUrl: String = "https://api2.manhastro.net"
 
-    override val lang: String = "pt-BR"
+    private val mangaSubString = "manga"
 
-    private val database: Map<String, JsonElement> by lazy {
+    private val oldMangaSubString = "lermanga"
+
+    private val database: Map<Int, MangaDto> by lazy {
         client.newCall(GET("$apiUrl/dados"))
             .execute()
-            .parseAs<JsonObject>()["data"]!!.jsonArray
-            .associateBy { it.jsonObject["manga_id"]!!.jsonPrimitive.toString() }
+            .parseAs<ResponseWrapper<List<MangaDto>>>()
+            .data
+            .associateBy { it.id }
     }
 
     override val supportsLatest: Boolean = true
 
     override fun popularMangaRequest(page: Int) = GET("$apiUrl/rank/mensal", headers)
 
-    private fun JsonElement.toSManga(): SManga {
-        val entry = this.jsonObject
-        return SManga.create().apply {
-            title = entry["titulo"]!!.jsonPrimitive.content
-            description = entry["descricao"]?.jsonPrimitive?.content
-            thumbnail_url = entry["imagem"]?.let { "https://${it.jsonPrimitive.content}" }
-            url = "/manga/${entry["manga_id"]!!.jsonPrimitive.content}"
-            genre = entry["generos"]?.jsonPrimitive
-                ?.content
-                ?.parseAs<List<String>>()
-                ?.joinToString()
-            initialized = true
-        }
-    }
     override fun popularMangaParse(response: Response): MangasPage {
         val mangas = response.parseAs<ResponseWrapper<List<PopularMangaDto>>>().data
             .distinctBy(PopularMangaDto::id)
-            .mapNotNull { dto ->
-                database[dto.id.toString()]?.let { jsonElement -> jsonElement.toSManga() }
-            }
+            .mapNotNull { dto -> database[dto.id]?.toSManga() }
         return MangasPage(mangas, false)
     }
 
@@ -71,10 +58,8 @@ class Manhastro : HttpSource() {
 
     override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> {
         return Observable.fromCallable {
-            val mangas = database.values.filter { jsonElement ->
-                val entry = jsonElement.jsonObject
-                entry["titulo"]!!.jsonPrimitive.content.contains(query) ||
-                    entry["titulo_brasil"]?.jsonPrimitive?.content?.contains(query) ?: false
+            val mangas = database.values.filter { dto ->
+                dto.title.contains(query) || dto.titleLocalized?.contains(query) ?: false
             }.map { it.toSManga() }
             MangasPage(mangas, hasNextPage = false)
         }
@@ -84,23 +69,60 @@ class Manhastro : HttpSource() {
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList) = throw UnsupportedOperationException()
 
-    override fun mangaDetailsParse(response: Response): SManga {
-        TODO("Not yet implemented")
+    override fun getMangaUrl(manga: SManga): String {
+        if (manga.url.contains(oldMangaSubString, ignoreCase = true)) {
+            throw IOException("Migrate a obra para a $name")
+        }
+        return "$baseUrl/$mangaSubString/${manga.url}"
+    }
+
+    override fun fetchMangaDetails(manga: SManga): Observable<SManga> {
+        return Observable.fromCallable {
+            if (manga.url.contains(oldMangaSubString)) {
+                return@fromCallable tryCompatibility(manga)
+            }
+            database[manga.url.toInt()]?.toSManga() ?: throw IOException("Não encontrado")
+        }
+    }
+
+    override fun mangaDetailsParse(response: Response) = throw UnsupportedOperationException()
+
+    override fun chapterListRequest(manga: SManga): Request {
+        val url = if (manga.url.contains(oldMangaSubString)) tryCompatibility(manga).url else manga.url
+        return GET("$apiUrl/dados/$url", headers)
+    }
+
+    override fun getChapterUrl(chapter: SChapter): String {
+        val ids = chapter.url.split(":")
+        return "$baseUrl/$mangaSubString/${ids.first()}/chapter/${ids.last()}"
     }
 
     override fun chapterListParse(response: Response): List<SChapter> {
-        TODO("Not yet implemented")
+        val mangaId = response.request.url.pathSegments.last().toInt()
+        return response.parseAs<ResponseWrapper<List<ChapterDto>>>().data
+            .map { it.toSChapter(mangaId) }
+            .sortedByDescending(SChapter::chapter_number)
+    }
+
+    override fun pageListRequest(chapter: SChapter): Request {
+        val chapterId = chapter.url.substringAfterLast(":")
+        return GET("$apiUrl/paginas/$chapterId", headers)
     }
 
     override fun pageListParse(response: Response): List<Page> {
-        TODO("Not yet implemented")
+        return response.parseAs<ResponseWrapper<PageDto>>().data.toPageList()
     }
 
-    override fun imageUrlParse(response: Response): String {
-        TODO("Not yet implemented")
-    }
+    override fun imageUrlParse(response: Response): String = ""
 
     // ============================== Utilities =================================
+
+    private fun tryCompatibility(manga: SManga): SManga {
+        val slug = manga.url.substringAfterLast("$oldMangaSubString/")
+        return database.values.firstOrNull { it.slug == slug }
+            ?.toSManga()
+            ?: throw IOException("Migrate a obra para a $name")
+    }
 
     private fun String.normalize(): String {
         return Normalizer.normalize(this, Normalizer.Form.NFD)
--- src/pt/manhastro/src/eu/kanade/tachiyomi/extension/pt/manhastro/ManhastroDto.kt
+++ src/pt/manhastro/src/eu/kanade/tachiyomi/extension/pt/manhastro/ManhastroDto.kt
@@ -1,8 +1,14 @@
 package eu.kanade.tachiyomi.extension.pt.manhastro
 
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import keiyoushi.utils.parseAs
+import keiyoushi.utils.tryParse
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Serializable
 class ResponseWrapper<T>(
@@ -30,11 +36,66 @@ class MangaDto(
     @SerialName("generos")
     val genres: String?,
 ) {
+
+    val slug: String = title.replace(" ", "-")
     fun toSManga(): SManga = SManga.create().apply {
         title = this@MangaDto.title
-        description = this@MangaDto.description
+        description = """
+            ${this@MangaDto.description}
+
+            Nome alternativo: $titleLocalized
+        """.trimIndent()
         thumbnail_url = "https://$thumbnailUrl"
-        url = "/manga/${this@MangaDto.id}"
-        genre = this@MangaDto.genres
+        url = this@MangaDto.id.toString()
+        genre = this@MangaDto.genres?.parseAs<List<String>>()?.joinToString()
+        initialized = true
+    }
+}
+
+@Serializable
+class ChapterDto(
+    @SerialName("capitulo_id")
+    val id: Int,
+    @SerialName("capitulo_nome")
+    val name: String,
+
+    @SerialName("capitulo_data")
+    val date: String,
+) {
+    fun toSChapter(mangaId: Int) = SChapter.create().apply {
+        name = this@ChapterDto.name
+        date_upload = dateFormat.tryParse(date)
+        NUMBER_REGEX.find(name)?.groupValues?.firstOrNull()?.let {
+            chapter_number = it.toFloat()
+        }
+        url = "$mangaId:${this@ChapterDto.id}"
+    }
+
+    companion object {
+        private val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT)
+        private val NUMBER_REGEX = """\d+(?:\.\d+)?""".toRegex()
+    }
+}
+
+@Serializable
+class PageDto(
+    val chapter: ItemDto,
+) {
+    @Serializable
+    class ItemDto(
+        @SerialName("baseUrl")
+        val url: String,
+        @SerialName("hash")
+        val pathSegment: String,
+        @SerialName("data")
+        val images: List<String>,
+    )
+
+    fun toPageList(): List<Page> {
+        return with(chapter) {
+            images.mapIndexed { index, image ->
+                Page(index, imageUrl = "$url/$pathSegment/$image")
+            }
+        }
     }
 }
