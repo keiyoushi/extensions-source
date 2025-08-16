155d3736460aba638733703c320fd57818273846
Zev Lonewolf <gleidsongonzagasilva@gmail.com>
2025-08-16 17:27:21 -0400

    ghost error 2

--- src/pt/niadd/src/eu/kanade/tachiyomi/extension/pt/niadd/NiaddPtBr.kt
+++ src/pt/niadd/src/eu/kanade/tachiyomi/extension/pt/niadd/NiaddPtBr.kt
@@ -1,22 +1,33 @@
 package eu.kanade.tachiyomi.extension.pt.niadd
 
 import eu.kanade.tachiyomi.source.SourceFactory
-import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.network.GET
+import okhttp3.Response
 import okhttp3.Request
 import org.jsoup.nodes.Document
-import rx.Observable
 
 class NiaddPtBr : HttpSource() {
+
     override val name = "Niadd PT-BR"
     override val baseUrl = "https://br.niadd.com"
-    override val lang = "pt"
+    override val lang = "pt-BR"
     override val supportsLatest = false
 
-    // retorna uma request de busca, mesmo que vazia
-    override fun fetchPopularManga(page: Int) = Observable.just(emptyList<SManga>())
-    override fun fetchSearchManga(page: Int, query: String, filters: FilterList) = Observable.just(emptyList<SManga>())
-    override fun fetchMangaDetails(manga: SManga) = Observable.just(manga)
-    override fun fetchChapterList(manga: SManga) = Observable.just(emptyList())
-    override fun fetchPageList(chapter: SChapter) = Observable.just(emptyList())
+    // Lista de mangás populares - por enquanto retorna vazio
+    override fun fetchPopularManga(page: Int) = MangasPage(emptyList(), false)
+
+    // Pesquisa de mangás - por enquanto retorna vazio
+    override fun fetchSearchManga(page: Int, query: String, filters: List<Filter>) =
+        MangasPage(emptyList(), false)
+
+    // Mangás mais recentes - não suportado
+    override fun fetchLatestUpdates(page: Int) = throw UnsupportedOperationException("Not supported")
+
+    // Requerido pelo HttpSource mas ainda vazio
+    override fun fetchMangaDetails(manga: SManga) = manga
+    override fun fetchChapterList(manga: SManga) = emptyList<Chapter>()
+    override fun fetchPageList(chapter: Chapter) = emptyList<Page>()
 }
