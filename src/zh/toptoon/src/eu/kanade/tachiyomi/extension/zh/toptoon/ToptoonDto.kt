838cc404e931d3127e629e3389ef2f4264e95188
Tanaka Shizuku <tanaka-shizuku@proton.me>
2025-08-17 18:22:03 +0800

    Fix ThumbnailDto

--- src/zh/toptoon/src/eu/kanade/tachiyomi/extension/zh/toptoon/ToptoonDto.kt
+++ src/zh/toptoon/src/eu/kanade/tachiyomi/extension/zh/toptoon/ToptoonDto.kt
@@ -1,15 +1,9 @@
 package eu.kanade.tachiyomi.extension.zh.toptoon
 
 import eu.kanade.tachiyomi.source.model.SManga
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.jsonPrimitive
 
@@ -37,30 +31,15 @@ class MetaDto(val title: String, val author: AuthorDto)
 @Serializable
 class AuthorDto(val authorString: String)
 
-@Serializable
 // "standard" in json can be either string or array, always decode as string
-class ThumbnailDto(@Serializable(with = StringOrArraySerializer::class) val standard: String) {
-    val url = "https://tw-contents-image.toptoon.net$standard"
+@Serializable
+class ThumbnailDto(private val standard: JsonElement) {
+    val url get() = when (standard) {
+        is JsonPrimitive -> "https://tw-contents-image.toptoon.net${standard.content}"
+        is JsonArray -> "https://tw-contents-image.toptoon.net${standard[0].jsonPrimitive.content}"
+        else -> throw Exception("Unexpected JSON type")
+    }
 }
 
 @Serializable
 class LastUpdatedDto(val pubDate: String)
-
-object StringOrArraySerializer : KSerializer<String> {
-    override val descriptor: SerialDescriptor =
-        PrimitiveSerialDescriptor("StringOrArray", PrimitiveKind.STRING)
-
-    override fun deserialize(decoder: Decoder): String {
-        val jsonDecoder = decoder as JsonDecoder
-        val element = jsonDecoder.decodeJsonElement()
-        return when (element) {
-            is JsonPrimitive -> element.content
-            is JsonArray -> element[0].jsonPrimitive.content
-            else -> throw Exception("Unexpected JSON type")
-        }
-    }
-
-    override fun serialize(encoder: Encoder, value: String) {
-        encoder.encodeString(value)
-    }
-}
