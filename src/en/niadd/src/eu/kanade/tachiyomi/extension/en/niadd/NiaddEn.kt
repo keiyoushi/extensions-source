ff28436ac141d9d2169c5d5895370e8a8e854f83
Zev <gleidsongonzagasilva@gmail.com>
2025-08-18 17:38:05 -0400

    Update NiaddEn.kt

--- src/en/niadd/src/eu/kanade/tachiyomi/extension/en/niadd/NiaddEn.kt
+++ src/en/niadd/src/eu/kanade/tachiyomi/extension/en/niadd/NiaddEn.kt
@@ -8,6 +8,7 @@ import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Request
+import okhttp3.Headers
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.net.URLEncoder
