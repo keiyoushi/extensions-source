178ca644eedd09926d279f582b33f71553bd331d
Zev <gleidsongonzagasilva@gmail.com>
2025-08-18 11:12:06 -0400

    Update NiaddEn.kt

--- src/en/niadd/src/eu/kanade/tachiyomi/extension/en/niadd/NiaddEn.kt
+++ src/en/niadd/src/eu/kanade/tachiyomi/extension/en/niadd/NiaddEn.kt
@@ -193,10 +193,12 @@ class NiaddEn : ParsedHttpSource() {
 
         var pageIndex = 0
         sortedUrls.forEach { url ->
-            val pageDoc = client.newCall(GET(
-                url,
-                headers,
-            )).execute().asJsoup()
+            val pageDoc = client.newCall(
+                GET(
+                    url,
+                    headers,
+                ),
+            ).execute().asJsoup()
 
             pageDoc.select("img.manga_pic").forEach { img ->
                 val imageUrl = img.absUrl("src")
