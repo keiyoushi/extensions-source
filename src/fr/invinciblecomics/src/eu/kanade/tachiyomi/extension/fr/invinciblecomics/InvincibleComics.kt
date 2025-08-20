7c08fb5cf16d5160144dbf1db3f52069dfd38101
Starmania <wycvhrt6vzscfpedxr@gmail.com>
2025-08-20 20:32:41 +0200

    Fix status detection

--- src/fr/invinciblecomics/src/eu/kanade/tachiyomi/extension/fr/invinciblecomics/InvincibleComics.kt
+++ src/fr/invinciblecomics/src/eu/kanade/tachiyomi/extension/fr/invinciblecomics/InvincibleComics.kt
@@ -83,9 +83,9 @@ class InvincibleComics : HttpSource() {
             description = document.selectFirst("strong:contains(Résumé)")?.parent()?.ownText()
             author = document.selectFirst("strong:contains(Auteur)")?.parent()?.ownText()
             genre = document.selectFirst("strong:contains(Genres)")?.parent()?.ownText()
-            status = when (document.selectFirst("strong:contains(Statut)")?.parent()?.ownText()) {
-                "En cours" -> SManga.ONGOING // OK
-                "Terminé" -> SManga.COMPLETED // TODO: Check if this is correct
+            status = when (document.selectFirst("strong:contains(Status)")?.parent()?.ownText()?.trim()) {
+                "En cours" -> SManga.ONGOING
+                "Términé" -> SManga.COMPLETED
                 else -> SManga.UNKNOWN
             }
         }
