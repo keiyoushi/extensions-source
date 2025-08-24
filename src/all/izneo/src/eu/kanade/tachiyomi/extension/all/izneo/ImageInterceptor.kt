451debdaa695c09d1bbe9eb5cb892830c11a9711
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2025-08-24 09:53:31 +0000

    Use CipherSource to decrypt responses by streaming (#10253)

--- src/all/izneo/src/eu/kanade/tachiyomi/extension/all/izneo/ImageInterceptor.kt
+++ src/all/izneo/src/eu/kanade/tachiyomi/extension/all/izneo/ImageInterceptor.kt
@@ -4,14 +4,14 @@ import android.util.Base64
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.Response
-import okhttp3.ResponseBody.Companion.toResponseBody
+import okhttp3.ResponseBody.Companion.asResponseBody
+import okio.buffer
+import okio.cipherSource
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 object ImageInterceptor : Interceptor {
-    private val mediaType = "image/jpeg".toMediaType()
-
     private inline val AES: Cipher
         get() = Cipher.getInstance("AES/CBC/PKCS7Padding")
 
@@ -31,7 +31,7 @@ object ImageInterceptor : Interceptor {
 
     private fun Response.decode(key: ByteArray, iv: ByteArray) = AES.let {
         it.init(Cipher.DECRYPT_MODE, SecretKeySpec(key, "AES"), IvParameterSpec(iv))
-        newBuilder().body(it.doFinal(body.bytes()).toResponseBody(mediaType)).build()
+        newBuilder().body(body.source().cipherSource(it).buffer().asResponseBody("image/jpeg".toMediaType())).build()
     }
 
     private fun String.atob() = Base64.decode(this, Base64.URL_SAFE)
