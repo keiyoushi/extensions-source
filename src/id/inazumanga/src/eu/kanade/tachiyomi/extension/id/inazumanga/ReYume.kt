8183dcf28901308788543c0133dc3abf8b030129
Luqman <16263232+Riztard@users.noreply.github.com>
2025-08-25 12:02:47 +0700

    ReYume: fix issue from changing site theme

--- src/id/inazumanga/src/eu/kanade/tachiyomi/extension/id/inazumanga/ReYume.kt
+++ src/id/inazumanga/src/eu/kanade/tachiyomi/extension/id/inazumanga/ReYume.kt
@@ -1,16 +1,12 @@
 package eu.kanade.tachiyomi.extension.id.inazumanga
 
-import eu.kanade.tachiyomi.multisrc.makaru.Makaru
-import eu.kanade.tachiyomi.source.model.Page
-import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Response
+import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
 
-class ReYume : Makaru("ReYume", "https://www.re-yume.my.id", "id") {
-    override fun pageListParse(response: Response): List<Page> {
-        val document = response.asJsoup()
+class ReYume : ZeistManga("ReYume", "https://www.re-yume.my.id", "id") {
 
-        return document.select(".post-body img").mapIndexed { idx, img ->
-            Page(idx, imageUrl = img.absUrl("src"))
-        }
-    }
+    override val popularMangaSelector = "div.PopularPosts div.grid > article"
+    override val popularMangaSelectorTitle = ".post-title a"
+    override val popularMangaSelectorUrl = ".item-thumbnail a"
+
+    override val pageListSelector = ".post-body img"
 }
