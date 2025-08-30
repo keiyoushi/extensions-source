2f47d33e1b30d7a2014956eccf01298b3e51a5e8
Manticore <133025162+manti-X@users.noreply.github.com>
2025-08-30 18:38:02 +0200

    remove duplicate functions and add cloudflare exception

--- src/en/rdscans/src/eu/kanade/tachiyomi/extension/en/rdscans/RDScans.kt
+++ src/en/rdscans/src/eu/kanade/tachiyomi/extension/en/rdscans/RDScans.kt
@@ -1,62 +1,47 @@
 package eu.kanade.tachiyomi.extension.en.rdscans
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
-import eu.kanade.tachiyomi.source.model.SChapter
-import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class RDScans : Madara(
     "RD Scans",
     "https://rdscans.com",
     "en",
-    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.US),
 ) {
     override val mangaSubString = "new"
+    override val mangaEntrySelector = ""
+    override val useNewChapterEndpoint = true
 
-    override fun genresRequest(): Request = GET("$baseUrl/?s=&post_type=wp-manga", headers)
-    override fun popularMangaSelector() = "div.page-item-detail"
-    override fun searchMangaSelector() = "div.c-tabs-item__content"
-
-    // Filter Webnovels
-    private fun parseAndFilterMangasPage(response: Response, selector: String, fromElement: (Element) -> SManga): MangasPage {
-        val document = response.asJsoup()
-        val manga = document.select(selector)
-            .filterNot { it.selectFirst("a")?.attr("title")?.contains("(WN)") ?: false }
-            .map(fromElement)
-        val hasNextPage = popularMangaNextPageSelector()?.let { document.selectFirst(it) } != null
-        return MangasPage(manga, hasNextPage)
+    private fun parseBrowsePage(mangasPage: MangasPage): MangasPage {
+        // Normally returns a 200 OK with a challenge page which leads to a "No results found" error for the user.
+        // This check throws an exception instead, prompting the user to view in WebView first.
+        if (mangasPage.mangas.isEmpty()) {
+            throw Exception("Failed to bypass Cloudflare")
+        }
+        return filterWebNovels(mangasPage)
     }
 
-    override fun popularMangaParse(response: Response): MangasPage =
-        parseAndFilterMangasPage(response, popularMangaSelector(), this::popularMangaFromElement)
-
-    override fun latestUpdatesParse(response: Response): MangasPage = popularMangaParse(response)
-
-    override fun searchMangaParse(response: Response): MangasPage =
-        parseAndFilterMangasPage(response, searchMangaSelector(), this::searchMangaFromElement)
-
-    override fun chapterListParse(response: Response): List<SChapter> {
-        val document = response.asJsoup()
-        launchIO { countViews(document) }
-        val mangaUrl = document.location().removeSuffix("/")
-        val xhrRequest = POST("$mangaUrl/ajax/chapters/", xhrHeaders)
-        val xhrResponse = client.newCall(xhrRequest).execute()
-        val chapterListDocument = xhrResponse.asJsoup()
-        return chapterListDocument.select(chapterListSelector()).map { chapterFromElement(it) }
+    private fun filterWebNovels(mangasPage: MangasPage): MangasPage {
+        val filteredMangas = mangasPage.mangas.filterNot {
+            it.title.contains("(WN)", ignoreCase = true)
+        }
+        return MangasPage(filteredMangas, mangasPage.hasNextPage)
     }
 
-    override val mangaDetailsSelectorStatus = ".post-content_item:has(.summary-heading:contains(Status)) .summary-content"
-    override val mangaDetailsSelectorDescription = "div.description-summary div.summary__content"
+    override fun popularMangaParse(response: Response): MangasPage {
+        return parseBrowsePage(super.popularMangaParse(response))
+    }
+
+    override fun latestUpdatesParse(response: Response): MangasPage {
+        return parseBrowsePage(super.latestUpdatesParse(response))
+    }
+
+    override fun searchMangaParse(response: Response): MangasPage {
+        return filterWebNovels(super.searchMangaParse(response))
+    }
 
     override fun pageListParse(document: Document): List<Page> {
         launchIO { countViews(document) }
