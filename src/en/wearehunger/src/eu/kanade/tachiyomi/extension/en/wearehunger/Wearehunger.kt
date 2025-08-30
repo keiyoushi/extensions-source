34003abac85b2673eadebb01f97f1e7b89183b66
Manticore <133025162+manti-X@users.noreply.github.com>
2025-08-30 14:55:31 +0200

    remove dateFormat

--- src/en/wearehunger/src/eu/kanade/tachiyomi/extension/en/wearehunger/Wearehunger.kt
+++ src/en/wearehunger/src/eu/kanade/tachiyomi/extension/en/wearehunger/Wearehunger.kt
@@ -1,12 +1,9 @@
 package eu.kanade.tachiyomi.extension.en.wearehunger
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class Wearehunger : Madara(
     "Wearehunger",
     "https://www.wearehunger.site",
     "en",
-    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.US),
 )
