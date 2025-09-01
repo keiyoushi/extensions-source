2701e09f6a018741643a7fb9809d86e3dd90521d
Luqman <16263232+Riztard@users.noreply.github.com>
2025-09-01 19:10:55 +0700

    ReYume: fix issue from changing site theme (#10279)

--- src/id/inazumanga/src/eu/kanade/tachiyomi/extension/id/inazumanga/ReYume.kt
+++ src/id/inazumanga/src/eu/kanade/tachiyomi/extension/id/inazumanga/ReYume.kt
@@ -1,16 +1,21 @@
 package eu.kanade.tachiyomi.extension.id.inazumanga
 
-import eu.kanade.tachiyomi.multisrc.makaru.Makaru
-import eu.kanade.tachiyomi.source.model.Page
-import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Response
+import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
 
-class ReYume : Makaru("ReYume", "https://www.re-yume.my.id", "id") {
-    override fun pageListParse(response: Response): List<Page> {
-        val document = response.asJsoup()
+class ReYume : ZeistManga("ReYume", "https://www.re-yume.my.id", "id") {
 
-        return document.select(".post-body img").mapIndexed { idx, img ->
-            Page(idx, imageUrl = img.absUrl("src"))
-        }
-    }
+    override val popularMangaSelector = "div.PopularPosts div.grid > article"
+    override val popularMangaSelectorTitle = ".post-title a"
+    override val popularMangaSelectorUrl = ".item-thumbnail a"
+
+    override val mangaDetailsSelector = "#main"
+    override val mangaDetailsSelectorGenres = "a[rel=tag]"
+    override val mangaDetailsSelectorAuthor = "#extra-info dl:contains(Author) dd"
+    override val mangaDetailsSelectorArtist = "#extra-info dl:contains(Artist) dd"
+    override val mangaDetailsSelectorAltName = "#extra-info dl:contains(Alternative) dd"
+    override val mangaDetailsSelectorInfo = "div:has(h1)"
+    override val mangaDetailsSelectorInfoTitle = "h1"
+    override val mangaDetailsSelectorInfoDescription = "span"
+
+    override val pageListSelector = ".post-body img"
 }
