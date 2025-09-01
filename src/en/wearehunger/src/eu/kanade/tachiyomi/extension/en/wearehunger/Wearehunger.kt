4c391663cbbd90dea55438afbfc0539888779611
manti <133025162+manti-X@users.noreply.github.com>
2025-09-01 11:08:55 +0200

    Add Wearehunger (#10329)

---
+++ src/en/wearehunger/src/eu/kanade/tachiyomi/extension/en/wearehunger/Wearehunger.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.en.wearehunger
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class Wearehunger : Madara(
+    "Wearehunger",
+    "https://www.wearehunger.site",
+    "en",
+)
