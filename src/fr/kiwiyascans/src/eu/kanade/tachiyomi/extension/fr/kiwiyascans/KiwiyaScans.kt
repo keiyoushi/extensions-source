acebd3cf0030aa916d0e20f21c77a361d36aeea8
unknown <manmitsu@hotmail.fr>
2025-09-10 22:35:55 +0200

    Add missing trailing comma

--- src/fr/kiwiyascans/src/eu/kanade/tachiyomi/extension/fr/kiwiyascans/KiwiyaScans.kt
+++ src/fr/kiwiyascans/src/eu/kanade/tachiyomi/extension/fr/kiwiyascans/KiwiyaScans.kt
@@ -8,7 +8,7 @@ class KiwiyaScans : MangaThemesia(
     "Kiwiya Scans",
     "https://kiwiyascans.com",
     "fr",
-    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH)
+    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH),
 ) {
     override fun chapterListSelector() = "ul li:has(div.chbox:not(:has(> span.mcl-price-num))):has(div.eph-num)"
 }
