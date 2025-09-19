916fe69dd82616017357cb8f38f9cb665e71f2da
AlphaBoom <rsshinide38@gmail.com>
2025-09-19 17:53:33 +0900

    Update extension name

--- src/ja/mangagun/src/eu/kanade/tachiyomi/extension/ja/mangagun/MangaGun.kt
+++ src/ja/mangagun/src/eu/kanade/tachiyomi/extension/ja/mangagun/MangaGun.kt
@@ -7,8 +7,6 @@ import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Cookie
-import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -101,38 +99,8 @@ class MangaGun : FMReader("NihonKuni", "https://$DOMAIN", "ja") {
         return chapterDate.timeInMillis
     }
 
-    private fun handleDdosProtect(document: Document): Document {
-        val key = document.select("script:containsData(document.cookie)").first()?.html()
-            ?.substringAfter("escape('")?.substringBefore("'")
-        if (key != null) {
-            // save cookie ct_anti_ddos_key
-            client.cookieJar.saveFromResponse(
-                baseUrl.toHttpUrl(),
-                listOf(
-                    Cookie.Builder()
-                        .name("ct_anti_ddos_key")
-                        .value(key)
-                        .domain(DOMAIN)
-                        .path("/")
-                        .build(),
-                ),
-            )
-        }
-        return document.select("noscript div a").attr("abs:href").let { url ->
-            client.newCall(GET(url, headers)).execute().asJsoup()
-        }
-    }
-
     override fun pageListParse(document: Document): List<Page> {
-        // I haven't encountered this DDoS protection page in a long time. We can consider removing this logic in the future.
-        val isDdosProtect = document.select("title").first()?.text()
-            ?.contains("Manga Gun - DDoS protection") ?: false
-        val doc = if (isDdosProtect) {
-            handleDdosProtect(document)
-        } else {
-            document
-        }
-        val html = doc.select("script:containsData(window.chapterImages)").first()!!.data()
+        val html = document.select("script:containsData(window.chapterImages)").first()!!.data()
             .substringAfter("window.chapterImages=\"").substringBefore("\"")
             .replace("\\/", "/")
             .replace("\\n", "")
