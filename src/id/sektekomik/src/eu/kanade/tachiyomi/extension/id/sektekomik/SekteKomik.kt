284b7d3f7554dd6856b734e4da7fe8b654826452
asdpatro-droid <asdpatro@gmail.com>
2025-09-19 11:48:09 +0200

    Delete src/id directory

--- src/id/sektekomik/src/eu/kanade/tachiyomi/extension/id/sektekomik/SekteKomik.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.id.sektekomik
-
-import eu.kanade.tachiyomi.multisrc.colorlibanime.ColorlibAnime
-
-class SekteKomik : ColorlibAnime("Sekte Komik", "https://sektekomik.xyz", "id")
