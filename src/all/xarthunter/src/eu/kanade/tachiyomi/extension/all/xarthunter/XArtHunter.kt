e26a421f405882b9201e499c5758564d88329997
asdpatro-droid <asdpatro@gmail.com>
2025-09-19 13:53:47 +0200

    Delete src/all/xarthunter directory

--- src/all/xarthunter/src/eu/kanade/tachiyomi/extension/all/xarthunter/XArtHunter.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.all.xarthunter
-
-import eu.kanade.tachiyomi.multisrc.masonry.Masonry
-
-class XArtHunter : Masonry("XArt Hunter", "https://www.xarthunter.com", "all")
