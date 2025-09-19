092947d6182988d21e28b5acd0aa2f0ae4515f9d
asdpatro-droid <asdpatro@gmail.com>
2025-09-19 11:48:59 +0200

    Delete src/pt directory

--- src/pt/dangoscan/src/eu/kanade/tachiyomi/extension/pt/dangoscan/DangoScan.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.pt.dangoscan
-
-import eu.kanade.tachiyomi.multisrc.peachscan.PeachScan
-
-class DangoScan : PeachScan("Dango Scan", "https://dangoscan.com.br", "pt-BR")
