e61c57787899e4d088e93c2f9c8560531eac36cd
asdpatro-droid <asdpatro@gmail.com>
2025-09-19 11:48:18 +0200

    Delete src/it directory

--- src/it/mangaworld/src/eu/kanade/tachiyomi/extension/it/mangaworld/Mangaworld.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.it.mangaworld
-
-import eu.kanade.tachiyomi.multisrc.mangaworld.MangaWorld
-
-class Mangaworld : MangaWorld(
-    "Mangaworld",
-    "https://www.mangaworld.cx",
-    "it",
-)
--- src/it/phoenixscans/src/eu/kanade/tachiyomi/extension/it/phoenixscans/PhoenixScans.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.it.phoenixscans
-
-import eu.kanade.tachiyomi.multisrc.pizzareader.PizzaReader
-
-class PhoenixScans : PizzaReader("Phoenix Scans", "https://www.phoenixscans.com", "it") {
-    override val versionId = 2
-}
