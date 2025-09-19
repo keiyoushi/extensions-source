85de4f957587eae26c0db1f1b4cfe7e6a79574cf
Chopper <156493704+choppeh@users.noreply.github.com>
2025-09-19 03:54:34 -0300

    CovenScan: Update domain (#10610)
    Update domain

--- src/pt/covenscan/src/eu/kanade/tachiyomi/extension/pt/covenscan/CovenScan.kt
+++ src/pt/covenscan/src/eu/kanade/tachiyomi/extension/pt/covenscan/CovenScan.kt
@@ -3,15 +3,12 @@ package eu.kanade.tachiyomi.extension.pt.covenscan
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
-import java.text.SimpleDateFormat
-import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 class CovenScan : Madara(
     "Coven Scan",
-    "https://cvnscan.com",
+    "https://covendasbruxonas.com",
     "pt-BR",
-    SimpleDateFormat("MMMM dd, yyyy", Locale("pt", "BR")),
 ) {
 
     override val client: OkHttpClient = super.client.newBuilder()
