f74b95032d44c84883bba11c010c57d2768a0cee
asdpatro-droid <asdpatro@gmail.com>
2025-09-19 13:50:34 +0200

    Delete src/all/pixiv directory

--- src/all/pixiv/src/eu/kanade/tachiyomi/extension/all/pixiv/PixivFactory.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.all.pixiv
-
-import eu.kanade.tachiyomi.source.Source
-import eu.kanade.tachiyomi.source.SourceFactory
-
-class PixivFactory : SourceFactory {
-    override fun createSources(): List<Source> =
-        listOf("ja", "en", "ko", "zh").map { lang -> Pixiv(lang) }
-}
