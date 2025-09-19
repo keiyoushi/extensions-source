7f39b48b0140311b0e8301b498b7e2cc652ac346
Chopper <156493704+choppeh@users.noreply.github.com>
2025-09-19 03:55:05 -0300

    Manhuarm: Fix dialog box empty and add translation support (#10625)
    Fix dialog box empty and add translation support

---
+++ src/all/manhuarm/src/eu/kanade/tachiyomi/extension/all/manhuarm/ManhuarmUtils.kt
@@ -0,0 +1,15 @@
+package eu.kanade.tachiyomi.extension.all.manhuarm
+
+class MachineTranslationsFactoryUtils
+
+data class Language(
+    val lang: String,
+    val target: String = lang,
+    val origin: String = "en",
+    val fontSize: Int = 28,
+    val disableFontSettings: Boolean = false,
+    val disableWordBreak: Boolean = false,
+    val disableTranslator: Boolean = false,
+    val supportNativeTranslation: Boolean = false,
+    val fontName: String = "comic_neue_bold",
+)
