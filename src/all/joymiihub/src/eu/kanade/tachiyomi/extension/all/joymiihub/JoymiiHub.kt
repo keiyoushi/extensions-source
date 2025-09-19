770f62a67aeefff1e8438ee88decd8ecc09f87ae
asdpatro-droid <asdpatro@gmail.com>
2025-09-19 13:43:37 +0200

    Delete src/all/joymiihub directory

--- src/all/joymiihub/src/eu/kanade/tachiyomi/extension/all/joymiihub/JoymiiHub.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.all.joymiihub
-
-import eu.kanade.tachiyomi.multisrc.masonry.Masonry
-
-class JoymiiHub : Masonry("Joymii Hub", "https://www.joymiihub.com", "all")
