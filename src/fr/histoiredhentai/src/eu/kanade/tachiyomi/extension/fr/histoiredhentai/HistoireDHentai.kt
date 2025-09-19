871106c83f8651f2c80a41f429d384ba1002eeb7
asdpatro-droid <asdpatro@gmail.com>
2025-09-19 11:47:58 +0200

    Delete src/fr directory

--- src/fr/histoiredhentai/src/eu/kanade/tachiyomi/extension/fr/histoiredhentai/HistoireDHentai.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.histoiredhentai
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class HistoireDHentai : Madara("HistoireDHentai", "https://hhentai.fr", "fr", SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH))
