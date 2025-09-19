5c24cfd77462a8166d008c39b029ebf594138726
asdpatro-droid <asdpatro@gmail.com>
2025-09-19 11:49:26 +0200

    Delete src/tr directory

--- src/tr/tempestscans/src/eu/kanade/tachiyomi/extension/tr/tempestscans/TempestScans.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.tr.tempestscans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class TempestScans : MangaThemesia(
-    "Tempest Scans",
-    "https://tempestmangas.com",
-    "tr",
-)
