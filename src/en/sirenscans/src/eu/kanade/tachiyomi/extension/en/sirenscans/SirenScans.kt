4b51e37299b59e4c5c0ae6595252a13375792c67
mrtear <91820779+mrtear@users.noreply.github.com>
2025-09-19 00:53:57 -0600

    Add Siren Scans (#10608)
    * sirenscans
    
    * 0

---
+++ src/en/sirenscans/src/eu/kanade/tachiyomi/extension/en/sirenscans/SirenScans.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.en.sirenscans
+
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+
+class SirenScans : Keyoapp(
+    "Siren Scans",
+    "https://sirenscans.com",
+    "en",
+)
