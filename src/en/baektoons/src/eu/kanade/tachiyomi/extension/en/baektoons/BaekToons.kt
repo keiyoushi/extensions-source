2058f40eb5e9501ca00a1f12d11eadc387c1d8b0
mrtear <91820779+mrtear@users.noreply.github.com>
2025-09-21 12:44:38 -0600

    Added BaekToons, LevaScans, RizzComic (unoriginal) (#10652)
    * baektoons
    
    * levascans
    
    * rizzcomicun (original)

---
+++ src/en/baektoons/src/eu/kanade/tachiyomi/extension/en/baektoons/BaekToons.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.en.baektoons
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class BaekToons : MangaThemesia(
+    "Baek Toons",
+    "https://baektoons.com",
+    "en",
+)
