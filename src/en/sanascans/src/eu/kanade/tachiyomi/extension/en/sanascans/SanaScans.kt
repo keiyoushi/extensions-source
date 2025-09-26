1339c385949bc7851332d448a4a7c88a8aae39eb
mrtear <91820779+mrtear@users.noreply.github.com>
2025-09-25 23:36:11 -0600

    Add SanaScans (#10722)
    sanascans

---
+++ src/en/sanascans/src/eu/kanade/tachiyomi/extension/en/sanascans/SanaScans.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.en.sanascans
+
+import eu.kanade.tachiyomi.multisrc.iken.Iken
+
+class SanaScans : Iken(
+    "Sana Scans",
+    "en",
+    "https://sanascans.com",
+    "https://api.sanascans.com",
+)
