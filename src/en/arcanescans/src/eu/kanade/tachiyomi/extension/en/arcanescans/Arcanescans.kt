b4a38bf110995c4f724230ec3c9ceef033e93848
Luqman <16263232+Riztard@users.noreply.github.com>
2025-10-03 04:52:21 +0700

    Arcanescans: fix browse, site changing theme (#10814)
    manga url same as previous theme

--- src/en/arcanescans/src/eu/kanade/tachiyomi/extension/en/arcanescans/Arcanescans.kt
+++ src/en/arcanescans/src/eu/kanade/tachiyomi/extension/en/arcanescans/Arcanescans.kt
@@ -1,5 +1,5 @@
 package eu.kanade.tachiyomi.extension.en.arcanescans
 
-import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 
-class Arcanescans : Madara("Arcanescans", "https://arcanescans.com", "en")
+class Arcanescans : MangaThemesia("Arcanescans", "https://arcanescans.com", "en")
