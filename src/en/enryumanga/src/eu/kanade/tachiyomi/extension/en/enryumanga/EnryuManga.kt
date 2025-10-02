20452d7a51dde551e154e6e1bc1f227912f08cf9
solkaz <jh.software.developer@gmail.com>
2025-10-02 23:53:03 +0200

    Remove EnryuManga as source (#10840)
    Remove EnryuManga
    
    Closes #10771

--- src/en/enryumanga/src/eu/kanade/tachiyomi/extension/en/enryumanga/EnryuManga.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.enryumanga
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class EnryuManga : MangaThemesia("EnryuManga", "https://enryucomics.com", "en")
