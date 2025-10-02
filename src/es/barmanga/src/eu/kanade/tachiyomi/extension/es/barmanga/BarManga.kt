2347aa3ede86852bda6a86b69a69d0bcab6b4303
bapeey <90949336+bapeey@users.noreply.github.com>
2025-10-02 16:52:33 -0500

    BarManga: Fix images not loading (#10835)
    fix images not loading

--- src/es/barmanga/src/eu/kanade/tachiyomi/extension/es/barmanga/BarManga.kt
+++ src/es/barmanga/src/eu/kanade/tachiyomi/extension/es/barmanga/BarManga.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.extension.es.barmanga
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.model.Page
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -13,4 +15,17 @@ class BarManga : Madara(
     override val useLoadMoreRequest = LoadMoreStrategy.Never
 
     override val mangaDetailsSelectorDescription = "div.flamesummary > div.manga-excerpt"
+
+    private val imageUrlRegex = """fetch\(['"](.*?)['"]\)""".toRegex()
+
+    override fun pageListParse(document: Document): List<Page> {
+        launchIO { countViews(document) }
+
+        return document.select(pageListParseSelector).mapIndexed { index, element ->
+            val script = element.selectFirst("script")?.data()
+            val imageUrl = script?.let { imageUrlRegex.find(it)?.groupValues?.get(1) }
+                ?: element.selectFirst("img")?.let { imageFromElement(it) }
+            Page(index, document.location(), imageUrl)
+        }
+    }
 }
