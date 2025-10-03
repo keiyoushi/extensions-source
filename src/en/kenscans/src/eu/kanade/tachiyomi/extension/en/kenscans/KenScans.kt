d0b6ddd7cce5fb44b307b11126a1b666d47e793c
Luqman <16263232+Riztard@users.noreply.github.com>
2025-10-03 14:28:33 +0700

    Ken Scans: fix browse, site changing theme

--- src/en/kenscans/src/eu/kanade/tachiyomi/extension/en/kenscans/KenScans.kt
+++ src/en/kenscans/src/eu/kanade/tachiyomi/extension/en/kenscans/KenScans.kt
@@ -1,9 +1,43 @@
 package eu.kanade.tachiyomi.extension.en.kenscans
 
-import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+import eu.kanade.tachiyomi.multisrc.iken.Iken
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.util.asJsoup
+import keiyoushi.utils.parseAs
+import okhttp3.Request
+import okhttp3.Response
 
-class KenScans : Keyoapp(
+class KenScans : Iken(
     "Ken Scans",
-    "https://kenscans.com",
     "en",
-)
+    "https://kenscans.com",
+    "https://api.kenscans.com",
+) {
+    override fun chapterListRequest(manga: SManga): Request {
+        // Migration from old web theme to the new one(Keyoapp -> Iken)
+        if (manga.url.startsWith("/series/")) {
+            throw Exception("Migrate from $name to $name (same extension)")
+        }
+        return GET("$baseUrl/series/${manga.url}", headers)
+    }
+
+    override fun pageListParse(response: Response): List<Page> {
+        // Migration from old web theme to the new one(Keyoapp -> Iken)
+        val url = response.request.url.toString().substringAfter(baseUrl)
+        if (url.startsWith("/chapter/")) {
+            throw Exception("Migrate from $name to $name (same extension)")
+        }
+
+        val document = response.asJsoup()
+
+        if (document.selectFirst("svg.lucide-lock") != null) {
+            throw Exception("Unlock chapter in webview")
+        }
+
+        return document.getNextJson("images").parseAs<List<PageParseDto>>().mapIndexed { idx, p ->
+            Page(idx, imageUrl = p.url)
+        }
+    }
+}
