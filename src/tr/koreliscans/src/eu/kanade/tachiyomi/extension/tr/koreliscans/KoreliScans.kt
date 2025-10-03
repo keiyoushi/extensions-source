ed7543616a7d37e180bd6707c9a70e6709f16431
Luqman <16263232+Riztard@users.noreply.github.com>
2025-10-03 18:15:47 +0700

    Koreli Scans: fix browse, site changing theme (#10815)

--- src/tr/koreliscans/src/eu/kanade/tachiyomi/extension/tr/koreliscans/KoreliScans.kt
+++ src/tr/koreliscans/src/eu/kanade/tachiyomi/extension/tr/koreliscans/KoreliScans.kt
@@ -1,23 +1,12 @@
 package eu.kanade.tachiyomi.extension.tr.koreliscans
 
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.source.model.MangasPage
-import okhttp3.Response
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class KoreliScans : Madara(
+class KoreliScans : MangaThemesia(
     "Koreli Scans",
     "https://koreliscans.net",
     "tr",
     dateFormat = SimpleDateFormat("d MMMM yyyy", Locale("tr")),
-) {
-    override val useLoadMoreRequest = LoadMoreStrategy.Never
-    override val useNewChapterEndpoint = true
-
-    override fun searchMangaParse(response: Response): MangasPage {
-        val mangasPage = super.searchMangaParse(response)
-        val filteredMangas = mangasPage.mangas.filterNot { it.title.endsWith(" Novel") }
-        return MangasPage(filteredMangas, mangasPage.hasNextPage)
-    }
-}
+)
