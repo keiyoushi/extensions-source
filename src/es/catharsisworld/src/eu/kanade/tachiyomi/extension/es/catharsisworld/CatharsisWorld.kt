613638547a04926e18b7d847cfa4e3b5b08e6f80
Genzales6 <nionzales462@gmail.com>
2025-10-03 05:15:02 -0600

    Catharsis World URL Updated (#10801)
    Closes #10794

--- src/es/catharsisworld/src/eu/kanade/tachiyomi/extension/es/catharsisworld/CatharsisWorld.kt
+++ src/es/catharsisworld/src/eu/kanade/tachiyomi/extension/es/catharsisworld/CatharsisWorld.kt
@@ -9,7 +9,7 @@ import org.jsoup.nodes.Element
 
 class CatharsisWorld : Madara(
     "Catharsis World",
-    "https://catharsisworld.akan01.com",
+    "https://catharsisworld.dig-it.info",
     "es",
 ) {
     override val versionId = 2
