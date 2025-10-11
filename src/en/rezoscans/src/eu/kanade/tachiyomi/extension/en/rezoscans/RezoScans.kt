71eadbd279502621d641d1fba2cc922062cece2c
Julio Reigen <paginasbr@gmail.com>
2025-10-10 23:53:46 -0300

    RezoScans: Update domain and theme (#10937)
    * fix: RezoScans
    
    * fix: lint
    
    * Update version id
    
    * update versionId

--- src/en/rezoscans/src/eu/kanade/tachiyomi/extension/en/rezoscans/RezoScans.kt
+++ src/en/rezoscans/src/eu/kanade/tachiyomi/extension/en/rezoscans/RezoScans.kt
@@ -1,9 +1,40 @@
 package eu.kanade.tachiyomi.extension.en.rezoscans
 
-import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+import eu.kanade.tachiyomi.multisrc.iken.Iken
+import eu.kanade.tachiyomi.network.GET
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
+import okhttp3.Response
 
-class RezoScans : Keyoapp(
+class RezoScans : Iken(
     "Rezo Scans",
-    "https://rezoscans.com",
     "en",
-)
+    "https://rezoscan.org",
+    "https://api.rezoscan.org",
+) {
+    override val versionId = 2
+
+    override fun latestUpdatesRequest(page: Int): Request {
+        val url = "$apiUrl/api/posts".toHttpUrl().newBuilder().apply {
+            addQueryParameter("page", page.toString())
+            addQueryParameter("perPage", perPage.toString())
+            addQueryParameter("tag", "new")
+            addQueryParameter("isNovel", "false")
+        }.build()
+
+        return GET(url, headers)
+    }
+
+    override fun popularMangaRequest(page: Int): Request {
+        val url = "$apiUrl/api/posts".toHttpUrl().newBuilder().apply {
+            addQueryParameter("page", page.toString())
+            addQueryParameter("perPage", perPage.toString())
+            addQueryParameter("tag", "hot")
+            addQueryParameter("isNovel", "false")
+        }.build()
+
+        return GET(url, headers)
+    }
+    override fun popularMangaParse(response: Response) = searchMangaParse(response)
+    val perPage = 18
+}
