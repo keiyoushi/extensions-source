0c989e6b74a55abd806f50d0e2f09c4ab0d8f732
Themis <26204972+Frearios@users.noreply.github.com>
2025-10-11 05:53:20 +0300

    Rawkuma: Update Domain (Temp Fix) (#10923)
    * Update Domain & Version Bump
    
    * Update Domain

--- src/ja/rawkuma/src/eu/kanade/tachiyomi/extension/ja/rawkuma/Rawkuma.kt
+++ src/ja/rawkuma/src/eu/kanade/tachiyomi/extension/ja/rawkuma/Rawkuma.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 
-class Rawkuma : MangaThemesia("Rawkuma", "https://rawkuma.net", "ja") {
+class Rawkuma : MangaThemesia("Rawkuma", "https://old.rawkuma.net", "ja") {
 
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(4)
