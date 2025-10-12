9e8086c6104f313e1419318737b810d07673ecee
ndrefg <48099550+ndrefg@users.noreply.github.com>
2025-10-12 23:44:24 +0100

    Alternativescans
    Created a extension for https://alternativescans.icu/

---
+++ src/en/alternativescans/build.gradle.kts
@@ -0,0 +1,9 @@
+ext {
+    set("extName", "Alternative Scans")
+    set("extVersionCode", 1)
+    set("overrideVersionCode", 0)
+    set("extClass", ".AlternativeScans")
+    set("baseUrl", "https://alternativescans.icu")
+}
+
+apply(from = "$rootDir/common.gradle")
