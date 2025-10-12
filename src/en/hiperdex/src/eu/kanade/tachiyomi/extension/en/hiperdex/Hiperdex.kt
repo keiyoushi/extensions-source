10737734e1eead1470723a0347dacc33cc8a90ac
Cuong-Tran <cuongtran.tm@gmail.com>
2025-10-13 00:56:16 +0700

    feat(Hiperdex): add preferences to remove title regex
    * feat(Hiperdex): add preferences to remove title regex
    
    * add original title to description
    
    * make companion
    
    * custom regex verification
    
    * verify regex

--- src/en/hiperdex/src/eu/kanade/tachiyomi/extension/en/hiperdex/Hiperdex.kt
+++ src/en/hiperdex/src/eu/kanade/tachiyomi/extension/en/hiperdex/Hiperdex.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.extension.en.hiperdex
 
 import android.widget.Toast
+import androidx.preference.CheckBoxPreference
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.lib.randomua.addRandomUAPreferenceToScreen
@@ -10,7 +11,9 @@ import eu.kanade.tachiyomi.lib.randomua.setRandomUserAgent
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.ConfigurableSource
+import eu.kanade.tachiyomi.source.model.SManga
 import keiyoushi.utils.getPreferences
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -53,12 +56,63 @@ class Hiperdex :
             }
         }.also { screen.addPreference(it) }
 
+        CheckBoxPreference(screen.context).apply {
+            key = "${REMOVE_TITLE_VERSION_PREF}_$lang"
+            title = "Remove version information from entry titles"
+            summary = "This removes version tags like '(Official)' or '(Uncensored)' from entry titles " +
+                "and helps identify duplicate entries in your library. " +
+                "To update existing entries, remove them from your library (unfavorite) and refresh manually. " +
+                "You might also want to clear the database in advanced settings."
+            setDefaultValue(false)
+        }.also { screen.addPreference(it) }
+
+        EditTextPreference(screen.context).apply {
+            key = "${REMOVE_TITLE_CUSTOM_PREF}_$lang"
+            title = "Custom regex to be removed from title"
+            summary = preferences.getString("${REMOVE_TITLE_CUSTOM_PREF}_$lang", "") ?: ""
+            setDefaultValue("")
+            setOnPreferenceChangeListener { _, newValue ->
+                runCatching {
+                    Regex(newValue as String)
+                }.onFailure {
+                    Toast.makeText(screen.context, it.message, Toast.LENGTH_LONG).show()
+                }.isSuccess
+            }
+        }.also { screen.addPreference(it) }
+
         addRandomUAPreferenceToScreen(screen)
     }
 
     override fun searchMangaSelector() = "#loop-content div.page-listing-item"
 
+    override fun mangaDetailsParse(document: Document): SManga {
+        return super.mangaDetailsParse(document).apply {
+            val cleanedTitle = title.let { originalTitle ->
+                var tempTitle = originalTitle
+                customRemoveTitle().takeIf { it.isNotEmpty() }?.let { customRegex ->
+                    runCatching {
+                        tempTitle = tempTitle.replace(Regex(customRegex), "")
+                    }
+                }
+                if (isRemoveTitleVersion()) {
+                    tempTitle = tempTitle.replace(titleRegex, "")
+                }
+                tempTitle.trim()
+            }
+            if (cleanedTitle != title) {
+                description = listOfNotNull(title, description)
+                    .joinToString("\n\n")
+                title = cleanedTitle
+            }
+        }
+    }
+
     private fun getPrefBaseUrl(): String = preferences.getString(BASE_URL_PREF, super.baseUrl)!!
+    private fun isRemoveTitleVersion(): Boolean {
+        return preferences.getBoolean("${REMOVE_TITLE_VERSION_PREF}_$lang", false)
+    }
+    private fun customRemoveTitle(): String =
+        preferences.getString("${REMOVE_TITLE_CUSTOM_PREF}_$lang", "")!!
 
     init {
         preferences.getString(DEFAULT_BASE_URL_PREF, null).let { defaultBaseUrl ->
@@ -77,5 +131,10 @@ class Hiperdex :
         private const val BASE_URL_PREF_SUMMARY = "The default settings will be applied when the extension is next updated"
         private const val DEFAULT_BASE_URL_PREF = "defaultBaseUrl"
         private const val RESTART_APP_MESSAGE = "Restart app to apply new setting."
+        private const val REMOVE_TITLE_VERSION_PREF = "REMOVE_TITLE_VERSION"
+        private const val REMOVE_TITLE_CUSTOM_PREF = "REMOVE_TITLE_CUSTOM"
+
+        private val titleRegex: Regex =
+            Regex("\\([^()]*\\)|\\{[^{}]*\\}|\\[(?:(?!]).)*]|«[^»]*»|〘[^〙]*〙|「[^」]*」|『[^』]*』|≪[^≫]*≫|﹛[^﹜]*﹜|〖[^〖〗]*〗|\uD81A\uDD0D.+?\uD81A\uDD0D|《[^》]*》|⌜.+?⌝|⟨[^⟩]*⟩|/Official|/ Official", RegexOption.IGNORE_CASE)
     }
 }
