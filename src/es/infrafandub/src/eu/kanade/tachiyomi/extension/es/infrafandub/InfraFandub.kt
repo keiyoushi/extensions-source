15c1e697d59d51650eb946c4f297779538c6d86a
Useles5 <126971764+Useles5@users.noreply.github.com>
2025-10-15 18:15:51 +0530

    Fix InfraFandub extension (#11035)
    * Fix InfraFandub extension
    
    * Apply review suggestions
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/es/infrafandub/src/eu/kanade/tachiyomi/extension/es/infrafandub/InfraFandub.kt
+++ src/es/infrafandub/src/eu/kanade/tachiyomi/extension/es/infrafandub/InfraFandub.kt
@@ -2,7 +2,9 @@ package eu.kanade.tachiyomi.extension.es.infrafandub
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.SChapter
 import okhttp3.OkHttpClient
+import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
@@ -19,5 +21,28 @@ class InfraFandub : Madara(
 
     override val useNewChapterEndpoint = true
 
-    override val mangaDetailsSelectorStatus = "div.post-content_item:contains(Estado) div.summary-content"
+    override fun popularMangaSelector() = "div.manga-item"
+    override val popularMangaUrlSelector = "div.title a"
+    override fun searchMangaSelector() = "div.manga-item"
+    override val searchMangaUrlSelector = "div.title a"
+
+    override val mangaDetailsSelectorTitle = "h1.series-title"
+    override val mangaDetailsSelectorAuthor = "div.series-details div.detail-item:contains(Autor) span.detail-value"
+    override val mangaDetailsSelectorArtist = "div.series-details div.detail-item:contains(Artista) span.detail-value"
+    override val mangaDetailsSelectorGenre = "div.genres a.genre-tag"
+    override val mangaDetailsSelectorDescription = "div.summary-text"
+    override val mangaDetailsSelectorThumbnail = "aside.sidebar img.series-cover"
+    override val mangaDetailsSelectorStatus = "div.series-details div.detail-item:contains(Estado) span.detail-value"
+
+    override fun chapterListSelector() = "div.chapters-list a.chapter-item"
+
+    override fun chapterFromElement(element: Element): SChapter {
+        val chapter = SChapter.create()
+        with(element) {
+            chapter.setUrlWithoutDomain(absUrl("href"))
+            chapter.name = selectFirst("span.chapter-number")!!.text()
+            chapter.date_upload = parseChapterDate(selectFirst("span.chapter-date")?.text())
+        }
+        return chapter
+    }
 }
