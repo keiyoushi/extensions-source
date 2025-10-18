bac1c6cc8492e6d2f40c0c013b46b27b57b9d723
Genzales6 <nionzales462@gmail.com>
2025-10-18 15:03:05 -0500

    MHScans / BiblioPanda Fix url

--- src/es/mhscans/src/eu/kanade/tachiyomi/extension/es/mhscans/MHScans.kt
+++ src/es/mhscans/src/eu/kanade/tachiyomi/extension/es/mhscans/MHScans.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 class MHScans : Madara(
     "MHScans",
-    "https://curiosidadtop.com",
+    "https://mh.baxoi.buzz",
     "es",
     dateFormat = SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("es")),
 ) {
