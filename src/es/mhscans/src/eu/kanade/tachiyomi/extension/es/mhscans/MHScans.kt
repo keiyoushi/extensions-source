864a7e13fce4065513f4bbab9a852e5f1a225c57
Genzales6 <nionzales462@gmail.com>
2025-10-19 04:16:38 -0500

    MHScans & BiblioPanda update url (#11132)
    * MHScans / BiblioPanda Fix url
    
    * MH changed domain last hour (1)
    
    * MH changed domain last hour (2)
    
    build.gradle

--- src/es/mhscans/src/eu/kanade/tachiyomi/extension/es/mhscans/MHScans.kt
+++ src/es/mhscans/src/eu/kanade/tachiyomi/extension/es/mhscans/MHScans.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 class MHScans : Madara(
     "MHScans",
-    "https://curiosidadtop.com",
+    "https://mh.topmanhuas.net",
     "es",
     dateFormat = SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("es")),
 ) {
