2d8cb5609c62c055523c0176a1f66099f7709698
Genzales6 <nionzales462@gmail.com>
2025-10-19 01:15:12 -0500

    MH changed domain last hour (1)

--- src/es/mhscans/src/eu/kanade/tachiyomi/extension/es/mhscans/MHScans.kt
+++ src/es/mhscans/src/eu/kanade/tachiyomi/extension/es/mhscans/MHScans.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 class MHScans : Madara(
     "MHScans",
-    "https://mh.baxoi.buzz",
+    "https://mh.topmanhuas.net",
     "es",
     dateFormat = SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("es")),
 ) {
