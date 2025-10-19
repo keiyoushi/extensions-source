8bb0c6feea4d38b344c6493e4ff3eafc2d46abb6
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-10-19 11:16:53 +0200

    Remove some dead sources (#11133)
    * Remove Manhwafull
    
    * Remove Mangahasu
    
    * Remove MangaScans
    
    * Remove MyShojo
    
    * Remove Iris Scans
    
    * Remove Constellar Scans
    
    * Remove King of Scans
    
    * Remove 1st-Kiss Manga.net
    
    * Remove Banana Manga
    
    * Remove Todaymic
    
    * Remove Comicsekai
    
    * Remove KataKomik
    
    * Remove Komik Lovers
    
    * Remove Mangakyo
    
    * Remove MangaYu
    
    * Remove Tenshi.id
    
    * Remove TukangKomik
    
    * Remove YuraManga
    
    * Remove Pojok Manga
    
    * Remove Scan-Manga
    
    * Remove Valkyrie Scan
    
    * Remove Starbound Scans
    
    * Remove Manga Rock Team
    
    * Remove Tres Daos Net

--- src/en/manhwafull/src/eu/kanade/tachiyomi/extension/en/manhwafull/Manhwafull.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.manhwafull
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class Manhwafull : Madara("Manhwafull", "https://manhwafull.com", "en", SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH))
