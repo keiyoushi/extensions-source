c78e5444bdc14b73872f3c5c0dc97a72450d42f0
Maxime Croisier <crmaxime@gmail.com>
2025-10-20 00:36:14 +0200

    Add Les Poroiniens source
    Adds the Les Poroiniens source, which uses the ScanR multisource.

---
+++ src/fr/lesporoiniens/src/eu/kanade/tachiyomi/extension/fr/lesporoiniens/LesPoroiniens.kt
@@ -0,0 +1,11 @@
+package eu.kanade.tachiyomi.extension.fr.lesporoiniens
+
+import eu.kanade.tachiyomi.multisrc.scanr.ScanR
+
+class LesPoroiniens : ScanR(
+    "Les Poroiniens",
+    "https://lesporoiniens.org",
+    "fr",
+    false,
+    "_",
+)
