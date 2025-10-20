3388bbf49b9a3770ade8c37d8b50f8284319a662
SupremeDeity <sheikhmohsin706@gmail.com>
2025-10-20 16:11:44 +0500

    Add icons and do fixes

--- src/en/zazamanga/src/eu/kanade/tachiyomi/extension/en/zazamanga/Zazamanga.kt
+++ src/en/zazamanga/src/eu/kanade/tachiyomi/extension/en/zazamanga/Zazamanga.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.extension.en.zazamanga
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
 import okhttp3.Request
 import org.jsoup.nodes.Element
@@ -10,6 +11,15 @@ class Zazamanga : Madara("Zazamanga", "https://www.zazamanga.com", "en") {
     override fun chapterListSelector() = "div.wp-manga-chapter"
     override fun searchMangaSelector() = "div.page-item-detail:not(.manga)"
     override val searchMangaUrlSelector = "p.widget-title a"
+    override fun popularMangaNextPageSelector(): String? = ".pagination li:last-child:not(.disabled)"
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/$mangaSubString/${searchPage(page)}?orderby=views", headers)
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/$mangaSubString/${searchPage(page)}?orderby=latest", headers)
+
+    override fun searchRequest(page: Int, query: String, filters: FilterList): Request {
+        val request = super.searchRequest(page, query, filters)
+        val url = request.url.toString().replace("m_orderby=", "orderby=")
+        return GET(url, request.headers)
+    }
 
     override fun imageRequest(page: Page): Request {
         return GET(page.imageUrl!!, headers)
