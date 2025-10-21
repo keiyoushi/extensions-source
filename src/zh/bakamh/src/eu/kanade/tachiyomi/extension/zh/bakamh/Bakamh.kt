0378fb3a13f544b7b1a0ca817f6f2e4f861a01b3
marioplus <nokiadafa@live.com>
2025-10-21 16:51:39 +0800

    fix(bakamh): fix unable to load chapter

--- src/zh/bakamh/src/eu/kanade/tachiyomi/extension/zh/bakamh/Bakamh.kt
+++ src/zh/bakamh/src/eu/kanade/tachiyomi/extension/zh/bakamh/Bakamh.kt
@@ -2,7 +2,9 @@ package eu.kanade.tachiyomi.extension.zh.bakamh
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Headers
+import okhttp3.Response
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -15,16 +17,50 @@ class Bakamh : Madara(
 ) {
     override val mangaDetailsSelectorStatus = ".post-content_item:contains(状态) .summary-content"
 
-    override fun chapterListSelector() = "div.tab-content li:has(a[data-chapter-url])"
-
     override fun headersBuilder(): Headers.Builder {
         return super.headersBuilder()
             .add("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
     }
 
-    override fun chapterFromElement(element: Element) = SChapter.create().apply {
-        val urlElement = element.selectFirst(chapterUrlSelector)!!
-        url = urlElement.attr("abs:data-chapter-url")
-        name = urlElement.text()
+    val chapterSelectors = listOf(
+        ".chapter-loveYou",
+        "li a[onclick]",
+        "li a",
+    )
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val mangaUrl = response.request.url.toString().lowercase()
+        val doc = response.asJsoup()
+        for (selector in chapterSelectors) {
+            val chapters = doc.select(selector)
+                .mapNotNull { paresChapter(it, mangaUrl) }
+            if (chapters.isNotEmpty()) {
+                return chapters
+            }
+        }
+        return emptyList()
+    }
+
+    fun paresChapter(element: Element, mangaUrl: String): SChapter? {
+        if (element.hasAttr("storage-chapter-url")) {
+            return SChapter.create().apply {
+                url = element.absUrl("storage-chapter-url")
+                name = element.text()
+                chapter_number = 0F
+            }
+        }
+
+        return element.attributes()
+            .find { attr ->
+                val value = attr.value.lowercase()
+                value.startsWith(mangaUrl) && value != mangaUrl
+            }
+            ?.let { attr ->
+                SChapter.create().apply {
+                    url = element.absUrl(attr.key)
+                    name = element.text()
+                    chapter_number = 0F
+                }
+            }
     }
 }
