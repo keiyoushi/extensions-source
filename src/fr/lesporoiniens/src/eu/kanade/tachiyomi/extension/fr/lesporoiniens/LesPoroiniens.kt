6048e1cba56ef09973aeb27fdb21a0f0ca796827
Emixam <crmaxime@gmail.com>
2025-10-22 07:27:37 +0200

    Add Les Poroiniens (#11156)
    * Add ScanR multi-source
    
    Adds the ScanR multi-source template, adapting code from the BigSolo source
    
    * Migrate BigSolo to ScanR multi-source
    
    Uses the ScanR multi-source template for the extension
    
    * Add Les Poroiniens source
    
    Adds the Les Poroiniens source, which uses the ScanR multisource.
    
    * Use named parameters

---
+++ src/fr/lesporoiniens/src/eu/kanade/tachiyomi/extension/fr/lesporoiniens/LesPoroiniens.kt
@@ -0,0 +1,11 @@
+package eu.kanade.tachiyomi.extension.fr.lesporoiniens
+
+import eu.kanade.tachiyomi.multisrc.scanr.ScanR
+
+class LesPoroiniens : ScanR(
+    name = "Les Poroiniens",
+    baseUrl = "https://lesporoiniens.org",
+    lang = "fr",
+    useHighLowQualityCover = false,
+    slugSeparator = "_",
+)
