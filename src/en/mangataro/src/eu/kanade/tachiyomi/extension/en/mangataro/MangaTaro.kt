f018b842795962af3793ecace05c9c56a63ed0ca
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-10-27 22:14:28 +0500

    unescape entities

--- src/en/mangataro/src/eu/kanade/tachiyomi/extension/en/mangataro/MangaTaro.kt
+++ src/en/mangataro/src/eu/kanade/tachiyomi/extension/en/mangataro/MangaTaro.kt
@@ -21,6 +21,7 @@ import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import org.jsoup.Jsoup
+import org.jsoup.parser.Parser
 import rx.Observable
 import java.lang.UnsupportedOperationException
 import java.util.Calendar
@@ -205,7 +206,7 @@ class MangaTaro : HttpSource() {
 
         return SManga.create().apply {
             url = MangaUrl(data.id.toString(), data.slug).toJsonString()
-            title = data.title.rendered
+            title = Parser.unescapeEntities(data.title.rendered, false)
             description = Jsoup.parseBodyFragment(data.content.rendered).wholeText()
             genre = buildSet {
                 addAll(data.embedded.getTerms("post_tag"))
