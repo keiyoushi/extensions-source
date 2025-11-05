1cb88af841a9ab28b70623057eb1e998f1dab6f2
fadiajlil2099 <fadiajlil2099@gmail.com>
2025-11-05 16:01:25 +0100

    Update Manhuarm.kt
    Changed Url

--- src/all/manhuarm/src/eu/kanade/tachiyomi/extension/all/manhuarm/Manhuarm.kt
+++ src/all/manhuarm/src/eu/kanade/tachiyomi/extension/all/manhuarm/Manhuarm.kt
@@ -43,7 +43,7 @@ class Manhuarm(
     private val language: Language,
 ) : Madara(
     "Manhuarm",
-    "https://manhuarm.com",
+    "https://manhuarmmtl.com",
     language.lang,
 ),
     ConfigurableSource {
