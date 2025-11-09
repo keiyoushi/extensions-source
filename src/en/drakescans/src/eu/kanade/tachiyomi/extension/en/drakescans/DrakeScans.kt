8e065ca48c6a1b0ad39c3f61ba398fc5b415caf1
dragon-masterk <83424830+dragon-masterk@users.noreply.github.com>
2025-11-08 23:25:08 -0500

    added support for paid chapter filtering

--- src/en/drakescans/src/eu/kanade/tachiyomi/extension/en/drakescans/DrakeScans.kt
+++ src/en/drakescans/src/eu/kanade/tachiyomi/extension/en/drakescans/DrakeScans.kt
@@ -1,19 +1,21 @@
 package eu.kanade.tachiyomi.extension.en.drakescans
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaAlt
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaPaidChapterHelper
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
 import eu.kanade.tachiyomi.source.model.Page
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import java.util.concurrent.TimeUnit
 
-class DrakeScans : MangaThemesia(
+class DrakeScans : MangaThemesiaAlt(
     "Drake Scans",
     "https://drakecomic.org",
     "en",
 ) {
     // madara -> mangathemesia
-    override val versionId = 2
+    override val versionId = 3
 
     override val client = super.client.newBuilder()
         .rateLimitHost(baseUrl.toHttpUrl(), 3, 1, TimeUnit.SECONDS)
@@ -28,5 +30,19 @@ class DrakeScans : MangaThemesia(
 
     companion object {
         val JETPACK_CDN_REGEX = """^https:\/\/i[0-9]\.wp\.com\/""".toRegex()
+    }
+
+    private val paidChapterHelper = MangaThemesiaPaidChapterHelper(lockedChapterSelector = "ul li.locked")
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        super.setupPreferenceScreen(screen)
+        paidChapterHelper.addHidePaidChaptersPreferenceToScreen(screen, intl)
+    }
+
+    override fun chapterListSelector(): String {
+        return paidChapterHelper.getChapterListSelectorBasedOnHidePaidChaptersPref(
+            super.chapterListSelector(),
+            preferences,
+        )
     }
 }
