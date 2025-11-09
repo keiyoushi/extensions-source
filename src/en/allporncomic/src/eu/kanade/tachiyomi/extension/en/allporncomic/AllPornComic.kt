34a0c24eb4b94b7d327ec959e5d1a0f1b051585a
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2025-11-09 07:08:00 +0700

    AllPornComic: Fix popular/latest (#11496)
    Fix popular/latest

--- src/en/allporncomic/src/eu/kanade/tachiyomi/extension/en/allporncomic/AllPornComic.kt
+++ src/en/allporncomic/src/eu/kanade/tachiyomi/extension/en/allporncomic/AllPornComic.kt
@@ -2,4 +2,6 @@ package eu.kanade.tachiyomi.extension.en.allporncomic
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class AllPornComic : Madara("AllPornComic", "https://allporncomic.com", "en")
+class AllPornComic : Madara("AllPornComic", "https://allporncomic.com", "en") {
+    override val mangaSubString = "porncomic"
+}
