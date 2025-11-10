f9ee995c2e6fa6210a01b5ff60b91852f3ea8aa9
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2025-11-10 23:31:08 +0700

    Don't use DTO chapter number as it is , not actual chapter number

--- src/en/kagane/src/eu/kanade/tachiyomi/extension/en/kagane/Dto.kt
+++ src/en/kagane/src/eu/kanade/tachiyomi/extension/en/kagane/Dto.kt
@@ -83,14 +83,11 @@ class ChapterDto(
         val releaseDate: String?,
         @SerialName("pages_count")
         val pagesCount: Int,
-        @SerialName("number_sort")
-        val number: Float,
     ) {
         fun toSChapter(): SChapter = SChapter.create().apply {
             url = "$seriesId;$id;$pagesCount"
             name = title
             date_upload = dateFormat.tryParse(releaseDate)
-            chapter_number = number
         }
     }
 
