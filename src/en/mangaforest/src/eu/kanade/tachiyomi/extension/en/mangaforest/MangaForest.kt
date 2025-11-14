c17c4686b29ccb3bee7ac84d0024f6d3da0dae24
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2025-11-15 00:30:09 +0700

    Revert "Remove dead sources"
    This reverts commit aa04da638a93e40e1c28eaa2d8f4fcbf2e5681d9.

---
+++ src/en/mangaforest/src/eu/kanade/tachiyomi/extension/en/mangaforest/MangaForest.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.mangaforest
+
+import eu.kanade.tachiyomi.multisrc.madtheme.MadTheme
+
+class MangaForest : MadTheme("MangaForest", "https://mangaforest.me", "en")
