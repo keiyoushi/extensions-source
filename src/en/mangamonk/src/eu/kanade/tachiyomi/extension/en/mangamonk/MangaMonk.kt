aa04da638a93e40e1c28eaa2d8f4fcbf2e5681d9
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2025-11-15 00:08:34 +0700

    Remove dead sources

--- src/en/mangamonk/src/eu/kanade/tachiyomi/extension/en/mangamonk/MangaMonk.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangamonk
-
-import eu.kanade.tachiyomi.multisrc.madtheme.MadTheme
-
-class MangaMonk : MadTheme("MangaMonk", "https://mangamonk.com", "en")
--- src/en/mangaxyz/src/eu/kanade/tachiyomi/extension/en/mangaxyz/MangaXYZ.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangaxyz
-
-import eu.kanade.tachiyomi.multisrc.madtheme.MadTheme
-
-class MangaXYZ : MadTheme("MangaXYZ", "https://mangaxyz.com", "en")
