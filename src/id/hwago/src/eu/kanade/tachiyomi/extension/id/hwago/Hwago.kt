2e3011ed21e1b57685fad5531970bb49b3972753
Mohamed-Waled <mohamedwaledfarag@gmail.com>
2025-11-14 23:55:42 +0200

    Remove the trailing slash

--- src/id/hwago/src/eu/kanade/tachiyomi/extension/id/hwago/Hwago.kt
+++ src/id/hwago/src/eu/kanade/tachiyomi/extension/id/hwago/Hwago.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 
 class Hwago : Madara(
     "Hwago",
-    "https://01.hwago.xyz/",
+    "https://01.hwago.xyz",
     "id",
     dateFormat = SimpleDateFormat("d MMMM yyyy", Locale("en")),
 ) {
