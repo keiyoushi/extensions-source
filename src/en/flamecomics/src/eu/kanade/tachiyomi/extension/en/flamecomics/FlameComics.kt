8f51bc803068c66ca7ec6f80b9f394ce385b164d
Kirill Kvit <44607316+kiryl-kvit@users.noreply.github.com>
2025-11-14 18:30:10 +0100

    FlameComics: update api contract (#11643)
    * Update api contract
    
    Update last_edit type to match updated FlameComics API
    
    * Fix extVersionCode

--- src/en/flamecomics/src/eu/kanade/tachiyomi/extension/en/flamecomics/FlameComics.kt
+++ src/en/flamecomics/src/eu/kanade/tachiyomi/extension/en/flamecomics/FlameComics.kt
@@ -57,7 +57,7 @@ class FlameComics : HttpSource() {
         imageApiUrlBuilder().apply {
             addPathSegment(seriesData.series_id.toString())
             addPathSegment(seriesData.cover)
-            addQueryParameter(seriesData.last_edit, null)
+            addQueryParameter(seriesData.last_edit.toString(), null)
         }.build().toString()
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request =
