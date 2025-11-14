0ee06614850f041772bfadda6b167333d472081f
felipegangorra <felipegangorra@gmail.com>
2025-11-14 18:52:44 -0300

    Remove the complex interceptor

--- src/pt/mangalivre/src/eu/kanade/tachiyomi/extension/pt/mangalivre/MangaLivre.kt
+++ src/pt/mangalivre/src/eu/kanade/tachiyomi/extension/pt/mangalivre/MangaLivre.kt
@@ -1,8 +1,6 @@
 package eu.kanade.tachiyomi.extension.pt.mangalivre
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import okhttp3.Interceptor
-import okhttp3.Response
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -13,27 +11,7 @@ class MangaLivre : Madara(
     SimpleDateFormat("MMMM dd, yyyy", Locale("pt")),
 ) {
 
-    private class MangaLivreInterceptor : Interceptor {
-        override fun intercept(chain: Interceptor.Chain): Response {
-            var request = chain.request()
-            val url = request.url.toString()
-
-            // APENAS REMOVE ?style=list - MANTÉM .tv
-            if (url.contains("?style=list")) {
-                val newUrl = url.replace("?style=list", "")
-                request = request.newBuilder()
-                    .url(newUrl)
-                    .build()
-            }
-
-            return chain.proceed(request)
-        }
-    }
-
-    override val client = super.client.newBuilder()
-        .addInterceptor(MangaLivreInterceptor())
-        .build()
-
+    override val chapterUrlSuffix: String = ""
     override val useNewChapterEndpoint = true
     override val id: Long = 2834885536325274328
 }
