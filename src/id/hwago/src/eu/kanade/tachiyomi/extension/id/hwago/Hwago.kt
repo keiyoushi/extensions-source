cd3e35d37118eb4a564162f339b75fce9b4a59ec
Mohamed Waled Farag Youssef <mohamedwaledfarag@gmail.com>
2025-11-15 17:23:23 +0200

    Updating Hwago Indonesia extension url (#11653)
    * Updating Hwago Indonesia extension url
    
    * Remove the trailing slash

--- src/id/hwago/src/eu/kanade/tachiyomi/extension/id/hwago/Hwago.kt
+++ src/id/hwago/src/eu/kanade/tachiyomi/extension/id/hwago/Hwago.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 
 class Hwago : Madara(
     "Hwago",
-    "https://hwago01.xyz",
+    "https://01.hwago.xyz",
     "id",
     dateFormat = SimpleDateFormat("d MMMM yyyy", Locale("en")),
 ) {
