4f78bae5baa07175bfcf1f3f61e30156dae1c0b7
Chopper <156493704+choppeh@users.noreply.github.com>
2025-11-18 16:29:38 -0300

    Add MangaLivreTo (#11706)
    * Add MangaLivreTo
    
    * Remove unneeded code

---
+++ src/pt/mangalivreto/src/eu/kanade/tachiyomi/extension/pt/mangalivreto/MangaLivreTo.kt
@@ -0,0 +1,21 @@
+package eu.kanade.tachiyomi.extension.pt.mangalivreto
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class MangaLivreTo : Madara(
+    "Manga Livre.to",
+    "https://mangalivre.to",
+    "pt-BR",
+    SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("pt")),
+) {
+    override val client = super.client.newBuilder()
+        .rateLimit(2)
+        .build()
+
+    override fun chapterListSelector() = ".listing-chapters-wrap .chapter-box"
+
+    override fun chapterDateSelector() = ".chapter-date"
+}
