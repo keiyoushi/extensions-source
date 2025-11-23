87d4f253c23a57e5ac6995e257a4f74c940859e2
Rafael B. <rafael.bellintani@bluecore.com.br>
2025-11-23 16:40:23 -0300

    fix: add clearence interceptor to correct cloudflare error, correct date format and fixed UA to receive cf-mitigate

--- src/pt/remangas/src/eu/kanade/tachiyomi/extension/pt/remangas/Remangas.kt
+++ src/pt/remangas/src/eu/kanade/tachiyomi/extension/pt/remangas/Remangas.kt
@@ -1,6 +1,11 @@
 package eu.kanade.tachiyomi.extension.pt.remangas
 
-import android.content.SharedPreferences
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.Toast
+import androidx.preference.CheckBoxPreference
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.lib.randomua.addRandomUAPreferenceToScreen
 import eu.kanade.tachiyomi.lib.randomua.getPrefCustomUA
@@ -9,36 +14,189 @@ import eu.kanade.tachiyomi.lib.randomua.setRandomUserAgent
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.ConfigurableSource
-import keiyoushi.utils.getPreferencesLazy
-import okhttp3.OkHttpClient
+import eu.kanade.tachiyomi.source.model.SManga
+import keiyoushi.utils.getPreferences
+import okhttp3.Headers
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
-import java.util.concurrent.TimeUnit
 
 class Remangas :
     Madara(
         "Remangas",
         "https://remangas.net",
         "pt-BR",
-        SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")),
+        SimpleDateFormat("dd/MM/yyyy", Locale("pt")),
     ),
     ConfigurableSource {
 
-    override val useLoadMoreRequest = LoadMoreStrategy.Always
+    override val mangaDetailsSelectorStatus = "div.summary-heading:contains(Status) + div.summary-content"
 
-    private val preferences: SharedPreferences by getPreferencesLazy()
+    private val USER_AGENT = "Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.7444.172 Mobile Safari/537.36"
 
-    override val client: OkHttpClient = super.client.newBuilder()
+    private val preferences = getPreferences()
+
+    override val baseUrl by lazy { getPrefBaseUrl() }
+
+    override val client = super.client.newBuilder()
+        .addNetworkInterceptor(ClearanceInterceptor())
         .setRandomUserAgent(
             preferences.getPrefUAType(),
             preferences.getPrefCustomUA(),
         )
-        .rateLimit(2)
-        .readTimeout(1, TimeUnit.MINUTES)
-        .connectTimeout(1, TimeUnit.MINUTES)
+        .rateLimit(3)
         .build()
 
+    override fun headersBuilder(): Headers.Builder = Headers.Builder()
+        .add(
+            "User-Agent",
+            USER_AGENT,
+        )
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+
     override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        EditTextPreference(screen.context).apply {
+            key = BASE_URL_PREF
+            title = BASE_URL_PREF_TITLE
+            summary = BASE_URL_PREF_SUMMARY
+            dialogTitle = BASE_URL_PREF_TITLE
+            dialogMessage = "Default URL:\n\t${super.baseUrl}"
+            setDefaultValue(super.baseUrl)
+            setOnPreferenceChangeListener { _, newValue ->
+                Toast.makeText(screen.context, RESTART_APP_MESSAGE, Toast.LENGTH_LONG).show()
+                true
+            }
+        }.also { screen.addPreference(it) }
+
+        CheckBoxPreference(screen.context).apply {
+            key = "${REMOVE_TITLE_VERSION_PREF}_$lang"
+            title = "Remove version information from entry titles"
+            summary = "This removes version tags like '(Official)' or '(Uncensored)' from entry titles " +
+                "and helps identify duplicate entries in your library. " +
+                "To update existing entries, remove them from your library (unfavorite) and refresh manually. " +
+                "You might also want to clear the database in advanced settings."
+            setDefaultValue(false)
+        }.also { screen.addPreference(it) }
+
+        EditTextPreference(screen.context).apply {
+            key = "${REMOVE_TITLE_CUSTOM_PREF}_$lang"
+            title = "Custom regex to be removed from title"
+            summary = customRemoveTitle()
+            setDefaultValue("")
+
+            val validate = { str: String ->
+                runCatching { Regex(str) }
+                    .map { true to "" }
+                    .getOrElse { false to it.message }
+            }
+
+            setOnBindEditTextListener { editText ->
+                editText.addTextChangedListener(
+                    object : TextWatcher {
+                        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                        override fun afterTextChanged(editable: Editable?) {
+                            editable ?: return
+                            val text = editable.toString()
+                            val valid = validate(text)
+                            editText.error = if (!valid.first) valid.second else null
+                            editText.rootView.findViewById<Button>(android.R.id.button1)?.isEnabled = editText.error == null
+                        }
+                    },
+                )
+            }
+
+            setOnPreferenceChangeListener { _, newValue ->
+                val (isValid, message) = validate(newValue as String)
+                if (isValid) {
+                    summary = newValue
+                } else {
+                    Toast.makeText(screen.context, message, Toast.LENGTH_LONG).show()
+                }
+                isValid
+            }
+        }.also { screen.addPreference(it) }
+
         addRandomUAPreferenceToScreen(screen)
+    }
+
+    override fun popularMangaFromElement(element: Element): SManga {
+        return super.popularMangaFromElement(element).apply {
+            title = title.cleanTitleIfNeeded()
+        }
+    }
+
+    override fun latestUpdatesFromElement(element: Element): SManga {
+        return super.latestUpdatesFromElement(element).apply {
+            title = title.cleanTitleIfNeeded()
+        }
+    }
+
+    override fun searchMangaFromElement(element: Element): SManga {
+        return super.searchMangaFromElement(element).apply {
+            title = title.cleanTitleIfNeeded()
+        }
+    }
+
+    override fun searchMangaSelector() = "#loop-content div.page-listing-item"
+
+    override val chapterUrlSuffix = ""
+
+    override fun mangaDetailsParse(document: Document): SManga {
+        return super.mangaDetailsParse(document).apply {
+            val cleanedTitle = title.cleanTitleIfNeeded()
+            if (cleanedTitle != title.trim()) {
+                description = listOfNotNull(title, description)
+                    .joinToString("\n\n")
+                title = cleanedTitle
+            }
+        }
+    }
+
+    private fun String.cleanTitleIfNeeded(): String {
+        var tempTitle = this
+        customRemoveTitle().takeIf { it.isNotEmpty() }?.let { customRegex ->
+            runCatching {
+                tempTitle = tempTitle.replace(Regex(customRegex), "")
+            }
+        }
+        if (isRemoveTitleVersion()) {
+            tempTitle = tempTitle.replace(titleRegex, "")
+        }
+        return tempTitle.trim()
+    }
+
+    private fun getPrefBaseUrl(): String = preferences.getString(BASE_URL_PREF, super.baseUrl)!!
+    private fun isRemoveTitleVersion(): Boolean {
+        return preferences.getBoolean("${REMOVE_TITLE_VERSION_PREF}_$lang", false)
+    }
+    private fun customRemoveTitle(): String =
+        preferences.getString("${REMOVE_TITLE_CUSTOM_PREF}_$lang", "")!!
+
+    init {
+        preferences.getString(DEFAULT_BASE_URL_PREF, null).let { defaultBaseUrl ->
+            if (defaultBaseUrl != super.baseUrl) {
+                preferences.edit()
+                    .putString(BASE_URL_PREF, super.baseUrl)
+                    .putString(DEFAULT_BASE_URL_PREF, super.baseUrl)
+                    .apply()
+            }
+        }
+    }
+
+    companion object {
+        private const val BASE_URL_PREF = "overrideBaseUrl"
+        private const val BASE_URL_PREF_TITLE = "Edit source URL (requires restart)"
+        private const val BASE_URL_PREF_SUMMARY = "The default settings will be applied when the extension is next updated"
+        private const val DEFAULT_BASE_URL_PREF = "defaultBaseUrl"
+        private const val RESTART_APP_MESSAGE = "Restart app to apply new setting."
+        private const val REMOVE_TITLE_VERSION_PREF = "REMOVE_TITLE_VERSION"
+        private const val REMOVE_TITLE_CUSTOM_PREF = "REMOVE_TITLE_CUSTOM"
+
+        private val titleRegex: Regex =
+            Regex("\\([^()]*\\)|\\{[^{}]*\\}|\\[(?:(?!]).)*]|«[^»]*»|〘[^〙]*〙|「[^」]*」|『[^』]*』|≪[^≫]*≫|﹛[^﹜]*﹜|〖[^〖〗]*〗|\uD81A\uDD0D.+?\uD81A\uDD0D|《[^》]*》|⌜.+?⌝|⟨[^⟩]*⟩|/Official|/ Official", RegexOption.IGNORE_CASE)
     }
 }
