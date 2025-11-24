b807c60a6513f8c919a209807f551e67f356af47
Felipe Ávila <79549074+FelipeGFA@users.noreply.github.com>
2025-11-24 10:55:51 -0300

    Fix SussyToons: Add new api, filters and fix search (#11605)
    * fix sussy: remover do greenshit lib-multisrc
    
    - add filtros
    
    add utils
    
    * remover themepkg
    
    * fix SussyToons: consertando a extensão para utilizar nova api
    
    - add nova api
    
    - remover lib-multisrc greenshit
    
    - add filtros
    
    - consertar pesquisa
    
    * remover arquivo de utilizades e migrar utils para dto
    
    * Add greenshit ao multisrc de volta...
    
    * fix erro no build do pr
    
    * greenshit: refatorando a lib
    
    - aba popular maidscan
    
    - aba latest maidscan
    
    - aba filtro maidscan
    
    * greenshit: refatorando toda lib + suporte para novels em texto
    
    sussyscan: add nova cdn, nova aba filtros, fix search, suporte para novel
    
    maidscan: nova url, nova api, add popular, add latest, add filtros, fix search, suporte para novels texto
    
    * revertendo mudanças no Data.kt
    
    * remover propriedades do dto nao utilizadas
    
    * fix aurorascan: add nova url, fix popular, fix latest, fix search, add suporte para caps em texto
    
    * mudar url aurora scan, bump version greenshit, simplificar e refatorar extensões
    
    * remove source morta
    
    * adiciona todos os filtros orderby de sussyscan
    
    * reviews
    
    * corrigindo reviews
    
    * fixing reviews and adding new api check for pageDTO

--- src/pt/maidscan/src/eu/kanade/tachiyomi/extension/pt/maidscan/MaidScan.kt
+++ src/pt/maidscan/src/eu/kanade/tachiyomi/extension/pt/maidscan/MaidScan.kt
@@ -1,27 +1,32 @@
 package eu.kanade.tachiyomi.extension.pt.maidscan
 
 import eu.kanade.tachiyomi.multisrc.greenshit.GreenShit
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.FilterList
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 
 class MaidScan : GreenShit(
     "Maid Scan",
-    "https://novo.empreguetes.site",
+    "https://empreguetes.xyz",
     "pt-BR",
-    scanId = 3,
 ) {
+    override val useWidthInThumbnail = false
+    override val defaultOrderBy = "data"
     override val targetAudience = TargetAudience.Shoujo
+    override val popularGenreId = "4"
+    override val latestGenreId = "4"
+    override val popularType = "periodo"
+    override val popularTypeValue = "geral"
+    override val latestEndpoint = "atualizacoes"
+    override val includeSlugInUrl = true
 
-    override val contentOrigin: ContentOrigin = ContentOrigin.Mobile
+    override fun headersBuilder() = super.headersBuilder()
+        .set("scan-id", "empreguetes.xyz")
 
     override val client: OkHttpClient = super.client.newBuilder()
         .connectTimeout(1, TimeUnit.MINUTES)
         .readTimeout(1, TimeUnit.MINUTES)
-        .rateLimit(2)
         .build()
 
-    override fun popularMangaRequest(page: Int) =
-        GET("$apiUrl/obras/ranking?periodo=geral&limite=5&gen_id=$targetAudience", headers)
+    override fun getFilterList() = FilterList()
 }
