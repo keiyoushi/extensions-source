386a6a1b4e9ea0327ecfbfa77656f9ee73937083
Felipe Ávila <79549074+FelipeGFA@users.noreply.github.com>
2025-11-24 17:38:34 -0300

    fix MangaStop: add interceptor (#11791)
    * add interceptor to bypass fake url
    
    * bump versioncode
    
    * change pagelistparse to response:response
    
    * revert to pageListParse(document: Document)
    
    * fix lint erro

--- src/pt/mangastop/src/eu/kanade/tachiyomi/extension/pt/mangastop/MangaStop.kt
+++ src/pt/mangastop/src/eu/kanade/tachiyomi/extension/pt/mangastop/MangaStop.kt
@@ -2,6 +2,10 @@ package eu.kanade.tachiyomi.extension.pt.mangastop
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.Page
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -18,4 +22,19 @@ class MangaStop : MangaThemesia(
     override val client = super.client.newBuilder()
         .rateLimit(3)
         .build()
+
+    override fun pageListParse(document: Document): List<Page> {
+        val pages = super.pageListParse(document)
+            .filterNot { it.imageUrl?.contains("mihon", true) == true }
+
+        if (pages.isNotEmpty()) return pages
+
+        return MangaThemesia.JSON_IMAGE_LIST_REGEX.find(document.toString())
+            ?.groupValues?.get(1)
+            ?.let { json.parseToJsonElement(it).jsonArray }
+            ?.mapIndexed { i, el ->
+                Page(i, document.location(), el.jsonPrimitive.content)
+            }
+            .orEmpty()
+    }
 }
