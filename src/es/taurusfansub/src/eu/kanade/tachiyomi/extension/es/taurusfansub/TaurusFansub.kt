ee0a2e7a6fba91cf47876c1cfd519224087938df
zrempz <rmontanezp@unsa.edu.pe>
2025-11-25 11:38:02 -0500

    fix: align selectors with new site layout

--- src/es/taurusfansub/src/eu/kanade/tachiyomi/extension/es/taurusfansub/TaurusFansub.kt
+++ src/es/taurusfansub/src/eu/kanade/tachiyomi/extension/es/taurusfansub/TaurusFansub.kt
@@ -2,8 +2,6 @@ package eu.kanade.tachiyomi.extension.es.taurusfansub
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import eu.kanade.tachiyomi.source.model.SManga
-import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
@@ -21,21 +19,9 @@ class TaurusFansub : Madara(
     override val useNewChapterEndpoint = true
     override val useLoadMoreRequest = LoadMoreStrategy.Always
 
-    override fun mangaDetailsParse(document: Document): SManga {
-        val manga = SManga.create()
+    override val popularMangaUrlSelectorImg = ".manga__thumb_item img"
 
-        manga.description = document.select("div.site-content div.summary_content p").text()
-        manga.genre = document.select("div.site-content div.summary_content div.genres-content").joinToString { it.text() }
-        manga.author = document.select("div.site-content div.summary_content div.tags-content").text()
-
-        val stado = document.select("div.site-content div.summary_content div.manga-title div.post-content_item div.summary-content").first()?.text()
-        manga.status = when (stado) {
-            "En Curso" -> { SManga.ONGOING }
-            "Completado" -> { SManga.COMPLETED }
-            else -> { SManga.UNKNOWN }
-        }
-        manga.artist = document.select("div.site-content div.summary_content div.tags-content").text()
-
-        return manga
-    }
+    override val mangaDetailsSelectorTitle = "h1.post-title"
+    override val mangaDetailsSelectorStatus = "div.manga-status span:last-child"
+    override val mangaDetailsSelectorDescription = "div.summary__content p"
 }
