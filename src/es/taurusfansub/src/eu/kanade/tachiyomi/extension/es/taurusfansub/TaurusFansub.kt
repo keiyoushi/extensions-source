075058808c885ef517381f5ea420b4e5025aeab1
zrempz <rmontanezp@unsa.edu.pe>
2025-11-25 13:33:10 -0500

    fix: correct genre parsing

--- src/es/taurusfansub/src/eu/kanade/tachiyomi/extension/es/taurusfansub/TaurusFansub.kt
+++ src/es/taurusfansub/src/eu/kanade/tachiyomi/extension/es/taurusfansub/TaurusFansub.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.extension.es.taurusfansub
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
@@ -24,4 +25,19 @@ class TaurusFansub : Madara(
     override val mangaDetailsSelectorTitle = "h1.post-title"
     override val mangaDetailsSelectorStatus = "div.manga-status span:last-child"
     override val mangaDetailsSelectorDescription = "div.summary__content p"
+
+    override fun parseGenres(document: Document): List<Genre> {
+        return document.select(".genres-filter .options a")
+            .mapNotNull { element ->
+                val id = element.attr("href")
+                    .substringAfter("genre=", "")
+                    .substringBefore("&")
+
+                val name = element.text()
+
+                Genre(name, id).takeIf {
+                    id.isNotEmpty() && name.isNotBlank()
+                }
+            }
+    }
 }
