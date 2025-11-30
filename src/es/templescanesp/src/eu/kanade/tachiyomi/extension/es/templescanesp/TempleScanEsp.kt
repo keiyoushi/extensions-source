0415027bff68f6382657c9b33795ff13ed5fb073
bapeey <90949336+bapeey@users.noreply.github.com>
2025-11-29 23:28:03 -0500

    fix pages not found

--- src/es/templescanesp/src/eu/kanade/tachiyomi/extension/es/templescanesp/TempleScanEsp.kt
+++ src/es/templescanesp/src/eu/kanade/tachiyomi/extension/es/templescanesp/TempleScanEsp.kt
@@ -7,15 +7,20 @@ import androidx.preference.PreferenceScreen
 import androidx.preference.SwitchPreferenceCompat
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
 import eu.kanade.tachiyomi.source.ConfigurableSource
+import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.utils.getPreferences
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.FormBody
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -123,6 +128,22 @@ class TempleScanEsp :
         name = element.selectFirst("div.grid > span")!!.text()
         date_upload = element.selectFirst("div.grid > div")?.text()?.let { parseChapterDate(it) } ?: 0
         setUrlWithoutDomain(element.selectFirst("a")!!.attr("href"))
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        var doc = document
+        val form = doc.selectFirst("form#redirect-form[method=post]")
+        if (form != null) {
+            val url = form.attr("action")
+            val headers = headersBuilder().set("Referer", doc.location()).build()
+            val body = FormBody.Builder()
+            form.select("input").forEach {
+                body.add(it.attr("name"), it.attr("value"))
+            }
+            doc = client.newCall(POST(url, headers, body.build())).execute().asJsoup()
+        }
+
+        return super.pageListParse(doc)
     }
 
     private fun Element.imageFromStyle(): String {
