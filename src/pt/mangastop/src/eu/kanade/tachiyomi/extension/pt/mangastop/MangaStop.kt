323611fc0334dbd1cbe35698f02c0859baa0a732
FelipeGFA <felipegabriel.avila6@gmail.com>
2025-12-01 00:12:32 -0300

    add cloudflare cliente and randomua

--- src/pt/mangastop/src/eu/kanade/tachiyomi/extension/pt/mangastop/MangaStop.kt
+++ src/pt/mangastop/src/eu/kanade/tachiyomi/extension/pt/mangastop/MangaStop.kt
@@ -1,33 +1,61 @@
+
 package eu.kanade.tachiyomi.extension.pt.mangastop
 
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.lib.cookieinterceptor.CookieInterceptor
+import eu.kanade.tachiyomi.lib.randomua.PREF_KEY_RANDOM_UA
+import eu.kanade.tachiyomi.lib.randomua.RANDOM_UA_VALUES
+import eu.kanade.tachiyomi.lib.randomua.UserAgentType
+import eu.kanade.tachiyomi.lib.randomua.addRandomUAPreferenceToScreen
+import eu.kanade.tachiyomi.lib.randomua.getPrefCustomUA
+import eu.kanade.tachiyomi.lib.randomua.getPrefUAType
+import eu.kanade.tachiyomi.lib.randomua.setRandomUserAgent
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.model.Page
+import keiyoushi.utils.getPreferencesLazy
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.Headers
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class MangaStop : MangaThemesia(
-    "Manga Stop",
-    "https://mangastop.net",
-    "pt-BR",
-    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("pt", "BR")),
-) {
-    override fun headersBuilder() = super.headersBuilder()
-        .set("Referer", "$baseUrl/")
-        .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-        .set("Accept-Language", "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3")
-        .set("Sec-Fetch-Dest", "document")
-        .set("Sec-Fetch-Mode", "navigate")
-        .set("Sec-Fetch-Site", "none")
-        .set("Sec-Fetch-User", "?1")
-        .set("Upgrade-Insecure-Requests", "1")
+class MangaStop :
+    MangaThemesia(
+        "Manga Stop",
+        "https://mangastop.net",
+        "pt-BR",
+        dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("pt", "BR")),
+    ),
+    ConfigurableSource {
 
-    override val client = super.client.newBuilder()
-        .rateLimit(3)
+    private val preferences by getPreferencesLazy {
+        if (getPrefUAType() != UserAgentType.OFF || getPrefCustomUA().isNullOrBlank().not()) {
+            return@getPreferencesLazy
+        }
+        edit().putString(PREF_KEY_RANDOM_UA, RANDOM_UA_VALUES.last()).apply()
+    }
+
+    override val client = network.cloudflareClient.newBuilder()
+        .addNetworkInterceptor(
+            CookieInterceptor(baseUrl.substringAfter("//"), "wpmanga-ada" to "1"),
+        )
+        .setRandomUserAgent(
+            preferences.getPrefUAType(),
+            preferences.getPrefCustomUA(),
+        )
+        .rateLimit(2)
         .build()
+
+    override fun headersBuilder(): Headers.Builder = super.headersBuilder()
+        .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+        .add("Accept-Language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7")
+        .add("Sec-Fetch-Dest", "document")
+        .add("Sec-Fetch-Mode", "navigate")
+        .add("Sec-Fetch-Site", "same-origin")
+        .add("Upgrade-Insecure-Requests", "1")
 
     override fun pageListParse(document: Document): List<Page> {
         val pages = super.pageListParse(document)
@@ -42,5 +70,9 @@ class MangaStop : MangaThemesia(
                 Page(i, document.location(), el.jsonPrimitive.content)
             }
             .orEmpty()
+    }
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        addRandomUAPreferenceToScreen(screen)
     }
 }
