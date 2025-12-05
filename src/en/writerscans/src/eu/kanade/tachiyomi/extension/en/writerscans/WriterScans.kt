4586b87f1166923ac4fc7d337946d163fd0f8207
manti <133025162+manti-X@users.noreply.github.com>
2025-12-05 19:22:17 +0100

    Add Writer Scans (#11900)

---
+++ src/en/writerscans/src/eu/kanade/tachiyomi/extension/en/writerscans/WriterScans.kt
@@ -0,0 +1,11 @@
+package eu.kanade.tachiyomi.extension.en.writerscans
+
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+
+class WriterScans : Keyoapp(
+    "Writer Scans",
+    "https://writerscans.com",
+    "en",
+) {
+    override fun popularMangaSelector() = "div:contains(Trending) + div .group.overflow-hidden"
+}
