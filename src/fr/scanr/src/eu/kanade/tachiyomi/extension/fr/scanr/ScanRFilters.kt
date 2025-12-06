f8edf1dd69281243490dd9e21cdbdc5abc1099e4
CriosChan <36739192+CriosChan@users.noreply.github.com>
2025-12-06 22:14:57 +0100

    ScanR: Add filtering (#12012)
    * ScanR: Add Filters
    
    * ScanR: Filtering optimization
    - Adult filtering
    
    * ScanR: Changes asked by vetleledaal

---
+++ src/fr/scanr/src/eu/kanade/tachiyomi/extension/fr/scanr/ScanRFilters.kt
@@ -0,0 +1,53 @@
+package eu.kanade.tachiyomi.extension.fr.scanr
+
+import eu.kanade.tachiyomi.source.model.Filter
+import okhttp3.HttpUrl
+
+interface UriFilter {
+    fun addToUri(builder: HttpUrl.Builder)
+}
+
+open class UriMultiSelectOption(name: String, val value: String, state: Boolean) : Filter.CheckBox(name, state)
+
+open class UriMultiSelectFilter(
+    name: String,
+    private val param: String,
+    options: List<UriMultiSelectOption>,
+) : UriFilter, Filter.Group<UriMultiSelectOption>(
+    name,
+    options,
+) {
+    override fun addToUri(builder: HttpUrl.Builder) {
+        val selected = state.filter { it.state }.map { it.value }
+        if (selected.isNotEmpty()) {
+            builder.addQueryParameter(param, selected.joinToString(","))
+        }
+    }
+}
+
+class TypeFilter : UriMultiSelectFilter(
+    "Type",
+    "type",
+    listOf(
+        UriMultiSelectOption("OS", "os", true),
+        UriMultiSelectOption("Séries", "series", true),
+    ),
+)
+
+class StatusFilter : UriMultiSelectFilter(
+    "Status",
+    "status",
+    listOf(
+        UriMultiSelectOption("Terminé", "completed", true),
+        UriMultiSelectOption("En cours", "ongoing", true),
+    ),
+)
+
+class AdultFilter : UriMultiSelectFilter(
+    "Adulte ?",
+    "adult",
+    listOf(
+        UriMultiSelectOption("+18", "18", true),
+        UriMultiSelectOption("Normal", "normal", true),
+    ),
+)
