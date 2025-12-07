a161e4ef018f047a8978e8e8c88e00f4fb36c203
Romain <wycvhrt6vzscfpedxr@gmail.com>
2025-12-07 21:42:29 +0100

    fix(raijinscans) : Fix "tachiyomi detection" v2 (#12044)
    fix(raijinscans) : new data url

--- src/fr/raijinscans/src/eu/kanade/tachiyomi/extension/fr/raijinscans/RaijinScans.kt
+++ src/fr/raijinscans/src/eu/kanade/tachiyomi/extension/fr/raijinscans/RaijinScans.kt
@@ -192,8 +192,11 @@ class RaijinScans : HttpSource() {
     // ========================== Page List =============================
     override fun pageListParse(response: Response): List<Page> {
         return response.asJsoup().select("div.protected-image-data").mapIndexed { index, element ->
-            val encodedUrl = element.attr("data-r").reversed()
-            val imageUrl = String(Base64.decode(encodedUrl, Base64.DEFAULT))
+            val encodedUrl = element.attr("data-v").reversed()
+            val xorKey = 93
+            val xored = String(Base64.decode(encodedUrl, Base64.DEFAULT))
+            val decoded = xored.map { (it.code xor xorKey).toChar() }.joinToString("")
+            val imageUrl = String(Base64.decode(decoded, Base64.DEFAULT))
             Page(index, imageUrl = imageUrl)
         }
     }
