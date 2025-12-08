606f5c8de04c67478c79be983ddbcf324e4c0b4b
Vetle Ledaal <vetle.ledaal@gmail.com>
2025-12-08 10:00:04 +0100

    MangaReader.cc → MangaReader.in: update domain (#12041)

--- src/en/mangareadercc/src/eu/kanade/tachiyomi/extension/en/mangareadercc/MangaReaderCC.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangareadercc
-
-import eu.kanade.tachiyomi.multisrc.paprika.PaprikaAlt
-
-class MangaReaderCC : PaprikaAlt("MangaReader.cc", "https://www.mangareader.cc", "en")
