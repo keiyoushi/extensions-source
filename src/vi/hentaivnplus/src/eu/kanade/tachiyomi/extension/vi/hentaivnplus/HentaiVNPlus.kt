fab99e15757070ba96846d0e04afc5ab34b3d784
are-are-are <62763969+dejavui@users.noreply.github.com>
2025-12-11 00:14:55 +0700

    HentaiVNPlus: Update domain (#12096)

--- src/vi/hentaivnplus/src/eu/kanade/tachiyomi/extension/vi/hentaivnplus/HentaiVNPlus.kt
+++ src/vi/hentaivnplus/src/eu/kanade/tachiyomi/extension/vi/hentaivnplus/HentaiVNPlus.kt
@@ -12,7 +12,7 @@ import java.util.Locale
 class HentaiVNPlus :
     Madara(
         "HentaiVN.plus",
-        "https://hentaivn.party",
+        "https://hentaivn.gold",
         "vi",
         dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ROOT),
     ),
