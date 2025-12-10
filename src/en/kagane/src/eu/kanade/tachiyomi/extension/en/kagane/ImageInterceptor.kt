6f458bcd055f3a5d00e3a2627e87905287be4e35
KenjieDec <65448230+KenjieDec@users.noreply.github.com>
2025-12-11 00:16:03 +0700

    Kagane | Supported Other Image Types (#12121)
    Supported Other Image Types

--- src/en/kagane/src/eu/kanade/tachiyomi/extension/en/kagane/ImageInterceptor.kt
+++ src/en/kagane/src/eu/kanade/tachiyomi/extension/en/kagane/ImageInterceptor.kt
@@ -99,8 +99,14 @@ open class ImageInterceptor : Interceptor {
     private fun processData(input: ByteArray, index: Int, seriesId: String, chapterId: String): ByteArray? {
         fun isValidImage(data: ByteArray): Boolean {
             return when {
+                // JPEG
                 data.size >= 2 && data[0] == 0xFF.toByte() && data[1] == 0xD8.toByte() -> true
-                data.size >= 2 && data[0] == 0xFF.toByte() && data[1] == 0x0A.toByte() -> true
+                // GIF
+                data.size >= 6 && (
+                    data.copyOfRange(0, 6).contentEquals("GIF87a".encodeToByteArray()) ||
+                        data.copyOfRange(0, 6).contentEquals("GIF89a".encodeToByteArray())
+                    ) -> true
+                // PNG
                 data.size >= 8 && data.copyOfRange(0, 8).contentEquals(
                     byteArrayOf(
                         0x89.toByte(),
@@ -113,11 +119,21 @@ open class ImageInterceptor : Interceptor {
                         0x0A,
                     ),
                 ) -> true
+                // WEBP
                 data.size >= 12 && data[0] == 'R'.code.toByte() && data[1] == 'I'.code.toByte() &&
                     data[2] == 'F'.code.toByte() && data[3] == 'F'.code.toByte() &&
                     data[8] == 'W'.code.toByte() && data[9] == 'E'.code.toByte() &&
                     data[10] == 'B'.code.toByte() && data[11] == 'P'.code.toByte() -> true
-                data.size >= 12 && data.copyOfRange(0, 12).contentEquals(
+                // HEIF
+                data.size >= 12 && data.copyOfRange(4, 8).contentEquals("ftyp".encodeToByteArray()) -> {
+                    val type = data.copyOfRange(8, 11)
+                    type.contentEquals("hei".encodeToByteArray()) ||
+                        type.contentEquals("hev".encodeToByteArray()) ||
+                        type.contentEquals("avi".encodeToByteArray())
+                }
+                // JXL
+                data.size >= 2 && data[0] == 0xFF.toByte() && data[1] == 0x0A.toByte() -> true
+                data.size >= 12 && data.copyOfRange(0, 8).contentEquals(
                     byteArrayOf(
                         0,
                         0,
