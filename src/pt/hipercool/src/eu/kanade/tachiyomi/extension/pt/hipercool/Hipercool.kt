22452377bd76c5afc07233cd53f2b4fcb0d9855e
WebDitto <webditto@proton.me>
2025-12-13 11:38:56 -0300

    fix (pt/hipercool): Fixed pt/Hipercool source (closes #11579) (#12145)

--- src/pt/hipercool/src/eu/kanade/tachiyomi/extension/pt/hipercool/Hipercool.kt
+++ src/pt/hipercool/src/eu/kanade/tachiyomi/extension/pt/hipercool/Hipercool.kt
@@ -10,6 +10,8 @@ class Hipercool : Madara("HipercooL", "https://hiper.cool", "pt-BR") {
     // Migrated from a custom CMS to Madara.
     override val versionId = 2
 
+    override val chapterUrlSuffix = ""
+
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(1, 2, TimeUnit.SECONDS)
         .build()
