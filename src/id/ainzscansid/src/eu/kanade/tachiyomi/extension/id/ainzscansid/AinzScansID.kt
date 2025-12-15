23131dd4485eac8978ae49f84a3dea3af16cd05c
MuhamadSyabitHidayattulloh <tebepc@gmail.com>
2025-12-15 12:18:45 +0700

    AinzScans: Update Domain (#12175)

--- src/id/ainzscansid/src/eu/kanade/tachiyomi/extension/id/ainzscansid/AinzScansID.kt
+++ src/id/ainzscansid/src/eu/kanade/tachiyomi/extension/id/ainzscansid/AinzScansID.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.extension.id.ainzscansid
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 
-class AinzScansID : MangaThemesia("Ainz Scans ID", "https://ainzscans.net", "id", "/series") {
+class AinzScansID : MangaThemesia("Ainz Scans ID", "https://ainzscans01.com", "id", "/series") {
 
     override val hasProjectPage = true
 }
