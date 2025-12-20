f0e8fe25c95d63ae571459ef5167546959d91521
Chopper <156493704+choppeh@users.noreply.github.com>
2025-12-20 03:56:00 -0300

    Update domains (#12256)
    * ArgosComics: Update domain
    
    * MangaStarz: Update domain
    
    * Shinigami: Update domain
    
    * DoujinsHell: Update domain
    
    * DomalFansub: Update domain

--- src/ar/mangastarz/src/eu/kanade/tachiyomi/extension/ar/mangastarz/MangaStarz.kt
+++ src/ar/mangastarz/src/eu/kanade/tachiyomi/extension/ar/mangastarz/MangaStarz.kt
@@ -1,16 +1,19 @@
 package eu.kanade.tachiyomi.extension.ar.mangastarz
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class MangaStarz : Madara(
     "Manga Starz",
-    "https://manga-starz.com",
+    "https://manga-starz.net",
     "ar",
     dateFormat = SimpleDateFormat("d MMMM، yyyy", Locale("ar")),
 ) {
     override val chapterUrlSuffix = ""
-    override val useLoadMoreRequest = LoadMoreStrategy.Always
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
     override val useNewChapterEndpoint = false
+
+    override fun latestUpdatesRequest(page: Int) = GET(baseUrl, headers)
 }
