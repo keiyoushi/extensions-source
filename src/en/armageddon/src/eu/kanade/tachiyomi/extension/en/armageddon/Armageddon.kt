d705c02a07d9e55f564e676e1989f4bc3b214067
manti <133025162+manti-X@users.noreply.github.com>
2025-12-20 07:54:04 +0100

    Armageddon: fix page parsing (#12251)
    fix page parsing

--- src/en/armageddon/src/eu/kanade/tachiyomi/extension/en/armageddon/Armageddon.kt
+++ src/en/armageddon/src/eu/kanade/tachiyomi/extension/en/armageddon/Armageddon.kt
@@ -1,9 +1,29 @@
 package eu.kanade.tachiyomi.extension.en.armageddon
 
+import android.util.Base64
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.Page
+import keiyoushi.utils.parseAs
+import org.jsoup.nodes.Document
 
 class Armageddon : MangaThemesia(
     name = "Armageddon",
     baseUrl = "https://www.silentquill.net",
     lang = "en",
-)
+) {
+    override fun pageListParse(document: Document): List<Page> {
+        val scriptContent = document.selectFirst("script:containsData(WyJodHRw)")?.data()
+            ?: return super.pageListParse(document)
+        val base64Match = Regex("""['"](WyJodHRw[\w+/=]+)['"]""").find(scriptContent)?.groupValues?.get(1)
+
+        return if (base64Match != null) {
+            val decoded = String(Base64.decode(base64Match, Base64.DEFAULT))
+            val images = decoded.parseAs<List<String>>()
+            images.mapIndexed { i, img ->
+                Page(i, document.location(), img)
+            }
+        } else {
+            super.pageListParse(document)
+        }
+    }
+}
