51b5caeab1f855962be764fb3626b9b27e114ea3
Sebastian Grez Werner <seba_grez_w@hotmail.com>
2025-12-23 01:05:58 -0300

    feat(es): add Escaneo de Leyendas source

---
+++ src/es/escaneodeleyendas/src/eu/kanade/tachiyomi/extension/es/escaneodeleyendas/EscaneoDeLeyendas.kt
@@ -0,0 +1,16 @@
+package eu.kanade.tachiyomi.extension.es.escaneodeleyendas
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class EscaneoDeLeyendas : Madara(
+    "Escaneo de Leyendas",
+    "https://escaneodeleyendas.com",
+    "es",
+    SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("es")),
+) {
+    override fun imageFromElement(element: org.jsoup.nodes.Element): String? {
+        return super.imageFromElement(element)?.trim()
+    }
+}
