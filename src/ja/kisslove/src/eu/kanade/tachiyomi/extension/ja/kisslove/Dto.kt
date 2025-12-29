b7d338e1d34f0666e6fed113801a12e12332c8bb
2Loong6 <1727963726@qq.com>
2025-12-29 22:35:05 +0800

    update kisslove api

---
+++ src/ja/kisslove/src/eu/kanade/tachiyomi/extension/ja/kisslove/Dto.kt
@@ -0,0 +1,79 @@
+package eu.kanade.tachiyomi.extension.ja.kisslove
+
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.jsoup.Jsoup
+import java.time.Instant
+
+@Serializable
+data class PagedManga(
+    val currentPage: Long,
+    val items: List<Manga>,
+    val totalItems: Long,
+    val totalPages: Long,
+)
+
+@Serializable
+data class Manga(
+    val artists: String?,
+    val authors: String,
+    val chapters: List<Chapter> = emptyList(),
+    val cover: String,
+    @SerialName("daily_views") val dailyViews: Long?,
+    val description: String = "",
+    val genres: String?,
+    @SerialName("group_uploader") val groupUploader: Long?,
+    val hidden: Int?,
+    val id: Long,
+    @SerialName("last_chapter") val lastChapter: Double,
+    @SerialName("last_update") val lastUpdate: String?,
+    @SerialName("m_status") val mStatus: Int,
+    val name: String,
+    @SerialName("other_name") val otherName: String,
+    val released: Int?,
+    val showads: Int?,
+    val slug: String,
+    val submitter: Long?,
+    val total: Long,
+    @SerialName("trans_group") val transGroup: String,
+    val views: Long,
+    @SerialName("vote_count") val voteCount: Long,
+) {
+    fun toSManga() = SManga.create().apply {
+        url = this@Manga.slug
+        title = this@Manga.name
+        artist = this@Manga.artists
+        author = this@Manga.authors
+        description = "${Jsoup.parse(this@Manga.description).text()}\n\n$otherName"
+        thumbnail_url = this@Manga.cover
+        genre = this@Manga.genres?.split(",")?.joinToString { it.trim() }
+        status = if (mStatus == 1) SManga.COMPLETED else SManga.ONGOING
+    }
+}
+
+@Serializable
+data class Chapter(
+    val chapter: Double,
+    val content: String = "",
+    val hidden: Int?,
+    val id: Long,
+    @SerialName("last_update") val lastUpdate: String,
+    val manga: String?,
+    val name: String?,
+    val showads: Int?,
+    val submitter: Long?,
+    val views: Long?,
+) {
+    fun toSChapter(slug: String) = SChapter.create().apply {
+        url = "$id/$slug-chapter-$chapter"
+        name = this@Chapter.name?.takeIf { it.isNotBlank() } ?: "Chapter $chapter"
+
+        date_upload = try {
+            Instant.parse(lastUpdate).toEpochMilli()
+        } catch (e: Exception) {
+            0L
+        }
+    }
+}
--- src/ja/kisslove/src/eu/kanade/tachiyomi/extension/ja/kisslove/KissLove.kt
+++ src/ja/kisslove/src/eu/kanade/tachiyomi/extension/ja/kisslove/KissLove.kt
@@ -1,88 +1,111 @@
 package eu.kanade.tachiyomi.extension.ja.kisslove
 
-import eu.kanade.tachiyomi.multisrc.fmreader.FMReader
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.HttpUrl.Companion.toHttpUrl
+import eu.kanade.tachiyomi.source.online.HttpSource
+import keiyoushi.utils.parseAs
+import okhttp3.Headers
 import okhttp3.Request
-import org.jsoup.nodes.Element
-import java.util.Calendar
+import okhttp3.Response
+import java.security.MessageDigest
 
-class KissLove : FMReader("KissLove", "https://klz9.com", "ja") {
+class KissLove : HttpSource() {
+    override val name = "KissLove"
+    override val baseUrl = "https://klz9.com"
+    override val lang = "ja"
+    override val supportsLatest = true
 
-    override val altNameSelector = "li:contains(Other name (s))"
+    override fun headersBuilder() = super.headersBuilder().apply {
+    }
 
-    override fun latestUpdatesRequest(page: Int) =
-        GET("$baseUrl/manga-list.html?page=$page&sort=last_update")
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/api/manga/trending-daily", sigAppend())
+
+    override fun popularMangaParse(response: Response): MangasPage {
+        val result = response.parseAs<List<Manga>>()
+        val mangas = result.map { it.toSManga() }
+        return MangasPage(mangas, false)
+    }
+
+    override fun getMangaUrl(manga: SManga): String {
+        return "$baseUrl/${manga.url}.html"
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/api/manga?page=$page&limit=36", sigAppend())
+
+    override fun latestUpdatesParse(response: Response): MangasPage {
+        val result = response.parseAs<PagedManga>()
+        val mangas = result.items.map { it.toSManga() }
+        val hasNextPage = result.currentPage < result.totalPages
+        return MangasPage(mangas, hasNextPage)
+    }
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request =
+        GET("$baseUrl/api/search?q=$query", sigAppend())
+
+    override fun searchMangaParse(response: Response): MangasPage = popularMangaParse(response)
+
+    override fun mangaDetailsRequest(manga: SManga): Request =
+        GET("$baseUrl/api/manga/slug/${manga.url}", sigAppend())
+
+    override fun mangaDetailsParse(response: Response): SManga {
+        val result = response.parseAs<Manga>()
+        return result.toSManga()
+    }
 
     override fun chapterListRequest(manga: SManga): Request {
-        val mangaId = MID_URL_REGEX.find(manga.url)
-            ?.groupValues?.get(1)
-            ?: throw Exception("Could not find manga id")
-
-        val xhrUrl = "$baseUrl/${generateRandomStr(25)}.lstc".toHttpUrl().newBuilder()
-            .addQueryParameter("slug", mangaId)
-            .build()
-
-        return GET(xhrUrl, headers)
+        return mangaDetailsRequest(manga)
     }
 
-    override fun chapterFromElement(element: Element, mangaTitle: String): SChapter {
-        return SChapter.create().apply {
-            element.select(chapterUrlSelector).first()!!.let {
-                setUrlWithoutDomain("$baseUrl/${it.attr("href")}")
-                name = it.attr("title")
-            }
-
-            date_upload = element.select(chapterTimeSelector)
-                .let { if (it.hasText()) parseChapterDate(it.text()) else 0 }
-        }
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val result = response.parseAs<Manga>()
+        val slug = result.slug
+        return result.chapters.map { it.toSChapter(slug) }
     }
 
-    private fun parseChapterDate(date: String): Long {
-        val value = date.split(' ')[dateValueIndex].toInt()
-        val chapterDate = Calendar.getInstance().apply {
-            set(Calendar.SECOND, 0)
-            set(Calendar.MILLISECOND, 0)
-        }
-
-        when (date.split(' ')[dateWordIndex]) {
-            "mins", "minutes" -> chapterDate.add(Calendar.MINUTE, -value)
-            "hours" -> chapterDate.add(Calendar.HOUR_OF_DAY, -value)
-            "days" -> chapterDate.add(Calendar.DATE, -value)
-            "weeks" -> chapterDate.add(Calendar.DATE, -value * 7)
-            "months" -> chapterDate.add(Calendar.MONTH, -value)
-            "years" -> chapterDate.add(Calendar.YEAR, -value)
-            else -> return 0
-        }
-
-        return chapterDate.timeInMillis
+    override fun getChapterUrl(chapter: SChapter): String {
+        val chapterSuffix = chapter.url.substringAfterLast("/")
+        return "$baseUrl/$chapterSuffix.html"
     }
 
     override fun pageListRequest(chapter: SChapter): Request {
-        val request = super.pageListRequest(chapter)
-        val response = client.newCall(request).execute()
-        val document = response.asJsoup()
-
-        val chapterId = document.selectFirst("#chapter")
-            ?.`val`()
-            ?: throw Exception("Could not find chapter id")
-
-        val xhrUrl = "$baseUrl/${generateRandomStr(30)}.iog".toHttpUrl().newBuilder()
-            .addQueryParameter("cid", chapterId)
-            .build()
-
-        return GET(xhrUrl, headers)
+        val id = chapter.url.substringBeforeLast("/")
+        return GET("$baseUrl/api/chapter/$id", sigAppend())
     }
 
-    private fun generateRandomStr(length: Int): String {
-        return (1..length).map { TO_PATH_CHARACTERS.random() }.joinToString("")
+    override fun pageListParse(response: Response): List<Page> {
+        val result = response.parseAs<Chapter>()
+        return result.content
+            .lines()
+            .filter { it.isNotBlank() }
+            .mapIndexed { i, img ->
+                Page(i, imageUrl = img)
+            }
     }
+
+    override fun getFilterList(): FilterList = FilterList(
+        Filter.Header("筛选"),
+    )
+
+    override fun imageUrlParse(response: Response): String = throw UnsupportedOperationException()
+
+    private fun sigAppend(): Headers = headers.newBuilder().apply {
+        val timestamp = (System.currentTimeMillis() / 1000).toString()
+        val payload = "$timestamp.$CLIENT_ID"
+        val digest = MessageDigest.getInstance("SHA-256")
+        val hashBytes = digest.digest(payload.toByteArray(Charsets.UTF_8))
+        val signature = hashBytes.joinToString("") { byte ->
+            "%02x".format(byte)
+        }
+        add("X-Client-Sig", signature)
+        add("X-Client-Ts", timestamp)
+    }.build()
 
     companion object {
-        private val TO_PATH_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-        private val MID_URL_REGEX = "-([^.]+).html".toRegex()
+        private const val CLIENT_ID = "KL9K40zaSyC9K40vOMLLbEcepIFBhUKXwELqxlwTEF"
     }
 }
