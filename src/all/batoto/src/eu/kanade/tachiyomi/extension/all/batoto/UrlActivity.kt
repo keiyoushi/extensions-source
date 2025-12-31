76f455133b6e48a90a1afae47aa33e8411db5ea8
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2025-12-31 12:59:26 +0700

    Merge structure

--- src/all/batoto/src/eu/kanade/tachiyomi/extension/all/batoto/UrlActivity.kt
+++ src/all/batoto/src/eu/kanade/tachiyomi/extension/all/batoto/UrlActivity.kt
@@ -8,61 +8,22 @@ import android.util.Log
 import kotlin.system.exitProcess
 
 class UrlActivity : Activity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val host = intent?.data?.host
-        val pathSegments = intent?.data?.pathSegments
 
-        if (host != null && pathSegments != null) {
-            val query = fromBatoTo(pathSegments)
+        val mainIntent = Intent().apply {
+            action = "eu.kanade.tachiyomi.SEARCH"
+            putExtra("query", intent.data.toString())
+            putExtra("filter", packageName)
+        }
 
-            if (query == null) {
-                Log.e("BatoToUrlActivity", "Unable to parse URI from intent $intent")
-                finish()
-                exitProcess(1)
-            }
-
-            val mainIntent = Intent().apply {
-                action = "eu.kanade.tachiyomi.SEARCH"
-                putExtra("query", query)
-                putExtra("filter", packageName)
-            }
-
-            try {
-                startActivity(mainIntent)
-            } catch (e: ActivityNotFoundException) {
-                Log.e("BatoToUrlActivity", e.toString())
-            }
+        try {
+            startActivity(mainIntent)
+        } catch (e: ActivityNotFoundException) {
+            Log.e("BatoTo v4", "Unable to launch activity", e)
         }
 
         finish()
         exitProcess(0)
-    }
-
-    private fun fromBatoTo(pathSegments: MutableList<String>): String? {
-        return if (pathSegments.size >= 2) {
-            val path = pathSegments[1] as java.lang.String?
-            if (path != null) {
-                var index = -1
-                for (i in path.indices) {
-                    if (path[i] == '-') {
-                        index = i
-                        break
-                    }
-                }
-
-                val id = if (index == -1) {
-                    path
-                } else {
-                    path.substring(0, index)
-                }
-                "ID:$id"
-            } else {
-                null
-            }
-        } else {
-            null
-        }
     }
 }
