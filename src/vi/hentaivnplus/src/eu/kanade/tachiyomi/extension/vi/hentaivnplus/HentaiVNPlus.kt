d88c256a8bc1dabf7f27ea1827efa69e71700116
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-01-02 02:29:45 +0700

    Update some domain (#12481)
    * TruyenTranh3Q: update domain and add on hiatus status for manga
    
    * VlogTruyen: Update domain
    
    * TopTruyen: Update domain
    
    * HentaiVNPlus: Update domain
    
    * FoxTruyen: Update domain and minor update
    
    * DocTruyen3Q: Update domain
    
    * HangTruyen: Update domain

--- src/vi/hentaivnplus/src/eu/kanade/tachiyomi/extension/vi/hentaivnplus/HentaiVNPlus.kt
+++ src/vi/hentaivnplus/src/eu/kanade/tachiyomi/extension/vi/hentaivnplus/HentaiVNPlus.kt
@@ -12,7 +12,7 @@ import java.util.Locale
 class HentaiVNPlus :
     Madara(
         "HentaiVN.plus",
-        "https://hentaivn.gold",
+        "https://hentaivn.cheap",
         "vi",
         dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ROOT),
     ),
