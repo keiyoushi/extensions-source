5a26f04947c82063da7a497ccecb38291a668d94
CriosChan <36739192+CriosChan@users.noreply.github.com>
2026-01-02 06:46:39 +0100

    Animesama: fetch latest url on runtime (#12401)
    * AnimeSama: Automatically retrieves the most recent functional URL (via the official website).
    
    * AnimeSama: Due to the automatic URL, links will no longer open in Mihon.
    
    * AnimeSama: Remove logs
    
    * AnimeSama: Use Interceptor to get latest URL
    
    * AnimeSama: Changes requested by AwkwardPeak7
    
    * AnimeSama: Remove logs

--- src/fr/animesama/src/eu/kanade/tachiyomi/extension/fr/animesama/AnimeSama.kt
+++ src/fr/animesama/src/eu/kanade/tachiyomi/extension/fr/animesama/AnimeSama.kt
@@ -30,15 +30,17 @@ class AnimeSama : ParsedHttpSource() {
 
     override val name = "AnimeSama"
 
-    override val baseUrl = "https://anime-sama.si"
+    override val baseUrl = "https://anime-sama.pw"
 
     private val cdn = "$baseUrl/s2/scans/"
 
     override val lang = "fr"
 
     override val supportsLatest = true
-
-    override val client: OkHttpClient = network.cloudflareClient
+    private val interceptor = AnimeSamaInterceptor(network.cloudflareClient, baseUrl, headers)
+    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
+        .addInterceptor(interceptor)
+        .build()
 
     override fun headersBuilder(): Headers.Builder = super.headersBuilder()
         .add("Accept-Language", "fr-FR")
@@ -341,5 +343,17 @@ class AnimeSama : ParsedHttpSource() {
             .build()
 
         return GET(page.imageUrl!!, imgHeaders)
+    }
+
+    fun convertUrlToLatestDomain(url: String): String {
+        return "${interceptor.getBaseUrl()!!}$url"
+    }
+
+    override fun getMangaUrl(manga: SManga): String {
+        return convertUrlToLatestDomain(manga.url)
+    }
+
+    override fun getChapterUrl(chapter: SChapter): String {
+        return convertUrlToLatestDomain(chapter.url)
     }
 }
