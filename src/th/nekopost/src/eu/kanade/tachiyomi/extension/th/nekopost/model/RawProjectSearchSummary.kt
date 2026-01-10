45ae76a2254644a592a143b9607bd49c098fa09c
keegang6705 <darunphobwi@gmail.com>
2026-01-11 05:53:41 +0700

    fix import ordering

--- src/th/nekopost/src/eu/kanade/tachiyomi/extension/th/nekopost/model/RawProjectSearchSummary.kt
+++ src/th/nekopost/src/eu/kanade/tachiyomi/extension/th/nekopost/model/RawProjectSearchSummary.kt
@@ -1,7 +1,7 @@
 package eu.kanade.tachiyomi.extension.th.nekopost.model
 
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Serializable
 data class RawProjectSearchSummary(
