867a42522a966706ba948dd5eff263954a1e6a55
MuhamadSyabitHidayattulloh <tebepc@gmail.com>
2026-01-10 23:08:12 +0700

    Kiryuu: fix HTTP 520 (#12616)
    * Kiryuu: fix HTTP 520
    
    * Kiryuu: bump version code to 45

--- src/id/kiryuu/src/eu/kanade/tachiyomi/extension/id/kiryuu/Kiryuu.kt
+++ src/id/kiryuu/src/eu/kanade/tachiyomi/extension/id/kiryuu/Kiryuu.kt
@@ -1,8 +1,11 @@
 package eu.kanade.tachiyomi.extension.id.kiryuu
 
 import eu.kanade.tachiyomi.multisrc.natsuid.NatsuId
+import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.SManga
 import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class Kiryuu : NatsuId(
     "Kiryuu",
@@ -13,4 +16,12 @@ class Kiryuu : NatsuId(
     override val id = 3639673976007021338
 
     override fun OkHttpClient.Builder.customizeClient() = rateLimit(4)
+
+    override fun chapterListRequest(manga: SManga): Request {
+        val url = super.chapterListRequest(manga).url.newBuilder()
+            .setQueryParameter("page", "1")
+            .build()
+
+        return GET(url, headers)
+    }
 }
