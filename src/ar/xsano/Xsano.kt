3aa0dfd17c0e02009d542bc7f603db256633401c
tolpamostafa55-pixel <tolpamostafa55@gmail.com>
2026-01-11 09:04:33 +0200

    Create Xsano.kt
    Add Xsano Manga source

---
+++ src/ar/xsano/Xsano.kt
@@ -0,0 +1,13 @@
+package eu.kanade.tachiyomi.extension.ar.xsano
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Xsano : Madara(
+    "Xsano Manga",
+    "https://www.xsano-manga.com",
+    "ar",
+    SimpleDateFormat("MMMM d, yyyy", Locale("ar"))
+)
+
