d651d0ea73f7db4a95172e5d26710b34dc42e1d9
ofir1bar <ofir1bar@gmail.com>
2026-01-19 20:23:04 +0200

    feat: add-source gafeland

---
+++ src/tr/gafeland/src/eu/kanade/tachiyomi/extension/tr/gafeland/gafeland.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.tr.gafeland
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class gafeland : MangaThemesia(
+    "gafeland",
+    "https://gafeland.com",
+    "tr",
+)
