68328419e49a2c78b075528af86a7baa8a511d27
ofir1bar <ofir1bar@gmail.com>
2026-01-20 20:42:51 +0200

    fix: PR comments

--- src/tr/gafeland/src/eu/kanade/tachiyomi/extension/tr/gafeland/gafeland.kt
+++ src/tr/gafeland/src/eu/kanade/tachiyomi/extension/tr/gafeland/gafeland.kt
@@ -3,10 +3,8 @@ package eu.kanade.tachiyomi.extension.tr.gafeland
 import android.util.Base64
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.source.model.Page
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonPrimitive
+import keiyoushi.utils.parseAs
 import org.jsoup.nodes.Document
-import java.lang.IllegalArgumentException
 
 class gafeland : MangaThemesia(
     "gafeland",
@@ -20,14 +18,15 @@ class gafeland : MangaThemesia(
             ?: return super.pageListParse(document)
         val data = Base64.decode(script.attr("src").substringAfter("base64,"), Base64.DEFAULT).toString(Charsets.UTF_8)
         val imageListJson = JSON_IMAGE_LIST_REGEX.find(data)?.destructured?.toList()?.get(0).orEmpty()
+
         val imageList = try {
-            json.parseToJsonElement(imageListJson).jsonArray
-        } catch (_: IllegalArgumentException) {
+            imageListJson.parseAs<List<String>>()
+        } catch (_: Exception) {
             emptyList()
         }
 
-        return imageList.mapIndexed { i, jsonEl ->
-            Page(i, imageUrl = jsonEl.jsonPrimitive.content)
+        return imageList.mapIndexed { i, url ->
+            Page(i, imageUrl = url)
         }
     }
 }
