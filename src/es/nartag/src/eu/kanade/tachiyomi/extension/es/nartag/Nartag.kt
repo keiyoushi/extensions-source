6ae7f8ce2cb58f4177d6a2c466783ca1714ec071
MuhamadSyabitHidayattulloh <tebepc@gmail.com>
2026-01-20 15:07:44 +0700

    Nartag: Update domain (#12826)
    * Nartag: Update Domain
    
    * Nartag: Fix date format

--- src/es/nartag/src/eu/kanade/tachiyomi/extension/es/nartag/Nartag.kt
+++ src/es/nartag/src/eu/kanade/tachiyomi/extension/es/nartag/Nartag.kt
@@ -4,11 +4,14 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class Nartag : Madara(
     "Traducciones Amistosas",
-    "https://traduccionesamistosas.topmanhuas.org",
+    "https://nartag.com",
     "es",
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
 ) {
     override val versionId = 2
 
