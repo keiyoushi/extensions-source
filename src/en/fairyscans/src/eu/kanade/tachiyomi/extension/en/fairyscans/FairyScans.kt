5893bffefe51c52e9f11da504436e4920348c642
Aaron <168236779+AaronDeimos@users.noreply.github.com>
2026-01-20 13:38:49 +0530

    FairyScans: add source (#12842)
    * add fairyscans
    
    * remove unnecessary

---
+++ src/en/fairyscans/src/eu/kanade/tachiyomi/extension/en/fairyscans/FairyScans.kt
@@ -0,0 +1,34 @@
+package eu.kanade.tachiyomi.extension.en.fairyscans
+
+import android.content.SharedPreferences
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaPaidChapterHelper
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.ConfigurableSource
+import keiyoushi.utils.getPreferences
+import okhttp3.OkHttpClient
+
+class FairyScans :
+    MangaThemesia("Fairy Scans", "https://fairyscans.com", "en"),
+    ConfigurableSource {
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(2)
+        .build()
+
+    private val preferences: SharedPreferences = getPreferences()
+
+    private val paidChapterHelper = MangaThemesiaPaidChapterHelper()
+
+    override fun chapterListSelector(): String {
+        return paidChapterHelper.getChapterListSelectorBasedOnHidePaidChaptersPref(
+            super.chapterListSelector(),
+            preferences,
+        )
+    }
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        paidChapterHelper.addHidePaidChaptersPreferenceToScreen(screen, intl)
+    }
+}
