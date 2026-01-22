d92c0fa2eb5ac12c5655d13b33cd5613efa22e34
dragon-masterk <83424830+dragon-masterk@users.noreply.github.com>
2026-01-21 22:17:11 -0500

    fix(madarascans)
    updated theme layout
    
    closes #12644

--- src/en/madarascans/src/eu/kanade/tachiyomi/extension/en/madarascans/MadaraScans.kt
+++ src/en/madarascans/src/eu/kanade/tachiyomi/extension/en/madarascans/MadaraScans.kt
@@ -1,12 +1,10 @@
 package eu.kanade.tachiyomi.extension.en.madarascans
 
-import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaPaidChapterHelper
-import eu.kanade.tachiyomi.source.ConfigurableSource
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import keiyoushi.utils.getPreferencesLazy
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -18,13 +16,10 @@ class MadaraScans :
         "en",
         mangaUrlDirectory = "/series",
         dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.US),
-    ),
-    ConfigurableSource {
-
-    override val dateFormatSelector = "yyyy/MM/dd"
+    ) {
 
     // support for both popular/latest tabs and search
-    override fun searchMangaSelector() = ".listupd, div.legend-inner"
+    override fun searchMangaSelector() = "div.listupd>div, div.legend-inner"
 
     override fun searchMangaFromElement(element: Element) = SManga.create().apply {
         thumbnail_url = element.select("img").imgAttr()
@@ -37,21 +32,14 @@ class MadaraScans :
     // manga details
     override val seriesDetailsSelector = "div.lh-container"
     override val seriesTitleSelector = "h1.lh-title"
-
-    // missing following fields from source
-    // override val seriesArtistSelector = ""
-    // override val seriesAuthorSelector = ""
-    // override val seriesTypeSelector = ""
-    // override val altNamePrefix = ""
-
     override val seriesDescriptionSelector = "div.lh-story > #manga-story"
     override val seriesAltNameSelector = ".fa-info-circle"
     override val seriesGenreSelector = ".lh-genres > .lh-genre-tag"
     override val seriesStatusSelector = "span.status-badge-lux"
     override val seriesThumbnailSelector = ".lh-poster > img"
 
-    // chapters
-    override fun chapterListSelector(): String = "#chapters-list-container > div"
+    // limiting chapters to free
+    override fun chapterListSelector(): String = ".ch-item.free"
 
     override fun chapterFromElement(element: Element) = SChapter.create().apply {
         val urlElements = element.select("a")
@@ -61,16 +49,37 @@ class MadaraScans :
         date_upload = dateElement.parseChapterDate()
     }
 
-    //todo: fix searching, instead of using /series/title= use /?s=
-
-    override val pageSelector = "div#readerArea img"
-
-    private val preferences by getPreferencesLazy()
-
-    //todo: fix lockedChapterSelector
-    private val paidChapterHelper = MangaThemesiaPaidChapterHelper(lockedChapterSelector = ".ch-price-side")
-
-    override fun setupPreferenceScreen(screen: PreferenceScreen) {
-        paidChapterHelper.addHidePaidChaptersPreferenceToScreen(screen, intl)
+    override fun getFilterList(): FilterList {
+        val filters = mutableListOf<Filter<*>>(
+            Filter.Separator(),
+            StatusFilter(intl["status_filter_title"], statusOptions),
+            TypeFilter(intl["type_filter_title"], typeFilterOptions),
+            OrderByFilter(intl["order_by_filter_title"], orderByFilterOptions),
+        )
+        if (!genrelist.isNullOrEmpty()) {
+            filters.addAll(
+                listOf(
+                    Filter.Header(intl["genre_exclusion_warning"]),
+                    GenreListFilter(intl["genre_filter_title"], getGenreList()),
+                ),
+            )
+        } else {
+            filters.add(
+                Filter.Header(intl["genre_missing_warning"]),
+            )
+        }
+        if (hasProjectPage) {
+            filters.addAll(
+                mutableListOf<Filter<*>>(
+                    Filter.Separator(),
+                    Filter.Header(intl["project_filter_warning"]),
+                    Filter.Header(intl.format("project_filter_name", name)),
+                    ProjectFilter(intl["project_filter_title"], projectFilterOptions),
+                ),
+            )
+        }
+        return FilterList(filters)
     }
+
+    override val pageSelector = ".pagination, .legendary-pagination, .magma-pagination"
 }
