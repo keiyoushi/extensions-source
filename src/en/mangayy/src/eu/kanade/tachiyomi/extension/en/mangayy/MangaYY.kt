69b25441e41a87da65a91af2b0040a3f5508d284
Aaron <aaronndeimos@gmail.com>
2026-01-23 10:39:36 +0530

    mangayy: add source

---
+++ src/en/mangayy/src/eu/kanade/tachiyomi/extension/en/mangayy/MangaYY.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.mangayy
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class MangaYY : Madara("MangaYY", "https://mangayy.org", "en") {
+    override fun searchMangaSelector() = popularMangaSelector()
+}
