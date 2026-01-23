15f04eb5847f93c4ce51d81af6c5d1112f194055
Aaron <aaronndeimos@gmail.com>
2026-01-23 21:44:15 +0530

    set useNewChapterEndpoint to true

--- src/en/mangayy/src/eu/kanade/tachiyomi/extension/en/mangayy/MangaYY.kt
+++ src/en/mangayy/src/eu/kanade/tachiyomi/extension/en/mangayy/MangaYY.kt
@@ -4,4 +4,5 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 
 class MangaYY : Madara("MangaYY", "https://mangayy.org", "en") {
     override fun searchMangaSelector() = popularMangaSelector()
+    override val useNewChapterEndpoint = true
 }
