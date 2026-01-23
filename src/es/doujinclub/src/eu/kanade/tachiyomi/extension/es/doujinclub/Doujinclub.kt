9c020940c6efdacb233a3271c305d2651d952c6a
bapeey <90949336+bapeey@users.noreply.github.com>
2026-01-23 11:55:03 -0500

    fixes

---
+++ src/es/doujinclub/src/eu/kanade/tachiyomi/extension/es/doujinclub/Doujinclub.kt
@@ -0,0 +1,11 @@
+package eu.kanade.tachiyomi.extension.es.doujinclub
+
+import eu.kanade.tachiyomi.multisrc.lectormoe.LectorMoe
+
+class Doujinclub : LectorMoe(
+    "Doujinclub",
+    "https://capibaratraductor.com/doujinclub",
+    "es",
+    organizationDomain = "doujinclub",
+    apiBaseUrl = "https://capibaratraductor.com",
+)
