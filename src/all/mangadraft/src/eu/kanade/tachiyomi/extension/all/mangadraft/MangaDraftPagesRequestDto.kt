aed4204597d37e5ab9ae8bfeb94a2ec8d7a29ad6
ofir baruch <ofir1bar@gmail.com>
2026-01-24 22:45:00 +0200

    feat: finished main features
    search query text doesn't work(not easy place to use it), no one-shot support

--- src/all/mangadraft/src/eu/kanade/tachiyomi/extension/all/mangadraft/MangaDraftPagesRequestDto.kt
+++ src/all/mangadraft/src/eu/kanade/tachiyomi/extension/all/mangadraft/MangaDraftPagesRequestDto.kt
@@ -1,86 +1,25 @@
 package eu.kanade.tachiyomi.extension.all.mangadraft.dto
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MangadraftPagesResponseDto(
-    val data: List<MangadraftPageDto>,
-)
-
-@Serializable
-data class MangadraftPageDto(
-    val id: Int,
-
-    @SerialName("project_category_id")
-    val projectCategoryId: Int,
-
-    @SerialName("page_number")
-    val pageNumber: Int,
-
-    val name: String?,
-
-    val type: String,
-
-    val text: String?,
-
-    @SerialName("nb_likes")
-    val nbLikes: Int,
-
-    @SerialName("nb_views")
-    val nbViews: Int,
-
-    @SerialName("nb_comments")
-    val nbComments: Int,
-
-    @SerialName("published_at")
-    val publishedAt: String,
-
-    @SerialName("published_ago")
-    val publishedAgo: String,
-
-    @SerialName("created_at")
-    val createdAt: String,
-
-    @SerialName("updated_at")
-    val updatedAt: String,
-
-    val delayed: Boolean,
-
-    @SerialName("published_date")
-    val publishedDate: String,
-
-    val nudity: Int,
-
-    val image: MangadraftImageDto?,
-
-    @SerialName("image_censored")
-    val imageCensored: MangadraftImageDto?,
-
+data class MangadraftPageDTO(
+    val id: Long,
+    val number: Int,
     val url: String,
-
-    @SerialName("url_censored")
-    val urlCensored: String?,
-
-    val viewed: Boolean?,
-)
-
-@Serializable
-data class MangadraftImageDto(
-    val id: Int,
-    val hash: String,
-
-    @SerialName("created_at")
-    val createdAt: String,
-
-    @SerialName("updated_at")
-    val updatedAt: String,
-
+    val viewed: Boolean,
+    val liked: Boolean,
+    val likes: Int,
+    val comments: Int,
+    val views: Int,
+    val cat: Long,
     val ago: String,
-
-    val url: String,
-    val img: String,
-
-    val w: Int?,
-    val h: Int?,
+    val delayed: Boolean,
+    val type: String,
+    val paying: Int,
+    val w: Int,
+    val h: Int,
 )
+
+// Top-level DTO: map of category ID -> list of pages
+typealias PagesByCategory = Map<String, List<MangadraftPageDTO>>
