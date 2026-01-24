83ddc2ddbe7915501c83e2d7674e7eb354d2fd4e
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-01-24 22:04:49 +0100

    Remove IsekaiScanManga (unoriginal)

--- src/en/isekaiscanmanga/src/eu/kanade/tachiyomi/extension/en/isekaiscanmanga/IsekaiScanManga.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.isekaiscanmanga
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class IsekaiScanManga : Madara("IsekaiScanManga (unoriginal)", "https://isekaiscanmanga.com", "en", SimpleDateFormat("dd MMM، yyyy", Locale.US))
