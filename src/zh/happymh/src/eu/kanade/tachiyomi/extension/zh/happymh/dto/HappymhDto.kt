92d4d16cc028c649b5fb5ff7d91ec00244930db7
Kim Yeung <31816636+miaachan@users.noreply.github.com>
2026-01-24 07:59:52 -0500

    Happymh: Fix WebpExceedRange (#12972)
    * fix: strip query params ?q=... when image exceed webp limit
    
    * chore: bump extVersionCode

--- src/zh/happymh/src/eu/kanade/tachiyomi/extension/zh/happymh/dto/HappymhDto.kt
+++ src/zh/happymh/src/eu/kanade/tachiyomi/extension/zh/happymh/dto/HappymhDto.kt
@@ -45,5 +45,5 @@ data class PageListResponseDto(val data: PageListData)
 @Serializable
 data class PageListData(val scans: List<PageDto>) {
     @Serializable
-    data class PageDto(val n: Int, val url: String)
+    data class PageDto(val n: Int, val url: String, val width: Int = 0, val height: Int = 0)
 }
