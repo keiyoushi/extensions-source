a3431c0ed21289263ae352b2d119d5a10189e3f0
Aaron <aaronndeimos@gmail.com>
2026-01-25 11:03:45 +0530

    shinigami: update url

--- src/id/shinigami/src/eu/kanade/tachiyomi/extension/id/shinigami/Shinigami.kt
+++ src/id/shinigami/src/eu/kanade/tachiyomi/extension/id/shinigami/Shinigami.kt
@@ -23,7 +23,7 @@ class Shinigami : HttpSource() {
 
     override val name = "Shinigami"
 
-    override val baseUrl = "https://08.shinigami.asia"
+    override val baseUrl = "https://09.shinigami.asia"
 
     private val apiUrl = "https://api.shngm.io"
 
