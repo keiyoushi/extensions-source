4f4881cc2bc0e54de7c2c909fec7e26ff1f835a0
Aaron <168236779+AaronDeimos@users.noreply.github.com>
2026-01-25 12:46:50 +0530

    update some urls (#12999)
    * mhscans: update url
    
    * shinigami: update url
    
    * umetruyen: update url
    
    * mangalink: update url
    
    * doujinku: update url
    
    * manhwadesu: update url
    
    * shinigami: update bump

--- src/id/manhwadesu/src/eu/kanade/tachiyomi/extension/id/manhwadesu/ManhwaDesu.kt
+++ src/id/manhwadesu/src/eu/kanade/tachiyomi/extension/id/manhwadesu/ManhwaDesu.kt
@@ -9,7 +9,7 @@ import java.util.Locale
 
 class ManhwaDesu : MangaThemesia(
     "ManhwaDesu",
-    "https://manhwadesu.io",
+    "https://manhwadesu.art",
     "id",
     "/komik",
     SimpleDateFormat("MMMM dd, yyyy", Locale("id")),
