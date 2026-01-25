a0fdbb64ee425106e17e7cf0e22fdbb0fa474676
ofir1bar <ofir1bar@gmail.com>
2026-01-25 21:48:33 +0200

    feat: PR comments and added scuff one-shot support

--- src/all/mangadraft/src/eu/kanade/tachiyomi/extension/all/mangadraft/MangaDraftPagesRequestDto.kt
+++ src/all/mangadraft/src/eu/kanade/tachiyomi/extension/all/mangadraft/MangaDraftPagesRequestDto.kt
@@ -3,23 +3,11 @@ package eu.kanade.tachiyomi.extension.all.mangadraft.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MangadraftPageDTO(
+data class MangaDraftPageDTO(
     val id: Long,
     val number: Int,
     val url: String,
-    val viewed: Boolean,
-    val liked: Boolean,
-    val likes: Int,
-    val comments: Int,
-    val views: Int,
-    val cat: Long,
-    val ago: String,
-    val delayed: Boolean,
-    val type: String,
-    val paying: Int,
-    val w: Int,
-    val h: Int,
 )
 
 // Top-level DTO: map of category ID -> list of pages
-typealias PagesByCategory = Map<String, List<MangadraftPageDTO>>
+typealias PagesByCategory = Map<String, List<MangaDraftPageDTO>>
