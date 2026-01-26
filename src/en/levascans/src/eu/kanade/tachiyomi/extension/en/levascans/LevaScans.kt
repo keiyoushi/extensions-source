be34f30a5c7631a394feb352384e287dc8457d9c
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-01-26 19:31:21 +0100

    Remove dead extensions (various issues) (#12988)
    * Remove Inova Scan Manga
    
    * Remove Zin-Manga.com
    
    * Remove Ignis Comic
    
    * Remove Invincible ComicsVF
    
    * Remove Anisa Manga
    
    * Remove Anteiku Scan
    
    * Remove Atikrost
    
    * Remove Aurora Scan
    
    * Remove Ayatoon
    
    * Remove ComicTop
    
    * Remove Comicz.net v2
    
    * Remove ED Scanlation
    
    * Remove En Ligne Manga
    
    * Remove Galinha Samurai Scan
    
    * Remove KomikGes
    
    * Remove KumoTran
    
    * Remove LegnMangas
    
    * Remove Leva Scans
    
    * Remove Manga Empress
    
    * Remove Manga Koinu
    
    * Remove MangasX
    
    * Remove Manhuaga
    
    * Remove Meitua.top
    
    * Remove Miae Translations
    
    * Remove RE Manga (Arabic)
    
    * Remove Rh2PlusManga
    
    * Remove RofanToon
    
    * Remove Rua Hap Chanh Day
    
    * Remove Safire Scan
    
    * Remove Superdoujin.org
    
    * Remove Terco Scans
    
    * Remove Time Naight
    
    * Remove Zandy no Fansub

--- src/en/levascans/src/eu/kanade/tachiyomi/extension/en/levascans/LevaScans.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.levascans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class LevaScans : MangaThemesia(
-    "Leva Scans",
-    "https://levascans.com",
-    "en",
-)
--- src/fr/edscanlation/src/eu/kanade/tachiyomi/extension/fr/edscanlation/EDScanlation.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.edscanlation
-
-import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
-
-class EDScanlation : Keyoapp("ED Scanlation", "https://edscanlation.fr", "fr")
