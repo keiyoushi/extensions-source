1eb58d1acea99dc7512b8dee83cec0bf1dcba38c
Aaron <aaronndeimos@gmail.com>
2026-01-29 20:54:17 +0530

    restore mantrazscan + update url and logo

---
+++ src/es/mantrazscan/src/eu/kanade/tachiyomi/extension/es/mantrazscan/MantrazScan.kt
@@ -0,0 +1,23 @@
+package eu.kanade.tachiyomi.extension.es.mantrazscan
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class MantrazScan : Madara(
+    "Mantraz Scan",
+    "https://manhwascan.lat",
+    "es",
+    SimpleDateFormat("dd/MM/yyyy", Locale("es")),
+) {
+    override val client = super.client.newBuilder()
+        .rateLimitHost(baseUrl.toHttpUrl(), 3, 1, TimeUnit.SECONDS)
+        .build()
+
+    override val useNewChapterEndpoint = true
+
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+}
