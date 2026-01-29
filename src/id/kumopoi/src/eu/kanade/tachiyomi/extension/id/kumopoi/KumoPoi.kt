7f05ff77b96166842261b59e8ec06fbf560ffac5
TheKingTermux <50316075+TheKingTermux@users.noreply.github.com>
2026-01-29 13:51:27 +0700

    [ID] Add some MangaThemesia ext (#13065)
    * Add some ext (ID)
    
    * hhmm
    
    * add kumopoi
    
    * Update KyoudaiManga.kt
    
    * Remove KyoudaiManga
    
    The website isn't using MangaThemesia now
    
    * Add DreamTeamsScans
    
    * Make VersionCode 0
    
    * Make it better

---
+++ src/id/kumopoi/src/eu/kanade/tachiyomi/extension/id/kumopoi/KumoPoi.kt
@@ -0,0 +1,16 @@
+package eu.kanade.tachiyomi.extension.id.kumopoi
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class KumoPoi : MangaThemesia(
+    "KumoPoi",
+    "https://kumopoi.org",
+    "id",
+    "/manga",
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.forLanguageTag("id")),
+) {
+
+    override val hasProjectPage = true
+}
