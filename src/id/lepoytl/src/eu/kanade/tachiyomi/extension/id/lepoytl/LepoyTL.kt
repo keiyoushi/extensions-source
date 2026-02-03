72d684f37f9d2f709926b5a5807f13805c234342
Syabit <tebepc@gmail.com>
2026-02-03 13:16:20 +0700

    LepoyTL (ID): add source (#13145)
    * LepoyTL (ID): add source
    
    * LepoyTL (ID): update icon

---
+++ src/id/lepoytl/src/eu/kanade/tachiyomi/extension/id/lepoytl/LepoyTL.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.id.lepoytl
+
+import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
+
+class LepoyTL : ZeistManga("LepoyTL", "https://www.lepoytl.my.id", "id")
