7565e4e14c704ca0c51dd2382fe1b2459ecbcc73
Aaron <168236779+AaronDeimos@users.noreply.github.com>
2026-02-08 20:31:47 +0530

    Elftoon: update url (#13260)
    * Revert "Elftoon: update url (#12600)"
    
    This reverts commit f0bc0e34aeb9fedb634e541c3e94093a84bf8bab.
    
    * bump

--- src/en/elftoon/src/eu/kanade/tachiyomi/extension/en/elftoon/ElfToon.kt
+++ src/en/elftoon/src/eu/kanade/tachiyomi/extension/en/elftoon/ElfToon.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.extension.en.elftoon
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 
-class ElfToon : MangaThemesia("Elf Toon", "https://elftoon.xyz", "en") {
+class ElfToon : MangaThemesia("Elf Toon", "https://elftoon.com", "en") {
 
     override fun chapterListSelector() = "#chapterlist li:not(:has(.gem-price-icon))"
 }
