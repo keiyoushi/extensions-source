b0d3a873d91c243ddeed8da7c700d2704b13ef74
manti <133025162+manti-X@users.noreply.github.com>
2026-02-08 16:01:09 +0100

    Comic Gardo: fix selectors (#13251)
    selectors

--- src/ja/comicgardo/src/eu/kanade/tachiyomi/extension/ja/comicgardo/ComicGardo.kt
+++ src/ja/comicgardo/src/eu/kanade/tachiyomi/extension/ja/comicgardo/ComicGardo.kt
@@ -2,34 +2,44 @@ package eu.kanade.tachiyomi.extension.ja.comicgardo
 
 import eu.kanade.tachiyomi.multisrc.gigaviewer.GigaViewer
 import eu.kanade.tachiyomi.source.model.SManga
-import okhttp3.OkHttpClient
 import org.jsoup.nodes.Element
 
-class ComicGardo : GigaViewer(
-    "Comic Gardo",
-    "https://comic-gardo.com",
-    "ja",
-    "https://cdn-img.comic-gardo.com/public/page",
-    isPaginated = true,
-) {
+class ComicGardo :
+    GigaViewer(
+        "Comic Gardo",
+        "https://comic-gardo.com",
+        "ja",
+        "https://cdn-img.comic-gardo.com/public/page",
+        isPaginated = true,
+    ) {
 
     override val supportsLatest: Boolean = false
 
-    override val client: OkHttpClient = super.client.newBuilder()
+    override val client = super.client.newBuilder()
         .addInterceptor(::imageIntercept)
         .build()
 
     override val publisher: String = "オーバーラップ"
 
-    override fun popularMangaSelector(): String = "ul.series-section-list li.series-section-item > a"
+    override fun popularMangaSelector(): String = "a[class^=SeriesListItem_link_]"
 
     override fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
-        title = element.selectFirst("h5.series-title")!!.text()
-        thumbnail_url = element.selectFirst("div.thumb img")!!.attr("data-src")
-        setUrlWithoutDomain(element.attr("href")!!)
+        title = element.selectFirst("[class^=SeriesListItem_series_title_]")!!.text()
+        thumbnail_url = element.selectFirst("img")?.absUrl("src")
+        setUrlWithoutDomain(element.selectFirst("a")!!.absUrl("href"))
+    }
+
+    override fun searchMangaSelector(): String = "ul[class^=SearchResult_search_result_list_] li"
+
+    override fun searchMangaFromElement(element: Element): SManga = SManga.create().apply {
+        title = element.selectFirst("p[class^=SearchResultItem_series_title_]")!!.text()
+        thumbnail_url = element.selectFirst("img")?.absUrl("src")
+        setUrlWithoutDomain(element.selectFirst("a")!!.absUrl("href"))
     }
 
     override fun getCollections(): List<Collection> = listOf(
-        Collection("連載一覧", ""),
+        Collection("連載作品", ""),
+        Collection("アンソロジー・読切", "oneshot"),
+        Collection("連載終了作品", "completed"),
     )
 }
