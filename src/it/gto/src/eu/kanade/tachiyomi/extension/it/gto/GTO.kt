ff6e60a72721f3ecfe11d81ae308c7735a4fa170
GH-Capital <syawalsawal099@gmail.com>
2026-02-08 17:30:03 +0700

    Delete src/it directory

--- src/it/gto/src/eu/kanade/tachiyomi/extension/it/gto/GTO.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.it.gto
-
-import eu.kanade.tachiyomi.multisrc.pizzareader.PizzaReader
-
-class GTO : PizzaReader("GTO The Great Site", "https://reader.gtothegreatsite.net", "it") {
-    override val versionId = 2
-}
--- src/it/mangaworldadult/src/eu/kanade/tachiyomi/extension/it/mangaworldadult/MangaworldAdult.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.it.mangaworldadult
-
-import eu.kanade.tachiyomi.multisrc.mangaworld.MangaWorld
-
-class MangaworldAdult : MangaWorld("MangaworldAdult", "https://www.mangaworldadult.net", "it")
