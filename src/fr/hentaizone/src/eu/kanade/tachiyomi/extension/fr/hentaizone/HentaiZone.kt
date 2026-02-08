105ab388bf391f7f84bca636ad602ef523afe7af
GH-Capital <syawalsawal099@gmail.com>
2026-02-08 17:29:55 +0700

    Delete src/fr directory

--- src/fr/hentaizone/src/eu/kanade/tachiyomi/extension/fr/hentaizone/HentaiZone.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.hentaizone
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class HentaiZone : Madara("HentaiZone", "https://hentaizone.xyz", "fr", dateFormat = SimpleDateFormat("MMM d, yyyy", Locale.FRENCH)) {
-    override val mangaSubString = "tous-les-mangas"
-}
--- src/fr/pantheonscan/src/eu/kanade/tachiyomi/extension/fr/pantheonscan/PantheonScan.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.pantheonscan
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class PantheonScan : Madara("Pantheon Scan", "https://pantheon-scan.com", "fr", dateFormat = SimpleDateFormat("d MMMM yyyy", Locale.FRANCE)) {
-    override val useNewChapterEndpoint = true
-}
