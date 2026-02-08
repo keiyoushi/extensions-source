7724bbbda65fc5996ddff86204e790f2f63f36a6
GH-Capital <syawalsawal099@gmail.com>
2026-02-08 17:30:43 +0700

    Delete src/ru directory

--- src/ru/senkognito/src/eu/kanade/tachiyomi/extension/ru/senkognito/Senkognito.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.ru.senkognito
-
-import eu.kanade.tachiyomi.multisrc.senkuro.Senkuro
-
-class Senkognito : Senkuro("Senkognito", "https://senkognito.com", "ru")
--- src/ru/senkuro/src/eu/kanade/tachiyomi/extension/ru/senkuro/Senkuro.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.ru.senkuro
-
-import eu.kanade.tachiyomi.multisrc.senkuro.Senkuro
-
-class Senkuro : Senkuro("Senkuro", "https://senkuro.com", "ru")
