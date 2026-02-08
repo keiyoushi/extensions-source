46a856e0d99f303bfa1b84345e7a28849570be42
GH-Capital <syawalsawal099@gmail.com>
2026-02-08 17:30:34 +0700

    Delete src/pt directory

--- src/pt/mangaflix/src/eu/kanade/tachiyomi/extension/pt/mangaflix/Filters.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.pt.mangaflix
-
-import eu.kanade.tachiyomi.source.model.Filter
-
-class Genre(name: String, val id: String) : Filter.CheckBox(name)
-
-class GenreFilter(genres: List<Genre>) : Filter.Group<Genre>("Gêneros", genres)
