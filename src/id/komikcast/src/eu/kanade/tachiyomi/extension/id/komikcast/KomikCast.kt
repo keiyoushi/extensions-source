0a09fb172dbdfbdae225b8513dda31c33794837e
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-02-09 11:10:40 +0500

    Clean up imports

--- src/id/komikcast/src/eu/kanade/tachiyomi/extension/id/komikcast/KomikCast.kt
+++ src/id/komikcast/src/eu/kanade/tachiyomi/extension/id/komikcast/KomikCast.kt
@@ -9,17 +9,11 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
 import keiyoushi.utils.parseAs
-import keiyoushi.utils.tryParse
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import java.text.DecimalFormat
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class KomikCast : HttpSource() {
 
