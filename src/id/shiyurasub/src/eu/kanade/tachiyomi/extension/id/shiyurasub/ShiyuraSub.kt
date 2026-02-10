f3c45bd2f1ff99bc8cc14c09e244ceaa2615172e
MuhamadSyabitHidayattulloh <tebepc@gmail.com>
2026-02-10 12:01:29 +0700

    vscode idiot

--- src/id/shiyurasub/src/eu/kanade/tachiyomi/extension/id/shiyurasub/ShiyuraSub.kt
+++ src/id/shiyurasub/src/eu/kanade/tachiyomi/extension/id/shiyurasub/ShiyuraSub.kt
@@ -17,40 +17,5 @@ class ShiyuraSub : ZeistManga("ShiyuraSub", "https://shiyurasub.blogspot.com", "
     override fun popularMangaRequest(page: Int): Request = latestUpdatesRequest(page)
     override fun popularMangaParse(response: Response): MangasPage = latestUpdatesParse(response)
 
-    override fun mangaDetailsParse(response: Response): SManga {
-        val document = response.asJsoup()
-
-        val manga = SManga.create().apply {
-            val profileManga = document.selectFirst(mangaDetailsSelector)!!
-            thumbnail_url = profileManga.selectFirst("img")!!.attr("abs:src")
-            genre = profileManga.select(mangaDetailsSelectorGenres)
-                .joinToString { it.text() }
-
-            val infoElement = profileManga.select(mangaDetailsSelectorInfo)
-            infoElement.forEach { element ->
-                val infoText = element.ownText().trim().ifEmpty { element.selectFirst(mangaDetailsSelectorInfoTitle)?.text()?.trim() ?: "" }
-                val descText = element.select(mangaDetailsSelectorInfoDescription).text().trim()
-                when {
-                    statusSelectorList.any { infoText.contains(it, ignoreCase = true) } -> {
-                        status = parseStatus(descText)
-                    }
-
-                    authorSelectorList.any { infoText.contains(it, ignoreCase = true) } -> {
-                        author = descText
-                    }
-
-                    artisSelectorList.any { infoText.contains(it, ignoreCase = true) } -> {
-                        artist = descText
-                    }
-                }
-            }
-        }
-
-        // PERBAIKAN: Gunakan selector yang benar untuk menargetkan paragraf deskripsi.
-        manga.description = document.select("div.p-13 > p")
-            .joinToString("\n\n") { it.text() }
-            .trim()
-
-        return manga
-    }
+    override val mangaDetailsSelectorDescription = "#synopsis ~ p"
 }
