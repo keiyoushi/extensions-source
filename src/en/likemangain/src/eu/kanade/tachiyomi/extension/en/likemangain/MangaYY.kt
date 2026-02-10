d9a6502a5f9df067b518cfda7ac8a3d66a1aaed8
Aaron <aaronndeimos@gmail.com>
2026-02-11 00:16:52 +0530

    LikeMangaIn: rebrand to mangayy

--- src/en/likemangain/src/eu/kanade/tachiyomi/extension/en/likemangain/MangaYY.kt
+++ src/en/likemangain/src/eu/kanade/tachiyomi/extension/en/likemangain/MangaYY.kt
@@ -1,8 +1,9 @@
-package eu.kanade.tachiyomi.extension.en.mangayy
+package eu.kanade.tachiyomi.extension.en.likemangain
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
 class MangaYY : Madara("MangaYY", "https://mangayy.org", "en") {
+    override val id = 828698548689586603
     override fun searchMangaSelector() = popularMangaSelector()
     override val useNewChapterEndpoint = true
 }
