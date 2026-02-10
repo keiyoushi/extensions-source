4cb46a6d7b6e23dc5b87fd243cb3304a72befeaf
Aaron <aaronndeimos@gmail.com>
2026-02-11 05:04:07 +0530

    fix date

--- src/en/likemangain/src/eu/kanade/tachiyomi/extension/en/likemangain/MangaYY.kt
+++ src/en/likemangain/src/eu/kanade/tachiyomi/extension/en/likemangain/MangaYY.kt
@@ -1,8 +1,16 @@
 package eu.kanade.tachiyomi.extension.en.likemangain
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class MangaYY : Madara("MangaYY", "https://mangayy.org", "en") {
+class MangaYY :
+    Madara(
+        "MangaYY",
+        "https://mangayy.org",
+        "en",
+        dateFormat = SimpleDateFormat("dd MMMM, yyyy", Locale.US),
+    ) {
     override val id = 828698548689586603
     override fun searchMangaSelector() = popularMangaSelector()
     override val useNewChapterEndpoint = true
