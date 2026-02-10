b957398fccf0270900d5fc2646cb4b74e93e9fdd
MuhamadSyabitHidayattulloh <tebepc@gmail.com>
2026-02-10 12:00:13 +0700

    ShiyuraSub (ID): fix NullPointerException : Null

--- src/id/shiyurasub/src/eu/kanade/tachiyomi/extension/id/shiyurasub/ShiyuraSub.kt
+++ src/id/shiyurasub/src/eu/kanade/tachiyomi/extension/id/shiyurasub/ShiyuraSub.kt
@@ -17,16 +17,40 @@ class ShiyuraSub : ZeistManga("ShiyuraSub", "https://shiyurasub.blogspot.com", "
     override fun popularMangaRequest(page: Int): Request = latestUpdatesRequest(page)
     override fun popularMangaParse(response: Response): MangasPage = latestUpdatesParse(response)
 
-    override val pageListSelector = "main.content article.container"
-
     override fun mangaDetailsParse(response: Response): SManga {
         val document = response.asJsoup()
-        val profileManga = document.selectFirst("main.content.post")!!
-        return SManga.create().apply {
-            thumbnail_url = profileManga.selectFirst("div.grid img")!!.attr("abs:src")
-            description = profileManga.select("#synopsis").text()
-            genre = profileManga.select("div.my-5 > a[rel=tag]")
+
+        val manga = SManga.create().apply {
+            val profileManga = document.selectFirst(mangaDetailsSelector)!!
+            thumbnail_url = profileManga.selectFirst("img")!!.attr("abs:src")
+            genre = profileManga.select(mangaDetailsSelectorGenres)
                 .joinToString { it.text() }
+
+            val infoElement = profileManga.select(mangaDetailsSelectorInfo)
+            infoElement.forEach { element ->
+                val infoText = element.ownText().trim().ifEmpty { element.selectFirst(mangaDetailsSelectorInfoTitle)?.text()?.trim() ?: "" }
+                val descText = element.select(mangaDetailsSelectorInfoDescription).text().trim()
+                when {
+                    statusSelectorList.any { infoText.contains(it, ignoreCase = true) } -> {
+                        status = parseStatus(descText)
+                    }
+
+                    authorSelectorList.any { infoText.contains(it, ignoreCase = true) } -> {
+                        author = descText
+                    }
+
+                    artisSelectorList.any { infoText.contains(it, ignoreCase = true) } -> {
+                        artist = descText
+                    }
+                }
+            }
         }
+
+        // PERBAIKAN: Gunakan selector yang benar untuk menargetkan paragraf deskripsi.
+        manga.description = document.select("div.p-13 > p")
+            .joinToString("\n\n") { it.text() }
+            .trim()
+
+        return manga
     }
 }
