0d51de9f175b31600a632fed9729ca77ef657a5b
Syabit <tebepc@gmail.com>
2026-02-11 11:30:23 +0700

    LepoyTL (ID): fix "NullPointerException: Null" & "No Pages Found" (#13294)

--- src/id/lepoytl/src/eu/kanade/tachiyomi/extension/id/lepoytl/LepoyTL.kt
+++ src/id/lepoytl/src/eu/kanade/tachiyomi/extension/id/lepoytl/LepoyTL.kt
@@ -2,4 +2,15 @@ package eu.kanade.tachiyomi.extension.id.lepoytl
 
 import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
 
-class LepoyTL : ZeistManga("LepoyTL", "https://www.lepoytl.my.id", "id")
+class LepoyTL : ZeistManga("LepoyTL", "https://www.lepoytl.my.id", "id") {
+
+    // =========================== Manga Details ============================
+    override val mangaDetailsSelector = "main"
+    override val mangaDetailsSelectorGenres = "aside dl a[rel=tag]"
+    override val mangaDetailsSelectorInfo = "#extra-info dl"
+    override val mangaDetailsSelectorInfoTitle = "dt"
+    override val mangaDetailsSelectorInfoDescription = "dd"
+
+    // =============================== Pages ================================
+    override val pageListSelector = "#reader"
+}
