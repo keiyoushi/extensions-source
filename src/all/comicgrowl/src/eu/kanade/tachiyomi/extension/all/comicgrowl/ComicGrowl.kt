8798a911fa401723669164236f8d89460be3f3f5
manti <133025162+manti-X@users.noreply.github.com>
2026-02-11 05:31:37 +0100

    Comic Growl/MagKan/KimiComi/Comic Pash: migrate to alt version (#13304)
    * comic growl
    
    * magkan
    
    * kimicomi
    
    * comic pash

--- src/all/comicgrowl/src/eu/kanade/tachiyomi/extension/all/comicgrowl/ComicGrowl.kt
+++ src/all/comicgrowl/src/eu/kanade/tachiyomi/extension/all/comicgrowl/ComicGrowl.kt
@@ -1,9 +1,11 @@
 package eu.kanade.tachiyomi.extension.all.comicgrowl
 
-import eu.kanade.tachiyomi.multisrc.comiciviewer.ComiciViewer
+import eu.kanade.tachiyomi.multisrc.comiciviewer.ComiciViewerAlt
 
-class ComicGrowl : ComiciViewer(
-    "Comic Growl",
-    "https://comic-growl.com",
-    "all",
-)
+class ComicGrowl :
+    ComiciViewerAlt(
+        "Comic Growl",
+        "https://comic-growl.com",
+        "all",
+        "https://comic-growl.com/api",
+    )
