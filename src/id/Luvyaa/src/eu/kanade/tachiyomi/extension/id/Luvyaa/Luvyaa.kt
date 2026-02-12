2e5e097514eda3b386f1f29b8857c6368333b984
NepThunder <tekchau21@gmail.com>
2026-02-12 14:03:50 +0545

    Ran spotlessApply

--- src/id/Luvyaa/src/eu/kanade/tachiyomi/extension/id/Luvyaa/Luvyaa.kt
+++ src/id/Luvyaa/src/eu/kanade/tachiyomi/extension/id/Luvyaa/Luvyaa.kt
@@ -4,9 +4,10 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class Luvyaa : MangaThemesia(
-    "Luvyaa",
-    "https://luvyaa.id",
-    "id",
-    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.US),
-)
+class Luvyaa :
+    MangaThemesia(
+        "Luvyaa",
+        "https://luvyaa.id",
+        "id",
+        dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.US),
+    )
