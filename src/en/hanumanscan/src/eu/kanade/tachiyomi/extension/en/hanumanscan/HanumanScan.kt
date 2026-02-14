e67233f1a03a374be39e5adc320c53f89fd900c4
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-02-14 12:09:46 +0100

    Remove parked domains (#13354)
    * Remove Adu Manga
    
    * Remove Dxd Scans
    
    * Remove Free Manga
    
    * Remove Fênix Manhwas
    
    * Remove Hanuman Scan
    
    * Remove Kai Scans
    
    * Remove La Zona del Lirio
    
    * Remove Manga BR
    
    * Remove Manga Hosted
    
    * Remove Manhwax
    
    * Remove xCaliBR Scans

--- src/en/hanumanscan/src/eu/kanade/tachiyomi/extension/en/hanumanscan/HanumanScan.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.hanumanscan
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class HanumanScan : MangaThemesia("Hanuman Scan", "https://hanumanscan.com", "en")
