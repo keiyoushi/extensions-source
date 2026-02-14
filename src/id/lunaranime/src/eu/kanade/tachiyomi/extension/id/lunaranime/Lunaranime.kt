1713624a2986ec0370dd6d2b911e1faad4ddeed2
GH-Capital <syawalsawal099@gmail.com>
2026-02-14 08:22:44 +0000

    feat(ext): add Lunaranime scaffold (id)

---
+++ src/id/lunaranime/src/eu/kanade/tachiyomi/extension/id/lunaranime/Lunaranime.kt
@@ -0,0 +1,11 @@
+package eu.kanade.tachiyomi.extension.id.lunaranime
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class Lunaranime : MangaThemesia(
+    "Lunaranime",
+    "https://lunaranime.example",
+    "id",
+) {
+    // TODO: implement site-specific parsing or overrides as needed
+}
