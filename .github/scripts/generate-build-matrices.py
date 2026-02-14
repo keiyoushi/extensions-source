760f212312c97037e0e3649545e72535b3d8068a
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-02-14 15:23:56 +0500

    resolve lib and multisrc dependencies directly

--- .github/scripts/generate-build-matrices.py
+++ .github/scripts/generate-build-matrices.py
@@ -12,7 +12,7 @@ MULTISRC_LIB_REGEX = re.compile(r"^lib-multisrc/(?P<multisrc>\w+)")
 LIB_REGEX = re.compile(r"^lib/(?P<lib>\w+)")
 MODULE_REGEX = re.compile(r"^:src:(?P<lang>\w+):(?P<extension>\w+)$")
 CORE_FILES_REGEX = re.compile(
-    r"^(buildSrc/|core/|gradle/|build\.gradle\.kts|common\.gradle|gradle\.properties|settings\.gradle\.kts|.github/scripts)"
+    r"^(buildSrc/|core/|gradle/|build\.gradle\.kts|common\.gradle|gradle\.properties|settings\.gradle\.kts)"
 )
 
 def run_command(command: str) -> str:
@@ -21,6 +21,62 @@ def run_command(command: str) -> str:
         print(result.stderr.strip())
         sys.exit(result.returncode)
     return result.stdout.strip()
+
+
+def resolve_multisrc_lib(libs: set[str]) -> set[str]:
+    """
+    returns all multisrc which depend on any of the
+    passed libs (/lib)
+    """
+    
+    lib_dependency = re.compile(
+        rf"project\([\"']:(?:lib):({'|'.join(map(re.escape, libs))})[\"']\)"
+    )
+    
+    multisrcs = set()
+    
+    for multisrc in Path("lib-multisrc").iterdir():
+        build_file = multisrc / "build.gradle.kts"
+        if not build_file.is_file():
+            continue
+        
+        with open(build_file) as f:
+            content = f.read()
+            
+        if (lib_dependency.search(content)):
+            multisrcs.add(multisrc.name)
+                
+    return multisrcs
+            
+def resolve_ext(multisrcs: set[str], libs: set[str]) -> set[tuple[str, str]]:
+    """
+    returns all extensions which depend on any of the
+    passed multisrcs or libs
+    """
+    
+    multisrc_dependency = re.compile(
+        rf"themePkg\s*=\s*['\"]({'|'.join(map(re.escape, multisrcs))})['\"]"
+    )
+    
+    lib_dependency = re.compile(
+        rf"project\([\"']:(?:lib):({'|'.join(map(re.escape, libs))})[\"']\)"
+    )
+    
+    extensions = set()
+    
+    for lang in Path("src").iterdir():
+        for extension in lang.iterdir():
+            build_file = extension / "build.gradle"
+            if not build_file.is_file():
+                continue
+            
+            with open(build_file) as f:
+                content = f.read()
+                
+            if (multisrc_dependency.search(content) or lib_dependency.search(content)):
+                extensions.add((lang.name, extension.name))
+    
+    return extensions
 
 def get_module_list(ref: str) -> tuple[list[str], list[str]]:
     diff_output = run_command(f"git diff --name-status {ref}").splitlines()
@@ -32,6 +88,7 @@ def get_module_list(ref: str) -> tuple[list[str], list[str]]:
     ]
         
     modules = set()
+    multisrcs = set()
     libs = set()
     deleted = set()
     core_files_changed = False
@@ -49,39 +106,27 @@ def get_module_list(ref: str) -> tuple[list[str], list[str]]:
         elif match := MULTISRC_LIB_REGEX.search(file):
             multisrc = match.group("multisrc")
             if Path("lib-multisrc", multisrc).is_dir():
-                libs.add(f":lib-multisrc:{multisrc}:printDependentExtensions")
+                multisrcs.add(multisrc)
         elif match := LIB_REGEX.search(file):
             lib = match.group("lib")
             if Path("lib", lib).is_dir():
-                libs.add(f":lib:{lib}:printDependentExtensions")
+                libs.add(lib)
+                
+    if core_files_changed:
+        return get_all_modules()
+                
+    multisrcs.update(resolve_multisrc_lib(libs))
+    
+    extensions = resolve_ext(multisrcs, libs)
+    modules.update([f":src:{lang}:{extension}" for lang, extension in extensions])
+    deleted.update([f"{lang}.{extension}" for lang, extension in extensions])
 
-    def is_extension_module(module: str) -> bool:
-        if not (match := MODULE_REGEX.search(module)):
-            return False
-        lang = match.group("lang")
-        extension = match.group("extension")
-        deleted.add(f"{lang}.{extension}")
-        return True
-
-    if len(libs) != 0 and not core_files_changed:
-        modules.update([
-            module for module in
-            run_command("./gradlew -q " + " ".join(libs)).splitlines()
-            if is_extension_module(module)
-        ])
-
-    if os.getenv("IS_PR_CHECK") != "true" and not core_files_changed:
+    if os.getenv("IS_PR_CHECK") != "true":
         with Path(".github/always_build.json").open() as always_build_file:
             always_build = json.load(always_build_file)
         for extension in always_build:
             modules.add(":src:" + extension.replace(".", ":"))
             deleted.add(extension)
-
-    if core_files_changed:
-        (all_modules, all_deleted) = get_all_modules()
-
-        modules.update(all_modules)
-        deleted.update(all_deleted)
 
     return list(modules), list(deleted)
 
