dbc43bae69775d5b8636c1a8577af873dc3a4fc6
Dimas Faiz <dimasfaizsatria@outlook.com>
2026-02-15 02:29:11 +0700

    chore forgot to delete ImageInterceptor

--- src/en/kagane/src/eu/kanade/tachiyomi/extension/en/kagane/ImageInterceptor.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.kagane
-
-import okhttp3.Interceptor
-import okhttp3.Response
-
-open class ImageInterceptor : Interceptor {
-
-    override fun intercept(chain: Interceptor.Chain): Response = chain.proceed(chain.request())
-}
