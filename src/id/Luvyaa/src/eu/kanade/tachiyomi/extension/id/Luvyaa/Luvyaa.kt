800eccde7d3201d22a9c519e1008b03aed37f5ab
Tek Narayan Chaudhary <84651805+NepThunder@users.noreply.github.com>
2026-02-14 06:09:16 -0500

    Luvyaa: Update URL (`luvyaa.id`) (#13333)
    * Source URL change
    
    * Ran spotlessApply

--- src/id/Luvyaa/src/eu/kanade/tachiyomi/extension/id/Luvyaa/Luvyaa.kt
+++ src/id/Luvyaa/src/eu/kanade/tachiyomi/extension/id/Luvyaa/Luvyaa.kt
@@ -1,12 +1,13 @@
-package eu.kanade.tachiyomi.extension.id.Luvyaa
+package eu.kanade.tachiyomi.extension.id.luvyaa
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class Luvyaa : MangaThemesia(
-    "Luvyaa",
-    "https://luvyaa.my.id",
-    "id",
-    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.US),
-)
+class Luvyaa :
+    MangaThemesia(
+        "Luvyaa",
+        "https://luvyaa.id",
+        "id",
+        dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.US),
+    )
