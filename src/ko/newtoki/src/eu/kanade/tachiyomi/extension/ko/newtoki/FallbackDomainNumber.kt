169e1bdccf0d6f39cffca6f3ec0c2cb8a520fd52
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-02-15 13:47:09 +0500

    NewToki: fix spotless error
    why did it suddenly decide to not work???

--- src/ko/newtoki/src/eu/kanade/tachiyomi/extension/ko/newtoki/FallbackDomainNumber.kt
+++ src/ko/newtoki/src/eu/kanade/tachiyomi/extension/ko/newtoki/FallbackDomainNumber.kt
@@ -7,4 +7,4 @@ package eu.kanade.tachiyomi.extension.ko.newtoki
  * Even if this value is built into the extension, the network call will fail
  * because of underscore character and the extension will update it on its own.
  */
-const val fallbackDomainNumber = "_failed_to_fetch_domain_number"
+const val FALLBACK_DOMAIN_NUMBER = "_failed_to_fetch_domain_number"
