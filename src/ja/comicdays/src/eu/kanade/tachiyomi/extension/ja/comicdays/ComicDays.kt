db376bd4fd8e73a9132d7ca7160f9352abc5ce95
manti <133025162+manti-X@users.noreply.github.com>
2026-02-19 16:22:51 +0100

    ?

--- src/ja/comicdays/src/eu/kanade/tachiyomi/extension/ja/comicdays/ComicDays.kt
+++ src/ja/comicdays/src/eu/kanade/tachiyomi/extension/ja/comicdays/ComicDays.kt
@@ -14,7 +14,7 @@ class ComicDays :
 
     override fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
         title = element.selectFirst("h4.daily-series-title")!!.text()
-        thumbnail_url = element.selectFirst("div.daily-series-thumb img")!!.absUrl("data-src")
+        thumbnail_url = element.selectFirst("div.daily-series-thumb img")?.absUrl("data-src")
         setUrlWithoutDomain(element.selectFirst("a.link")!!.absUrl("href"))
     }
 
