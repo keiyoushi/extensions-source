689c078ba2268e98356978b1cfab05e95993ab58
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-02-19 07:19:41 +0100

    Remove parked domains (#13462)
    * Remove Athena Manga
    
    * Remove First Kiss Manhua
    
    * Remove Foy Scan
    
    * Remove Futari
    
    * Remove MangaPeak
    
    * Remove Manhua AZ
    
    * Remove Manhwa Freake
    
    * Remove ReadMangaFree
    
    * Remove Seraph Manga
    
    * Remove Manga Leveling
    
    * Remove KomikGO

--- src/en/readmangafree/src/eu/kanade/tachiyomi/extension/en/readmangafree/ReadMangaFree.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.readmangafree
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class ReadMangaFree : Madara("ReadMangaFree", "https://readmangafree.net", "en") {
-    override val useNewChapterEndpoint = false
-}
