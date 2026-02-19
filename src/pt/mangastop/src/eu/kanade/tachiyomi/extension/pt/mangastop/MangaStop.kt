67991c958dc2a3fff5eb97609a09d7c277925d88
WebDitto <webditto@proton.me>
2026-02-19 15:37:07 -0300

    Manga Stop: Fixed images and search (Closes #13248)

--- src/pt/mangastop/src/eu/kanade/tachiyomi/extension/pt/mangastop/MangaStop.kt
+++ src/pt/mangastop/src/eu/kanade/tachiyomi/extension/pt/mangastop/MangaStop.kt
@@ -10,13 +10,16 @@ import eu.kanade.tachiyomi.lib.randomua.getPrefCustomUA
 import eu.kanade.tachiyomi.lib.randomua.getPrefUAType
 import eu.kanade.tachiyomi.lib.randomua.setRandomUserAgent
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.ConfigurableSource
+import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
 import keiyoushi.utils.getPreferencesLazy
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.Headers
+import okhttp3.Request
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -38,6 +41,20 @@ class MangaStop :
     }
 
     override val client = network.cloudflareClient.newBuilder()
+        .addInterceptor { chain ->
+            val request = chain.request()
+            // For covers
+            if (chain.request().url.host.contains("images")) {
+                val newRequest = request.newBuilder().apply {
+                    header("Sec-Fetch-Dest", "image")
+                    header("Sec-Fetch-Mode", "no-cors")
+                    header("Sec-Fetch-Site", "same-site")
+                }.build()
+                chain.proceed(newRequest)
+            } else {
+                chain.proceed(request)
+            }
+        }
         .addNetworkInterceptor(
             CookieInterceptor(baseUrl.substringAfter("//"), "wpmanga-ada" to "1"),
         )
@@ -71,6 +88,23 @@ class MangaStop :
                 Page(i, document.location(), el.jsonPrimitive.content)
             }
             .orEmpty()
+    }
+
+    override fun imageRequest(page: Page): Request {
+        val newHeaders = headersBuilder()
+            .set("Accept", "image/avif,image/webp,image/png,image/jpeg,*/*")
+            .set("Sec-Fetch-Dest", "image")
+            .set("Sec-Fetch-Mode", "no-cors")
+            .set("Sec-Fetch-Site", "same-site")
+            .set("Referer", page.url)
+            .build()
+
+        return GET(page.imageUrl!!, newHeaders)
+    }
+
+    override fun getFilterList(): FilterList {
+        val filters = super.getFilterList().filterNot { it is AuthorFilter || it is YearFilter }
+        return FilterList(filters)
     }
 
     override fun setupPreferenceScreen(screen: PreferenceScreen) {
