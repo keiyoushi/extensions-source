6c7d3c58104cea5df4fbece1970ad76e90579511
manti <133025162+manti-X@users.noreply.github.com>
2026-02-21 06:33:36 +0100

    MangaBang: alt comici (#13485)

--- src/ja/mangabang/src/eu/kanade/tachiyomi/extension/ja/mangabang/MangaBang.kt
+++ src/ja/mangabang/src/eu/kanade/tachiyomi/extension/ja/mangabang/MangaBang.kt
@@ -1,16 +1,18 @@
 package eu.kanade.tachiyomi.extension.ja.mangabang
 
-import eu.kanade.tachiyomi.multisrc.comiciviewer.ComiciViewer
+import eu.kanade.tachiyomi.multisrc.comiciviewer.ComiciViewerAlt
 
 class MangaBang :
-    ComiciViewer(
+    ComiciViewerAlt(
         "MangaBang Comics",
         "https://comics.manga-bang.com",
         "ja",
+        "https://comics.manga-bang.com/api",
     ) {
     override fun getFilterOptions(): List<Pair<String, String>> = listOf(
         Pair("ランキング", "/ranking/manga"),
-        Pair("完結", "/category/manga?type=完結"),
-        Pair("連載", "/category/manga?type=連載中"),
+        Pair("更新順", "/series/list/up"),
+        Pair("新作順", "/series/list/new"),
+        Pair("完結", "/category/manga/complete"),
     )
 }
