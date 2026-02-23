999a3b095a4c6d1541d1dc2b4edf91d33b740bb7
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-02-23 17:57:41 +0100

    Force pagination for Manhwa-raw (#13534)

--- src/all/manhwadashraw/src/eu/kanade/tachiyomi/extension/all/manhwadashraw/ManhwaDashRaw.kt
+++ src/all/manhwadashraw/src/eu/kanade/tachiyomi/extension/all/manhwadashraw/ManhwaDashRaw.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.extension.all.manhwadashraw
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.model.MangasPage
+import okhttp3.Response
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -16,4 +18,8 @@ class ManhwaDashRaw :
     override val mangaDetailsSelectorStatus = "div.post-content_item:contains(Status) > div.summary-content"
     override val mangaDetailsSelectorDescription = "div.post-content_item:contains(Summary) div.summary-container"
     override val pageListParseSelector = "div.page-break img.wp-manga-chapter-img"
+
+    override fun latestUpdatesParse(response: Response): MangasPage = MangasPage(super.latestUpdatesParse(response).mangas, true)
+    override fun popularMangaParse(response: Response): MangasPage = MangasPage(super.popularMangaParse(response).mangas, true)
+    override fun searchMangaParse(response: Response): MangasPage = MangasPage(super.searchMangaParse(response).mangas, true)
 }
