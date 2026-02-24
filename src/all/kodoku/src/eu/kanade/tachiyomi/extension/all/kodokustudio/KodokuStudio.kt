352f5a43d555dedba775bae0da76a3b249f935e6
Jochem Kuipers <jochem@kuipers.cc>
2026-02-24 13:00:52 +0100

    move source file to correct directory

--- src/all/kodoku/src/eu/kanade/tachiyomi/extension/all/kodokustudio/KodokuStudio.kt
+++ src/all/kodoku/src/eu/kanade/tachiyomi/extension/all/kodokustudio/KodokuStudio.kt
@@ -1,8 +1,8 @@
-package eu.kanade.tachiyomi.extension.all.kodoku
+package eu.kanade.tachiyomi.extension.all.kodoku.eu.kanade.tachiyomi.extension.all.kodokustudio
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class KodokuStudio :
+class `KodokuStudio.kt` :
     Madara(
         "Kodoku Studio",
         "https://kodokustudio.com",
