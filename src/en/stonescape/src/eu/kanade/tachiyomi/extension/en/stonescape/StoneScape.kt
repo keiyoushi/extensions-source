7114d88223de259115ef5cff3df5c53148503542
darwin-256 <viktorasordinass@gmail.com>
2026-02-25 04:44:11 +0200

    Refactor StoneScape class for error handling and requests
    Added interceptors to handle 500 errors and updated manga request URLs.

--- src/en/stonescape/src/eu/kanade/tachiyomi/extension/en/stonescape/StoneScape.kt
+++ src/en/stonescape/src/eu/kanade/tachiyomi/extension/en/stonescape/StoneScape.kt
@@ -1,22 +1,26 @@
 package eu.kanade.tachiyomi.extension.en.stonescape
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class StoneScape :
-    Madara(
-        "StoneScape",
-        "https://stonescape.xyz",
-        "en",
-        SimpleDateFormat("MMMM dd, yyyy", Locale.ENGLISH),
-    ) {
+class StoneScape : Madara("StoneScape", "https://stonescape.xyz", "en", SimpleDateFormat("MMMM dd, yyyy", Locale.ENGLISH)) {
     override val mangaSubString = "series"
 
-    override val chapterUrlSelector = "li > a"
+    // Fix for site returning 500 error while still providing valid HTML
+    override val client = super.client.newBuilder().addInterceptor { chain ->
+        val res = chain.proceed(chain.request())
+        if (res.code == 500) res.newBuilder().code(200).build() else res
+    }.build()
 
-    override val mangaDetailsSelectorAuthor = ".post-content .manga-authors a"
-    override val mangaDetailsSelectorDescription = ".manga-about, ${super.mangaDetailsSelectorDescription}"
+    // Redirecting Browse/Latest to the correct URL
+    override fun popularMangaRequest(page: Int) = GET("$baseUrl/manhwaseries/page/$page/", headers)
+    override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/manhwaseries/page/$page/?m_orderby=latest", headers)
 
+    // Selectors for listing and details
+    override fun popularMangaSelector() = "div.page-item-detail.manga"
+    override val mangaDetailsSelectorAuthor = ".manga-authors a"
+    override val mangaDetailsSelectorDescription = "div.manga-summary"
     override fun chapterListSelector() = "li.wp-manga-chapter:not(.premium-block)"
 }
