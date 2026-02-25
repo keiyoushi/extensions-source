dcd6b125a9fe2fb314624806ca0015a7ea2a9aef
darwin-256 <viktorasordinass@gmail.com>
2026-02-25 06:59:49 +0200

    Update StoneScape.kt
    Fixed it so github checker accepts the file

--- src/en/stonescape/src/eu/kanade/tachiyomi/extension/en/stonescape/StoneScape.kt
+++ src/en/stonescape/src/eu/kanade/tachiyomi/extension/en/stonescape/StoneScape.kt
@@ -6,7 +6,13 @@ import okhttp3.OkHttpClient
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class StoneScape : Madara("StoneScape", "https://stonescape.xyz", "en", SimpleDateFormat("MMMM dd, yyyy", Locale.ENGLISH)) {
+class StoneScape :
+    Madara(
+        "StoneScape",
+        "https://stonescape.xyz",
+        "en",
+        SimpleDateFormat("MMMM dd, yyyy", Locale.ENGLISH),
+    ) {
     override val mangaSubString = "series"
 
     override val client: OkHttpClient = super.client.newBuilder().addInterceptor { chain ->
@@ -14,18 +20,24 @@ class StoneScape : Madara("StoneScape", "https://stonescape.xyz", "en", SimpleDa
         val url = res.request.url.toString()
         if (res.code == 500 && (url.contains("/$mangaSubString/") || url.contains("/manhwaseries/"))) {
             res.newBuilder().code(200).build()
-        } else res
+        } else {
+            res
+        }
     }.build()
 
     override val useLoadMoreRequest = LoadMoreStrategy.Always
 
     override fun popularMangaRequest(page: Int) = GET("$baseUrl/manhwaseries/page/$page/", headers)
+
     override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/manhwaseries/page/$page/?m_orderby=latest", headers)
 
     override fun popularMangaSelector() = "div.page-item-detail.manga"
+
     override val mangaDetailsSelectorAuthor = ".author.meta a"
+
     override val mangaDetailsSelectorDescription = ".manga-summary"
 
     override val chapterUrlSelector = "li > a"
+
     override fun chapterListSelector() = "li.wp-manga-chapter:not(.premium-block)"
 }
