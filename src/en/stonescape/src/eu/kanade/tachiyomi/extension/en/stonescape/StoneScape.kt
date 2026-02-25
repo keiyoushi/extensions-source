b2bb2af2fae5cea81b2bf574ee20d19668d79f89
darwin-256 <viktorasordinass@gmail.com>
2026-02-25 06:50:03 +0200

    Refactor StoneScape client and update selectors

--- src/en/stonescape/src/eu/kanade/tachiyomi/extension/en/stonescape/StoneScape.kt
+++ src/en/stonescape/src/eu/kanade/tachiyomi/extension/en/stonescape/StoneScape.kt
@@ -2,28 +2,30 @@ package eu.kanade.tachiyomi.extension.en.stonescape
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.GET
+import okhttp3.OkHttpClient
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class StoneScape : Madara("StoneScape", "https://stonescape.xyz", "en", SimpleDateFormat("MMMM dd, yyyy", Locale.ENGLISH)) {
     override val mangaSubString = "series"
 
-    // Fix for the HTTP 500 error mentioned in issue #13343
-    override val client = super.client.newBuilder().addInterceptor { chain ->
+    override val client: OkHttpClient = super.client.newBuilder().addInterceptor { chain ->
         val res = chain.proceed(chain.request())
-        if (res.code == 500) res.newBuilder().code(200).build() else res
+        val url = res.request.url.toString()
+        if (res.code == 500 && (url.contains("/$mangaSubString/") || url.contains("/manhwaseries/"))) {
+            res.newBuilder().code(200).build()
+        } else res
     }.build()
 
-    // Redirect to the correct Comics listing page
+    override val useLoadMoreRequest = LoadMoreStrategy.Always
+
     override fun popularMangaRequest(page: Int) = GET("$baseUrl/manhwaseries/page/$page/", headers)
     override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/manhwaseries/page/$page/?m_orderby=latest", headers)
 
-    // Selectors matched to your provided HTML
     override fun popularMangaSelector() = "div.page-item-detail.manga"
     override val mangaDetailsSelectorAuthor = ".author.meta a"
     override val mangaDetailsSelectorDescription = ".manga-summary"
-    
-    // Specifically target the link with the text to avoid empty thumbnail links
-    override val chapterUrlSelector = "li.wp-manga-chapter > a"
+
+    override val chapterUrlSelector = "li > a"
     override fun chapterListSelector() = "li.wp-manga-chapter:not(.premium-block)"
 }
