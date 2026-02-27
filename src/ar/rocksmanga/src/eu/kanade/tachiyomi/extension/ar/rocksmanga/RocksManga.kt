6d35dd5afdecde78faa0301abaf5a31bc1130e33
xMohnad <mohnad00967@gmail.com>
2026-02-27 00:02:58 +0000

    RocksManga: fix search and filters implementation

--- src/ar/rocksmanga/src/eu/kanade/tachiyomi/extension/ar/rocksmanga/RocksManga.kt
+++ src/ar/rocksmanga/src/eu/kanade/tachiyomi/extension/ar/rocksmanga/RocksManga.kt
@@ -1,8 +1,14 @@
 package eu.kanade.tachiyomi.extension.ar.rocksmanga
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
+import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -37,8 +43,82 @@ class RocksManga :
 
     override val useLoadMoreRequest = LoadMoreStrategy.Never
     override val useNewChapterEndpoint = false
-    override val fetchGenres = false
     override val filterNonMangaItems = false
+
+    override fun searchRequest(page: Int, query: String, filters: FilterList): Request {
+        val urlBuilder = "$baseUrl/".toHttpUrl().newBuilder()
+        return if (query.isNotBlank()) {
+            urlBuilder.addPathSegment(searchPage(page))
+            urlBuilder.addQueryParameter("s", query)
+            GET(urlBuilder.build(), headers)
+        } else {
+            var pathSegments: String? = null
+
+            filters.forEach { filter ->
+                val segment = when (filter) {
+                    is TypeFilter -> filter.toUriPart()
+                    is GenreFilter -> filter.toUriPart()
+                    else -> ""
+                }
+
+                if (segment.isNotBlank()) {
+                    pathSegments = when (filter) {
+                        is TypeFilter -> "manga-type/$segment/${searchPage(page)}"
+                        is GenreFilter -> "manga-genre/$segment/${searchPage(page)}"
+                        else -> pathSegments
+                    }
+                }
+            }
+
+            pathSegments?.let { urlBuilder.addPathSegments(it) }
+            GET(urlBuilder.build(), headers)
+        }
+    }
+    private val typeFilters: Array<Pair<String, String>> = arrayOf(
+        "ALL" to "",
+        "كوميك" to "comic",
+        "مانجا" to "manga",
+        "مانها" to "manhua",
+        "مانهوا" to "manhwa",
+        "ون شوت" to "one-shot",
+    )
+
+    private class GenreFilter(vals: Array<Pair<String, String>>) : UriPartFilter("Category", vals)
+    private class TypeFilter(vals: Array<Pair<String, String>>) : UriPartFilter("Type", vals)
+
+    override fun parseGenres(document: Document): List<Genre> = document
+        .selectFirst("#nav-menu li:contains(التصنيفات) ul")
+        ?.select("li a")
+        .orEmpty()
+        .map { a ->
+            Genre(
+                a!!.text(),
+                a.attr("href")
+                    .substringAfter("/manga-genre/")
+                    .substringBefore("/"),
+            )
+        }
+
+    override fun getFilterList(): FilterList {
+        launchIO { fetchGenres() }
+
+        return FilterList(
+            if (genresList.isNotEmpty()) {
+                listOf(
+                    Filter.Header("NOTE: Filters are ignored when using text search."),
+                    Filter.Header("NOTE: Type and Genre cannot be used together. Genre will take priority."),
+                    Filter.Separator(),
+                    TypeFilter(typeFilters),
+                    GenreFilter(arrayOf(Pair("ALL", "")) + genresList.map { Pair(it.name, it.id) }.toTypedArray()),
+                )
+            } else {
+                listOf(
+                    TypeFilter(typeFilters),
+                    Filter.Header(intl["genre_missing_warning"]),
+                )
+            },
+        )
+    }
 
     override fun chapterFromElement(element: Element): SChapter {
         val chapter = super.chapterFromElement(element)
