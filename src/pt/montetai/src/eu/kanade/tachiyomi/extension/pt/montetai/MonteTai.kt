b59cb695444b845e76c1e273e0894315c8229900
Chopper <156493704+choppeh@users.noreply.github.com>
2026-02-28 09:25:24 -0300

    MonteTai: Fix manga details and chapters (#13641)
    Fix manga details and chapters

--- src/pt/montetai/src/eu/kanade/tachiyomi/extension/pt/montetai/MonteTai.kt
+++ src/pt/montetai/src/eu/kanade/tachiyomi/extension/pt/montetai/MonteTai.kt
@@ -1,6 +1,10 @@
 package eu.kanade.tachiyomi.extension.pt.montetai
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.SChapter
+import keiyoushi.utils.tryParse
+import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -11,5 +15,22 @@ class MonteTai :
         "pt-BR",
         SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")),
     ) {
-    override val useNewChapterEndpoint = true
+    override val client = super.client.newBuilder()
+        .rateLimit(3, 1)
+        .build()
+
+    override val mangaDetailsSelectorThumbnail = ".mtx-cover img"
+    override val mangaDetailsSelectorAuthor = ".mtx-side-item:contains(Autor) .mtx-side-value"
+    override val mangaDetailsSelectorArtist = ".mtx-side-item:contains(Artista) .mtx-side-value"
+    override val mangaDetailsSelectorGenre = ".mtx-chip-list a"
+    override val mangaDetailsSelectorDescription = ".entry-content.entry-content-single"
+    override val mangaDetailsSelectorStatus = ".mtx-pill-status"
+
+    override fun chapterListSelector() = ".mtx-chapter-item"
+
+    override fun chapterFromElement(element: Element) = SChapter.create().apply {
+        name = element.selectFirst(".mtx-chapter-title")!!.text()
+        date_upload = dateFormat.tryParse(element.selectFirst(".mtx-chapter-meta")?.text())
+        setUrlWithoutDomain(element.absUrl("href"))
+    }
 }
