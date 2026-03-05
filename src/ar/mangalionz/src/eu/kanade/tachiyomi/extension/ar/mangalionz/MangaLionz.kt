48ef5f2c04746041340053a5b1b8c6f7717a1136
xMohnad <mohnad00967@gmail.com>
2026-03-05 03:09:04 +0000

    MangaLionz: remove popularMangaFromElement method

--- src/ar/mangalionz/src/eu/kanade/tachiyomi/extension/ar/mangalionz/MangaLionz.kt
+++ src/ar/mangalionz/src/eu/kanade/tachiyomi/extension/ar/mangalionz/MangaLionz.kt
@@ -1,8 +1,6 @@
 package eu.kanade.tachiyomi.extension.ar.mangalionz
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.source.model.SManga
-import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -15,23 +13,6 @@ class MangaLionz :
     ) {
 
     override val useLoadMoreRequest = LoadMoreStrategy.Always
-
-    override fun popularMangaFromElement(element: Element): SManga {
-        val manga = SManga.create()
-
-        with(element) {
-            selectFirst(popularMangaUrlSelector)!!.let {
-                manga.setUrlWithoutDomain(it.attr("abs:href"))
-                manga.title = it.ownText()
-            }
-
-            selectFirst("img")?.let {
-                manga.thumbnail_url = imageFromElement(it)?.replace("mangalionz", "mangalek")
-            }
-        }
-
-        return manga
-    }
 
     override val chapterUrlSuffix = ""
 }
