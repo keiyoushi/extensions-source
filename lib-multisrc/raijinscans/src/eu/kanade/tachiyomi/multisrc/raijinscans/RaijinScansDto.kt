775f15d01b2403483d848e766eaf799736c7e7d7
Luigi <19366824+luigidotmoe@users.noreply.github.com>
2026-03-05 12:12:12 +0100

    Refactor RaijinScans into multisrc + Add Pornhwa.io (#13760)
    Add Pornhwa.io + Redesign RaijinScans into multisrc

--- lib-multisrc/raijinscans/src/eu/kanade/tachiyomi/multisrc/raijinscans/RaijinScansDto.kt
+++ lib-multisrc/raijinscans/src/eu/kanade/tachiyomi/multisrc/raijinscans/RaijinScansDto.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.fr.raijinscans
+package eu.kanade.tachiyomi.multisrc.raijinscans
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -14,14 +14,4 @@ class LatestUpdatesDataDto(
     @SerialName("manga_html") val mangaHtml: String,
     @SerialName("current_page") val currentPage: Int,
     @SerialName("total_pages") val totalPages: Int,
-)
-
-@Serializable
-class ImageResponseDto(
-    val data: ImageResponseDataDto,
-)
-
-@Serializable
-class ImageResponseDataDto(
-    val u: List<String>,
 )
