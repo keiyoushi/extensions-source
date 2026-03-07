56654a7ffaae8ad4f88b071d0cf9ed4206bc921a
xMohnad <mohnad00967@gmail.com>
2026-03-07 23:44:05 +0000

    HijalaScans: enable usePopularMangaApi to fix popular manga

--- src/en/hijalascans/src/eu/kanade/tachiyomi/extension/en/hijalascans/HijalaScans.kt
+++ src/en/hijalascans/src/eu/kanade/tachiyomi/extension/en/hijalascans/HijalaScans.kt
@@ -1,10 +1,6 @@
 package eu.kanade.tachiyomi.extension.en.hijalascans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.MangasPage
-import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Response
 
 class HijalaScans :
@@ -14,12 +10,8 @@ class HijalaScans :
         "https://en-hijala.com",
         "https://api.en-hijala.com",
     ) {
-    override fun popularMangaRequest(page: Int) = GET(baseUrl, headers)
 
-    override fun popularMangaParse(response: Response): MangasPage {
-        val entries = response.asJsoup().select(".splide__track li > a").mapNotNull {
-            titleCache[it.absUrl("href").substringAfter("series/")]?.toSManga()
-        }.distinctBy(SManga::url)
-        return MangasPage(entries, false)
-    }
+    override val usePopularMangaApi = true
+
+    override fun popularMangaParse(response: Response) = searchMangaParse(response)
 }
