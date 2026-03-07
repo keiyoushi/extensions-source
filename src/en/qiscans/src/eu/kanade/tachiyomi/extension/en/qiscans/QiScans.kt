358f8407afc8b7692685fad66b8d28498d9123e2
xMohnad <mohnad00967@gmail.com>
2026-03-07 23:37:30 +0000

    QiScans: migrate source to new Iken changes

--- src/en/qiscans/src/eu/kanade/tachiyomi/extension/en/qiscans/QiScans.kt
+++ src/en/qiscans/src/eu/kanade/tachiyomi/extension/en/qiscans/QiScans.kt
@@ -1,12 +1,8 @@
 package eu.kanade.tachiyomi.extension.en.qiscans
 
-import eu.kanade.tachiyomi.multisrc.iken.GenreFilter
 import eu.kanade.tachiyomi.multisrc.iken.Iken
-import eu.kanade.tachiyomi.multisrc.iken.SelectFilter
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import eu.kanade.tachiyomi.source.model.Filter
-import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.util.asJsoup
@@ -15,8 +11,6 @@ import kotlinx.serialization.Serializable
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
-import rx.Observable
-import rx.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 class QiScans :
@@ -39,7 +33,7 @@ class QiScans :
         val url = "$apiUrl/api/query".toHttpUrl().newBuilder().apply {
             // 'query' instead of 'posts'
             addQueryParameter("page", page.toString())
-            addQueryParameter("perPage", "18")
+            addQueryParameter("perPage", Iken.PER_PAGE.toString())
             addQueryParameter("orderBy", "updatedAt")
         }.build()
         return GET(url, headers)
@@ -62,61 +56,4 @@ class QiScans :
             Page(idx, imageUrl = p.url)
         }
     }
-
-    private var genresList: List<Pair<String, String>> = emptyList()
-    private var fetchGenresAttempts = 0
-
-    private fun fetchGenres() {
-        try {
-            val response = client.newCall(GET("$apiUrl/api/genres", headers)).execute()
-            genresList = response.parseAs<List<GenreDto>>()
-                .map { Pair(it.name, it.id.toString()) }
-        } catch (e: Throwable) {
-        } finally {
-            fetchGenresAttempts++
-        }
-    }
-
-    override fun getFilterList(): FilterList {
-        if (genresList.isEmpty() && fetchGenresAttempts < 3) {
-            Observable.fromCallable { fetchGenres() }
-                .subscribeOn(Schedulers.io())
-                .subscribe()
-        }
-
-        val filters = mutableListOf<Filter<*>>(
-            SortFilter(),
-            StatusFilter(),
-        )
-
-        if (genresList.isNotEmpty()) {
-            filters.add(GenreFilter(genresList))
-        } else {
-            filters.add(Filter.Header("Press 'Reset' to attempt to load genres"))
-        }
-        return FilterList(filters)
-    }
-
-    private class SortFilter :
-        SelectFilter(
-            "Sort",
-            "orderBy",
-            listOf(
-                Pair("Popularity", "totalViews"),
-                Pair("Latest", "updatedAt"),
-            ),
-        )
-
-    private class StatusFilter :
-        SelectFilter(
-            "Status",
-            "seriesStatus",
-            listOf(
-                Pair("All", ""),
-                Pair("Ongoing", "ONGOING"),
-                Pair("Hiatus", "HIATUS"),
-                Pair("Completed", "COMPLETED"),
-                Pair("Dropped", "DROPPED"),
-            ),
-        )
 }
