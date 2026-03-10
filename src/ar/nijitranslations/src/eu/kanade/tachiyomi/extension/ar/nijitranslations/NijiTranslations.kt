fe24658d0b7fcdeaa6c88b019fe1494c136c2f3a
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-03-10 23:36:00 +0100

    Remove Niji Translations

--- src/ar/nijitranslations/src/eu/kanade/tachiyomi/extension/ar/nijitranslations/NijiTranslations.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.ar.nijitranslations
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class NijiTranslations : Madara("Niji Translations", "https://niji-translations.com", "ar", SimpleDateFormat("MMMM dd, yyyy", Locale("ar")))
