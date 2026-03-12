52ca7ffac6b14de8a93fe2ceb59806fec47ca46f
mr-glemsi <gorkemgedik053@gmail.com>
2026-03-12 15:58:22 +0300

    test ElderManga.kt

--- src/tr/eldermanga/src/eu/kanade/tachiyomi/extension/tr/eldermanga/ElderManga.kt
+++ src/tr/eldermanga/src/eu/kanade/tachiyomi/extension/tr/eldermanga/ElderManga.kt
@@ -3,15 +3,13 @@ package eu.kanade.tachiyomi.extension.tr.eldermanga
 import eu.kanade.tachiyomi.multisrc.uzaymanga.UzayManga
 import okhttp3.Headers
 
-class ElderManga :
-    UzayManga(
-        "Elder Manga",
-        "https://eldermanga.com",
-        lang = "tr",
-        versionId = 1,
-        cdnUrl = "https://eldermangacdn2.efsaneler.can.re",
-    ) {
-    
+class ElderManga : UzayManga(
+    "Elder Manga",
+    "https://eldermanga.com",
+    lang = "tr",
+    versionId = 1,
+    cdnUrl = "https://eldermangacdn2.efsaneler.can.re",
+) {
     override fun headersBuilder(): Headers.Builder = super.headersBuilder()
         .set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
         .set("Accept", "application/json, text/plain, */*")
