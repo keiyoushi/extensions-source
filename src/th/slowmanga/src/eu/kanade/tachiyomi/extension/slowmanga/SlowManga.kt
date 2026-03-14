f31c3ed828642720b6d24b78b388149a0950e8a8
NepThunder <tekchau21@gmail.com>
2026-03-14 22:39:00 +0545

    [Th] Slow Manga: Source Request

---
+++ src/th/slowmanga/src/eu/kanade/tachiyomi/extension/slowmanga/SlowManga.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.th.slowmanga
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class SlowManga : MangaThemesia("Slow Manga", "https://www.slow-manga.com", "th")
