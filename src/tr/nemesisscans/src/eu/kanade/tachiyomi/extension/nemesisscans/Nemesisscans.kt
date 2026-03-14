49b22bbbd6eb77302279a13f277c43cd29bdbd74
NepThunder <tekchau21@gmail.com>
2026-03-14 23:05:04 +0545

    [TR] Nemesis scans: Source Request

---
+++ src/tr/nemesisscans/src/eu/kanade/tachiyomi/extension/nemesisscans/Nemesisscans.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.tr.nemesisscans
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class Nemesisscans : MangaThemesia("Nemesisscans", "https://nemesisscans.com", "tr")
