a04a218b8015b7c97ee467eb19a75516fb4eb8ba
NepThunder <tekchau21@gmail.com>
2026-03-15 12:33:47 +0545

    [ALL] 9hentia: Source Request

---
+++ src/all/ninehentaimtl/src/eu/kanade/tachiyomi/extension/all/ninehentaimtl/Elog.kt
@@ -0,0 +1,43 @@
+package eu.kanade.tachiyomi.extension.all.ninehentaimtl
+
+import android.util.Log
+import okhttp3.Interceptor
+import okhttp3.Response
+
+private const val TAG = "ELOGGER"
+
+class Elog : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+
+        Log.d(
+            TAG,
+            buildString {
+                appendLine("***************Request**********")
+                appendLine("URL         : ${request.url}")
+                appendLine("Method      : ${request.method}")
+                // append("Content-Type: ${request.body?.contentType() ?: "N/A"}")
+                // appendLine("\n***********************************")
+            },
+        )
+
+        val response = chain.proceed(request)
+
+        // if (response.code != 200) {
+        Log.d(
+            TAG,
+            buildString {
+                appendLine("***************RESPONSE**********")
+                appendLine("URL         : ${response.request.url}")
+                appendLine("Status Code : ${response.code} ${response.message}")
+                append("Content-Type: ${response.header("Content-Type") ?: "N/A"}")
+                appendLine("\n***********************************")
+            },
+        )
+        // }
+
+        return response
+    }
+}
+
+// override val client = network.cloudflareClient.newBuilder().addInterceptor(Elog()).build()
