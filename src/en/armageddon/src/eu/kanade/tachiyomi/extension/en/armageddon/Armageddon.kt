8b6ab6a5c7b5e1da9d0cefd83f2c55e7003e036f
manti <133025162+manti-X@users.noreply.github.com>
2026-03-15 05:48:28 +0100

    Armageddon: fix NullPointerException
    - fix selectors

--- src/en/armageddon/src/eu/kanade/tachiyomi/extension/en/armageddon/Armageddon.kt
+++ src/en/armageddon/src/eu/kanade/tachiyomi/extension/en/armageddon/Armageddon.kt
@@ -1,10 +1,6 @@
 package eu.kanade.tachiyomi.extension.en.armageddon
 
-import android.util.Base64
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import eu.kanade.tachiyomi.source.model.Page
-import keiyoushi.utils.parseAs
-import org.jsoup.nodes.Document
 
 class Armageddon :
     MangaThemesia(
@@ -12,19 +8,11 @@ class Armageddon :
         baseUrl = "https://www.silentquill.net",
         lang = "en",
     ) {
-    override fun pageListParse(document: Document): List<Page> {
-        val scriptContent = document.selectFirst("script:containsData(WyJodHRw)")?.data()
-            ?: return super.pageListParse(document)
-        val base64Match = Regex("""['"](WyJodHRw[\w+/=]+)['"]""").find(scriptContent)?.groupValues?.get(1)
+    override val seriesTitleSelector = "h1.kdt8-left-title"
 
-        return if (base64Match != null) {
-            val decoded = String(Base64.decode(base64Match, Base64.DEFAULT))
-            val images = decoded.parseAs<List<String>>()
-            images.mapIndexed { i, img ->
-                Page(i, document.location(), img)
-            }
-        } else {
-            super.pageListParse(document)
-        }
-    }
+    override val seriesThumbnailSelector = ".kdt8-cover img"
+
+    override val seriesDescriptionSelector = ".kdt8-synopsis"
+
+    override val seriesGenreSelector = ".kdt8-genres a.kdt8-genre-tag"
 }
