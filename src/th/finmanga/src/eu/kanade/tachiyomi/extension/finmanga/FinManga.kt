6968bdcaa1df4af895484751c73a40ca18b86a11
Tek Narayan Chaudhary <84651805+NepThunder@users.noreply.github.com>
2026-03-15 20:56:46 +0545

    [TH] Fin Manga: Source Request (#14036)

---
+++ src/th/finmanga/src/eu/kanade/tachiyomi/extension/finmanga/FinManga.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.th.finmanga
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class FinManga : MangaThemesia("Fin Manga", "https://www.fin-manga.com", "th")
