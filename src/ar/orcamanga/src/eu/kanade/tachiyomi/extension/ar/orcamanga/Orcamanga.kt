8aa0be3bb766aa04d3e8840f8eead91cfeab90b9
NepThunder <tekchau21@gmail.com>
2026-03-15 17:27:13 +0545

    [AR] Orca Manga: Source Request

---
+++ src/ar/orcamanga/src/eu/kanade/tachiyomi/extension/ar/orcamanga/Orcamanga.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.ar.orcamanga
+
+import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
+
+class Orcamanga : ZeistManga("Orca Manga", "https://www.orcamanga.site", "ar")
