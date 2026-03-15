6d1aa8aa26ce263187a701aeb3ddc6bd07b559cf
NepThunder <tekchau21@gmail.com>
2026-03-15 22:26:24 +0545

    [JA] Jmanga: Source Request

---
+++ src/ja/jmanga/src/eu/kanade/tachiyomi/extension/ja/jmanga/Jmanga.kt
@@ -0,0 +1,27 @@
+package eu.kanade.tachiyomi.extension.ja.jmanga
+
+import eu.kanade.tachiyomi.multisrc.mangareader.MangaReader
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
+
+class Jmanga : MangaReader("Jmanga", "https://jmanga.codes", "ja") {
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val url = baseUrl.toHttpUrl().newBuilder().apply {
+            if (query.isNotBlank()) {
+                addQueryParameter("q", query)
+            } else {
+                addPathSegment("filter")
+                val filterList = filters.ifEmpty { getFilterList() }
+                filterList.filterIsInstance<MangaReader.UriFilter>().forEach {
+                    it.addToUri(this)
+                }
+            }
+            addPage(page, this)
+        }.build()
+
+        return GET(url, headers)
+    }
+}
