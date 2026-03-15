6df6414d227a449e9089dea561a1a13aa793ccc5
Tek Narayan Chaudhary <84651805+NepThunder@users.noreply.github.com>
2026-03-15 20:56:55 +0545

    [TH] Go Manga: Source Request (#14033)

---
+++ src/th/gomanga/src/eu/kanade/tachiyomi/extension/gomanga/Gomanga.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.th.gomanga
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class Gomanga : MangaThemesia("Go Manga", "https://www.go-manga.com", "th")
