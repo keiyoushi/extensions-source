946799d5ff287ebcd9ca75a9823b374617cab14d
NepThunder <tekchau21@gmail.com>
2026-03-18 13:58:17 +0545

    Changes

--- src/ja/jmanga/src/eu/kanade/tachiyomi/extension/ja/jmanga/Jmanga.kt
+++ src/ja/jmanga/src/eu/kanade/tachiyomi/extension/ja/jmanga/Jmanga.kt
@@ -1,5 +1,7 @@
 package eu.kanade.tachiyomi.extension.ja.jmanga
 
+import android.content.SharedPreferences
+import android.widget.Toast
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.multisrc.mangareader.MangaReader
@@ -14,18 +16,40 @@ class Jmanga :
     MangaReader("Jmanga", "https://jmanga.codes", "ja"),
     ConfigurableSource {
 
-    private val preferences by getPreferencesLazy()
+    private val defaultBaseUrl = "https://jmanga.codes"
 
-    private val baseDefaultUrl = "https://jmanga.codes"
-    override val baseUrl: String get() = preferences.getString(BASE_URL_PREF_KEY, baseDefaultUrl)!!
+    private var cachedBaseUrl: String = ""
+    private val SharedPreferences.prefBaseUrl: String get() {
+        return cachedBaseUrl.takeIf(String::isNotBlank)
+            ?: getString(BASE_URL_PREF_KEY, defaultBaseUrl)!!.also { cachedBaseUrl = it }
+    }
+
+    private val preferences: SharedPreferences by getPreferencesLazy {
+        getString(DEFAULT_BASE_URL_PREF, "").let { domain ->
+            if (domain != defaultBaseUrl) {
+                edit()
+                    .putString(BASE_URL_PREF_KEY, defaultBaseUrl)
+                    .putString(DEFAULT_BASE_URL_PREF, defaultBaseUrl)
+                    .apply()
+            }
+        }
+    }
+
+    override val baseUrl: String get() = preferences.prefBaseUrl
 
     override fun setupPreferenceScreen(screen: PreferenceScreen) {
         EditTextPreference(screen.context).apply {
             key = BASE_URL_PREF_KEY
-            title = "Base URL"
-            summary = "Change if the site moves to a new domain. Default: $baseDefaultUrl"
-            setDefaultValue(baseDefaultUrl)
-            dialogTitle = "Base URL"
+            title = BASE_URL_PREF_TITLE
+            summary = URL_PREF_SUMMARY
+            setDefaultValue(defaultBaseUrl)
+            dialogTitle = BASE_URL_PREF_TITLE
+            dialogMessage = "Default: $defaultBaseUrl"
+
+            setOnPreferenceChangeListener { _, _ ->
+                Toast.makeText(screen.context, RESTART_APP_MESSAGE, Toast.LENGTH_LONG).show()
+                true
+            }
         }.also(screen::addPreference)
     }
 
@@ -48,5 +72,9 @@ class Jmanga :
 
     companion object {
         private const val BASE_URL_PREF_KEY = "BASE_URL"
+        private const val DEFAULT_BASE_URL_PREF = "defaultBaseUrl"
+        private const val BASE_URL_PREF_TITLE = "Base URL"
+        private const val URL_PREF_SUMMARY= "Change if the site moves to a new domain. Leave blank to use default URL"
+        private const val RESTART_APP_MESSAGE = "Restart app to apply new setting."
     }
 }
