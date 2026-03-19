97d913d24b7d6d49e2db177bcdd7cb61df01671c
NepThunder <tekchau21@gmail.com>
2026-03-19 14:14:44 +0545

    "changes"

--- src/en/mangadrama/src/eu/kanade/tachiyomi/extension/en/mangadrama/MangaDrama.kt
+++ src/en/mangadrama/src/eu/kanade/tachiyomi/extension/en/mangadrama/MangaDrama.kt
@@ -6,8 +6,8 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara.LoadMoreStrategy
 class MangaDrama : Madara("Manga Drama", "https://mangadrama.com", "en") {
 
     override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val useNewChapterEndpoint = true
+    override fun chapterListSelector() = "li.wp-manga-chapter.free-chap"
 
-    override fun searchMangaSelector() = "div.page-item-detail:not(:has(a[href*='bilibilicomics.com']))$mangaEntrySelector"
-
-    override val searchMangaUrlSelector = "div.post-title a"
+    override fun searchMangaSelector() = popularMangaSelector()
 }
