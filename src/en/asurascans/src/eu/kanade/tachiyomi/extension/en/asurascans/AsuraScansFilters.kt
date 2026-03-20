baca059057ace30d96b2e71c62354e1c6e599708
bapeey <90949336+bapeey@users.noreply.github.com>
2026-03-20 02:23:09 -0500

    new site

--- src/en/asurascans/src/eu/kanade/tachiyomi/extension/en/asurascans/AsuraScansFilters.kt
+++ src/en/asurascans/src/eu/kanade/tachiyomi/extension/en/asurascans/AsuraScansFilters.kt
@@ -1,16 +1,55 @@
 package eu.kanade.tachiyomi.extension.en.asurascans
 
 import eu.kanade.tachiyomi.source.model.Filter
+import okhttp3.HttpUrl
 
-class Genre(title: String, val id: Int) : Filter.CheckBox(title)
-class GenreFilter(title: String, genres: List<Genre>) : Filter.Group<Genre>(title, genres)
+interface UriFilter {
+    fun addToUri(builder: HttpUrl.Builder)
+}
 
-class StatusFilter(title: String, statuses: List<Pair<String, String>>) : UriPartFilter(title, statuses)
+class SortFilter(defaultSort: String? = null) :
+    UriSortFilter(
+        name = "Sort By",
+        sortParam = "sort",
+        orderParam = "order",
+        vals = arrayOf(
+            "Latest Update" to "latest",
+            "Popular" to "popular",
+            "Rating" to "rating",
+            "A-Z" to "title",
+            "Newest" to "update",
+        ),
+        defaultValue = defaultSort,
+        ascendingValue = "asc",
+        descendingValue = "desc",
+    )
 
-class TypeFilter(title: String, types: List<Pair<String, String>>) : UriPartFilter(title, types)
+open class UriSortFilter(
+    name: String,
+    private val sortParam: String,
+    private val orderParam: String,
+    private val vals: Array<Pair<String, String>>,
+    defaultValue: String? = null,
+    private val ascendingValue: String,
+    private val descendingValue: String,
+) : Filter.Sort(
+    name,
+    vals.map { it.first }.toTypedArray(),
+    Selection(
+        vals.indexOfFirst { it.second == defaultValue }
+            .takeIf { it != -1 } ?: 0,
+        ascending = false,
+    ),
+),
+    UriFilter {
 
-class OrderFilter(title: String, orders: List<Pair<String, String>>) : UriPartFilter(title, orders)
+    override fun addToUri(builder: HttpUrl.Builder) {
+        val state = state ?: return
 
-open class UriPartFilter(displayName: String, val vals: List<Pair<String, String>>) : Filter.Select<String>(displayName, vals.map { it.first }.toTypedArray()) {
-    fun toUriPart() = vals[state].second
+        val selected = vals[state.index].second
+        val order = if (state.ascending) ascendingValue else descendingValue
+
+        builder.addQueryParameter(sortParam, selected)
+        builder.addQueryParameter(orderParam, order)
+    }
 }
