53e01161101ecf57809ec81f00ea8e0b196f152f
bapeey <90949336+bapeey@users.noreply.github.com>
2026-03-24 00:47:44 -0500

    Add LectorMoe sources (#14250)
    * add sources
    
    * bump

---
+++ src/es/bakaguyascanlation/src/eu/kanade/tachiyomi/extension/es/bakaguyascanlation/BakaguyaScanlation.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.es.bakaguyascanlation
+
+import eu.kanade.tachiyomi.multisrc.lectormoe.LectorMoe
+
+class BakaguyaScanlation :
+    LectorMoe(
+        "Bakaguya Scanlation",
+        "https://capibaratraductor.com/bakaguya",
+        "es",
+    )
