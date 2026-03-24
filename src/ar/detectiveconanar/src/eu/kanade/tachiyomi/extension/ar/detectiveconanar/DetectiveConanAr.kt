732f7dc8e8b0bef4f97240a7c555a56fc056260b
Allan Chiron <allanloannchiron@gmail.com>
2026-03-24 22:58:56 +0100

    Delete src/ar directory

--- src/ar/detectiveconanar/src/eu/kanade/tachiyomi/extension/ar/detectiveconanar/DetectiveConanAr.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.ar.detectiveconanar
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class DetectiveConanAr : Madara("شبكة كونان العربية", "https://manga.detectiveconanar.com", "ar")
