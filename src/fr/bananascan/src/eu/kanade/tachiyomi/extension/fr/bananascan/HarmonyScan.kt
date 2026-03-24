dc7341f3e9953c16323270e330239bcf06096058
Allan Chiron <allanloannchiron@gmail.com>
2026-03-24 22:38:09 +0100

    Fix typo in comment from 'Banana-Scann' to 'Banana-Scan'

--- src/fr/bananascan/src/eu/kanade/tachiyomi/extension/fr/bananascan/HarmonyScan.kt
+++ src/fr/bananascan/src/eu/kanade/tachiyomi/extension/fr/bananascan/HarmonyScan.kt
@@ -3,6 +3,6 @@ package eu.kanade.tachiyomi.extension.fr.bananascan
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
 class HarmonyScan : Madara("Harmony-Scan", "https://harmony-scan.fr", "fr") {
-    // formally Banana-Scann
+    // formally Banana-Scan
     override val id = 3121632933690925888
 }
