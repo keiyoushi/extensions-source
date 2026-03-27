301658719c6c7514238815e562509db7e3cb1f55
Hasan <hturkyilmaz19@posta.pau.edu.tr>
2026-03-27 14:32:50 +0300

    Add date format

--- src/tr/amangaplanet/src/eu/kanade/tachiyomi/extension/amangaplanet/AmangaPlanet.kt
+++ src/tr/amangaplanet/src/eu/kanade/tachiyomi/extension/amangaplanet/AmangaPlanet.kt
@@ -1,5 +1,13 @@
 package eu.kanade.tachiyomi.extension.tr.amangaplanet
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class AmangaPlanet : MangaThemesia("Amanga Planet", "https://www.amangaplanet.com.tr", "tr")
+class AmangaPlanet :
+    MangaThemesia(
+        name = "Amanga Planet",
+        baseUrl = "https://www.amangaplanet.com.tr",
+        lang = "tr",
+        dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale("tr", "TR")),
+    )
