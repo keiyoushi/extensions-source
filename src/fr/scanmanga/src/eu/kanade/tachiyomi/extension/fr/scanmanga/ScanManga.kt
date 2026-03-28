4346ae9fb5aec73b962267ec349e5d3d0e5d76d9
Romain <wycvhrt6vzscfpedxr@gmail.com>
2026-03-28 14:25:09 +0100

    fix(fr/scanmanga): nice day (#14268)

--- src/fr/scanmanga/src/eu/kanade/tachiyomi/extension/fr/scanmanga/ScanManga.kt
+++ src/fr/scanmanga/src/eu/kanade/tachiyomi/extension/fr/scanmanga/ScanManga.kt
@@ -46,7 +46,7 @@ class ScanManga :
 
     override val supportsLatest = true
 
-    protected val preferences by getPreferencesLazy()
+    private val preferences by getPreferencesLazy()
 
     override val client = super.client.newBuilder()
         .addNetworkInterceptor { chain ->
@@ -203,7 +203,7 @@ class ScanManga :
 
     // Pages
     private fun decodeHunter(obfuscatedJs: String): String {
-        val regex = Regex("""eval\(function\(\w,\w,\w,\w,\w,\w\)\{.*?\}\("([^"]+)",\d+,"([^"]+)",(\d+),(\d+),\d+\)\)""")
+        val regex = Regex("""eval\(function\(\w,\w,\w,\w,\w,\w,\w\)\{.*?\}\("([^"]+)",\d+,"([^"]+)",(\d+),(\d+),\d+\)\)""")
         val (encoded, mask, intervalStr, optionStr) = regex.find(obfuscatedJs)?.destructured
             ?: error("Failed to match obfuscation pattern: $obfuscatedJs")
 
