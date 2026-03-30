6802627c7b257a3081516567a6bea2afa31ce690
Aaron <168236779+AaronDeimos@users.noreply.github.com>
2026-03-30 07:31:13 +0530

    ErosScans rebrand to ScytheScans (#14345)
    * Remove scythescans
    
    * ErosScans rebrand to ScytheScans

--- src/en/erosscans/src/eu/kanade/tachiyomi/extension/en/erosscans/ErosScans.kt
+++ src/en/erosscans/src/eu/kanade/tachiyomi/extension/en/erosscans/ErosScans.kt
@@ -1,35 +1,41 @@
 package eu.kanade.tachiyomi.extension.en.erosscans
 
+import android.util.Base64
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import okhttp3.Interceptor
-import okhttp3.Response
+import eu.kanade.tachiyomi.source.model.Page
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
+import org.jsoup.nodes.Document
 
 class ErosScans :
     MangaThemesia(
-        "Eros Scans",
-        "https://erosxsun.xyz",
+        "Scythe Scans",
+        "https://scythescans.com",
         "en",
     ) {
+
+    override val id = 1124131000360667434
+
     override val client = super.client.newBuilder()
-        .addInterceptor(::cdnRedirectInterceptor)
         .rateLimit(3)
         .build()
 
-    private fun cdnRedirectInterceptor(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        if (request.url.host != "erosscans.xyz") {
-            return chain.proceed(request)
+    override fun pageListParse(document: Document): List<Page> {
+        val script = document.selectFirst("script[src^=data:text/javascript;base64,dHNfcmVhZGVyLnJ1bih7]")
+            ?: return super.pageListParse(document)
+
+        val decoded = Base64.decode(script.attr("src").substringAfter("base64,"), Base64.DEFAULT).decodeToString()
+
+        val imageListJson = JSON_IMAGE_LIST_REGEX.find(decoded)?.destructured?.toList()?.get(0).orEmpty()
+        val imageList = try {
+            json.parseToJsonElement(imageListJson).jsonArray
+        } catch (_: IllegalArgumentException) {
+            return super.pageListParse(document)
         }
 
-        val newUrl = request.url.newBuilder()
-            .host(baseUrl.toHttpUrl().host)
-            .build()
-        val newRequest = request.newBuilder()
-            .url(newUrl)
-            .build()
-
-        return chain.proceed(newRequest)
+        return imageList.mapIndexed { index, element ->
+            Page(index, imageUrl = element.jsonPrimitive.content)
+        }
     }
 }
