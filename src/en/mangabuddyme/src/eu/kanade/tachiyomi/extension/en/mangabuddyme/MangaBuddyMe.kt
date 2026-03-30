5b5c53be1b95a35c7671dfd8d98f55aa59f7ddcb
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-03-30 21:23:07 +0200

    Add MangaBuddy.me

---
+++ src/en/mangabuddyme/src/eu/kanade/tachiyomi/extension/en/mangabuddyme/MangaBuddyMe.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.mangabuddyme
+
+import eu.kanade.tachiyomi.multisrc.madtheme.MadTheme
+
+class MangaBuddyMe : MadTheme("MangaBuddy.me", "https://mangabuddy.me", "en")
