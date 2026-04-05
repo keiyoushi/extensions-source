5e97cc92886e3a1dfc08873d634696e59d96ef8b
bapeey <90949336+bapeey@users.noreply.github.com>
2026-04-04 22:11:37 -0500

    add whitepearl

---
+++ src/es/whitepearltranslations/src/eu/kanade/tachiyomi/extension/es/whitepearltranslations/WhitePearlTranslations.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.es.whitepearltranslations
+
+import eu.kanade.tachiyomi.multisrc.lectormoe.LectorMoe
+
+class WhitePearlTranslations :
+    LectorMoe(
+        "White Pearl Translations",
+        "https://capibaratraductor.com/whitepearl",
+        "es",
+    )
