7317c936f4dd921b7a9f1b03ff9c36c5b5020970
bapeey <90949336+bapeey@users.noreply.github.com>
2026-04-04 22:33:26 -0500

    add spanish lang

--- src/all/hdoujin/src/eu/kanade/tachiyomi/extension/all/hdoujin/HDoujinFactory.kt
+++ src/all/hdoujin/src/eu/kanade/tachiyomi/extension/all/hdoujin/HDoujinFactory.kt
@@ -6,6 +6,7 @@ class HDoujinFactory : SourceFactory {
     override fun createSources() = listOf(
         HDoujin("all"),
         HDoujin("en", "english"),
+        HDoujin("es", "spanish"),
         HDoujin("ja", "japanese"),
         HDoujin("kr", "korean"),
         HDoujin("zh", "chinese"),
