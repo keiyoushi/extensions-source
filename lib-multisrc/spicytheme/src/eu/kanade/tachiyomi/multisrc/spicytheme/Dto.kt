fa435fb8942567dc0df5824684ceefbe412ea8c4
iq8 <266871934+disk-iq8@users.noreply.github.com>
2026-04-05 14:54:20 +0300

    switch to JsonTransforming

--- lib-multisrc/spicytheme/src/eu/kanade/tachiyomi/multisrc/spicytheme/Dto.kt
+++ lib-multisrc/spicytheme/src/eu/kanade/tachiyomi/multisrc/spicytheme/Dto.kt
@@ -5,13 +5,11 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.model.UpdateStrategy
 import keiyoushi.utils.tryParse
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonTransformingSerializer
 import kotlinx.serialization.serializer
 import java.text.SimpleDateFormat
 
@@ -86,19 +84,8 @@ data class ChapterImagesDto(
 
 // Get first item only in case of JsonArray
 
-object FirstobjOrObj : KSerializer<ChapterImagesDto> {
-
-    override val descriptor = ChapterImagesDto.serializer().descriptor
-
-    override fun deserialize(decoder: Decoder): ChapterImagesDto {
-        val el = (decoder as JsonDecoder).decodeJsonElement()
-        val obj = if (el is JsonArray) el[0] else el
-        return decoder.json.decodeFromJsonElement(ChapterImagesDto.serializer(), el)
-    }
-
-    override fun serialize(encoder: Encoder, value: ChapterImagesDto) {
-        encoder.encodeSerializableValue(serializer(), value)
-    }
+object FirstobjOrObj : JsonTransformingSerializer<ChapterImagesDto>(ChapterImagesDto.serializer()) {
+    override fun transformDeserialize(element: JsonElement): JsonElement = if (element is JsonArray) element[0] else element
 }
 
 fun MangaDto.toSManga() = SManga.create().apply {
