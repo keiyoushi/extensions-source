77f401837216709da55d77dd7749d94f8db9d1cd
Syabit <tebepc@gmail.com>
2026-04-11 14:31:51 +0700

    Revert "Hive Scans: fix Missing FieldException (#14670)"
    This reverts commit 9fe2a270bf8e8ffa9341b188fcaaa0052f950d25.

--- src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/HiveScans.kt
+++ src/en/infernalvoidscans/src/eu/kanade/tachiyomi/extension/en/infernalvoidscans/HiveScans.kt
@@ -1,7 +1,6 @@
 package eu.kanade.tachiyomi.extension.en.infernalvoidscans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
-import okhttp3.ResponseBody.Companion.toResponseBody
 
 class HiveScans :
     Iken(
@@ -18,28 +17,7 @@ class HiveScans :
             val headers = request.headers.newBuilder()
                 .set("Cache-Control", "max-age=0")
                 .build()
-
-            val response = chain.proceed(request.newBuilder().headers(headers).build())
-
-            if (request.url.encodedPath.endsWith("/api/query")) {
-                val body = response.body ?: return@addInterceptor response
-                val contentType = body.contentType()
-                val bodyString = body.string()
-
-                // The API removed the `isNovel` field from the /api/query endpoint which crashes the parser.
-                // We inject it manually right before the `postTitle` property inside the SearchResponse mapping.
-                val newBody = if (!bodyString.contains("\"isNovel\"")) {
-                    bodyString.replace("\"postTitle\":", "\"isNovel\":false,\"postTitle\":")
-                } else {
-                    bodyString
-                }
-
-                return@addInterceptor response.newBuilder()
-                    .body(newBody.toResponseBody(contentType))
-                    .build()
-            }
-
-            response
+            chain.proceed(request.newBuilder().headers(headers).build())
         }
         .build()
 
