441971a95a2303bb8a99694ea2fb1391a0b565b9
darwin-256 <viktorasordinass@gmail.com>
2026-04-12 11:50:50 +0300

    Update GreedScans.kt

--- src/en/greedscans/src/eu/kanade/tachiyomi/extension/en/greedscans/GreedScans.kt
+++ src/en/greedscans/src/eu/kanade/tachiyomi/extension/en/greedscans/GreedScans.kt
@@ -1,10 +1,33 @@
 package eu.kanade.tachiyomi.extension.en.greedscans
 
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesiaPaidChapterHelper
+import eu.kanade.tachiyomi.source.ConfigurableSource
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 class GreedScans :
     MangaThemesia(
-        "greed scans",
+        "Greed Scans",
         "https://greedscans.com",
         "en",
+    ),
+    ConfigurableSource {
+
+    private val preferences: SharedPreferences =
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+
+    private val paidChapterHelper = MangaThemesiaPaidChapterHelper()
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        paidChapterHelper.addHidePaidChaptersPreferenceToScreen(screen, intl)
+    }
+
+    override fun chapterListSelector(): String = paidChapterHelper.getChapterListSelectorBasedOnHidePaidChaptersPref(
+        super.chapterListSelector(),
+        preferences,
     )
+}
