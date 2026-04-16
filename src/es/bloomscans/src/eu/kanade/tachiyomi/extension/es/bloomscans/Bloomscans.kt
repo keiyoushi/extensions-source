022bdccae7c70de34356025f9ca3c4e75de39a4f
bapeey <90949336+bapeey@users.noreply.github.com>
2026-04-15 21:57:00 -0500

    update selectors

--- src/es/bloomscans/src/eu/kanade/tachiyomi/extension/es/bloomscans/Bloomscans.kt
+++ src/es/bloomscans/src/eu/kanade/tachiyomi/extension/es/bloomscans/Bloomscans.kt
@@ -1,5 +1,22 @@
 package eu.kanade.tachiyomi.extension.es.bloomscans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class Bloomscans : MangaThemesia("Bloom Scans", "https://bloomscans.com", "es", mangaUrlDirectory = "/series")
+class Bloomscans :
+    MangaThemesia(
+        "Bloom Scans",
+        "https://bloomscans.com",
+        "es",
+        mangaUrlDirectory = "/series",
+        dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("es")),
+    ) {
+    override val seriesTitleSelector = ".lrs-title"
+    override val seriesThumbnailSelector = "img.lrs-cover"
+    override val seriesDescriptionSelector = ".lrs-syn-wrap"
+    override val seriesStatusSelector = ".lrs-infotable tr:contains(Status) td:last-child"
+    override val seriesGenreSelector = ".lrs-genre"
+
+    override fun chapterListSelector() = "#lrs-native-chapterlist li"
+}
