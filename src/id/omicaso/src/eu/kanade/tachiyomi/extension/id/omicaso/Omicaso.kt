92c91077acdf25164dabadd1b1f1e75c89d49bb0
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-04-16 19:12:06 +0200

    Update domain for Omicaso

--- src/id/omicaso/src/eu/kanade/tachiyomi/extension/id/omicaso/Omicaso.kt
+++ src/id/omicaso/src/eu/kanade/tachiyomi/extension/id/omicaso/Omicaso.kt
@@ -2,4 +2,4 @@ package eu.kanade.tachiyomi.extension.id.omicaso
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 
-class Omicaso : MangaThemesia("Omicaso", "https://omicaso.in", "id")
+class Omicaso : MangaThemesia("Omicaso", "https://omicaso.org", "id")
