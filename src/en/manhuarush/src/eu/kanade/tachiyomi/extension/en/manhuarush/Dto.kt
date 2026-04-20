8d55d074f603496ae1f86aa2f3b6835ed46a61e7
darwin-256 <viktorasordinass@gmail.com>
2026-04-20 11:41:32 +0300

    Update Dto.kt

--- src/en/manhuarush/src/eu/kanade/tachiyomi/extension/en/manhuarush/Dto.kt
+++ src/en/manhuarush/src/eu/kanade/tachiyomi/extension/en/manhuarush/Dto.kt
@@ -4,14 +4,13 @@ import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import keiyoushi.utils.tryParse
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 class MangaDetailsDto(
-    @SerialName("text") private val text: String? = null,
-    @SerialName("chapters") val chapters: List<ChapterDto>,
-    @SerialName("mangadexId") val mangadexId: String,
+    val text: String? = null,
+    val chapters: List<ChapterDto>,
+    val mangadexId: String,
 ) {
     fun toSManga(): SManga = SManga.create().apply {
         description = text
@@ -20,20 +19,20 @@ class MangaDetailsDto(
 
 @Serializable
 class ChapterDto(
-    @SerialName("chapter") private val chapter: String,
-    @SerialName("title") private val title: String? = null,
-    @SerialName("createdAt") private val createdAt: String? = null,
+    val chapter: String,
+    val title: String,
+    val createdAt: String? = null,
 ) {
     fun toSChapter(mangadexId: String): SChapter = SChapter.create().apply {
         url = "/reader/$mangadexId/$chapter"
-        name = "Chapter $chapter" + (if (!title.isNullOrEmpty()) " - $title" else "")
+        name = "Chapter $chapter" + (if (title.isNotEmpty()) " - $title" else "")
         date_upload = manhuaRushDateFormat.tryParse(createdAt)
     }
 }
 
 @Serializable
 class ReaderDto(
-    @SerialName("imageUrls") private val imageUrls: List<String>,
+    val imageUrls: List<String>,
 ) {
     fun toPages(): List<Page> = imageUrls.mapIndexed { index, url ->
         Page(index, imageUrl = url)
