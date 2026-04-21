87818afeed42577f1e114300fa3c36c8138071f3
Manticore <133025162+manti-X@users.noreply.github.com>
2026-04-21 17:04:54 +0200

    proto

--- core/src/main/kotlin/keiyoushi/utils/Protobuf.kt
+++ core/src/main/kotlin/keiyoushi/utils/Protobuf.kt
@@ -17,52 +17,67 @@ val PROTOBUF_MEDIA_TYPE = "application/protobuf".toMediaType()
 
 /**
  * Decodes a [ByteArray] into an object of type [T] using Protobuf deserialization.
+ *
+ * @param proto The [ProtoBuf] instance to use for deserialization.
  */
-inline fun <reified T> ByteArray.decodeProto(): T = protoInstance.decodeFromByteArray<T>(this)
+inline fun <reified T> ByteArray.decodeProto(proto: ProtoBuf = protoInstance): T = proto.decodeFromByteArray<T>(this)
 
 /**
  * Encodes the object into a [ByteArray] using Protobuf serialization.
+ *
+ * @param proto The [ProtoBuf] instance to use for serialization.
  */
-inline fun <reified T : Any> T.encodeProto(): ByteArray = protoInstance.encodeToByteArray(this)
+inline fun <reified T : Any> T.encodeProto(proto: ProtoBuf = protoInstance): ByteArray = proto.encodeToByteArray(this)
 
 /**
  * Parses the response body into an object of type [T] using Protobuf deserialization.
  *
  * The response is automatically closed after reading.
+ *
+ * @param proto The [ProtoBuf] instance to use for deserialization.
  */
-inline fun <reified T> Response.parseAsProto(): T = use { protoInstance.decodeFromByteArray(it.body.bytes()) }
+inline fun <reified T> Response.parseAsProto(proto: ProtoBuf = protoInstance): T = use { it.body.bytes().decodeProto(proto) }
 
 /**
  * Parses the response body into an object of type [T] using Protobuf deserialization.
  *
  * The response is automatically closed after reading.
  *
+ * @param proto The [ProtoBuf] instance to use for deserialization.
  * @param transform A function to transform the raw [ByteArray] before it is decoded.
  */
-inline fun <reified T> Response.parseAsProto(transform: (ByteArray) -> ByteArray): T = use { transform(it.body.bytes()).decodeProto() }
+inline fun <reified T> Response.parseAsProto(proto: ProtoBuf = protoInstance, transform: (ByteArray) -> ByteArray): T = use { transform(it.body.bytes()).decodeProto(proto) }
 
 /**
  * Parses this [ResponseBody] into an object of type [T] using Protobuf deserialization.
  *
  * The body is automatically closed after reading.
+ *
+ * @param proto The [ProtoBuf] instance to use for deserialization.
  */
-inline fun <reified T> ResponseBody.parseAsProto(): T = protoInstance.decodeFromByteArray(bytes())
+inline fun <reified T> ResponseBody.parseAsProto(proto: ProtoBuf = protoInstance): T = bytes().decodeProto(proto)
 
 /**
  * Encodes the object into a Protobuf [RequestBody] with the [PROTOBUF_MEDIA_TYPE] content type.
+ *
+ * @param proto The [ProtoBuf] instance to use for serialization.
  */
-inline fun <reified T : Any> T.toRequestBodyProto(): RequestBody = encodeProto().toRequestBody(PROTOBUF_MEDIA_TYPE)
+inline fun <reified T : Any> T.toRequestBodyProto(proto: ProtoBuf = protoInstance): RequestBody = encodeProto(proto).toRequestBody(PROTOBUF_MEDIA_TYPE)
 
 /**
  * Decodes a Base64-encoded string into an object of type [T] using Protobuf deserialization.
  *
  * The string is expected to be encoded with [Base64.NO_WRAP].
+ *
+ * @param proto The [ProtoBuf] instance to use for deserialization.
  */
-inline fun <reified T> String.decodeProtoBase64(): T = protoInstance.decodeFromByteArray(Base64.decode(this, Base64.NO_WRAP))
+inline fun <reified T> String.decodeProtoBase64(proto: ProtoBuf = protoInstance): T = Base64.decode(this, Base64.NO_WRAP).decodeProto(proto)
 
 /**
  * Encodes the object to a Protobuf [ByteArray] and returns it as a Base64-encoded string.
  *
  * The resulting string is encoded with [Base64.NO_WRAP].
+ *
+ * @param proto The [ProtoBuf] instance to use for serialization.
  */
-inline fun <reified T : Any> T.encodeProtoBase64(): String = Base64.encodeToString(encodeProto(), Base64.NO_WRAP)
+inline fun <reified T : Any> T.encodeProtoBase64(proto: ProtoBuf = protoInstance): String = Base64.encodeToString(encodeProto(proto), Base64.NO_WRAP)
