4064fbd749402dad179405e4aed2017755191e71
mozzaru <rianyanuar52@gmail.com>
2026-04-23 13:36:14 +0700

    fix

--- src/id/mgkomik/src/eu/kanade/tachiyomi/extension/id/mgkomik/MGKomik.kt
+++ src/id/mgkomik/src/eu/kanade/tachiyomi/extension/id/mgkomik/MGKomik.kt
@@ -30,7 +30,6 @@ class MGKomik :
     override val mangaSubString = "komik"
 
     override fun headersBuilder() = super.headersBuilder().apply {
-        set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36")
         set("Referer", "$baseUrl/")
         set("Sec-Fetch-Site", "none")
     }
