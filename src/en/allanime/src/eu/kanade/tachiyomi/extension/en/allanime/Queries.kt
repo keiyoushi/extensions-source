40a5b4d68dac1d5c1ad2d9aadbc745d36e1fa044
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-04-25 18:50:00 +0500

    allanime: fix chapter list & refactor

--- src/en/allanime/src/eu/kanade/tachiyomi/extension/en/allanime/Queries.kt
+++ src/en/allanime/src/eu/kanade/tachiyomi/extension/en/allanime/Queries.kt
@@ -1,57 +1,24 @@
 package eu.kanade.tachiyomi.extension.en.allanime
 
-private fun buildQuery(queryAction: () -> String): String = queryAction()
-    .trimIndent()
-    .replace("%", "$")
-
-val POPULAR_QUERY: String = buildQuery {
-    """
-        query (
-            %type: VaildPopularTypeEnumType!
-            %size: Int!
-            %page: Int
-            %dateRange: Int
-            %allowAdult: Boolean
-            %allowUnknown: Boolean
+const val POPULAR_QUERY: String = $$"""
+    query (
+        $type: VaildPopularTypeEnumType!
+        $size: Int!
+        $page: Int
+        $dateRange: Int
+        $allowAdult: Boolean
+        $allowUnknown: Boolean
+    ) {
+        queryPopular(
+            type: $type
+            size: $size
+            dateRange: $dateRange
+            page: $page
+            allowAdult: $allowAdult
+            allowUnknown: $allowUnknown
         ) {
-            queryPopular(
-                type: %type
-                size: %size
-                dateRange: %dateRange
-                page: %page
-                allowAdult: %allowAdult
-                allowUnknown: %allowUnknown
-            ) {
-                recommendations {
-                    anyCard {
-                        _id
-                        name
-                        thumbnail
-                        englishName
-                    }
-                }
-            }
-        }
-    """
-}
-
-val SEARCH_QUERY: String = buildQuery {
-    """
-        query (
-            %search: SearchInput
-            %size: Int
-            %page: Int
-            %translationType: VaildTranslationTypeMangaEnumType
-            %countryOrigin: VaildCountryOriginEnumType
-        ) {
-            mangas(
-                search: %search
-                limit: %size
-                page: %page
-                translationType: %translationType
-                countryOrigin: %countryOrigin
-            ) {
-                edges {
+            recommendations {
+                anyCard {
                     _id
                     name
                     thumbnail
@@ -59,61 +26,85 @@ val SEARCH_QUERY: String = buildQuery {
                 }
             }
         }
-    """
-}
+    }
+"""
 
-val DETAILS_QUERY: String = buildQuery {
-    """
-        query (%id: String!) {
-            manga(_id: %id) {
+const val SEARCH_QUERY: String = $$"""
+    query (
+        $search: SearchInput
+        $size: Int
+        $page: Int
+        $translationType: VaildTranslationTypeMangaEnumType
+        $countryOrigin: VaildCountryOriginEnumType
+    ) {
+        mangas(
+            search: $search
+            limit: $size
+            page: $page
+            translationType: $translationType
+            countryOrigin: $countryOrigin
+        ) {
+            edges {
                 _id
                 name
                 thumbnail
-                description
-                authors
-                genres
-                tags
-                status
-                altNames
                 englishName
             }
         }
-    """
-}
+    }
+"""
 
-val CHAPTERS_QUERY: String = buildQuery {
-    """
-        query (%id: String!, %chapterNumStart: Float!, %chapterNumEnd: Float!) {
-            episodeInfos(
-                showId: %id
-                episodeNumStart: %chapterNumStart
-                episodeNumEnd: %chapterNumEnd
-            ) {
-                episodeIdNum
-                notes
-                uploadDates
-            }
+const val DETAILS_QUERY: String = $$"""
+    query ($id: String!) {
+        manga(_id: $id) {
+            _id
+            name
+            thumbnail
+            description
+            authors
+            genres
+            tags
+            status
+            altNames
+            englishName
         }
-    """
-}
+    }
+"""
 
-val PAGE_QUERY: String = buildQuery {
-    """
-        query (
-            %id: String!
-            %translationType: VaildTranslationTypeMangaEnumType!
-            %chapterNum: String!
+const val CHAPTERS_QUERY: String = $$"""
+    query ($id: String!, $showId: String!) {
+        manga(_id: $id) {
+            _id
+            name
+            availableChaptersDetail
+        }
+        episodeInfos(
+            showId: $showId
+            episodeNumStart: 0
+            episodeNumEnd: 9999
         ) {
-            chapterPages(
-                mangaId: %id
-                translationType: %translationType
-                chapterString: %chapterNum
-            ) {
-                edges {
-                    pictureUrls
-                    pictureUrlHead
-                }
+            episodeIdNum
+            notes
+            uploadDates
+        }
+    }
+"""
+
+const val PAGE_QUERY: String = $$"""
+    query (
+        $id: String!
+        $translationType: VaildTranslationTypeMangaEnumType!
+        $chapterNum: String!
+    ) {
+        chaptersForRead(
+            mangaId: $id
+            translationType: $translationType
+            chapterString: $chapterNum
+        ) {
+            edges {
+                pictureUrls
+                pictureUrlHead
             }
         }
-    """
-}
+    }
+"""
