5243f156fe6c0f5ed8cbcf99f30f4edb93b15ce3
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-04-26 21:25:32 +0700

    WPComics change to HttpSource (#15091)
    * WPComics change to HttpSource and bump version
    
    * Use Response.asJsoup() in parsing methods
    
    Change parsing methods to accept okhttp3.Response and convert it to a Jsoup Document via eu.kanade.tachiyomi.util.asJsoup(). Update pageListParse and mangaDetailsParse signatures and bodies across several extensions, add/import okhttp3.Response and asJsoup, and remove direct org.jsoup.nodes.Document usage. Affected files: Raw18.kt, DocTruyen3Q.kt, NetTruyenCO.kt, NetTruyenX.kt, NhatTruyen.kt, TopTruyen.kt. This centralizes response-to-Document conversion and keeps parsing consistent with network responses.
    
    * Lint

--- src/ja/raw18/src/eu/kanade/tachiyomi/extension/ja/raw18/Raw18.kt
+++ src/ja/raw18/src/eu/kanade/tachiyomi/extension/ja/raw18/Raw18.kt
@@ -5,8 +5,10 @@ import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
+import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
@@ -26,7 +28,8 @@ class Raw18 : WPComics("Raw18", "https://raw18.tel", "ja", SimpleDateFormat("yyy
 
     override fun searchMangaSelector() = popularMangaSelector()
 
-    override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
+    override fun mangaDetailsParse(response: Response): SManga = SManga.create().apply {
+        val document = response.asJsoup()
         document.selectFirst("article#item-detail").let { info ->
             author = info?.selectFirst("li.author p.col-xs-8")?.text()
             status = info?.selectFirst("li.status p.col-xs-8")?.text().toStatus()
