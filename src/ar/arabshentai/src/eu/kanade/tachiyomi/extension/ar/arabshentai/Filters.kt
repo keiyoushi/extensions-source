f2b10e9f2d2923fe310fb79a51e2cb5ef18e4f8d
darwin-256 <viktorasordinass@gmail.com>
2026-04-27 23:15:47 +0300

    Update and rename ArabsHentaiFilters.kt to Filters.kt

--- src/ar/arabshentai/src/eu/kanade/tachiyomi/extension/ar/arabshentai/Filters.kt
+++ src/ar/arabshentai/src/eu/kanade/tachiyomi/extension/ar/arabshentai/Filters.kt
@@ -2,17 +2,6 @@ package eu.kanade.tachiyomi.extension.ar.arabshentai
 
 import eu.kanade.tachiyomi.source.model.Filter
 
-class StatusFilter :
-    Filter.Group<FilterCheckbox>(
-        "الحالة",
-        arrayOf(
-            Pair("مستمرة", "on-going"),
-            Pair("مكتملة", "end"),
-            Pair("ملغية", "canceled"),
-            Pair("متوقفة حالياً", "on-hold"),
-        ).map { FilterCheckbox(it.first, it.second) },
-    )
-
 internal var genreList: List<Pair<String, String>> = emptyList()
 
 class FilterCheckbox(name: String, val uriPart: String) : Filter.CheckBox(name)
@@ -26,6 +15,17 @@ class GenresOpFilter :
             Pair("يحتوي على إحدى التصنيفات المدرجة", ""),
             Pair("يحتوي على جميع التصنيفات المدرجة", "1"),
         ),
+    )
+
+class StatusFilter :
+    Filter.Group<FilterCheckbox>(
+        "الحالة",
+        arrayOf(
+            Pair("مستمرة", "on-going"),
+            Pair("مكتملة", "end"),
+            Pair("ملغية", "canceled"),
+            Pair("متوقفة حالياً", "on-hold"),
+        ).map { FilterCheckbox(it.first, it.second) },
     )
 
 open class UriPartFilter(displayName: String, private val pairs: Array<Pair<String, String>>) : Filter.Select<String>(displayName, pairs.map { it.first }.toTypedArray()) {
