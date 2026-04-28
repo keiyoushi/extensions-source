b7e40c31aa2eb84620359a8deb4549dec2b8fa3e
Sergio Telesca <sergio.telesca@infosfera.biz>
2026-04-28 08:05:27 +0200

    fix: use CookieInterceptor for AsiaToon cookie

--- src/en/asiatoon/src/eu/kanade/tachiyomi/extension/en/asiatoon/AsiaToon.kt
+++ src/en/asiatoon/src/eu/kanade/tachiyomi/extension/en/asiatoon/AsiaToon.kt
@@ -8,6 +8,7 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
+import keiyoushi.lib.cookieinterceptor.CookieInterceptor
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
@@ -58,11 +59,12 @@ class AsiaToon : HttpSource() {
         "Romance",
     )
 
-    override val client = network.cloudflareClient
+    override val client = network.cloudflareClient.newBuilder()
+        .addNetworkInterceptor(CookieInterceptor(baseUrl.toHttpUrl().host, "hc_vfs" to "Y"))
+        .build()
 
     override fun headersBuilder() = super.headersBuilder()
         .set("Referer", "$baseUrl/")
-        .set("Cookie", "hc_vfs=Y")
 
     override fun popularMangaRequest(page: Int) = GET("$baseUrl/en/genres?page=$page", headers)
 
