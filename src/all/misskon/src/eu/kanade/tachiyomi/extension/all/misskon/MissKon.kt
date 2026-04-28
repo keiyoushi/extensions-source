83c58409117f883365dcbb105f2fcb7818c1d286
darwin-256 <viktorasordinass@gmail.com>
2026-04-28 12:07:01 +0300

    Update MissKon.kt

--- src/all/misskon/src/eu/kanade/tachiyomi/extension/all/misskon/MissKon.kt
+++ src/all/misskon/src/eu/kanade/tachiyomi/extension/all/misskon/MissKon.kt
@@ -4,23 +4,24 @@ import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.model.UpdateStrategy
+import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.utils.firstInstance
 import keiyoushi.utils.tryParse
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
-class MissKon : SimpleParsedHttpSource() {
+class MissKon : HttpSource() {
 
     override val baseUrl = "https://misskon.com"
     override val lang = "all"
@@ -31,9 +32,7 @@ class MissKon : SimpleParsedHttpSource() {
         .rateLimitHost(baseUrl.toHttpUrl(), 10, 1, TimeUnit.SECONDS)
         .build()
 
-    override fun simpleMangaSelector() = "article.item-list"
-
-    override fun simpleMangaFromElement(element: Element): SManga {
+    private fun mangaFromElement(element: Element): SManga {
         val titleEL = element.selectFirst(".post-box-title")!!
         return SManga.create().apply {
             title = titleEL.text()
@@ -43,16 +42,25 @@ class MissKon : SimpleParsedHttpSource() {
         }
     }
 
-    override fun simpleNextPageSelector(): String? = null
-
     // region popular
     override fun popularMangaRequest(page: Int) = GET("$baseUrl/top3/", headers)
+
+    override fun popularMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select("article.item-list").map { mangaFromElement(it) }
+        return MangasPage(mangas, false)
+    }
     // endregion
 
     // region latest
     override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/page/$page", headers)
 
-    override fun latestUpdatesNextPageSelector() = ".current + a.page"
+    override fun latestUpdatesParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select("article.item-list").map { mangaFromElement(it) }
+        val hasNextPage = document.selectFirst(".current + a.page") != null
+        return MangasPage(mangas, hasNextPage)
+    }
     // endregion
 
     // region Search
@@ -68,12 +76,17 @@ class MissKon : SimpleParsedHttpSource() {
         }
     }
 
-    override fun searchMangaNextPageSelector() = "div.content > div.pagination > span.current + a"
+    override fun searchMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select("article.item-list").map { mangaFromElement(it) }
+        val hasNextPage = document.selectFirst("div.content > div.pagination > span.current + a") != null
+        return MangasPage(mangas, hasNextPage)
+    }
     // endregion
 
     // region Details
-    override fun mangaDetailsParse(document: Document): SManga {
-        val postInnerEl = document.selectFirst("article > .post-inner")!!
+    override fun mangaDetailsParse(response: Response): SManga {
+        val postInnerEl = response.asJsoup().selectFirst("article > .post-inner")!!
         return SManga.create().apply {
             title = postInnerEl.select(".post-title").text()
             genre = postInnerEl.select(".post-tag > a").joinToString { it.text() }
@@ -82,7 +95,6 @@ class MissKon : SimpleParsedHttpSource() {
         }
     }
 
-    override fun chapterFromElement(element: Element) = throw UnsupportedOperationException()
     override fun chapterListParse(response: Response): List<SChapter> {
         val doc = response.asJsoup()
         val dateUploadStr = doc.selectFirst(".entry img")?.absUrl("data-src")
@@ -104,8 +116,11 @@ class MissKon : SimpleParsedHttpSource() {
     // endregion
 
     // region Pages
-    override fun pageListParse(document: Document): List<Page> = document.select("div.post-inner > div.entry > p > img")
+    override fun pageListParse(response: Response): List<Page> = response.asJsoup()
+        .select("div.post-inner > div.entry > p > img")
         .mapIndexed { i, imgEl -> Page(i, imageUrl = imgEl.absUrl("data-src")) }
+
+    override fun imageUrlParse(response: Response): String = throw UnsupportedOperationException()
     // endregion
 
     override fun getFilterList(): FilterList = FilterList(
@@ -115,10 +130,8 @@ class MissKon : SimpleParsedHttpSource() {
     )
 
     companion object {
-
         private val FULL_DATE_REGEX = Regex("""/(\d{4}/\d{2}/\d{2})/""")
         private val YEAR_MONTH_REGEX = Regex("""/(\d{4}/\d{2})/""")
-
         private val FULL_DATE_FORMAT = SimpleDateFormat("yyyy/MM/dd", Locale.US)
     }
 }
