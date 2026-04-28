fe91f5ca90fe52ea382e6cddc509a5aa01c900e1
darwin-256 <viktorasordinass@gmail.com>
2026-04-28 10:57:39 +0300

    Update Comikey.kt

--- src/all/comikey/src/eu/kanade/tachiyomi/extension/all/comikey/Comikey.kt
+++ src/all/comikey/src/eu/kanade/tachiyomi/extension/all/comikey/Comikey.kt
@@ -21,32 +21,36 @@ import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.lib.i18n.Intl
 import keiyoushi.utils.getPreferencesLazy
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
+import keiyoushi.utils.parseAs
+import keiyoushi.utils.tryParse
 import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import rx.Observable
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.TimeZone
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
+
+private val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ROOT).apply {
+    timeZone = TimeZone.getTimeZone("UTC")
+}
 
 open class Comikey(
     final override val lang: String,
     override val name: String = "Comikey",
     override val baseUrl: String = "https://comikey.com",
     private val defaultLanguage: String = "en",
-) : ParsedHttpSource(),
+) : HttpSource(),
     ConfigurableSource {
 
     private val gundamUrl: String = "https://gundam.comikey.net"
@@ -60,16 +64,6 @@ open class Comikey(
     override fun headersBuilder() = super.headersBuilder()
         .add("Referer", "$baseUrl/")
 
-    private val dateFormat by lazy {
-        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ROOT)
-    }
-
-    private val json = Json {
-        ignoreUnknownKeys = true
-        explicitNulls = false
-        isLenient = true
-    }
-
     private val intl = Intl(
         language = lang,
         baseLanguage = "en",
@@ -81,19 +75,17 @@ open class Comikey(
 
     override fun popularMangaRequest(page: Int) = GET("$baseUrl/comics/?order=-views&page=$page", headers)
 
-    override fun popularMangaSelector() = searchMangaSelector()
+    override fun popularMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select("div.series-listing[data-view=list] > ul > li").map(::searchMangaFromElement)
+        val hasNextPage = document.selectFirst("ul.pagination li.next-page:not(.disabled)") != null
 
-    override fun popularMangaFromElement(element: Element) = searchMangaFromElement(element)
-
-    override fun popularMangaNextPageSelector() = searchMangaNextPageSelector()
+        return MangasPage(mangas, hasNextPage)
+    }
 
     override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/comics/?page=$page", headers)
 
-    override fun latestUpdatesSelector() = searchMangaSelector()
-
-    override fun latestUpdatesFromElement(element: Element) = searchMangaFromElement(element)
-
-    override fun latestUpdatesNextPageSelector() = searchMangaNextPageSelector()
+    override fun latestUpdatesParse(response: Response) = popularMangaParse(response)
 
     override fun fetchSearchManga(
         page: Int,
@@ -127,34 +119,38 @@ open class Comikey(
         return GET(url, headers)
     }
 
-    override fun searchMangaSelector() = "div.series-listing[data-view=list] > ul > li"
+    override fun searchMangaParse(response: Response) = popularMangaParse(response)
 
-    override fun searchMangaFromElement(element: Element) = SManga.create().apply {
+    private fun searchMangaFromElement(element: Element) = SManga.create().apply {
         element.selectFirst("div.series-data span.title a")!!.let {
-            setUrlWithoutDomain(it.attr("href"))
+            setUrlWithoutDomain(it.attr("abs:href"))
             title = it.text()
         }
 
-        description = element.select("div.excerpt p").text() +
-            "\n\n" +
-            element.select("div.desc p").text()
+        description = buildString {
+            append(element.select("div.excerpt p").text())
+            append("\n\n")
+            append(element.select("div.desc p").text())
+        }
         genre = element.select("ul.category-listing li a").joinToString { it.text() }
-        thumbnail_url = element.selectFirst("div.image picture img")?.absUrl("src")
+        thumbnail_url = element.selectFirst("div.image picture img")?.attr("abs:src")
     }
 
-    override fun searchMangaNextPageSelector() = "ul.pagination li.next-page:not(.disabled)"
-
-    override fun mangaDetailsParse(document: Document): SManga {
-        val data = json.decodeFromString<ComikeyComic>(
-            document.selectFirst("script#comic")!!.data(),
-        )
+    override fun mangaDetailsParse(response: Response): SManga {
+        val document = response.asJsoup()
+        val data = document.selectFirst("script#comic")!!.data().parseAs<ComikeyComic>()
 
         return SManga.create().apply {
             url = data.link
             title = data.name
             author = data.author.joinToString { it.name }
             artist = data.artist.joinToString { it.name }
-            description = "\"${data.excerpt}\"\n\n${data.description}"
+            description = buildString {
+                append("\"")
+                append(data.excerpt)
+                append("\"\n\n")
+                append(data.description)
+            }
             thumbnail_url = "$baseUrl${data.fullCover}"
             status = when (data.updateStatus) {
                 // HACK: Comikey Brasil
@@ -168,7 +164,7 @@ open class Comikey(
 
                 3 -> SManga.ON_HIATUS
 
-                in (4..14) -> SManga.ONGOING
+                in 4..14 -> SManga.ONGOING
 
                 // daily, weekly, bi-weekly, monthly, every day of the week
                 else -> SManga.UNKNOWN
@@ -189,9 +185,7 @@ open class Comikey(
     override fun chapterListParse(response: Response): List<SChapter> {
         val document = response.asJsoup()
         val mangaSlug = response.request.url.pathSegments[1]
-        val mangaData = json.decodeFromString<ComikeyComic>(
-            document.selectFirst("script#comic")!!.data(),
-        )
+        val mangaData = document.selectFirst("script#comic")!!.data().parseAs<ComikeyComic>()
         val defaultChapterPrefix = if (mangaData.format == 2) "episode" else "chapter"
 
         val chapterUrl = gundamUrl.toHttpUrl().newBuilder().apply {
@@ -212,12 +206,8 @@ open class Comikey(
             addQueryParameter("language", lang.lowercase())
             gundamToken?.let { addQueryParameter("token", gundamToken) }
         }.build()
-        val data = json.decodeFromString<ComikeyEpisodeListResponse>(
-            client.newCall(GET(chapterUrl, headers))
-                .execute()
-                .body
-                .string(),
-        )
+
+        val data = client.newCall(GET(chapterUrl, headers)).execute().parseAs<ComikeyEpisodeListResponse>()
         val currentTime = System.currentTimeMillis()
 
         return data.episodes
@@ -234,26 +224,18 @@ open class Comikey(
                         }
                     }
                     chapter_number = it.number
-                    date_upload = try {
-                        dateFormat.parse(it.releasedAt)!!.time
-                    } catch (e: Exception) {
-                        0L
-                    }
+                    date_upload = dateFormat.tryParse(it.releasedAt)
                 }
             }
             .filter { it.date_upload <= currentTime }
             .reversed()
     }
 
-    override fun chapterListSelector() = throw UnsupportedOperationException()
-
-    override fun chapterFromElement(element: Element) = throw UnsupportedOperationException()
-
     override fun fetchPageList(chapter: SChapter): Observable<List<Page>> = Observable.fromCallable {
         pageListParse(chapter)
     }
 
-    override fun pageListParse(document: Document) = throw UnsupportedOperationException()
+    override fun pageListParse(response: Response): List<Page> = throw UnsupportedOperationException()
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun pageListParse(chapter: SChapter): List<Page> {
@@ -261,7 +243,7 @@ open class Comikey(
 
         val handler = Handler(Looper.getMainLooper())
         val latch = CountDownLatch(1)
-        val jsInterface = JsInterface(latch, json, intl)
+        val jsInterface = JsInterface(latch, intl)
         var webView: WebView? = null
 
         handler.post {
@@ -275,36 +257,12 @@ open class Comikey(
             innerWv.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
             innerWv.addJavascriptInterface(jsInterface, interfaceName)
 
-            // Somewhat useful if you need to debug WebView issues. Don't delete.
-            //
-            /*innerWv.webChromeClient = object : WebChromeClient() {
-                override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
-                    if (consoleMessage == null) { return false }
-                    val logContent = "wv: ${consoleMessage.message()} (${consoleMessage.sourceId()}, line ${consoleMessage.lineNumber()})"
-                    when (consoleMessage.messageLevel()) {
-                        ConsoleMessage.MessageLevel.DEBUG -> Log.d("comikey", logContent)
-                        ConsoleMessage.MessageLevel.ERROR -> Log.e("comikey", logContent)
-                        ConsoleMessage.MessageLevel.LOG -> Log.i("comikey", logContent)
-                        ConsoleMessage.MessageLevel.TIP -> Log.i("comikey", logContent)
-                        ConsoleMessage.MessageLevel.WARNING -> Log.w("comikey", logContent)
-                        else -> Log.d("comikey", logContent)
-                    }
-
-                    return true
-                }
-            }*/
-
             innerWv.webViewClient = object : WebViewClient() {
                 override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                     super.onPageStarted(view, url, favicon)
                     view?.evaluateJavascript(webviewScript.replace("__interface__", interfaceName)) {}
                 }
 
-                // If you're logged in, the manifest URL sent to the client is not a direct link;
-                // it only redirects to the real one when you call it.
-                //
-                // In order to avoid a later call and remove an avenue for sniffing out users,
-                // we intercept said request so we can grab the real manifest URL.
                 override fun shouldInterceptRequest(
                     view: WebView?,
                     request: WebResourceRequest?,
@@ -312,7 +270,7 @@ open class Comikey(
                     val url = request?.url
                         ?: return super.shouldInterceptRequest(view, request)
 
-                    if (url.host?.matches(Regex("""relay-\w+\.epub\.rocks""")) != true || url.path?.endsWith("/manifest") != true) {
+                    if (url.host?.matches(RELAY_HOST_REGEX) != true || url.path?.endsWith("/manifest") != true) {
                         return super.shouldInterceptRequest(view, request)
                     }
 
@@ -368,10 +326,10 @@ open class Comikey(
 
                 if (it.alternate.isNotEmpty() && it.height == 2048 && it.type == "image/jpeg") {
                     addPathSegments(
-                        it.alternate.first {
-                            val dimension = if (webtoon) it.width else it.height
+                        it.alternate.first { alt ->
+                            val dimension = if (webtoon) alt.width else alt.height
 
-                            dimension <= 1536 && it.type == "image/webp"
+                            dimension <= 1536 && alt.type == "image/webp"
                         }.href,
                     )
                 } else {
@@ -385,7 +343,7 @@ open class Comikey(
         }
     }
 
-    override fun imageUrlParse(document: Document) = throw UnsupportedOperationException()
+    override fun imageUrlParse(response: Response): String = throw UnsupportedOperationException()
 
     override fun getFilterList() = getComikeyFilters(intl)
 
@@ -442,7 +400,6 @@ open class Comikey(
 
     private class JsInterface(
         private val latch: CountDownLatch,
-        private val json: Json,
         private val intl: Intl,
     ) {
         var manifest: ComikeyEpisodeManifest? = null
@@ -475,7 +432,7 @@ open class Comikey(
             }
 
             this.act = act
-            manifest = json.decodeFromString<ComikeyEpisodeManifest>(rawData)
+            manifest = rawData.parseAs<ComikeyEpisodeManifest>()
 
             latch.countDown()
         }
@@ -484,5 +441,6 @@ open class Comikey(
     companion object {
         internal const val PREFIX_SLUG_SEARCH = "slug:"
         internal const val PREF_HIDE_LOCKED_CHAPTERS = "hide_locked_chapters"
+        private val RELAY_HOST_REGEX = Regex("""relay-\w+\.epub\.rocks""")
     }
 }
