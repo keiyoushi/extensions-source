8840ae29973a18c7ea3da966520689a0e6492e94
darwin-256 <viktorasordinass@gmail.com>
2026-04-28 13:03:51 +0300

    Update YellowNote.kt

--- src/all/yellownote/src/eu/kanade/tachiyomi/extension/all/yellownote/YellowNote.kt
+++ src/all/yellownote/src/eu/kanade/tachiyomi/extension/all/yellownote/YellowNote.kt
@@ -1,18 +1,19 @@
 package eu.kanade.tachiyomi.extension.all.yellownote
 
 import androidx.preference.PreferenceScreen
-import eu.kanade.tachiyomi.extension.all.yellownote.YellowNoteFilters.SortSelector
-import eu.kanade.tachiyomi.extension.all.yellownote.YellowNotePreferences.baseUrl
-import eu.kanade.tachiyomi.extension.all.yellownote.YellowNotePreferences.language
-import eu.kanade.tachiyomi.extension.all.yellownote.YellowNotePreferences.preferenceMigration
+import eu.kanade.tachiyomi.extension.all.yellownote.Preferences.baseUrl
+import eu.kanade.tachiyomi.extension.all.yellownote.Preferences.language
+import eu.kanade.tachiyomi.extension.all.yellownote.Preferences.preferenceMigration
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.model.UpdateStrategy
+import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.lib.i18n.Intl
 import keiyoushi.utils.firstInstance
@@ -27,7 +28,7 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 class YellowNote :
-    SimpleParsedHttpSource(),
+    HttpSource(),
     ConfigurableSource {
 
     override val id get() = 170542391855030753
@@ -64,61 +65,89 @@ class YellowNote :
     // 100P + 2V
     private val mediaCountRegex = """\d+P( \+ \d+V)?""".toRegex()
 
+    private val mangaSelector = "div.list.photo-list > div.item.photo, div.list.amateur-list > div.item.amateur"
+    private val nextPageSelector = "div.pager:first-of-type > a.pager-next"
+    private val imageSelector = "div.list.photo-items > div.item.photo-image, div.list.amateur-items > div.item.amateur-image"
+
     override fun setupPreferenceScreen(screen: PreferenceScreen) {
-        YellowNotePreferences.buildPreferences(screen.context, intl)
+        Preferences.buildPreferences(screen.context, intl)
             .forEach(screen::addPreference)
     }
 
-    override fun simpleMangaSelector() = "div.list.photo-list > div.item.photo, div.list.amateur-list > div.item.amateur"
+    private fun parseMangaList(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select(mangaSelector).map { element ->
+            SManga.create().apply {
+                val mangaEl = element.selectFirst("a")!!
+                setUrlWithoutDomain(mangaEl.absUrl("href"))
 
-    override fun simpleMangaFromElement(element: Element) = SManga.create().apply {
-        val mangaEl = element.selectFirst("a")!!
-        setUrlWithoutDomain(mangaEl.absUrl("href"))
+                val formatMediaCount = element.select("div.tags > div")
+                    .map { it.text() }
+                    .firstOrNull { mediaCountRegex.matches(it) }
+                    ?.let { "($it)" }
+                    .orEmpty()
+                title = "${mangaEl.attr("title")}$formatMediaCount"
 
-        val formatMediaCount = element.select("div.tags > div")
-            .map { it.text() }
-            .firstOrNull { mediaCountRegex.matches(it) }
-            ?.let { "($it)" }
-            .orEmpty()
-        title = "${mangaEl.attr("title")}$formatMediaCount"
-
-        thumbnail_url = parseUrlFormStyle(mangaEl.selectFirst("div.img"))
-
-        update_strategy = UpdateStrategy.ONLY_FETCH_ONCE
+                thumbnail_url = parseUrlFormStyle(mangaEl.selectFirst("div.img"))
+                update_strategy = UpdateStrategy.ONLY_FETCH_ONCE
+            }
+        }
+        val hasNextPage = document.selectFirst(nextPageSelector) != null
+        return MangasPage(mangas, hasNextPage)
     }
 
-    fun parseUrlFormStyle(element: Element?): String? = element
+    private fun parseUrlFormStyle(element: Element?): String? = element
         ?.attr("style")
         ?.let { styleUrlRegex.find(it) }
         ?.groupValues
         ?.get(1)
 
-    override fun simpleNextPageSelector() = "div.pager:first-of-type > a.pager-next"
-
     override fun popularMangaRequest(page: Int) = GET("$baseUrl/photos/sort-hot/$page.html", headers)
+    override fun popularMangaParse(response: Response): MangasPage = parseMangaList(response)
 
     override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/photos/$page.html", headers)
+    override fun latestUpdatesParse(response: Response): MangasPage = parseMangaList(response)
 
-    override fun mangaDetailsParse(document: Document) = SManga.create().apply {
-        val infoCardElement = document.selectFirst("div.info-card.photo-detail")!!
-        val name = parseInfoByIcon(infoCardElement, "i.fa-address-card")!!
-        val mediaCount = parseInfoByIcon(infoCardElement, "i.fa-image")!!
-        val no = parseInfoByIcon(infoCardElement, "i.fa-file")?.let { " $it" }.orEmpty()
-        val categories =
-            parseInfosByIcon(infoCardElement, "i.fa-video-camera")?.filter { it != "-" }
-        val filters = parseInfosByIcon(infoCardElement, "i.fa-filter")
-        val tags = parseInfosByIcon(infoCardElement, "i.fa-tags")
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val categorySelector = filters.firstInstance<CategorySelector>()
+        val sortSelector = filters.firstInstance<SortSelector>()
+        val uriPart = when {
+            query.isBlank() -> categorySelector.toUriPart()
+            else -> "photos/keyword-$query"
+        }
 
-        title = "$name$no($mediaCount)"
-        author = infoCardElement.selectFirst("div.item.floating")
-            ?.text()
-            ?: parseInfoByIcon(infoCardElement, "i.fa-circle-user")
-        genre = listOfNotNull(categories, filters, tags)
-            .flatten()
-            .takeIf { it.isNotEmpty() }
-            ?.joinToString(", ")
-        status = SManga.COMPLETED
-        update_strategy = UpdateStrategy.ONLY_FETCH_ONCE
+        val httpUrl = baseUrl.toHttpUrl().newBuilder()
+            .addPathSegments(uriPart)
+            .addPathSegment(sortSelector.toUriPart())
+            .addPathSegments("$page.html")
+            .build()
+        return GET(httpUrl, headers)
+    }
+    override fun searchMangaParse(response: Response): MangasPage = parseMangaList(response)
+
+    override fun mangaDetailsParse(response: Response): SManga {
+        val document = response.asJsoup()
+        return SManga.create().apply {
+            val infoCardElement = document.selectFirst("div.info-card.photo-detail")!!
+            val name = parseInfoByIcon(infoCardElement, "i.fa-address-card")!!
+            val mediaCount = parseInfoByIcon(infoCardElement, "i.fa-image")!!
+            val no = parseInfoByIcon(infoCardElement, "i.fa-file")?.let { " $it" }.orEmpty()
+            val categories =
+                parseInfosByIcon(infoCardElement, "i.fa-video-camera")?.filter { it != "-" }
+            val filters = parseInfosByIcon(infoCardElement, "i.fa-filter")
+            val tags = parseInfosByIcon(infoCardElement, "i.fa-tags")
+
+            title = "$name$no($mediaCount)"
+            author = infoCardElement.selectFirst("div.item.floating")
+                ?.text()
+                ?: parseInfoByIcon(infoCardElement, "i.fa-circle-user")
+            genre = listOfNotNull(categories, filters, tags)
+                .flatten()
+                .takeIf { it.isNotEmpty() }
+                ?.joinToString(", ")
+            status = SManga.COMPLETED
+            update_strategy = UpdateStrategy.ONLY_FETCH_ONCE
+        }
     }
 
     private fun parseInfosByIcon(infoCardElement: Element, iconClass: String): List<String>? = infoCardElement
@@ -132,7 +161,6 @@ class YellowNote :
         ?.selectFirst("div.text")
         ?.text()
 
-    override fun chapterFromElement(element: Element) = throw UnsupportedOperationException()
     override fun chapterListParse(response: Response): List<SChapter> {
         val doc = response.asJsoup()
         val infoCardElement = doc.selectFirst("div.info-card.photo-detail")!!
@@ -160,35 +188,21 @@ class YellowNote :
         return null
     }
 
-    private val imageSelector =
-        "div.list.photo-items > div.item.photo-image, div.list.amateur-items > div.item.amateur-image"
-
-    override fun pageListParse(document: Document): List<Page> = document.select(imageSelector)
-        .mapIndexed { i, imageElement ->
-            val url = parseUrlFormStyle(imageElement.selectFirst("div.img"))!!
-            Page(i, imageUrl = url)
-        }
-
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val categorySelector = filters.firstInstance<YellowNoteFilters.CategorySelector>()
-        val sortSelector = filters.firstInstance<SortSelector>()
-        val uriPart = when {
-            query.isBlank() -> categorySelector.toUriPart()
-            else -> "photos/keyword-$query"
-        }
-
-        val httpUrl = baseUrl.toHttpUrl().newBuilder()
-            .addPathSegments(uriPart)
-            .addPathSegment(sortSelector.toUriPart())
-            .addPathSegments("$page.html")
-            .build()
-        return GET(httpUrl, headers)
+    override fun pageListParse(response: Response): List<Page> {
+        val document = response.asJsoup()
+        return document.select(imageSelector)
+            .mapIndexed { i, imageElement ->
+                val url = parseUrlFormStyle(imageElement.selectFirst("div.img"))!!
+                Page(i, imageUrl = url)
+            }
     }
 
+    override fun imageUrlParse(response: Response): String = throw UnsupportedOperationException()
+
     override fun getFilterList() = FilterList(
-        YellowNoteFilters.createSortSelector(intl),
+        Filters.createSortSelector(intl),
         Filter.Separator(),
         Filter.Header(intl["filter.header.ignored-when-search"]),
-        YellowNoteFilters.createCategorySelector(intl),
+        Filters.createCategorySelector(intl),
     )
 }
