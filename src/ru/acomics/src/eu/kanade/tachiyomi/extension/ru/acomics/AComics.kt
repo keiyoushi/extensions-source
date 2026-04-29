3f53e4e1e9de7bf59db61f7cac3d3901c5266678
darwin-256 <viktorasordinass@gmail.com>
2026-04-29 18:09:18 +0300

    Update AComics.kt

--- src/ru/acomics/src/eu/kanade/tachiyomi/extension/ru/acomics/AComics.kt
+++ src/ru/acomics/src/eu/kanade/tachiyomi/extension/ru/acomics/AComics.kt
@@ -3,19 +3,18 @@ package eu.kanade.tachiyomi.extension.ru.acomics
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
 
-class AComics : ParsedHttpSource() {
+class AComics : HttpSource() {
 
     override val name = "AComics"
 
@@ -39,25 +38,25 @@ class AComics : ParsedHttpSource() {
     // ============================== Popular ===============================
     override fun popularMangaRequest(page: Int): Request = searchMangaRequest(page, "", Sort.POPULAR)
 
-    override fun popularMangaSelector() = "section.serial-card"
-
-    override fun popularMangaFromElement(element: Element) = SManga.create().apply {
-        thumbnail_url = element.selectFirst("a > img")?.absUrl("data-real-src")
-        element.selectFirst("h2 > a")!!.run {
-            setUrlWithoutDomain(attr("href") + "/about")
-            title = text()
+    override fun popularMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select("section.serial-card").map { element ->
+            SManga.create().apply {
+                thumbnail_url = element.selectFirst("a > img")?.absUrl("data-real-src")
+                element.selectFirst("h2 > a")!!.run {
+                    setUrlWithoutDomain(attr("href") + "/about")
+                    title = text()
+                }
+            }
         }
+        val hasNextPage = document.selectFirst("a.infinite-scroll") != null
+        return MangasPage(mangas, hasNextPage)
     }
-
-    override fun popularMangaNextPageSelector() = "a.infinite-scroll"
 
     // =============================== Latest ===============================
     override fun latestUpdatesRequest(page: Int): Request = searchMangaRequest(page, "", Sort.LATEST)
 
-    override fun latestUpdatesSelector() = popularMangaSelector()
-
-    override fun latestUpdatesFromElement(element: Element) = popularMangaFromElement(element)
-    override fun latestUpdatesNextPageSelector() = popularMangaNextPageSelector()
+    override fun latestUpdatesParse(response: Response): MangasPage = popularMangaParse(response)
 
     // =============================== Search ===============================
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
@@ -94,8 +93,6 @@ class AComics : ParsedHttpSource() {
                             .map { it.toString() }
                         ratings.addAll(selected)
                     }
-
-                    // ---
 
                     is ComicType -> {
                         comicType = when (filter.state) {
@@ -159,7 +156,7 @@ class AComics : ParsedHttpSource() {
         return GET(urlBuilder.build(), headers)
     }
 
-    fun HttpUrl.Builder.addIndexedQueryParameters(
+    private fun HttpUrl.Builder.addIndexedQueryParameters(
         name: String,
         values: Iterable<String?>,
         collapse: Boolean,
@@ -170,20 +167,19 @@ class AComics : ParsedHttpSource() {
         }
     }
 
-    override fun searchMangaSelector() = popularMangaSelector()
-
-    override fun searchMangaFromElement(element: Element) = popularMangaFromElement(element)
-
-    override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
+    override fun searchMangaParse(response: Response): MangasPage = popularMangaParse(response)
 
     // =========================== Manga Details ============================
-    override fun mangaDetailsParse(document: Document) = SManga.create().apply {
-        val article = document.selectFirst("article.common-article")!!
-        with(article) {
-            title = selectFirst(".page-header-with-menu h1")!!.text()
-            genre = select("p.serial-about-badges a.category").joinToString { it.text() }
-            author = select("p.serial-about-authors a, p:contains(Автор оригинала)").joinToString { it.ownText() }
-            description = selectFirst("section.serial-about-text")?.text()
+    override fun mangaDetailsParse(response: Response): SManga {
+        val document = response.asJsoup()
+        return SManga.create().apply {
+            val article = document.selectFirst("article.common-article")!!
+            with(article) {
+                title = selectFirst(".page-header-with-menu h1")!!.text()
+                genre = select("p.serial-about-badges a.category").joinToString { it.text() }
+                author = select("p.serial-about-authors a, p:contains(Автор оригинала)").joinToString { it.ownText() }
+                description = selectFirst("section.serial-about-text")?.text()
+            }
         }
     }
 
@@ -206,68 +202,16 @@ class AComics : ParsedHttpSource() {
         }
     }
 
-    override fun chapterListSelector(): Nothing = throw UnsupportedOperationException()
-
-    override fun chapterFromElement(element: Element): SChapter = throw UnsupportedOperationException()
-
     // =============================== Pages ================================
-    override fun pageListParse(document: Document): List<Page> {
+    override fun pageListParse(response: Response): List<Page> {
+        val document = response.asJsoup()
         val imageElement = document.selectFirst("img.issue")!!
         return listOf(Page(0, imageUrl = imageElement.absUrl("src")))
     }
 
-    override fun imageUrlParse(document: Document) = throw UnsupportedOperationException()
+    override fun imageUrlParse(response: Response): String = throw UnsupportedOperationException()
 
     // ============================== Filters ===============================
-    private class Genre(name: String, val id: Int) : Filter.CheckBox(name)
-    private class Rating(name: String, val id: Int) : Filter.CheckBox(name, state = true)
-
-    private class Categories :
-        Filter.Group<Genre>(
-            "Категории",
-            listOf(
-                Genre("Животные", 1),
-                Genre("Драма", 2),
-                Genre("Фентези", 3),
-                Genre("Игры", 4),
-                Genre("Юмор", 5),
-                Genre("Журнал", 6),
-                Genre("Паранормальное", 7),
-                Genre("Конец света", 8),
-                Genre("Романтика", 9),
-                Genre("Фантастика", 10),
-                Genre("Бытовое", 11),
-                Genre("Стимпанк", 12),
-                Genre("Супергерои", 13),
-                Genre("Детектив", 14),
-                Genre("Историческое", 15),
-            ),
-        )
-
-    private class Ratings :
-        Filter.Group<Rating>(
-            "Возрастная категория",
-            listOf(
-                Rating("NR", 1),
-                Rating("G", 2),
-                Rating("PG", 3),
-                Rating("PG-13", 4),
-                Rating("R", 5),
-                Rating("NC-17", 6),
-            ),
-        )
-
-    private class ComicType : Filter.Select<String>("Тип комикса", arrayOf("Все", "Оригинальный", "Перевод")) // "0", "orig", "trans"
-    private class Publication : Filter.Select<String>("Публикация", arrayOf("Все", "Завершенный", "Продолжающийся")) // "0", "no", "yes"
-    private class Subscription : Filter.Select<String>("Подписка", arrayOf("Все", "В моей ленте", "Кроме моей ленты")) // "0", "yes", "no"
-    private class MinPages : Filter.Text("Минимум страниц", state = "2")
-    private class Sort(state: Int = 1) : Filter.Select<String>("Сортировка", arrayOf("по дате обновления", "по количеству подписчиков", "по количеству выпусков", "по алфавиту"), state = state) { // "last_update", "subscr_count", "issue_count", "serial_name"
-        companion object {
-            val LATEST = FilterList(Ratings(), Sort(0))
-            val POPULAR = FilterList(Ratings(), Sort(1))
-        }
-    }
-
     override fun getFilterList() = FilterList(
         Categories(),
         Ratings(),
