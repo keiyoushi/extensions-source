393a9bf3c8f1134fa76bf0d270333c5e5f4834a9
darwin-256 <viktorasordinass@gmail.com>
2026-04-29 19:25:30 +0300

    Update MangaKuro.kt

--- src/ja/mangakuro/src/eu/kanade/tachiyomi/extension/ja/mangakuro/MangaKuro.kt
+++ src/ja/mangakuro/src/eu/kanade/tachiyomi/extension/ja/mangakuro/MangaKuro.kt
@@ -2,16 +2,17 @@ package eu.kanade.tachiyomi.extension.ja.mangakuro
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.source.online.HttpSource
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
+import okhttp3.Response
 
-class MangaKuro : ParsedHttpSource() {
+class MangaKuro : HttpSource() {
 
     override val name = "MangaKuro"
 
@@ -20,18 +21,6 @@ class MangaKuro : ParsedHttpSource() {
     override val lang = "ja"
 
     override val supportsLatest = true
-
-    override fun popularMangaSelector() = ".story_item"
-
-    override fun latestUpdatesSelector() = popularMangaSelector()
-
-    override fun searchMangaSelector() = popularMangaSelector()
-
-    override fun popularMangaNextPageSelector() = "a[title='Last Page']"
-
-    override fun latestUpdatesNextPageSelector() = popularMangaNextPageSelector()
-
-    override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
 
     private fun mangaRequestBuilder(type: String, page: Int, key: String, value: String) = GET(
         baseUrl.toHttpUrl().newBuilder().apply {
@@ -44,55 +33,69 @@ class MangaKuro : ParsedHttpSource() {
 
     override fun popularMangaRequest(page: Int): Request = mangaRequestBuilder("all-manga", page, "sort", "views")
 
+    override fun popularMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select(".story_item").mapNotNull { element ->
+            val a = element.selectFirst("a") ?: return@mapNotNull null
+            SManga.create().apply {
+                setUrlWithoutDomain(a.absUrl("href"))
+                title = element.selectFirst(".mg_name a")?.text().orEmpty()
+                thumbnail_url = element.selectFirst("img")?.absUrl("src")
+            }
+        }
+        val hasNextPage = document.selectFirst("a[title='Last Page']") != null
+        return MangasPage(mangas, hasNextPage)
+    }
+
     override fun latestUpdatesRequest(page: Int): Request = mangaRequestBuilder("all-manga", page, "sort", "latest-updated")
+
+    override fun latestUpdatesParse(response: Response): MangasPage = popularMangaParse(response)
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request = mangaRequestBuilder("search", page, "keyword", query)
 
-    override fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
-        setUrlWithoutDomain(element.selectFirst("a")!!.absUrl("href"))
-        title = element.selectFirst(".mg_name a")!!.text()
-        thumbnail_url = element.selectFirst("img")!!.absUrl("src")
-    }
+    override fun searchMangaParse(response: Response): MangasPage = popularMangaParse(response)
 
-    override fun latestUpdatesFromElement(element: Element): SManga = popularMangaFromElement(element)
-
-    override fun searchMangaFromElement(element: Element): SManga = popularMangaFromElement(element)
-
-    override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
-        author = document.selectFirst("div:has(.lnr-user) + .info_value")?.text()
-        status = document.selectFirst("div:has(.lnr-leaf) + .info_value")?.text()
-            .orEmpty().let { parseStatus(it) }
-        description = document.selectFirst(".detail_reviewContent")?.text()
-        thumbnail_url = document.selectFirst(".detail_avatar img")?.absUrl("src")
+    override fun mangaDetailsParse(response: Response): SManga {
+        val document = response.asJsoup()
+        return SManga.create().apply {
+            author = document.selectFirst("div:has(.lnr-user) + .info_value")?.text()
+            status = parseStatus(document.selectFirst("div:has(.lnr-leaf) + .info_value")?.text().orEmpty())
+            description = document.selectFirst(".detail_reviewContent")?.text()
+            thumbnail_url = document.selectFirst(".detail_avatar img")?.absUrl("src")
+        }
     }
 
     private fun parseStatus(status: String) = when {
         status.contains("進行中") -> SManga.ONGOING
-
-        // status.contains("Completed") -> SManga.COMPLETED / I only found OnGoing Titles and i have no idea what string they would use
         else -> SManga.UNKNOWN
     }
 
-    override fun chapterListSelector() = ".chapter_box .item"
-
-    override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
-        val a = element.selectFirst("a")!!
-        setUrlWithoutDomain(a.absUrl("href"))
-        name = a.text().substringAfter("# ")
-        // date_upload = no real date in web
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val document = response.asJsoup()
+        return document.select(".chapter_box .item").mapNotNull { element ->
+            val a = element.selectFirst("a") ?: return@mapNotNull null
+            SChapter.create().apply {
+                setUrlWithoutDomain(a.absUrl("href"))
+                name = a.text().substringAfter("# ")
+            }
+        }
     }
 
-    private val chapterIDRegex = """CHAPTER_ID = (\d+);""".toRegex()
-    private val imageRegex = """src=\\"([^"]+)\\""".toRegex()
+    override fun pageListParse(response: Response): List<Page> {
+        val html = response.body.string()
+        val id = chapterIDRegex.find(html)?.groupValues?.get(1) ?: return emptyList()
 
-    override fun pageListParse(document: Document): List<Page> {
-        val id = chapterIDRegex.find(document.outerHtml())?.groupValues?.get(1) ?: return listOf()
-
-        val response = client.newCall(GET("$baseUrl/ajax/image/list/chap/$id", headers)).execute()
-        return imageRegex.findAll(response.body.string()).mapIndexed { idx, img ->
-            Page(idx, imageUrl = img.groupValues[1])
-        }.toList()
+        return client.newCall(GET("$baseUrl/ajax/image/list/chap/$id", headers)).execute().use { apiResponse ->
+            imageRegex.findAll(apiResponse.body.string()).mapIndexed { idx, img ->
+                Page(idx, imageUrl = img.groupValues[1])
+            }.toList()
+        }
     }
 
-    override fun imageUrlParse(document: Document): String = ""
+    override fun imageUrlParse(response: Response): String = throw UnsupportedOperationException("Not used.")
+
+    companion object {
+        private val chapterIDRegex = """CHAPTER_ID\s*=\s*(\d+);""".toRegex()
+        private val imageRegex = """src=\\"([^"]+)\\"""".toRegex()
+    }
 }
