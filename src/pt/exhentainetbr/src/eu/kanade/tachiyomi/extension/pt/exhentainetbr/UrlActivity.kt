b09eae7e70b43a26b220fddfe46ae8e6d41e87ba
darwin-256 <viktorasordinass@gmail.com>
2026-04-29 03:02:33 +0300

    Update and rename ExHentaiNetBRUrlActivity.kt to UrlActivity.kt

--- src/pt/exhentainetbr/src/eu/kanade/tachiyomi/extension/pt/exhentainetbr/UrlActivity.kt
+++ src/pt/exhentainetbr/src/eu/kanade/tachiyomi/extension/pt/exhentainetbr/UrlActivity.kt
@@ -7,7 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import kotlin.system.exitProcess
 
-class ExHentaiNetBRUrlActivity : Activity() {
+class UrlActivity : Activity() {
 
     private val tag = javaClass.simpleName
 
