a1ecdd0b40b2f0c14259dc6f4e317b122b10d318
darwin-256 <viktorasordinass@gmail.com>
2026-04-30 20:53:17 +0300

    Refactor DigitalComicMuseum to use HttpSource

--- src/en/digitalcomicmuseum/src/eu/kanade/tachiyomi/extension/en/digitalcomicmuseum/DigitalComicMuseum.kt
+++ src/en/digitalcomicmuseum/src/eu/kanade/tachiyomi/extension/en/digitalcomicmuseum/DigitalComicMuseum.kt
@@ -3,10 +3,12 @@ package eu.kanade.tachiyomi.extension.en.digitalcomicmuseum
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.source.online.HttpSource
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Interceptor
@@ -15,10 +17,8 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import okhttp3.Response
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
 
-class DigitalComicMuseum : ParsedHttpSource() {
+class DigitalComicMuseum : HttpSource() {
     override val baseUrl = "https://digitalcomicmuseum.com"
     override val lang = "en"
     override val name = "Digital Comic Museum"
@@ -29,48 +29,49 @@ class DigitalComicMuseum : ParsedHttpSource() {
         .build()
 
     // Latest
-    override fun latestUpdatesFromElement(element: Element): SManga {
-        val manga = SManga.create()
-        manga.thumbnail_url = element.select("img").attr("abs:src")
-        manga.setUrlWithoutDomain(element.select("a").attr("abs:href"))
-        manga.title = element.select("a").text()
-        return manga
+
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/stats.php?ACT=latest&start=${page - 1}00&limit=100", headers)
+
+    override fun latestUpdatesParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select("tbody > .mainrow").map { element ->
+            SManga.create().apply {
+                thumbnail_url = element.selectFirst("img")?.attr("abs:src")
+                val link = element.selectFirst("a")!!
+                setUrlWithoutDomain(link.attr("abs:href"))
+                title = link.text()
+            }
+        }
+        val hasNextPage = document.selectFirst("img[alt=Next]") != null
+        return MangasPage(mangas, hasNextPage)
     }
-
-    override fun latestUpdatesNextPageSelector() = "img[alt=Next]"
-
-    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/stats.php?ACT=latest&start=${page - 1}00&limit=100")
-
-    override fun latestUpdatesSelector() = "tbody > .mainrow"
 
     // Popular
 
-    override fun popularMangaFromElement(element: Element) = latestUpdatesFromElement(element)
-    override fun popularMangaNextPageSelector() = latestUpdatesNextPageSelector()
-    override fun popularMangaSelector() = latestUpdatesSelector()
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/stats.php?ACT=topdl&start=${page - 1}00&limit=100", headers)
 
-    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/stats.php?ACT=topdl&start=${page - 1}00&limit=100")
-
-    // Search
-
-    override fun searchMangaFromElement(element: Element): SManga {
-        val manga = SManga.create()
-        val baseElement = element.selectFirst("td > a")!!
-        manga.setUrlWithoutDomain(baseElement.attr("abs:href"))
-        manga.title = baseElement.text()
-        return manga
+    override fun popularMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select("tbody > .mainrow").map { element ->
+            SManga.create().apply {
+                thumbnail_url = element.selectFirst("img")?.attr("abs:src")
+                val link = element.selectFirst("a")!!
+                setUrlWithoutDomain(link.attr("abs:href"))
+                title = link.text()
+            }
+        }
+        val hasNextPage = document.selectFirst("img[alt=Next]") != null
+        return MangasPage(mangas, hasNextPage)
     }
 
-    override fun searchMangaNextPageSelector() = "Not supported"
-    override fun searchMangaSelector() = "#search-results tbody > tr"
+    // Search
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         val requestBody: RequestBody = MultipartBody.Builder()
             .setType(MultipartBody.FORM)
             .addFormDataPart("terms", query)
             .build()
-        val requestHeaders: Headers = Headers.Builder()
-            .addAll(headers)
+        val requestHeaders: Headers = headers.newBuilder()
             .add("Content-Type", "multipart/form-data")
             .build()
         val url = "$baseUrl/index.php".toHttpUrl().newBuilder()
@@ -79,17 +80,33 @@ class DigitalComicMuseum : ParsedHttpSource() {
         return POST(url.toString(), requestHeaders, requestBody)
     }
 
+    override fun searchMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select("#search-results tbody > tr").map { element ->
+            SManga.create().apply {
+                val baseElement = element.selectFirst("td > a")!!
+                setUrlWithoutDomain(baseElement.attr("abs:href"))
+                title = baseElement.text()
+            }
+        }
+        return MangasPage(mangas, false)
+    }
+
     // Details
 
-    override fun mangaDetailsParse(document: Document): SManga {
+    override fun mangaDetailsParse(response: Response): SManga {
+        val document = response.asJsoup()
         val manga = SManga.create()
         val elements = document.select(".tableborder")
-        manga.title = elements.first()!!.select("#catname").text()
-        manga.setUrlWithoutDomain(elements.first()!!.select("#catname > a").attr("abs:href"))
-        manga.thumbnail_url = elements.first()!!.selectFirst("table img")!!.attr("abs:src")
+        val firstElement = elements.first() ?: return manga
+
+        manga.title = firstElement.select("#catname").text()
+        manga.setUrlWithoutDomain(firstElement.selectFirst("#catname > a")!!.attr("abs:href"))
+        manga.thumbnail_url = firstElement.selectFirst("table img")?.attr("abs:src")
+
         elements.forEach {
-            when (it.select("#catname").text()) {
-                "Description" -> manga.description = it.selectFirst("table")!!.text()
+            when (it.selectFirst("#catname")?.text()) {
+                "Description" -> manga.description = it.selectFirst("table")?.text()
             }
         }
         return manga
@@ -97,31 +114,37 @@ class DigitalComicMuseum : ParsedHttpSource() {
 
     // Chapters
 
-    override fun chapterFromElement(element: Element): SChapter {
-        val chapter = SChapter.create()
-        chapter.name = element.select("#catname").text()
-        chapter.setUrlWithoutDomain(element.select(".tablefooter a:first-of-type").attr("abs:href"))
-        return chapter
-    }
-
-    override fun chapterListSelector() = ".tableborder:first-of-type"
-
-    override fun pageListParse(document: Document): List<Page> {
-        val pages = mutableListOf<Page>()
-        document.select(".latest-slide > .slick-slide > a").forEachIndexed { index, element ->
-            pages.add(Page(index, element.attr("abs:href")))
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val document = response.asJsoup()
+        return document.select(".tableborder:first-of-type").map { element ->
+            SChapter.create().apply {
+                name = element.select("#catname").text()
+                setUrlWithoutDomain(element.selectFirst(".tablefooter a:first-of-type")!!.attr("abs:href"))
+            }
         }
-        return pages
     }
 
-    override fun imageUrlParse(document: Document): String = document.select("body > a:nth-of-type(2) > img").attr("src")
+    // Pages
+
+    override fun pageListParse(response: Response): List<Page> {
+        val document = response.asJsoup()
+        return document.select(".latest-slide > .slick-slide > a").mapIndexed { index, element ->
+            Page(index, url = element.attr("abs:href"))
+        }
+    }
+
+    override fun imageUrlParse(response: Response): String {
+        val document = response.asJsoup()
+        return document.selectFirst("body > a:nth-of-type(2) > img")?.attr("abs:src") ?: ""
+    }
 
     // Interceptor
+
     private fun errorIntercept(chain: Interceptor.Chain): Response {
-        val response: Response = chain.proceed(chain.request())
+        val response = chain.proceed(chain.request())
         if (response.code == 403) {
-            val newRequest = response.request
-            return client.newCall(newRequest).execute()
+            response.close()
+            return client.newCall(response.request).execute()
         }
         return response
     }
