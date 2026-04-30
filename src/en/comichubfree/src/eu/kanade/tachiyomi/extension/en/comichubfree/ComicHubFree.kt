da574e0815c1aea12a80db85cdf2a5c89a220e05
darwin-256 <viktorasordinass@gmail.com>
2026-04-30 20:41:57 +0300

    Refactor ComicHubFree to extend HttpSource

--- src/en/comichubfree/src/eu/kanade/tachiyomi/extension/en/comichubfree/ComicHubFree.kt
+++ src/en/comichubfree/src/eu/kanade/tachiyomi/extension/en/comichubfree/ComicHubFree.kt
@@ -2,21 +2,21 @@ package eu.kanade.tachiyomi.extension.en.comichubfree
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.utils.tryParse
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class ComicHubFree : ParsedHttpSource() {
+class ComicHubFree : HttpSource() {
     private val dateFormat = SimpleDateFormat("d-MMM-yyyy", Locale.getDefault())
 
     override val baseUrl = "https://comichubfree.com"
@@ -29,48 +29,28 @@ class ComicHubFree : ParsedHttpSource() {
 
     override val client = network.cloudflareClient
 
-    override fun popularMangaSelector() = ".movie-list-index > .cartoon-box:has(.detail)"
-
-    override fun latestUpdatesSelector() = popularMangaSelector()
-
-    override fun searchMangaSelector() = popularMangaSelector()
-
-    override fun popularMangaNextPageSelector() = "ul.pagination a[rel=next]:not(hidden)"
-
-    override fun latestUpdatesNextPageSelector() = popularMangaNextPageSelector()
-
-    override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
-
-    override fun chapterListSelector() = "div.episode-list > div > table > tbody > tr"
-
-    override fun chapterListParse(response: Response): List<SChapter> {
-        val chapters = mutableListOf<SChapter>()
-
-        fun parseChapters(document: Document) {
-            document.select(chapterListSelector()).map { chapters.add(chapterFromElement(it)) }
-            document.selectFirst(popularMangaNextPageSelector())?.let { it ->
-                parseChapters(client.newCall(GET(it.absUrl("href"), headers)).execute().asJsoup())
-            }
-        }
-
-        parseChapters(response.asJsoup())
-
-        return chapters
-    }
-
-    override fun imageUrlParse(document: Document) = ""
-
-    private fun Element.imageAttr(): String = when {
-        hasAttr("data-src") -> absUrl("data-src")
-        else -> absUrl("src")
-    }
-
     override fun popularMangaRequest(page: Int): Request {
         val url = "$baseUrl/popular-comic".toHttpUrl().newBuilder().apply {
             addQueryParameter("page", page.toString())
         }.build()
 
         return GET(url, headers)
+    }
+
+    override fun popularMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+
+        val mangas = document.select(".movie-list-index > .cartoon-box:has(.detail)").map { element ->
+            SManga.create().apply {
+                setUrlWithoutDomain(element.selectFirst("a")!!.absUrl("href"))
+                title = element.selectFirst("h3")!!.text()
+                thumbnail_url = element.selectFirst("img")?.imageAttr()
+            }
+        }
+
+        val hasNextPage = document.selectFirst("ul.pagination a[rel=next]:not(hidden)") != null
+
+        return MangasPage(mangas, hasNextPage)
     }
 
     override fun latestUpdatesRequest(page: Int): Request {
@@ -81,6 +61,8 @@ class ComicHubFree : ParsedHttpSource() {
         return GET(url, headers)
     }
 
+    override fun latestUpdatesParse(response: Response): MangasPage = popularMangaParse(response)
+
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         val url = "$baseUrl/search-comic".toHttpUrl().newBuilder().apply {
             addQueryParameter("key", query)
@@ -90,44 +72,37 @@ class ComicHubFree : ParsedHttpSource() {
         return GET(url, headers)
     }
 
-    override fun pageListRequest(chapter: SChapter): Request {
-        val url = (baseUrl + chapter.url + "/all").toHttpUrl()
+    override fun searchMangaParse(response: Response): MangasPage = popularMangaParse(response)
 
-        return GET(url, headers)
-    }
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val chapters = mutableListOf<SChapter>()
+        var document = response.asJsoup()
 
-    override fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
-        setUrlWithoutDomain(element.selectFirst("a")!!.absUrl("href"))
-        title = element.selectFirst("h3")!!.text()
-        val image = element.selectFirst("img")
-        thumbnail_url = image?.imageAttr()
-    }
+        while (true) {
+            document.select("div.episode-list > div > table > tbody > tr").mapTo(chapters) { element ->
+                val urlElement = element.selectFirst("a")!!
+                val dateElement = element.select("td:last-of-type")
 
-    override fun latestUpdatesFromElement(element: Element): SManga = popularMangaFromElement(element)
+                SChapter.create().apply {
+                    setUrlWithoutDomain(urlElement.attr("abs:href"))
+                    name = urlElement.text()
+                    date_upload = dateFormat.tryParse(dateElement.text())
+                }
+            }
 
-    override fun searchMangaFromElement(element: Element): SManga = popularMangaFromElement(element)
-
-    override fun chapterFromElement(element: Element): SChapter {
-        val urlElement = element.selectFirst("a")!!
-        val dateElement = element.select("td:last-of-type")
-
-        val chapter = SChapter.create().apply {
-            setUrlWithoutDomain(urlElement.attr("href"))
-            name = urlElement.text()
-            date_upload = dateFormat.tryParse(dateElement.text())
+            val nextUrl = document.selectFirst("ul.pagination a[rel=next]:not(hidden)")?.absUrl("href")
+            if (nextUrl.isNullOrEmpty()) {
+                break
+            }
+            document = client.newCall(GET(nextUrl, headers)).execute().asJsoup()
         }
 
-        return chapter
+        return chapters
     }
 
-    private fun parseStatus(status: String): Int = when (status) {
-        "Ongoing" -> SManga.ONGOING
-        "Completed" -> SManga.COMPLETED
-        else -> SManga.UNKNOWN
-    }
-
-    override fun mangaDetailsParse(document: Document): SManga {
-        val infoElement = document.selectFirst("div.movie-info")!!
+    override fun mangaDetailsParse(response: Response): SManga {
+        val document = response.asJsoup()
+        val infoElement = document.selectFirst("div.movie-info") ?: return SManga.create()
         val seriesInfoElement = infoElement.selectFirst("div.series-info")
         val seriesDescriptionElement = infoElement.selectFirst("div#film-content")
 
@@ -136,18 +111,37 @@ class ComicHubFree : ParsedHttpSource() {
 
         val image = seriesInfoElement?.selectFirst("img")
 
-        val manga = SManga.create().apply {
+        return SManga.create().apply {
             description = seriesDescriptionElement?.text()
             thumbnail_url = image?.imageAttr()
             author = authorElement?.text()
-            status = statusElement?.text().orEmpty().let { parseStatus(it) }
+            status = parseStatus(statusElement?.text().orEmpty())
         }
-
-        return manga
     }
 
-    override fun pageListParse(document: Document): List<Page> = document.select("img.chapter_img").mapIndexed { index, element ->
-        val img = element.imageAttr()
-        Page(index, imageUrl = img)
-    }.distinctBy { it.imageUrl }
+    override fun pageListRequest(chapter: SChapter): Request {
+        val url = (baseUrl + chapter.url + "/all").toHttpUrl()
+
+        return GET(url, headers)
+    }
+
+    override fun pageListParse(response: Response): List<Page> {
+        val document = response.asJsoup()
+        return document.select("img.chapter_img").mapIndexed { index, element ->
+            Page(index, imageUrl = element.imageAttr())
+        }.distinctBy { it.imageUrl }
+    }
+
+    override fun imageUrlParse(response: Response): String = throw UnsupportedOperationException("Not used.")
+
+    private fun parseStatus(status: String): Int = when (status) {
+        "Ongoing" -> SManga.ONGOING
+        "Completed" -> SManga.COMPLETED
+        else -> SManga.UNKNOWN
+    }
+
+    private fun Element.imageAttr(): String = when {
+        hasAttr("data-src") -> absUrl("data-src")
+        else -> absUrl("src")
+    }
 }
