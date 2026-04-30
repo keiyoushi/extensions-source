4d59608be2f3bfe6ba56cca55e62bffbdf1f79de
darwin-256 <viktorasordinass@gmail.com>
2026-04-30 21:37:35 +0300

    Update Honkaiimpact.kt

--- src/en/honkaiimpact/src/eu/kanade/tachiyomi/extension/en/honkaiimpact/Honkaiimpact.kt
+++ src/en/honkaiimpact/src/eu/kanade/tachiyomi/extension/en/honkaiimpact/Honkaiimpact.kt
@@ -7,38 +7,23 @@ import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.float
-import kotlinx.serialization.json.int
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
+import keiyoushi.utils.parseAs
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
 import rx.Observable
-import uy.kohesive.injekt.injectLazy
-import java.text.SimpleDateFormat
-import java.util.Locale
 import java.util.concurrent.TimeUnit
 
-class Honkaiimpact : ParsedHttpSource() {
+class Honkaiimpact : HttpSource() {
 
     override val name = "Honkai Impact 3rd"
     override val baseUrl = "https://manga.honkaiimpact3.com"
     override val lang = "en"
     override val supportsLatest = false
 
-    private var searchQuery = ""
-    private val json: Json by injectLazy()
-
     override val client: OkHttpClient = network.cloudflareClient.newBuilder()
-        // .addInterceptor(filteringInterceptor)
         .connectTimeout(1, TimeUnit.MINUTES)
         .readTimeout(1, TimeUnit.MINUTES)
         .retryOnConnectionFailure(true)
@@ -46,75 +31,60 @@ class Honkaiimpact : ParsedHttpSource() {
         .build()
 
     // Popular
-    override fun popularMangaSelector() = "a[href*=book]"
-    override fun popularMangaNextPageSelector(): String? = null
-    override fun popularMangaRequest(page: Int) = GET("$baseUrl/book", headers) // .newBuilder().tag(String::class.java, searchQuery).build()
-    override fun popularMangaFromElement(element: Element) = mangaFromElement(element)
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/book", headers)
+    override fun popularMangaParse(response: Response): MangasPage {
+        val mangas = response.asJsoup().select("a[href*=book]").map { it.toSManga() }
+        return MangasPage(mangas, false)
+    }
 
     // Latest
-    override fun latestUpdatesSelector() = throw UnsupportedOperationException()
-    override fun latestUpdatesNextPageSelector(): String? = null
-    override fun latestUpdatesRequest(page: Int) = throw UnsupportedOperationException()
-    override fun latestUpdatesFromElement(element: Element) = throw UnsupportedOperationException()
+    override fun latestUpdatesRequest(page: Int): Request = throw UnsupportedOperationException()
+    override fun latestUpdatesParse(response: Response): MangasPage = throw UnsupportedOperationException()
 
     // Search
-    override fun searchMangaSelector() = "a[href*=book]"
-    override fun searchMangaNextPageSelector(): String? = null
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request = GET("$baseUrl/book", headers)
-    override fun searchMangaFromElement(element: Element) = mangaFromElement(element)
 
     override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> = client.newCall(searchMangaRequest(page, query, filters))
         .asObservableSuccess()
         .map { response ->
-            val document = response.asJsoup()
-            val mangas = document.select(searchMangaSelector()).map { element ->
-                searchMangaFromElement(element)
-            }.filter { manga ->
-                query.isEmpty() || manga.title.contains(query.trim(), ignoreCase = true)
-            }
-            val hasNextPage = searchMangaNextPageSelector()?.let { selector ->
-                document.selectFirst(selector)
-            } != null
-            MangasPage(mangas, hasNextPage)
+            val mangas = response.asJsoup()
+                .select("a[href*=book]")
+                .map { it.toSManga() }
+                .filter { manga ->
+                    query.isEmpty() || manga.title.contains(query.trim(), ignoreCase = true)
+                }
+            MangasPage(mangas, false)
         }
 
-    private fun mangaFromElement(element: Element): SManga {
-        val manga = SManga.create()
-        manga.setUrlWithoutDomain(element.attr("abs:href"))
-        manga.title = element.select(".container-title").text()
-        manga.thumbnail_url = element.select(".container-cover img").attr("abs:src")
-        return manga
+    override fun searchMangaParse(response: Response): MangasPage = throw UnsupportedOperationException()
+
+    // Manga details
+    override fun mangaDetailsRequest(manga: SManga): Request = GET(baseUrl + manga.url, headers)
+    override fun mangaDetailsParse(response: Response): SManga {
+        val document = response.asJsoup()
+        return SManga.create().apply {
+            thumbnail_url = document.select("img.cover").attr("abs:src")
+            description = document.select("div.detail_info1").text()
+            title = document.select("div.title").text()
+        }
     }
 
-    override fun mangaDetailsParse(document: Document): SManga {
-        val manga = SManga.create()
-        manga.thumbnail_url = document.select("img.cover").attr("abs:src")
-        manga.description = document.select("div.detail_info1").text().trim()
-        manga.title = document.select("div.title").text().trim()
-        return manga
+    // Chapter list
+    override fun chapterListRequest(manga: SManga): Request = GET(baseUrl + manga.url + "/get_chapter", headers)
+
+    override fun chapterListParse(response: Response): List<SChapter> = response.parseAs<List<Dto>>().map { it.toSChapter() }
+
+    // Page list
+    override fun pageListRequest(chapter: SChapter): Request = GET(baseUrl + chapter.url, headers)
+    override fun pageListParse(response: Response): List<Page> = response.asJsoup().select("img.lazy.comic_img").mapIndexed { i, el ->
+        Page(i, imageUrl = el.attr("data-original"))
     }
 
-    override fun chapterListSelector() = throw UnsupportedOperationException()
-    override fun chapterFromElement(element: Element) = throw UnsupportedOperationException()
-    override fun chapterListRequest(manga: SManga) = GET(baseUrl + manga.url + "/get_chapter", headers)
-    override fun chapterListParse(response: Response): List<SChapter> {
-        val jsonResult = json.parseToJsonElement(response.body.string()).jsonArray
+    override fun imageUrlParse(response: Response): String = throw UnsupportedOperationException()
 
-        return jsonResult.map { jsonEl -> createChapter(jsonEl.jsonObject) }
+    private fun org.jsoup.nodes.Element.toSManga() = SManga.create().apply {
+        setUrlWithoutDomain(attr("abs:href"))
+        title = select(".container-title").text()
+        thumbnail_url = select(".container-cover img").attr("abs:src")
     }
-
-    private fun createChapter(jsonObj: JsonObject) = SChapter.create().apply {
-        name = jsonObj["title"]!!.jsonPrimitive.content
-        url = "/book/${jsonObj["bookid"]!!.jsonPrimitive.int}/${jsonObj["chapterid"]!!.jsonPrimitive.int}"
-        date_upload = parseDate(jsonObj["timestamp"]!!.jsonPrimitive.content)
-        chapter_number = jsonObj["chapterid"]!!.jsonPrimitive.float
-    }
-
-    private fun parseDate(date: String): Long = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).parse(date)?.time ?: 0
-
-    override fun pageListParse(document: Document): List<Page> = document.select("img.lazy.comic_img").mapIndexed { i, el ->
-        Page(i, "", el.attr("data-original"))
-    }
-
-    override fun imageUrlParse(document: Document) = ""
 }
