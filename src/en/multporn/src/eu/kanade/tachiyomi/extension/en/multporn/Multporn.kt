277e07c70b005ff4dccd9fd42cc53792692eeb3b
darwin-256 <viktorasordinass@gmail.com>
2026-05-01 03:26:17 +0300

    Update Multporn.kt

--- src/en/multporn/src/eu/kanade/tachiyomi/extension/en/multporn/Multporn.kt
+++ src/en/multporn/src/eu/kanade/tachiyomi/extension/en/multporn/Multporn.kt
@@ -3,15 +3,14 @@ package eu.kanade.tachiyomi.extension.en.multporn
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.asObservable
 import eu.kanade.tachiyomi.network.asObservableSuccess
-import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
-import kotlinx.serialization.json.Json
+import keiyoushi.utils.firstInstanceOrNull
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
@@ -20,17 +19,13 @@ import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import rx.Observable
 import rx.schedulers.Schedulers
-import uy.kohesive.injekt.injectLazy
-import java.util.Locale
 
-class Multporn : ParsedHttpSource() {
+class Multporn : HttpSource() {
 
     override val name = "Multporn"
     override val lang: String = "en"
     override val baseUrl = "https://multporn.net"
     override val supportsLatest = true
-
-    private val json: Json by injectLazy()
 
     override fun headersBuilder(): Headers.Builder = Headers.Builder()
         .add("User-Agent", HEADER_USER_AGENT)
@@ -42,7 +37,7 @@ class Multporn : ParsedHttpSource() {
         val url = "$baseUrl/best".toHttpUrl().newBuilder()
             .addQueryParameter("page", page.toString())
 
-        (if (filters.isEmpty()) getFilterList(POPULAR_DEFAULT_SORT_BY_FILTER_STATE) else filters).forEach {
+        (if (filters.isEmpty()) getMultpornFilterList(POPULAR_DEFAULT_SORT_BY_FILTER_STATE) else filters).forEach {
             when (it) {
                 is SortBySelectFilter -> url.addQueryParameter("sort_by", it.selected.uri)
                 is SortOrderSelectFilter -> url.addQueryParameter("sort_order", it.selected.uri)
@@ -55,12 +50,12 @@ class Multporn : ParsedHttpSource() {
     }
 
     override fun popularMangaRequest(page: Int) = buildPopularMangaRequest(page - 1)
-    override fun popularMangaSelector() = ".masonry-item"
-    override fun popularMangaNextPageSelector() = ".pager-next a"
-    override fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
-        url = element.select(".views-field-title a").attr("href")
-        title = element.select(".views-field-title").text()
-        thumbnail_url = element.select("img").attr("abs:src")
+
+    override fun popularMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select(popularMangaSelector).map { popularMangaFromElement(it) }
+        val hasNextPage = document.select(popularMangaNextPageSelector).firstOrNull() != null
+        return MangasPage(mangas, hasNextPage)
     }
 
     // Latest
@@ -69,7 +64,7 @@ class Multporn : ParsedHttpSource() {
         val url = "$baseUrl/new".toHttpUrl().newBuilder()
             .addQueryParameter("page", page.toString())
 
-        (if (filters.isEmpty()) getFilterList(LATEST_DEFAULT_SORT_BY_FILTER_STATE) else filters).forEach {
+        (if (filters.isEmpty()) getMultpornFilterList(LATEST_DEFAULT_SORT_BY_FILTER_STATE) else filters).forEach {
             when (it) {
                 is SortBySelectFilter -> url.addQueryParameter("sort_by", it.selected.uri)
                 is SortOrderSelectFilter -> url.addQueryParameter("sort_order", it.selected.uri)
@@ -83,11 +78,12 @@ class Multporn : ParsedHttpSource() {
 
     override fun latestUpdatesRequest(page: Int) = buildLatestMangaRequest(page - 1)
 
-    override fun latestUpdatesSelector() = popularMangaSelector()
-
-    override fun latestUpdatesNextPageSelector() = popularMangaNextPageSelector()
-
-    override fun latestUpdatesFromElement(element: Element) = popularMangaFromElement(element)
+    override fun latestUpdatesParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select(popularMangaSelector).map { popularMangaFromElement(it) }
+        val hasNextPage = document.select(popularMangaNextPageSelector).firstOrNull() != null
+        return MangasPage(mangas, hasNextPage)
+    }
 
     // Search
 
@@ -96,9 +92,7 @@ class Multporn : ParsedHttpSource() {
         val mangas = document.select("#content .col-1:contains(Views:),.col-2:contains(Views:)")
             .map { popularMangaFromElement(it) }
 
-        val hasNextPage = popularMangaNextPageSelector().let {
-            document.select(it).firstOrNull()
-        } != null
+        val hasNextPage = document.select(popularMangaNextPageSelector).firstOrNull() != null
 
         return MangasPage(mangas, hasNextPage)
     }
@@ -108,7 +102,7 @@ class Multporn : ParsedHttpSource() {
             .addQueryParameter("page", (page - 1).toString())
             .addQueryParameter("search_api_views_fulltext", query)
 
-        (if (filtersArg.isEmpty()) getFilterList(SEARCH_DEFAULT_SORT_BY_FILTER_STATE) else filtersArg).forEach {
+        (if (filtersArg.isEmpty()) getMultpornFilterList(SEARCH_DEFAULT_SORT_BY_FILTER_STATE) else filtersArg).forEach {
             when (it) {
                 is SortBySelectFilter -> url.addQueryParameter("sort_by", it.selected.uri)
                 is SearchTypeSelectFilter -> url.addQueryParameter("type", it.selected.uri)
@@ -119,11 +113,11 @@ class Multporn : ParsedHttpSource() {
         return GET(url.build(), headers)
     }
 
-    private fun buildTextSearchFilterRequests(page: Int, filters: List<TextSearchFilter>): List<Request> = filters.map {
+    private fun buildTextSearchFilterRequests(page: Int, filters: List<TextSearchFilter>): List<Request> = filters.flatMap {
         it.stateURIs.map { queryURI ->
             GET("$baseUrl/${it.uri}/$queryURI?page=0,$page")
         }
-    }.flatten()
+    }
 
     private fun squashMangasPageObservables(observables: List<Observable<MangasPage?>>): Observable<MangasPage> = Observable.from(observables)
         .flatMap { it.observeOn(Schedulers.io()) }
@@ -131,15 +125,13 @@ class Multporn : ParsedHttpSource() {
         .map { it.filterNotNull() }
         .map { pages ->
             MangasPage(
-                pages.map { it.mangas }.flatten().distinctBy { it.url },
+                pages.flatMap { it.mangas }.distinctBy { it.url },
                 pages.any { it.hasNextPage },
             )
         }
 
-    override fun searchMangaSelector() = popularMangaSelector()
-
     override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> {
-        val sortByFilterType = filters.findInstance<SortBySelectFilter>()?.requestType ?: POPULAR_REQUEST_TYPE
+        val sortByFilterType = filters.firstInstanceOrNull<SortBySelectFilter>()?.requestType ?: POPULAR_REQUEST_TYPE
         val textSearchFilters = filters.filterIsInstance<TextSearchFilter>().filter { it.state.isNotBlank() }
 
         return when {
@@ -177,7 +169,7 @@ class Multporn : ParsedHttpSource() {
     }
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val sortByFilterType = filters.findInstance<SortBySelectFilter>()?.requestType ?: POPULAR_REQUEST_TYPE
+        val sortByFilterType = filters.firstInstanceOrNull<SortBySelectFilter>()?.requestType ?: POPULAR_REQUEST_TYPE
 
         return when {
             query.isNotEmpty() || sortByFilterType == SEARCH_REQUEST_TYPE -> buildSearchMangaRequest(page - 1, query, filters)
@@ -186,9 +178,12 @@ class Multporn : ParsedHttpSource() {
         }
     }
 
-    override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
-
-    override fun searchMangaFromElement(element: Element) = popularMangaFromElement(element)
+    override fun searchMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+        val mangas = document.select(popularMangaSelector).map { popularMangaFromElement(it) }
+        val hasNextPage = document.select(popularMangaNextPageSelector).firstOrNull() != null
+        return MangasPage(mangas, hasNextPage)
+    }
 
     // Details
 
@@ -198,9 +193,10 @@ class Multporn : ParsedHttpSource() {
         "field-name-field-img-group",
         "field-name-field-hentai-img-group",
         "field-name-field-rule-63-section",
-    ).map { document.select(".$it a").map { a -> a.text().trim() } }.flatten()
+    ).flatMap { document.select(".$it a").map { a -> a.text() } }
 
-    override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
+    override fun mangaDetailsParse(response: Response): SManga = SManga.create().apply {
+        val document = response.asJsoup()
         title = document.select("h1#page-title").text()
 
         val infoMap = listOf(
@@ -208,16 +204,16 @@ class Multporn : ParsedHttpSource() {
             "Characters",
             "Tags",
             "Author",
-        ).map {
-            it to document.select(".field:has(.field-label:contains($it:)) .links a").map { t -> t.text().trim() }
-        }.toMap()
+        ).associateWith {
+            document.select(".field:has(.field-label:contains($it:)) .links a").map { t -> t.text() }
+        }
 
         artist = (infoMap.getValue("Author") + parseUnlabelledAuthorNames(document))
             .distinct().joinToString()
         author = artist
 
         genre = listOf("Tags", "Section", "Characters")
-            .map { infoMap.getValue(it) }.flatten().distinct().joinToString()
+            .flatMap { infoMap.getValue(it) }.distinct().joinToString()
 
         status = infoMap["Section"]?.firstOrNull { it == "Ongoings" }?.let { SManga.ONGOING } ?: SManga.COMPLETED
 
@@ -237,10 +233,6 @@ class Multporn : ParsedHttpSource() {
 
     // Chapters
 
-    override fun chapterListSelector(): String = throw UnsupportedOperationException()
-
-    override fun chapterFromElement(element: Element): SChapter = throw UnsupportedOperationException()
-
     override fun chapterListParse(response: Response): List<SChapter> = throw UnsupportedOperationException()
 
     override fun fetchChapterList(manga: SManga): Observable<List<SChapter>> = Observable.just(
@@ -255,147 +247,32 @@ class Multporn : ParsedHttpSource() {
 
     // Pages
 
-    override fun pageListParse(document: Document): List<Page> = document.select(".jb-image img").mapIndexed { i, image ->
-        Page(i, imageUrl = image.attr("abs:src").replace("/styles/juicebox_2k/public", "").substringBefore("?"))
+    override fun pageListParse(response: Response): List<Page> {
+        val document = response.asJsoup()
+        return document.select(".jb-image img").mapIndexed { i, image ->
+            Page(i, imageUrl = image.absUrl("src").replace("/styles/juicebox_2k/public", "").substringBefore("?"))
+        }
     }
 
-    override fun imageUrlParse(document: Document) = throw UnsupportedOperationException()
+    override fun imageUrlParse(response: Response) = throw UnsupportedOperationException()
+
+    // Selectors
+
+    private val popularMangaSelector = ".masonry-item"
+    private val popularMangaNextPageSelector = ".pager-next a"
+
+    private fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
+        setUrlWithoutDomain(element.select(".views-field-title a").attr("abs:href"))
+        title = element.select(".views-field-title").text()
+        thumbnail_url = element.select("img").attr("abs:src")
+    }
 
     // Filters
 
-    override fun getFilterList() = getFilterList(POPULAR_DEFAULT_SORT_BY_FILTER_STATE)
-
-    open class URIFilter(open val name: String, open val uri: String)
-
-    class RequestTypeURIFilter(
-        val requestType: String,
-        override var name: String,
-        override val uri: String,
-    ) : URIFilter(name, uri)
-
-    open class URISelectFilter(name: String, open val filters: List<URIFilter>, state: Int = 0) : Filter.Select<String>(name, filters.map { it.name }.toTypedArray(), state) {
-        open val selected: URIFilter
-            get() = filters[state]
-    }
-
-    open class TypeSelectFilter(name: String, filters: List<URIFilter>) : URISelectFilter(name, filters)
-
-    private class PopularTypeSelectFilter(filters: List<URIFilter>) : TypeSelectFilter("Popular Type", filters)
-
-    private class LatestTypeSelectFilter(filters: List<URIFilter>) : TypeSelectFilter("Latest Type", filters)
-
-    private class SearchTypeSelectFilter(filters: List<URIFilter>) : TypeSelectFilter("Search Type", filters)
-
-    open class TextSearchFilter(name: String, val uri: String) : Filter.Text(name) {
-        val stateURIs: List<String>
-            get() {
-                return state.split(",").filter { it != "" }.map {
-                    Regex("[^A-Za-z0-9]").replace(it, " ").trim()
-                        .replace("\\s+".toRegex(), "_").lowercase(Locale.getDefault())
-                }.distinct()
-            }
-    }
-
-    private class SortBySelectFilter(override val filters: List<RequestTypeURIFilter>, state: Int) :
-        URISelectFilter(
-            "Sort By",
-            filters.map { filter ->
-                filter.let {
-                    it.name = "[${it.requestType}] ${it.name}"
-                    it
-                }
-            },
-            state,
-        ) {
-        val requestType: String
-            get() = filters[state].requestType
-    }
-
-    private class SortOrderSelectFilter(filters: List<URIFilter>) : URISelectFilter("Order By", filters)
-
-    private fun getFilterList(sortByFilterState: Int) = FilterList(
-        Filter.Header("Text search only works with Relevance and Author"),
-        SortBySelectFilter(getSortByFilters(), sortByFilterState),
-        Filter.Header("Order By only works with Popular and Latest"),
-        SortOrderSelectFilter(getSortOrderFilters()),
-        Filter.Header("Type filters apply based on selected Sort By option"),
-        PopularTypeSelectFilter(getPopularTypeFilters()),
-        LatestTypeSelectFilter(getLatestTypeFilters()),
-        SearchTypeSelectFilter(getSearchTypeFilters()),
-        Filter.Separator(),
-        Filter.Header("Filters below ignore text search and all options above"),
-        Filter.Header("Query must match title's non-special characters"),
-        Filter.Header("Separate queries with comma (,)"),
-        TextSearchFilter("Comic Tags", "category"),
-        TextSearchFilter("Comic Characters", "characters"),
-        TextSearchFilter("Comic Authors", "authors_comics"),
-        TextSearchFilter("Comic Sections", "comics"),
-        TextSearchFilter("Manga Categories", "category_hentai"),
-        TextSearchFilter("Manga Characters", "characters_hentai"),
-        TextSearchFilter("Manga Authors", "authors_hentai_comics"),
-        TextSearchFilter("Manga Sections", "hentai_manga"),
-        TextSearchFilter("Picture Authors", "authors_albums"),
-        TextSearchFilter("Picture Sections", "pictures"),
-        TextSearchFilter("Hentai Sections", "hentai"),
-        TextSearchFilter("Rule 63 Sections", "rule_63"),
-        TextSearchFilter("Gay Tags", "category_gay"),
-    )
-
-    private fun getPopularTypeFilters() = listOf(
-        URIFilter("Comics", "1"),
-        URIFilter("Hentai Manga", "2"),
-        URIFilter("Cartoon Pictures", "3"),
-        URIFilter("Hentai Pictures", "4"),
-        URIFilter("Rule 63", "10"),
-        URIFilter("Author Albums", "11"),
-    )
-
-    private fun getLatestTypeFilters() = listOf(
-        URIFilter("Comics", "1"),
-        URIFilter("Hentai Manga", "2"),
-        URIFilter("Cartoon Pictures", "3"),
-        URIFilter("Hentai Pictures", "4"),
-        URIFilter("Author Albums", "10"),
-    )
-
-    private fun getSearchTypeFilters() = listOf(
-        URIFilter("Comics", "1"),
-        URIFilter("Hentai Manga", "2"),
-        URIFilter("Gay Comics", "3"),
-        URIFilter("Cartoon Pictures", "4"),
-        URIFilter("Hentai Pictures", "5"),
-        URIFilter("Rule 63", "11"),
-        URIFilter("Humor", "13"),
-    )
-
-    private fun getSortByFilters() = listOf(
-        RequestTypeURIFilter(POPULAR_REQUEST_TYPE, "Total Views", "totalcount_1"),
-        RequestTypeURIFilter(POPULAR_REQUEST_TYPE, "Views Today", "daycount"),
-        RequestTypeURIFilter(POPULAR_REQUEST_TYPE, "Last Viewed", "timestamp"),
-        RequestTypeURIFilter(LATEST_REQUEST_TYPE, "Date Posted", "created"),
-        RequestTypeURIFilter(LATEST_REQUEST_TYPE, "Date Updated", "changed"),
-        RequestTypeURIFilter(SEARCH_REQUEST_TYPE, "Relevance", "search_api_relevance"),
-        RequestTypeURIFilter(SEARCH_REQUEST_TYPE, "Author", "author"),
-    )
-
-    private fun getSortOrderFilters() = listOf(
-        URIFilter("Descending", "DESC"),
-        URIFilter("Ascending", "ASC"),
-    )
-
-    private inline fun <reified T> Iterable<*>.findInstance() = find { it is T } as? T
+    override fun getFilterList() = getMultpornFilterList(POPULAR_DEFAULT_SORT_BY_FILTER_STATE)
 
     companion object {
-
-        const val LATEST_DEFAULT_SORT_BY_FILTER_STATE = 3
-        const val POPULAR_DEFAULT_SORT_BY_FILTER_STATE = 0
-        const val SEARCH_DEFAULT_SORT_BY_FILTER_STATE = 5
-
-        const val LATEST_REQUEST_TYPE = "Latest"
-        const val POPULAR_REQUEST_TYPE = "Popular"
-        const val SEARCH_REQUEST_TYPE = "Search"
-
-        const val HEADER_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
-        const val HEADER_CONTENT_TYPE = "application/x-www-form-urlencoded; charset=UTF-8"
+        private const val HEADER_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
+        private const val HEADER_CONTENT_TYPE = "application/x-www-form-urlencoded; charset=UTF-8"
     }
 }
