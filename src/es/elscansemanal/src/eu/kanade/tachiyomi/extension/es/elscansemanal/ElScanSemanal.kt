6f124b97bdc193843d8a80afc0b4d189019349f9
bapeey <90949336+bapeey@users.noreply.github.com>
2026-05-02 14:18:53 -0500

    add source

---
+++ src/es/elscansemanal/src/eu/kanade/tachiyomi/extension/es/elscansemanal/ElScanSemanal.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.es.elscansemanal
+
+import eu.kanade.tachiyomi.multisrc.lectormoe.LectorMoe
+
+class ElScanSemanal :
+    LectorMoe(
+        "El Scan Semanal",
+        "https://capibaratraductor.com/elscansemanal",
+        "es",
+    )
