ca79a6be09ee2dc4231eb9ed88aeedd54bf5039a
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-05-07 17:27:21 +0500

    HentaiNexus (EN): Pass raw URL via UrlActivity

--- src/en/hentainexus/src/eu/kanade/tachiyomi/extension/en/hentainexus/UrlActivity.kt
+++ src/en/hentainexus/src/eu/kanade/tachiyomi/extension/en/hentainexus/UrlActivity.kt
@@ -14,22 +14,17 @@ import kotlin.system.exitProcess
 class UrlActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val pathSegments = intent?.data?.pathSegments
-        if (pathSegments != null && pathSegments.size > 1) {
-            val id = pathSegments[1]
-            val mainIntent = Intent().apply {
-                action = "eu.kanade.tachiyomi.SEARCH"
-                putExtra("query", "${HentaiNexus.PREFIX_ID_SEARCH}$id")
-                putExtra("filter", packageName)
-            }
 
-            try {
-                startActivity(mainIntent)
-            } catch (e: ActivityNotFoundException) {
-                Log.e("UrlActivity", e.toString())
-            }
-        } else {
-            Log.e("UrlActivity", "Could not parse URI from intent $intent")
+        val mainIntent = Intent().apply {
+            action = "eu.kanade.tachiyomi.SEARCH"
+            putExtra("query", intent.data.toString())
+            putExtra("filter", packageName)
+        }
+
+        try {
+            startActivity(mainIntent)
+        } catch (e: ActivityNotFoundException) {
+            Log.e("UrlActivity", "Unable to launch activity", e)
         }
 
         finish()
