cfb3b44228876c19a54ac59751d13372bc185485
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-05-07 17:27:23 +0500

    Kuaikanmanhua (ZH): Pass raw URL via UrlActivity

--- src/zh/kuaikanmanhua/src/eu/kanade/tachiyomi/extension/zh/kuaikanmanhua/KuaikanmanhuaUrlActivity.kt
+++ src/zh/kuaikanmanhua/src/eu/kanade/tachiyomi/extension/zh/kuaikanmanhua/KuaikanmanhuaUrlActivity.kt
@@ -10,26 +10,17 @@ import kotlin.system.exitProcess
 class KuaikanmanhuaUrlActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val host = intent?.data?.host
-        val pathSegments = intent?.data?.pathSegments
-        if (pathSegments != null && pathSegments.size > 1) {
-            val id = when (host) {
-                "m.kuaikanmanhua.com" -> pathSegments[1]
-                else -> pathSegments[2]
-            }
-            val mainIntent = Intent().apply {
-                action = "eu.kanade.tachiyomi.SEARCH"
-                putExtra("query", "${Kuaikanmanhua.TOPIC_ID_SEARCH_PREFIX}$id")
-                putExtra("filter", packageName)
-            }
 
-            try {
-                startActivity(mainIntent)
-            } catch (e: ActivityNotFoundException) {
-                Log.e("KkmhUrlActivity", e.toString())
-            }
-        } else {
-            Log.e("KkmhUrlActivity", "could not parse uri from intent $intent")
+        val mainIntent = Intent().apply {
+            action = "eu.kanade.tachiyomi.SEARCH"
+            putExtra("query", intent.data.toString())
+            putExtra("filter", packageName)
+        }
+
+        try {
+            startActivity(mainIntent)
+        } catch (e: ActivityNotFoundException) {
+            Log.e("KkmhUrlActivity", "Unable to launch activity", e)
         }
 
         finish()
