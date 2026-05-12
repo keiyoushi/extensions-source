ce0a59f393bc0703860620066b43bd9315d9ddbf
CriosChan <thomas.notolruob@gmail.com>
2026-05-12 19:03:05 +0200

    PornhwaFR: Changes asked by Muhamad

--- src/fr/pornhwafr/src/eu/kanade/tachiyomi/extension/fr/pornhwafr/PornhwaFR.kt
+++ src/fr/pornhwafr/src/eu/kanade/tachiyomi/extension/fr/pornhwafr/PornhwaFR.kt
@@ -1,21 +1,9 @@
 package eu.kanade.tachiyomi.extension.fr.pornhwafr
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import eu.kanade.tachiyomi.source.model.SManga
-import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class PornhwaFR : MangaThemesia("Pornwha.fr", "https://pornhwa.fr", "fr", mangaUrlDirectory = "/catalogue", dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.FRENCH)) {
     override val altNamePrefix = "Nom alternatif : "
-    override fun String?.parseStatus(): Int = when {
-        this == null -> SManga.UNKNOWN
-        this.contains("En Cours", ignoreCase = true) -> SManga.ONGOING
-        this.contains("Terminé", ignoreCase = true) -> SManga.COMPLETED
-        else -> SManga.UNKNOWN
-    }
-
-    override fun mangaDetailsParse(document: Document): SManga = super.mangaDetailsParse(document).apply {
-        status = document.select(seriesStatusSelector).text().parseStatus()
-    }
 }
