1d0aaf00f2903803b4bd1015e51d58f594320509
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-05-16 00:26:56 +0500

    utils: update util functions
    - remove `inline` where not needed
    - use `decodeFromBufferedSource` for okio based streaming
    
    # Conflicts:
    #	gradle/libs.versions.toml

--- core/src/main/kotlin/keiyoushi/utils/Json.kt
+++ core/src/main/kotlin/keiyoushi/utils/Json.kt
@@ -1,24 +1,27 @@
 package keiyoushi.utils
 
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.decodeFromJsonElement
-import kotlinx.serialization.json.decodeFromStream
+import kotlinx.serialization.json.okio.decodeFromBufferedSource
+import kotlinx.serialization.serializer
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
+import okio.buffer
+import okio.source
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 import java.io.InputStream
 
 val jsonInstance: Json = Injekt.get()
 
+val JSON_MEDIA_TYPE = "application/json".toMediaType()
+
 /**
  * Parses JSON string into an object of type [T].
  */
-inline fun <reified T> String.parseAs(json: Json = jsonInstance): T = json.decodeFromString(this)
+inline fun <reified T> String.parseAs(json: Json = jsonInstance): T = json.decodeFromString(serializer(), this)
 
 /**
  * Parses JSON string into an object of type [T], applying a [transform] function to the string before parsing.
@@ -31,7 +34,9 @@ inline fun <reified T> String.parseAs(json: Json = jsonInstance, transform: (Str
 /**
  * Parses the response body into an object of type [T].
  */
-inline fun <reified T> Response.parseAs(json: Json = jsonInstance): T = use { json.decodeFromStream(it.body.byteStream()) }
+inline fun <reified T> Response.parseAs(json: Json = jsonInstance): T = use {
+    json.decodeFromBufferedSource(serializer(), it.body.source())
+}
 
 /**
  * Parses the response body into an object of type [T], applying a transformation to the raw JSON string before parsing.
@@ -39,28 +44,32 @@ inline fun <reified T> Response.parseAs(json: Json = jsonInstance): T = use { js
  * @param json The [Json] instance to use for parsing. Defaults to the injected instance.
  * @param transform A function to transform the JSON string before it's decoded.
  */
-inline fun <reified T> Response.parseAs(json: Json = jsonInstance, transform: (String) -> String): T = use { it.body.string().parseAs(json, transform) }
+inline fun <reified T> Response.parseAs(json: Json = jsonInstance, transform: (String) -> String): T = use {
+    it.body.string().parseAs(json, transform)
+}
 
 /**
  * Parses a [JsonElement] into an object of type [T].
  *
  * @param json The [Json] instance to use for parsing. Defaults to the injected instance.
  */
-inline fun <reified T> JsonElement.parseAs(json: Json = jsonInstance): T = json.decodeFromJsonElement(this)
+inline fun <reified T> JsonElement.parseAs(json: Json = jsonInstance): T = json.decodeFromJsonElement(serializer(), this)
 
 /**
  * Parses a [InputStream] into an object of type [T]
  *
  * @param json The [Json] instance to use for parsing. Defaults to the injected instance.
  */
-inline fun <reified T> InputStream.parseAs(json: Json = jsonInstance): T = use { json.decodeFromStream(it) }
+inline fun <reified T> InputStream.parseAs(json: Json = jsonInstance): T = use {
+    json.decodeFromBufferedSource(serializer(), it.source().buffer())
+}
 
 /**
  * Serializes the object to a JSON string.
  */
-inline fun <reified T> T.toJsonString(json: Json = jsonInstance): String = json.encodeToString(this)
+inline fun <reified T> T.toJsonString(json: Json = jsonInstance): String = json.encodeToString(serializer(), this)
 
 /**
  * Encodes the object to a Response Body.
  */
-inline fun <reified T> T.toJsonRequestBody(json: Json = jsonInstance): RequestBody = toJsonString(json).toRequestBody("application/json".toMediaType())
+inline fun <reified T> T.toJsonRequestBody(json: Json = jsonInstance): RequestBody = toJsonString(json).toRequestBody(JSON_MEDIA_TYPE)
