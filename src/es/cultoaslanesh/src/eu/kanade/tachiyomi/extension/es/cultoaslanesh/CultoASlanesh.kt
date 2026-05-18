59ecef3152c3be0a8cb39383147be133cf85f68b
bapeey <90949336+bapeey@users.noreply.github.com>
2026-05-18 02:23:44 -0500

    Add some CapibaraTraductor sources (#15954)
    * add c-k sources
    
    * fix name
    
    * adjust names and nsfw flags
    
    * oops

---
+++ src/es/cultoaslanesh/src/eu/kanade/tachiyomi/extension/es/cultoaslanesh/CultoASlanesh.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.es.cultoaslanesh
+
+import eu.kanade.tachiyomi.multisrc.lectormoe.LectorMoe
+
+class CultoASlanesh :
+    LectorMoe(
+        "Culto a Slanesh",
+        "https://capibaratraductor.com/cultoaslanesh",
+        "es",
+    )
---
+++ src/es/hangyakunoscanlation/src/eu/kanade/tachiyomi/extension/es/hangyakunoscanlation/HangyakuNoScanlation.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.es.hangyakunoscanlation
+
+import eu.kanade.tachiyomi.multisrc.lectormoe.LectorMoe
+
+class HangyakuNoScanlation :
+    LectorMoe(
+        "Hangyaku no Scanlation",
+        "https://capibaratraductor.com/hangyakunoscanlation",
+        "es",
+    )
