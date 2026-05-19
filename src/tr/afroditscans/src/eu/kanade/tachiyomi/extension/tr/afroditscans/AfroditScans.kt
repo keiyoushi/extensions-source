3dc5294322c3f58948be8a9b7bc9ec307d046046
luigidotmoe <19366824+luigidotmoe@users.noreply.github.com>
2026-05-19 20:06:42 +0200

    self migrate

--- src/tr/afroditscans/src/eu/kanade/tachiyomi/extension/tr/afroditscans/AfroditScans.kt
+++ src/tr/afroditscans/src/eu/kanade/tachiyomi/extension/tr/afroditscans/AfroditScans.kt
@@ -1,6 +1,10 @@
 package eu.kanade.tachiyomi.extension.tr.afroditscans
 
 import eu.kanade.tachiyomi.multisrc.uzaymanga.UzayManga
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.Request
+import okhttp3.Response
 
 class AfroditScans :
     UzayManga(
@@ -9,4 +13,22 @@ class AfroditScans :
         lang = "tr",
         versionId = 1,
         cdnUrl = "https://afroditcdn1.efsaneler.can.re",
-    )
+    ) {
+    override fun chapterListRequest(manga: SManga): Request {
+        // Migration from Madara to UzayManga
+        if (manga.url.startsWith("/manga/") && !manga.url.substringAfter("/manga/").substringBefore("/").all { it.isDigit() }) {
+            throw Exception("Migrate from $name to $name (same extension)")
+        }
+        return super.chapterListRequest(manga)
+    }
+
+    override fun pageListParse(response: Response): List<Page> {
+        // Migration from Madara to UzayManga
+        val url = response.request.url.toString().substringAfter(baseUrl)
+        if (!url.startsWith("/manga/")) {
+            throw Exception("Migrate from $name to $name (same extension)")
+        }
+
+        return super.pageListParse(response)
+    }
+}
