2cfcd6510b88993980f69d63fa450bd1b00a2319
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-05-21 11:19:04 +0500

    remove comment

--- core/src/main/kotlin/keiyoushi/utils/Context.kt
+++ core/src/main/kotlin/keiyoushi/utils/Context.kt
@@ -4,5 +4,4 @@ import android.app.Application
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
-// This ensures the type token isn't duplicated at each call site
 val applicationContext: Application get() = Injekt.get()
