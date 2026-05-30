5f3470a8171c78bcc900c462489d3198cd433dea
Rafou771 <rafgirardvienot@gmail.com>
2026-05-30 16:16:01 +0200

    Kun Manga (EN): Run spotlessApply

--- src/en/kunmanga/src/eu/kanade/tachiyomi/extension/en/kunmanga/KunManga.kt
+++ src/en/kunmanga/src/eu/kanade/tachiyomi/extension/en/kunmanga/KunManga.kt
@@ -10,23 +10,21 @@ import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.util.asJsoup
-
 import keiyoushi.utils.parseAs
 import keiyoushi.utils.tryParse
-
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
-
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.TimeZone
 
-class KunManga : Madara(
-    "Kun Manga",
-    "https://www.kunmanga.online",
-    "en",
-) {
+class KunManga :
+    Madara(
+        "Kun Manga",
+        "https://www.kunmanga.online",
+        "en",
+    ) {
 
     override val client = network.client.newBuilder()
         .rateLimitHost(baseUrl.toHttpUrl(), 2)
