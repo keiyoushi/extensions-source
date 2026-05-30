bd1d04d6ebbcfdc615e50985577a5f8f61873d4b
bapeey <90949336+bapeey@users.noreply.github.com>
2026-05-30 15:22:21 -0500

    extract duplicated logic

--- src/es/catmanhwas/src/eu/kanade/tachiyomi/extension/es/catmanhwas/SvelteParser.kt
+++ src/es/catmanhwas/src/eu/kanade/tachiyomi/extension/es/catmanhwas/SvelteParser.kt
@@ -15,45 +15,29 @@ class SvelteParser(val data: JsonArray) {
     fun parse(): JsonElement = resolve(data[0])
 
     private fun dereference(index: Int): JsonElement = when (val value = data[index]) {
-        is JsonArray -> resolve(value)
-        is JsonObject -> resolve(value)
+        is JsonArray, is JsonObject -> resolve(value)
         else -> value
     }
 
-    private fun resolve(element: JsonElement): JsonElement = when (element) {
-        is JsonArray -> {
-            JsonArray(
-                element.map {
-                    val index = (it as? JsonPrimitive)?.intOrNull
-                    if (
-                        index != null &&
-                        !it.isString &&
-                        index in data.indices
-                    ) {
-                        dereference(index)
-                    } else {
-                        resolve(it)
-                    }
-                },
-            )
+    private fun resolveReference(element: JsonElement): JsonElement {
+        val index = (element as? JsonPrimitive)?.intOrNull
+
+        return if (
+            index != null &&
+            !element.isString &&
+            index in data.indices
+        ) {
+            dereference(index)
+        } else {
+            resolve(element)
         }
-        is JsonObject -> {
-            buildJsonObject {
-                element.forEach { (key, value) ->
-                    val index = (value as? JsonPrimitive)?.intOrNull
-                    put(
-                        key,
-                        if (
-                            index != null &&
-                            !value.isString &&
-                            index in data.indices
-                        ) {
-                            dereference(index)
-                        } else {
-                            resolve(value)
-                        },
-                    )
-                }
+    }
+
+    private fun resolve(element: JsonElement): JsonElement = when (element) {
+        is JsonArray -> JsonArray(element.map(::resolveReference))
+        is JsonObject -> buildJsonObject {
+            element.forEach { (key, value) ->
+                put(key, resolveReference(value))
             }
         }
         else -> element
