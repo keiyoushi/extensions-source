cfef6d18f84d007e62319c35e9f4665801d7c65a
darwin-256 <16855574+darwin-256@users.noreply.github.com>
2026-05-31 10:04:39 +0300

    KaliScan Merge extensions (#16089)
    * Delete src/en/kaliscanio directory
    
    * Delete src/en/kaliscanme directory
    
    * Delete src/en/mgjinx directory
    
    * Update extName and overrideVersionCode in build.gradle
    
    * Implement configurable source with mirror preferences
    
    Refactor KaliScanCom class to implement ConfigurableSource and add mirror selection preferences.
    
    * Update src/en/kaliscancom/src/eu/kanade/tachiyomi/extension/en/kaliscancom/KaliScanCom.kt
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
    
    * Update KaliScanCom.kt
    
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>
    
    ---------
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
    Co-authored-by: Vetle Ledaal <vetle.ledaal@gmail.com>

--- src/en/kaliscanio/src/eu/kanade/tachiyomi/extension/en/kaliscanio/KaliScanIo.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.kaliscanio
-
-import eu.kanade.tachiyomi.multisrc.madtheme.MadTheme
-
-class KaliScanIo : MadTheme("KaliScan.io", "https://kaliscan.io", "en") {
-    override val useLegacyApi = true
-}
