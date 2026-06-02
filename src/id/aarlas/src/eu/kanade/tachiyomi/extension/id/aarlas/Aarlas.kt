4babcd1c5048f897ab5edaa378f22646a01933fe
Rafou376 <rafgirardvienot@gmail.com>
2026-06-02 19:52:32 +0200

    ZeistManga: improve date and fix some child ext

--- src/id/aarlas/src/eu/kanade/tachiyomi/extension/id/aarlas/Aarlas.kt
+++ src/id/aarlas/src/eu/kanade/tachiyomi/extension/id/aarlas/Aarlas.kt
@@ -1,5 +1,19 @@
 package eu.kanade.tachiyomi.extension.id.aarlas
 
 import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.TimeZone
 
-class Aarlas : ZeistManga("Aarlas", "https://aarlas.blogspot.com", "id")
+class Aarlas :
+    ZeistManga(
+        "Aarlas",
+        "https://www.arlas.online",
+        "id",
+    ) {
+    override val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.ROOT).apply {
+        timeZone = TimeZone.getTimeZone("Asia/Jakarta")
+    }
+
+    override val preferChapterUpdatedDate = true
+}
