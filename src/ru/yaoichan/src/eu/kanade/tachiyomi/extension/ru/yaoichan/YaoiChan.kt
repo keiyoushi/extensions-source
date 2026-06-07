4d2c2585ee71c120e8bec386b63c1ee54f50a622
darwin-256 <16855574+darwin-256@users.noreply.github.com>
2026-06-07 23:21:15 +0300

    yaoichan

--- src/ru/yaoichan/src/eu/kanade/tachiyomi/extension/ru/yaoichan/YaoiChan.kt
+++ src/ru/yaoichan/src/eu/kanade/tachiyomi/extension/ru/yaoichan/YaoiChan.kt
@@ -2,7 +2,6 @@ package eu.kanade.tachiyomi.extension.ru.yaoichan
 
 import eu.kanade.tachiyomi.multisrc.multichan.MultiChan
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
@@ -90,83 +89,9 @@ class YaoiChan : MultiChan("YaoiChan", "https://yaoi-chan.me", "ru") {
         return GET(url, headers)
     }
 
-    private class GenreList(genres: List<Genre>) : Filter.Group<Genre>("Тэги", genres)
-    private class Genre(name: String, val id: String = name.replace(' ', '_')) : Filter.TriState(name)
-    private class Status : Filter.Select<String>("Статус", arrayOf("Все", "Перевод завершен", "Выпуск завершен", "Онгоинг", "Новые главы"))
-    private class OrderBy :
-        Filter.Sort(
-            "Сортировка",
-            arrayOf("Дата", "Популярность", "Имя", "Главы"),
-            Selection(1, false),
-        )
-
     override fun getFilterList() = FilterList(
         Status(),
         OrderBy(),
         GenreList(getGenreList()),
-    )
-
-    private fun getGenreList() = listOf(
-        Genre("18 плюс"),
-        Genre("bdsm"),
-        Genre("арт"),
-        Genre("бара"),
-        Genre("боевик"),
-        Genre("боевые искусства"),
-        Genre("вампиры"),
-        Genre("веб"),
-        Genre("гарем"),
-        Genre("гендерная интрига"),
-        Genre("героическое фэнтези"),
-        Genre("групповой секс"),
-        Genre("детектив"),
-        Genre("дзёсэй"),
-        Genre("додзинси"),
-        Genre("драма"),
-        Genre("игра"),
-        Genre("инцест"),
-        Genre("искусство"),
-        Genre("история"),
-        Genre("киберпанк"),
-        Genre("комедия"),
-        Genre("литРПГ"),
-        Genre("махо-сёдзё"),
-        Genre("меха"),
-        Genre("мистика"),
-        Genre("мужская беременность"),
-        Genre("музыка"),
-        Genre("научная фантастика"),
-        Genre("омегаверс"),
-        Genre("переодевание"),
-        Genre("повседневность"),
-        Genre("постапокалиптика"),
-        Genre("приключения"),
-        Genre("психология"),
-        Genre("романтика"),
-        Genre("самурайский боевик"),
-        Genre("сборник"),
-        Genre("сверхъестественное"),
-        Genre("сетакон"),
-        Genre("сказка"),
-        Genre("спорт"),
-        Genre("супергерои"),
-        Genre("сэйнэн"),
-        Genre("сёдзё"),
-        Genre("сёдзё-ай"),
-        Genre("сёнэн"),
-        Genre("сёнэн-ай"),
-        Genre("тентакли"),
-        Genre("трагедия"),
-        Genre("триллер"),
-        Genre("ужасы"),
-        Genre("фантастика"),
-        Genre("фурри"),
-        Genre("фэнтези"),
-        Genre("школа"),
-        Genre("эротика"),
-        Genre("юмор"),
-        Genre("юри"),
-        Genre("яой"),
-        Genre("ёнкома"),
     )
 }
