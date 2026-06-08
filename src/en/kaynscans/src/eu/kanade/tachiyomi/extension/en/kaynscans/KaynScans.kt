33287657da12b0d76dcdf9aaaa6ab19dfb168dfa
iq8 <266871934+disk-iq8@users.noreply.github.com>
2026-06-08 15:49:58 +0300

    remove versionId change

--- src/en/kaynscans/src/eu/kanade/tachiyomi/extension/en/kaynscans/KaynScans.kt
+++ src/en/kaynscans/src/eu/kanade/tachiyomi/extension/en/kaynscans/KaynScans.kt
@@ -1,6 +1,10 @@
 package eu.kanade.tachiyomi.extension.en.kaynscans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.Request
+import okhttp3.Response
 
 class KaynScans :
     Iken(
@@ -9,5 +13,19 @@ class KaynScans :
         "https://kaynscan.org",
         "https://api.kaynscan.org",
     ) {
-    override val versionId = 2
+
+    // Migrate from Keyoapp to Iken by checking non slug-only urls
+    override fun chapterListRequest(m: SManga): Request {
+        if (m.url.startsWith('/')) {
+            throw Exception("Migrate entry from '$name' to '$name'")
+        }
+        return super.chapterListRequest(m)
+    }
+
+    override fun pageListParse(r: Response): List<Page> {
+        if (r.request.url.pathSegments.size > 1) {
+            throw Exception("Migrate entry from '$name' to '$name'")
+        }
+        return super.pageListParse(r)
+    }
 }
