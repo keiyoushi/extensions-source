d39304bca16e428ed67ad117a180b8571c4c710a
darwin-256 <16855574+darwin-256@users.noreply.github.com>
2026-06-08 21:13:35 +0300

    Refactor searchMangaRequest for improved clarity

--- src/ru/yaoichan/src/eu/kanade/tachiyomi/extension/ru/yaoichan/YaoiChan.kt
+++ src/ru/yaoichan/src/eu/kanade/tachiyomi/extension/ru/yaoichan/YaoiChan.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.extension.ru.yaoichan
 import eu.kanade.tachiyomi.multisrc.multichan.MultiChan
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
+import keiyoushi.utils.firstInstanceOrNull
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 
@@ -11,81 +12,74 @@ class YaoiChan : MultiChan("YaoiChan", "https://yaoi-chan.me", "ru") {
     override val id: Long = 2466512768990363955
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val url = if (query.isNotEmpty()) {
-            baseUrl.toHttpUrl().newBuilder()
+        val pageNum = page.coerceAtLeast(1)
+        if (query.isNotEmpty()) {
+            val url = baseUrl.toHttpUrl().newBuilder()
                 .addQueryParameter("do", "search")
                 .addQueryParameter("subaction", "search")
                 .addQueryParameter("story", query)
-                .addQueryParameter("search_start", page.toString())
+                .addQueryParameter("search_start", pageNum.toString())
                 .build()
                 .toString()
-        } else {
-            var genres = ""
-            var order = ""
-            var statusParam = true
-            var status = ""
-            for (filter in if (filters.isEmpty()) getFilterList() else filters) {
-                when (filter) {
-                    is GenreList -> {
-                        filter.state.forEach { f ->
-                            if (!f.isIgnored()) {
-                                genres += (if (f.isExcluded()) "-" else "") + f.id + '+'
-                            }
+            return GET(url, headers)
+        }
+
+        var genres = ""
+        var statusParam = true
+        var status = ""
+
+        val filterList = filters.ifEmpty { getFilterList() }
+
+        filterList.forEach { filter ->
+            when (filter) {
+                is GenreList -> {
+                    filter.state.forEach { f ->
+                        if (!f.isIgnored()) {
+                            genres += (if (f.isExcluded()) "-" else "") + f.id + '+'
                         }
                     }
-
-                    is OrderBy -> {
-                        if (filter.state!!.ascending && filter.state!!.index == 0) {
-                            statusParam = false
-                        }
-                    }
-
-                    is Status -> status = arrayOf("", "all_done", "end", "ongoing", "new_ch")[filter.state]
-
-                    else -> {}
                 }
-            }
-
-            if (genres.isNotEmpty()) {
-                for (filter in filters) {
-                    when (filter) {
-                        is OrderBy -> {
-                            order = if (filter.state!!.ascending) {
-                                arrayOf("", "&n=favasc", "&n=abcdesc", "&n=chasc")[filter.state!!.index]
-                            } else {
-                                arrayOf("&n=dateasc", "&n=favdesc", "&n=abcasc", "&n=chdesc")[filter.state!!.index]
-                            }
-                        }
-
-                        else -> {}
+                is OrderBy -> {
+                    if (filter.state!!.ascending && filter.state!!.index == 0) {
+                        statusParam = false
                     }
                 }
-                if (statusParam) {
-                    "$baseUrl/tags/${genres.dropLast(1)}$order?offset=${20 * (page - 1)}&status=$status"
-                } else {
-                    "$baseUrl/tags/$status/${genres.dropLast(1)}/$order?offset=${20 * (page - 1)}"
-                }
-            } else {
-                for (filter in filters) {
-                    when (filter) {
-                        is OrderBy -> {
-                            order = if (filter.state!!.ascending) {
-                                arrayOf("manga/new", "manga/new&n=favasc", "manga/new&n=abcdesc", "manga/new&n=chasc")[filter.state!!.index]
-                            } else {
-                                arrayOf("manga/new&n=dateasc", "mostfavorites", "catalog", "sortch")[filter.state!!.index]
-                            }
-                        }
-
-                        else -> {}
-                    }
-                }
-                if (statusParam) {
-                    "$baseUrl/$order?offset=${20 * (page - 1)}&status=$status"
-                } else {
-                    "$baseUrl/$order/$status?offset=${20 * (page - 1)}"
-                }
+                is Status -> status = arrayOf("", "all_done", "end", "ongoing", "new_ch")[filter.state]
+                else -> {}
             }
         }
+
+        val orderBy = filterList.firstInstanceOrNull<OrderBy>()
+        val url = if (genres.isNotEmpty()) {
+            val order = orderBy?.let {
+                if (it.state!!.ascending) {
+                    arrayOf("", "&n=favasc", "&n=abcdesc", "&n=chasc")[it.state!!.index]
+                } else {
+                    arrayOf("&n=dateasc", "&n=favdesc", "&n=abcasc", "&n=chdesc")[it.state!!.index]
+                }
+            } ?: ""
+
+            if (statusParam) {
+                "$baseUrl/tags/${genres.dropLast(1)}$order?offset=${20 * (pageNum - 1)}&status=$status"
+            } else {
+                "$baseUrl/tags/$status/${genres.dropLast(1)}/$order?offset=${20 * (pageNum - 1)}"
+            }
+        } else {
+            val order = orderBy?.let {
+                if (it.state!!.ascending) {
+                    arrayOf("manga/new", "manga/new&n=favasc", "manga/new&n=abcdesc", "manga/new&n=chasc")[it.state!!.index]
+                } else {
+                    arrayOf("manga/new&n=dateasc", "mostfavorites", "catalog", "sortch")[it.state!!.index]
+                }
+            } ?: "manga/new"
+
+            if (statusParam) {
+                "$baseUrl/$order?offset=${20 * (pageNum - 1)}&status=$status"
+            } else {
+                "$baseUrl/$order/$status?offset=${20 * (pageNum - 1)}"
+            }
+        }
+
         return GET(url, headers)
     }
 
