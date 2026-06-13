73c731609d37d2ab37dd15971b0cb818f7ca2619
darwin-256 <16855574+darwin-256@users.noreply.github.com>
2026-06-13 19:16:31 +0300

    Add GraphQL queries and update variable classes

--- lib-multisrc/senkuro/src/eu/kanade/tachiyomi/multisrc/senkuro/SenkuroQueries.kt
+++ lib-multisrc/senkuro/src/eu/kanade/tachiyomi/multisrc/senkuro/SenkuroQueries.kt
@@ -2,46 +2,184 @@ package eu.kanade.tachiyomi.multisrc.senkuro
 
 import kotlinx.serialization.Serializable
 
-@Serializable
-class FetchMangasVariables(
-    val after: String? = null,
-    val bookmark: ExcludeInclude = ExcludeInclude(),
-    val chapters: EmptyObject = EmptyObject,
-    val format: ExcludeInclude = ExcludeInclude(),
-    val label: ExcludeInclude = ExcludeInclude(),
-    val orderDirection: String = "DESC",
-    val orderField: String = "POPULARITY_SCORE",
-    val originCountry: ExcludeInclude = ExcludeInclude(),
-    val rating: ExcludeInclude = ExcludeInclude(),
-    val releasedOn: EmptyObject = EmptyObject,
-    val search: String? = null,
-    val source: ExcludeInclude = ExcludeInclude(),
-    val status: ExcludeInclude = ExcludeInclude(),
-    val translitionStatus: ExcludeInclude = ExcludeInclude(),
-    val type: ExcludeInclude = ExcludeInclude(),
-)
+val SEARCH_QUERY = $$"""
+query searchTachiyomiManga(
+  $query: String,
+  $type: MangaTachiyomiSearchTypeFilter,
+  $status: MangaTachiyomiSearchStatusFilter,
+  $rating: MangaTachiyomiSearchRatingFilter,
+  $format: MangaTachiyomiSearchFormatFilter,
+  $translationStatus: MangaTachiyomiSearchTranslationStatusFilter,
+  $label: MangaTachiyomiSearchLabelFilter,
+  $orderBy: MangaTachiyomiOrder,
+  $offset: Int
+) {
+  mangaTachiyomiSearch(
+    query: $query,
+    type: $type,
+    status: $status,
+    rating: $rating,
+    format: $format,
+    translationStatus: $translationStatus,
+    label: $label,
+    orderBy: $orderBy,
+    offset: $offset
+  ) {
+    mangas {
+      id
+      slug
+      originalName {
+        lang
+        content
+      }
+      titles {
+        lang
+        content
+      }
+      alternativeNames {
+        lang
+        content
+      }
+      cover {
+        original {
+          url
+        }
+      }
+    }
+  }
+}
+"""
+
+val FILTERS_QUERY = $$"""
+query fetchTachiyomiSearchFilters {
+  mangaTachiyomiSearchFilters {
+    labels {
+      id
+      rootId
+      slug
+      titles {
+        lang
+        content
+      }
+    }
+  }
+}
+"""
+
+val DETAILS_QUERY = $$"""
+query fetchTachiyomiManga($mangaId: ID!) {
+  mangaTachiyomiInfo(mangaId: $mangaId) {
+    id
+    slug
+    originalName {
+      lang
+      content
+    }
+    titles {
+      lang
+      content
+    }
+    alternativeNames {
+      lang
+      content
+    }
+    localizations {
+      lang
+      description
+    }
+    type
+    rating
+    status
+    formats
+    labels {
+      id
+      rootId
+      slug
+      titles {
+        lang
+        content
+      }
+    }
+    translationStatus
+    cover {
+      original {
+        url
+      }
+    }
+    mainStaff {
+      roles
+      person {
+        name
+      }
+    }
+  }
+}
+"""
+
+val CHAPTERS_QUERY = $$"""
+query fetchTachiyomiChapters(
+  $mangaId: ID!
+) {
+  mangaTachiyomiChapters(
+    mangaId: $mangaId
+  ) {
+    message
+    chapters {
+      id
+      slug
+      branchId
+      name
+      teamIds
+      number
+      volume
+      createdAt
+      updatedAt
+    }
+    teams {
+      id
+      slug
+      name
+    }
+  }
+}
+"""
+
+val PAGES_QUERY = $$"""
+query fetchTachiyomiChapterPages(
+  $mangaId: ID!,
+  $chapterId: ID!
+) {
+  mangaTachiyomiChapterPages(
+    mangaId: $mangaId,
+    chapterId: $chapterId
+  ) {
+    pages {
+      url
+    }
+  }
+}
+"""
 
 @Serializable
-object EmptyObject
+class SearchTachiyomiMangaVariables(
+    val query: String? = null,
+    val type: ExcludeInclude? = null,
+    val status: ExcludeInclude? = null,
+    val rating: ExcludeInclude? = null,
+    val format: ExcludeInclude? = null,
+    val translationStatus: ExcludeInclude? = null,
+    val label: ExcludeInclude? = null,
+    val orderBy: OrderByDto? = null,
+    val offset: Int? = null,
+)
 
 @Serializable
 class ExcludeInclude(
-    val exclude: List<String> = emptyList(),
     val include: List<String> = emptyList(),
-)
-
-@Serializable
-class FetchMangaVariables(
-    val slug: String,
-)
-
-@Serializable
-class FetchMangaChaptersVariables(
-    val after: String? = null,
-    val branchId: String,
-    val number: String? = null,
-    val orderBy: OrderByDto = OrderByDto("DESC", "NUMBER"),
-)
+    val exclude: List<String> = emptyList(),
+) {
+    fun isNotEmpty() = include.isNotEmpty() || exclude.isNotEmpty()
+}
 
 @Serializable
 class OrderByDto(
@@ -50,7 +188,20 @@ class OrderByDto(
 )
 
 @Serializable
-class FetchMangaChapterVariables(
-    val slug: String,
-    val cdnQuality: String = "auto",
+class FetchTachiyomiMangaVariables(
+    val mangaId: String,
 )
+
+@Serializable
+class FetchTachiyomiChaptersVariables(
+    val mangaId: String,
+)
+
+@Serializable
+class FetchTachiyomiChapterPagesVariables(
+    val mangaId: String,
+    val chapterId: String,
+)
+
+@Serializable
+object EmptyObject
