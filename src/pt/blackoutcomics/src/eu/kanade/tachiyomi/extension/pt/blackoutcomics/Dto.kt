2de20b1891fb6a2a24c002d126ff7f68931cd997
darwin-256 <16855574+darwin-256@users.noreply.github.com>
2026-06-16 00:06:20 +0300

    Blackout Comics Fix Pages (#16714)
    * Increment version code from 9 to 10
    
    * Refactor SearchItem properties to private and add method
    
    * Refactor BlackoutComics extension for improved parsing

--- src/pt/blackoutcomics/src/eu/kanade/tachiyomi/extension/pt/blackoutcomics/Dto.kt
+++ src/pt/blackoutcomics/src/eu/kanade/tachiyomi/extension/pt/blackoutcomics/Dto.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.extension.pt.blackoutcomics
 
+import eu.kanade.tachiyomi.source.model.SManga
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -10,8 +11,14 @@ class SearchResponse(
 
 @Serializable
 class SearchItem(
-    @SerialName("PJT_ID") val id: Int,
-    @SerialName("PJT_NAME") val name: String,
-    @SerialName("PJT_IMG_PR") val imgPr: String? = null,
-    @SerialName("PJT_IMG_PR_URL") val imgUrl: String? = null,
-)
+    @SerialName("PJT_ID") private val id: Int,
+    @SerialName("PJT_NAME") private val name: String,
+    @SerialName("PJT_IMG_PR") private val imgPr: String? = null,
+    @SerialName("PJT_IMG_PR_URL") private val imgUrl: String? = null,
+) {
+    fun toSManga(baseUrl: String) = SManga.create().apply {
+        title = name
+        url = "/comics/$id"
+        thumbnail_url = imgUrl ?: ("$baseUrl/" + imgPr)
+    }
+}
