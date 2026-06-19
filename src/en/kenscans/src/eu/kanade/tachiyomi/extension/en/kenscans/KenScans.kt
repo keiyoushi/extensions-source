006a19d04ea1cbd76ad7ea8fa74f6aab6f16180e
darwin-256 <16855574+darwin-256@users.noreply.github.com>
2026-06-20 00:55:19 +0300

    KenScans Fix Chapter date parsing (#16794)
    * Update build.gradle
    
    * Fix chapter list parsing in KenScans extension

--- src/en/kenscans/src/eu/kanade/tachiyomi/extension/en/kenscans/KenScans.kt
+++ src/en/kenscans/src/eu/kanade/tachiyomi/extension/en/kenscans/KenScans.kt
@@ -2,9 +2,11 @@ package eu.kanade.tachiyomi.extension.en.kenscans
 
 import eu.kanade.tachiyomi.multisrc.iken.Iken
 import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import okhttp3.Request
 import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
 
 class KenScans :
     Iken(
@@ -19,6 +21,18 @@ class KenScans :
             throw Exception("Migrate from $name to $name (same extension)")
         }
         return super.chapterListRequest(manga)
+    }
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val bodyString = response.body.string()
+            .replace("\"createdAt\":", "\"_createdAt\":")
+            .replace("\"updatedAt\":", "\"createdAt\":")
+
+        val newResponse = response.newBuilder()
+            .body(bodyString.toResponseBody(response.body.contentType()))
+            .build()
+
+        return super.chapterListParse(newResponse)
     }
 
     override fun pageListParse(response: Response): List<Page> {
