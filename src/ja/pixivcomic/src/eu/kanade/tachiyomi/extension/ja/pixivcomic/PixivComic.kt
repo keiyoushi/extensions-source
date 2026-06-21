da36231139a86d636e852d9181897c70ec44895f
Manticore <133025162+manti-X@users.noreply.github.com>
2026-06-21 21:48:22 +0200

    hm

--- src/ja/pixivcomic/src/eu/kanade/tachiyomi/extension/ja/pixivcomic/PixivComic.kt
+++ src/ja/pixivcomic/src/eu/kanade/tachiyomi/extension/ja/pixivcomic/PixivComic.kt
@@ -325,7 +325,7 @@ abstract class PixivComic : HttpSource() {
     }
 
     override suspend fun fetchRelatedMangaList(manga: SManga): List<SManga> = emptyList()
-    override suspend fun getMangaByUrl(url: HttpUrl): SManga = throw UnsupportedOperationException()
+    override suspend fun getMangaByUrl(url: HttpUrl): SManga? = null
 
     companion object {
         private const val HIDE_LOCKED_PREF_KEY = "hide_locked"
