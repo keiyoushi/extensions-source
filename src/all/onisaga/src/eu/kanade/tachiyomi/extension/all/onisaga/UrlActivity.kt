57b63350403c5b11be20879cf255580c2e9877ef
Alpha-782 <253939700+Alpha-782@users.noreply.github.com>
2026-06-23 16:02:09 +0300

    OniSaga [Multi]: Add Source (#16726)
    * Add OniSaga
    
    * Improve UrlActivity
    
    * AndroidManifest
    
    * Delete wrong AndroidManifest
    
    * Factory & Token
    
    * Delete old Dto
    
    * Delete old Filters
    
    * Fix error 429
    
    * Chapter dates & minor improvements
    
    * Remove unnecessary comments
    
    * Add more preferences & improvements
    
    * Add rate preference
    
    * Change parseChapterDate
    
    * Cache livewire state & optimize chapter list
    
    * Mandatory titles

---
+++ src/all/onisaga/src/eu/kanade/tachiyomi/extension/all/onisaga/UrlActivity.kt
@@ -0,0 +1,30 @@
+package eu.kanade.tachiyomi.extension.all.onisaga
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import kotlin.system.exitProcess
+
+class UrlActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val data = intent?.data
+        if (data != null) {
+            val mainIntent = Intent("eu.kanade.tachiyomi.SEARCH").apply {
+                putExtra("query", data.toString())
+                putExtra("filter", packageName)
+            }
+
+            try {
+                startActivity(mainIntent)
+            } catch (e: ActivityNotFoundException) {
+                Log.e("OniSaga", "Unable to launch activity", e)
+            }
+        }
+
+        finish()
+        exitProcess(0)
+    }
+}
