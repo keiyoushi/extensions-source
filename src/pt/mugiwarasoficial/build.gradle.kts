b472295ee250361ed6bd615c1cc0839eb9096647
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-06-26 10:28:41 +0500

    Update build.gradle.kts

--- src/pt/mugiwarasoficial/build.gradle.kts
+++ src/pt/mugiwarasoficial/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "Mugiwaras Oficial"
     className = "MugiwarasOficial"
-    versionCode = 2
+    versionCode = 3
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
     theme = "madara"
