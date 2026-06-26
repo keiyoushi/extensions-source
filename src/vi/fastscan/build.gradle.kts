2ac6401ffd2e47e6d991f1f9349f41fb2991753e
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-06-26 17:40:35 +0700

    FastScan refactor

--- src/vi/fastscan/build.gradle.kts
+++ src/vi/fastscan/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "FastScan"
     className = "FastScan"
-    versionCode = 2
+    versionCode = 3
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
 }
