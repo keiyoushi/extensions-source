66f7648a73428d5206317f2bf4b9e62aa7fb3fe7
amebacelestial <naopoggers@gmail.com>
2026-06-26 02:41:48 -0300

    fix manga livre http 403 (#16934)
    * Add Referer and x-toonlivre-client headers
    
    * Increment version code from 64 to 65
    
    * Enhance MangaLivre with signed chapter headers
    
    Updated versionId to 3 and added signed chapter headers functionality with SHA-256 hashing.
    
    * update toonshit signature handling
    
    Updated versionId to 4 and modified signature generation logic.
    
    * versionid fix
    
    * update
    
    Removed legacy signature calculation and updated the signature method for chapter headers.
    
    * Refactor MangaLivre by removing unused methods
    
    Removed unused methods for signing chapter headers and SHA-256 hashing.
    
    * stupid bums
    
    * update
    
    * add bundle dump
    
    * spotlessapply
    
    * Fix rate limit configuration for OkHttpClient
    
    * Update build.gradle
    
    * Update build.gradle.kts
    
    ---------
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/pt/mangalivre/build.gradle.kts
+++ src/pt/mangalivre/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "Manga Livre"
     className = "MangaLivre"
-    versionCode = 64
+    versionCode = 65
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
 }
