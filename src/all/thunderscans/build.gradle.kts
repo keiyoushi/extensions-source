b2485fb722d396c70e35d963c1d1d3809a5b85d5
Prem Kumar <60751338+prem-k-r@users.noreply.github.com>
2026-06-26 11:11:08 +0530

    Thunder Scans: Fix missing locked chapters (#17002)
    * Use unique URLs for locked chapters
    
    * Increment overrideVersionCode to 13
    
    * Apply Spotless formatting
    
    * Update build.gradle
    
    * Update build.gradle.kts
    
    ---------
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/all/thunderscans/build.gradle.kts
+++ src/all/thunderscans/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "Thunder Scans"
     className = "ThunderScansFactory"
-    versionCode = 12
+    versionCode = 13
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
     theme = "mangathemesia"
