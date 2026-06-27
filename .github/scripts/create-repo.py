05c251bd64b4b402b6ff589f8449fe9afdcc12b6
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-06-27 11:20:38 +0500

    CI: use new index format for merging index files (#17031)

--- .github/scripts/create-repo.py
+++ .github/scripts/create-repo.py
@@ -6,19 +6,22 @@ import subprocess
 from pathlib import Path
 from zipfile import ZipFile
 
+
 def hex_to_float(hex_str):
     int_val = int(hex_str, 16)
-    float_val = struct.unpack('>f', struct.pack('>I', int_val))[0]
+    float_val = struct.unpack(">f", struct.pack(">I", int_val))[0]
     return round(float_val, 1)
+
 
 PACKAGE_NAME_REGEX = re.compile(r"package: name='([^']+)'")
 VERSION_CODE_REGEX = re.compile(r"versionCode='([^']+)'")
 VERSION_NAME_REGEX = re.compile(r"versionName='([^']+)'")
 CONTENT_WARNING_REGEX = re.compile(r"'tachiyomix.contentWarning' value='([^']+)'")
 EXTENSION_LIB_REGEX = re.compile(r"'tachiyomix.extensionLib' value='([^']+)'")
-APPLICATION_LABEL_REGEX = re.compile(r"^application-label:'([^']+)'", re.MULTILINE)
-APPLICATION_ICON_320_REGEX = re.compile(r"^application-icon-320:'([^']+)'", re.MULTILINE)
-LANGUAGE_REGEX = re.compile(r"tachiyomi-([^.]+)")
+EXTENSION_NAME_REGEX = re.compile(r"'tachiyomix.name' value='([^']+)'")
+APPLICATION_ICON_320_REGEX = re.compile(
+    r"^application-icon-320:'([^']+)'", re.MULTILINE
+)
 
 *_, ANDROID_BUILD_TOOLS = (Path(os.environ["ANDROID_HOME"]) / "build-tools").iterdir()
 REPO_DIR = Path("repo")
@@ -27,10 +30,17 @@ REPO_ICON_DIR = REPO_DIR / "icon"
 
 REPO_ICON_DIR.mkdir(parents=True, exist_ok=True)
 
+APK_BASE_URL = (
+    "https://raw.githubusercontent.com/keiyoushi/extensions/refs/heads/repo/apk"
+)
+ICON_BASE_URL = (
+    "https://raw.githubusercontent.com/keiyoushi/extensions/refs/heads/repo/icon"
+)
+
 with open("output.json", encoding="utf-8") as f:
     inspector_data = json.load(f)
 
-index_data = []
+extensions = []
 
 for apk in REPO_APK_DIR.iterdir():
     badging = subprocess.check_output(
@@ -47,48 +57,41 @@ for apk in REPO_APK_DIR.iterdir():
     package_name = PACKAGE_NAME_REGEX.search(package_info).group(1)
     application_icon = APPLICATION_ICON_320_REGEX.search(badging).group(1)
 
-    with ZipFile(apk) as z, z.open(application_icon) as i, (
-        REPO_ICON_DIR / f"{package_name}.png"
-    ).open("wb") as f:
+    with (
+        ZipFile(apk) as z,
+        z.open(application_icon) as i,
+        (REPO_ICON_DIR / f"{package_name}.png").open("wb") as f,
+    ):
         f.write(i.read())
 
-    language = LANGUAGE_REGEX.search(apk.name).group(1)
     sources = inspector_data[package_name]
-
-    if len(sources) == 1:
-        source_language = sources[0]["lang"]
-
-        if (
-            source_language != language
-            and source_language not in {"all", "other"}
-            and language not in {"all", "other"}
-        ):
-            language = source_language
-
     content_warning = int(CONTENT_WARNING_REGEX.search(badging).group(1))
     extension_lib = hex_to_float(EXTENSION_LIB_REGEX.search(badging).group(1).strip())
+    ext_name = EXTENSION_NAME_REGEX.search(badging).group(1)
 
-    extension_data = {
-        "name": APPLICATION_LABEL_REGEX.search(badging).group(1),
-        "pkg": package_name,
-        "apk": apk.name,
-        "lang": language,
-        "code": int(VERSION_CODE_REGEX.search(package_info).group(1)),
-        "version": VERSION_NAME_REGEX.search(package_info).group(1),
-        "contentWarning": content_warning,
-        "libVersion": extension_lib,
-        "sources": [
-            {
-                "name": source["name"],
-                "lang": source["lang"],
-                "id": source["id"],
-                "baseUrl": source["baseUrl"],
-            }
-            for source in sources
-        ],
-    }
+    extensions.append(
+        {
+            "name": ext_name,
+            "packageName": package_name,
+            "resources": {
+                "apkUrl": f"{APK_BASE_URL}/{apk.name}",
+                "iconUrl": f"{ICON_BASE_URL}/{package_name}.png",
+            },
+            "extensionLib": str(extension_lib),
+            "versionCode": int(VERSION_CODE_REGEX.search(package_info).group(1)),
+            "versionName": VERSION_NAME_REGEX.search(package_info).group(1),
+            "contentWarning": content_warning + 1,
+            "sources": [
+                {
+                    "id": int(source["id"]),
+                    "name": source["name"],
+                    "language": source["lang"],
+                    "homeUrl": source["baseUrl"],
+                }
+                for source in sources
+            ],
+        }
+    )
 
-    index_data.append(extension_data)
-
-with REPO_DIR.joinpath("index.json").open("w", encoding="utf-8") as index_file:
-    json.dump(index_data, index_file, ensure_ascii=False, separators=(",", ":"))
+with REPO_DIR.joinpath("index.json").open("w", encoding="utf-8") as f:
+    json.dump({"extensions": extensions}, f, ensure_ascii=False, separators=(",", ":"))
--- .github/scripts/merge-repo.py
+++ .github/scripts/merge-repo.py
@@ -13,6 +13,8 @@ import index_pb2
 REMOTE_REPO: Path = Path.cwd()
 LOCAL_REPO: Path = REMOTE_REPO.parent.joinpath("main/repo")
 
+LANGUAGE_REGEX = re.compile(r"tachiyomi-([^.]+)")
+
 to_delete: list[str] = json.loads(sys.argv[1])
 
 for module in to_delete:
@@ -25,89 +27,100 @@ for module in to_delete:
         print(file.name)
         file.unlink(missing_ok=True)
 
-shutil.copytree(src=LOCAL_REPO.joinpath("apk"), dst=REMOTE_REPO.joinpath("apk"), dirs_exist_ok = True)
-shutil.copytree(src=LOCAL_REPO.joinpath("icon"), dst=REMOTE_REPO.joinpath("icon"), dirs_exist_ok = True)
-
-with REMOTE_REPO.joinpath("index.min.json").open() as remote_index_file:
-    remote_index = json.load(remote_index_file)
-
-with LOCAL_REPO.joinpath("index.json").open() as local_index_file:
-    local_index = json.load(local_index_file)
-
-legacy_index = [
-    item for item in remote_index
-    if not any([item["pkg"].endswith(f".{module}") for module in to_delete])
-]
-legacy_index.extend(local_index)
-legacy_index.sort(key=lambda x: x["pkg"])
-
-def extract_extension_lib(version: str) -> str:
-    if match := re.search(r'(\d+)\.(\d+)', version):
-        return f"{match.group(1)}.{match.group(2)}"
-
-    raise ValueError(f"Version {version} doesn't contain MAJOR.MINOR")
-
-index = index_pb2.Index(
-    name = "Keiyoushi",
-    badgeLabel = "KEI",
-    signingKey = "9add655a78e96c4ec7a53ef89dccb557cb5d767489fac5e785d671a5a75d4da2",
-    contact=index_pb2.Contact(
-        website="https://keiyoushi.github.io",
-        discord="https://discord.gg/3FbCpdKbdY"
-    ),
-    extensionList=index_pb2.ExtensionList(
-        extensions=[
-            index_pb2.Extension(
-                name=extension["name"].replace("Tachiyomi: ", ""),
-                packageName=extension["pkg"],
-                resources=index_pb2.Resources(
-                    apkUrl=f"https://raw.githubusercontent.com/keiyoushi/extensions/refs/heads/repo/apk/{extension["apk"]}",
-                    iconUrl=f"https://raw.githubusercontent.com/keiyoushi/extensions/refs/heads/repo/icon/{extension["pkg"]}.png",
-                ),
-                extensionLib=str(extension.get("libVersion")) if "libVersion" in extension else extract_extension_lib(extension["version"]),
-                versionCode=extension["code"],
-                versionName=extension["version"],
-                contentWarning=int(extension.get("contentWarning", 2 if extension.get("nsfw", 0) == 1 else 0)) + 1,
-                sources=[
-                    index_pb2.Source(
-                        id=int(source["id"]),
-                        name=source["name"],
-                        language=source["lang"],
-                        homeUrl=source["baseUrl"],
-                    )
-                    for source in extension["sources"]
-                ]
-            )
-            for extension in legacy_index
-        ]
-    )
+shutil.copytree(
+    src=LOCAL_REPO.joinpath("apk"), dst=REMOTE_REPO.joinpath("apk"), dirs_exist_ok=True
+)
+shutil.copytree(
+    src=LOCAL_REPO.joinpath("icon"),
+    dst=REMOTE_REPO.joinpath("icon"),
+    dirs_exist_ok=True,
 )
 
-with REMOTE_REPO.joinpath("index.json").open("w", encoding="utf-8") as index_file:
-    index_file.write(json_format.MessageToJson(index, always_print_fields_with_no_presence=False, preserving_proto_field_name=True))
+with REMOTE_REPO.joinpath("index.json").open() as f:
+    remote_proto = json_format.Parse(f.read(), index_pb2.Index())
 
-with REMOTE_REPO.joinpath("index.pb").open("wb") as index_pb_file:
-    index_pb_file.write(index.SerializeToString())
+with LOCAL_REPO.joinpath("index.json").open() as f:
+    local_proto = json_format.Parse(f.read(), index_pb2.ExtensionList())
 
-with REMOTE_REPO.joinpath("index.pb.gz").open("wb") as index_pb_file:
-    index_pb_file.write(gzip.compress(index.SerializeToString()))
+all_extensions = [
+    ext
+    for ext in remote_proto.extensionList.extensions
+    if not any(ext.packageName.endswith(f".{module}") for module in to_delete)
+]
+all_extensions.extend(local_proto.extensions)
+all_extensions.sort(key=lambda ext: ext.packageName)
 
-legacy_json_index = []
-for entry in legacy_index:
-    legacy_entry = entry.copy()
-    cw = legacy_entry.get("contentWarning", 0)
-    legacy_entry["nsfw"] = 1 if cw > 1 else 0
-    legacy_entry.pop("libVersion", None)
-    legacy_entry.pop("contentWarning", None)
-    legacy_json_index.append(legacy_entry)
+index = index_pb2.Index(
+    name="Keiyoushi",
+    badgeLabel="KEI",
+    signingKey="9add655a78e96c4ec7a53ef89dccb557cb5d767489fac5e785d671a5a75d4da2",
+    contact=index_pb2.Contact(
+        website="https://keiyoushi.github.io", discord="https://discord.gg/3FbCpdKbdY"
+    ),
+    extensionList=index_pb2.ExtensionList(extensions=all_extensions),
+)
 
-with REMOTE_REPO.joinpath("index.min.json").open("w", encoding="utf-8") as index_min_file:
-    json.dump(legacy_json_index, index_min_file, ensure_ascii=False, separators=(",", ":"))
+with REMOTE_REPO.joinpath("index.json").open("w", encoding="utf-8") as f:
+    f.write(
+        json_format.MessageToJson(
+            index,
+            always_print_fields_with_no_presence=False,
+            preserving_proto_field_name=True,
+        )
+    )
 
-with REMOTE_REPO.joinpath("index.html").open("w", encoding="utf-8") as index_html_file:
-    index_html_file.write('<!DOCTYPE html>\n<html>\n<head>\n<meta charset="UTF-8">\n<title>apks</title>\n</head>\n<body>\n<pre>\n')
-    for entry in legacy_index:
-        apk_escaped = 'apk/' + html.escape(entry["apk"])
-        name_escaped = html.escape(entry["name"])
-        index_html_file.write(f'<a href="{apk_escaped}">{name_escaped}</a>\n')
-    index_html_file.write('</pre>\n</body>\n</html>\n')
+with REMOTE_REPO.joinpath("index.pb").open("wb") as f:
+    f.write(index.SerializeToString())
+
+with REMOTE_REPO.joinpath("index.pb.gz").open("wb") as f:
+    f.write(gzip.compress(index.SerializeToString()))
+
+
+def get_legacy_lang(ext) -> str:
+    apk_filename = ext.resources.apkUrl.split("/")[-1]
+    lang = LANGUAGE_REGEX.search(apk_filename).group(1)
+    if len(ext.sources) == 1:
+        source_language = ext.sources[0].language
+        if (
+            source_language != lang
+            and source_language not in {"all", "other"}
+            and lang not in {"all", "other"}
+        ):
+            lang = source_language
+    return lang
+
+
+legacy_json_index = [
+    {
+        "name": f"Tachiyomi: {ext.name}",
+        "pkg": ext.packageName,
+        "apk": ext.resources.apkUrl.split("/")[-1],
+        "lang": get_legacy_lang(ext),
+        "code": ext.versionCode,
+        "version": ext.versionName,
+        "nsfw": 1 if ext.contentWarning > 2 else 0,
+        "sources": [
+            {
+                "name": source.name,
+                "lang": source.language,
+                "id": str(source.id),
+                "baseUrl": source.homeUrl,
+            }
+            for source in ext.sources
+        ],
+    }
+    for ext in all_extensions
+]
+
+with REMOTE_REPO.joinpath("index.min.json").open("w", encoding="utf-8") as f:
+    json.dump(legacy_json_index, f, ensure_ascii=False, separators=(",", ":"))
+
+with REMOTE_REPO.joinpath("index.html").open("w", encoding="utf-8") as f:
+    f.write(
+        '<!DOCTYPE html>\n<html>\n<head>\n<meta charset="UTF-8">\n<title>apks</title>\n</head>\n<body>\n<pre>\n'
+    )
+    for ext in all_extensions:
+        apk_escaped = html.escape(ext.resources.apkUrl)
+        name_escaped = html.escape(f"Tachiyomi: {ext.name}")
+        f.write(f'<a href="{apk_escaped}">{name_escaped}</a>\n')
+    f.write("</pre>\n</body>\n</html>\n")
