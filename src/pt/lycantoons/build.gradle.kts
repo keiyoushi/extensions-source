a657dc2cdc7a36955edaae973c2aa1ba583e41d8
iq8 <266871934+disk-iq8@users.noreply.github.com>
2026-06-28 02:22:22 +0300

    switch to nextJs on chapterList

--- src/pt/lycantoons/build.gradle.kts
+++ src/pt/lycantoons/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "Lycan Toons"
     className = "LycanToons"
-    versionCode = 5
+    versionCode = 6
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
     baseUrl = "https://lycantoons.com"
