a1d9349a15f80e8ffe58ea43218a05ad340d8a1d
iq8 <266871934+disk-iq8@users.noreply.github.com>
2026-06-28 12:17:08 +0300

    Update selectors

--- src/en/aquamanga/build.gradle.kts
+++ src/en/aquamanga/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "Aqua Manga"
     className = "AquaManga"
-    versionCode = 11
+    versionCode = 12
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
     theme = "madara"
