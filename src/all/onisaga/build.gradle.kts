d3c39511b11f7e3b45d921305ed2ffbd0010dbf3
Alpha-782 <253939700+Alpha-782@users.noreply.github.com>
2026-06-28 13:18:09 +0300

    OniSaga [Multi]: Update NSFW Filtering (#17040)
    * Update NSFW filtering
    
    * Bump

--- src/all/onisaga/build.gradle.kts
+++ src/all/onisaga/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "OniSaga"
     className = "OniSagaFactory"
-    versionCode = 1
+    versionCode = 2
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
 
