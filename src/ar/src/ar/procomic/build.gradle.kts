55e7847c992c73a6068158710f2e420c2d9a6493
abedelkaderhachimi120-alt <abedelkaderhachimi120@gmail.com>
2026-06-28 19:55:21 +0100

    Create build.gradle.kts

---
+++ src/ar/src/ar/procomic/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "Procomic"
+    className = "Procomic"
+    versionCode = 0
+    contentWarning = ContentWarning.NSFW
+    libVersion = "1.4"
+    theme = "madara"
+    baseUrl = "https://procomic.pro/"
+}
