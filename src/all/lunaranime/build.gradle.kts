9dc2574e3814fb8aaa20fac4e7c4d6c778b71a36
disk-iq8 <grinersam14@gmail.com>
2026-06-29 11:27:29 +0300

    Lunar Manga (Multi): Fix key loading (#17081)
    Update key detection
    
    Co-authored-by: iq8 <266871934+disk-iq8@users.noreply.github.com>

--- src/all/lunaranime/build.gradle.kts
+++ src/all/lunaranime/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "Lunar Manga"
     className = "LunarAnimeFactory"
-    versionCode = 9
+    versionCode = 10
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
 }
