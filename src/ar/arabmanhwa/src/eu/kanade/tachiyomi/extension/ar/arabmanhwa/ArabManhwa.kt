490823ce1fa0f70f7d6a01853dc73e4f7c1003bd
yemen <hanihameda@gmail.com>
2026-06-29 21:13:38 +0300

    إضافة مصدر arabmanhwa الجديد

---
+++ src/ar/arabmanhwa/src/eu/kanade/tachiyomi/extension/ar/arabmanhwa/ArabManhwa.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.ar.arabmanhwa
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class ArabManhwa : Madara("ArabManhwa", "https://arabmanhwa.com", "ar") {
+    override val mangaSubString = "manga"
+}
