98c729115985203850602924a203d2f7eb8b479a
ZenVinny <101535789+ZenVinny@users.noreply.github.com>
2026-06-29 18:35:30 -0500

    Change content warning from NSFW to MIXED

--- src/ja/dokiraw/build.gradle.kts
+++ src/ja/dokiraw/build.gradle.kts
@@ -6,7 +6,7 @@ keiyoushi {
     name = "Dokiraw"
     className = "Dokiraw"
     versionCode = 4
-    contentWarning = ContentWarning.NSFW
+    contentWarning = ContentWarning.MIXED
     libVersion = "1.4"
     theme = "liliana"
     baseUrl = "https://dokiraw.win"
