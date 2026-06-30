bdcbdcf7f7a00e352e6d6bfa49ea2da0d1ee3c17
Rafou376 <108695858+Rafou376@users.noreply.github.com>
2026-06-30 16:39:56 +0200

    Roumanwu (ZH): migrate to new dsl (#17163)
    * Roumanwu (ZH): update mirrors and cleanup
    
    * migrate to new dsl

--- src/zh/roumanwu/build.gradle.kts
+++ src/zh/roumanwu/build.gradle.kts
@@ -5,7 +5,18 @@ plugins {
 keiyoushi {
     name = "Roumanwu"
     className = "Roumanwu"
-    versionCode = 18
+    versionCode = 19
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
+    source {
+        lang = "zh"
+
+        // 地址: https://rou.pub/dizhi or https://rdz3.xyz/dizhi
+        baseUrl("https://rouman5.com") {
+            mirrors = listOf(
+                "https://roum27.xyz",
+            )
+        }
+    }
 }
