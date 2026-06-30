491a5533baedcdbe55bac046b24317f420033eb3
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-06-30 21:35:38 +0700

    GocTruyenTranhVui to new dsl

--- src/vi/goctruyentranhvui/build.gradle.kts
+++ src/vi/goctruyentranhvui/build.gradle.kts
@@ -9,6 +9,13 @@ keiyoushi {
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
 
+    source {
+        lang = "vi"
+        baseUrl("https://goctruyentranhvui30.com") {
+            withCustom.set(true)
+        }
+    }
+
     deeplink {
         host("goctruyentranhvui30.com")
         path("/truyen/..*")
