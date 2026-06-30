112459e9d3b035bc0cfffa8bb3221eac57cbc6fd
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-06-30 21:43:13 +0700

    VinaHentai to new dsl

--- src/vi/vinahentai/build.gradle.kts
+++ src/vi/vinahentai/build.gradle.kts
@@ -8,4 +8,11 @@ keiyoushi {
     versionCode = 10
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
+    source {
+        lang = "vi"
+        baseUrl("https://vinahentai.cloud") {
+            withCustom.set(true)
+        }
+    }
 }
