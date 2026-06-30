56b6d37e392c293dc84ee96c9089b34176869e8a
Secozzi <49240133+Secozzi@users.noreply.github.com>
2026-06-30 13:11:14 +0200

    Move Zmanga to new dsl (#17121)
    * Update multisrc class
    
    * Automated migration

--- src/id/maidmanga/build.gradle.kts
+++ src/id/maidmanga/build.gradle.kts
@@ -4,10 +4,13 @@ plugins {
 
 keiyoushi {
     name = "Maid - Manga"
-    className = "MaidManga"
     versionCode = 10
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "zmanga"
-    baseUrl = "https://www.maid.my.id"
+
+    source {
+        lang = "id"
+        baseUrl = "https://www.maid.my.id"
+    }
 }
