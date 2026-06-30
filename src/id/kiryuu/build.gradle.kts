f63533620dcba73d55ecf59c81227e91f3069e07
Secozzi <secozzi@proton.me>
2026-06-30 13:19:04 +0200

    Automatic migration

--- src/id/kiryuu/build.gradle.kts
+++ src/id/kiryuu/build.gradle.kts
@@ -4,10 +4,15 @@ plugins {
 
 keiyoushi {
     name = "Kiryuu"
-    className = "Kiryuu"
     versionCode = 51
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "natsuid"
-    baseUrl = "https://v6.kiryuu.to"
+
+    source {
+        lang = "id"
+        baseUrl = "https://v6.kiryuu.to"
+        // Formerly "Kiryuu (WP Manga Stream)"
+        id = 3639673976007021338L
+    }
 }
