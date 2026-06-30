fd7b24ee3b4f1a00181d39f7eabf755749f2d41b
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-06-30 21:41:12 +0700

    Otruyen to new dsl

--- src/vi/otruyen/build.gradle.kts
+++ src/vi/otruyen/build.gradle.kts
@@ -8,4 +8,9 @@ keiyoushi {
     versionCode = 2
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
+
+    source {
+        lang = "vi"
+        baseUrl = "https://otruyen.cc"
+    }
 }
