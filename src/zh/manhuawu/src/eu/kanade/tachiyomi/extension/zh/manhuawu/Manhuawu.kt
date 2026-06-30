d1b79e9b2c3be60017d57a15508fd5e3e6eab29e
Secozzi <secozzi@proton.me>
2026-06-30 22:00:23 +0200

    Automatic migration

--- src/zh/manhuawu/src/eu/kanade/tachiyomi/extension/zh/manhuawu/Manhuawu.kt
+++ src/zh/manhuawu/src/eu/kanade/tachiyomi/extension/zh/manhuawu/Manhuawu.kt
@@ -1,5 +1,7 @@
 package eu.kanade.tachiyomi.extension.zh.manhuawu
 
 import eu.kanade.tachiyomi.multisrc.mccms.MCCMSWeb
+import keiyoushi.annotation.Source
 
-class Manhuawu : MCCMSWeb("漫画屋", "https://www.mhua5.com")
+@Source
+abstract class Manhuawu : MCCMSWeb()
