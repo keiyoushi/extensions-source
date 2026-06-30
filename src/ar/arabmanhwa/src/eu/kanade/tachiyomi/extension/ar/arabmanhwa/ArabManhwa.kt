5884a95289567e01ab88aae43610f9f7a35058b4
yemen <hanihameda@gmail.com>
2026-06-30 18:26:56 +0300

    fix extensions

--- src/ar/arabmanhwa/src/eu/kanade/tachiyomi/extension/ar/arabmanhwa/ArabManhwa.kt
+++ src/ar/arabmanhwa/src/eu/kanade/tachiyomi/extension/ar/arabmanhwa/ArabManhwa.kt
@@ -2,12 +2,10 @@ package eu.kanade.tachiyomi.extension.ar.arabmanhwa
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class ArabManhwa :
-    Madara(
-        "ArabManhwa",
-        "https://arabmanhwa.com",
-        "ar",
-    ) {
+class ArabManhwa : Madara() {
+    override val name = "ArabManhwa"
+    override val baseUrl = "https://arabmanhwa.com"
+    override val lang = "ar"
 
-    override val mangaSubString = "manga"
+    override val useNewChapterEndpoint = true
 }
