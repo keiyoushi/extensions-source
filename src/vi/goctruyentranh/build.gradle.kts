80181dcd6d0449dcf8d587db058aef23a15ebb75
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-06-30 21:35:27 +0700

    GocTruyenTranh to new dsl

--- src/vi/goctruyentranh/build.gradle.kts
+++ src/vi/goctruyentranh/build.gradle.kts
@@ -8,4 +8,11 @@ keiyoushi {
     versionCode = 12
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
+    source {
+        lang = "vi"
+        baseUrl("https://goctruyentranh.com") {
+            withCustom.set(true)
+        }
+    }
 }
