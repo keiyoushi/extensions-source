f72e3a1edb46bc4a3f647775925cd1c2637e618f
Exinaus <exinaus@gmail.com>
2026-06-30 11:37:32 +0300

    MultiChan move to new dsl

--- src/ru/yaoichan/build.gradle.kts
+++ src/ru/yaoichan/build.gradle.kts
@@ -4,10 +4,16 @@ plugins {
 
 keiyoushi {
     name = "YaoiChan"
-    className = "YaoiChan"
     versionCode = 5
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "multichan"
-    baseUrl = "https://yaoi-chan.me"
+
+    source {
+        baseUrl("https://yaoi-chan.me") {
+            withCustom = true
+        }
+        lang = "ru"
+        id = 2466512768990363955L
+    }
 }
