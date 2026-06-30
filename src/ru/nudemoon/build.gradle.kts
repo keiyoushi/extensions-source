37256f97901d0df2a714782f7bc6cd719cd28c2a
Exinaus <exinaus@gmail.com>
2026-06-30 16:16:23 +0300

    NudeMoon move to new dsl

--- src/ru/nudemoon/build.gradle.kts
+++ src/ru/nudemoon/build.gradle.kts
@@ -2,10 +2,21 @@ plugins {
     alias(kei.plugins.extension)
 }
 
+dependencies {
+    implementation(project(":lib:cookieinterceptor"))
+}
+
 keiyoushi {
     name = "Nude-Moon"
     className = "Nudemoon"
     versionCode = 29
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
+    source {
+        baseUrl("https://nude-moon.org") {
+            withCustom = true
+        }
+        lang = "ru"
+    }
 }
