a990af7f85f359f467de358118cee7182a78c494
Secozzi <secozzi@proton.me>
2026-07-01 00:12:36 +0200

    Automatic migration

--- src/en/arcrelight/build.gradle.kts
+++ src/en/arcrelight/build.gradle.kts
@@ -4,10 +4,13 @@ plugins {
 
 keiyoushi {
     name = "Arc-Relight"
-    className = "ArcRelight"
     versionCode = 0
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
     theme = "mangadventure"
-    baseUrl = "https://arc-relight.com"
+
+    source {
+        lang = "en"
+        baseUrl = "https://arc-relight.com"
+    }
 }
