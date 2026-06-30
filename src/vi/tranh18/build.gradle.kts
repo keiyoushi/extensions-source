ac4ec06bbfdaa5bebbfb1b09839f9bf0357607d2
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-06-30 21:41:43 +0700

    Tranh18 to new dsl

--- src/vi/tranh18/build.gradle.kts
+++ src/vi/tranh18/build.gradle.kts
@@ -8,4 +8,11 @@ keiyoushi {
     versionCode = 5
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
+    source {
+        lang = "vi"
+        baseUrl("https://tranh18.cc") {
+            withCustom.set(true)
+        }
+    }
 }
