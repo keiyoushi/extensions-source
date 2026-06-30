b03f655449668e11d275cd179000f783f67a8c9e
Exinaus <exinaus@gmail.com>
2026-06-30 18:37:07 +0300

    Migrate LibGroup to new dsl

--- src/ru/yaoilib/build.gradle.kts
+++ src/ru/yaoilib/build.gradle.kts
@@ -4,10 +4,15 @@ plugins {
 
 keiyoushi {
     name = "YaoiLib"
-    className = "YaoiLib"
     versionCode = 5
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "libgroup"
-    baseUrl = "https://slashlib.me"
+
+    source {
+        baseUrl("https://slashlib.me") {
+            withCustom = true
+        }
+        lang = "ru"
+    }
 }
