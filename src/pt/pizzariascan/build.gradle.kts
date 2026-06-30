9f8bb4ef80c7f21254f08ae2e91745227e4204c8
Secozzi <secozzi@proton.me>
2026-06-30 22:49:32 +0200

    Automatic migration

--- src/pt/pizzariascan/build.gradle.kts
+++ src/pt/pizzariascan/build.gradle.kts
@@ -4,10 +4,14 @@ plugins {
 
 keiyoushi {
     name = "PizzariaScan"
-    className = "Pizzariascan"
     versionCode = 47
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "mangawork"
-    baseUrl = "https://pizzariacomics.com"
+
+    source {
+        lang = "pt-BR"
+        baseUrl = "https://pizzariacomics.com"
+        id = 3359822911747375789L
+    }
 }
