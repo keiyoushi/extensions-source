fca746e9c5bd11c0026648c4e7a5ef0d8e0ae0e7
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-06-30 21:42:45 +0700

    TruyenQQ to new dsl

--- src/vi/truyenqq/build.gradle.kts
+++ src/vi/truyenqq/build.gradle.kts
@@ -8,4 +8,11 @@ keiyoushi {
     versionCode = 24
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
+
+    source {
+        lang = "vi"
+        baseUrl("https://truyenqqko.com") {
+            withCustom.set(true)
+        }
+    }
 }
