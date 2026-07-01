5f6fd76deb5b92651c022d28742df4c527ae1f93
Rafou376 <108695858+Rafou376@users.noreply.github.com>
2026-07-01 15:15:36 +0200

    XGMN (ALL): migrate to new dsl

--- src/all/xgmn/build.gradle.kts
+++ src/all/xgmn/build.gradle.kts
@@ -4,8 +4,13 @@ plugins {
 
 keiyoushi {
     name = "XGMN"
-    className = "XGMN"
-    versionCode = 3
+    versionCode = 4
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
+    source {
+        lang = "all"
+        name = "性感美女"
+        baseUrl = "http://xgmn8.vip"
+    }
 }
