2dfabb011f3c8f1e21417c61f38f65dae0d51695
Rafou376 <108695858+Rafou376@users.noreply.github.com>
2026-07-01 10:46:02 +0200

    Komiic (ZH): migrate to new dsl (#17181)
    * Komiic (ZH): migrate to new dsl
    
    * add zh translation for mirrors

--- src/zh/komiic/src/eu/kanade/tachiyomi/extension/zh/komiic/Preferences.kt
+++ src/zh/komiic/src/eu/kanade/tachiyomi/extension/zh/komiic/Preferences.kt
@@ -3,20 +3,9 @@ package eu.kanade.tachiyomi.extension.zh.komiic
 import android.content.Context
 import androidx.preference.ListPreference
 
-const val BASE_URL_PREF = "BASE_URL"
 const val CHAPTER_FILTER_PREF = "CHAPTER_FILTER"
 
-val mirrorUrls = arrayOf("komiic.com", "komiic.cc")
-
 fun preferencesInternal(context: Context) = arrayOf(
-    ListPreference(context).apply {
-        key = BASE_URL_PREF
-        title = "鏡像站點"
-        summary = "註：各站點之間不共享登錄狀態"
-        entries = Array(mirrorUrls.size) { mirrorUrls[it] }
-        entryValues = Array(mirrorUrls.size) { it.toString() }
-        setDefaultValue("0")
-    },
     ListPreference(context).apply {
         key = CHAPTER_FILTER_PREF
         title = "章節列表顯示"
