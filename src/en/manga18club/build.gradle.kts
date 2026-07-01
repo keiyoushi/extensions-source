c710a480b79ebdfdb8e6941a8e5264d822fa7afb
Secozzi <49240133+Secozzi@users.noreply.github.com>
2026-07-01 10:47:14 +0200

    Move Manga18 to new dsl (#17203)
    * Update multisrc class
    
    * Automatic migration

--- src/en/manga18club/build.gradle.kts
+++ src/en/manga18club/build.gradle.kts
@@ -4,10 +4,13 @@ plugins {
 
 keiyoushi {
     name = "Manga18.Club"
-    className = "Manga18Club"
     versionCode = 0
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "manga18"
-    baseUrl = "https://manga18.club"
+
+    source {
+        lang = "en"
+        baseUrl = "https://manga18.club"
+    }
 }
--- src/zh/hanman18/build.gradle.kts
+++ src/zh/hanman18/build.gradle.kts
@@ -4,10 +4,13 @@ plugins {
 
 keiyoushi {
     name = "HANMAN18"
-    className = "HANMAN18"
     versionCode = 0
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "manga18"
-    baseUrl = "https://hanman18.com"
+
+    source {
+        lang = "zh"
+        baseUrl = "https://hanman18.com"
+    }
 }
