9377c8d4302158a7d1a799a06924ca9b94fabb75
darwin-256 <16855574+darwin-256@users.noreply.github.com>
2026-07-02 23:26:22 +0300

    src\id\mangakuri to DSL

--- src/id/mangakuri/build.gradle.kts
+++ src/id/mangakuri/build.gradle.kts
@@ -4,8 +4,13 @@ plugins {
 
 keiyoushi {
     name = "Mangakuri"
-    className = "Mangakuri"
     versionCode = 32
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
+    source {
+        lang = "id"
+        baseUrl = "https://lc1.mangakuri.online"
+        versionId = 2
+    }
 }
