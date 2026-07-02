6b6c1998ecfad54cafd0230485d6c5eea6af4319
darwin-256 <16855574+darwin-256@users.noreply.github.com>
2026-07-02 23:42:30 +0300

    src\vi\minotruyen to DSL

--- src/vi/minotruyen/build.gradle.kts
+++ src/vi/minotruyen/build.gradle.kts
@@ -4,13 +4,35 @@ plugins {
 
 keiyoushi {
     name = "MinoTruyen"
-    className = "MinoTruyenFactory"
     versionCode = 3
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
+    source {
+        name = "MinoTruyen Manga"
+        lang = "vi"
+        baseUrl("https://minotruyenv5.xyz") {
+            withCustom.set(true)
+        }
+    }
+
+    source {
+        name = "MinoTruyen Comics"
+        lang = "vi"
+        baseUrl("https://minotruyenv5.xyz") {
+            withCustom.set(true)
+        }
+    }
+
+    source {
+        name = "MinoTruyen Hentai"
+        lang = "vi"
+        baseUrl("https://minotruyenv5.xyz") {
+            withCustom.set(true)
+        }
+    }
 }
 
 dependencies {
-
     implementation(project(":lib:cryptoaes"))
 }
