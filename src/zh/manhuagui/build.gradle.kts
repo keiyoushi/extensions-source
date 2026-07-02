ddf501081a633b8aa5090e36078649f54b570080
Secozzi <secozzi@proton.me>
2026-07-02 17:15:57 +0200

    Move zh/manhuagui to new dsl

--- src/zh/manhuagui/build.gradle.kts
+++ src/zh/manhuagui/build.gradle.kts
@@ -9,6 +9,18 @@ keiyoushi {
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
 
+    source {
+        name = "漫画柜"
+        lang = "zh"
+        baseUrl("https://www.manhuagui.com") {
+            mirrors = listOf(
+                "https://tw.manhuagui.com",
+                "https://www.mhgui.com",
+                "https://tw.mhgui.com",
+            )
+        }
+    }
+
     deeplink {
         host("manhuagui.com")
         host("m.manhuagui.com")
