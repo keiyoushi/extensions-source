94c9e9be52eb1533ff095ed2444a7855bc28447e
Forza06 <159862527+Forza06@users.noreply.github.com>
2026-07-02 08:26:49 +0300

    RagnarScans (tr): Fix domain and chapter list parsing (#17229)

--- src/tr/ragnarscans/src/eu/kanade/tachiyomi/extension/tr/ragnarscans/RagnarScans.kt
+++ src/tr/ragnarscans/src/eu/kanade/tachiyomi/extension/tr/ragnarscans/RagnarScans.kt
@@ -1,7 +1,12 @@
 package eu.kanade.tachiyomi.extension.tr.ragnarscans
 
 import eu.kanade.tachiyomi.multisrc.initmanga.InitManga
+import eu.kanade.tachiyomi.source.model.SChapter
 import keiyoushi.annotation.Source
+import keiyoushi.utils.tryParse
+import org.jsoup.nodes.Element
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Source
 abstract class RagnarScans : InitManga() {
@@ -9,4 +14,19 @@ abstract class RagnarScans : InitManga() {
     override val mangaUrlDirectory = "manga"
 
     override val popularUrlSlug = "en-cok-takip-edilenler"
+
+    private val ragnarDateFormat = SimpleDateFormat("d MMMM yyyy HH:mm", Locale("tr"))
+
+    override fun chapterListSelector() = "div.chapter-list > div.uk-position-relative"
+
+    override fun chapterFromElement(element: Element) = SChapter.create().apply {
+        setUrlWithoutDomain(element.selectFirst("a")!!.absUrl("href"))
+
+        name = element.selectFirst("div.uk-flex-none")?.text()
+            ?: element.select("a").text()
+
+        val dateStr = element.selectFirst("div.uk-text-meta")?.attr("uk-tooltip")
+            ?.substringAfter("title: ")?.substringBefore(";")
+        date_upload = ragnarDateFormat.tryParse(dateStr)
+    }
 }
