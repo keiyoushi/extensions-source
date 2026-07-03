0a9b594292083f31a0e985d841c5e8be17cbaecb
darwin-256 <16855574+darwin-256@users.noreply.github.com>
2026-07-03 23:10:01 +0300

    src\all\komga to DSL

--- src/all/komga/build.gradle.kts
+++ src/all/komga/build.gradle.kts
@@ -4,13 +4,18 @@ plugins {
 
 keiyoushi {
     name = "Komga"
-    className = "KomgaFactory"
-    versionCode = 65
+    versionCode = 66
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
+
+    source {
+        name = "Komga"
+        lang = "all"
+        baseUrl = "https://example.com" // Dummy URL, overridden by skipCodeGen = true
+        skipCodeGen = true
+    }
 }
 
 dependencies {
-
     implementation("org.apache.commons:commons-text:1.11.0")
 }
