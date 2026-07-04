7083785c8f43cf7ae993cb81b650a1dead84e69a
Anh Vũ <yu.development@gmail.com>
2026-07-05 00:03:05 +0700

    Add missing seconds lib

--- src/vi/dilib/src/eu/kanade/tachiyomi/extension/vi/dilib/Dilib.kt
+++ src/vi/dilib/src/eu/kanade/tachiyomi/extension/vi/dilib/Dilib.kt
@@ -14,6 +14,7 @@ import keiyoushi.utils.firstInstanceOrNull
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
+import kotlin.time.Duration.Companion.second
 
 @Source
 abstract class Dilib : HttpSource() {
