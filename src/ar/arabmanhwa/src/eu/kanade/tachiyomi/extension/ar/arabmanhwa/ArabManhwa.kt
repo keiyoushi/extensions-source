9e99f74c2dd5a8317090f34c7a44e61c3303be50
yemen <hanihameda@gmail.com>
2026-07-04 09:18:04 +0300

    feat(ar): add Brown Manga source

--- src/ar/arabmanhwa/src/eu/kanade/tachiyomi/extension/ar/arabmanhwa/ArabManhwa.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.ar.arabmanhwa
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class ArabManhwa : Madara("ArabManhwa", "https://arabmanhwa.com", "ar") {
-    override val useNewChapterEndpoint = true
-}
