006e97441463138899f0b9ad758d8e7faa4829a8
yemen <hanihameda@gmail.com>
2026-07-05 11:22:52 +0300

    Add all recent changes for BrownManga

---
+++ src/ar/brownmanga/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "Brown Manga"
+    versionCode = 1
+    contentWarning = ContentWarning.SAFE
+    libVersion = "1.4"
+
+    source {
+        lang = "ar"
+        baseUrl = "https://brownmanga.site"
+        skipCodeGen.set(true)
+    }
+}
