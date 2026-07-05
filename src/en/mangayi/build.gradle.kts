d4e8a04c01c04db8728e10e6b2019a70a68731ef
darwin-256 <16855574+darwin-256@users.noreply.github.com>
2026-07-05 21:28:35 +0300

    Add MangaYi

---
+++ src/en/mangayi/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "MangaYi"
+    versionCode = 1
+    contentWarning = ContentWarning.SAFE
+    libVersion = "1.4"
+
+    source {
+        lang = "en"
+        baseUrl = "https://mangayi.com"
+    }
+}
