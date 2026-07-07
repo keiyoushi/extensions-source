5c2be51e3dacc47675ac8dcc9a510af4e06adca8
Exinaus <exinaus@gmail.com>
2026-07-07 15:45:06 +0300

    remove search dto also

--- src/en/templescan/src/eu/kanade/tachiyomi/extension/en/templescan/Dto.kt
+++ src/en/templescan/src/eu/kanade/tachiyomi/extension/en/templescan/Dto.kt
@@ -8,26 +8,6 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 @Serializable
-class SearchWrapper(
-    val total: Int,
-    val page: Int,
-    val projects: List<SearchSeries> = emptyList(),
-)
-
-@Serializable
-class SearchSeries(
-    private val title: String,
-    @SerialName("series_slug") private val slug: String,
-    private val thumbnail: String,
-) {
-    fun toSManga() = SManga.create().apply {
-        url = "/comic/$slug"
-        title = this@SearchSeries.title
-        thumbnail_url = thumbnail
-    }
-}
-
-@Serializable
 class BrowseSeries(
     @SerialName("series_slug") private val slug: String,
     val title: String,
