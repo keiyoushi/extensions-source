7a006d3cd3ce95b219fb135160b6d4bf8b65939a
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-07-09 11:31:15 +0500

    add HentaiLoop (#15074)
    * add HentaiLoop
    
    * move to separate pr
    
    * icons
    
    * update
    
    * count views

---
+++ src/all/hentailoop/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "HentaiLoop"
+    versionCode = 1
+    contentWarning = ContentWarning.NSFW
+    libVersion = "1.4"
+
+    source {
+        lang = "all"
+        baseUrl = "https://hentailoop.com"
+    }
+
+    deeplink {
+        host("hentailoop.com")
+        path("/manga/..*")
+    }
+}
