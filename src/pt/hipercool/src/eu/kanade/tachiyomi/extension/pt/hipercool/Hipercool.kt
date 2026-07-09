dc2c9abbc1ad60228f0918d78eee5b4f23941fcd
iq8 <266871934+disk-iq8@users.noreply.github.com>
2026-07-10 02:35:54 +0300

    Fix HiperCool auth

--- src/pt/hipercool/src/eu/kanade/tachiyomi/extension/pt/hipercool/Hipercool.kt
+++ src/pt/hipercool/src/eu/kanade/tachiyomi/extension/pt/hipercool/Hipercool.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.extension.pt.hipercool
 
 import eu.kanade.tachiyomi.multisrc.hiper.Hiper
+import eu.kanade.tachiyomi.network.GET
 import keiyoushi.annotation.Source
 import keiyoushi.network.rateLimit
 import kotlin.time.Duration.Companion.seconds
@@ -8,7 +9,21 @@ import kotlin.time.Duration.Companion.seconds
 @Source
 abstract class Hipercool : Hiper() {
 
+    private var cookieFetched = false
+
+    private val acceptHeaders = headersBuilder()
+        .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+        .build()
+
     override val client = network.client.newBuilder()
+        // Fetch baseUrl with accept headers which populates a needed cookie.
+        .addInterceptor { chain ->
+            if (!cookieFetched) {
+                network.client.newCall(GET(baseUrl, acceptHeaders)).execute().close()
+                cookieFetched = true
+            }
+            chain.proceed(chain.request())
+        }
         .rateLimit(3, 1.seconds)
         .build()
 }
