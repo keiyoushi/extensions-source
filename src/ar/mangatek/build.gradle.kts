a1fbeff9b2abd331bd64f489498ddba9d5ea84d1
Abdelkaderhachimi <35611391+Abdelkaderhachimi@users.noreply.github.com>
2026-07-10 10:49:18 +0100

    Update libVersion to 1.41 in build.gradle.kts

--- src/ar/mangatek/build.gradle.kts
+++ src/ar/mangatek/build.gradle.kts
@@ -6,7 +6,7 @@ keiyoushi {
     name = "MangaTek"
     versionCode = 3
     contentWarning = ContentWarning.SAFE
-    libVersion = "1.4"
+    libVersion = "1.41"
 
     source {
         lang = "ar"
