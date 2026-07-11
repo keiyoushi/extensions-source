3ac387ff7ce2461c35e8abd9ef3d57ff4589f267
ZenVinny <101535789+ZenVinny@users.noreply.github.com>
2026-07-11 01:47:29 -0500

    Change content warning from NSFW to MIXED

--- src/en/mangatown/build.gradle.kts
+++ src/en/mangatown/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "Mangatown"
     versionCode = 10
-    contentWarning = ContentWarning.NSFW
+    contentWarning = ContentWarning.MIXED
     libVersion = "1.4"
 
     source {
