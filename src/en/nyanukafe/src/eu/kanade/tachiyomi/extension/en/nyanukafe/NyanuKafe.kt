e4a35389b640cf295202eb5376cd9d58acf0e1f4
Prem Kumar <prem12321kumar@gmail.com>
2026-07-13 03:56:52 +0530

    Keyoapp: Add/Fix Alt Titles

--- src/en/nyanukafe/src/eu/kanade/tachiyomi/extension/en/nyanukafe/NyanuKafe.kt
+++ src/en/nyanukafe/src/eu/kanade/tachiyomi/extension/en/nyanukafe/NyanuKafe.kt
@@ -7,7 +7,6 @@ import keiyoushi.annotation.Source
 abstract class NyanuKafe : Keyoapp() {
     override fun popularMangaSelector(): String = ".series-splide .splide__slide:not(.splide__slide--clone)"
 
-    override val descriptionSelector: String = "div.grid > div#expand_content > p"
     override val statusSelector: String = "div.w-full.flex-wrap > div:eq(3) > div:last-child"
     override val authorSelector: String = "div.w-full.flex-wrap > div:eq(0) > div:last-child"
     override val artistSelector: String = "div.w-full.flex-wrap > div:eq(1) > div:last-child"
