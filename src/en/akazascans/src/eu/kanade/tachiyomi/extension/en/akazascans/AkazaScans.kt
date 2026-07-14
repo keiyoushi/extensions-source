89e924580b36778cdfd1d00e305aef1f944f9065
Anh Vũ <yu.development@gmail.com>
2026-07-14 14:38:16 +0700

    Add Akaza Scans (#17604)

---
+++ src/en/akazascans/src/eu/kanade/tachiyomi/extension/en/akazascans/AkazaScans.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.akazascans
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import keiyoushi.annotation.Source
+
+@Source
+abstract class AkazaScans : MangaThemesia()
