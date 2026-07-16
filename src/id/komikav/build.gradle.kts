dcfb7bada026544916a8a00e792690575488d311
Muhammad Brian Abdillah <mohammadbrianabdillah@gmail.com>
2026-07-16 19:14:07 +0700

    Apkomik: Add Mimick Browser-like Headers (#17624)
    * APKOMIK: add browser-like headers to image requests
    
    * APKOMIK: update version code

--- src/id/komikav/build.gradle.kts
+++ src/id/komikav/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 keiyoushi {
     name = "APKOMIK"
-    versionCode = 5
+    versionCode = 6
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
     theme = "mangathemesia"
--- src/id/komikav/src/eu/kanade/tachiyomi/extension/id/komikav/Apkomik.kt
+++ src/id/komikav/src/eu/kanade/tachiyomi/extension/id/komikav/Apkomik.kt
@@ -1,8 +1,11 @@
 package eu.kanade.tachiyomi.extension.id.komikav
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.Page
 import keiyoushi.annotation.Source
 import keiyoushi.network.rateLimit
+import okhttp3.Request
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -15,4 +18,17 @@ abstract class Apkomik : MangaThemesia() {
         .build()
 
     override val hasProjectPage = true
+
+    override fun imageRequest(page: Page): Request {
+        val newHeaders = headersBuilder()
+            .add("Accept", "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8")
+            .add("Referer", "$baseUrl/")
+            .add("Sec-GPC", "1")
+            .add("Sec-Fetch-Dest", "image")
+            .add("Sec-Fetch-Mode", "no-cors")
+            .add("Sec-Fetch-Site", "same-origin")
+            .build()
+
+        return GET(page.imageUrl!!, newHeaders)
+    }
 }
