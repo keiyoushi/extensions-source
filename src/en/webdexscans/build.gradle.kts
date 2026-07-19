f10b65c2a712113cb8e39ca82f06a312c0e77b5c
Secozzi <secozzi@proton.me>
2026-07-19 14:28:36 +0200

    Migrate WebDexScans to 1.6

--- src/en/webdexscans/build.gradle.kts
+++ src/en/webdexscans/build.gradle.kts
@@ -6,13 +6,18 @@ plugins {
 
 keiyoushi {
     name = "Webdex Scans"
-    versionCode = 52
+    versionCode = 53
     contentWarning = ContentWarning.SAFE
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "en"
         baseUrl = "https://webdexscans.com"
+        // Hardcode versionId to force users to migrate their old Madara entries.
         versionId = 2
+    }
+
+    deeplink {
+        path("/series/..*")
     }
 }
