41c0a0f37e9056318d9ed0dc24f97d7490b49cdd
FourTOne5 <107297513+FourTOne5@users.noreply.github.com>
2026-07-21 17:33:28 +0600

    Stop populating legacy index file (#17795)
    The apps only supporting legacy index are not supported anyway.

--- .github/scripts/publish-repo.py
+++ .github/scripts/publish-repo.py
@@ -16,7 +16,6 @@ import index_pb2
 APPLICATION_ICON_320_REGEX = re.compile(
     r"^application-icon-320:'([^']+)'", re.MULTILINE
 )
-LANGUAGE_REGEX = re.compile(r"tachiyomi-([^.]+)")
 
 
 @cache
@@ -151,46 +150,6 @@ with REPO_DIR.joinpath("index.json").open("w", encoding="utf-8") as f:
 
 with REPO_DIR.joinpath("index.pb").open("wb") as f:
     f.write(gzip.compress(index.SerializeToString()))
-
-
-def get_legacy_lang(ext) -> str:
-    apk_filename = ext.resources.apkUrl.split("/")[-1]
-    lang = LANGUAGE_REGEX.search(apk_filename).group(1)
-    if len(ext.sources) == 1:
-        source_language = ext.sources[0].language
-        if (
-            source_language != lang
-            and source_language not in {"all", "other"}
-            and lang not in {"all", "other"}
-        ):
-            lang = source_language
-    return lang
-
-
-legacy_json_index = [
-    {
-        "name": f"Tachiyomi: {ext.name}",
-        "pkg": ext.packageName,
-        "apk": ext.resources.apkUrl.split("/")[-1],
-        "lang": get_legacy_lang(ext),
-        "code": ext.versionCode,
-        "version": ext.versionName,
-        "nsfw": 1 if ext.contentWarning > 2 else 0,
-        "sources": [
-            {
-                "name": source.name,
-                "lang": source.language,
-                "id": str(source.id),
-                "baseUrl": source.homeUrl,
-            }
-            for source in ext.sources
-        ],
-    }
-    for ext in all_extensions
-]
-
-with REPO_DIR.joinpath("index.min.json").open("w", encoding="utf-8") as f:
-    json.dump(legacy_json_index, f, ensure_ascii=False, separators=(",", ":"))
 
 with REPO_DIR.joinpath("index.html").open("w", encoding="utf-8") as f:
     f.write(
