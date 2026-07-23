0358c510dde4e027f8c72ad90d03bae5ce7ac7aa
風来坊 <306338673+furaibo9714@users.noreply.github.com>
2026-07-24 00:48:09 +0200

    PizzaReader multisrc: extlib 1.6

--- src/all/hniscantrad/src/eu/kanade/tachiyomi/extension/all/hniscantrad/HNIScantrad.kt
+++ src/all/hniscantrad/src/eu/kanade/tachiyomi/extension/all/hniscantrad/HNIScantrad.kt
@@ -1,29 +1,7 @@
 package eu.kanade.tachiyomi.extension.all.hniscantrad
 
 import eu.kanade.tachiyomi.multisrc.pizzareader.PizzaReader
-import eu.kanade.tachiyomi.source.model.SManga
 import keiyoushi.annotation.Source
-import kotlinx.serialization.json.Json
 
 @Source
-abstract class HNIScantrad : PizzaReader() {
-    override val json = Json {
-        ignoreUnknownKeys = true
-        coerceInputValues = true
-    }
-
-    override fun String.toStatus(): Int = if (isEmpty()) {
-        SManga.UNKNOWN
-    } else {
-        when (substring(0, 7)) {
-            "In cors" -> SManga.ONGOING
-            "On goin" -> SManga.ONGOING
-            "Complet" -> SManga.COMPLETED
-            "Conclus" -> SManga.COMPLETED
-            "Conclud" -> SManga.COMPLETED
-            "Licenzi" -> SManga.LICENSED
-            "License" -> SManga.LICENSED
-            else -> SManga.UNKNOWN
-        }
-    }
-}
+abstract class HNIScantrad : PizzaReader()
--- src/fr/mangacorporation/src/eu/kanade/tachiyomi/extension/fr/mangacorporation/MangaCorporation.kt
+++ src/fr/mangacorporation/src/eu/kanade/tachiyomi/extension/fr/mangacorporation/MangaCorporation.kt
@@ -2,12 +2,6 @@ package eu.kanade.tachiyomi.extension.fr.mangacorporation
 
 import eu.kanade.tachiyomi.multisrc.pizzareader.PizzaReader
 import keiyoushi.annotation.Source
-import kotlinx.serialization.json.Json
 
 @Source
-abstract class MangaCorporation : PizzaReader() {
-    override val json: Json = Json {
-        coerceInputValues = true
-        ignoreUnknownKeys = true
-    }
-}
+abstract class MangaCorporation : PizzaReader()
