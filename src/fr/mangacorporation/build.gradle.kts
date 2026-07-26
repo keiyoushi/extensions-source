e7e08d954ce62da13900cfb577dc2d122757c04b
furaibo9714 <306338673+furaibo9714@users.noreply.github.com>
2026-07-26 08:43:35 +0200

    PizzaReader multisrc: extlib 1.6 (#17888)
    * PizzaReader multisrc: extlib 1.6
    
    * review

--- src/fr/mangacorporation/build.gradle.kts
+++ src/fr/mangacorporation/build.gradle.kts
@@ -8,7 +8,7 @@ keiyoushi {
     name = "Manga-Corporation"
     versionCode = 1
     contentWarning = ContentWarning.SAFE
-    libVersion = "1.4"
+    libVersion = "1.6"
     theme = "pizzareader"
 
     source {
