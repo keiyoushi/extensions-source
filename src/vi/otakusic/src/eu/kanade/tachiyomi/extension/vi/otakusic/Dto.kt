8ec50438c7775a06f57c696c75acf57989e99162
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-07-26 13:44:06 +0700

    Otakusic: Add deeplink, lib 1.6 (#17930)

--- src/vi/otakusic/src/eu/kanade/tachiyomi/extension/vi/otakusic/Dto.kt
+++ src/vi/otakusic/src/eu/kanade/tachiyomi/extension/vi/otakusic/Dto.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.json.JsonPrimitive
 
 @Serializable
 class ChaptersResponse(
-    val data: List<ChapterDto> = emptyList(),
+    val data: List<ChapterDto>,
 )
 
 @Serializable
@@ -14,10 +14,31 @@ class ChapterDto(
     @SerialName("chapter_name") val chapterName: JsonPrimitive,
     @SerialName("chapter_slug") val chapterSlug: String,
     @SerialName("chapter_original_slug") val chapterOriginalSlug: String,
-    @SerialName("manga_slug") val mangaSlug: String,
-    @SerialName("is_locked") val isLocked: Boolean = false,
     @SerialName("api_url") val apiUrl: String? = null,
     @SerialName("updated_at") val updatedAt: String? = null,
     @SerialName("public_at") val publicAt: String? = null,
     val status: String? = null,
+)
+
+@Serializable
+class ChapterPagesResponse(
+    val data: ChapterPagesData,
+)
+
+@Serializable
+class ChapterPagesData(
+    @SerialName("domain_cdn") val domainCdn: String,
+    val item: ChapterPagesItem,
+)
+
+@Serializable
+class ChapterPagesItem(
+    @SerialName("chapter_path") val chapterPath: String,
+    @SerialName("chapter_image") val chapterImages: List<ChapterImage>,
+)
+
+@Serializable
+class ChapterImage(
+    @SerialName("image_page") val page: Int,
+    @SerialName("image_file") val file: String,
 )
