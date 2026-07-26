f6abaf7c1ea2c30a58645dea58dea3333fba5b25
Yu <yu.development@gmail.com>
2026-07-26 14:04:33 +0700

    Update fetchMangaUpdate to parse chapters unconditionally

--- src/vi/vinahentai/src/eu/kanade/tachiyomi/extension/vi/vinahentai/VinaHentai.kt
+++ src/vi/vinahentai/src/eu/kanade/tachiyomi/extension/vi/vinahentai/VinaHentai.kt
@@ -201,7 +201,7 @@ abstract class VinaHentai : KeiSource() {
 
         return SMangaUpdate(
             manga = parseMangaDetails(document, manga),
-            chapters = if (fetchChapters) fetchChapters(document) else chapters,
+            chapters = fetchChapters(document),
         )
     }
 
