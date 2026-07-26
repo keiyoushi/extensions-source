2d69fdf4b1d4acc0dff7ce9af51d87ed9b89aaa5
Fermín Cirella <fermincirella.ar@gmail.com>
2026-07-26 04:44:54 -0300

    HentaiNexus: Add preference for image quality + ext-lib 1.6 (#17943)
    * HentaiNexus: Add preference for image quality
    
    * HentaiNexus: Improve image quality handling
    
    * HentaiNexus: Migrate to ext-lib 1.6
    
    * HentaiNexus: Apply feedback

--- src/en/hentainexus/build.gradle.kts
+++ src/en/hentainexus/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 
 keiyoushi {
     name = "HentaiNexus"
-    versionCode = 18
+    versionCode = 19
     contentWarning = ContentWarning.NSFW
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "en"
