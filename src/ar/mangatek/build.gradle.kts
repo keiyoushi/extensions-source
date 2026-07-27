70049aa7ff8cb256eb0876c7244dbece98b21707
xMohnad <mohnad00967@gmail.com>
2026-07-27 23:48:48 +0300

    MangaTek: 1.6

--- src/ar/mangatek/build.gradle.kts
+++ src/ar/mangatek/build.gradle.kts
@@ -8,10 +8,15 @@ keiyoushi {
     name = "MangaTek"
     versionCode = 3
     contentWarning = ContentWarning.SAFE
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "ar"
         baseUrl = "https://mangatek.com"
+    }
+
+    deeplink {
+        path("/manga/..*")
+        path("/reader/..*")
     }
 }
