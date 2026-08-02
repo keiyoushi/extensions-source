0047c87423c33d9fdc65cb48cf811465e8118513
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-08-02 20:19:34 +0500

    fix

--- src/all/mangacrazy/src/eu/kanade/tachiyomi/extension/all/mangacrazy/MangaCrazy.kt
+++ src/all/mangacrazy/src/eu/kanade/tachiyomi/extension/all/mangacrazy/MangaCrazy.kt
@@ -1,9 +1,12 @@
 package eu.kanade.tachiyomi.extension.all.mangacrazy
 
-import eu.kanade.tachiyomi.multisrc.madara.MadaraNoAjax
+import eu.kanade.tachiyomi.multisrc.madara.Madara
 import keiyoushi.annotation.Source
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @Source
-abstract class MangaCrazy : MadaraNoAjax() {
+abstract class MangaCrazy : Madara() {
     override val chapterMode = ChapterMode.MangaAjax
+    override val chapterDateFormat = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale.ENGLISH)
 }
