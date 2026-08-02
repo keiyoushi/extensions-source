83e18da9a1cc1f3cfebb6b341447281b8181492c
furaibo9714 <306338673+furaibo9714@users.noreply.github.com>
2026-08-02 15:10:11 +0200

    VerComics: extlib 1.6 (#18116)
    * VerComics: extlib 1.6
    
    * fetchDetails

--- src/es/chochox/build.gradle.kts
+++ src/es/chochox/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 
 keiyoushi {
     name = "ChoChoX"
-    versionCode = 0
+    versionCode = 1
     contentWarning = ContentWarning.NSFW
-    libVersion = "1.4"
+    libVersion = "1.6"
     theme = "vercomics"
 
     source {
