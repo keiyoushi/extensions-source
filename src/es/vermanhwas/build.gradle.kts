a18ac233a74d64c150a9d08360ae35a2f8944bcf
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-08-02 20:49:24 +0500

    migrate

--- src/es/vermanhwas/build.gradle.kts
+++ src/es/vermanhwas/build.gradle.kts
@@ -8,7 +8,7 @@ keiyoushi {
     name = "Ver Manhwas"
     versionCode = 2
     contentWarning = ContentWarning.NSFW
-    libVersion = "1.4"
+    libVersion = "1.6"
     theme = "madara"
 
     source {
