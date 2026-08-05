cd1da1f8ec512807368a7d340bad20394c225b31
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-08-05 11:16:28 +0500

    Apply suggestions from code review
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/en/alphamanga/src/eu/kanade/tachiyomi/extension/en/alphamanga/AlphaManga.kt
+++ src/en/alphamanga/src/eu/kanade/tachiyomi/extension/en/alphamanga/AlphaManga.kt
@@ -90,12 +90,12 @@ abstract class AlphaManga :
     }
 
     // load desktop selectors
-    private val desktopHeaders = headersBuilder()
+    private val desktopHeaders get() = headersBuilder()
         .set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36")
         .build()
 
     // force mobile ua for high resolution images
-    private val mobileHeaders = headersBuilder()
+    private val mobileHeaders get() = headersBuilder()
         .set("User-Agent", "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Mobile Safari/537.36")
         .build()
 
