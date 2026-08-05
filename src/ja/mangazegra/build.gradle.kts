409013b0fbaead99e801c36c2d2142cd8f841970
manti <133025162+manti-X@users.noreply.github.com>
2026-08-05 20:09:20 +0200

    a

--- src/ja/mangazegra/build.gradle.kts
+++ src/ja/mangazegra/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 keiyoushi {
     name = "Manga Zegra"
-    versionCode = 1
+    versionCode = 0
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
     theme = "comiciviewer"
