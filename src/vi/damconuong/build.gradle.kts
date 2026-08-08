ad8a785d264085b59a61bb353bd9591a0feeb221
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-08-08 15:37:55 +0200

    Update domain for 9 extensions

--- src/vi/damconuong/build.gradle.kts
+++ src/vi/damconuong/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 
 keiyoushi {
     name = "DamCoNuong"
-    versionCode = 8
+    versionCode = 9
     contentWarning = ContentWarning.NSFW
     libVersion = "1.6"
 
     source {
         lang = "vi"
-        baseUrl = "https://damconuong.store"
+        baseUrl = "https://damconuong.sbs"
     }
 
     deeplink {
