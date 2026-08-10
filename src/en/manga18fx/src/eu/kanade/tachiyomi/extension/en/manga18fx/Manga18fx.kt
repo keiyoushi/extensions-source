c45b923495b177ec73b5c61a6c27ed97211557b1
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-08-10 18:46:48 +0200

    Migrate Manga18fx

--- src/en/manga18fx/src/eu/kanade/tachiyomi/extension/en/manga18fx/Manga18fx.kt
+++ src/en/manga18fx/src/eu/kanade/tachiyomi/extension/en/manga18fx/Manga18fx.kt
@@ -1,36 +1,35 @@
 package eu.kanade.tachiyomi.extension.en.manga18fx
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.model.SMangaUpdate
 import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.annotation.Source
+import keiyoushi.network.get
+import keiyoushi.utils.parseAs
+import keiyoushi.utils.toJsonElement
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
 import okhttp3.HttpUrl.Companion.toHttpUrl
-import okhttp3.Request
-import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import org.jsoup.select.Evaluator
-import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 // The site isn't actually based on Madara but reproduces it very well
 @Source
 abstract class Manga18fx : Madara() {
-    override val dateFormat = SimpleDateFormat("dd MMM yy", Locale.ENGLISH)
+    override val chapterDateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM yy", Locale.ENGLISH)
 
-    override val fetchGenres = false
     override val sendViewCount = false
 
-    override fun popularMangaRequest(page: Int) = GET(baseUrl, headers)
-
-    override fun popularMangaParse(response: Response): MangasPage {
-        val document = response.asJsoup()
-        loadGenres(document)
+    override suspend fun getPopularManga(page: Int): MangasPage {
+        val document = client.get(baseUrl).asJsoup()
         val block = document.selectFirst(Evaluator.Class("trending-block"))!!
         val mangas = block.select(Evaluator.Tag("a")).map(::mangaFromElement)
         return MangasPage(mangas, false)
@@ -42,11 +41,9 @@ abstract class Manga18fx : Madara() {
         thumbnail_url = element.selectFirst(Evaluator.Tag("img"))!!.attr("data-src")
     }
 
-    override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/page/$page", headers)
+    override suspend fun getLatestUpdates(page: Int): MangasPage = parseLatest(client.get("$baseUrl/page/$page").asJsoup())
 
-    override fun latestUpdatesParse(response: Response): MangasPage {
-        val document = response.asJsoup()
-        loadGenres(document)
+    private fun parseLatest(document: Document): MangasPage {
         val mangas = document.select(Evaluator.Class("bsx-item")).map {
             mangaFromElement(it.selectFirst(Evaluator.Tag("a"))!!)
         }
@@ -55,20 +52,14 @@ abstract class Manga18fx : Madara() {
         return MangasPage(mangas, hasNextPage)
     }
 
-    override fun fetchSearchManga(page: Int, query: String, filters: FilterList) = super.fetchSearchManga(page, query, filters).doOnNext {
-        for (manga in it.mangas) {
-            manga.url = manga.url.removeSuffix("/")
-        }
-    }
-
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+    override suspend fun getSearchMangaList(page: Int, query: String, filters: FilterList): MangasPage {
         if (query.isEmpty()) {
             filters.forEach { filter ->
                 if (filter is GenreFilter) {
-                    return GET(filter.vals[filter.state].id, headers)
+                    return parseLatest(client.get(filter.vals[filter.state].url).asJsoup())
                 }
             }
-            return latestUpdatesRequest(page)
+            return getLatestUpdates(page)
         }
 
         val url = "$baseUrl/search".toHttpUrl().newBuilder()
@@ -76,31 +67,38 @@ abstract class Manga18fx : Madara() {
             .addQueryParameter("page", page.toString())
             .build()
 
-        return GET(url, headers)
+        return parseLatest(client.get(url).asJsoup())
     }
 
-    override fun searchMangaParse(response: Response) = latestUpdatesParse(response)
+    // Site has no Madara post ID
+    override suspend fun fetchMangaUpdate(manga: SManga, chapters: List<SChapter>, fetchDetails: Boolean, fetchChapters: Boolean): SMangaUpdate {
+        val document = client.get(baseUrl.toHttpUrl().resolve(manga.url)!!).asJsoup()
+        return SMangaUpdate(
+            if (fetchDetails) parseDetails(document, "", manga.url) else manga,
+            if (fetchChapters) fetchChapters(manga.url, "", document) else chapters,
+        )
+    }
 
     override val mangaDetailsSelectorDescription = ".dsct"
 
-    override fun chapterListParse(response: Response): List<SChapter> {
-        val document = response.asJsoup()
-        val container = document.selectFirst(Evaluator.Class("row-content-chapter"))!!
-        return container.children().map(::chapterFromElement)
-    }
+    override fun chapterListSelector() = ".row-content-chapter > *"
 
-    override fun chapterDateSelector() = "span.chapter-time"
+    override val chapterDateSelector = "span.chapter-time"
 
     class GenreFilter(val vals: List<Genre>) : Filter.Select<String>("Genre", vals.map { it.name }.toTypedArray())
 
-    private fun loadGenres(document: Document) {
-        genresList = document.select(".header-bottom li a").map {
+    override suspend fun fetchFilterData(): JsonElement {
+        val document = client.get(baseUrl).asJsoup()
+        return document.select(".header-bottom li a").map {
             val href = it.attr("href")
             val url = if (href.startsWith("http")) href else "$baseUrl/$href"
 
             Genre(it.text(), url)
-        }
+        }.toJsonElement()
     }
+
+    @Serializable
+    class Genre(val name: String, val url: String)
 
     private var hardCodedTypes: List<Genre> = listOf(
         Genre("Manhwa", "$baseUrl/manga-genre/manhwa"),
@@ -108,13 +106,15 @@ abstract class Manga18fx : Madara() {
         Genre("Raw", "$baseUrl/manga-genre/raw"),
     )
 
-    override fun getFilterList(): FilterList {
+    override fun getFilterList(data: JsonElement?): FilterList {
+        val genres = data?.parseAs<List<Genre>>()
+
         val filters = buildList(2) {
             add(Filter.Header("Filters are ignored for text search!"))
 
-            if (genresList.isNotEmpty()) {
+            if (!genres.isNullOrEmpty()) {
                 add(
-                    GenreFilter(hardCodedTypes + genresList),
+                    GenreFilter(hardCodedTypes + genres),
                 )
             } else {
                 add(
