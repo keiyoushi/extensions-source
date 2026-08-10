cf9b227b3f60bc7e043bd6ec9f4cf17ae28c6af8
zLuckKing <medeiro33.lm@gmai.com>
2026-08-10 13:08:04 -0300

    MangaLivre: fix formatting

--- src/pt/mangalivre/src/eu/kanade/tachiyomi/extension/pt/mangalivre/MangaLivre.kt
+++ src/pt/mangalivre/src/eu/kanade/tachiyomi/extension/pt/mangalivre/MangaLivre.kt
@@ -24,10 +24,10 @@ import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
+import rx.Observable
 import java.io.IOException
 import java.util.Collections
 import java.util.LinkedHashSet
-import rx.Observable
 import kotlin.time.Duration.Companion.seconds
 
 @Source
