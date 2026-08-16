5c7815f5e2945c35d634b533ecb2fbee7cad8ebf
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-08-16 20:48:55 +0200

    migrate

--- src/ar/detectiveconanar/src/eu/kanade/tachiyomi/extension/ar/detectiveconanar/DetectiveConanAr.kt
+++ src/ar/detectiveconanar/src/eu/kanade/tachiyomi/extension/ar/detectiveconanar/DetectiveConanAr.kt
@@ -2,10 +2,11 @@ package eu.kanade.tachiyomi.extension.ar.detectiveconanar
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import keiyoushi.annotation.Source
-import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 @Source
 abstract class DetectiveConanAr : Madara() {
-    override val dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("ar"))
+    override val chapterDateFormat = DateTimeFormatter.ofPattern("MMMM dd, yyyy", Locale("ar"))
+    override val chapterMode = ChapterMode.MangaAjax
 }
--- src/fr/scanhentaimenu/build.gradle.kts
+++ src/fr/scanhentaimenu/build.gradle.kts
@@ -8,7 +8,7 @@ keiyoushi {
     name = "X-Manga"
     versionCode = 4
     contentWarning = ContentWarning.NSFW
-    libVersion = "1.4"
+    libVersion = "1.6"
     theme = "madara"
 
     source {
