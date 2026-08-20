82f48b6142c5c2fc7d007187b9f49daed4389f16
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-08-20 10:31:38 +0700

    Raw18: Migrate to libVersion 1.6

--- src/ja/raw18/build.gradle.kts
+++ src/ja/raw18/build.gradle.kts
@@ -8,13 +8,13 @@ keiyoushi {
     name = "Raw18"
     versionCode = 8
     contentWarning = ContentWarning.NSFW
-    libVersion = "1.4"
+    libVersion = "1.6"
     theme = "wpcomics"
 
     source {
         lang = "ja"
         baseUrl {
-            custom("https://raw18.wtf")
+            custom("https://raw18.casa")
         }
     }
 }
