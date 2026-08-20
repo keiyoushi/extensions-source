90bd69873e6359cdfa41e51ad2551e93ef7f0d5f
Veloruze <55428245+Veloruze@users.noreply.github.com>
2026-08-20 23:27:59 +0700

    Jmanga: update domain to jmanga.land (#18531)
    Fixes #18306

--- src/ja/jmanga/build.gradle.kts
+++ src/ja/jmanga/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 keiyoushi {
     name = "Jmanga"
-    versionCode = 3
+    versionCode = 4
     contentWarning = ContentWarning.MIXED
     libVersion = "1.4"
     theme = "mangareader"
@@ -14,7 +14,7 @@ keiyoushi {
     source {
         lang = "ja"
         baseUrl {
-            custom("https://jmanga.loan")
+            custom("https://jmanga.land")
         }
     }
 }
