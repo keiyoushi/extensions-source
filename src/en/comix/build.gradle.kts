efde69f3798a1b6f665823c4f73ccd23ab9ae72d
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-08-21 15:47:50 +0500

    Comix: improvements and 1.6 (#18532)
    * Comix: improvements and 1.6
    
    * Update Comix.kt
    
    * reset timeout
    
    * more efficient chapter list fetch in webview
    
    * deduplicate fix for fast path
    
    * fix tags
    
    * randomize

--- src/en/comix/build.gradle.kts
+++ src/en/comix/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 
 keiyoushi {
     name = "Comix"
-    versionCode = 34
+    versionCode = 35
     contentWarning = ContentWarning.MIXED
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "en"
