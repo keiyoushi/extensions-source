70fb57b45ec11cf165d93ba3f5233eed44abb895
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-08-23 15:35:22 +0200

    MangaYi: migrate from 1.4 to 1.6

--- src/en/mangayi/src/eu/kanade/tachiyomi/extension/en/mangayi/MangaYi.kt
+++ src/en/mangayi/src/eu/kanade/tachiyomi/extension/en/mangayi/MangaYi.kt
@@ -1,84 +1,57 @@
 package eu.kanade.tachiyomi.extension.en.mangayi
 
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.online.HttpSource
+import eu.kanade.tachiyomi.source.model.SMangaUpdate
 import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.annotation.Source
+import keiyoushi.network.get
+import keiyoushi.network.post
 import keiyoushi.network.rateLimit
+import keiyoushi.source.KeiSource
 import keiyoushi.utils.parseAs
 import keiyoushi.utils.toJsonRequestBody
-import keiyoushi.utils.tryParse
-import okhttp3.Headers
-import okhttp3.Request
-import okhttp3.Response
-import java.text.SimpleDateFormat
+import keiyoushi.utils.tryParseDate
+import okhttp3.OkHttpClient
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.Locale
-import java.util.TimeZone
 
 @Source
-abstract class MangaYi : HttpSource() {
+abstract class MangaYi : KeiSource() {
 
     override val supportsLatest = false
 
-    override val client = network.client.newBuilder()
-        .rateLimit(2)
-        .build()
-
-    override fun headersBuilder(): Headers.Builder = super.headersBuilder()
-        .add("Referer", "$baseUrl/")
+    override fun OkHttpClient.Builder.configureClient() = rateLimit(2)
 
     // ============================== Popular ==============================
 
-    override fun popularMangaRequest(page: Int): Request {
+    override suspend fun getPopularManga(page: Int): MangasPage {
         val payload = SearchRequestDto(t = 1)
-        return POST(
-            url = "$baseUrl/api/search",
-            headers = headers.newBuilder()
-                .add("Accept", "application/json")
-                .build(),
-            body = payload.toJsonRequestBody(),
-        )
-    }
-
-    override fun popularMangaParse(response: Response): MangasPage {
         val mangas = response.parseAs<List<MangaDto>>().map { it.toSManga() }
         return MangasPage(mangas, false)
     }
 
     // ============================== Latest ===============================
 
-    override fun latestUpdatesRequest(page: Int): Request = throw UnsupportedOperationException()
-
-    override fun latestUpdatesParse(response: Response): MangasPage = throw UnsupportedOperationException()
+    override suspend fun getLatestUpdates(page: Int): MangasPage = throw UnsupportedOperationException()
 
     // ============================== Search ===============================
 
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+    override suspend fun getSearchMangaList(page: Int, query: String, filters: FilterList): MangasPage {
         val payload = SearchRequestDto(s = query)
-        return POST(
-            url = "$baseUrl/api/search",
-            headers = headers.newBuilder()
-                .add("Accept", "application/json")
-                .build(),
-            body = payload.toJsonRequestBody(),
-        )
+        val mangas = response.parseAs<List<MangaDto>>().map { it.toSManga() }
+        return MangasPage(mangas, false)
     }
 
-    override fun searchMangaParse(response: Response): MangasPage = popularMangaParse(response)
+    // ============================== Updates ==============================
 
-    // ============================== Details ==============================
-
-    override fun mangaDetailsRequest(manga: SManga): Request = GET("$baseUrl/read/${manga.url}/", headers)
-
-    override fun mangaDetailsParse(response: Response): SManga {
-        val document = response.asJsoup()
-        return SManga.create().apply {
+    override suspend fun fetchMangaUpdate(manga: SManga, chapters: List<SChapter>, fetchDetails: Boolean, fetchChapters: Boolean): SMangaUpdate {
+        val document = client.get("$baseUrl/read/${manga.url}/").asJsoup()
+        val manga2 = SManga.create().apply {
             title = document.selectFirst("h1.title")!!.text()
             author = document.selectFirst(".authors")?.text()
             description = document.select(".summary p").joinToString("\n") { it.text() }
@@ -86,6 +59,17 @@ abstract class MangaYi : HttpSource() {
             status = document.selectFirst(".stat:contains(Status) .value")?.text().parseStatus()
             thumbnail_url = document.selectFirst(".cover-wrapper img.cover-image")?.attr("abs:src")
         }
+
+        val chapters = document.select("div.chapters a.c:not(.unreleased)")
+        val chapters2 = chapters.map { element ->
+            SChapter.create().apply {
+                setUrlWithoutDomain(element.absUrl("href"))
+                name = element.selectFirst(".t")!!.text()
+                date_upload = dateFormat.tryParseDate(element.selectFirst(".chapter-date")?.text())
+            }
+        }
+
+        return SMangaUpdate(manga2, chapters2)
     }
 
     private fun String?.parseStatus(): Int = when (this?.lowercase()) {
@@ -96,41 +80,18 @@ abstract class MangaYi : HttpSource() {
         else -> SManga.UNKNOWN
     }
 
-    // ============================= Chapters ==============================
-
-    override fun chapterListRequest(manga: SManga): Request = mangaDetailsRequest(manga)
-
-    override fun chapterListParse(response: Response): List<SChapter> {
-        val document = response.asJsoup()
-        val chapters = document.select("div.chapters a.c:not(.unreleased)")
-
-        if (chapters.isEmpty()) return emptyList()
-
-        return chapters.map { element ->
-            SChapter.create().apply {
-                setUrlWithoutDomain(element.absUrl("href"))
-                name = element.selectFirst(".t")!!.text()
-                date_upload = dateFormat.tryParse(element.selectFirst(".chapter-date")?.text())
-            }
-        }
-    }
-
     // =============================== Pages ===============================
 
-    override fun pageListParse(response: Response): List<Page> {
-        val document = response.asJsoup()
+    override suspend fun getPageList(chapter: SChapter): List<Page> {
+        val document = client.get(getChapterUrl(chapter)).asJsoup()
         return document.select("div.images img").mapIndexed { index, img ->
             Page(index, imageUrl = img.attr("abs:src"))
         }
     }
 
-    override fun imageUrlParse(response: Response): String = throw UnsupportedOperationException()
-
     // ============================= Utilities =============================
 
     companion object {
-        private val dateFormat = SimpleDateFormat("d MMMM yyyy", Locale.ENGLISH).apply {
-            timeZone = TimeZone.getTimeZone("UTC")
-        }
+        private val dateFormat = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH).withZone(ZoneId.of("UTC"))
     }
 }
