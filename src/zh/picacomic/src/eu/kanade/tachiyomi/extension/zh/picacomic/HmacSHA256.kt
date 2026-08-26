5c3c0828583a983f51ee7a6aab191dbc5521181b
disk-iq8 <grinersam14@gmail.com>
2026-08-26 15:45:08 +0300

    Picacomic (ZH): Fix auth and webview urls + 1.6 (#18640)
    * Fix auth and 1.6
    
    * Clean prefs
    
    ---------
    
    Co-authored-by: iq8 <266871934+disk-iq8@users.noreply.github.com>

--- src/zh/picacomic/src/eu/kanade/tachiyomi/extension/zh/picacomic/HmacSHA256.kt
+++ src/zh/picacomic/src/eu/kanade/tachiyomi/extension/zh/picacomic/HmacSHA256.kt
@@ -6,19 +6,16 @@ import javax.crypto.spec.SecretKeySpec
 // copy from https://github.com/czp3009/picacomic-api
 private const val ALGORITHM = "HmacSHA256"
 
-private typealias MacResult = ByteArray
-
 internal fun hmacSHA256(key: String, data: String) = Mac.getInstance(ALGORITHM).apply {
-    init(SecretKeySpec(key.toByteArray(), algorithm))
-}.doFinal(data.toByteArray()) as MacResult
+    init(SecretKeySpec(key.toByteArray(), ALGORITHM))
+}.doFinal(data.toByteArray())
 
 @Suppress("SpellCheckingInspection")
 private val hexTable = "0123456789abcdef".toCharArray()
 
-@OptIn(ExperimentalUnsignedTypes::class)
-internal fun MacResult.convertToString() = buildString(size * 2) {
+internal fun ByteArray.convertToString() = buildString(size * 2) {
     this@convertToString.forEach {
-        val value = it.toUByte().toInt()
+        val value = it.toInt() and 0xff
         append(hexTable[value ushr 4])
         append(hexTable[value and 0x0f])
     }
