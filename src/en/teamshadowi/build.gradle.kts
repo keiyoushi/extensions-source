b8ce931ea42cde80fecb6ee0439a7c78642d7195
Secozzi <secozzi@proton.me>
2026-08-28 19:35:09 +0200

    Migrate TeamShadowi to 1.6

--- src/en/teamshadowi/build.gradle.kts
+++ src/en/teamshadowi/build.gradle.kts
@@ -6,12 +6,16 @@ plugins {
 
 keiyoushi {
     name = "Team Shadowi"
-    versionCode = 1
+    versionCode = 2
     contentWarning = ContentWarning.MIXED
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "en"
         baseUrl = "https://www.team-shadowi.com"
+    }
+
+    deeplink {
+        path("/series/..*")
     }
 }
