332f63bda3f722dbe8fe53ec282c817586bc0c2d
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-08-29 15:05:47 +0500

    Ortega Scans 1.6 (#18686)
    * update
    
    * bump
    
    * http url

--- src/fr/ortegascans/build.gradle.kts
+++ src/fr/ortegascans/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 
 keiyoushi {
     name = "Ortega Scans"
-    versionCode = 2
+    versionCode = 3
     contentWarning = ContentWarning.NSFW
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "fr"
