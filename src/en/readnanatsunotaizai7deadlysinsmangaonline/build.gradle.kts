3b901effef18852833e164b3519fe84456da31ef
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-08-29 12:06:49 +0200

    Update domain for 4 extensions (#18694)

--- src/en/readnanatsunotaizai7deadlysinsmangaonline/build.gradle.kts
+++ src/en/readnanatsunotaizai7deadlysinsmangaonline/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 
 keiyoushi {
     name = "Read Nanatsu no Taizai 7 Deadly Sins Manga Online"
-    versionCode = 3
+    versionCode = 4
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
     theme = "mangacatalog"
 
     source {
         lang = "en"
-        baseUrl = "https://ww7.read7deadlysins.com"
+        baseUrl = "https://ww8.read7deadlysins.com"
     }
 }
