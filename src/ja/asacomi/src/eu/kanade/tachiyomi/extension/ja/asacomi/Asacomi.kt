cc1458239eee79d87393fcbfdfbcac7f5b4d652b
manti <133025162+manti-X@users.noreply.github.com>
2026-08-29 21:41:40 +0200

    Add Asacomi

---
+++ src/ja/asacomi/src/eu/kanade/tachiyomi/extension/ja/asacomi/Asacomi.kt
@@ -0,0 +1,20 @@
+package eu.kanade.tachiyomi.extension.ja.asacomi
+
+import eu.kanade.tachiyomi.multisrc.comiciviewer.ComiciViewer
+import eu.kanade.tachiyomi.network.GET
+import keiyoushi.annotation.Source
+import okhttp3.Request
+
+@Source
+abstract class Asacomi : ComiciViewer() {
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/category/manga/$page", headers)
+
+    override fun getFilterOptions(): List<Pair<String, String>> = listOf(
+        Pair("ランキング", "/ranking/manga"),
+        Pair("更新順", "/series/list/up"),
+        Pair("新作順", "/series/list/new"),
+        Pair("読み切り", "/category/manga/oneShot"),
+        Pair("完結", "/category/manga/complete"),
+        Pair("その他", "/category/manga/day/8"),
+    )
+}
