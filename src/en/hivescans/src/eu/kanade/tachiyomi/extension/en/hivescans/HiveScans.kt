f16e4224cd9af2bcf1826db2849e37357f818e51
Prem Kumar <60751338+prem-k-r@users.noreply.github.com>
2026-09-02 17:02:36 +0530

    Iken: Remove false scanlator (#18787)
    Iken: remove false scanlator

--- src/en/hivescans/src/eu/kanade/tachiyomi/extension/en/hivescans/HiveScans.kt
+++ src/en/hivescans/src/eu/kanade/tachiyomi/extension/en/hivescans/HiveScans.kt
@@ -3,9 +3,6 @@ package eu.kanade.tachiyomi.extension.en.hivescans
 import eu.kanade.tachiyomi.multisrc.iken.Iken
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Page
-import eu.kanade.tachiyomi.source.model.SChapter
-import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.model.SMangaUpdate
 import keiyoushi.annotation.Source
 import okhttp3.CacheControl
 import okhttp3.Request
@@ -18,13 +15,4 @@ abstract class HiveScans : Iken() {
         headers,
         CacheControl.Builder().noCache().build(),
     )
-
-    override suspend fun fetchMangaUpdate(
-        manga: SManga,
-        chapters: List<SChapter>,
-        fetchDetails: Boolean,
-        fetchChapters: Boolean,
-    ): SMangaUpdate = super.fetchMangaUpdate(manga, chapters, fetchDetails, fetchChapters).apply {
-        this.chapters.forEach { it.scanlator = null }
-    }
 }
