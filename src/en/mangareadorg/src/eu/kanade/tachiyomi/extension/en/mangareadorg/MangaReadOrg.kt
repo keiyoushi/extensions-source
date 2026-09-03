1c60711e5ae33b97e6a958cd72dcfdeebdc8a85e
SEK171 <SEK171PRO@gmail.com>
2026-09-03 01:00:02 +0100

    Fix: Update MangaReadOrg chapterMode to MangaAjax

--- src/en/mangareadorg/src/eu/kanade/tachiyomi/extension/en/mangareadorg/MangaReadOrg.kt
+++ src/en/mangareadorg/src/eu/kanade/tachiyomi/extension/en/mangareadorg/MangaReadOrg.kt
@@ -8,4 +8,8 @@ import java.util.Locale
 @Source
 abstract class MangaReadOrg : Madara() {
     override val chapterDateFormat = DateTimeFormatter.ofPattern("dd.MM.yyy", Locale.US)
+
+    // Switch to the new JS-based AJAX endpoint because
+    // the raw HTML doesn't contain the chapters initially
+    override val chapterMode = ChapterMode.MangaAjax
 }
