b08e469e2a065e06994878b5c5b8ff2c8f1de662
Chopper <156493704+choppeh@users.noreply.github.com>
2026-09-04 01:29:10 -0300

    Bump version

--- src/pt/xxxyaoi/build.gradle.kts
+++ src/pt/xxxyaoi/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 keiyoushi {
     name = "XXX Yaoi"
-    versionCode = 2
+    versionCode = 3
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "madaralegacy"
