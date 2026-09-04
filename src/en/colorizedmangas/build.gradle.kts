71182cd563894ea554f51136f766d5915cf2f35c
glbskmnh <pekatour.bin@gmail.com>
2026-09-05 01:52:51 +0200

    Colorized Mangas source

---
+++ src/en/colorizedmangas/build.gradle.kts
@@ -0,0 +1,21 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
+
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "Colorized Mangas"
+    versionCode = 1
+    contentWarning = ContentWarning.SAFE
+    libVersion = "1.6"
+
+    source {
+        baseUrl = "https://colorizedmangas.com"
+        lang = "en"
+    }
+
+    deeplink {
+        path("/..*")
+    }
+}
