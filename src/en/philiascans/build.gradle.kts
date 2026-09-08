7a3bd485f08b9dda0a9d0ab11c96634177f08bce
Eradev <admin@eradev.com>
2026-09-08 03:55:51 -0400

    [Philia Scans] 1.6 Migration

--- src/en/philiascans/build.gradle.kts
+++ src/en/philiascans/build.gradle.kts
@@ -8,7 +8,7 @@ keiyoushi {
     name = "Philia Scans"
     versionCode = 60
     contentWarning = ContentWarning.SAFE
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "en"
