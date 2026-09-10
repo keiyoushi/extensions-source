162bd574fe50909eacc6e574479f92fad79bf82e
Stysusss <158248053+stysus@users.noreply.github.com>
2026-09-11 04:18:40 +0700

    Komiku: migrate to 1.6

--- src/id/komiku/build.gradle.kts
+++ src/id/komiku/build.gradle.kts
@@ -6,12 +6,16 @@ plugins {
 
 keiyoushi {
     name = "Komiku"
-    versionCode = 21
+    versionCode = 1
     contentWarning = ContentWarning.SAFE
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "id"
         baseUrl = "https://komiku.org"
+    }
+
+    deeplink {
+        path("/manga/..*")
     }
 }
