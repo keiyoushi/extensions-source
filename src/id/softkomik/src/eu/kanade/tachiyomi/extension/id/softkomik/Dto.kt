a60b5f8d14ba0aaf4f4a07f500035b4651bb85b1
Romain <wycvhrt6vzscfpedxr@gmail.com>
2026-09-11 08:11:35 +0200

    fix(id/softkomik): update API host and endpoints (#18865)
    * fix(id/softkomik): update API host and endpoints
    
    Also widen the chapter page data match predicate to handle cases where imageSrc is omitted.
    
    Fixes #18618, fixes #18272, fixes #17894
    
    * feat: page fix, lib1.6, dto field names, versionCode reset
    
    * perf: drop lazy
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- src/id/softkomik/src/eu/kanade/tachiyomi/extension/id/softkomik/Dto.kt
+++ src/id/softkomik/src/eu/kanade/tachiyomi/extension/id/softkomik/Dto.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.extension.id.softkomik
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -13,7 +14,7 @@ data class LibDataDto(
 data class MangaDto(
     val gambar: String,
     val title: String,
-    val title_slug: String,
+    @SerialName("title_slug") val titleSlug: String,
     val status: String? = null,
     val type: String? = null,
 )
@@ -23,7 +24,7 @@ data class MangaDetailsDto(
     val gambar: String,
     val title: String,
     val author: String? = null,
-    val Genre: List<String>? = emptyList(),
+    @SerialName("Genre") val genre: List<String>? = emptyList(),
     val sinopsis: String? = null,
     val status: String? = null,
     val type: String? = null,
@@ -41,9 +42,11 @@ data class ChapterPageImagesDto(
 
 @Serializable
 data class ChapterPageDataDto(
-    val _id: String,
-    val imageSrc: List<String>,
+    @SerialName("_id") val id: String,
+    val imageSrc: List<String> = emptyList(),
     val storageInter2: Boolean? = false,
+    val backBS3: Boolean? = false,
+    @SerialName("created_at") val createdAt: String,
 )
 
 @Serializable
@@ -56,6 +59,13 @@ data class SessionDto(
     val ex: Long,
     val sign: String,
     val token: String,
+    val contentAccess: ContentAccessDto? = null,
+)
+
+@Serializable
+data class ContentAccessDto(
+    val token: String,
+    val sign: String,
 )
 
 @Serializable
