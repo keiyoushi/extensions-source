5264dfa58036a510e7d6c75c713e618eb920bd16
Chopper <156493704+choppeh@users.noreply.github.com>
2026-09-11 14:00:29 -0300

    Add AcervoEremita

---
+++ src/pt/acervoeremita/build.gradle.kts
@@ -0,0 +1,17 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
+
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "AcervoEremita"
+    versionCode = 1
+    contentWarning = ContentWarning.MIXED
+    libVersion = "1.6"
+
+    source {
+        lang = "pt-BR"
+        baseUrl = "https://acervoeremita.com"
+    }
+}
