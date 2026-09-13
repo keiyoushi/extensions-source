7ffa052623868b4f70b1b7790f961389b4cb62f4
History_exe <33642965+History-exe@users.noreply.github.com>
2026-09-13 22:00:44 +0800

    Fix(zh/favcomic): add auto-login support and fix author parsing

--- src/zh/favcomic/build.gradle.kts
+++ src/zh/favcomic/build.gradle.kts
@@ -6,20 +6,13 @@ plugins {
 
 keiyoushi {
     name = "FavComic"
-    versionCode = 2
+    versionCode = 4
     contentWarning = ContentWarning.MIXED
     libVersion = "1.6"
 
     source {
         name = "喜漫漫画"
         lang = "zh"
-        baseUrl {
-            mirrors(
-                "https://www.favcomic.com",
-                "https://www.favcomic.xyz",
-                "https://www.favcomic.net",
-                "https://www.favcomic.cc",
-            )
-        }
+        baseUrl = "https://www.favcomic.com"
     }
 }
