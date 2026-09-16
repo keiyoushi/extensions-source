9263f37d95df742b7aff7bf48427e9a52544d9c6
Exinaus <exinaus@gmail.com>
2026-09-16 09:01:26 +0300

    AComics (ru): migrate to 1.6 (#19113)
    * AComics migrate to 1.6
    
    * add filter categories
    
    * upd
    
    * cleanup

--- src/ru/acomics/src/eu/kanade/tachiyomi/extension/ru/acomics/Filters.kt
+++ src/ru/acomics/src/eu/kanade/tachiyomi/extension/ru/acomics/Filters.kt
@@ -1,53 +1,51 @@
 package eu.kanade.tachiyomi.extension.ru.acomics
 
 import eu.kanade.tachiyomi.source.model.Filter
-import eu.kanade.tachiyomi.source.model.FilterList
 
-class Genre(name: String, val id: Int) : Filter.CheckBox(name)
-class Rating(name: String, val id: Int) : Filter.CheckBox(name, state = true)
+// ============================== Order ===============================
+abstract class SelectFilter(
+    name: String,
+    private val options: List<Pair<String, String>>,
+    defaultValue: String? = null,
+) : Filter.Select<String>(
+    name,
+    options.map { it.first }.toTypedArray(),
+    options.indexOfFirst { it.second == defaultValue }.takeIf { it != -1 } ?: 0,
+) {
+    val selected get() = options[state].second.takeIf { it.isNotBlank() }
+}
 
-class Categories :
-    Filter.Group<Genre>(
-        "Категории",
-        listOf(
-            Genre("Животные", 1),
-            Genre("Драма", 2),
-            Genre("Фентези", 3),
-            Genre("Игры", 4),
-            Genre("Юмор", 5),
-            Genre("Журнал", 6),
-            Genre("Паранормальное", 7),
-            Genre("Конец света", 8),
-            Genre("Романтика", 9),
-            Genre("Фантастика", 10),
-            Genre("Бытовое", 11),
-            Genre("Стимпанк", 12),
-            Genre("Супергерои", 13),
-            Genre("Детектив", 14),
-            Genre("Историческое", 15),
-        ),
-    )
+// ============================== Multi Value ===============================
+internal class MultiValueOption(name: String, val value: String) : Filter.CheckBox(name)
 
-class Ratings :
-    Filter.Group<Rating>(
-        "Возрастная категория",
-        listOf(
-            Rating("NR", 1),
-            Rating("G", 2),
-            Rating("PG", 3),
-            Rating("PG-13", 4),
-            Rating("R", 5),
-            Rating("NC-17", 6),
-        ),
-    )
+internal abstract class MultiValueFilter(
+    name: String,
+    options: List<Pair<String, String>>,
+) : Filter.Group<MultiValueOption>(
+    name = name,
+    state = options.map { MultiValueOption(it.first, it.second) },
+) {
+    val selected get() = state.filter { it.state }.map { it.value }.takeIf { it.isNotEmpty() }
+}
 
-class ComicType : Filter.Select<String>("Тип комикса", arrayOf("Все", "Оригинальный", "Перевод")) // "0", "orig", "trans"
-class Publication : Filter.Select<String>("Публикация", arrayOf("Все", "Завершенный", "Продолжающийся")) // "0", "no", "yes"
-class Subscription : Filter.Select<String>("Подписка", arrayOf("Все", "В моей ленте", "Кроме моей ленты")) // "0", "yes", "no"
-class MinPages : Filter.Text("Минимум страниц", state = "2")
-class Sort(state: Int = 1) : Filter.Select<String>("Сортировка", arrayOf("по дате обновления", "по количеству подписчиков", "по количеству выпусков", "по алфавиту"), state = state) { // "last_update", "subscr_count", "issue_count", "serial_name"
+// ============================== Filter data ===============================
+internal class OrderBy(data: List<Pair<String, String>>) : SelectFilter("Сортировка", data, "subscr_count")
+internal class ComicType(data: List<Pair<String, String>>) : SelectFilter("Тип комикса", data, "0")
+internal class Publication(data: List<Pair<String, String>>) : SelectFilter("Публикация", data, "0")
+internal class Subscription(data: List<Pair<String, String>>) : SelectFilter("Подписка", data, "0")
+internal class MinPages : Filter.Text("Минимум страниц", state = "2")
+internal class Genres(data: List<Pair<String, String>>) : MultiValueFilter("Категории", data)
+internal class AgeRatings(data: List<Pair<String, String>>) : MultiValueFilter("Возрастная категория", data) {
+    init {
+        state.forEach { filter -> if (filter.name != "NC-17") filter.state = true }
+    }
+}
+internal class Categories : SelectFilter("Разделы поиска", data, "comics") {
     companion object {
-        val LATEST = FilterList(Ratings(), Sort(0))
-        val POPULAR = FilterList(Ratings(), Sort(1))
+        val data = listOf(
+            "Песочница" to "sandbox",
+            "Каталог" to "comics",
+            "Рекомендуемые" to "featured",
+        )
     }
 }
