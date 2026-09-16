2319e0525ad64db3314eb5410ced5511ed344986
manti <133025162+manti-X@users.noreply.github.com>
2026-09-16 08:01:22 +0200

    LineManga: 1.6, optimize page parsing (#19123)
    * LineManga: 1.6
    
    * note
    
    * WeekFields

--- src/ja/linemanga/src/eu/kanade/tachiyomi/extension/ja/linemanga/Filters.kt
+++ src/ja/linemanga/src/eu/kanade/tachiyomi/extension/ja/linemanga/Filters.kt
@@ -2,30 +2,34 @@ package eu.kanade.tachiyomi.extension.ja.linemanga
 
 import eu.kanade.tachiyomi.source.model.Filter
 
+const val DAILY_LIST = "daily_list"
+const val GENDER_RANKING = "periodic/gender_ranking"
+const val GENRE_LIST = "genre_list"
+
 class CategoryFilter :
     SelectFilter(
         "Category",
         arrayOf(
-            Triple("月曜日", "daily_list", "2"),
-            Triple("火曜日", "daily_list", "3"),
-            Triple("水曜日", "daily_list", "4"),
-            Triple("木曜日", "daily_list", "5"),
-            Triple("金曜日", "daily_list", "6"),
-            Triple("土曜日", "daily_list", "7"),
-            Triple("日曜日", "daily_list", "1"),
-            Triple("総合ランキング", "periodic/gender_ranking", "0"),
-            Triple("少年・青年ランキング", "periodic/gender_ranking", "1"),
-            Triple("少女・女性ランキング", "periodic/gender_ranking", "2"),
-            Triple("バトル・アクション", "genre_list", "0001"),
-            Triple("ファンタジー・SF", "genre_list", "0002"),
-            Triple("恋愛", "genre_list", "0003"),
-            Triple("スポーツ", "genre_list", "0004"),
-            Triple("ミステリー・ホラー", "genre_list", "0005"),
-            Triple("裏社会・アングラ", "genre_list", "0006"),
-            Triple("ヒューマンドラマ", "genre_list", "0007"),
-            Triple("歴史・時代", "genre_list", "0008"),
-            Triple("コメディ・ギャグ", "genre_list", "0009"),
-            Triple("その他", "genre_list", "ffff"),
+            Triple("月曜日", DAILY_LIST, "2"),
+            Triple("火曜日", DAILY_LIST, "3"),
+            Triple("水曜日", DAILY_LIST, "4"),
+            Triple("木曜日", DAILY_LIST, "5"),
+            Triple("金曜日", DAILY_LIST, "6"),
+            Triple("土曜日", DAILY_LIST, "7"),
+            Triple("日曜日", DAILY_LIST, "1"),
+            Triple("総合ランキング", GENDER_RANKING, "0"),
+            Triple("少年・青年ランキング", GENDER_RANKING, "1"),
+            Triple("少女・女性ランキング", GENDER_RANKING, "2"),
+            Triple("バトル・アクション", GENRE_LIST, "0001"),
+            Triple("ファンタジー・SF", GENRE_LIST, "0002"),
+            Triple("恋愛", GENRE_LIST, "0003"),
+            Triple("スポーツ", GENRE_LIST, "0004"),
+            Triple("ミステリー・ホラー", GENRE_LIST, "0005"),
+            Triple("裏社会・アングラ", GENRE_LIST, "0006"),
+            Triple("ヒューマンドラマ", GENRE_LIST, "0007"),
+            Triple("歴史・時代", GENRE_LIST, "0008"),
+            Triple("コメディ・ギャグ", GENRE_LIST, "0009"),
+            Triple("その他", GENRE_LIST, "ffff"),
         ),
     )
 
