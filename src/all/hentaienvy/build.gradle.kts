9de6a8eb88cfece4c3d0715f62451176c8e48c96
disk-iq8 <grinersam14@gmail.com>
2026-09-18 07:50:31 +0300

    HentaiEnvy (Multi): Fix selectors & pageList (#19161)
    HentaiEnvy: Fix selectors & pageList
    
    Same as HentaiZap so merge and move pageList to theme
    
    Co-authored-by: iq8 <266871934+disk-iq8@users.noreply.github.com>

--- src/all/hentaienvy/build.gradle.kts
+++ src/all/hentaienvy/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 keiyoushi {
     name = "HentaiEnvy"
-    versionCode = 0
+    versionCode = 1
     contentWarning = ContentWarning.NSFW
     libVersion = "1.6"
     theme = "galleryadults"
