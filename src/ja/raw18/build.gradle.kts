2215d63f5f08a9615a3356adce272d66f65d3496
Stysusss <158248053+stysus@users.noreply.github.com>
2026-09-18 22:01:49 +0700

    Raw18: Update domain and fix search selector (#19193)
    Raw18: Update domain, fix search selector, and add deeplink

--- src/ja/raw18/build.gradle.kts
+++ src/ja/raw18/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 keiyoushi {
     name = "Raw18"
-    versionCode = 10
+    versionCode = 11
     contentWarning = ContentWarning.NSFW
     libVersion = "1.6"
     theme = "wpcomics"
@@ -14,7 +14,11 @@ keiyoushi {
     source {
         lang = "ja"
         baseUrl {
-            custom("https://raw18.im")
+            custom("https://raw18.icu")
         }
+    }
+
+    deeplink {
+        path("/manga/..*")
     }
 }
