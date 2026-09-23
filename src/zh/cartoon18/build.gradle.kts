db536352cfb0904274cd649e52dd0a9e1d30c5c6
Stysusss <158248053+stysus@users.noreply.github.com>
2026-09-23 21:08:11 +0700

    Cartoon18: 1.6 (#19298)
    * Cartoon18: Migrate to 1.6
    
    * Fix

--- src/zh/cartoon18/build.gradle.kts
+++ src/zh/cartoon18/build.gradle.kts
@@ -6,12 +6,19 @@ plugins {
 
 keiyoushi {
     name = "Cartoon18"
-    versionCode = 4
+    versionCode = 5
     contentWarning = ContentWarning.NSFW
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "zh"
         baseUrl = "https://www.cartoon18.com"
+    }
+
+    deeplink {
+        host("www.cartoon18.com")
+        host("cartoon18.com")
+        path("/v/..*")
+        path("/zh-hans/v/..*")
     }
 }
