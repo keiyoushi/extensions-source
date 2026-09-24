2170cff5d85bb9705ad49f4f790511c21f7bd7c2
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-09-24 09:00:21 +0700

    lint

--- src/vi/moetruyen/src/eu/kanade/tachiyomi/extension/vi/moetruyen/MoeTruyen.kt
+++ src/vi/moetruyen/src/eu/kanade/tachiyomi/extension/vi/moetruyen/MoeTruyen.kt
@@ -308,9 +308,7 @@ abstract class MoeTruyen : KeiSource() {
             }
     }
 
-    private fun isImgxProtected(readerPages: Element): Boolean {
-        return readerPages.attr("data-reader-imgx-access-url").isNotBlank()
-    }
+    private fun isImgxProtected(readerPages: Element): Boolean = readerPages.attr("data-reader-imgx-access-url").isNotBlank()
 
     private suspend fun fetchImgxPages(chapterUrl: String, pageCount: Int): List<Page> {
         if (pageCount <= 0) return emptyList()
