9c1e531221125503f0225a1ae362e1ce2e45daa3
iq8 <266871934+disk-iq8@users.noreply.github.com>
2026-09-24 11:37:08 +0300

    Remove down

--- src/en/pawmanga/src/eu/kanade/tachiyomi/extension/en/pawmanga/PawManga.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.pawmanga
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import keiyoushi.annotation.Source
-
-@Source
-abstract class PawManga : Madara() {
-    override val useNewChapterEndpoint = true
-}
