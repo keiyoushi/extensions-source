a691f90345e79302513af2851e89d141f303efa9
Bartuzen <git@bartuzen.dev>
2026-09-24 09:19:52 +0300

    Add rsc header back

--- src/en/templescan/src/eu/kanade/tachiyomi/extension/en/templescan/TempleScan.kt
+++ src/en/templescan/src/eu/kanade/tachiyomi/extension/en/templescan/TempleScan.kt
@@ -30,6 +30,7 @@ abstract class TempleScan : KeiSource() {
     override fun Headers.Builder.configureHeaders() = apply {
         set("Sec-Fetch-Dest", "document")
         set("Sec-Fetch-Mode", "navigate")
+        set("rsc", "1")
     }
 
     override suspend fun getPopularManga(page: Int) = getSearchMangaList(page, "", OrderFilter.POPULAR)
