dc0fccb4bcb85433581fea6df84286ea76720cb8
Stysusss <158248053+stysus@users.noreply.github.com>
2026-09-25 11:48:12 +0700

    Add Aster Scans

---
+++ src/en/asterscans/src/eu/kanade/tachiyomi/extension/en/asterscans/AsterScans.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.asterscans
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import keiyoushi.annotation.Source
+
+@Source
+abstract class AsterScans : MangaThemesia()
