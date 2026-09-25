10e014a07e50ca507a94904e1bff9062d1ab7adf
Stysusss <158248053+stysus@users.noreply.github.com>
2026-09-26 05:12:44 +0700

    Uzaymanga (multisrc): 1.6

--- src/tr/uzaymanga/build.gradle.kts
+++ src/tr/uzaymanga/build.gradle.kts
@@ -8,7 +8,7 @@ keiyoushi {
     name = "Uzay Manga"
     versionCode = 42
     contentWarning = ContentWarning.SAFE
-    libVersion = "1.4"
+    libVersion = "1.6"
     theme = "uzaymanga"
 
     source {
