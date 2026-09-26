96e71822ce91ec40ec39c84e0b3bcaa4acbe350e
Exinaus <exinaus@gmail.com>
2026-09-26 15:46:11 +0300

    Mangarama (uk): fix images + new filters (#19353)
    * fix and upd
    
    * bump

--- src/uk/mangarama/build.gradle.kts
+++ src/uk/mangarama/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 keiyoushi {
     name = "Mangarama"
     pkgName = "uk.pureskill"
-    versionCode = 0
+    versionCode = 1
     contentWarning = ContentWarning.SAFE
     libVersion = "1.6"
     theme = "madara"
