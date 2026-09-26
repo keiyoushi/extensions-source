5a948c4dd9b03ddb4b5c5279dead75f3d5b6dca4
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-09-26 17:49:57 +0500

    likemanga: 1.6 (#19349)

--- src/en/likemanga/build.gradle.kts
+++ src/en/likemanga/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 
 keiyoushi {
     name = "LikeManga"
-    versionCode = 8
+    versionCode = 0
     contentWarning = ContentWarning.SAFE
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "en"
