f5c98bfd91d780a03c0b393f84bf78b6eae04077
FourTOne5 <107297513+FourTOne5@users.noreply.github.com>
2026-09-26 09:54:18 +0600

    Love4u (ja.welovemangaone)

--- src/ja/welovemangaone/build.gradle.kts
+++ src/ja/welovemangaone/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 
 keiyoushi {
     name = "Love4u"
-    versionCode = 2
+    versionCode = 0
     contentWarning = ContentWarning.MIXED
-    libVersion = "1.4"
+    libVersion = "1.6"
     theme = "fmreader"
 
     source {
