e1c6dc3daf64e8de669e80a5a91711d7dc03a434
Vetle Ledaal <vetle.ledaal@gmail.com>
2026-09-26 14:45:48 +0200

    InManga: migrate to 1.6 (#19333)
    * InManga: migrate to 1.6
    
    * 1.6isms
    
    * deeplink support - manga and chapter
    
    * remove custom POST headers

--- src/es/inmanga/build.gradle.kts
+++ src/es/inmanga/build.gradle.kts
@@ -6,12 +6,16 @@ plugins {
 
 keiyoushi {
     name = "InManga"
-    versionCode = 4
+    versionCode = 1
     contentWarning = ContentWarning.SAFE
-    libVersion = "1.4"
+    libVersion = "1.6"
 
     source {
         lang = "es"
         baseUrl = "https://inmanga.com"
+    }
+
+    deeplink {
+        path("/ver/manga/..*")
     }
 }
